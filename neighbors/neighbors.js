cb443b45012cdeab977761166242cbd3ee9a4344
Sed458 <sed458@nyu.edu>
2020-06-10 15:55:27 -0400

    added in_front function and function descriptions

--- neighbors/neighbors.js
+++ neighbors/neighbors.js
@@ -1,44 +1,69 @@
 function on_position(a, neighbors) {
+  /** Returns all neighbors that share an agent's position */
   if (!a.position) {
     throw new Error("agent must have position");
   }
   on_pos = [];
   neighbors.forEach((neighbor) => {
-    if (a.position.length === neighbor.position.length) {
-      eq_flag = true;
-      for (let i = 0; i < a.position.length; i++) {
-        if (a.position[i] != neighbor.position[i]) {
-          eq_flag = false;
-        }
-      }
-      if (eq_flag) {
-        on_pos.push(neighbor);
-      }
+    if (
+      a.position[0] === neighbor.position[0] &&
+      a.position[1] === neighbor.position[1] &&
+      a.position[2] === neighbor.position[2]
+    ) {
+      on_pos.push(neighbor);
     }
   })
+
   return on_pos;
 }
 
 function in_radius(a, neighbors, radius = 1) {
+  /** Returns all neighbors within a certain vision radius of an agent.
+  *   Defaults vision radius to 1
+  */
   if (!a.position) {
     throw new Error("agent must have position");
   }
   x_bounds = [a.position[0] + radius, a.position[0] - radius];
   y_bounds = [a.position[1] + radius, a.position[1] - radius];
+  z_bounds = [a.position[2] + radius, a.position[2] - radius];
 
   in_rad = []
-
   neighbors.forEach((neighbor) => {
     if (
       neighbor.position[0] <= x_bounds[0] &&
       neighbor.position[0] >= x_bounds[1] &&
       neighbor.position[1] <= y_bounds[0] &&
-      neighbor.position[1] >= y_bounds[1]
+      neighbor.position[1] >= y_bounds[1] &&
+      neighbor.position[2] <= z_bounds[0] &&
+      neighbor.position[2] >= z_bounds[1]
     ) {
       in_rad.push(neighbor);
     }
   })
+
   return in_rad;
 }
 
-module.exports = { on_position, in_radius }
+function in_front(a, neighbors) {
+  /** Searches and returns all neighbors whose position is in front of an agent*/
+  if (!a.position) {
+    throw new Error("agent must have position");
+  }
+
+  n_front = []
+  neighbors.forEach((neighbor) => {
+    if (
+      neighbor.position[0] >= a.position[0] &&
+      neighbor.position[1] >= a.position[1] &&
+      neighbor.position[0] !== a.position[0] &&
+      neighbor.position[1] !== a.position[1]
+    ) {
+      n_front.push(neighbor);
+    }
+  })
+
+  return n_front;
+}
+
+module.exports = { on_position, in_radius, in_front }
--- neighbors/neighbors.test.js
+++ neighbors/neighbors.test.js
@@ -1,19 +1,24 @@
-const { on_position, in_radius } = require('./neighbors.js');
+const { on_position, in_radius, in_front } = require('./neighbors.js');
 
-const a = {"position": [1,1]}
-const b = {"position": [1,2]}
-const c = {"position": [-1,1]}
-const d = {"position": [1,1]}
-const e = {"position": [1,1]}
-const f = {"position": [3,2]}
-const g = {"position": [6,7]}
-const h = {"position": [6,9]}
-const i = {"position": [4,9]}
+const a = {"position": [1,1,0]}
+const b = {"position": [1,2,0]}
+const c = {"position": [-1,1,0]}
+const d = {"position": [1,1,0]}
+const e = {"position": [1,1,1]}
+const f = {"position": [3,2,0]}
+const g = {"position": [6,7,0]}
+const h = {"position": [6,9,0]}
+const i = {"position": [4,9,0]}
+const j = {"position": [3,2,0]}
 
 test('find neighbors with same position', () => {
-  expect(on_position(a, [b, c, d, e, f])).toEqual([{"position": [1,1]}, {"position": [1,1]}])
+  expect(on_position(a, [b, c, d, e, f])).toEqual([{"position": [1,1,0]}])
 })
 
 test('find neighbors within a radius of 2', () => {
-  expect(in_radius(g, [a, b, c, d, e, f, h, i], 2)).toEqual([{"position": [6,9]}, {"position": [4,9]}])
+  expect(in_radius(g, [a, b, c, d, e, f, h, i], 2)).toEqual([{"position": [6,9,0]}, {"position": [4,9,0]}])
+})
+
+test('find neighbors in front of [3,2]', () => {
+  expect(in_front(f, [a, b, c, e, g, h, i, j])).toEqual([{"position": [6,7,0]}, {"position": [6,9,0]}, {"position": [4,9,0]}])
 })
