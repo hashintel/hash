3c61768efd2690f664f773d92a624a2f63b33b36
Ciaran Morinan <cm@sohostrategy.com>
2021-05-27 19:17:07 +0100

    delete unnecessary files

--- packages/hash/backend/loader.js
+++
@@ -1,8 +0,0 @@
-const { buildSchema } = require('graphql');
-const { readFileSync } = require('fs');
-
-module.exports = function(schemaString, config) {
-  // Your logic for loading your GraphQLSchema
-  console.log({ schemaString, config })
-  return buildSchema(readFileSync(schemaString, { encoding: 'utf-8' }));
-};
