dbc369bb32155c596e6229c4bf7a85864eaa9beb
Ciaran Morinan <cm@sohostrategy.com>
2021-07-01 17:37:37 +0100

    Create page feature. Add Tailwind (to test it out)

---
+++ packages/hash/frontend/src/components/Modals/CreatePage/CreatePage.tsx
@@ -0,0 +1,52 @@
+import { FormEvent, useState, VoidFunctionComponent } from "react";
+import { useCreatePage } from "../../hooks/useCreatePage";
+import { Modal } from "../Modal";
+
+import styles from "./CreatePage.module.scss";
+
+type CreatePageProps = {
+  close: () => void;
+  namespaceId: string;
+  show: boolean;
+};
+
+export const CreatePage: VoidFunctionComponent<CreatePageProps> = ({
+  close,
+  namespaceId,
+  show,
+}) => {
+  const [title, setTitle] = useState("");
+
+  const { create } = useCreatePage();
+
+  const createPage = (event: FormEvent) => {
+    event.preventDefault();
+    create({
+      variables: { namespaceId, properties: { title } },
+    });
+  };
+
+  return (
+    <Modal show={show} close={close}>
+      <form className={styles.CreatePage} onSubmit={createPage}>
+        <h2>
+          Don't be afraid of a blank page...
+        </h2>
+
+        <label>Title</label>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="What is this document?"
+          required
+          type="text"
+          value={title}
+        />
+
+        <button className="button big" type="submit">
+          Create
+        </button>
+
+      </form>
+    </Modal>
+  );
+};
--- packages/hash/frontend/src/pages/_app.tsx
+++ packages/hash/frontend/src/pages/_app.tsx
@@ -7,10 +7,14 @@ import { createApolloClient } from "../graphql/createApolloClient";
 
 import "../../styles/prism.css";
 import "../../styles/globals.scss";
+import "../../styles/tailwind.css";
+import { useEffect } from "react";
 
 export const apolloClient = createApolloClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => console.log("App rendered"), [])
   return (
     <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
