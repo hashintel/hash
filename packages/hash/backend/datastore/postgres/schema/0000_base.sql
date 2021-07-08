fd463083393e9a9867c755e6aa5ae9e1f2ecdf84
Eadan Fahey <ef@hash.ai>
2021-07-08 16:55:50 +0100

    style(backend): update comments in schema/0000_base.sql

--- packages/hash/backend/datastore/postgres/schema/0000_base.sql
+++ packages/hash/backend/datastore/postgres/schema/0000_base.sql
@@ -3,14 +3,11 @@ create table if not exists entity_types (
     name       text not null unique
 );
 
--- create table if not exists link_types (
---     id         serial primary key,
---     name       text not null unique
--- );
 
 create table if not exists shards (
     shard_id uuid primary key
 );
+
 
 create table if not exists entities (
     shard_id   uuid not null references shards (shard_id),
@@ -24,9 +21,8 @@ create table if not exists entities (
     primary key(shard_id, id)
 );
 
-/*
-This table is used to lookup the shard an entity belongs to.
-*/
+
+/** For entity ID : shard ID lookups */
 create table if not exists entity_shard (
     entity_id uuid not null primary key,
     shard_id  uuid not null,
@@ -34,6 +30,8 @@ create table if not exists entity_shard (
     foreign key (shard_id, entity_id) references entities (shard_id, id)
 );
 
+
+/** Stores parent --> child link references */
 create table if not exists outgoing_links (
     shard_id       uuid not null,
     entity_id      uuid not null,
@@ -46,6 +44,8 @@ create table if not exists outgoing_links (
     primary key (shard_id, entity_id, child_id)
 );
 
+
+/** Stores reverse child --> parent link references */
 create table if not exists incoming_links (
     shard_id        uuid not null,
     entity_id       uuid not null,
@@ -57,23 +57,3 @@ create table if not exists incoming_links (
 
     primary key (shard_id, entity_id, parent_id)
 );
-
-
--- create table if not exists links (
---     shard_id   uuid not null references shards (shard_id),
---     src_id     uuid not null,
---     dst_id     uuid not null,
---     -- type       integer not null references link_types (id),
---     -- properties jsonb,
---     -- created_at timestamp with time zone not null,
---     -- updated_at timestamp with time zone not null,
-
---     foreign key (shard_id, src_id) references entities (shard_id, id),
---     foreign key (shard_id, dst_id) references entities (shard_id, id),
-
---     -- This index optimizes queries for parent -> child relationships.
---     primary key(shard_id, src_id, dst_id)
--- );
-
--- -- This index optimizes for child -> parent relationships.
--- create unique index if not exists links_shard_dst_src on links (shard_id, dst_id, src_id);
