41d6c6115f139e5d6b0d821516174ecb2c6b3970
Eadan Fahey <ef@hash.ai>
2021-07-08 16:42:23 +0100

    feat(backend): add special cases for finding links in Pages and Blocks
    Page and Block entities currently do not have __linkedData fields for
    referencing their linked entities. We need to treat these entity types
    as special cases for now.

--- packages/hash/backend/src/db/postgres/util.ts
+++ packages/hash/backend/src/db/postgres/util.ts
@@ -4,13 +4,25 @@ const isObject = (v: any) => typeof v === "object" && v !== null;
 
 const isObjectOrArray = (v: any) => Array.isArray(v) || isObject(v);
 
-/** Recursively traverse the properties of an entity, gathering the IDs of all entities
+/**
+ * Recursively traverse the properties of an entity, gathering the IDs of all entities
  * it references through the __linkedData field.
- */
-export const gatherLinks = (entity: Entity) => {
+ * */
+export const gatherLinks = (entity: Entity): string[] => {
   // Note: the implementation is not actually recursive. NodeJS does not perform tail-call
   // optimization which presents a performance penalty for deeply nested entities.
   // Instead, we're doing an iterative depth-first search of the object tree.
+
+  // Block entities are a special case. They (currently) don't have a __linkedData field,
+  // but reference the entity they wrap with an "entityId" property.
+  if (entity.type === "Block") {
+    return [entity.properties.entityId as string];
+  }
+
+  // Page entities are another special case
+  if (entity.type === "Page") {
+    return entity.properties.contents.map((c: { entityId: string; }) => c.entityId as string);
+  }
 
   if (!isObject(entity.properties)) {
     throw new Error(
