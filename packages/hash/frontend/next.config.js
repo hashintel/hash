12b8bbff9ab89643ef7ea74ca2c6e211a41b59a7
Nate Higgins <nat@nath.is>
2021-07-09 14:36:08 +0100

    fix: redirect to first page

--- packages/hash/frontend/next.config.js
+++ packages/hash/frontend/next.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        // @todo set this correctly
-        destination: "/2/page1",
-        permanent: false,
-      },
-    ];
-  },
-};
+module.exports = {};
