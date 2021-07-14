4243174aed1d78032e7bfe4a7654a795f2abf0ca
Ciaran Morinan <cm@sohostrategy.com>
2021-07-14 15:49:33 +0100

    style: replace tailwind with twind

--- packages/hash/frontend/src/pages/_app.tsx
+++ packages/hash/frontend/src/pages/_app.tsx
@@ -4,16 +4,14 @@ require("setimmediate");
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { ApolloProvider } from "@apollo/client/react";
 import { createApolloClient } from "../graphql/createApolloClient";
+import withTwindApp from "@twind/next/app";
 
 import "../../styles/prism.css";
 import "../../styles/globals.scss";
-import "../../styles/tailwind.css";
-import { useEffect } from "react";
 
 export const apolloClient = createApolloClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => console.log("App rendered"), []);
   return (
     <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
@@ -21,4 +19,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default withTwindApp(MyApp);
