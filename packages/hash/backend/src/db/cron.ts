ac03b3f3fa40f868996d353b17e42a47e6cf65e7
Ben Werner <ben.werner01@gmail.com>
2021-07-21 12:53:15 +0100

    fix(backend): set pruneLoginCodes cron job to run at 5am

--- packages/hash/backend/src/db/cron.ts
+++ packages/hash/backend/src/db/cron.ts
@@ -5,7 +5,7 @@ import { DBAdapter } from "./adapter";
 export const setupCronJobs = (db: DBAdapter, logger: Logger) => {
   // Once a day (at midnight) prune expired login codes from the datastore
   new CronJob(
-    "0 0 0 * * *",
+    "0 0 5 * * *", // 5am
     () =>
       db
         .pruneLoginCodes()
