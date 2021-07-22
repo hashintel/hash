b3f524bcef7646967d8ed1edd7b3db4bb3dec640
Akash Joshi <akash.joshi@viit.ac.in>
2021-07-22 11:17:34 +0530

    change tsx line endings

--- packages/hash/blocks/embed/src/webpack-dev-server.tsx
+++ packages/hash/blocks/embed/src/webpack-dev-server.tsx
@@ -2,7 +2,7 @@
  * This is the entry point for developing and debugging.
  * This file is not bundled with the library during the build process.
  */
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Component from "./index";
 
@@ -53,14 +53,6 @@ function AppComponent() {
         id={"uuid-1234-id"}
         childEntityId={"uuid-1234-id"}
         getEmbedBlock={getEmbedBlock}
-        initialHtml={
-          undefined
-
-          // `<iframe src="https://www.youtube.com/embed/dlKBmzQbiyU?feature=oembed" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" width="200" height="113" frameborder="0"></iframe>`
-
-          //           `<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Yep! So tricky to decide whether it’s a bad idea or people don’t “trust” you yet in the ecosystem.<br><br>Most signups have been from the demo but I have a few just from talking about it on here and the forum.</p>&mdash; Archie Edwards (@archiethedev) <a href="https://twitter.com/archiethedev/status/1415981800577544195?ref_src=twsrc%5Etfw">July 16, 2021</a></blockquote>
-          // <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`
-        }
         update={updateBlockData}
       />
     </div>
