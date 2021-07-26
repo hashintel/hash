f7a775d9f19b61a6f4a7f1edd1f8ec7390b4e316
Marius Runge <mr@hash.ai>
2021-07-26 17:50:53 +0200

    feat(frontend): intro block-protocol package

---
+++ packages/hash/block-protocol/README.md
@@ -0,0 +1,6 @@
+# HASH.dev block-protocol
+
+## Introduction
+This document specifies a protocol for defining web “blocks” - discrete components displayed on a web page or other application - and how they communicate with any application embedding them. [...]
+
+The current draft of spec can be found on the drive.
---
+++ packages/hash/block-protocol/src/index.ts
@@ -0,0 +1,96 @@
+export type BlockProps = object;
+
+type BlockVariant = {
+  description?: string;
+  icon?: string;
+  name?: string;
+  properties?: BlockProps;
+};
+
+/**
+ * @todo type all as unknown and check properly
+ * we can't rely on people defining the JSON correctly
+ */
+export type BlockMetadata = {
+  author?: string;
+  description?: string;
+  externals?: Record<string, string>;
+  license?: string;
+  name?: string;
+  schema?: string;
+  source?: string;
+  variants?: BlockVariant[];
+  version?: string;
+};
+
+export type BlockProtocolUpdatePayload<T> = {
+  entityType: string;
+  entityId: string;
+  accountId: string;
+  data: T;
+};
+
+export type BlockProtocolCreatePayload<T> = {
+  entityType: string;
+  data: T;
+  pageAccountId: string;
+  userId: string;
+};
+
+export type BlockProtocolAggregateOperation = {
+  page?: number;
+  perPage?: number;
+  sort?: {
+    field: string;
+    desc?: boolean | undefined;
+  };
+};
+
+export type BlockProtocolAggregatePayload = {
+  entityType: string;
+  operation: BlockProtocolAggregateOperation;
+  accountId: string;
+};
+
+export type BlockProtocolCreateFn = {
+  <T>(actions: BlockProtocolCreatePayload<T>[]): void;
+};
+
+export type BlockProtocolUpdateFn = {
+  <T>(actions: BlockProtocolUpdatePayload<T>[]): Promise<void>;
+};
+
+export type BlockProtocolAggregateFn = {
+  (action: BlockProtocolAggregatePayload): void;
+};
+
+export type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | JSONObject;
+
+export type JSONObject = { [key: string]: JSONValue }
+
+export interface JSONArray extends Array<JSONValue> {}
+
+/**
+ * Block Protocol-specified properties,
+ * which the embedding application should provide.
+ */
+export type BlockProtocolProps = {
+  aggregate?: BlockProtocolCreateFn;
+  aggregateLoading?: boolean;
+  aggregateError?: Error;
+  create?: BlockProtocolCreateFn;
+  createLoading?: boolean;
+  createError?: Error;
+  id?: string;
+  schemas?: Record<string, JSONObject>;
+  type?: string;
+  update?: BlockProtocolUpdateFn;
+  updateLoading?: boolean;
+  updateError?: Error;
+};
