373771848edeacc510c20a8924aa5ccb2c1242a5
Eadan Fahey <ef@hash.ai>
2021-08-03 11:21:48 +0100

    test(integration): add page creation & update integration test

--- packages/hash/integration/src/tests/integration.test.ts
+++ packages/hash/integration/src/tests/integration.test.ts
@@ -38,3 +38,45 @@ test("can create org", async () => {
   expect(res.createdAt).toEqual(res.updatedAt);
   expect(res.type).toEqual("Org");
 });
+
+test("can create page", async () => {
+  const page = await client.createPage({
+    accountId: ACCOUNT_ID,
+    properties: {
+      title: "My first page",
+    },
+  });
+
+  const updatedPage = await client.insertBlocksIntoPage({
+    accountId: ACCOUNT_ID,
+    pageId: page.id,
+    blocks: [
+      {
+        accountId: ACCOUNT_ID,
+        componentId: "https://block.blockprotocol.org/header",
+        entityType: "Text",
+        entityProperties: {
+          texts: [{ text: "Hello World!" }],
+        },
+      },
+    ],
+  });
+
+  expect(updatedPage.metadataId).toEqual(page.metadataId);
+  expect(updatedPage.id).not.toEqual(page.id); // new version
+  expect(updatedPage.history).toHaveLength(2);
+  expect(updatedPage.history).toEqual([
+    { createdAt: updatedPage.createdAt, entityId: updatedPage.id },
+    { createdAt: page.createdAt, entityId: page.id },
+  ]);
+  expect(updatedPage.properties.title).toEqual("My first page");
+
+  // We inserted a block at the beginning of the page. The remaining blocks should
+  // be the same.
+  expect(updatedPage.properties.contents.length).toEqual(
+    page.properties.contents.length + 1
+  );
+  expect(updatedPage.properties.contents.slice(1)).toEqual(
+    page.properties.contents
+  );
+});
