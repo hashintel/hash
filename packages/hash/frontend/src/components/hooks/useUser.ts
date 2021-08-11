1d196e9fd612c104a3bf9298ab28cd6ec31ed5a4
teenoh <ugbalaachinike@gmail.com>
2021-08-11 13:16:43 +0100

    refactor(frontend): properly type useUser hook

--- packages/hash/frontend/src/components/hooks/useUser.ts
+++ packages/hash/frontend/src/components/hooks/useUser.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { meQuery } from "../../graphql/queries/user.queries";
-import { User } from '../../graphql/apiTypes.gen'
+import { MeQuery } from "../../graphql/apiTypes.gen";
 
 /**
  * Returns an array containing:
@@ -8,12 +8,11 @@ import { User } from '../../graphql/apiTypes.gen'
  * [0] the authenticated user (if any)
  *
  * [1] a function to refetch the user from the API (ApolloClient will update the cache with the return)
- * 
+ *
  * [2] a boolean to check if the api call is still loading
  */
-export const useUser = (): [User | null, () => void, boolean] => {
-    const { data, refetch, loading } = useQuery(meQuery)
-    const user = data?.me
+export const useUser = (): [MeQuery["me"] | undefined, () => void, boolean] => {
+  const { data, refetch, loading } = useQuery<MeQuery["me"]>(meQuery);
 
-    return [user, refetch, loading]
-}
+  return [data, refetch, loading];
+};
