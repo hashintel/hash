4d7b31d463037b44a546f5f87c8cec6361ba22cb
teenoh <ugbalaachinike@gmail.com>
2021-08-12 12:17:01 +0100

    feat(frontend): remove auth user from cache on logout

--- packages/hash/frontend/src/components/hooks/useLogout.ts
+++ packages/hash/frontend/src/components/hooks/useLogout.ts
@@ -1,4 +1,4 @@
-import { useMutation } from "@apollo/client";
+import { useApolloClient, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
 
 import { LogoutMutation } from "../../graphql/apiTypes.gen";
@@ -6,8 +6,12 @@ import { logout } from "../../graphql/queries/user.queries";
 
 export const useLogout = () => {
   const router = useRouter();
+  const client = useApolloClient();
+
   const [logoutFn, { loading, error }] = useMutation<LogoutMutation>(logout, {
     onCompleted: () => {
+      client.cache.evict({ fieldName: "me" });
+      client.cache.gc();
       void router.push("/");
     },
   });
