cf66b150e3e00ff1ff75b666191abc578506757b
Ciaran Morinan <cm@sohostrategy.com>
2021-08-13 16:13:13 +0100

    fix(fe): don't import named from default-exporting module

--- packages/hash/frontend/src/graphql/createApolloClient.ts
+++ packages/hash/frontend/src/graphql/createApolloClient.ts
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-import { possibleTypes } from "./fragmentTypes.gen.json";
+import possibleTypes from "./fragmentTypes.gen.json";
 
 export const createApolloClient = (name?: string) =>
   new ApolloClient({
     uri: "http://localhost:5001/graphql",
-    cache: new InMemoryCache({ possibleTypes }),
+    cache: new InMemoryCache({ possibleTypes: possibleTypes.possibleTypes }),
     credentials: "include",
     name,
   });
