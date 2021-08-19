56b4738e1e214a1eea814780d30583299f87d890
Ciaran Morinan <cm@sohostrategy.com>
2021-08-19 09:14:11 +0100

    feat(be): add entity type table + system type creation to db migration

---
+++ packages/hash/backend/datastore/postgres/scripts/0002_system_types_generate.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+const generatedIds = require("./data/generatedIds.json");
+const { SYSTEM_ACCOUNT_NAME } = require("../../../src/lib/config");
+const { entityTypeJson } = require("./0001_system_account_generate");
+
+const now = new Date().toISOString();
+
+const { types } = generatedIds;
+const systemAccount = generatedIds.orgs[SYSTEM_ACCOUNT_NAME];
+
+// This generates the system types we rely on being in the system in various queries/mutations
+// _EXCEPT_ the "Org" type, which is created as part of the 'system account' setup
+
+let sqlString = "";
+
+for (const typeName of ["Block", "Page", "Text", "User"]) {
+  const type = types[typeName];
+
+  sqlString += `insert into entity_types (
+  entity_type_id, account_id, name, versioned,
+  created_by, created_at, updated_at
+) values (
+  '${type.fixedId}', '${systemAccount.fixedId}', '${typeName}', true,
+  '${systemAccount.fixedId}', '${now}', '${now}'
+);
+insert into entity_type_versions (
+  entity_type_id, entity_type_version_id, account_id, 
+  properties, created_by, created_at, updated_at
+) values (
+  '${type.fixedId}', '${type.firstVersionId}', '${systemAccount.fixedId}',
+  '${entityTypeJson(typeName)}', 
+  '${systemAccount.fixedId}', '${now}', '${now}'
+);
+
+`;
+
+}
+
+const outputPath = path.join(__dirname, "../schema/0002_system_types.sql");
+fs.writeFileSync(outputPath, sqlString);
---
+++ packages/hash/backend/datastore/postgres/scripts/generateIds.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const { Uuid4 } = require("id128");
+const {SYSTEM_ACCOUNT_NAME} = require("../../../src/lib/config");
+
+const requiredIds = {
+  orgs: [SYSTEM_ACCOUNT_NAME],
+  types: ["Block", "EntityType", "Org", "Page", "Text", "User"]
+};
+
+/** @todo replace this when implementation in the backend/src/util changes */
+const genId = () => Uuid4.generate().toCanonical().toLowerCase();
+
+(() => {
+  const generatedIds = {};
+
+  for (const [entityType, nameList] of Object.entries(requiredIds)) {
+    generatedIds[entityType] = {};
+
+    for (const name of nameList) {
+      const fixedId = genId();
+      const firstVersionId = genId();
+      generatedIds[entityType][name] = {
+        fixedId,
+        firstVersionId
+      };
+    }
+  }
+  const output = JSON.stringify(generatedIds, undefined, 2);
+
+  const outputPath = path.join(__dirname, "data/generatedIds.json");
+  fs.writeFileSync(outputPath, output);
+})();
