b4b9814eeb7db8d0dee327a33d9e60c99b9cf83b
teenoh <ugbalaachinike@gmail.com>
2021-08-25 06:46:39 +0100

    chore(frontend): hide page header in auth routes

--- packages/hash/frontend/src/components/layout/PageLayout/PageLayout.tsx
+++ packages/hash/frontend/src/components/layout/PageLayout/PageLayout.tsx
@@ -1,10 +1,15 @@
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { PageHeader } from "../PageHeader/PageHeader";
 
+const AUTH_ROUTES = ["/login", "/signup"];
+
 export const PageLayout: FC = ({ children }) => {
+  const router = useRouter();
+
   return (
     <>
-      <PageHeader />
+      {!AUTH_ROUTES.includes(router.pathname) ? <PageHeader /> : null}
       {children}
     </>
   );
