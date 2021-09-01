3c1c7a8fec8f744df9c14480c73070dd7c6d3c60
Marius Runge <mr@hash.ai>
2021-09-01 17:53:04 +0200

    style(backend,shared): lint + format

--- packages/hash/backend/src/db/index.ts
+++ packages/hash/backend/src/db/index.ts
@@ -1,3 +1,3 @@
-export { DBAdapter, DBClient } from "./adapter";
+export type { DBAdapter, DBClient } from "./adapter";
 export { PostgresAdapter } from "./postgres";
 export { setupCronJobs } from "./cron";
