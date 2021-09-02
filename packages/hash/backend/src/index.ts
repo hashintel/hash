28c319ff4757be331e4efb1211acf9a5a7e5f749
Eadan Fahey <ef@hash.ai>
2021-09-02 10:54:31 +0100

    refactor(be): pass credentials directly to PostgresAdapter
    Rather than reading from environment variables inside the file where
    PostgresAdapter is defined, refactor the class constructor such that
    the credentials and other connection config is passed as an argument.
    This makes it clearer about how the connection is formed.

--- packages/hash/backend/src/index.ts
+++ packages/hash/backend/src/index.ts
@@ -66,7 +66,16 @@ const app = express();
 const PORT = process.env.PORT ?? 5001;
 
 // Connect to the database
-const db = new PostgresAdapter(logger, statsd);
+const pgConfig = {
+  host: getRequiredEnv("HASH_PG_HOST"),
+  user: getRequiredEnv("HASH_PG_USER"),
+  password: getRequiredEnv("HASH_PG_PASSWORD"),
+  database: getRequiredEnv("HASH_PG_DATABASE"),
+  port: parseInt(getRequiredEnv("HASH_PG_PORT")),
+
+  maximumPoolSize: 10, // @todo: needs tuning
+};
+const db = new PostgresAdapter(pgConfig, logger, statsd);
 
 // Set sensible default security headers: https://www.npmjs.com/package/helmet
 // Temporarily disable contentSecurityPolicy for the GraphQL playground
