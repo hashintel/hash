d70320ddcfe92a3adbc065f32722ff393f736527
Eadan Fahey <ef@hash.ai>
2021-09-03 13:48:50 +0100

    tool: in hooks/install.sh fail if OS is Windows

--- hooks/install.sh
+++ hooks/install.sh
@@ -3,6 +3,14 @@
 
 # Install all hooks in this directory into ../.git/hooks.
 
+# @todo: get this script to work on Windows
+os=$(uname | tr '[:upper:]' '[:lower:]');
+if grep -qE 'windows' <<< "$os"; then
+  echo "Windows is not supported. Please install the hooks manually"
+  exit 1;
+fi;
+
+
 dir=$(cd "$(dirname "$0")" || exit; pwd -P);
 
 for f in "$dir"/*
