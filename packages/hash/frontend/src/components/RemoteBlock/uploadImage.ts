cdf19c13dd273845fd641130342fc3864d43eb3c
Akash Joshi <akash.joshi@viit.ac.in>
2021-09-07 13:21:24 +0530

    refactor(frontend): reject if no file exists

--- packages/hash/frontend/src/components/RemoteBlock/uploadImage.ts
+++ packages/hash/frontend/src/components/RemoteBlock/uploadImage.ts
@@ -8,27 +8,25 @@ export const uploadImage = async ({
 }): Promise<{
   src?: string;
 }> => {
-  
   if (imgURL?.trim()) {
     return { src: imgURL };
   }
 
-  if (file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        // have to keep `if` first because of Typescript
-        if (event.target?.result) {
-          resolve({ src: event.target.result.toString() });
-        } else {
-          reject(new Error("Couldn't read your file"));
-        }
-      };
-
-      reader.readAsDataURL(file);
-    });
+  if (!file) {
+    throw new Error("Please enter a valid image URL or select a file below");
   }
 
-  throw new Error("Please enter a valid image URL or select a file below");
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      if (event.target?.result) {
+        resolve({ src: event.target.result.toString() });
+      } else {
+        reject(new Error("Couldn't read your file"));
+      }
+    };
+
+    reader.readAsDataURL(file);
+  });
 };
