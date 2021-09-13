6f6f7b57b138d45acf673741f0f34940f41f7c34
Nate Higgins <nat@nath.is>
2021-09-13 15:41:36 +0100

    feat: wrap graphql fetch to put the operation name in the query param

--- packages/hash/shared/src/graphql/createApolloClient.ts
+++ packages/hash/shared/src/graphql/createApolloClient.ts
@@ -1,4 +1,5 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { RequestInfo, RequestInit } from "node-fetch";
 
 import possibleTypes from "./fragmentTypes.gen.json";
 
@@ -9,10 +10,30 @@ export const createApolloClient = (name?: string) => {
       ? require("node-fetch")
       : (globalThis as any).fetch;
 
+  /**
+   * This wraps fetch to inject the query operation name into the URL, which makes it easier
+   * to identify in dev tools.
+   *
+   * @todo disable this in production due to caching concerns
+   */
+  const wrappedFetch = (uri: RequestInfo, options: RequestInit | undefined) => {
+    let operationName: string | null = null;
+
+    if (typeof options?.body === "string") {
+      try {
+        ({ operationName } = JSON.parse(options.body));
+      } catch {}
+    }
+
+    return ponyfilledFetch(
+      operationName ? `${uri}?${operationName}` : uri,
+      options
+    );
+  };
   const httpLink = new HttpLink({
     uri: "http://localhost:5001/graphql",
     credentials: "include",
-    fetch: ponyfilledFetch,
+    fetch: wrappedFetch,
   });
 
   return new ApolloClient({
