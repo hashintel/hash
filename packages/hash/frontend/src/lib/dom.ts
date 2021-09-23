d4ae52de3e832fdc89e2473884d713e2374342df
espretto <espretto@posteo.net>
2021-09-23 13:51:19 +0200

    style(frontend): avoid comma operator

--- packages/hash/frontend/src/lib/dom.ts
+++ packages/hash/frontend/src/lib/dom.ts
@@ -2,5 +2,8 @@
  * used to ensure the node is a child of the given parent
  * @returns true if noop
  */
-export const ensureMounted = <T extends Node>(child: T, parent: Element) =>
-  child.parentNode === parent || (parent.appendChild(child), false);
+export const ensureMounted = <T extends Node>(child: T, parent: Element) => {
+  if (child.parentNode === parent) return true;
+  parent.appendChild(child);
+  return false;
+};
