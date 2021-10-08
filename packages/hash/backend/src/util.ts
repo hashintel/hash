9a00b95481235c8eedbf5eb76404c6f5cf09dcf7
Eadan Fahey <ef@hash.ai>
2021-10-08 20:40:46 +0100

    feat(be): add topologicalSort fn to src/util.ts & associated tests

--- packages/hash/backend/src/util.ts
+++ packages/hash/backend/src/util.ts
@@ -1,4 +1,5 @@
 import { Uuid4 } from "id128";
+import { uniq } from "lodash";
 
 /**
  * Generate a new ID.
@@ -52,4 +53,58 @@ export const isRecord = (thing: unknown): thing is Record<string, any> => {
     return false;
   }
   return true;
+};
+
+/**
+ * Perform a topological sort [1] on an array of `edges` forming a directed acyclic graph.
+ * [1] https://en.wikipedia.org/wiki/Topological_sorting
+ * @returns a topological ordering of the nodes represented by the graph in `edges`.
+ * @throws an error if the graph has a cycle.
+ * */
+export const topologicalSort = <T>(edges: [T, T][]) => {
+  // Implementation based on Kahn's algorithm. See:
+  // https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm)
+
+  // Find nodes with no incoming edge, i.e. the root node(s)
+  const nonRootNodes = new Set(edges.map(([_, node]) => node));
+  const rootNodes = uniq(
+    edges.map(([node, _]) => node).filter((node) => !nonRootNodes.has(node))
+  );
+
+  // Pre-compute the incoming and outgoing edges for each node as an optimization
+  const outgoingEdges = new DefaultMap<T, Set<T>>(() => new Set());
+  for (const [n1, n2] of edges) {
+    outgoingEdges.get(n1).add(n2);
+  }
+  const incomingEdges = new DefaultMap<T, Set<T>>(() => new Set());
+  for (const [n1, n2] of edges) {
+    incomingEdges.get(n2).add(n1);
+  }
+
+  const sort = [];
+  const stack = [...rootNodes];
+
+  while (stack.length > 0) {
+    const node = stack.pop()!;
+    sort.push(node);
+    const nodeOutgoing = outgoingEdges.get(node);
+    for (const child of nodeOutgoing.values()) {
+      const childIncoming = incomingEdges.get(child)!;
+      // Remove the (node, child) edge from the graph
+      nodeOutgoing.delete(child);
+      childIncoming.delete(node);
+      if (childIncoming.size === 0) {
+        stack.push(child);
+      }
+    }
+  }
+
+  // If there are any edges left, then the graph is not acyclic
+  for (const [_, children] of outgoingEdges.entries()) {
+    if (children.size !== 0) {
+      throw new Error("graph is not acyclic");
+    }
+  }
+
+  return sort;
 };
