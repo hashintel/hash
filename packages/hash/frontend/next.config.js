56103e2afcc921df3f4bfd5cf58395a629624f42
Ciaran Morinan <cm@sohostrategy.com>
2021-10-14 20:18:31 +0100

    chore(fe): add webpack sandbox config, bundle analyzer, related deps

--- packages/hash/frontend/next.config.js
+++ packages/hash/frontend/next.config.js
@@ -1,26 +1,50 @@
 const path = require("path");
 const withTM = require("next-transpile-modules")(["@hashintel/hash-shared"]); // pass the modules you would like to see transpiled
 const withImages = require("next-images");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = withImages(
-  withTM({
-    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx','page.jsx'],
-    webpack5: false,
-    webpack: (config) => {
-      // help out nextjs plugin next-transpile-modules to correctly resolve monorepo dependencies
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "@hashintel/hash-shared": path.join(
-          __dirname,
-          "../../..",
-          "node_modules/@hashintel/hash-shared/dist"
-        ),
-      };
+module.exports = withBundleAnalyzer(
+  withImages(
+    withTM({
+      pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.jsx"],
+      webpack5: false,
+      webpack: (config) => {
+        // help out nextjs plugin next-transpile-modules to correctly resolve monorepo dependencies
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "@hashintel/hash-shared": path.join(
+            __dirname,
+            "../../..",
+            "node_modules/@hashintel/hash-shared/dist"
+          ),
+        };
 
-      return config;
-    },
-    sassOptions: {
-      prependData: `
+        //  Build the sandbox HTML, which will have the sandbox script injected
+        const framedBlockFolder = "/src/components/sandbox/FramedBlock";
+        config.plugins.push(
+          new HtmlWebpackPlugin({
+            filename: "static/sandbox.html",
+            template: path.join(
+              __dirname,
+              "/src/components/sandbox/FramedBlock/index.html"
+            ),
+            chunks: ["sandbox"],
+          })
+        );
+        return Object.assign({}, config, {
+          entry: () =>
+            config.entry().then((entry) =>
+              Object.assign({}, entry, {
+                sandbox: path.join(__dirname, framedBlockFolder, "index.tsx"),
+              })
+            ),
+        });
+      },
+      sassOptions: {
+        prependData: `
       $grey-bg: rgba(241, 243, 246, 0.3);
       $grey-border: #e5e6e7;
       $black-almost: #1b1d24;
@@ -29,6 +53,7 @@ module.exports = withImages(
       $bright-pink: #ff008b;
       $bright-blue: #2482ff;
       `,
-    },
-  })
+      },
+    })
+  )
 );
