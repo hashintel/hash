01092978b3118b36d1ebd7e3b9bc80687db0374a
Nate Higgins <nat@nath.is>
2021-10-15 11:16:36 +0100

    chore: add comment

--- packages/hash/backend/src/graphql/resolvers/block/blockEntity.ts
+++ packages/hash/backend/src/graphql/resolvers/block/blockEntity.ts
@@ -25,6 +25,15 @@ export const blockEntity: Resolver<
 
   const mappedEntity = entity.toGQLUnknownEntity();
 
+  /**
+   * `Block.entity` is typed in GraphQL as JSONData. This is because
+   * Entity.properties (in some cases) is typed as JSONData too, which can
+   * contain entities, which will then by typed as JSONData, which means if we
+   * want the frontend to be able to treat these equally, we'll want
+   * Block.entity to be JSONData. However, this means any Entity.* custom
+   * resolvers won't be triggered for properties on Block.entity. This means
+   * we need to manually resolve any links contained within the entity.
+   */
   await resolveLinkedData(ctx, entity.accountId, entity.properties, info);
 
   return mappedEntity;
