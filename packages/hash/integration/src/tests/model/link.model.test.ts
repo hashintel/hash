c204da125edae5e6424d0e70047ebd9bd23e0344
Ben Werner <ben.werner01@gmail.com>
2021-11-01 17:55:04 +0000

    feat(int): Link model class create link on versioned source entities tests

--- packages/hash/integration/src/tests/model/link.model.test.ts
+++ packages/hash/integration/src/tests/model/link.model.test.ts
@@ -46,7 +46,9 @@ beforeAll(async () => {
 
 describe("Link model class ", () => {
   it("static isPathValid method correctly validates JSON path correctly", () => {
-    expect(Link.isPathValid("$.this[0].path['should'].be[\"valid\"]")).toBe(true);
+    expect(Link.isPathValid("$.this[0].path['should'].be[\"valid\"]")).toBe(
+      true
+    );
     expect(Link.isPathValid("thispathisn'tvalid")).toBe(false);
     expect(Link.isPathValid("$.this.is.not.valid.")).toBe(false);
   });
@@ -121,6 +123,101 @@ describe("Link model class ", () => {
     expect(retrievedLink.dstAccountId).toBe(link.dstAccountId);
     expect(retrievedLink.dstEntityId).toBe(link.dstEntityId);
     expect(retrievedLink.dstEntityVersionId).toBe(link.dstEntityVersionId);
+  });
+
+  it("can create outgoing link on non-versioned entity", async () => {
+    const accountId = existingUser.accountId;
+    const createdById = existingUser.entityId;
+
+    const [sourceEntity, destinationEntity] = await Promise.all([
+      Entity.create(db)({
+        accountId,
+        createdById,
+        versioned: false,
+        entityTypeId: dummyEntityType.entityId,
+        properties: {},
+      }),
+      Entity.create(db)({
+        accountId,
+        createdById,
+        versioned: false,
+        entityTypeId: dummyEntityType.entityId,
+        properties: {},
+      }),
+    ]);
+
+    const intialSourceEntityId = sourceEntity.entityVersionId;
+
+    const link = await Link.create(db)({
+      path: "test",
+      source: sourceEntity,
+      destination: destinationEntity,
+    });
+
+    expect(link.srcEntityId).toBe(sourceEntity.entityId);
+    expect(Array.from(link.srcEntityVersionIds)).toEqual([intialSourceEntityId]);
+    expect(sourceEntity.entityVersionId).toBe(intialSourceEntityId);
+  });
+
+  it("can create outgoing link on versioned source entity", async () => {
+    const accountId = existingUser.accountId;
+    const createdById = existingUser.entityId;
+
+    const [versionedSourceEntity, destinationEntity] = await Promise.all([
+      Entity.create(db)({
+        accountId,
+        createdById,
+        versioned: true,
+        entityTypeId: dummyEntityType.entityId,
+        properties: {},
+      }),
+      Entity.create(db)({
+        accountId,
+        createdById,
+        versioned: false,
+        entityTypeId: dummyEntityType.entityId,
+        properties: {},
+      }),
+    ]);
+
+    const entityVersionIds = [versionedSourceEntity.entityVersionId];
+
+    const link = await Link.create(db)({
+      path: "test",
+      source: versionedSourceEntity,
+      destination: destinationEntity,
+    });
+
+    entityVersionIds.push(versionedSourceEntity.entityVersionId);
+
+    expect(entityVersionIds[0]).not.toBe(entityVersionIds[1]);
+
+    // The first version of the entity should have 0 outgoing links
+
+    const e1 = (await Entity.getEntity(db)({
+      accountId,
+      entityVersionId: entityVersionIds[0],
+    }))!;
+
+    expect(e1).not.toBeNull();
+
+    const e1OutgoingLinks = await e1.getOutgoingLinks(db);
+
+    expect(e1OutgoingLinks.length).toBe(0);
+
+    // The second version of the entity should have 1 outgoing link
+
+    const e2 = (await Entity.getEntity(db)({
+      accountId,
+      entityVersionId: entityVersionIds[1],
+    }))!;
+
+    expect(e2).not.toBeNull();
+
+    const e2OutgoingLinks = await e2.getOutgoingLinks(db);
+
+    expect(e2OutgoingLinks.length).toBe(1);
+    expect(e2OutgoingLinks[0].linkId).toBe(link.linkId);
   });
 });
 
