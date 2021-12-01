84bea1ad17e6411f97a37b08a17bbf7dc3ec5674
Akash Joshi <akash.joshi@viit.ac.in>
2021-12-01 16:36:57 +0530

    feat(fe): add loading before redirecting user

--- packages/hash/frontend/src/pages/index.page.tsx
+++ packages/hash/frontend/src/pages/index.page.tsx
@@ -1,23 +1,31 @@
+import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { tw } from "twind";
 
+import styles from "./index.module.scss";
 import { useUser } from "../components/hooks/useUser";
-import { LoginModal } from "../components/Modals/AuthModal/LoginModal";
 
 export default function Home() {
   const router = useRouter();
-  const { user, refetch } = useUser();
+  const { user } = useUser();
 
-  if (user) {
-    // Temporarily redirect logged in user to their account page
-    void router.push(`/${user.accountId}`);
-  }
+  useEffect(() => {
+    if (user) {
+      // Temporarily redirect logged in user to their account page
+      void router.push(`/${user.accountId}`);
+    } else {
+      void router.push("/login");
+    }
+  }, [router, user]);
 
   return (
-    <LoginModal
-      show
-      onLoggedIn={() => {
-        void refetch();
-      }}
-    />
+    <main className={styles.Main}>
+      <section
+        style={{ marginTop: "30vh" }}
+        className={tw`flex justify-center`}
+      >
+        <h1>Loading ...</h1>
+      </section>
+    </main>
   );
 }
--- tests/guest-user.spec.ts
+++ tests/guest-user.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("guest user navigation to login and signup pages", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("HASH Workspace");
+  await page.goto("/login");
 
   await expect(page.locator("text=Sign in to your account")).toBeVisible();
   await expect(page.locator("text=No account? No problem")).toBeVisible();
