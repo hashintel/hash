5443f53bea16c1cab3ed11e461cfcee1dc501e84
Akash Joshi <akash.joshi@viit.ac.in>
2021-12-02 11:28:45 +0530

    fix(fe): add explicit import

--- packages/hash/frontend/src/components/pages/MainComponentWrapper.tsx
+++ packages/hash/frontend/src/components/pages/MainComponentWrapper.tsx
@@ -1,8 +1,10 @@
+import { ReactNode } from "react";
+
 import styles from "../../pages/index.module.scss";
 import { PageSidebar } from "../layout/PageSidebar/PageSidebar";
 
 export const MainComponentWrapper: React.VoidFunctionComponent<{
-  children: React.ReactNode;
+  children: ReactNode;
 }> = ({ children }) => {
   return (
     <div className={styles.MainWrapper}>
