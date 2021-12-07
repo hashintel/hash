4971ab56c4c3b9de450686fec46f053cba8bbb3f
Tim Diekmann <tim.diekmann@3dvision.de>
2021-12-07 12:51:47 +0100

    Enable debug logging by default in debug builds only

--- packages/engine/src/utils.rs
+++ packages/engine/src/utils.rs
@@ -1,5 +1,5 @@
 pub fn init_logger() {
-    if std::env::var("RUST_LOG").is_err() {
+    if cfg!(debug_assertions) && std::env::var("RUST_LOG").is_err() {
         std::env::set_var(
             "RUST_LOG",
             "hash_cloud=debug,hash_engine=debug,cli=debug,server=debug,proto=debug,nano=debug,apiclient=debug",
