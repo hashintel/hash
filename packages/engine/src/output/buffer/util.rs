0104095b2956998c60eaf6ed8ecebfd045b7de0a
Alfred Mountfield <alfie.mountfield@gmail.com>
2021-12-09 17:37:56 +0000

    Removing currently obsolete (and broken) output parts cleanup code

--- packages/engine/src/output/buffer/util.rs
+++ packages/engine/src/output/buffer/util.rs
@@ -7,6 +7,7 @@ use crate::output::error::{Error, Result};
 /// Shared memory cleanup in the process hard crash case.
 /// Not required for pod instances.
 pub fn cleanup_experiment(experiment_id: &str) -> Result<()> {
+    log::trace!("Cleaning up experiment: {}", experiment_id);
     let shm_files = glob::glob(&format!("/dev/shm/shm_{}_*", experiment_id))
         .map_err(|e| Error::Unique(format!("cleanup glob error: {}", e)))?;
     for file in shm_files {
@@ -18,21 +19,30 @@ pub fn cleanup_experiment(experiment_id: &str) -> Result<()> {
         }
     }
 
-    remove_experiment_parts(experiment_id).or_else(|e| {
-        let s = e.to_string();
-        if s.contains("kind: NotFound") {
-            Ok(())
-        } else {
-            Err(Error::from(s))
-        }
-    })?;
+    // TODO: We don't want to be deleting the parts files by default. We should figure out what to
+    //   do with this.
+    // remove_experiment_parts(experiment_id).or_else(|e| {
+    //     let err_string = e.to_string();
+    //     if err_string.contains("kind: NotFound") {
+    //         Ok(())
+    //     } else {
+    //         Err(Error::from(format!(
+    //             "Error while trying to remove experiment parts folder: {err_string}"
+    //         )))
+    //     }
+    // })?;
 
     Ok(())
 }
 
+#[allow(dead_code)]
 fn remove_experiment_parts(experiment_id: &str) -> Result<()> {
     let mut base_path = PathBuf::from(RELATIVE_PARTS_FOLDER);
+    // TODO: this is unused at the moment so it's fine, but this logic is wrong, we name our folders
+    //  differently, we should update the design to store the paths and use them here when we use
+    //  the clean up code again
     base_path.push(experiment_id);
+    log::trace!("Removing all parts files in: {base_path:?}");
     std::fs::remove_dir_all(base_path)?;
     Ok(())
 }
