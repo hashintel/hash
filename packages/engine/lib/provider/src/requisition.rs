a6420dab891ae042704315af3b676a9db9cca93c
Tim Diekmann <tim.diekmann@3dvision.de>
2022-01-03 15:44:38 +0100

    Add `Provider` API for context information on errors (and possibly other things)
    Add tests to provider
    
    
    Fix newline on Cargo.toml

---
+++ packages/engine/lib/provider/src/requisition.rs
@@ -0,0 +1,58 @@
+use crate::{tags, Requisition, TagValue, TypeTag};
+
+impl<'p> Requisition<'p, '_> {
+    /// Provide a value with the given `TypeTag`.
+    pub fn provide<I>(&mut self, value: I::Type) -> &mut Self
+    where
+        I: TypeTag<'p>,
+    {
+        if let Some(res @ TagValue(Option::None)) =
+            self.0.tagged.downcast_mut::<tags::OptionTag<I>>()
+        {
+            res.0 = Some(value);
+        }
+        self
+    }
+
+    /// Provide a value or other type with only static lifetimes.
+    pub fn provide_value<T, F>(&mut self, f: F) -> &mut Self
+    where
+        T: 'static,
+        F: FnOnce() -> T,
+    {
+        self.provide_with::<tags::Value<T>, F>(f)
+    }
+
+    /// Provide a reference, note that the referee type must be bounded by `'static`, but may be
+    /// unsized.
+    pub fn provide_ref<T: ?Sized + 'static>(&mut self, value: &'p T) -> &mut Self {
+        self.provide::<tags::Ref<T>>(value)
+    }
+
+    /// Provide a value with the given `TypeTag`, using a closure to prevent unnecessary work.
+    pub fn provide_with<I, F>(&mut self, f: F) -> &mut Self
+    where
+        I: TypeTag<'p>,
+        F: FnOnce() -> I::Type,
+    {
+        if let Some(res @ TagValue(Option::None)) =
+            self.0.tagged.downcast_mut::<tags::OptionTag<I>>()
+        {
+            res.0 = Some(f());
+        }
+        self
+    }
+}
+
+/// A concrete request for a tagged value. Can be coerced to `Requisition` to be passed to provider
+/// methods.
+pub(super) type ConcreteRequisition<'p, I> = RequisitionImpl<TagValue<'p, tags::OptionTag<I>>>;
+
+/// Implementation detail shared between `Requisition` and `ConcreteRequisition`.
+///
+/// Generally this value is used through the `Requisition` type as an `&mut Requisition<'p>` out
+/// parameter, or constructed with the `ConcreteRequisition<'p, I>` type alias.
+#[repr(transparent)]
+pub(super) struct RequisitionImpl<T: ?Sized> {
+    pub(super) tagged: T,
+}
---
+++ packages/engine/lib/provider/src/tags.rs
@@ -0,0 +1,87 @@
+//! Type tags are used to identify a type using a separate value. This module includes type tags for
+//! some very common types.
+//!
+//! Many users of the provider APIs will not need to use type tags at all. But if you want to use
+//! them with more complex types (typically those including lifetime parameters), you will
+//! need to write your own tags.
+
+use core::marker::PhantomData;
+
+use crate::TypeTag;
+
+/// Type-based `TypeTag` for `&'p T` types.
+#[derive(Debug)]
+pub struct Ref<T: ?Sized + 'static>(PhantomData<T>);
+
+impl<'p, T: ?Sized + 'static> TypeTag<'p> for Ref<T> {
+    type Type = &'p T;
+}
+
+/// Type-based `TypeTag` for static `T` types.
+#[derive(Debug)]
+pub struct Value<T: 'static>(PhantomData<T>);
+
+impl<'p, T: 'static> TypeTag<'p> for Value<T> {
+    type Type = T;
+}
+
+/// Tag combinator to wrap the given tag's value in an `Option<T>`
+#[derive(Debug)]
+pub struct OptionTag<I>(PhantomData<I>);
+
+impl<'p, I: TypeTag<'p>> TypeTag<'p> for OptionTag<I> {
+    type Type = Option<I::Type>;
+}
+
+/// Tag combinator to wrap the given tag's value in an `Result<T, E>`
+#[derive(Debug)]
+pub struct ResultTag<I, E>(PhantomData<I>, PhantomData<E>);
+
+impl<'p, I: TypeTag<'p>, E: TypeTag<'p>> TypeTag<'p> for ResultTag<I, E> {
+    type Type = Result<I::Type, E::Type>;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{tags, tests::ERR};
+
+    #[test]
+    fn reference() {
+        assert_eq!(
+            crate::request_by_type_tag::<tags::Ref<usize>, _>(&ERR),
+            Some(&2)
+        );
+    }
+
+    #[test]
+    fn value() {
+        assert_eq!(
+            crate::request_by_type_tag::<tags::Value<usize>, _>(&ERR),
+            Some(1)
+        );
+    }
+
+    #[test]
+    fn option() {
+        assert_eq!(
+            crate::request_by_type_tag::<tags::OptionTag<tags::Value<usize>>, _>(&ERR),
+            Some(Some(5))
+        );
+    }
+
+    #[test]
+    fn result() {
+        assert_eq!(
+            crate::request_by_type_tag::<tags::ResultTag<tags::Value<u32>, tags::Value<i32>>, _>(
+                &ERR
+            ),
+            Some(Ok(6))
+        );
+        assert_eq!(
+            crate::request_by_type_tag::<tags::ResultTag<tags::Value<i32>, tags::Value<u32>>, _>(
+                &ERR
+            ),
+            Some(Err(7))
+        );
+    }
+}
