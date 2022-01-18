006ee1eb02bada8d1ec57f25f3a6d73ee2e16bf6
Liana Pigeot <32243840+nialna@users.noreply.github.com>
2022-01-18 17:48:55 +0000

    improved log text for when running external services without opensearch

--- external-services/opensearch/start.sh
+++ external-services/opensearch/start.sh
@@ -4,6 +4,8 @@ if [[ "$HASH_OPENSEARCH_ENABLED" = "true" ]]; then
   source /usr/share/opensearch/opensearch-docker-entrypoint.sh;
 else
   echo "Not using Open Search because HASH_OPENSEARCH_ENABLED is not equal to true.";
-  echo "Please change .env.local and restart the container.";
+  echo "Opensearch is currently disabled by default in Hash to save memory (the opensearch docker image is pretty memory intensive)";
+  echo "There are also issues with the search-loader at the moment. Enable opensearch at your own risk.";
+  echo "To enable Opensearch, add HASH_OPENSEARCH_ENABLED=true to your .env.local file";
   exit 0;
 fi
