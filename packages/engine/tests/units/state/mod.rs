7946c3cefc3dc92b3cb03bcceb83f1e0343dd0a3
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-01-18 17:58:15 +0100

    Add integration tests for neighbors (#178)

--- packages/engine/tests/units/state/mod.rs
+++ packages/engine/tests/units/state/mod.rs
@@ -2,4 +2,4 @@ mod edit;
 
 // TODO: Fix behaviorIndex() returns `undefined`
 //   see https://app.asana.com/0/1201481007343159/1201671951990162/f
-// crate::run_test!(behavior_index);
+crate::run_test!(behavior_index, #[ignore]);
