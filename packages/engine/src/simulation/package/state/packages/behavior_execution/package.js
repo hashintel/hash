e2930bca180bef6ab0e96d558eee476b70126050
Tim Diekmann <tim.diekmann@3dvision.de>
2022-01-19 16:26:20 +0100

    Add Todo on how to act when tests are failing once other runners are added

--- packages/engine/src/simulation/package/state/packages/behavior_execution/package.js
+++ packages/engine/src/simulation/package/state/packages/behavior_execution/package.js
@@ -197,6 +197,10 @@ const run_task = (
       i_behavior < n_behaviors;
       ++i_behavior
     ) {
+      // TODO: reevaluate when other runners are implemented: If there are failing tests,
+      //   - either this has to be changed to `agent_state.behavior_index = i_behavior` and `AgentState.behaviorIndex()`
+      //     has to be adjusted to use `behavior_index` instead of `__i_behavior`, or
+      //   - `agent_state.behavior_index = i_behavior;` has to be added.
       agent_state.__i_behavior = i_behavior;
 
       const key = behavior_ids.get(i_behavior);
