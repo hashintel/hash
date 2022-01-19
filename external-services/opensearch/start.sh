eb592336a1aa82f1ee6a6ff85ab4e85d8289622f
Liana Pigeot <32243840+nialna@users.noreply.github.com>
2022-01-19 14:31:35 +0000

    corrected opensearch capitalisation

--- external-services/opensearch/start.sh
+++ external-services/opensearch/start.sh
@@ -3,9 +3,9 @@
 if [[ "$NEXT_PUBLIC_HASH_OPENSEARCH_ENABLED" = "true" ]]; then
   source /usr/share/opensearch/opensearch-docker-entrypoint.sh;
 else
-  echo "Not using Opensearch because NEXT_PUBLIC_HASH_OPENSEARCH_ENABLED is not equal to true.";
-  echo "Opensearch is currently disabled by default in Hash to save memory (the opensearch docker image is pretty memory intensive)";
-  echo "There are also issues with the search-loader at the moment. Enable opensearch at your own risk.";
-  echo "To enable Opensearch, add NEXT_PUBLIC_HASH_OPENSEARCH_ENABLED=true to your .env.local file";
+  echo "Not using OpenSearch because NEXT_PUBLIC_HASH_OPENSEARCH_ENABLED is not equal to true.";
+  echo "OpenSearch is currently disabled by default in Hash to save memory (the OpenSearch docker image is pretty memory intensive)";
+  echo "There are also issues with the search-loader at the moment. Enable OpenSearch at your own risk.";
+  echo "To enable OpenSearch, add NEXT_PUBLIC_HASH_OPENSEARCH_ENABLED=true to your .env.local file";
   exit 0;
 fi
