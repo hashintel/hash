c0d2603a5a7a8b6779021e5fff990cfc5c3b66dc
Tim Diekmann <tim.diekmann@3dvision.de>
2022-01-19 13:06:26 +0100

    Add `mapbox` and `stop_simulation` tests

--- packages/engine/tests/units/message/mod.rs
+++ packages/engine/tests/units/message/mod.rs
@@ -6,10 +6,15 @@ run_test!(one_recipient);
 run_test!(multiple_recipients);
 
 run_test!(all_types);
+// TODO: Make mapbox work
+//   see https://app.asana.com/0/1199548034582004/1200329934588478/f
+run_test!(mapbox);
 
 run_test!(create_agent);
 run_test!(remove_agent);
-
 // TODO: Fix `DataStore` error when removing last agent
 //   see https://app.asana.com/0/1201481007343159/1201681219656029/f
 run_test!(remove_self, #[ignore]);
+// TODO: Handle stop messages
+//   see https://app.asana.com/0/1199550852792314/1201630005867419/f
+run_test!(stop_simulation, #[ignore]);
