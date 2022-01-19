dc71e6f89aa6e577e2f3e4c5dbb30b34810e1a5d
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-01-19 12:51:21 +0100

    Fix immutable CSV test to actually try to mutate the dataset
    Co-authored-by: Alfred Mountfield <am@hash.ai>

--- packages/engine/tests/units/data/csv/immutable/src/behaviors/test.js
+++ packages/engine/tests/units/data/csv/immutable/src/behaviors/test.js
@@ -2,6 +2,6 @@
  * Ensures a dataset is immutable
  */
 const behavior = (state, context) => {
-  context.data()["dataset.csv"][0][0];
+  context.data()["dataset.csv"][0][0] = "4";
   state.column = context.data()["dataset.csv"][0];
 };
