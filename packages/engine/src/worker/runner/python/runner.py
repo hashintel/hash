75ec738df4e7c34e92e674fb710a8294a4f74bb2
litvand <al@hash.ai>
2022-01-24 17:16:08 +0200

    Better Python error messages

--- packages/engine/src/worker/runner/python/runner.py
+++ packages/engine/src/worker/runner/python/runner.py
@@ -8,6 +8,7 @@ from context import SimInitContext
 from package import Package
 from sim import Sim
 from message import Messenger, MESSAGE_TYPE
+from util import format_exc_info
 
 
 class Runner:
@@ -16,7 +17,7 @@ class Runner:
             self.messenger = Messenger(experiment_id, worker_index)
         except Exception as e:
             # Can't do much if messenger init fails.
-            logging.error("Messenger init failed: " + str(e))
+            logging.error(f"Messenger init failed: {e}")
             raise e
 
         self.batches = Batches()
@@ -25,9 +26,57 @@ class Runner:
 
         try:
             self.experiment_ctx = self.start_experiment()
-        except Exception as e:  # Have to catch generic Exception
-            self.handle_runner_error(sys.exc_info())
+        except Exception as e:
+            # Have to catch generic Exception -- if we knew what the error
+            # in the runner was, we could have fixed it in the first place.
+            self._handle_runner_error(sys.exc_info())
             raise e
+
+    # See `format_exc_info`.
+    def _handle_runner_error(self, exc_info):
+        # User errors definitely need to be sent back to the Rust process, so
+        # they can be sent further to the user and displayed.
+
+        # Package error sending is more of a nice-to-have, but helps users
+        # report bugs to package authors.
+
+        # Runner errors just need to reach our logs, so their contents don't
+        # really need to be sent back, but it's still good to notify the
+        # Rust process that a runner error occurred, so it immediately knows
+        # that the runner exited.
+
+        error = f"Runner error: {format_exc_info(exc_info)}"
+        logging.error(error)  # First make sure the error gets logged; then try to send it.
+        self.messenger.send_runner_error(error)
+        time.sleep(2)  # Give the Rust process time to receive the error message.
+        self.kill()
+
+    # See `format_exc_info`.
+    def _handle_pkg_error(self, pkg, origin, exc_info):
+        error = f"Package `{pkg.name}` {origin}: {format_exc_info(exc_info)}"
+        # TODO: Custom log level(s) for non-engine (i.e. package/user) errors/warnings,
+        #       e.g. `logging.external_error`?
+        logging.error(error)  # First make sure the error gets logged; then try to send it.
+        self.messenger.send_pkg_error(error)
+
+    # Returns whether any user errors occurred during this package's experiment init
+    def _handle_experiment_init_result(self, pkg, result):
+        if not result:
+            return False
+
+        prefix = f"Package `{pkg.name}` experiment init: "
+        warnings = result.get('warnings')
+        if warnings is not None:
+            warnings = tuple(prefix+w for w in warnings)
+            self.messenger.send_user_warnings(warnings)
+
+        errors = result.get('errors')
+        if errors is not None:
+            errors = tuple(prefix+e for e in errors)
+            self.messenger.send_user_errors(errors)
+            return True
+
+        return False
 
     def start_experiment(self):
         init = self.messenger.recv_init()
@@ -40,9 +89,19 @@ class Runner:
             )
 
             if pkg.start_experiment is not None:
-                pkg.start_experiment(
-                    pkg.experiment, config.payload, experiment_ctx
-                )
+                try:
+                    result = pkg.start_experiment(
+                        pkg.experiment, config.payload, experiment_ctx
+                    )
+                    were_user_errors = self._handle_experiment_init_result(pkg, result)
+                    if were_user_errors:
+                        # TODO: Should we kill the runner here or let the Rust process
+                        #       terminate it after receiving user errors?
+                        return experiment_ctx
+
+                except Exception:
+                    # Have to catch generic exception, because package could throw anything.
+                    self._handle_pkg_error(pkg, "experiment init", sys.exc_info())
 
         return experiment_ctx
 
@@ -58,9 +117,9 @@ class Runner:
                     if r and (pkg.type == "context" or pkg.type == "state"):
                         sim.maybe_add_custom_fns(r, "loaders", pkg)
                         sim.maybe_add_custom_fns(r, "getters", pkg)
-                except Exception as e:  # Have to catch anything
-                    # TODO: Better error string
-                    self.messenger.send_pkg_error(str(e))
+                except Exception:
+                    # Have to catch generic exception, because package could throw anything.
+                    self._handle_pkg_error(pkg, "sim init", sys.exc_info())
 
     def run_task(self, sim_id, group_idx, pkg_id, task_id, task_msg):
         sim = self.sims[sim_id]
@@ -75,15 +134,9 @@ class Runner:
         try:
             # TODO: Pass `task_id` to package?
             continuation = pkg.run_task(pkg.experiment, pkg.sims[sim_id], task_msg, state, ctx) or {}
-
         except Exception:
             # Have to catch generic Exception, because package could throw anything.
-            e = str(traceback.format_exception(*sys.exc_info()))
-            e = f"Package {pkg.name} error: {e}"
-            # TODO: Custom log level(s) for non-engine (i.e. package/user) errors/warnings,
-            #       e.g. `logging.external_error`?
-            logging.error(e)
-            self.messenger.send_pkg_error(e)
+            self._handle_pkg_error(pkg, "run_task", sys.exc_info())
             return
 
         changes = state.flush_changes(sim.schema)
