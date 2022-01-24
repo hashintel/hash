8f564749b56b75af6c6e500b03979bc1ad61887a
litvand <al@hash.ai>
2022-01-24 17:22:11 +0200

    Fix Python behavior execution

--- packages/engine/src/simulation/package/state/packages/behavior_execution/package.py
+++ packages/engine/src/simulation/package/state/packages/behavior_execution/package.py
@@ -2,22 +2,23 @@ import sys
 import traceback
 
 
-def hash_behavior_id(behavior_id):
-    # `behavior_id[0]` is language index and we support fewer than 10 languages.
-    # TODO: Either keep in sync with behavior id generation in Rust part of
-    #       behavior execution package or change Rust part to generate
+def _hash_behavior_id(lang_index, id_within_lang):
+    # TODO: Either keep in sync with behavior id generation on Rust side of
+    #       behavior execution package or change Rust side to generate
     #       single numbers (rather than pairs of numbers) in the first place.
-    return (behavior_id[0] * 10) + behavior_id[1]
+
+    # We support fewer than 10 languages.
+    return id_within_lang*10 + lang_index
 
 
 # `behavior_descs` should be a list of objects that have fields `id`, `name`, `source`, `columns`,
 # `language` and `dyn_access`.
-def load_behaviors(behavior_descs):
+def _load_behaviors(behavior_descs):
     behaviors = {}
     warnings = []  # TODO: Accumulate warnings automatically with something like `hash_util.warn`
     for desc in behavior_descs:
-        if desc['language'] != "py":
-            behaviors[hash_behavior_id(desc['id'])] = {
+        if desc['language'] != "Python":
+            behaviors[_hash_behavior_id(desc['id'][0], desc['id'][1])] = {
                 "name": desc['name'],
                 "language": desc['language'],
             }
@@ -26,15 +27,15 @@ def load_behaviors(behavior_descs):
         try:
             # behavior_globals should contain a callable `behavior` if the user's code is correct
             behavior_globals = {}
-            bytecode = compile(desc['source'].decode("utf-8"), desc['name'], "exec")
+            bytecode = compile(desc['source'], desc['name'], "exec")
             exec(bytecode, behavior_globals)
             behavior_fn = behavior_globals.get("behavior")
             
             if callable(behavior_fn):
-                behaviors[hash_behavior_id(desc['id'])] = {
+                behaviors[_hash_behavior_id(desc['id'][0], desc['id'][1])] = {
                     "name": desc['name'],
                     "language": desc['language'],
-                    "required_col_names": desc['columns'],
+                    "required_col_names": None,  # TODO: Propagate desc['columns'] here.
                     "dyn_access": desc['dyn_access'],
                     "fn": behavior_fn
                 }
@@ -42,7 +43,7 @@ def load_behaviors(behavior_descs):
                 warnings.append(
                     f"Couldn't load behavior: No function named 'behavior': {desc['name']}"
                 )
-            
+
         except Exception:
             # Have to catch generic `Exception`, because user's code could throw anything.
             n_pkg_fns = 2
@@ -58,19 +59,19 @@ def load_behaviors(behavior_descs):
     return behaviors, warnings
 
 
-def start_experiment(experiment, init_message, experiment_context):
-    experiment['behaviors'], warnings = load_behaviors(init_message)
+def start_experiment(experiment, init_message, _experiment_context):
+    experiment['behaviors'], warnings = _load_behaviors(init_message)
     return {
         "warnings": warnings
     }
 
 
-def format_behavior_error(behavior_name, exc_info):
+def _format_behavior_error(behavior_name, exc_info):
     n_pkg_fns = 2
-    return f"Behavior error: {traceback.format_exception(*exc_info)[n_pkg_fns:]}"
+    return f"Behavior `{behavior_name}` error: {traceback.format_exception(*exc_info)[n_pkg_fns:]}"
 
 
-def postprocess(agent_state):
+def _postprocess(agent_state):
     msgs = agent_state.messages
     for m in msgs:
         # Types are checked here because flush might not happen until
@@ -78,68 +79,88 @@ def postprocess(agent_state):
         # and if type becomes correct in the meantime, the error won't
         # be raised at all, so whether an error is raised would depend
         # on the rest of the behavior chain, not just the current behavior.
+        # TODO: OPTIM But maybe delay type-checking until flush after all
+        #       if it has a big performance impact, since it wouldn't affect
+        #       correct user code and isn't very likely to affect incorrect
+        #       code.
 
         if isinstance(m["to"], str):
             m["to"] = [m["to"]]
         elif type(m["to"]) != list:
-            raise TypeError("Message `to` field must be list: " + str(m))
+            raise TypeError(f"Message `to` field must be a list or a string: {m}")
 
         if not isinstance(m["type"], str):
-            raise TypeError("Message `type` field must be list: " + str(m))
+            raise TypeError(f"Message `type` field must be a string: {m}")
 
-    # TODO: `if agent_state._hasattr('position'):` unnecessary because built-in?
-    position = agent_state.position
-    if position is not None:
-        while len(position) < 3:
-            position.append(0.0)
+    # TODO: OPTIM hasattr outside of loop over agents might be better than
+    #       this try/except inside the loop.
+    try:
+        position = agent_state.position
+        if position is not None:  # TODO: OPTIM Is position nullable?
+            while len(position) < 3:
+                position.append(0.0)
+    except AttributeError:
+        pass
 
-    direction = agent_state.direction
-    if direction is not None:
-        while len(direction) < 3:
-            direction.append(0.0)
+    # TODO: OPTIM Same todos as for position
+    try:
+        direction = agent_state.direction
+        if direction is not None:
+            while len(direction) < 3:
+                direction.append(0.0)
+    except AttributeError:
+        pass
 
 
-def run_task(experiment, sim, _task_message, group_state, group_context):
+# For each agent in the given group, execute behaviors and postprocess state,
+# starting after the last behavior already executed (during this step / more generally
+# behavior execution package call) and stopping when all behaviors are executed or
+# the next behavior is in a different language (i.e. not Python).
+def run_task(experiment, _sim, _task_message, group_state, group_context):
     next_lang = None
     agent_state = None
     agent_context = None
-    
+
+    # TODO: Propagate field specs to runners and use in state and context objects
+    behavior_ids_field_key = '_PRIVATE_14_behavior_ids'
+
     for i_agent in range(group_state.n_agents()):
-    
-        # Reuse `agent_state` object.
+        # Reuse `agent_state` and `agent_context` objects.
         agent_state = group_state.get_agent(i_agent, agent_state)
+        agent_context = group_context.get_agent(i_agent, agent_context)
 
-        behavior_ids = agent_state.__behaviors
-        i_behavior = agent_state.__i_behavior # Need `i_behavior` outside loop scope.
-        while i_behavior < len(behavior_ids):
-            agent_state.__i_behavior = i_behavior
+        # ids of behaviors of this agent
+        behavior_ids = getattr(agent_state, behavior_ids_field_key)
 
-            behavior = experiment.behaviors[hash_behavior_id(behavior_ids[i_behavior])]
-            if behavior.language != "js":
-                next_lang = behavior.language  # Multiple assignments are fine.
+        # `behavior_index` is the index of the first behavior that
+        # hasn't been executed yet (during this step / package call).
+        for i_behavior in range(int(agent_state.behavior_index), len(behavior_ids)):
+            agent_state.behavior_index = i_behavior  # Keep up to date for use in behaviors.
+
+            # Behavior ids are shallow-loaded as an optimization, so
+            # need to convert to a Python object here.
+            # TODO: OPTIM Use `.values` attribute after upgrading Arrow.
+            b_id = behavior_ids[i_behavior].as_py()
+            behavior = experiment['behaviors'][_hash_behavior_id(b_id[0], b_id[1])]
+            if behavior['language'] != "Python":
+                next_lang = behavior['language']  # Multiple assignments are fine.
                 break
-            
-            group_state.set_dynamic_access(behavior.dyn_access)
-            agent_context = group_context.get_agent(i_agent, agent_context)    
-    
+
+            agent_state.set_dynamic_access(behavior['dyn_access'])
             try:
-                behavior.fn(agent_state, agent_context)
-                postprocess(agent_state)
+                behavior['fn'](agent_state, agent_context)
+                _postprocess(agent_state)
                 
-            except Exception as e:
+            except Exception:
                 # Have to catch generic `Exception`, because user's code could throw anything.
                 
-                error = format_behavior_error(behavior.name, sys.exc_info())
-                agent_state.__i_behavior = i_behavior
+                error = _format_behavior_error(behavior['name'], sys.exc_info())
+                agent_state.behavior_index = i_behavior
                 return {
-                    "target": "main",
+                    "target": "Main",
                     "errors": [error]
                 }
 
-            i_behavior += 1
-
-        agent_state.__i_behavior = i_behavior
-
     return {
-        "target": next_lang if next_lang is not None else "main"
+        "target": next_lang if next_lang is not None else "Main"
     }
