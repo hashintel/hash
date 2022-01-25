24e78d9a60a27358ba29bad9f092802882dddcb6
Tim Diekmann <tim.diekmann@3dvision.de>
2022-01-25 14:09:43 +0100

    Unify Python behavior styling

--- packages/engine/tests/units/state/edit/non_existing_field/src/behaviors/test.py
+++ packages/engine/tests/units/state/edit/non_existing_field/src/behaviors/test.py
@@ -1,5 +1,3 @@
-#
-# Reading non-existing field `non_existing_field`
-#
 def behavior(state, context):
-  state.set("field_to_be_set", state.get("non_existing_field"))
+    """Reading non-existing field `non_existing_field`"""
+    state.set("field_to_be_set", state.get("non_existing_field"))
