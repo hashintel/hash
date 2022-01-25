b9851a5aebe20e56fa66212fd75f994ed428afee
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-01-25 18:44:46 +0100

    Add Python integration tests for datasets (#220)

--- packages/engine/tests/units/data/csv/mod.rs
+++ packages/engine/tests/units/data/csv/mod.rs
@@ -4,3 +4,10 @@ mod js {
     run_test!(access, JavaScript);
     run_test!(immutable, JavaScript);
 }
+
+mod py {
+    use crate::run_test;
+
+    run_test!(access, Python, #[ignore]);
+    run_test!(immutable, Python, #[ignore]);
+}
