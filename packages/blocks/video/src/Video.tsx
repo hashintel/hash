d9ae0d76a63abe54252ebe51763a12cec1d26616
Akash Joshi <akash.joshi@viit.ac.in>
2022-01-27 20:04:53 +0530

    feat: create fallbacks and examples for media blocks

--- packages/blocks/video/src/Video.tsx
+++ packages/blocks/video/src/Video.tsx
@@ -20,6 +20,7 @@ type FileType = Awaited<ReturnType<BlockProtocolUploadFileFunction>>;
 
 type AppProps = {
   initialCaption?: string;
+  url?: string;
 };
 
 type BlockProtocolUpdateEntitiesActionData = Pick<
@@ -93,6 +94,7 @@ export const Video: BlockComponent<AppProps> = (props) => {
     linkedEntities,
     uploadFile,
     updateEntities,
+    url,
   } = props;
 
   // TODO: Consider replacing multiple states with useReducer()
@@ -288,7 +290,7 @@ export const Video: BlockComponent<AppProps> = (props) => {
     });
   };
 
-  if (stateObject.src?.trim()) {
+  if (stateObject.src?.trim() || url) {
     return (
       <div className={tw`flex justify-center text-center w-full`}>
         <div className={tw`max-w-full`}>
@@ -298,7 +300,7 @@ export const Video: BlockComponent<AppProps> = (props) => {
             style={{
               maxWidth: "100%",
             }}
-            src={stateObject.src}
+            src={stateObject.src ? stateObject.src : url}
           />
 
           <input
--- packages/blocks/video/src/webpack-dev-server.tsx
+++ packages/blocks/video/src/webpack-dev-server.tsx
@@ -77,9 +77,12 @@ const App = () => {
         accountId="account-asdasd"
         createLinks={createLinks}
         deleteLinks={deleteLinks}
-        initialCaption="ASDASDASDSAD"
+        initialCaption="A blooming flower"
         entityId="entity-asdasd"
         uploadFile={uploadFile}
+        url={
+          "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm"
+        }
       />
     </div>
   );
