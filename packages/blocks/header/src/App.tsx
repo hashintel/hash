33f06b47ef64701d6f313192c91502224aebb0d7
Akash Joshi <akash.joshi@viit.ac.in>
2022-01-27 17:04:47 +0530

    fix: para and header rendering in hub

--- packages/blocks/header/src/App.tsx
+++ packages/blocks/header/src/App.tsx
@@ -5,20 +5,28 @@ type AppProps = {
   color?: string;
   level?: number;
   editableRef?: RefCallback<HTMLElement>;
+  text?: string;
 };
 
 export const App: BlockComponent<AppProps> = ({
   color,
   level = 1,
   editableRef,
+  text,
 }) => {
   // @todo set type correctly
   const Header = `h${level}` as any;
 
-  return (
+  return editableRef ? (
     <Header
       style={{ fontFamily: "Arial", color: color ?? "black", marginBottom: 0 }}
       ref={editableRef}
     />
+  ) : (
+    <Header
+      style={{ fontFamily: "Arial", color: color ?? "black", marginBottom: 0 }}
+    >
+      {text}
+    </Header>
   );
 };
