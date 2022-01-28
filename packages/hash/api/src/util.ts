846fec8574acf67b39ab7cc6876d0074ce7b1ab9
Ahmad Sattar <thehabbos007@gmail.com>
2022-01-28 11:24:09 +0100

    Move utility functions

--- packages/hash/api/src/util.ts
+++ packages/hash/api/src/util.ts
@@ -1,8 +1,13 @@
 import { JSONObject } from "blockprotocol";
 import { Uuid4 } from "id128";
-import { uniq } from "lodash";
 import { CreateEntityArgs } from "./model";
 import { isSystemType } from "./types/entityTypes";
+
+export {
+  DefaultMap,
+  topologicalSort,
+  treeFromParentReferences,
+} from "@hashintel/hash-shared/util";
 
 /**
  * Generate a new ID.
@@ -24,26 +29,6 @@ export const exactlyOne = (...items: any[]): boolean =>
   items
     .map((val) => val !== null && val !== undefined)
     .reduce((acc, val) => (val ? 1 : 0) + acc, 0) === 1;
-
-/** A `Map` which creates a default value if the value for a key is not set. */
-export class DefaultMap<K, V> extends Map<K, V> {
-  private makeDefault: () => V;
-
-  constructor(makeDefault: () => V) {
-    super();
-    this.makeDefault = makeDefault;
-  }
-
-  get = (key: K) => {
-    let value = super.get(key);
-    if (value) {
-      return value;
-    }
-    value = this.makeDefault();
-    super.set(key, value);
-    return value;
-  };
-}
 
 export const isRecord = (thing: unknown): thing is Record<string, any> => {
   if (typeof thing !== "object") {
@@ -113,60 +98,6 @@ export const createEntityArgsBuilder = (params: {
   }
 
   return args;
-};
-
-/**
- * Perform a topological sort [1] on an array of `edges` forming a directed acyclic graph.
- * [1] https://en.wikipedia.org/wiki/Topological_sorting
- * @returns a topological ordering of the nodes represented by the graph in `edges`.
- * @throws an error if the graph has a cycle.
- * */
-export const topologicalSort = <T>(edges: [T, T][]) => {
-  // Implementation based on Kahn's algorithm. See:
-  // https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm)
-
-  // Find nodes with no incoming edge, i.e. the root node(s)
-  const nonRootNodes = new Set(edges.map(([_, node]) => node));
-  const rootNodes = uniq(
-    edges.map(([node, _]) => node).filter((node) => !nonRootNodes.has(node)),
-  );
-
-  // Pre-compute the incoming and outgoing edges for each node as an optimization
-  const outgoingEdges = new DefaultMap<T, Set<T>>(() => new Set());
-  for (const [n1, n2] of edges) {
-    outgoingEdges.get(n1).add(n2);
-  }
-  const incomingEdges = new DefaultMap<T, Set<T>>(() => new Set());
-  for (const [n1, n2] of edges) {
-    incomingEdges.get(n2).add(n1);
-  }
-
-  const sort = [];
-  const stack = [...rootNodes];
-
-  while (stack.length > 0) {
-    const node = stack.pop()!;
-    sort.push(node);
-    const nodeOutgoing = outgoingEdges.get(node);
-    for (const child of nodeOutgoing.values()) {
-      const childIncoming = incomingEdges.get(child)!;
-      // Remove the (node, child) edge from the graph
-      nodeOutgoing.delete(child);
-      childIncoming.delete(node);
-      if (childIncoming.size === 0) {
-        stack.push(child);
-      }
-    }
-  }
-
-  // If there are any edges left, then the graph is not acyclic
-  for (const [_, children] of outgoingEdges.entries()) {
-    if (children.size !== 0) {
-      throw new Error("graph is not acyclic");
-    }
-  }
-
-  return sort;
 };
 
 /** Returns the set intersection of `left` and `right`. */
@@ -308,60 +239,3 @@ export const linkedTreeFlatten = <
  * @param reference is the name of the parent reference property on the element
  * @param recursive is the name of the property that deals with children of an element. This will be populated with the tree structure.
  */
-export const treeFromParentReferences = <
-  Element extends {
-    [_ in Key]: string;
-  } & { [_ in Ref]?: string } & { [_ in Rec]?: Element[] },
-  Key extends string,
-  Ref extends string,
-  Rec extends string,
->(
-  elements: Element[],
-  key: Key,
-  reference: Ref,
-  recursive: Rec,
-) => {
-  const topologicallySorted = topologicalSort(
-    elements
-      .filter((element) => element[reference] != null)
-      .map((element) => [element[key] as string, element[reference]]),
-  );
-
-  const mapping = new Map<string, Element>();
-
-  for (const element of elements) {
-    mapping.set(element[key], element);
-  }
-
-  for (const currentId of topologicallySorted) {
-    const current = mapping.get(currentId);
-    if (!current) {
-      continue;
-    }
-    const existingParent = mapping.get(current[reference]);
-    if (!existingParent) {
-      (current[reference] as string | undefined) = undefined;
-      continue;
-    }
-
-    /**
-     * Topological sorting handles the case of cyclic references.
-     * Otherwise a check like the following could be relevant.
-     * if ((current[key] as string) === existingParent[reference]) {
-     *    throw new Error(
-     *      `Circular references given. Unable to reconstruct tree at ${current[key]}`,
-     *    );
-     *  }
-     * */
-
-    if (existingParent[recursive]) {
-      existingParent[recursive].push(current);
-    } else {
-      (existingParent[recursive] as Element[]) = [current];
-    }
-  }
-
-  return Array.from(mapping.values()).filter(
-    (element) => element[reference] == null,
-  );
-};
--- packages/hash/shared/src/util.ts
+++ packages/hash/shared/src/util.ts
@@ -1,8 +1,7 @@
 // @todo this should be defined elsewhere
 
+import { uniq } from "lodash";
 import { FileProperties } from "./graphql/apiTypes.gen";
-
-export { topologicalSort, treeFromParentReferences } from "../../api/src/util";
 
 export type DistributiveOmit<T, K extends keyof any> = T extends any
   ? Omit<T, K>
@@ -61,5 +60,137 @@ export const isFileProperties = (props: {}): props is FileProperties => {
     "size" in props &&
     "url" in props &&
     "storageType" in props
+  );
+};
+
+/** A `Map` which creates a default value if the value for a key is not set. */
+export class DefaultMap<K, V> extends Map<K, V> {
+  private makeDefault: () => V;
+
+  constructor(makeDefault: () => V) {
+    super();
+    this.makeDefault = makeDefault;
+  }
+
+  get = (key: K) => {
+    let value = super.get(key);
+    if (value) {
+      return value;
+    }
+    value = this.makeDefault();
+    super.set(key, value);
+    return value;
+  };
+}
+
+/**
+ * Perform a topological sort [1] on an array of `edges` forming a directed acyclic graph.
+ * [1] https://en.wikipedia.org/wiki/Topological_sorting
+ * @returns a topological ordering of the nodes represented by the graph in `edges`.
+ * @throws an error if the graph has a cycle.
+ * */
+export const topologicalSort = <T>(edges: [T, T][]) => {
+  // Implementation based on Kahn's algorithm. See:
+  // https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm)
+
+  // Find nodes with no incoming edge, i.e. the root node(s)
+  const nonRootNodes = new Set(edges.map(([_, node]) => node));
+  const rootNodes = uniq(
+    edges.map(([node, _]) => node).filter((node) => !nonRootNodes.has(node)),
+  );
+
+  // Pre-compute the incoming and outgoing edges for each node as an optimization
+  const outgoingEdges = new DefaultMap<T, Set<T>>(() => new Set());
+  for (const [n1, n2] of edges) {
+    outgoingEdges.get(n1).add(n2);
+  }
+  const incomingEdges = new DefaultMap<T, Set<T>>(() => new Set());
+  for (const [n1, n2] of edges) {
+    incomingEdges.get(n2).add(n1);
+  }
+
+  const sort = [];
+  const stack = [...rootNodes];
+
+  while (stack.length > 0) {
+    const node = stack.pop()!;
+    sort.push(node);
+    const nodeOutgoing = outgoingEdges.get(node);
+    for (const child of nodeOutgoing.values()) {
+      const childIncoming = incomingEdges.get(child)!;
+      // Remove the (node, child) edge from the graph
+      nodeOutgoing.delete(child);
+      childIncoming.delete(node);
+      if (childIncoming.size === 0) {
+        stack.push(child);
+      }
+    }
+  }
+
+  // If there are any edges left, then the graph is not acyclic
+  for (const [_, children] of outgoingEdges.entries()) {
+    if (children.size !== 0) {
+      throw new Error("graph is not acyclic");
+    }
+  }
+
+  return sort;
+};
+
+export const treeFromParentReferences = <
+  Element extends {
+    [_ in Key]: string;
+  } & { [_ in Ref]?: string } & { [_ in Rec]?: Element[] },
+  Key extends string,
+  Ref extends string,
+  Rec extends string,
+>(
+  elements: Element[],
+  key: Key,
+  reference: Ref,
+  recursive: Rec,
+) => {
+  const topologicallySorted = topologicalSort(
+    elements
+      .filter((element) => element[reference] != null)
+      .map((element) => [element[key] as string, element[reference]]),
+  );
+
+  const mapping = new Map<string, Element>();
+
+  for (const element of elements) {
+    mapping.set(element[key], element);
+  }
+
+  for (const currentId of topologicallySorted) {
+    const current = mapping.get(currentId);
+    if (!current) {
+      continue;
+    }
+    const existingParent = mapping.get(current[reference]);
+    if (!existingParent) {
+      (current[reference] as string | undefined) = undefined;
+      continue;
+    }
+
+    /**
+     * Topological sorting handles the case of cyclic references.
+     * Otherwise a check like the following could be relevant.
+     * if ((current[key] as string) === existingParent[reference]) {
+     *    throw new Error(
+     *      `Circular references given. Unable to reconstruct tree at ${current[key]}`,
+     *    );
+     *  }
+     * */
+
+    if (existingParent[recursive]) {
+      existingParent[recursive].push(current);
+    } else {
+      (existingParent[recursive] as Element[]) = [current];
+    }
+  }
+
+  return Array.from(mapping.values()).filter(
+    (element) => element[reference] == null,
   );
 };
