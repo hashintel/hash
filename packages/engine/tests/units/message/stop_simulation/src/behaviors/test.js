b8a4396486e8a2ac55c1a3eee70139d3931fd09b
Tim Diekmann <tim.diekmann@3dvision.de>
2022-01-28 15:32:49 +0100

    Revert "Let the CI fail for example output"
    This reverts commit 8e684e7ae866d4428b3b958c2c6e9616c2c7b019.

--- packages/engine/tests/units/message/stop_simulation/src/behaviors/test.js
+++ packages/engine/tests/units/message/stop_simulation/src/behaviors/test.js
@@ -8,7 +8,7 @@ const behavior = (state, context) => {
         to: "hash",
         type: "stop",
         data: {
-          status: "error",
+          status: "success",
           reason: "test",
         },
       },
