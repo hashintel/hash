98938c77d954684fce383d53324a53a4fc4f7c06
Tim Diekmann <tim.diekmann@3dvision.de>
2022-02-02 15:35:28 +0100

    Remove optimization experiments

--- packages/engine/tests/popular/mod.rs
+++ packages/engine/tests/popular/mod.rs
@@ -2,10 +2,6 @@
 mod interconnected_call_center {
     use crate::run_test;
 
-    // optimization experiment is not implemented
-    run_test!(interconnected_call_center, experiment: optimize_number_of_centers, #[ignore]);
-    // optimization experiment is not implemented
-    run_test!(interconnected_call_center, experiment: optimize_transfer_capacity, #[ignore]);
     // Bug: https://app.asana.com/0/1201707629991362/1201756436717252/f
     run_test!(interconnected_call_center, experiment: call_time_linspace, #[ignore]);
     // Bug: https://app.asana.com/0/1201707629991362/1201756436717252/f
