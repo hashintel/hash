9d850cb23e00eb2e382a3422116b6fea4e1161fe
Tim Diekmann <tim.diekmann@3dvision.de>
2022-02-07 15:16:10 +0100

    Apply rustfmt

--- packages/engine/tests/experiment/mod.rs
+++ packages/engine/tests/experiment/mod.rs
@@ -207,8 +207,12 @@ pub async fn run_test_suite(
             {
                 expected
                     .assert_subset_of(&states, &globals, &analysis)
-                    .unwrap_or_else(|_| panic!("Output of simulation {} does not match expected output in experiment",
-                        output_idx + 1));
+                    .unwrap_or_else(|_| {
+                        panic!(
+                            "Output of simulation {} does not match expected output in experiment",
+                            output_idx + 1
+                        )
+                    });
             }
             timings.push(test_result.duration.as_nanos());
         }
