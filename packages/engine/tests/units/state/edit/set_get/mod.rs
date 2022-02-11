a06758cf75da55a83e5d3bf5515b147ccbfca29f
Tim Diekmann <tim.diekmann@3dvision.de>
2022-02-11 17:27:30 +0100

    Add a test for fixed size list of numbers with set/get notation

--- packages/engine/tests/units/state/edit/set_get/mod.rs
+++ packages/engine/tests/units/state/edit/set_get/mod.rs
@@ -3,8 +3,10 @@ mod js {
 
     run_test!(bool, JavaScript);
     run_test!(bool_array, JavaScript);
+    run_test!(bool_array_fixed, JavaScript);
     run_test!(number, JavaScript);
     run_test!(number_array, JavaScript);
+    run_test!(number_array_fixed, JavaScript);
     run_test!(string, JavaScript);
     run_test!(object, JavaScript);
     run_test!(object_array, JavaScript);
@@ -15,8 +17,10 @@ mod py {
 
     run_test!(bool, Python, #[ignore]);
     run_test!(bool_array, Python, #[ignore]);
+    run_test!(bool_array_fixed, Python, #[ignore]);
     run_test!(number, Python, #[ignore]);
     run_test!(number_array, Python, #[ignore]);
+    run_test!(number_array_fixed, Python, #[ignore]);
     run_test!(string, Python, #[ignore]);
     run_test!(object, Python, #[ignore]);
     run_test!(object_array, Python, #[ignore]);
