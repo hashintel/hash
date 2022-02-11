d96aae5efa374f1bf3de684fa5aad4a596929049
Tim Diekmann <tim.diekmann@3dvision.de>
2022-02-11 18:58:37 +0100

    Add a test for list of strings with index notation

---
+++ packages/engine/tests/units/state/edit/index/string_array/src/behaviors/test.py
@@ -0,0 +1,10 @@
+def behavior(state, context):
+    """Gets and sets a string array using index notation"""
+    state["s1_is_list"] = type(state["s1"]) is list
+    state["s1_0_is_string"] = type(state["s1"][0]) is str
+
+    state["s2"] = state["s1"] + ["buzz"]
+    state["s1"].insert(0, "bazz")
+
+    state["s2_is_list"] = type(state["s1"]) is list
+    state["s2_0_is_string"] = type(state["s2"][0]) is str
