38ee12fa2f72fd4242d8ccb889afe90b8310fd2d
Tim Diekmann <tim.diekmann@3dvision.de>
2022-02-14 14:41:40 +0100

    Improve checking for array type in JS state tests

--- packages/engine/tests/units/state/empty/array/src/behaviors/test.js
+++ packages/engine/tests/units/state/empty/array/src/behaviors/test.js
@@ -2,6 +2,6 @@
  * Verifies the type of the list
  */
 const behavior = (state, context) => {
-  state.l1_is_list = typeof state.l1 === "object";
+  state.l1_is_list = Array.isArray(state.l1);
   state.l1_is_empty = state.l1.length === 0;
 };
