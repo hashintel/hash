d9b3de34e2799a28c7c229413b48639e27247545
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-02-15 16:26:20 +0000

    Adding a webpack project to produce an apache-arrow bundle

---
+++ packages/engine/arrow-js/src/index.js
@@ -0,0 +1,2 @@
+export * from "apache-arrow";
+export { TextEncoder, TextDecoder } from "@zxing/text-encoding";
---
+++ packages/engine/arrow-js/webpack.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle_arrow.js",
+    library: "bundle_arrow",
+  },
+  mode: "production",
+};
