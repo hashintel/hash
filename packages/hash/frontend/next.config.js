e4c4e803da3001ef7b89037b23dcfa0fa535322f
Alexander Kachkaev <alexander@kachkaev.ru>
2022-02-17 17:01:53 +0000

    Cleanup next.config.js

--- packages/hash/frontend/next.config.js
+++ packages/hash/frontend/next.config.js
@@ -65,13 +65,13 @@ module.exports = withSentryConfig(
       },
       sassOptions: {
         prependData: `
-      $grey-bg: rgba(241, 243, 246, 0.3);
-      $grey-border: #e5e6e7;
-      $black-almost: #1b1d24;
-      
-      $bright-purple: rgb(95, 71, 255);
-      $bright-pink: #ff008b;
-      $bright-blue: #2482ff;
+          $grey-bg: rgba(241, 243, 246, 0.3);
+          $grey-border: #e5e6e7;
+          $black-almost: #1b1d24;
+
+          $bright-purple: rgb(95, 71, 255);
+          $bright-pink: #ff008b;
+          $bright-blue: #2482ff;
       `,
       },
     }),
