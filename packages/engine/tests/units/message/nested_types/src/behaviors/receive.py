61bf08d700fa23dddd68438a167aef613829c2d3
Alfred Mountfield <am@hash.ai>
2022-02-18 17:49:05 +0000

    Re-enabling Tests (#329)
    * Fixing nested_types unit integration test
    
    * Re-enabling published_display_behaviors and message::nested_types

--- packages/engine/tests/units/message/nested_types/src/behaviors/receive.py
+++ packages/engine/tests/units/message/nested_types/src/behaviors/receive.py
@@ -5,13 +5,4 @@ def behavior(state, context):
     if len(messages) > 0:
         data = messages[0]["data"]
 
-        state.number = data["struct"]["number"]
-        state.string = data["struct"]["string"]
-        state.bool = data["struct"]["bool"]
-        state.struct = data["struct"]["struct"]
-        state.number_array = data["struct"]["number_array"]
-        state.bool_array = data["struct"]["bool_array"]
-        state.struct_array = data["struct"]["struct_array"]
-        state.fixed_number_array = data["struct"]["fixed_number_array"]
-        state.fixed_bool_array = data["struct"]["fixed_bool_array"]
-        state.fixed_struct_array = data["struct"]["fixed_struct_array"]
+        state.struct = data["struct"]
