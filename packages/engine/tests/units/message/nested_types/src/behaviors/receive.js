e93c930520f55a1221c51060c6c57119f388832e
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-02-18 16:59:18 +0000

    Fixing nested_types unit integration test

--- packages/engine/tests/units/message/nested_types/src/behaviors/receive.js
+++ packages/engine/tests/units/message/nested_types/src/behaviors/receive.js
@@ -7,15 +7,17 @@ const behavior = (state, context) => {
   if (messages.length > 0) {
     const data = messages[0].data;
 
-    state.number = data.struct.number;
-    state.string = data.struct.string;
-    state.bool = data.struct.bool;
-    state.struct = data.struct.struct;
-    state.number_array = data.struct.number_array;
-    state.bool_array = data.struct.bool_array;
-    state.struct_array = data.struct.struct_array;
-    state.fixed_number_array = data.struct.fixed_number_array;
-    state.fixed_bool_array = data.struct.fixed_bool_array;
-    state.fixed_struct_array = data.struct.fixed_struct_array;
+    state.struct = {
+      number: data.struct.number,
+      string: data.struct.string,
+      bool: data.struct.bool,
+      struct: data.struct.struct,
+      number_array: data.struct.number_array,
+      bool_array: data.struct.bool_array,
+      struct_array: data.struct.struct_array,
+      fixed_number_array: data.struct.fixed_number_array,
+      fixed_bool_array: data.struct.fixed_bool_array,
+      fixed_struct_array: data.struct.fixed_struct_array,
+    };
   }
 };
