8cc42d8e5b4bd11f88424aa7f8033372a7f60ca0
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-02-18 17:31:48 +0000

    Cleaning up nested_types

--- packages/engine/tests/units/message/nested_types/src/behaviors/receive.js
+++ packages/engine/tests/units/message/nested_types/src/behaviors/receive.js
@@ -7,17 +7,6 @@ const behavior = (state, context) => {
   if (messages.length > 0) {
     const data = messages[0].data;
 
-    state.struct = {
-      number: data.struct.number,
-      string: data.struct.string,
-      bool: data.struct.bool,
-      struct: data.struct.struct,
-      number_array: data.struct.number_array,
-      bool_array: data.struct.bool_array,
-      struct_array: data.struct.struct_array,
-      fixed_number_array: data.struct.fixed_number_array,
-      fixed_bool_array: data.struct.fixed_bool_array,
-      fixed_struct_array: data.struct.fixed_struct_array,
-    };
+    state.struct = data.struct;
   }
 };
--- packages/engine/tests/units/message/nested_types/src/behaviors/receive.py
+++ packages/engine/tests/units/message/nested_types/src/behaviors/receive.py
@@ -5,16 +5,4 @@ def behavior(state, context):
     if len(messages) > 0:
         data = messages[0]["data"]
 
-        state.struct = {
-            "number": data["struct"]["number"],
-            "string": data["struct"]["string"],
-            "bool": data["struct"]["bool"],
-            "struct": data["struct"]["struct"],
-            "number_array": data["struct"]["number_array"],
-            "bool_array": data["struct"]["bool_array"],
-            "struct_array": data["struct"]["struct_array"],
-            "fixed_number_array": data["struct"]["fixed_number_array"],
-            "fixed_bool_array": data["struct"]["fixed_bool_array"],
-            "fixed_struct_array": data["struct"]["fixed_struct_array"]
-        }
-
+        state.struct = data["struct"]
