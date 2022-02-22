23c78ab71e37057a4e561caf9f06d488f373e46a
Ahmad Sattar <thehabbos007@gmail.com>
2022-02-22 11:35:10 +0100

    Document types and util functions

--- packages/hash/api/src/db/postgres/types.ts
+++ packages/hash/api/src/db/postgres/types.ts
@@ -3,12 +3,14 @@ import {
   DatabaseTransactionConnectionType,
 } from "slonik";
 
+// The variant kinds are distinct types.
+// Enum and union types unify at the top level type, which isn't desired for these typings.
 export type PoolConnectionKind = "PoolConnectionKind";
 export type TransactionConnectionKind = "TransactionConnection";
 
 /**
- * The types given for different connection kinds cannot be used as values directly.
- * This object maps the connection kind to its desired type.
+ * The types given for {@link PoolConnectionKind} and {@link TransactionConnectionKind} cannot be used as values directly.
+ * This object maps a connection kind to its type.
  */
 export const ConnectionKind = {
   PoolConnection: <PoolConnectionKind>"PoolConnectionKind",
@@ -21,15 +23,33 @@ export type TransactionConnection = Omit<
   "transaction"
 >;
 
+/**
+ * Defines a tagging of type variants such that they can be narrowed at runtime.
+ */
 export type Tag<Typ, UnionTag> = Typ & { _tag: UnionTag };
 
+/**
+ * Postgres connection types.
+ * Note that {@link TransactionConnection} does not allow for nested transactions.
+ * Construction of these types can be done through {@link poolConnection} or {@link transactionConnection}
+ */
 export type Connection =
   | Tag<PoolConnection, PoolConnectionKind>
   | Tag<TransactionConnection, TransactionConnectionKind>;
 
+/**
+ * Construct {@link PoolConnection} variant tagged with {@link PoolConnectionKind}
+ * @param database pool connection
+ * @returns a {@link Connection}
+ */
 export const poolConnection = (connection: PoolConnection): Connection =>
   Object.assign(connection, { _tag: ConnectionKind.PoolConnection });
 
+/**
+ * Construct {@link TransactionConnection} variant tagged with {@link TransactionConnectionKind}
+ * @param database transaction connection
+ * @returns a {@link Connection}
+ */
 export const transactionConnection = (
   connection: TransactionConnection,
 ): Connection =>
--- packages/hash/api/src/db/postgres/util.ts
+++ packages/hash/api/src/db/postgres/util.ts
@@ -3,6 +3,13 @@ import { Connection, ConnectionKind, transactionConnection } from "./types";
 
 export { poolConnection, transactionConnection } from "./types";
 
+/**
+ * Conditionally create transaction or use active transaction depending on connection kind.
+ * If the connection given is a top-level connection i.e. not a transaction, a transaction will be created.
+ * Otherwise we simply make use of the transaction connection as is.
+ * @param connection either top-level or one transaction level.
+ * @returns handler callback
+ */
 export const transaction =
   (connection: Connection) =>
   async <T>(handler: (conn: Connection) => Promise<T>): Promise<T> => {
