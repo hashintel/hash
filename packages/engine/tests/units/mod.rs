ab2d2191aa736a7d6276210e2c3464dc8bf77741
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-02-23 18:31:39 +0000

    Adding `target_group_upper_bound`

--- packages/engine/tests/units/mod.rs
+++ packages/engine/tests/units/mod.rs
@@ -1,6 +1,6 @@
 //! Tests different simulations designed to verify a discrete functionality.
 //!
-//! Each simulation only tries to test a single functionality like setting the state or retreiving
+//! Each simulation only tries to test a single functionality like setting the state or retrieving
 //! neighbors, so in some ways it follows a unit-testing philosophy, but just on user-exposed
 //! functionalities.
 
