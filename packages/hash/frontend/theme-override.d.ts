900ef7a3fe0c5004b81e0366092907de8537ac43
teenoh <ugbalaachinike@gmail.com>
2022-02-24 22:24:10 +0100

    feat: update typography

--- packages/hash/frontend/theme-override.d.ts
+++ packages/hash/frontend/theme-override.d.ts
@@ -1,28 +1,55 @@
 declare module "@mui/material/styles" {
-  type PaletteValue = {
-    10: string;
-    20: string;
-    30: string;
-    40: string;
-    50: string;
-    60: string;
-    70: string;
-    80: string;
-    90: string;
-    100: string;
-  };
   interface Palette {
-    gray: PaletteValue;
+    purple: {
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      subtle: string;
+    };
+    teal: {
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+    };
+    orange: {
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+    };
+    red: {
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+    };
+    gray: {
+      10: string;
+      20: string;
+      30: string;
+      40: string;
+      50: string;
+      60: string;
+      70: string;
+      80: string;
+    };
     grey: undefined;
-    blue: PaletteValue;
-    purple: PaletteValue;
-    red: PaletteValue;
-    orange: PaletteValue;
-    green: PaletteValue;
-    yellow: PaletteValue;
-    pink: PaletteValue;
-    teal: PaletteValue;
-    mint: PaletteValue;
   }
 
   interface TypographyVariants {
@@ -39,72 +66,39 @@ declare module "@mui/material/styles" {
   }
 
   interface TypographyVariantsOptions {
-    bpTitle?: React.CSSProperties;
-    bpSubtitle?: React.CSSProperties;
-    bpHeading1?: React.CSSProperties;
-    bpHeading2?: React.CSSProperties;
-    bpHeading3?: React.CSSProperties;
-    bpHeading4?: React.CSSProperties;
-    bpSmallCaps?: React.CSSProperties;
-    bpLargeText?: React.CSSProperties;
-    bpBodyCopy?: React.CSSProperties;
-    bpSmallCopy?: React.CSSProperties;
-    bpMicroCopy?: React.CSSProperties;
+    title?: React.CSSProperties;
+    h1?: React.CSSProperties;
+    h2?: React.CSSProperties;
+    h3?: React.CSSProperties;
+    h4?: React.CSSProperties;
+    h5?: React.CSSProperties;
+    mediumCaps?: React.CSSProperties;
+    smallCaps?: React.CSSProperties;
+    regularTextParagraphs?: React.CSSProperties;
+    regularTextLabels?: React.CSSProperties;
+    smallTextParagraphs?: React.CSSProperties;
+    smallTextLabels?: React.CSSProperties;
+    microText?: React.CSSProperties;
   }
 }
 
 declare module "@mui/material/Typography" {
   interface TypographyPropsVariantOverrides {
-    bpTitle: true;
-    bpSubtitle: true;
-    bpHeading1: true;
-    bpHeading2: true;
-    bpHeading3: true;
-    bpHeading4: true;
-    bpSmallCaps: true;
-    bpLargeText: true;
-    bpBodyCopy: true;
-    bpSmallCopy: true;
-    bpMicroCopy: true;
+    title: true;
+    h1: true;
+    h2: true;
+    h3: true;
+    h4: true;
+    h5: true;
+    mediumCaps: true;
+    smallCaps: true;
+    regularTextParagaphs: true;
+    regularTextLabels: true;
+    smallTextParagraphs: true;
+    smallTextLabels: true;
+    microText: true;
   }
 }
 
-declare module "@mui/material/Button" {
-  interface ButtonPropsVariantOverrides {
-    transparent: true;
-    primary: true;
-    secondary: true;
-    tertiary: true;
-    // Disable defaults
-    contained: false;
-    outlined: false;
-    text: false;
-  }
-
-  interface ButtonPropsColorOverrides {
-    purple: true;
-    teal: true;
-    warning: true;
-    danger: true;
-    gray: true;
-    // Disable defaults
-    primary: false;
-    secondary: false;
-    success: false;
-    error: false;
-    info: false;
-    warning: false;
-  }
-
-  interface ButtonPropsSizeOverrides {}
-}
-
-declare module "@mui/material/Paper" {
-  interface PaperPropsVariantOverrides {
-    teal: true;
-    purple: true;
-  }
-}
-
-// eslint-disable-next-line import/no-default-export -- @see https://github.com/mui-org/material-ui/issues/28244
+// eslint-disable-next-line import/no-default-export  -- https://github.com/mui-org/material-ui/issues/28244
 export default "";
