0774572b22d4082d31a01583d33e2cc3b060c28c
teenoh <ugbalaachinike@gmail.com>
2022-02-24 16:01:37 +0100

    feat: base setup for mui

--- packages/hash/frontend/assets.d.ts
+++ packages/hash/frontend/assets.d.ts
@@ -1,3 +1,4 @@
+declare module "*.ttf";
 declare module "*.svg" {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   // eslint-disable-next-line import/no-default-export -- third-party requirement
--- packages/hash/frontend/src/pages/_document.page.ts
+++ packages/hash/frontend/src/pages/_document.page.ts
@@ -1,7 +1,9 @@
+import createEmotionServer from "@emotion/server/types/create-instance";
 import withTwindDocument from "@twind/next/document";
 import Document, { DocumentContext } from "next/document";
 
 import twindConfig from "../../twind.config";
+import { createEmotionCache } from "../theme";
 
 class MyDocument extends Document {
   static async getInitialProps(context: DocumentContext) {
@@ -9,5 +11,69 @@ class MyDocument extends Document {
     return { ...initialProps };
   }
 }
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with static-site generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  const originalRenderPage = ctx.renderPage;
+
+  // You can consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  // However, be aware that it can have global side effects.
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        // eslint-disable-next-line react/function-component-definition
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // This is important. It prevents emotion to render invalid HTML.
+  // See https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
 
 export default withTwindDocument(twindConfig, MyDocument);
