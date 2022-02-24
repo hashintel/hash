9e9729256545b2aae4fd76996a9e4c7be6eee17a
teenoh <ugbalaachinike@gmail.com>
2022-02-24 22:24:10 +0100

    feat: update typography

--- packages/hash/frontend/src/theme/typography.ts
+++ packages/hash/frontend/src/theme/typography.ts
@@ -1,91 +1,104 @@
-import { ThemeOptions, createTheme } from "@mui/material";
+import { ThemeOptions } from "@mui/material";
 import { customColors } from "./palette";
-
-const defaultTheme = createTheme();
 
 const fallbackFonts = [`"Helvetica"`, `"Arial"`, "sans-serif"];
 
 export const typography: ThemeOptions["typography"] = {
   fontFamily: [`"Inter"`, ...fallbackFonts].join(", "),
   fontSize: 16,
-  bpTitle: {
+
+  title: {
     fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step-6)",
-    lineHeight: 1,
-    fontWeight: 700,
-    color: customColors.gray["80"],
-  },
-  bpSubtitle: {
-    fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step-3)",
-    lineHeight: 1.1,
-    fontWeight: 200,
-    color: customColors.gray["80"],
-  },
-  bpHeading1: {
-    fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step-5)",
+    // fontSize: "var(--step-6)",
+    fontSize: 61,
     lineHeight: 1.1,
     fontWeight: 700,
     color: customColors.gray["80"],
   },
-  bpHeading2: {
+  h1: {
     fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step-4)",
+    // fontSize: "var(--step-5)",
+    fontSize: 48.8,
+    lineHeight: 1.1,
+    fontWeight: 700,
+    color: customColors.gray["80"],
+  },
+  h2: {
+    fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
+    // fontSize: "var(--step-4)",
+    fontSize: 39,
     fontWeight: 400,
     lineHeight: 1.2,
     color: customColors.gray["80"],
   },
-  bpHeading3: {
+  h3: {
     fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step-3)",
-    lineHeight: 1.1,
+    // fontSize: "var(--step-3)",
+    fontSize: 31.5,
+    lineHeight: 1.2,
     color: customColors.gray["70"],
   },
-  bpHeading4: {
+  h4: {
     fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step-2)",
-    lineHeight: 1.1,
+    // fontSize: "var(--step-2)",
+    fontSize: 24,
+    lineHeight: 1.3,
     color: customColors.gray["70"],
   },
-  bpSmallCaps: {
+  h5: {
     fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
-    fontSize: "var(--step--1)",
+    // fontSize: "var(--step-2)",
+    fontSize: 20,
+    lineHeight: 1.2,
+    color: customColors.gray["70"],
+  },
+  mediumCaps: {
+    fontFamily: [`"Apercu Pro"`, ...fallbackFonts].join(", "),
+    // fontSize: "var(--step--1)",
+    fontSize: 16,
     lineHeight: 1.3,
     color: customColors.gray["70"],
     textTransform: "uppercase",
   },
-  bpLargeText: {
-    fontSize: "var(--step-1)",
-    lineHeight: 1.1,
-    color: customColors.gray["80"],
-  },
-  bpBodyCopy: {
-    fontSize: "var(--step-0)",
-    fontWeight: 400,
-    lineHeight: 1.7,
-    color: customColors.gray["80"],
-    maxWidth: "62ch",
-    "& a": {
-      fontWeight: 600,
-      transition: defaultTheme.transitions.create("color"),
-      color: customColors.purple[600],
-      "&:hover": {
-        color: customColors.purple["600"],
-      },
-    },
-    /** @todo: figure out how to type this */
-  } as any,
-  bpSmallCopy: {
-    fontWeight: 500,
-    fontSize: "var(--step--1)",
+  smallCaps: {
+    // fontSize: "var(--step-1)",
+    fontSize: 12,
     lineHeight: 1.5,
     color: customColors.gray["80"],
   },
-  bpMicroCopy: {
+  regularTextParagraphs: {
+    // fontSize: "var(--step-0)",
+    fontSize: 16,
+    fontWeight: 400,
+    lineHeight: 1.5,
+    color: customColors.gray["80"],
+  },
+  regularTextLabels: {
     fontWeight: 500,
-    fontSize: "var(--step--2)",
+    // fontSize: "var(--step--1)",
+    fontSize: 16,
     lineHeight: 1.1,
+    color: customColors.gray["80"],
+  },
+  smallTextParagraphs: {
+    // fontSize: "var(--step-0)",
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 1.5,
+    color: customColors.gray["80"],
+  },
+  smallTextLabels: {
+    fontWeight: 500,
+    // fontSize: "var(--step--1)",
+    fontSize: 14,
+    lineHeight: 1.3,
+    color: customColors.gray["80"],
+  },
+  microText: {
+    fontWeight: 500,
+    // fontSize: "var(--step--2)",
+    fontSize: 13,
+    lineHeight: 1.4,
     color: customColors.gray["80"],
   },
 };
