a76c3f8f402b9c80ef1f7ea32fbdb20717094bd0
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-03-01 16:58:59 +0100

    Add regression test for current arrow error (#356)

--- packages/engine/tests/units/state/mod.rs
+++ packages/engine/tests/units/state/mod.rs
@@ -3,8 +3,10 @@ mod empty;
 
 mod js {
     crate::run_test!(behavior_index, JavaScript);
+    crate::run_test!(nullable_fixed_size_list, JavaScript);
 }
 
 mod py {
     crate::run_test!(behavior_index, Python, #[ignore]);
+    crate::run_test!(nullable_fixed_size_list, Python, #[ignore]);
 }
