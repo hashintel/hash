a9fee3c98dda5671db269ad93f5937fec57ae57e
Ahmad Sattar <thehabbos007@gmail.com>
2022-03-04 15:05:52 +0100

    Mockdata fix

--- packages/hash/integration/src/mockData/entityTypes.ts
+++ packages/hash/integration/src/mockData/entityTypes.ts
@@ -77,8 +77,7 @@ export const createEntityTypes =
           items: { $ref: entityType.properties.$id },
         };
 
-        entityType = await EntityType.update(db, {
-          entityId: entityType.entityId,
+        entityType = await entityType.update(db, {
           ...baseParams,
           schema: taskEntityType.schema,
         });
