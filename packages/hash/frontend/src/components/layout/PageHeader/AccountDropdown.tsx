4a8015b3056046a31f37f288dd53c9208e022bc0
MaggieAppleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-03-04 13:24:18 +0000

    typography and spacing cleanup

--- packages/hash/frontend/src/components/layout/PageHeader/AccountDropdown.tsx
+++ packages/hash/frontend/src/components/layout/PageHeader/AccountDropdown.tsx
@@ -129,14 +129,13 @@ export const AccountDropdown: VoidFunctionComponent<AccountDropdownProps> = ({
           color: theme.palette.gray[40],
         }}
       >
-        <Box px={2} pb={1} pt={1.5}>
+        <Box px={2} pt={1} pb={1.5}>
           <Typography
             variant="smallTextLabels"
             sx={{
               color: theme.palette.gray[80],
               fontWeight: 700,
             }}
-            mb={0.25}
           >
             {user.properties.preferredName}
           </Typography>
