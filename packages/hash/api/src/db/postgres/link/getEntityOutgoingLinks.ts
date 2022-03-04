62b787469a57db64faf342f1b9509b6f7bc8fe4d
Ben Werner <ben.werner01@gmail.com>
2022-03-04 09:44:21 +0000

    refactor: remove duplicate documentation

--- packages/hash/api/src/db/postgres/link/getEntityOutgoingLinks.ts
+++ packages/hash/api/src/db/postgres/link/getEntityOutgoingLinks.ts
@@ -7,15 +7,6 @@ import {
   DbLinkWithVersionRow,
 } from "./sql/links.util";
 
-/**
- * Get all outgoing links for a versioned or non-versioned source entity
- * that are "active" (i.e. have not been removed), or were "active" at a
- * particular timestamp.
- * @param params.sourceAccountId the account id of the source entity
- * @param params.sourceEntityId the entityId of the source entity
- * @param params.activeAt the timestamp where the links were "active" (optional)
- * @param params.path the path of the link (optional)
- */
 export const getEntityOutgoingLinks = async (
   conn: Connection,
   params: {
