d23d459f537f2aa94ae1d9fde9dc6a90afa77d37
Akash Joshi <akash.joshi@viit.ac.in>
2022-03-07 16:18:10 +0530

    fix: update mobile search icon to be iconbutton

--- packages/hash/frontend/src/components/layout/PageHeader/SearchBar/MobileSearch.tsx
+++ packages/hash/frontend/src/components/layout/PageHeader/SearchBar/MobileSearch.tsx
@@ -1,46 +1,6 @@
-import { Box, Button, InputBase, styled } from "@mui/material";
+import { Box, Button, InputBase, IconButton } from "@mui/material";
 
 import { SearchIcon } from "../../../icons";
-
-// @todo don't use styled when this file and DesktopSearch are merged
-// https://github.com/hashintel/hash/pull/358#discussion_r817736799
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 1.5),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  lineHeight: "18px",
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: theme.spacing(4.5),
-    transition: theme.transitions.create("width"),
-    border: `1px solid ${theme.palette.gray[30]}`,
-    borderRadius: "6px",
-    position: "relative",
-
-    ":focus": {
-      margin: "-1px",
-      border: `2px solid ${theme.palette.blue[70]}`,
-    },
-  },
-}));
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  marginRight: theme.spacing(1),
-  width: "100%",
-}));
 
 export const MobileSearch: React.FC<{
   displayedQuery: string;
@@ -59,13 +19,30 @@ export const MobileSearch: React.FC<{
     <Box
       style={{ display: "flex", width: "100%", background: "white", zIndex: 1 }}
     >
-      <Search>
-        <SearchIconWrapper>
+      <Box
+        sx={(theme) => ({
+          position: "relative",
+          borderRadius: theme.shape.borderRadius,
+          marginRight: theme.spacing(1),
+          width: "100%",
+        })}
+      >
+        <Box
+          sx={(theme) => ({
+            padding: theme.spacing(0, 1.5),
+            height: "100%",
+            position: "absolute",
+            pointerEvents: "none",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          })}
+        >
           <SearchIcon
             sx={(theme) => ({ height: theme.spacing(2), width: "auto" })}
           />
-        </SearchIconWrapper>
-        <StyledInputBase
+        </Box>
+        <InputBase
           placeholder="Search for anything"
           type="text"
           value={displayedQuery}
@@ -75,8 +52,27 @@ export const MobileSearch: React.FC<{
             setQueryText(event.target.value);
           }}
           inputProps={{ "aria-label": "search" }}
+          sx={(theme) => ({
+            color: "inherit",
+            lineHeight: "18px",
+            width: "100%",
+            "& .MuiInputBase-input": {
+              padding: theme.spacing(1, 1, 1, 0),
+              // vertical padding + font size from searchIcon
+              paddingLeft: theme.spacing(4.5),
+              transition: theme.transitions.create("width"),
+              border: `1px solid ${theme.palette.gray[30]}`,
+              borderRadius: "6px",
+              position: "relative",
+
+              ":focus": {
+                margin: "-1px",
+                border: `2px solid ${theme.palette.blue[70]}`,
+              },
+            },
+          })}
         />
-      </Search>
+      </Box>
       <Button
         onClick={() => {
           setQueryText("");
@@ -88,7 +84,7 @@ export const MobileSearch: React.FC<{
       </Button>
     </Box>
   ) : (
-    <Box
+    <IconButton
       sx={{
         height: "100%",
         display: "flex",
@@ -100,6 +96,6 @@ export const MobileSearch: React.FC<{
       <SearchIcon
         sx={(theme) => ({ height: theme.spacing(2), width: "auto" })}
       />
-    </Box>
+    </IconButton>
   );
 };
