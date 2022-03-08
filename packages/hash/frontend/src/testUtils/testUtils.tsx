6f0288aa412fad33fc859697ca53e06ac4f26f42
Akash Joshi <akash.joshi@viit.ac.in>
2022-03-08 12:06:09 +0530

    fix: jest tests

--- packages/hash/frontend/src/testUtils/testUtils.tsx
+++ packages/hash/frontend/src/testUtils/testUtils.tsx
@@ -1,10 +1,17 @@
 import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import { createTheme, ThemeProvider } from "@mui/material";
 // eslint-disable-next-line no-restricted-imports
 import { render, RenderOptions } from "@testing-library/react";
+
+import { borderRadii } from "../theme/borderRadii";
+import { palette } from "../theme/palette";
+import { boxShadows, dropShadows } from "../theme/shadows";
 
 type CustomRenderOptions = RenderOptions & {
   mocks?: MockedResponse[];
 };
+
+const theme = createTheme({ palette, boxShadows, dropShadows, borderRadii });
 
 const customRender = (
   ui: React.ReactElement,
@@ -12,9 +19,11 @@ const customRender = (
 ) => {
   const { mocks = [] } = options;
   const Wrapper: React.FC = ({ children }) => (
-    <MockedProvider mocks={mocks} addTypename={false}>
-      {children}
-    </MockedProvider>
+    <ThemeProvider theme={theme}>
+      <MockedProvider mocks={mocks} addTypename={false}>
+        {children}
+      </MockedProvider>
+    </ThemeProvider>
   );
 
   return render(ui, { wrapper: Wrapper, ...options });
