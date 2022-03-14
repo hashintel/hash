56da8fca6103d3c7568e501d0ddedec4de661eaf
Akash Joshi <akash.joshi@viit.ac.in>
2022-03-14 10:33:33 +0530

    fix: make react-no-danger an error and disable it in person card (#396)

--- packages/blocks/person/src/App.tsx
+++ packages/blocks/person/src/App.tsx
@@ -100,7 +100,7 @@ export const App: BlockComponent<AppProps> = ({
               {safeAnchor && (
                 <div className="person-link">
                   <LinkIcon />
-
+                  {/* eslint-disable-next-line react/no-danger */}
                   <span dangerouslySetInnerHTML={{ __html: safeAnchor }} />
                 </div>
               )}
