b18c835c2ff040a66a0ef6959ffb38ef96ba493c
Nate Higgins <nat@nath.is>
2022-03-18 15:59:05 +0000

    Replace the gradient

--- apps/dev/src/pages/index.page.tsx
+++ apps/dev/src/pages/index.page.tsx
@@ -22,8 +22,38 @@ const Hero: VFC = () => (
     component="header"
     py={16}
     sx={{
-      background: "url(/gradient.png) no-repeat",
-      backgroundPosition: "right top",
+      position: "relative",
+      "&:before": {
+        content: `""`,
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        backgroundImage: `
+          linear-gradient(
+            180deg,
+            rgba(255, 255, 255, 0) 0%,
+            rgb(255, 255, 255, 1) 90%
+          ),
+          linear-gradient(
+            25deg,
+            hsl(0deg 0% 100%) 0%,
+            hsl(197deg 100% 94%) 31%,
+            hsl(196deg 100% 88%) 47%,
+            hsl(196deg 100% 82%) 58%,
+            hsl(198deg 100% 74%) 66%,
+            hsl(201deg 100% 67%) 73%,
+            hsl(206deg 100% 63%) 78%,
+            hsl(211deg 100% 61%) 82%,
+            hsl(217deg 100% 61%) 85%,
+            hsl(230deg 100% 65%) 89%,
+            hsl(252deg 95% 64%) 97%
+          );
+        `,
+        width: "100%",
+        height: 400,
+        zIndex: -1,
+      },
     }}
   >
     <Container>
