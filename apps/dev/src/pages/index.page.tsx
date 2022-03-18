a27476427402a159c2a7696a5662afdaa0515c6a
Nate Higgins <nat@nath.is>
2022-03-18 13:46:39 +0000

    Start wireframing the homepage

--- apps/dev/src/pages/index.page.tsx
+++ apps/dev/src/pages/index.page.tsx
@@ -1,11 +1,42 @@
+import { Container, Stack, Typography, Box, Button } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 
+/**
+ * @todo gradient
+ */
 const Home: NextPage = () => {
   return (
-    <Head>
-      <title>HASH</title>
-    </Head>
+    <>
+      <Head>
+        <title>HASH</title>
+      </Head>
+      <Container>
+        <Stack direction="row" alignItems="center">
+          <Box>
+            <Typography>HASH for Developers</Typography>
+            <Typography>Help build the future of decision-making</Typography>
+            {/** @todo divider, link */}
+            <Typography>
+              HASH is an organization building the next generation of
+              simulation, decision-making, and knowledge management tools.
+            </Typography>
+            <Typography>
+              Here you’ll find resources for developers who want to use our open
+              source projects and build blocks, plugins, and integrations for
+              the ecosystem.
+            </Typography>
+          </Box>
+          <Button
+            variant="primary"
+            size="medium"
+            href="https://blockprotocol.org"
+          >
+            Visit blockprotocol.org
+          </Button>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
