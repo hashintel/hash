6a1fe262392f0532a84079abca289d55d0c20552
Akash Joshi <akash.joshi@viit.ac.in>
2022-03-18 12:58:27 +0530

    fix: revert search change

--- packages/hash/frontend/src/blocks/page/createSuggester/fuzzySearchBy.ts
+++ packages/hash/frontend/src/blocks/page/createSuggester/fuzzySearchBy.ts
@@ -1,15 +1,25 @@
+/**
+ * used to find matches of search in the given choices using the fuzzy search algorithm
+ * @see https://en.wikipedia.org/wiki/Approximate_string_matching
+ */
 export const fuzzySearchBy = <T>(
   choices: T[],
   search: string,
   getter: (choice: T) => string,
 ): T[] => {
+  const lowerSearch = search.toLowerCase();
+  const searchLength = search.length;
+
   return choices.filter((choice) => {
     const match = getter(choice).toLowerCase();
 
-    if (match.includes(search)) {
-      return true;
+    let offset = 0;
+    for (let searchIndex = 0; searchIndex < searchLength; searchIndex++) {
+      const matchIndex = match.indexOf(lowerSearch.charAt(searchIndex), offset);
+      if (matchIndex < 0) return false;
+      offset = matchIndex + 1;
     }
 
-    return false;
+    return true;
   });
 };
