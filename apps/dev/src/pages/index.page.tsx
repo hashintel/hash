8d300e2e71f743297481f9da52cd7c892778d5c3
Nate Higgins <nat@nath.is>
2022-03-18 17:57:21 +0000

    Projects on home page WIP

--- apps/dev/src/pages/index.page.tsx
+++ apps/dev/src/pages/index.page.tsx
@@ -1,9 +1,17 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import {
+  Box,
+  Container,
+  Stack,
+  Typography,
+  typographyClasses,
+} from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { ComponentProps, VFC } from "react";
+import { ComponentProps, FC, ReactNode, VFC } from "react";
+import { Button } from "../components/Button";
 
-const HeroDivider: VFC<ComponentProps<typeof Stack>> = (props) => {
+const StylishDivider: VFC<ComponentProps<typeof Stack>> = (props) => {
   const bgcolor = "orange.400";
   const size = 12;
   return (
@@ -64,7 +72,7 @@ const Hero: VFC = () => (
         <Typography variant="hashHeading1" component="h2" mb={5}>
           Help build the future of decision-making
         </Typography>
-        <HeroDivider mb={5} />
+        <StylishDivider mb={5} />
         <Box width={{ xs: 1, md: 550 }}>
           <Typography mb={2} sx={{ lineHeight: 1.5 }}>
             HASH is an organization building the next generation of simulation,
@@ -81,6 +89,128 @@ const Hero: VFC = () => (
   </Box>
 );
 
+const Project: FC<{
+  title: ReactNode;
+  buttons: ReactNode;
+}> = ({ buttons, children, title }) => {
+  return (
+    // @todo check this with the design
+    <Stack direction={{ xs: "column", md: "row" }} spacing={9}>
+      <Box sx={{ width: 420 }}>
+        <Typography
+          variant="hashHeading4"
+          component="h4"
+          sx={{ color: "black", fontWeight: 500, mb: 2 }}
+        >
+          {title}
+        </Typography>
+        <Stack
+          sx={{
+            lineHeight: 1.5,
+            [`& .${typographyClasses.root}`]: { lineHeight: "inherit" },
+          }}
+          mb={3}
+          spacing={4}
+        >
+          {children}
+        </Stack>
+        {/** @todo check this spacing */}
+        <Stack direction="row" spacing={1.5}>
+          {buttons}
+        </Stack>
+      </Box>
+      <Box>
+        {/** @todo scaling, alt text */}
+        <Image src="/home/projects/bp.svg" width={445} height={326.26} />
+      </Box>
+    </Stack>
+  );
+};
+
+const Projects: VFC<ComponentProps<typeof Stack>> = (props) => {
+  return (
+    <Stack
+      {...props}
+      component="section"
+      direction={{ xs: "column", md: "row" }}
+      spacing={6}
+    >
+      <Stack mb={{ md: 6 }} width={{ xs: 1, md: 240 }} spacing={4}>
+        <Typography variant="hashHeading4" component="h3">
+          Our projects
+        </Typography>
+        <StylishDivider />
+      </Stack>
+      <Stack spacing={10}>
+        <Project
+          title="Block Protocol"
+          buttons={
+            /* @todo icon */
+            <Button href="https://blockprotocol.org">
+              Visit blockprotocol.org
+            </Button>
+          }
+        >
+          <Typography>
+            The Block Protocol is an open-source standard and registry for
+            sharing interactive blocks connected to structured data.
+          </Typography>
+          <Typography>
+            You can build your own blocks, embed them in a website, or allow
+            your users to embed blocks directly within your application.
+          </Typography>
+        </Project>
+        <Project
+          title="HASH"
+          buttons={
+            <>
+              {/* @todo action */}
+              <Button>Download</Button>
+              {/* @todo link */}
+              <Button variant="secondary">Read the setup guide</Button>
+            </>
+          }
+        >
+          <Typography>
+            HASH is our forthcoming open-source, all-in-one workspace platform
+            built around structured data and interactive blocks. It feels like
+            taking notes, but works like a powerful database.{" "}
+            {/** @todo check styles */}
+            <strong>
+              Please note: the current version is not yet ready for use.
+            </strong>
+          </Typography>
+          {/** @todo links */}
+          <Typography>
+            Download and run it yourself, or sign up for the hosted platform
+            waitlist at hash.ai/platform/hash
+          </Typography>
+        </Project>
+        <Project
+          title="hEngine"
+          buttons={
+            <>
+              {/* @todo action */}
+              <Button>Download</Button>
+              {/* @todo link */}
+              <Button variant="secondary">Read the setup guide</Button>
+            </>
+          }
+        >
+          <Typography>
+            hEngine is a simulation engine that allows you to run agent-based
+            simulations using your own private and public data from HASH.
+          </Typography>
+          <Typography>
+            The HASH all-in-one workspace will make it easy to clean, organise,
+            and transform your data first.
+          </Typography>
+        </Project>
+      </Stack>
+    </Stack>
+  );
+};
+
 const Home: NextPage = () => {
   return (
     <>
@@ -88,6 +218,9 @@ const Home: NextPage = () => {
         <title>HASH</title>
       </Head>
       <Hero />
+      <Container>
+        <Projects mb={12} />
+      </Container>
     </>
   );
 };
