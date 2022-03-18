373cb57c88fc16625d55d5fc020e5e6d570ba7f7
Nate Higgins <nat@nath.is>
2022-03-18 13:52:56 +0000

    Remove blockprotocol button

--- apps/dev/src/pages/index.page.tsx
+++ apps/dev/src/pages/index.page.tsx
@@ -1,4 +1,4 @@
-import { Container, Stack, Typography, Box, Button } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 
@@ -12,29 +12,20 @@ const Home: NextPage = () => {
         <title>HASH</title>
       </Head>
       <Container>
-        <Stack direction="row" alignItems="center">
-          <Box>
-            <Typography>HASH for Developers</Typography>
-            <Typography>Help build the future of decision-making</Typography>
-            {/** @todo divider, link */}
-            <Typography>
-              HASH is an organization building the next generation of
-              simulation, decision-making, and knowledge management tools.
-            </Typography>
-            <Typography>
-              Here you’ll find resources for developers who want to use our open
-              source projects and build blocks, plugins, and integrations for
-              the ecosystem.
-            </Typography>
-          </Box>
-          <Button
-            variant="primary"
-            size="medium"
-            href="https://blockprotocol.org"
-          >
-            Visit blockprotocol.org
-          </Button>
-        </Stack>
+        <Box>
+          <Typography>HASH for Developers</Typography>
+          <Typography>Help build the future of decision-making</Typography>
+          {/** @todo divider, link */}
+          <Typography>
+            HASH is an organization building the next generation of simulation,
+            decision-making, and knowledge management tools.
+          </Typography>
+          <Typography>
+            Here you’ll find resources for developers who want to use our open
+            source projects and build blocks, plugins, and integrations for the
+            ecosystem.
+          </Typography>
+        </Box>
       </Container>
     </>
   );
