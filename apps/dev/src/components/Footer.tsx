dbf8095aad902e7323d82b0d361078da49bd9abc
Nate Higgins <nat@nath.is>
2022-03-18 02:42:36 +0000

    Footer layout

--- apps/dev/src/components/Footer.tsx
+++ apps/dev/src/components/Footer.tsx
@@ -1,8 +1,12 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Divider, Stack, Typography } from "@mui/material";
 import { FC } from "react";
+import { Link } from "./Link";
 import { Logo } from "./Logo";
 import { Spacer } from "./Spacer";
 
+/**
+ * @todo Fonts, colors, etc
+ */
 export const Footer: FC = () => (
   <Box
     component="footer"
@@ -18,10 +22,42 @@ export const Footer: FC = () => (
       background: "linear-gradient(359.56deg, #FFFFFF 59.36%, #F7F8FA 99.57%)",
     }}
   >
-    <Stack direction="row" spacing={10}>
+    <Stack direction={{ xs: "column", sm: "row" }} spacing={{ xs: 6, sm: 10 }}>
       <Stack>
         <Logo />
-        <Spacer y={3} />
+        <Spacer y={2} sm={{ y: 3 }} />
+        <Typography sx={{ width: { xs: 1, sm: 271 } }}>
+          Open-source resources and tools for developers who want to build the
+          future of decision-making with HASH
+        </Typography>
+        {/** @todo icon row */}
+      </Stack>
+      <Stack direction="row" spacing={{ xs: 6, sm: 10 }}>
+        <Stack>
+          <Typography>Resources</Typography>
+          <Divider />
+          <Stack spacing={2}>
+            <Typography>Guides</Typography>
+            <Typography>Blog</Typography>
+            <Typography>FAQs</Typography>
+            <Typography>Careers</Typography>
+            <Typography>Contact Us</Typography>
+            <Typography>Terms of Service</Typography>
+          </Stack>
+        </Stack>
+        <Stack>
+          <Typography>Our projects</Typography>
+          <Divider />
+          {/** @todo where do these go? */}
+          {/** @todo badges */}
+          <Stack spacing={2}>
+            <Link href="https://blockprotocol.org">Block Protocol</Link>
+            <Link href="https://hash.ai">HASH</Link>
+            <Link href="https://hash.ai">hEngine</Link>
+            <Link href="https://core.hash.ai">hCore</Link>
+            <Link href="https://hash.ai">hCloud</Link>
+          </Stack>
+        </Stack>
       </Stack>
     </Stack>
   </Box>
--- apps/dev/src/components/Spacer.tsx
+++ apps/dev/src/components/Spacer.tsx
@@ -1,27 +1,60 @@
-import { Box } from "@mui/material";
+import { Box, Breakpoint, Theme } from "@mui/material";
+import { SystemStyleObject } from "@mui/system";
 import { VFC } from "react";
 
-export const Spacer: VFC<
-  | {
-      x?: number;
-      y?: number;
-      basis?: number;
-      flex?: undefined;
-    }
-  | { x?: undefined; y?: undefined; basis?: undefined; flex: true }
-> = ({ x, y, flex, basis }) => (
+type FlexProps = { flex: true };
+type InnerNotFlexProps = {
+  x?: number;
+  y?: number;
+  basis?: number;
+};
+
+type NotFlexProps = InnerNotFlexProps & {
+  [key in Breakpoint]?: InnerNotFlexProps;
+};
+
+type SpacerProps =
+  | ({ [key in keyof FlexProps]?: undefined } & NotFlexProps)
+  | ({ [key in keyof NotFlexProps]?: undefined } & FlexProps);
+
+const spacerStyles = ({
+  x,
+  y,
+  basis,
+}: InnerNotFlexProps | void = {}): SystemStyleObject<Theme> => ({
+  width: (theme) => (x ? theme.spacing(x) : undefined),
+  height: (theme) => (y ? theme.spacing(y) : undefined),
+  flexBasis: (theme) => (basis ? theme.spacing(basis) : undefined),
+});
+
+export const Spacer: VFC<SpacerProps> = ({
+  x,
+  y,
+  flex,
+  basis,
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+}) => (
   <Box
     data-testid="Spacer"
     sx={
       flex
         ? { flex: 1 }
-        : {
-            width: (theme) => (x ? theme.spacing(x) : undefined),
-            height: (theme) => (y ? theme.spacing(y) : undefined),
-            flexBasis: (theme) => (basis ? theme.spacing(basis) : undefined),
-            flexGrow: 0,
-            flexShrink: 0,
-          }
+        : [
+            {
+              ...spacerStyles({ x, y, basis }),
+              flexGrow: 0,
+              flexShrink: 0,
+            },
+            xs ? spacerStyles(xs) : {},
+            sm ? spacerStyles(sm) : {},
+            md ? spacerStyles(md) : {},
+            lg ? spacerStyles(lg) : {},
+            xl ? spacerStyles(xl) : {},
+          ]
     }
   />
 );
