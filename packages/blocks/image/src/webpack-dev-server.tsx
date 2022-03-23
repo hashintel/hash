1be1055e3bd444429fbb897fcf1d1a78aebf9253
Akash Joshi <akash.joshi@viit.ac.in>
2022-03-23 16:04:17 +0530

    refactor: remove BP function fallbacks from image, video and embed blocks

--- packages/blocks/image/src/webpack-dev-server.tsx
+++ packages/blocks/image/src/webpack-dev-server.tsx
@@ -2,12 +2,6 @@
  * webpack-dev-server entry point for debugging.
  * This file is not bundled with the library during the build process.
  */
-import {
-  BlockProtocolCreateLinksFunction,
-  BlockProtocolDeleteLinksFunction,
-  BlockProtocolLink,
-  BlockProtocolUploadFileFunction,
-} from "blockprotocol";
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -19,71 +13,13 @@ import Component from "./index";
 const node = document.getElementById("app");
 
 const App = () => {
-  const uploadFile: BlockProtocolUploadFileFunction = async ({
-    file,
-    url,
-    mediaType,
-  }) => {
-    return new Promise((resolve, reject) => {
-      if (!file && !url) {
-        reject(
-          new Error(
-            `Please enter a valid ${mediaType} URL or select a file below`,
-          ),
-        );
-        return;
-      }
-
-      if (url?.trim()) {
-        resolve({
-          accountId: "xxx",
-          entityId: "xxx",
-          url,
-          mediaType,
-        });
-        return;
-      }
-
-      if (file) {
-        const reader = new FileReader();
-
-        reader.onload = (event) => {
-          if (event.target?.result) {
-            resolve({
-              accountId: "xxx",
-              entityId: "xxx",
-              url: event.target.result.toString(),
-              mediaType,
-            });
-          } else {
-            reject(new Error("Couldn't read your file"));
-          }
-        };
-
-        reader.readAsDataURL(file);
-      }
-    });
-  };
-
-  const createLinks: BlockProtocolCreateLinksFunction = async () => {
-    const results: BlockProtocolLink[] = [];
-    return results;
-  };
-
-  const deleteLinks: BlockProtocolDeleteLinksFunction = async () => {
-    return [true];
-  };
-
   return (
     <div className={tw`mt-5`}>
       <MockBlockDock>
         <Component
           accountId="account-asdasd"
-          createLinks={createLinks}
-          deleteLinks={deleteLinks}
           entityId="entity-asdasd"
           initialCaption="Image of a Dog"
-          uploadFile={uploadFile}
           url="https://placedog.net/450/300"
         />
       </MockBlockDock>
--- packages/blocks/video/src/webpack-dev-server.tsx
+++ packages/blocks/video/src/webpack-dev-server.tsx
@@ -2,12 +2,6 @@
  * webpack-dev-server entry point for debugging.
  * This file is not bundled with the library during the build process.
  */
-import {
-  BlockProtocolCreateLinksFunction,
-  BlockProtocolDeleteLinksFunction,
-  BlockProtocolLink,
-  BlockProtocolUploadFileFunction,
-} from "blockprotocol";
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -18,71 +12,13 @@ import Component from "./index";
 const node = document.getElementById("app");
 
 const App = () => {
-  const uploadFile: BlockProtocolUploadFileFunction = async ({
-    file,
-    url,
-    mediaType,
-  }) => {
-    return new Promise((resolve, reject) => {
-      if (!file && !url) {
-        reject(
-          new Error(
-            `Please enter a valid ${mediaType} URL or select a file below`,
-          ),
-        );
-        return;
-      }
-
-      if (url?.trim()) {
-        resolve({
-          accountId: "account-xxx",
-          entityId: "xxx",
-          url,
-          mediaType,
-        });
-        return;
-      }
-
-      if (file) {
-        const reader = new FileReader();
-
-        reader.onload = (event) => {
-          if (event.target?.result) {
-            resolve({
-              accountId: "account-xxx",
-              entityId: "xxx",
-              url: event.target.result.toString(),
-              mediaType,
-            });
-          } else {
-            reject(new Error("Couldn't read your file"));
-          }
-        };
-
-        reader.readAsDataURL(file);
-      }
-    });
-  };
-
-  const createLinks: BlockProtocolCreateLinksFunction = async () => {
-    const results: BlockProtocolLink[] = [];
-    return results;
-  };
-
-  const deleteLinks: BlockProtocolDeleteLinksFunction = async () => {
-    return [true];
-  };
-
   return (
     <div style={{ marginTop: 20 }}>
       <MockBlockDock>
         <Component
           accountId="account-asdasd"
-          createLinks={createLinks}
-          deleteLinks={deleteLinks}
           initialCaption="A blooming flower"
           entityId="entity-asdasd"
-          uploadFile={uploadFile}
           url="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm"
         />
       </MockBlockDock>
