7f5df7dda747a5f0c83b9169c43aafc81a199311
teenoh <ugbalaachinike@gmail.com>
2022-03-25 15:55:53 +0100

    add radio button styles

--- packages/hash/frontend/src/theme/components/inputs/MuiCheckboxThemeOptions.tsx
+++ packages/hash/frontend/src/theme/components/inputs/MuiCheckboxThemeOptions.tsx
@@ -1,16 +1,39 @@
-import { Components, Theme } from "@mui/material";
+import { Components, Theme, svgIconClasses } from "@mui/material";
 import { CheckboxBlankIcon } from "../icons/CheckboxBlankIcon";
 import { CheckboxIcon } from "../icons/CheckboxIcon";
+
+const focusBorderOffset = 4;
+const focusBorderWidth = 2;
+const checkboxBorderRadius = 3;
 
 export const MuiCheckboxThemeOptions: Components<Theme>["MuiCheckbox"] = {
   defaultProps: {
     disableFocusRipple: true,
     disableRipple: true,
     disableTouchRipple: true,
-    icon: <CheckboxBlankIcon />, // todo-mui fix issue with blank icon
+    icon: <CheckboxBlankIcon />,
     checkedIcon: <CheckboxIcon />,
   },
   styleOverrides: {
-    root: ({ theme }) => ({}),
+    root: ({ theme }) => ({
+      padding: 0,
+
+      [`.${svgIconClasses.root}`]: {
+        position: "relative",
+      },
+
+      [`&.Mui-focusVisible:after, &:focus-visible:after`]: {
+        content: `""`,
+        border: "2px solid red",
+        position: "absolute",
+        top: -focusBorderOffset,
+        left: -focusBorderOffset,
+        right: -focusBorderOffset,
+        bottom: -focusBorderOffset,
+        borderWidth: focusBorderWidth,
+        borderColor: theme.palette.blue[70],
+        borderRadius: checkboxBorderRadius + focusBorderOffset,
+      },
+    }),
   },
 };
