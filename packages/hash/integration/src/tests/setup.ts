37f1b38416faaeb9bbf332872177399871a97fb6
Ben Werner <ben.werner01@gmail.com>
2022-03-29 13:25:23 +0200

    refactor: create datastore yarn workspace
    eslint

--- packages/hash/integration/src/tests/setup.ts
+++ packages/hash/integration/src/tests/setup.ts
@@ -4,13 +4,15 @@ import { monorepoRootDir } from "@hashintel/hash-backend-utils/environment";
 export const recreateDbAndRunSchemaMigrations = async () => {
   await execa(
     "yarn",
-    ["workspace", "@hashintel/hash-postgres", "recreate-db"],
-    { cwd: monorepoRootDir },
+    ["workspace", "@hashintel/hash-datastore", "pg:recreate-db"],
+    {
+      cwd: monorepoRootDir,
+    },
   );
 
   await execa(
     "yarn",
-    ["workspace", "@hashintel/hash-postgres", "run-schema-migrations"],
+    ["workspace", "@hashintel/hash-datastore", "pg:run-schema-migrations"],
     { cwd: monorepoRootDir },
   );
 };
