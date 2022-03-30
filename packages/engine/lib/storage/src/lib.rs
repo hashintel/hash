cb77214b39820c01ef44bfd18ae59907d86130db
Tim Diekmann <tim.diekmann@3dvision.de>
2022-03-30 17:23:22 +0200

    Improve doc

--- packages/engine/lib/storage/src/lib.rs
+++ packages/engine/lib/storage/src/lib.rs
@@ -1,8 +1,8 @@
-//! Library for low-level to store and load arrow-data.
+//! Defines how data is stored in shared memory and provides an (optional) arrow-interface to it.
 //!
 //! This crates consists of two main parts:
 //!   - [`memory`] contains structures and functions to store and load data to/from shared memory.
-//!     Each [`Memory`] tracks a memory [`Segment`] consisting of four parts:
+//!     Each memory [`Segment`] consisting of four parts:
 //!
 //!       1) Arrow Schema
 //!       2) Header
@@ -12,9 +12,10 @@
 //!     See the [`Memory`] documentation for further information.
 //!
 //!   - [`batch`] contains arrow-data to be stored in a memory [`Segment`]. Each [`ArrowBatch`] is
-//!     associated with one [`Segment`]. For further information please see the [`ArrowBatch`] API.
+//!     associated with one [`Segment`] and several other data. For further information please see
+//!     the [`ArrowBatch`] API.
 //!
-//! Furthermore, there is a [`meta`] module used for meta-data used in an [`ArrowBatch`] and an
+//! Furthermore, there is a [`meta`] module used for meta-data in an [`ArrowBatch`] and an
 //! [`ffi`] module exposing basic functionality to interact with memory and batches.
 //!
 //! [`Segment`]: crate::memory::Segment
