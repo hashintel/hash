e9342b05cc8bd16c57a815841d635ed7724bedf0
teenoh <ugbalaachinike@gmail.com>
2022-03-31 12:01:42 +0100

    add todo comment to move createPage to frontend queries

--- packages/hash/frontend/src/graphql/queries/page.queries.ts
+++ packages/hash/frontend/src/graphql/queries/page.queries.ts
@@ -1,5 +1,16 @@
 import { gql } from "@apollo/client";
 
+//
+// into this file since it is currently only used by the
+// frontend package
+
+/**
+ * @todo: move createPage from shared/src/../page.queries
+ * into this file since it is currently only used by the
+ * frontend package
+ * @see https://github.com/hashintel/hash/pull/409#discussion_r833559404
+ */
+
 export const setParentPage = gql`
   mutation setParentPage(
     $accountId: ID!
