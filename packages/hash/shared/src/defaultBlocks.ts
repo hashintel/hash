ef204906cd9a7ebf5d2885d7f99a56c9e9b1ff52
Ciaran Morinan <cm@sohostrategy.com>
2022-03-31 17:15:51 +0100

    replace blockPaths map with a defaultBlocks array

---
+++ packages/hash/shared/src/defaultBlocks.ts
@@ -0,0 +1,20 @@
+/**
+ * Default blocks loaded for ever user.
+ *
+ * @todo allow users to configure their own default block list, and store in db.
+ *    this should be a list of additions and removals from this default list,
+ *    to allow us to add new default blocks that show up for all users.
+ *    we currently store this in localStorage - see UserBlockProvider.
+ */
+export const defaultBlocks = [
+  "https://blockprotocol.org/blocks/@hash/paragraph",
+  "https://blockprotocol.org/blocks/@hash/header",
+  "https://blockprotocol.org/blocks/@hash/callout",
+  "https://blockprotocol.org/blocks/@hash/person",
+  "https://blockprotocol.org/blocks/@hash/image",
+  "https://blockprotocol.org/blocks/@hash/table",
+  "https://blockprotocol.org/blocks/@hash/divider",
+  "https://blockprotocol.org/blocks/@hash/embed",
+  "https://blockprotocol.org/blocks/@hash/code",
+  "https://blockprotocol.org/blocks/@hash/video",
+];
