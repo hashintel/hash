ef8e2906c6a334f879d83fe74d8e7790cd4ded8d
Dylan Ancel <dylan.ancel@gmail.com>
2022-04-01 15:37:34 +0200

    Update packages/engine/src/worker/runner/javascript/mod.rs
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>

--- packages/engine/src/worker/runner/javascript/mod.rs
+++ packages/engine/src/worker/runner/javascript/mod.rs
@@ -2030,7 +2030,7 @@ extern "C" fn near_heap_limit_callback(
     _initial_heap_limit: usize,
 ) -> usize {
     tracing::warn!(
-        "V8 max heap limit almost reached! Use the '--js-runner-max-heap-size' argument to raise \
+        "A JavaScript runner almost reached its heap limit! Use the '--js-runner-max-heap-size' argument to raise \
          the limit."
     );
 
