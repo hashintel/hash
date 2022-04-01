b6ae3bd910fd0beca7be44c8696b825bb9027248
Tim Diekmann <tim.diekmann@3dvision.de>
2022-04-01 16:53:14 +0200

    Satisfy rustfmt

--- packages/engine/lib/stateful/src/field/mod.rs
+++ packages/engine/lib/stateful/src/field/mod.rs
@@ -12,7 +12,7 @@ pub const UUID_V4_LEN: usize = 16;
 pub use self::{
     field_type::{FieldType, FieldTypeVariant, PresetFieldType},
     fixed_size::IsFixedSize,
-    key::{FieldKey, PRIVATE_PREFIX, HIDDEN_PREFIX},
+    key::{FieldKey, HIDDEN_PREFIX, PRIVATE_PREFIX},
     scope::FieldScope,
     source::FieldSource,
     spec::FieldSpec,
