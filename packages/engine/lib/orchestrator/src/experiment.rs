4205d881176f30ba78ab9f2615f9644f1ed98a91
Dylan Ancel <dylan.ancel@gmail.com>
2022-04-04 16:47:05 +0200

    Update packages/engine/lib/orchestrator/src/experiment.rs
    Co-authored-by: Alfred Mountfield <am@hash.ai>

--- packages/engine/lib/orchestrator/src/experiment.rs
+++ packages/engine/lib/orchestrator/src/experiment.rs
@@ -101,7 +101,7 @@ pub struct ExperimentConfig {
     /// See "--num-workers" to set the number of JavaScript runners
     /// executing in parallel.
     ///
-    /// This setting is most of the time better off left to its default value.
+    /// Unless you have specific need, this setting should be left at its default value.
     /// It could be beneficial if your are going to store a large amount of "eternal" (=lives as
     /// long as the simulation is running) data on the heap.
     /// Example: you know that at any given time you have 500MB of unreachable data on the
