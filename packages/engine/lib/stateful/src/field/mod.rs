9dc3b66869f45a0f6cc2870762a924b7a0511e17
Tim Diekmann <tim.diekmann@3dvision.de>
2022-04-04 12:38:03 +0200

    Add module-level docs for `field`

--- packages/engine/lib/stateful/src/field/mod.rs
+++ packages/engine/lib/stateful/src/field/mod.rs
@@ -1,4 +1,11 @@
-//! TODO: DOC
+//! Provides an hEngine-specific representation of Arrows `Field` and `DataType`.
+//!
+//! As we are using only a subset of types provided by `Arrow`, our types are represented by
+//! [`FieldType`] rather than [`arrow::DataType`]. As [`FieldType`] can't be used in
+//! [`arrow::Field`], this module provides [`FieldSpec`] instead.
+//!
+//! For naming a [`FieldSpec`], [`FieldKey`] is provided, as a name has strict requirements on how
+//! it's built.
 
 mod field_type;
 mod fixed_size;
