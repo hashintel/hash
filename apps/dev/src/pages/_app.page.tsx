3479566eb1cd7a6b28de6a028065df68969d592c
Nate Higgins <nat@nath.is>
2022-04-05 01:35:23 +0100

    Add GA

--- apps/dev/src/pages/_app.page.tsx
+++ apps/dev/src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import { EmotionCache } from "@emotion/react";
 import type { AppProps } from "next/app";
-import { VFC } from "react";
+import { useRouter } from "next/router";
+import { useEffect, VFC } from "react";
 import "../../styles/globals.css";
 import { PageLayout } from "../components/PageLayout";
 import { theme } from "../theme";
@@ -11,6 +12,21 @@ type MyAppProps = {
 } & AppProps;
 
 const MyApp: VFC<MyAppProps> = ({ Component, pageProps, emotionCache }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      (window as any).gtag?.("config", "[Tracking ID]", {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <MuiProvider emotionCache={emotionCache} theme={theme}>
       <PageLayout>
--- apps/dev/src/pages/_document.page.tsx
+++ apps/dev/src/pages/_document.page.tsx
@@ -25,6 +25,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-2JDBVXSZV8"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2JDBVXSZV8');
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
