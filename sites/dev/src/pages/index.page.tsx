c6fec8870c9bf92d9a6555f20f3755efdf684ad6
David Wilkinson <6226576+nonparibus@users.noreply.github.com>
2022-04-05 20:17:18 +0100

    Update sites/dev/src/pages/index.page.tsx

--- sites/dev/src/pages/index.page.tsx
+++ sites/dev/src/pages/index.page.tsx
@@ -282,7 +282,7 @@ const Projects: VFC<ComponentProps<typeof Stack>> = (props) => {
                   <FaIcon name="arrow-up-right-from-square" type="solid" />
                 }
               >
-                Download on Github
+                Download on GitHub
               </Button>
             }
             image={
