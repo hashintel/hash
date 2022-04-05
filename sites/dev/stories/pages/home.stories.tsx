ff4ae088ff4b0cc53facdfa6036f61473222eca4
Nate Higgins <nat@nath.is>
2022-04-05 02:22:04 +0100

    Fix homepage story in storybook

--- sites/dev/stories/pages/home.stories.tsx
+++ sites/dev/stories/pages/home.stories.tsx
@@ -1,8 +1,16 @@
+import { PageLayout } from "../../src/components/PageLayout";
 import Home from "../../src/pages/index.page";
+
+export const HomePage = () => (
+  <PageLayout>
+    <Home />
+  </PageLayout>
+);
 
 export default {
   title: "Pages/Home",
-  component: Home,
+  component: HomePage,
+  parameters: {
+    layout: "fullscreen",
+  },
 };
-
-export const HomePage = () => <Home />;
