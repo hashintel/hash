e5daeed10c52d2d290c607276418c057a427f178
Tim Diekmann <tim.diekmann@3dvision.de>
2022-04-07 17:23:03 +0200

    Improve documentation on `proxy`

--- packages/engine/lib/stateful/src/proxy/mod.rs
+++ packages/engine/lib/stateful/src/proxy/mod.rs
@@ -1,20 +1,15 @@
-//! Provides structs that behave as guards for the read/write locks on batches and pools that
-//! you can send between threads.
+//! Guards without lifetimes, which can be sent between threads.
 //!
-//! Proxies are basically just self-referential structs, which is why they use pointers/unsafe code
-//! internally. Usually locking a [`RwLock`] would give a Guard that has a lifetime depending on the
-//! [`RwLock`], but in our use case, this isn’t necessary, because as long as the Proxy exists, an
-//! [`Arc`] containing the [`RwLock`] will exist, so there’s no possibility of the Proxy referencing
-//! a [`RwLock`] that no longer exists. In other words, the Proxy is like a self-referential struct
-//! containing both a guard and an [`Arc`] with the [`RwLock`] that the guard refers to, so as long
-//! as the `Proxy` exists, the [`RwLock`] will also exist and the guard will refer to an existing
-//! [`RwLock`].
+//! Usually locking a [`RwLock`] would give a guard that has a lifetime depending on the [`RwLock`],
+//! but for a `Proxy`, this isn’t necessary, because as long as the `Proxy` exists, the underlying
+//! data will be locked, so it's not possible to create another `Proxy` if the existing `Proxy` or
+//! the new `Proxy` is writable.
 //!
-//! Also, the guard is actually just the pointer inside the [`RwLock`], so we don’t need to store it
-//! in a separate field. Also, the [`RwLock`] is like a [`Box`] in the sense that even if it is
-//! moved, its contents stay in the same place on the heap, so they don’t need to be pinned --
-//! therefore, if the `Proxy` is moved, the references returned by the Proxy to its [`RwLock`]s
-//! contents (i.e. its “guard”) will still be valid.
+//! As the `Proxy` is just a pointer into the [`RwLock`], proxies are very small. This also implies,
+//! that the data behind a `Proxy` is automatically [`Pin`]ned.
+//!
+//! [`RwLock`]: std::lock::RwLock
+//! [`Pin`]: core::pin::Pin
 
 mod batch;
 mod pool;
