14c2388cef44459d3f6ccbbf15326019ec7043a3
Tim Diekmann <tim.diekmann@3dvision.de>
2022-04-07 11:22:35 +0200

    Improve docs on `FieldScope`

--- packages/engine/lib/stateful/src/field/scope.rs
+++ packages/engine/lib/stateful/src/field/scope.rs
@@ -1,10 +1,11 @@
 const HIDDEN_PREFIX: &str = "_HIDDEN_";
 const PRIVATE_PREFIX: &str = "_PRIVATE_";
 
-/// Defines scope of access to Fields, where the order of the variants of this enum define an
-/// ordering of the scopes, where being defined lower implies a wider scope where more things can
-/// access it.
-/// i.e. Engine < Private < Hidden < Agent,
+/// Defines scope of access to fields.
+///
+/// The order of the variants of `FieldScope` define an ordering of the scopes, where being defined
+/// lower implies a wider scope where more things can access it, i.e. `Private` < `Hidden` <
+/// `Agent`.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum FieldScope {
     /// Only the source package/engine agents and other packages don't
