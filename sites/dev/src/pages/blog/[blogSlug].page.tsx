94e48c08902fce265f91db35f52b5ef71e400adb
Nate Higgins <nat@nath.is>
2022-04-07 17:06:31 +0100

    Put header image in

--- sites/dev/src/pages/blog/[blogSlug].page.tsx
+++ sites/dev/src/pages/blog/[blogSlug].page.tsx
@@ -2,6 +2,7 @@ import { Container, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 const BlogPostPage: NextPage = () => {
   return (
@@ -11,22 +12,39 @@ const BlogPostPage: NextPage = () => {
         <title>Holding down the fort – HASH for Developers</title>
       </Head>
       <Box pt={8}>
-        <Container>
-          <Box width="49.25%">
-            <Typography variant="hashHeading1" mb={3}>
-              Holding down the fort: security and permissioning in block-based
-              systems
-            </Typography>
-            <Typography variant="hashLargeText" mb={5}>
-              Chocolate sugar plum brownie cupcake chocolate bar toffee brownie
-              cake. Chocolate sugar plum brownie cupcake.
-            </Typography>
-            <Stack direction="horizontal">
-              <Box>Test</Box>
-              <Box>Test</Box>
-            </Stack>
+        <Box position="relative">
+          <Container>
+            <Box width="49.25%">
+              <Typography variant="hashHeading1" mb={3}>
+                Holding down the fort: security and permissioning in block-based
+                systems
+              </Typography>
+              <Typography variant="hashLargeText" mb={5} color="gray.80">
+                Chocolate sugar plum brownie cupcake chocolate bar toffee
+                brownie cake. Chocolate sugar plum brownie cupcake.
+              </Typography>
+              <Stack direction="row">
+                <Box>Test</Box>
+                <Box>Test</Box>
+              </Stack>
+            </Box>
+          </Container>
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              transform: "translateY(-50%)",
+              right: 0,
+              width: "45%",
+              maxHeight: "100%",
+              background: "black",
+              aspectRatio: "654 / 445",
+              borderRadius: 4,
+            }}
+          >
+            <Image src="/temp.png" layout="fill" />
           </Box>
-        </Container>
+        </Box>
       </Box>
     </>
   );
