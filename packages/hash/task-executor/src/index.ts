c30c3f1d11b9bfb978e13f56c2002a6580a889ad
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-04-08 16:25:12 +0100

    Migrating task-executor to express

--- packages/hash/task-executor/src/index.ts
+++ packages/hash/task-executor/src/index.ts
@@ -1,35 +1,16 @@
-import { createServer, IncomingMessage, ServerResponse } from "http";
+import express from "express";
+import { json } from "body-parser";
 import { executeTask } from "./execution";
 
 const port = 5010;
 
-const requestHandler = async (
-  request: IncomingMessage,
-  response: ServerResponse,
-) => {
-  const url = request.url;
-  let result = "Fallback response";
-  try {
-    if (url === "/python") {
-      result = await executeTask("python", ["-m", "src.tasks.demo"]);
-    }
-  } catch (err: any) {
-    console.log(err);
-    response.statusCode = 500;
-    response.end(JSON.stringify(err));
-    return;
-  }
+const app = express();
+app.use(json());
 
-  response.end(JSON.stringify(result));
-};
-
-// eslint-disable-next-line @typescript-eslint/no-misused-promises -- createServer doesn't accept a `Promise<void>` as it expects just a void return signature but we don't want to block
-const server = createServer(requestHandler);
-
-server.on("error", (err) => {
-  console.error(`Error occurred: ${err}`);
+app.get("/python", (_, res) => {
+  executeTask("python", ["-m", "src.tasks.demo"])
+    .then((result) => res.status(200).json(result))
+    .catch((err) => res.status(500).json(err));
 });
 
-server.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}...`));
