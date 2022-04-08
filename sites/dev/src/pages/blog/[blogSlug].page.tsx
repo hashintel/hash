e2eeb997dc3f5a7f630ce04a5e9df9d28b6a3186
Nate Higgins <nat@nath.is>
2022-04-08 16:22:30 +0100

    Redo post head layout to support image growing

--- sites/dev/src/pages/blog/[blogSlug].page.tsx
+++ sites/dev/src/pages/blog/[blogSlug].page.tsx
@@ -1,11 +1,110 @@
 import { Container, Stack, Typography } from "@mui/material";
-import { Box, styled } from "@mui/system";
+import { Box } from "@mui/system";
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import postImage from "../../../public/temp.png";
+import { ReactNode, VFC } from "react";
 import postAvatar from "../../../public/david.png";
+import postImage from "../../../public/temp.png";
 
+const PostHead: VFC<{ title: ReactNode; subtitle: ReactNode }> = ({
+  title,
+  subtitle,
+}) => (
+  <Box pt={8}>
+    <Container
+      sx={(theme) => ({
+        // @todo check these spacings
+        mb: { xs: 9.25, md: 15.5 },
+        [theme.breakpoints.up("md")]: {
+          mx: 0,
+          maxWidth: "calc(100vw - ((100vw - var(--size)) / 2)) !important",
+          ml: "calc((100vw - var(--size)) / 2)",
+          pr: "0px !important",
+        },
+      })}
+    >
+      <Stack direction={{ xs: "column", md: "row" }} alignItems="center">
+        <Box
+          sx={(theme) => ({
+            [theme.breakpoints.up("md")]: {
+              width: "calc(var(--size) * 0.4925)",
+              pr: 0,
+              mr: theme.spacing(9.625),
+            },
+          })}
+        >
+          <Typography variant="hashHeading1" mb={3}>
+            {title}
+          </Typography>
+          <Typography variant="hashLargeText" mb={5} color="gray.80">
+            {subtitle}
+          </Typography>
+          <Stack direction={{ xs: "column", md: "row" }}>
+            <Typography
+              variant="hashSmallText"
+              fontStyle="italic"
+              color="gray.80"
+              order={{ xs: 0, md: 1 }}
+              sx={[
+                {
+                  order: 0,
+                },
+                (theme) => ({
+                  [theme.breakpoints.down("md")]: {
+                    mb: 3,
+                  },
+                  [theme.breakpoints.up("md")]: {
+                    order: 1,
+                    ml: "auto",
+                    alignSelf: "end",
+                  },
+                }),
+              ]}
+            >
+              January 24th, 2022
+            </Typography>
+            <Stack direction="row">
+              <Box
+                width={48}
+                height={48}
+                borderRadius={48}
+                overflow="hidden"
+                position="relative"
+              >
+                <Image src={postAvatar} layout="fill" />
+              </Box>
+              <Stack ml={2} direction="column" spacing={0.5}>
+                <Typography variant="hashMediumCaps" color="purple.600">
+                  Chris Kingle
+                </Typography>
+                <Typography variant="hashMediumCaps">
+                  Platform Engineer at HASH
+                </Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Box>
+        <Box
+          flex={1}
+          position="relative"
+          borderRadius={{
+            xs: "4px",
+            md: "4px 0 0 4px",
+          }}
+          overflow="hidden"
+          order={{ xs: -1, md: 1 }}
+          width={{ xs: 1, md: "auto" }}
+          mb={{ xs: 3, md: 0 }}
+        >
+          <Image src={postImage} layout="responsive" />
+        </Box>
+      </Stack>
+    </Container>
+  </Box>
+);
+
+// @todo semantics
 const BlogPostPage: NextPage = () => {
   return (
     <>
@@ -13,100 +112,20 @@ const BlogPostPage: NextPage = () => {
         {/** @todo check this */}
         <title>Holding down the fort – HASH for Developers</title>
       </Head>
-      <Box pt={8}>
-        {/** @todo check these spacings */}
-        <Box position="relative" mb={{ xs: 9.25, md: 15.5 }}>
-          <Container>
-            <Box
-              sx={[
-                {
-                  width: "100%",
-                  height: "100%",
-                  position: "relative",
-                  overflow: "hidden",
-                },
-                (theme) => ({
-                  [theme.breakpoints.up("md")]: {
-                    position: "absolute",
-                    top: "50%",
-                    transform: "translateY(-50%)",
-                    right: 0,
-                    width: "45vw",
-                    maxHeight: "100%",
-
-                    ".image": {
-                      borderRadius: "4px 0 0 4px",
-                    },
-                  },
-                  [theme.breakpoints.down("md")]: {
-                    mb: 3,
-
-                    ".image": {
-                      borderRadius: "4px",
-                    },
-                  },
-                }),
-              ]}
-            >
-              <Image src={postImage} layout="responsive" className="image" />
-            </Box>
-            <Box width={{ xs: 1, md: "49.25%" }}>
-              <Typography variant="hashHeading1" mb={3}>
-                Holding down the fort: security and permissioning in block-based
-                systems
-              </Typography>
-              <Typography variant="hashLargeText" mb={5} color="gray.80">
-                Chocolate sugar plum brownie cupcake chocolate bar toffee
-                brownie cake. Chocolate sugar plum brownie cupcake.
-              </Typography>
-              <Stack direction={{ xs: "column", md: "row" }}>
-                <Typography
-                  variant="hashSmallText"
-                  fontStyle="italic"
-                  color="gray.80"
-                  order={{ xs: 0, md: 1 }}
-                  sx={[
-                    {
-                      order: 0,
-                    },
-                    (theme) => ({
-                      [theme.breakpoints.down("md")]: {
-                        mb: 3,
-                      },
-                      [theme.breakpoints.up("md")]: {
-                        order: 1,
-                        ml: "auto",
-                        alignSelf: "end",
-                      },
-                    }),
-                  ]}
-                >
-                  January 24th, 2022
-                </Typography>
-                <Stack direction="row">
-                  <Box
-                    width={48}
-                    height={48}
-                    borderRadius={48}
-                    overflow="hidden"
-                    position="relative"
-                  >
-                    <Image src={postAvatar} layout="fill" />
-                  </Box>
-                  <Stack ml={2} direction="column" spacing={0.5}>
-                    <Typography variant="hashMediumCaps" color="purple.600">
-                      Chris Kingle
-                    </Typography>
-                    <Typography variant="hashMediumCaps">
-                      Platform Engineer at HASH
-                    </Typography>
-                  </Stack>
-                </Stack>
-              </Stack>
-            </Box>
-          </Container>
-        </Box>
-      </Box>
+      <PostHead
+        title={
+          <>
+            Holding down the fort: security and permissioning in block-based
+            systems
+          </>
+        }
+        subtitle={
+          <>
+            Chocolate sugar plum brownie cupcake chocolate bar toffee brownie
+            cake. Chocolate sugar plum brownie cupcake.
+          </>
+        }
+      />
       <Container>
         <Typography component="p">
           The computer only knows you've added a bunch of &lt;p&gt; tags to a
--- sites/dev/src/theme/components/layout/MuiContainerThemeOptions.ts
+++ sites/dev/src/theme/components/layout/MuiContainerThemeOptions.ts
@@ -1,9 +1,13 @@
 import { Components } from "@mui/material";
 
+// @todo rename this variables, use them instead of interpolation
 const size = (padding: number) => ({
-  maxWidth: `${1200 + padding * 2}px`,
-  paddingLeft: `${padding}px`,
-  paddingRight: `${padding}px`,
+  "--maxWidth": `${1200 + padding * 2}px`,
+  "--size": `min(100vw, var(--maxWidth))`,
+  "--padding": `${padding}px`,
+  maxWidth: "var(--maxWidth)",
+  paddingLeft: `var(--padding)`,
+  paddingRight: `var(--padding)`,
 });
 
 export const MuiContainerThemeOptions: Components["MuiContainer"] = {
