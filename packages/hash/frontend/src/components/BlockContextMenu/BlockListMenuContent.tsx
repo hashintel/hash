93f75508083f2ad249e6d0ab17fbb71a2598b617
teenoh <ugbalaachinike@gmail.com>
2022-04-11 09:01:40 +0100

    create separate component for block list submenu

---
+++ packages/hash/frontend/src/components/BlockContextMenu/BlockListMenuContent.tsx
@@ -0,0 +1,87 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  Box,
+  InputAdornment,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  TextField,
+} from "@mui/material";
+import { PopupState } from "material-ui-popup-state/core";
+import { useEffect, useRef, useState, VFC } from "react";
+import {
+  BlockSuggesterProps,
+  getVariantIcon,
+} from "../../blocks/page/createSuggester/BlockSuggester";
+import { useFilteredBlocks } from "../../blocks/page/createSuggester/useFilteredBlocks";
+import { useUserBlocks } from "../../blocks/userBlocks";
+import { FontAwesomeIcon } from "../../shared/icons";
+
+type BlockListMenuContentProps = {
+  popupState?: PopupState;
+  blockSuggesterProps: BlockSuggesterProps;
+};
+
+export const BlockListMenuContent: VFC<BlockListMenuContentProps> = ({
+  blockSuggesterProps,
+  popupState,
+}) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const { value: userBlocks } = useUserBlocks();
+  const blocks = useFilteredBlocks(searchQuery, userBlocks);
+
+  useEffect(() => {
+    if (popupState?.isOpen) {
+      searchInputRef.current?.focus();
+    }
+  }, [popupState]);
+
+  return (
+    <MenuList>
+      <Box sx={{ mx: 0.75 }}>
+        <TextField
+          placeholder="Search for entities"
+          fullWidth
+          size="xs"
+          onChange={(evt) => {
+            setSearchQuery(evt.target.value);
+          }}
+          onKeyDown={(evt) => {
+            evt.stopPropagation();
+          }}
+          value={searchQuery}
+          InputProps={{
+            inputRef: searchInputRef,
+            startAdornment: (
+              <InputAdornment position="start">
+                <FontAwesomeIcon icon={faSearch} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+      {blocks.map((option) => (
+        <MenuItem
+          onClick={() => {
+            blockSuggesterProps.onChange(option.variant, option.meta);
+          }}
+          key={`${option.meta.name}/${option.variant.name}`}
+        >
+          <ListItemIcon>
+            <Box
+              component="img"
+              width={16}
+              height={16}
+              overflow="hidden"
+              alt={option.variant.name}
+              src={getVariantIcon(option)}
+            />
+          </ListItemIcon>
+          <ListItemText primary={option?.variant.name} />
+        </MenuItem>
+      ))}
+    </MenuList>
+  );
+};
