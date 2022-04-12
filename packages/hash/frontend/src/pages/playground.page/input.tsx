e5538948191194867df3d9dcb1b0efe307c35620
teenoh <ugbalaachinike@gmail.com>
2022-04-12 14:08:40 +0100

    break playground into smaller components

---
+++ packages/hash/frontend/src/pages/playground.page/input.tsx
@@ -0,0 +1,160 @@
+import {
+  faEnvelope,
+  faQuestionCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Box,
+  Switch,
+  Radio,
+  Checkbox,
+  Stack,
+  InputAdornment,
+  Typography,
+} from "@mui/material";
+import { FontAwesomeIcon } from "../../shared/icons";
+import { Button, FormInline, TextField } from "../../shared/ui";
+
+export const Inputs = () => {
+  return (
+    <Box>
+      <Switch />
+      <br />
+      <Switch size="small" />
+      <br />
+      <Radio sx={{ mr: 2 }} name="btn" value="a" />
+      <Radio name="btn" value="b" />
+      <br />
+      <Checkbox sx={{ mr: 2 }} />
+      <Checkbox checked />
+      <br />
+      <br />
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <TextField
+          defaultValue="small"
+          placeholder="Search for anything"
+          size="small"
+        />
+        <TextField
+          defaultValue="medium"
+          placeholder="Search for anything"
+          size="medium"
+        />
+        <TextField
+          defaultValue="large"
+          placeholder="Search for anything"
+          size="large"
+        />
+      </Stack>
+      <br />
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <TextField placeholder="Search for anything" error />
+        <TextField
+          defaultValue="password1"
+          placeholder="Search for anything"
+          error
+        />
+        <TextField
+          placeholder="Search for anything"
+          defaultValue="name@email.com"
+          size="large"
+          success
+        />
+      </Stack>
+      <br />
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <TextField
+          defaultValue="password1"
+          placeholder="Search for anything"
+          error
+          helperText="Your password must be less than 4 characters."
+        />
+        <TextField
+          defaultValue="password1"
+          placeholder="Search for anything"
+          helperText="Make your password short and easy to guess"
+        />
+      </Stack>
+      <br />
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <TextField
+          placeholder="Search for anything"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <TextField
+          placeholder="Search for anything"
+          error
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <FontAwesomeIcon icon={faQuestionCircle} />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          placeholder="Search for anything"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment
+                sx={({ palette }) => ({
+                  backgroundColor: palette.gray[10],
+                  color: palette.gray[60],
+                  paddingRight: "12px",
+                  border: `1px solid ${palette.gray[30]}`,
+                })}
+                position="start"
+              >
+                <Typography
+                  sx={({ palette }) => ({
+                    color: palette.gray[60],
+                  })}
+                  variant="regularTextLabels"
+                >
+                  https://
+                </Typography>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+      <br />
+      <Stack direction="row" alignItems="flex-start" spacing={2}>
+        <TextField
+          label="Password"
+          helperText="Make your password is short and easy to guess"
+          required
+          showLabelCornerHint
+        />
+        <TextField
+          label="Email"
+          placeholder="you@example.com"
+          showLabelCornerHint
+        />
+        <TextField
+          label="Todo item"
+          defaultValue="my thing"
+          placeholder="you@example.com"
+        />
+      </Stack>
+      <br />
+      <Stack direction="row" alignItems="flex-start" spacing={2}>
+        <TextField placeholder="Something about myself" multiline />
+        <TextField defaultValue="Something about myself" multiline autoResize />
+      </Stack>
+      <br />
+      <Stack direction="row" alignItems="flex-start" spacing={2}>
+        <FormInline>
+          <TextField defaultValue="start typing" />
+          <Button>Submit</Button>
+        </FormInline>
+      </Stack>
+    </Box>
+  );
+};
---
+++ packages/hash/frontend/src/pages/playground.page/select.tsx
@@ -0,0 +1,12 @@
+import { Box, Stack } from "@mui/material";
+import {} from "../../shared/ui";
+
+export const SelectMenus = () => {
+  return (
+    <Box>
+      <Stack direction="row" alignItems="center" spacing={2}>
+        {/* <Box></Box> */}
+      </Stack>
+    </Box>
+  );
+};
