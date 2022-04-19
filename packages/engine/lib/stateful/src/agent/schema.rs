ec3d2590673fc2f6bb1713a988bb159b51d792cb
Tim Diekmann <tim.diekmann@3dvision.de>
2022-04-19 17:11:12 +0200

    Clarify documentation on `AgentSchema`

--- packages/engine/lib/stateful/src/agent/schema.rs
+++ packages/engine/lib/stateful/src/agent/schema.rs
@@ -5,7 +5,7 @@ use memory::arrow::{meta, meta::conversion::HashStaticMeta};
 
 use crate::{error::Result, field::FieldSpecMap};
 
-/// Describes the memory format for every [`Agent`]-containing [`AgentBatch`].
+/// Describes the memory format for [`AgentBatch`]es.
 ///
 /// It contains the dual representation of both the [`FieldSpec`] and the Arrow schema.
 ///
