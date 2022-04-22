84ef90528e2e247c18b6ebddc213d04d145a040e
Nate Higgins <nate@nateh.uk>
2022-04-22 16:00:38 +0100

    Update mobile nav design

--- sites/dev/src/components/Navbar.tsx
+++ sites/dev/src/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 import {
+  alpha,
   Box,
   buttonClasses,
   Container,
+  Fade,
   IconButton,
   Slide,
   Stack,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
+import { SxProps, Theme } from "@mui/system";
 import clsx from "clsx";
 import { useRouter } from "next/router";
 import { useState, VFC } from "react";
@@ -18,6 +21,35 @@ import { FontAwesomeIcon } from "./icons/FontAwesomeIcon";
 import { Logo } from "./Logo";
 
 export const NAV_HEIGHT = 58;
+
+const NAV_BUTTON_STYLES: SxProps<Theme> = {
+  [`.${buttonClasses.root}`]: {
+    "&:not(.NavLink)": {
+      "&.MuiButton-primary": {
+        borderColor: "yellow.500",
+        "&, svg": {
+          color: (theme) => `${theme.palette.yellow[900]} !important`,
+        },
+
+        ":hover, :focus-visible, &.Button--focus:not(:disabled)": {
+          backgroundColor: "yellow.400",
+        },
+      },
+
+      "&.MuiButton-tertiary": {
+        borderColor: "gray.20",
+
+        "&, svg": {
+          color: "gray.70",
+        },
+
+        ":focus-visible, &.Button--focus:not(:disabled)": {
+          borderColor: "gray.40",
+        },
+      },
+    },
+  },
+};
 
 const DesktopNav: VFC = () => {
   const router = useRouter();
@@ -36,47 +68,25 @@ const DesktopNav: VFC = () => {
         direction="row"
         spacing={2}
         ml="auto"
-        sx={{
-          [`.${buttonClasses.root}`]: {
-            minHeight: 32,
-            py: 1,
-            borderRadius: 30,
-            borderWidth: 1,
+        sx={[
+          NAV_BUTTON_STYLES,
+          {
+            [`.${buttonClasses.root}`]: {
+              minHeight: 32,
+              py: 1,
+              borderRadius: 30,
+              borderWidth: 1,
 
-            "&:after": {
-              borderWidth: 3,
-              left: -6,
-              top: -6,
-              right: -6,
-              bottom: -6,
-            },
-
-            "&:not(.NavLink)": {
-              "&.MuiButton-primary": {
-                borderColor: "yellow.500",
-                "&, svg": {
-                  color: (theme) => `${theme.palette.yellow[900]} !important`,
-                },
-
-                ":hover, :focus-visible, &.Button--focus:not(:disabled)": {
-                  backgroundColor: "yellow.400",
-                },
-              },
-
-              "&.MuiButton-tertiary": {
-                borderColor: "gray.20",
-
-                "&, svg": {
-                  color: "gray.70",
-                },
-
-                ":focus-visible, &.Button--focus:not(:disabled)": {
-                  borderColor: "gray.40",
-                },
+              "&:after": {
+                borderWidth: 3,
+                left: -6,
+                top: -6,
+                right: -6,
+                bottom: -6,
               },
             },
           },
-        }}
+        ]}
       >
         <Button
           size="medium"
@@ -138,31 +148,54 @@ const MobileNav: VFC<{ open: boolean; onMenuClose: () => void }> = ({
 }) => {
   const router = useRouter();
   return (
-    <Slide in={open}>
-      <Box
-        sx={{
-          position: "fixed",
-          width: 1,
-          top: NAV_HEIGHT,
-          bottom: 0,
-          bgcolor: "white",
-          zIndex: (theme) => theme.zIndex.appBar - 1,
-          left: 0,
-          [`.${buttonClasses.startIcon}, .${buttonClasses.endIcon}`]: {
-            svg: {
-              fontSize: "18px !important",
-            },
-          },
-        }}
-      >
-        <Container
+    <>
+      <Fade in={open}>
+        <Box
           sx={{
-            height: "100%",
-            pt: 1,
-            pb: 2,
+            position: "fixed",
+            width: 1,
+            top: NAV_HEIGHT,
+            bottom: 0,
+            height: 1,
+            bgcolor: (theme) => alpha(theme.palette.gray[40], 0.4),
+            zIndex: (theme) => theme.zIndex.appBar - 2,
           }}
+        />
+      </Fade>
+      <Slide in={open}>
+        <Box
+          sx={[
+            NAV_BUTTON_STYLES,
+            {
+              position: "fixed",
+              width: 1,
+              top: NAV_HEIGHT,
+              bgcolor: "white",
+              zIndex: (theme) => theme.zIndex.appBar - 1,
+              left: 0,
+              [`.${buttonClasses.startIcon}, .${buttonClasses.endIcon}`]: {
+                svg: {
+                  fontSize: "18px !important",
+                },
+              },
+              boxShadow: `
+              0px 51px 87px rgba(50, 65, 111, 0.07),
+              0px 33.0556px 50.9514px rgba(50, 65, 111, 0.0531481),
+              0px 19.6444px 27.7111px rgba(50, 65, 111, 0.0425185),
+              0px 10.2px 14.1375px rgba(50, 65, 111, 0.035),
+              0px 4.15556px 7.08889px rgba(50, 65, 111, 0.0274815),
+              0px 0.944444px 3.42361px rgba(50, 65, 111, 0.0168519)
+            `,
+              borderRadius: "0px 0px 6px 6px",
+            },
+          ]}
         >
-          <Stack height={1} spacing={1}>
+          <Container
+            sx={{
+              pt: 1,
+              pb: 3.5,
+            }}
+          >
             <Stack
               spacing={2}
               alignItems="flex-start"
@@ -177,13 +210,14 @@ const MobileNav: VFC<{ open: boolean; onMenuClose: () => void }> = ({
                   }}
                 />
               }
+              mb={4}
             >
               <Button
                 className="NavLink"
                 size="large"
                 variant="primary"
                 href="https://hash.ai"
-                endIcon={
+                startIcon={
                   <FaIcon name="arrow-up-right-from-square" type="solid" />
                 }
               >
@@ -202,28 +236,29 @@ const MobileNav: VFC<{ open: boolean; onMenuClose: () => void }> = ({
                 Blog
               </Button>
             </Stack>
-            <Button
-              sx={{ mt: "auto !important" }}
-              variant="tertiary"
-              startIcon={<FaIcon name="discord" type="brands" />}
-              size="large"
-              href="https://hash.ai/discord"
-            >
-              Chat to us on Discord
-            </Button>
-            <Button
-              size="large"
-              variant="primary"
-              startIcon={<FaIcon name="envelope" type="regular" />}
-              href="#subscribe"
-              onClick={() => onMenuClose()}
-            >
-              Join the mailing list
-            </Button>
-          </Stack>
-        </Container>
-      </Box>
-    </Slide>
+            <Stack spacing={1.25}>
+              <Button
+                variant="tertiary"
+                startIcon={<FaIcon name="discord" type="brands" />}
+                size="large"
+                href="https://hash.ai/discord"
+              >
+                Chat to us on Discord
+              </Button>
+              <Button
+                size="large"
+                variant="primary"
+                startIcon={<FaIcon name="envelope" type="regular" />}
+                href="#subscribe"
+                onClick={() => onMenuClose()}
+              >
+                Join the mailing list
+              </Button>
+            </Stack>
+          </Container>
+        </Box>
+      </Slide>
+    </>
   );
 };
 
