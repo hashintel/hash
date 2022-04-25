63ecebfa156b966fbedd436e9f0e4bf642f98ffe
teenoh <ugbalaachinike@gmail.com>
2022-04-25 16:06:10 +0100

    base setup

---
+++ packages/blocks/drawing-canvas/src/app.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import { BlockComponent } from "blockprotocol/react";
+import { Tldraw } from "@tldraw/tldraw";
+
+type AppProps = {
+  name: string;
+};
+
+export const App: BlockComponent<AppProps> = ({ entityId, name }) => (
+  <>
+    <h1>Hello, {name}!</h1>
+    <p>
+      The entityId of this block is {entityId}. Use it to update its data when
+      calling updateEntities.
+    </p>
+    <div>
+      <Tldraw />
+    </div>
+  </>
+);
