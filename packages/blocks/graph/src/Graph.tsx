01320a19003ce3509f293d76bc18c0c5778974c1
Ben Werner <ben.werner01@gmail.com>
2022-04-25 16:00:00 +0100

    ft: populate graph using predefined aggregation

--- packages/blocks/graph/src/Graph.tsx
+++ packages/blocks/graph/src/Graph.tsx
@@ -1,50 +1,45 @@
 import * as React from "react";
 
 import * as echarts from "echarts/core";
-import "echarts/lib/component/grid";
-import { LineChart, LineSeriesOption } from "echarts/charts";
+import {
+  LineChart,
+  LineSeriesOption,
+  ScatterChart,
+  ScatterSeriesOption,
+} from "echarts/charts";
 import { GridComponent } from "echarts/components";
 import { SVGRenderer } from "echarts/renderers";
 
+export type SeriesOption = LineSeriesOption | ScatterSeriesOption;
+
 // Combine an Option type with only required components and charts via ComposeOption
-type ECOption = echarts.ComposeOption<LineSeriesOption>;
+export type ECOption = echarts.ComposeOption<SeriesOption>;
+
+export type SupportedSeriesType = "scatter" | "line";
 
 // Register the required components
-echarts.use([LineChart, GridComponent, SVGRenderer]);
+echarts.use([LineChart, ScatterChart, GridComponent, SVGRenderer]);
 
-type GraphProps = {};
+type GraphProps = {
+  options: ECOption;
+};
 
-export const Graph: React.FC<GraphProps> = ({}) => {
+export const Graph: React.FC<GraphProps> = ({ options }) => {
   const wrapperRef = React.useRef<HTMLDivElement>(null);
 
   const [chart, setChart] = React.useState<echarts.ECharts>();
-  const [echartOptions, setEchartOptions] = React.useState<ECOption>({
-    xAxis: {
-      type: "category",
-      data: ["A", "B", "C"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [120, 200, 150],
-        type: "line",
-      },
-    ],
-  });
 
   React.useEffect(() => {
     if (wrapperRef.current) {
       setChart(echarts.init(wrapperRef.current));
     }
-  }, [wrapperRef.current]);
+  }, [wrapperRef]);
 
   React.useEffect(() => {
     if (chart) {
-      chart.setOption(echartOptions);
+      chart.setOption(options);
     }
-  }, [chart, echartOptions]);
+  }, [chart, options]);
 
   return <div style={{ width: "100%", height: 500 }} ref={wrapperRef} />;
 };
--- packages/blocks/graph/src/app.tsx
+++ packages/blocks/graph/src/app.tsx
@@ -1,16 +1,124 @@
 import * as React from "react";
 
 import { BlockComponent } from "blockprotocol/react";
-import { Graph } from "./Graph";
+import { Graph, SeriesOption, ECOption } from "./Graph";
 
-type AppProps = {
-  name: string;
+type AppProps = {};
+
+type GraphDefinition = {
+  title: string;
+  xAxisName: string;
+  yAxisName: string;
 };
 
-export const App: BlockComponent<AppProps> = ({ entityId, name }) => (
-  <>
-    <h1>Hello, {name}!</h1>
-    <h2>I am a graph block :)</h2>
-    <Graph />
-  </>
-);
+type SeriesDefinition = {
+  seriesType: SeriesOption["type"];
+  entityTypeId: string;
+  xValuePropertyKey: string;
+  yValuePropertyKey: string;
+};
+
+type Series = NonNullable<ECOption["series"]>;
+
+export const App: BlockComponent<AppProps> = ({
+  accountId,
+  aggregateEntities,
+}) => {
+  if (!aggregateEntities) {
+    throw new Error("");
+  }
+
+  /** @todo: store this on the block entity */
+  const [graphDefinition] = React.useState<GraphDefinition>({
+    title: "Graph Block",
+    xAxisName: "X Axis",
+    yAxisName: "Y Axis",
+  });
+
+  const [seriesDefinitions] = React.useState<SeriesDefinition[]>([
+    {
+      seriesType: "scatter",
+      entityTypeId: "91288e7d-dde6-47cf-b388-b3330073dba7",
+      xValuePropertyKey: "x",
+      yValuePropertyKey: "y",
+    },
+  ]);
+
+  const [series, setSeries] = React.useState<Series>([]);
+
+  const populateSeries = React.useCallback(
+    async (definitions: SeriesDefinition[]) => {
+      const updatedSeries: Series = await Promise.all(
+        definitions.map(
+          async ({
+            seriesType,
+            entityTypeId,
+            xValuePropertyKey,
+            yValuePropertyKey,
+          }) => {
+            const { results: allEntitiesOfType } = await aggregateEntities({
+              accountId,
+              operation: {
+                entityTypeId,
+              },
+            });
+
+            return {
+              type: seriesType,
+              data: allEntitiesOfType.map((properties) => {
+                if (!properties[xValuePropertyKey]) {
+                  throw new Error(
+                    `No property with key '${xValuePropertyKey}' found on entity`,
+                  );
+                }
+                if (!properties[xValuePropertyKey]) {
+                  throw new Error(
+                    `No property with key '${xValuePropertyKey}' found on entity`,
+                  );
+                }
+
+                const xValue = properties[xValuePropertyKey];
+                if (typeof xValue !== "number") {
+                  throw new Error("The x value is not a number");
+                }
+
+                const yValue = properties[yValuePropertyKey];
+                if (typeof yValue !== "number") {
+                  throw new Error("The y value is not a number");
+                }
+
+                return [xValue, yValue];
+              }),
+            };
+          },
+        ),
+      );
+
+      setSeries(updatedSeries);
+    },
+    [aggregateEntities, setSeries, accountId],
+  );
+
+  React.useEffect(() => {
+    void populateSeries(seriesDefinitions);
+  }, [seriesDefinitions, populateSeries]);
+
+  return (
+    <>
+      <h1>{graphDefinition.title}</h1>
+      <Graph
+        options={{
+          yAxis: {
+            type: "value",
+            name: graphDefinition.yAxisName,
+          },
+          xAxis: {
+            type: "value",
+            name: graphDefinition.xAxisName,
+          },
+          series,
+        }}
+      />
+    </>
+  );
+};
