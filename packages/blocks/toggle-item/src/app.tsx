c412819f9cda7e2b34f9413958751e50f8669bf1
Ciaran Morinan <c@hash.ai>
2022-04-26 17:47:49 +0100

    toggle-item with text inputs instead of editableRef

--- packages/blocks/toggle-item/src/app.tsx
+++ packages/blocks/toggle-item/src/app.tsx
@@ -3,21 +3,48 @@ import React, { RefCallback } from "react";
 import { BlockComponent } from "blockprotocol/react";
 
 type AppProps = {
-  editableRef?: RefCallback<HTMLElement>;
-  title: HTMLElement;
-  content: HTMLDetailsElement;
+  title: string;
+  content: string;
 };
 
 export const App: BlockComponent<AppProps> = ({
-  editableRef,
+  accountId,
+  entityId,
   title,
   content,
+  updateEntities,
 }) => {
+  const updateContent = (text: string, field: "content" | "title") => {
+    updateEntities([
+      {
+        accountId,
+        entityId,
+        data: {
+          content,
+          title,
+          [field]: text,
+        },
+      },
+    ]);
+  };
+
   return (
     <div>
-      <details ref={editableRef}>
-        <summary>{editableRef ? undefined : title}</summary>
-        {editableRef ? undefined : content}
+      <details>
+        <summary>
+          <input
+            defaultValue={title}
+            type="text"
+            placeholder="Write a title"
+            onBlur={(event) => updateContent(event.target.value, "title")}
+          />
+        </summary>
+        <input
+          defaultValue={content}
+          type="text"
+          placeholder="Your detailed content"
+          onBlur={(event) => updateContent(event.target.value, "content")}
+        />
       </details>
     </div>
   );
