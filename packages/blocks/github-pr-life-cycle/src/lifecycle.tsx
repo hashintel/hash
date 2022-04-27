f027115da86802879a9063077642ab54c2095a71
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-04-27 11:43:23 +0100

    Adding open event to timeline, timeToMerge, avatars

--- packages/blocks/github-pr-life-cycle/src/lifecycle.tsx
+++ packages/blocks/github-pr-life-cycle/src/lifecycle.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
+import { uniqBy } from "lodash";
+import moment from "moment";
 
-import { GithubIssueEvent, GithubPullRequest, GithubReview } from "./types";
+import {
+  GithubIssueEvent,
+  GithubPullRequest,
+  GithubReview,
+  isDefined,
+} from "./types";
 
 export type GithubPrLifeCycleProps = {
   pullRequest: GithubPullRequest;
@@ -15,17 +24,41 @@ export type GithubPrLifeCycleProps = {
   events: GithubIssueEvent[];
 };
 
+export const Reviewer = (login: string, avatar_url?: string | null) => (
+  <Stack direction="row" spacing={1}>
+    <Avatar
+      alt={login}
+      src={avatar_url ?? undefined}
+      sx={{ width: "0.8em", height: "0.8em" }}
+      style={{ alignSelf: "center" }}
+    />
+    <span>{login}</span>
+  </Stack>
+);
+
 export const GithubPrLifeCycle: React.FunctionComponent<
   GithubPrLifeCycleProps
 > = ({ pullRequest, reviews, events }) => {
   const maxIdx = events.length - 1;
+  const uniqueReviewers = uniqBy(
+    reviews.map((review) => {
+      return { login: review.user!.login, avatar_url: review.user!.avatar_url };
+    }),
+    "login",
+  );
 
+  const timeToMerge =
+    pullRequest.closed_at != null
+      ? moment(pullRequest.closed_at).from(moment(pullRequest.created_at), true)
+      : undefined;
+  // there isn't an issue event for opening so we manually make an object to append to the timeline
+  const openedEvent = { id: pullRequest.node_id, event: "opened" };
   return (
     <Grid container className="lifeCycleContainer">
       <Grid item xs={4}>
         <div className="timeline">
           <Timeline position="left">
-            {events.map((event, idx) => {
+            {[openedEvent, ...events].map((event, idx) => {
               return (
                 <TimelineItem key={event.id}>
                   <TimelineSeparator>
@@ -46,21 +79,30 @@ export const GithubPrLifeCycle: React.FunctionComponent<
             <span style={{ color: "grey" }}>#{pullRequest.number}</span>
           </h1>
           <h2>
-            Status: <span style={{ color: "green" }}>Merged</span>
+            Status: <span style={{ color: "green" }}>{pullRequest.state}</span>
           </h2>
-          <br />
-          Merged After: 2 Days and 2 Hours
-          <br />
-          Reviews: 4
-          <br />
-          Reviewers:
-          <ol>
-            <li>kachkaev</li>
-            <li>semgrep-app</li>
-            <li>teenoh</li>
-            <li>nathggns</li>
-          </ol>
-          <br />
+          <Stack>
+            {timeToMerge ? <div>Merged After: {timeToMerge}</div> : undefined}
+            <div>Reviews: {reviews.length}</div>
+            <div>
+              Pending Reviewers:
+              {pullRequest.requested_reviewers
+                ?.filter(isDefined)
+                .map((reviewer) => (
+                  <div key={reviewer.login}>
+                    {Reviewer(reviewer.login!, reviewer.avatar_url)}
+                  </div>
+                ))}
+            </div>
+            <div>
+              Reviewed By:
+              {uniqueReviewers.map((reviewer) => (
+                <div key={reviewer.login}>
+                  {Reviewer(reviewer.login!, reviewer.avatar_url)}
+                </div>
+              ))}
+            </div>
+          </Stack>
         </div>
       </Grid>
     </Grid>
