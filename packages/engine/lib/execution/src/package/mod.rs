f0e7d4dfeb54f5e0f01482252272527ec947a596
Tim Diekmann <tim.diekmann@3dvision.de>
2022-05-02 17:43:47 +0200

    Fix package doc

--- packages/engine/lib/execution/src/package/mod.rs
+++ packages/engine/lib/execution/src/package/mod.rs
@@ -1,3 +1,149 @@
+//! # Creating a new package
+//!
+//! The following example will guide through creating and enabling a new, simple state package.
+//!
+//! First of all, the [`Package`](`state::Package`) and its
+//! [`PackageCreator`](state::PackageCreator) has to be created:
+//!
+//! ```
+//! # use std::sync::Arc;
+//! #
+//! # use async_trait::async_trait;
+//! # use execution::{
+//! #     package::{
+//! #         state::{StatePackage, StatePackageCreator},
+//! #         Package, PackageComms, PackageCreator, PackageCreatorConfig, PackageInitConfig,
+//! #     },
+//! #     Result,
+//! # };
+//! # use stateful::{context::Context, field::FieldSpecMapAccessor, state::State};
+//! # use tracing::Span;
+//! pub struct GreetingPackage;
+//!
+//! impl Package for GreetingPackage {
+//!     fn start_message(&self) -> Result<serde_json::Value> {
+//!         Ok(serde_json::Value::Null)
+//!     }
+//! }
+//!
+//! #[async_trait]
+//! impl StatePackage for GreetingPackage {
+//!     async fn run(&mut self, _state: &mut State, _context: &Context) -> Result<()> {
+//!         println!("Hello HASH!");
+//!         Ok(())
+//!     }
+//!
+//!     fn span(&self) -> Span {
+//!         Span::current()
+//!     }
+//! }
+//!
+//! pub struct GreetingCreator;
+//!
+//! impl PackageCreator for GreetingCreator {
+//!     fn init_message(&self) -> Result<serde_json::Value> {
+//!         Ok(serde_json::Value::Null)
+//!     }
+//! }
+//!
+//! impl<C> StatePackageCreator<C> for GreetingCreator {
+//!     fn create(
+//!         &self,
+//!         config: &PackageCreatorConfig,
+//!         init_config: &PackageInitConfig,
+//!         comms: PackageComms<C>,
+//!         accessor: FieldSpecMapAccessor,
+//!     ) -> Result<Box<dyn StatePackage>> {
+//!         Ok(Box::new(GreetingPackage))
+//!     }
+//! }
+//! ```
+//!
+//! Next, the package needs to be added to the known package [`Name`](state::Name)s, to the
+//! [`METADATA`](state::METADATA), and to the [`PackageCreator`](state::PackageCreator):
+//!
+//! ```
+//! # use std::{collections::HashMap, sync::Arc};
+//! #
+//! # use execution::{
+//! #     package::{Dependencies, PackageInitConfig, PackageType},
+//! #     Result,
+//! # };
+//! # use stateful::field::PackageId;
+//! # struct PackageCreators;
+//! # struct GreetingCreator;
+//! # impl GreetingCreator {
+//! #     fn dependencies() -> Dependencies {
+//! #         Dependencies::new()
+//! #     }
+//! # }
+//! # #[derive(PartialEq, Eq, Hash)]
+//! pub enum Name {
+//!     BehaviorExecution,
+//!     Topology,
+//!     Greeting,
+//! }
+//!
+//! impl PackageCreators {
+//!     pub(crate) fn initialize_for_experiment_run(
+//!         &self,
+//!         experiment_config: &Arc<PackageInitConfig>,
+//!     ) -> Result<()> {
+//!         use Name::{BehaviorExecution, Greeting, Topology};
+//!         # let mut m = HashMap::new();
+//!         // ...
+//!         m.insert(Greeting, Box::new(GreetingCreator));
+//!         // ...
+//!         # Ok(())
+//!     }
+//! }
+//!
+//! # pub struct PackageMetadata { id: Option<PackageId>, dependencies: Dependencies };
+//! # lazy_static::lazy_static! {
+//! pub static ref METADATA: HashMap<Name, PackageMetadata> = {
+//!     use Name::{BehaviorExecution, Greeting, Topology};
+//!     # let mut id_creator = std::iter::repeat(PackageId::from(0));
+//!     # let mut m = HashMap::new();
+//!     // ...
+//!     m.insert(
+//!         Greeting,
+//!         PackageMetadata {
+//!             id: id_creator.next(),
+//!             dependencies: GreetingCreator::dependencies(),
+//!         },
+//!     );
+//!     // ...
+//!     # m
+//! };
+//! # }
+//! ```
+//!
+//! As a final step, the package has to be loaded. As currently there is no way for configuring
+//! packages dynamically, this should be added to the default packages in the package
+//! [`Config`](crate::config::PackageConfig):
+//!
+//! ```
+//! # struct Config;
+//! # enum StatePackage { BehaviorExecution, Topology, Greeting };
+//! impl Config {
+//!     fn default_state_packages() -> Vec<StatePackage> {
+//!         vec![
+//!             StatePackage::BehaviorExecution,
+//!             StatePackage::Topology,
+//!             StatePackage::Greeting,
+//!         ]
+//!     }
+//! }
+//! ```
+// TODO: Add example for creating a runner-based package
+//   see https://app.asana.com/0/0/1202153839209941/f
+//   Things to cover:
+//    - Task has to be added for the package type
+//    - package.js and package.py has to be created
+//    - which functions has to be added?
+//    - ...
+// TODO: Add docs on different package types (also mention the different signatures)
+
 pub mod context;
 pub mod init;
 pub mod output;
