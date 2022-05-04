9e8b1259004b0e7cea4850cac6e2b97c77b6cc58
leudz <dylan.ancel@gmail.com>
2022-05-04 17:24:57 +0200

    Precise import function error

--- packages/engine/src/worker/runner/javascript/mod.rs
+++ packages/engine/src/worker/runner/javascript/mod.rs
@@ -133,7 +133,7 @@ impl<'s> JsPackage<'s> {
                 if !(func_or_undefined.is_function() || func_or_undefined.is_undefined()) {
                     return Err(Error::PackageImport(
                         path.clone(),
-                        format!("{fn_name} should be a function"),
+                        format!("{fn_name} should be a function or undefined"),
                     ));
                 }
 
