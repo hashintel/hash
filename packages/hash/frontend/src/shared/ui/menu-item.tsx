e98de1bbfd3fd7fbc38df12d677b3bb1a66ed307
teenoh <ugbalaachinike@gmail.com>
2022-05-09 14:48:10 +0100

    add custom menu item component

---
+++ packages/hash/frontend/src/shared/ui/menu-item.tsx
@@ -0,0 +1,63 @@
+// eslint-disable-next-line no-restricted-imports
+import Link from "next/link";
+import {
+  listItemIconClasses,
+  listItemTextClasses,
+  MenuItem as MuiMenuItem,
+  MenuItemProps as MuiMenuItemProps,
+  typographyClasses,
+} from "@mui/material";
+import { FC, forwardRef, ReactNode, useMemo } from "react";
+import { isHrefExternal } from "./link";
+
+export type MenuItemProps = {
+  children: ReactNode;
+  href?: string;
+  faded?: boolean;
+} & MuiMenuItemProps;
+
+export const MenuItem: FC<MenuItemProps> = forwardRef(
+  ({ children, href, sx = [], faded, ...props }, ref) => {
+    const linkProps = useMemo(() => {
+      if (href && isHrefExternal(href)) {
+        return {
+          rel: "noopener",
+          target: "_blank",
+          href,
+        };
+      }
+
+      return {};
+    }, [href]);
+
+    const Component = (
+      <MuiMenuItem
+        sx={[
+          ({ palette }) => ({
+            ...(faded && {
+              [`& .${listItemTextClasses.primary}, .${typographyClasses.root}`]:
+                {
+                  color: palette.gray[60],
+                },
+              [`& .${listItemIconClasses.root}, & svg`]: {
+                color: palette.gray[40],
+              },
+            }),
+          }),
+          ...(Array.isArray(sx) ? sx : [sx]),
+        ]}
+        {...props}
+        {...linkProps}
+        ref={ref}
+      >
+        {children}
+      </MuiMenuItem>
+    );
+
+    if (href && !isHrefExternal(href)) {
+      return <Link href={href}>{Component}</Link>;
+    }
+
+    return Component;
+  },
+);
