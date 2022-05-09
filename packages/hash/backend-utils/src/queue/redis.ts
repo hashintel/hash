53c2f50bb037d65529a4e9fdc446890e9865d1e8
Ahmad Sattar <thehabbos007@gmail.com>
2022-05-09 18:36:35 +0200

    Cache docker layers for deployment action and folder trigger (#532)
    - Cache docker layers
    - Cache optimize docker images
    - Only deploy when changes to backend has been made
    - Allow manual deployment

--- packages/hash/backend-utils/src/queue/redis.ts
+++ packages/hash/backend-utils/src/queue/redis.ts
@@ -1,8 +1,8 @@
 import { randomUUID } from "crypto";
 
-import { sleep } from "@hashintel/hash-shared/sleep";
 import { QueueProducer, QueueExclusiveConsumer } from "./adapter";
 import { AsyncRedisClient } from "../redis";
+import { sleep } from "../utils";
 
 // The interval on which a consumer which owns the queue will re-affirm their ownership.
 const QUEUE_CONSUMER_OWNERSHIP_HEARTBEAT_MS = 3_000;
