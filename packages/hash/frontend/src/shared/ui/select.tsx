a434feaf8e307182e78fa491d31e4554d965c4f3
teenoh <ugbalaachinike@gmail.com>
2022-05-10 11:35:31 +0100

    add custom select component

--- packages/hash/frontend/src/shared/ui/select.tsx
+++ packages/hash/frontend/src/shared/ui/select.tsx
@@ -1,25 +1,87 @@
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 import {
+  Box,
+  BoxProps,
+  Collapse,
+  InputLabel,
+  outlinedInputClasses,
   Select as MuiSelect,
   SelectProps as MuiSelectProps,
+  Typography,
 } from "@mui/material";
 import { FC, forwardRef, ReactNode } from "react";
+import { FontAwesomeIcon } from "../icons";
 
 export type SelectProps = {
   children: ReactNode;
-} & MuiSelectProps;
+  error?: boolean;
+  helperText?: ReactNode;
+  sx?: BoxProps["sx"];
+  selectSx?: MuiSelectProps["sx"];
+} & Omit<MuiSelectProps, "sx">;
+
+// @todo add custom class for component so it's easier to target the
+// component using css
+
+// For some reasons, renderValue prop doesn't properly infer types
+// when we use this wrapper Select component. But works properly
+// when the default Select component from MUI is used
+// see select-with-search-checkbox.tsx in playground.page
+// @todo figure out why this happens and fix
+
+// @todo
 
 export const Select: FC<SelectProps> = forwardRef(
-  ({ children, sx = [], ...props }, ref) => {
+  (
+    { children, sx = [], selectSx = [], error, helperText, label, ...props },
+    ref,
+  ) => {
     return (
-      <MuiSelect
-        // sx={[({ palette }) => ({
-
-        // }), ...(Array.isArray(sx) ? sx : [sx])]}
-        {...props}
-        ref={ref}
-      >
-        {children}
-      </MuiSelect>
+      <Box sx={sx}>
+        {label && (
+          <InputLabel {...(props.labelId && { id: props.labelId })}>
+            {label}
+          </InputLabel>
+        )}
+        <MuiSelect
+          fullWidth
+          sx={[
+            ({ palette }) => ({
+              ...(error && {
+                [`.${outlinedInputClasses.notchedOutline}`]: {
+                  borderColor: `${palette.orange[50]} !important`,
+                },
+              }),
+            }),
+            ...(Array.isArray(selectSx) ? selectSx : [selectSx]),
+          ]}
+          {...props}
+          ref={ref}
+        >
+          {children}
+        </MuiSelect>
+        <Collapse in={!!helperText}>
+          <Box display="flex" alignItems="center" mt={1}>
+            <FontAwesomeIcon
+              sx={({ palette }) => ({
+                mr: 1,
+                color: palette.orange[50],
+              })}
+              icon={faCircleExclamation}
+            />
+            <Typography
+              variant="microText"
+              sx={({ palette }) => ({
+                color: palette.orange[80],
+              })}
+            >
+              {helperText}
+            </Typography>
+          </Box>
+        </Collapse>
+      </Box>
     );
   },
 );
+
+Select.displayName = "Select";
