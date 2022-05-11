b5647013d2e5f9f7398fd34c0723773b82f635cc
Dylan Ancel <dylan.ancel@gmail.com>
2022-05-11 18:15:39 +0200

    Skip Arrow validation for ArrayData (#559)

--- packages/engine/lib/execution/src/runner/javascript/mod.rs
+++ packages/engine/lib/execution/src/runner/javascript/mod.rs
@@ -1217,8 +1217,13 @@ impl<'s> ThreadLocalRunner<'s> {
                 .null_count(data.null_count + target_len - data.len);
         }
 
-        // TODO: OPTIM: skip validation within `build()` for non-debug builds
-        Ok(builder.build()?)
+        // TODO: Either move to Arrow2 or fix the validation error
+        // Arrow2 task: https://app.asana.com/0/1199548034582004/1201999214936733/f
+        //
+        // Arrow's null checking fails for a couple of tests when trying to validate the
+        // conversion from JS to Rust. This is likely a false-positive and will be fixed by moving
+        // to Arrow2. In the meantime we bypass the validation.
+        Ok(unsafe { builder.build_unchecked() })
     }
 
     fn flush_batch(
