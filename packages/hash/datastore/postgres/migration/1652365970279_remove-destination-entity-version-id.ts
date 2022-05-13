5cdf35e83f8c94c921430b8c6b2dc274549956d7
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2022-05-13 06:03:48 +0100

    Update packages/hash/datastore/postgres/migration/1652365970279_remove-destination-entity-version-id.ts
    Co-authored-by: Ben Werner <42802102+benwerner01@users.noreply.github.com>

--- packages/hash/datastore/postgres/migration/1652365970279_remove-destination-entity-version-id.ts
+++ packages/hash/datastore/postgres/migration/1652365970279_remove-destination-entity-version-id.ts
@@ -8,15 +8,19 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.addColumn("links", {
-    destination_entity_version_id: {
-      type: "uuid",
-      comment: stripNewLines(`
+  pgm.addColumn(
+    "links",
+    {
+      destination_entity_version_id: {
+        type: "uuid",
+        comment: stripNewLines(`
           The entity version id of a specific version of the link's destination
           entity which is defined only if this link is pinned to a specific version
           of the destination entity. When set to null, the link is to the latest
           version of the destination entity.
         `),
+      },
     },
-  });
+    { ifNotExists: true },
+  );
 }
