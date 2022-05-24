cf91b0e0036326410e7f3aaa1043fee02240059d
Ciaran Morinan <c@hash.ai>
2022-05-24 13:32:18 +0100

    make entityTypeId optional in aggregateEntities

--- packages/hash/api/src/graphql/resolvers/entity/aggregateEntity.ts
+++ packages/hash/api/src/graphql/resolvers/entity/aggregateEntity.ts
@@ -18,13 +18,14 @@ export const dbAggregateEntity =
     const multiSort = operation?.multiSort ?? [{ field: "updatedAt" }];
     const multiFilter = operation?.multiFilter;
 
-    // TODO: this returns an array of all entities of the given type in the account.
+    // TODO: this returns an array of all entities matching the type filter (if any) in the account.
     // We should perform the sorting & filtering in the database for better performance.
     // For pagination, using a database cursor may be an option.
-    const entities = await Entity.getEntitiesByType(db, {
+    const entities = await Entity.getAccountEntities(db, {
       accountId,
-      entityTypeId,
-      latestOnly: true,
+      entityTypeFilter: {
+        entityTypeId,
+      },
     });
 
     const startIndex = pageNumber === 1 ? 0 : (pageNumber - 1) * itemsPerPage;
