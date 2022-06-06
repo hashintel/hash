351c9d2c3e02b65305a33641266745b4f0695dfb
teenoh <ugbalaachinike@gmail.com>
2022-06-06 15:13:52 +0100

    attempt to reduce git diff

--- packages/hash/frontend/src/shared/ui.ts
+++ packages/hash/frontend/src/shared/ui.ts
@@ -1,3 +1,3 @@
-export * from "./ui/button";
-export * from "./ui/menu-item";
-export * from "./ui/link";
+export * from "./app-ui/button";
+export * from "./app-ui/menu-item";
+export * from "./app-ui/link";
