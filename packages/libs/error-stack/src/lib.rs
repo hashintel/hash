76f341f83ec54e4939cf28e3b8cbb03a65e2227f
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-06-07 17:52:25 +0200

    Support storing the `FrameKind` when creating a `Frame` (#615)

--- packages/libs/error-stack/src/lib.rs
+++ packages/libs/error-stack/src/lib.rs
@@ -216,7 +216,7 @@ pub use self::future::FutureExt;
 pub use self::hook::HookAlreadySet;
 pub use self::{
     context::Context,
-    frame::Frame,
+    frame::{Frame, FrameKind},
     macros::*,
     report::Report,
     result::{IntoReport, Result, ResultExt},
@@ -230,7 +230,7 @@ pub(crate) mod test_helper {
     };
     use core::{fmt, fmt::Formatter};
 
-    use crate::{Context, Report};
+    use crate::{Context, Frame, FrameKind, Report};
 
     #[derive(Debug, PartialEq)]
     pub struct ContextA;
@@ -255,6 +255,9 @@ pub(crate) mod test_helper {
     #[cfg(feature = "std")]
     impl std::error::Error for ContextB {}
 
+    #[cfg(not(feature = "std"))]
+    impl Context for ContextB {}
+
     pub fn capture_error<E>(closure: impl FnOnce() -> Result<(), Report<E>>) -> Report<E> {
         match closure() {
             Ok(_) => panic!("Expected an error"),
@@ -264,5 +267,9 @@ pub(crate) mod test_helper {
 
     pub fn messages<E>(report: &Report<E>) -> Vec<String> {
         report.frames().map(ToString::to_string).collect()
+    }
+
+    pub fn frame_kinds<E>(report: &Report<E>) -> Vec<FrameKind> {
+        report.frames().map(Frame::kind).collect()
     }
 }
