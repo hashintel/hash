da721907db343664bca1f9263ae340547729e2a2
Ugbala Valentino <ugbalaachinike@gmail.com>
2022-06-07 19:57:44 +0100

    Restyle Countdown block (#583)
    * add base styles
    
    * add date picker override styles
    
    * remove need for selectsrange
    
    * update countdown logic
    
    * add flag to determine if time is displayed
    
    * removed seconds from countdown
    
    * design adjustments
    
    * styling adjustments
    
    * add preview image and update block schema
    
    Co-authored-by: Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>

---
+++ packages/blocks/countdown/src/countdown-title.tsx
@@ -0,0 +1,53 @@
+import React, { ChangeEvent, useEffect, useRef, VFC } from "react";
+
+type CountdownTitleProps = {
+  value: string | undefined;
+  onChangeText: (val: string) => void;
+  onBlur: () => void;
+};
+
+export const CountdownTitle: VFC<CountdownTitleProps> = ({
+  value,
+  onChangeText,
+  onBlur,
+}) => {
+  const textareaEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!textareaEl.current) return;
+    textareaEl.current.textContent = value ?? "";
+  }, [value]);
+
+  const handleChange = (evt: ChangeEvent<HTMLDivElement>) => {
+    if (!textareaEl.current) return;
+    textareaEl.current.textContent = evt.target.textContent;
+    onChangeText(evt.currentTarget.textContent ?? "");
+  };
+
+  return (
+    <div className="countdown-title">
+      <div
+        ref={textareaEl}
+        className="title-input"
+        contentEditable
+        placeholder="Event name"
+        onInput={handleChange}
+        onBlur={onBlur}
+      />
+      <button onClick={onBlur} type="button">
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 17 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15.5 4.5C15.5 4.78125 15.375 5.03125 15.1875 5.21875L7.1875 13.2188C7 13.4062 6.75 13.5 6.5 13.5C6.21875 13.5 5.96875 13.4062 5.78125 13.2188L1.78125 9.21875C1.59375 9.03125 1.5 8.78125 1.5 8.5C1.5 7.9375 1.9375 7.5 2.5 7.5C2.75 7.5 3 7.625 3.1875 7.8125L6.5 11.0938L13.7812 3.8125C13.9688 3.625 14.2188 3.5 14.5 3.5C15.0312 3.5 15.5 3.9375 15.5 4.5Z"
+            fill="currentColor"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
---
+++ packages/blocks/countdown/src/date-picker-input.tsx
@@ -0,0 +1,132 @@
+import React, {
+  FC,
+  forwardRef,
+  HTMLAttributes,
+  MutableRefObject,
+  useRef,
+} from "react";
+import DatePicker, { ReactDatePickerProps } from "react-datepicker";
+
+const CalenderIcon: FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <svg
+      width="17"
+      height="16"
+      viewBox="0 0 17 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+    >
+      <g opacity="0.5">
+        <path
+          d="M6.25 2H10.75V0.75C10.75 0.34375 11.0625 0 11.5 0C11.9062 0 12.25 0.34375 12.25 0.75V2H13.5C14.5938 2 15.5 2.90625 15.5 4V14C15.5 15.125 14.5938 16 13.5 16H3.5C2.375 16 1.5 15.125 1.5 14V4C1.5 2.90625 2.375 2 3.5 2H4.75V0.75C4.75 0.34375 5.0625 0 5.5 0C5.90625 0 6.25 0.34375 6.25 0.75V2ZM3 14C3 14.2812 3.21875 14.5 3.5 14.5H13.5C13.75 14.5 14 14.2812 14 14V6H3V14Z"
+          fill="currentColor"
+        />
+      </g>
+    </svg>
+  );
+};
+
+const CustomInput = forwardRef<
+  HTMLInputElement,
+  HTMLAttributes<HTMLInputElement> & { displayTime: boolean }
+>(({ ...props }, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <input
+        data-expanded={props.displayTime ? "true" : "false"}
+        type="text"
+        ref={(element) => {
+          if (!ref || !inputRef) return;
+
+          (inputRef as MutableRefObject<HTMLInputElement | null>).current =
+            element;
+          if (typeof ref === "function") {
+            ref(element);
+          } else {
+            // eslint-disable-next-line no-param-reassign
+            ref.current = element;
+          }
+        }}
+        {...props}
+      />
+      <CalenderIcon onClick={() => inputRef.current?.focus()} />
+    </>
+  );
+});
+
+type CustomTimeInputProps = {
+  onChange?: (val: string) => void;
+  displayTime: boolean;
+  setDisplayTime: (val: boolean) => void;
+} & Omit<HTMLAttributes<HTMLInputElement>, "onChange">;
+
+const CustomTimeInput = forwardRef<HTMLInputElement, CustomTimeInputProps>(
+  ({ displayTime, setDisplayTime, ...props }, ref) => {
+    if (!displayTime) {
+      return (
+        <button
+          className="react-datepicker-time__btn"
+          type="button"
+          onClick={() => setDisplayTime(true)}
+        >
+          Add time +
+        </button>
+      );
+    }
+
+    return (
+      <>
+        <input
+          ref={ref}
+          type="time"
+          className="react-datepicker-time__input"
+          placeholder="Time"
+          name="time-input"
+          required
+          {...props}
+          onChange={(evt) => {
+            props.onChange?.(evt.target.value || "00:00");
+          }}
+        />
+        <button
+          className="react-datepicker-time__btn react-datepicker-time__btn--remove"
+          type="button"
+          onClick={() => setDisplayTime(false)}
+        >
+          Hide time
+        </button>
+      </>
+    );
+  },
+);
+
+type DatePickerInputProps = {
+  displayTime: boolean;
+  setDisplayTime: (val: boolean) => void;
+} & ReactDatePickerProps<never, boolean>;
+
+export const DatePickerInput = forwardRef<DatePicker, DatePickerInputProps>(
+  ({ displayTime, setDisplayTime, ...props }, ref) => {
+    return (
+      <div>
+        <DatePicker
+          ref={ref}
+          placeholderText="Select a date"
+          dateFormat={displayTime ? "MMM d, yyyy h:mm aa" : "MMM d, yyyy"}
+          {...props}
+          customInput={<CustomInput displayTime={displayTime} />}
+          customTimeInput={
+            <CustomTimeInput
+              displayTime={displayTime}
+              setDisplayTime={setDisplayTime}
+            />
+          }
+          showTimeInput
+        />
+      </div>
+    );
+  },
+);
