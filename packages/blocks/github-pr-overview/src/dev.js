84fdd1a92159755dc6028c93c4f55a6ac631c87a
teenoh <ugbalaachinike@gmail.com>
2022-06-09 17:26:09 +0100

    cleanups

--- packages/blocks/github-pr-overview/src/dev.js
+++ packages/blocks/github-pr-overview/src/dev.js
@@ -8,18 +8,15 @@ import ReactDOM from "react-dom";
 import { MockBlockDock } from "mock-block-dock";
 
 import Component from "./index";
-// import exampleGraph from "../example-graph.json";
+import exampleGraph from "../example-graph.json";
 
 const node = document.getElementById("app");
 
 const App = () => (
   <MockBlockDock
-    // initialEntities={exampleGraph.entities}
-    // initialEntityTypes={exampleGraph.entityTypes}
-    // initialLinks={exampleGraph.links}
-    initialEntities={[]}
-    initialEntityTypes={[]}
-    initialLinks={[]}
+    initialEntities={exampleGraph.entities}
+    initialEntityTypes={exampleGraph.entityTypes}
+    initialLinks={exampleGraph.links}
   >
     <Component entityId="test-block-1" name="World" />
   </MockBlockDock>
