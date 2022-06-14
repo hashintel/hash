1d45fdc60136637d18144730c4c1e0e54fd5ad0f
Ahmad Sattar <thehabbos007@gmail.com>
2022-06-14 14:20:55 +0200

    Adding caption to the embed

--- sites/hashdev/src/components/CalculationBlock.tsx
+++ sites/hashdev/src/components/CalculationBlock.tsx
@@ -4,27 +4,32 @@ const CalcBlock = require("calculation-block").default;
 
 // eslint-disable-next-line import/no-default-export
 export default () => (
-  <MockBlockDock
-    blockDefinition={{ ReactComponent: CalcBlock }}
-    blockEntity={{
-      entityId: "calculation-block-1",
-      entityTypeId: "calculation-block",
-      properties: {
-        cells: [
-          [["A", 1], "Employees"],
-          [["B", 1], "Count"],
-          [["C", 1], "Average"],
-          [["A", 2], "=count()"],
-          [["B", 2], "=sum(employees)"],
-          [["C", 2], "=B2/A2"],
-          [["A", 3], "=count()"],
-        ],
-        rows: [
-          [1, null],
-          [2, "Company"],
-          [3, "Person"],
-        ],
-      },
-    }}
-  />
+  <>
+    <MockBlockDock
+      blockDefinition={{ ReactComponent: CalcBlock }}
+      blockEntity={{
+        entityId: "calculation-block-1",
+        entityTypeId: "calculation-block",
+        properties: {
+          cells: [
+            [["A", 1], "Employees"],
+            [["B", 1], "Count"],
+            [["C", 1], "Average"],
+            [["A", 2], "=count()"],
+            [["B", 2], "=sum(employees)"],
+            [["C", 2], "=B2/A2"],
+            [["A", 3], "=count()"],
+          ],
+          rows: [
+            [1, null],
+            [2, "Company"],
+            [3, "Person"],
+          ],
+        },
+      }}
+    />
+    <p style={{ textAlign: "center", marginTop: 0 }}>
+      <small>The resulting calculation block. Play around with it!</small>
+    </p>
+  </>
 );
