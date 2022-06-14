a0a5cfb93755a99f688d7ebb167086b27e95dfdb
Ciaran Morinan <c@hash.ai>
2022-06-14 21:58:42 +0100

    replace AppProps with BlockEntityProperties in block-scripts

--- packages/blocks/paragraph/src/app.tsx
+++ packages/blocks/paragraph/src/app.tsx
@@ -2,11 +2,14 @@ import React, { RefCallback } from "react";
 
 import { BlockComponent } from "blockprotocol/react";
 
-type AppProps = {
+type BlockEntityProperties = {
   editableRef?: RefCallback<HTMLElement>;
   text?: string;
 };
 
-export const App: BlockComponent<AppProps> = ({ editableRef, text }) => {
+export const App: BlockComponent<BlockEntityProperties> = ({
+  editableRef,
+  text,
+}) => {
   return editableRef ? <p ref={editableRef} /> : <p>{text}</p>;
 };
