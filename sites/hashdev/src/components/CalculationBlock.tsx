97ca44d2f1a08109b48ab7e45d52148f1d89a82a
Ahmad Sattar <thehabbos007@gmail.com>
2022-06-14 13:48:00 +0200

    Update mock-block-dock

--- sites/hashdev/src/components/CalculationBlock.tsx
+++ sites/hashdev/src/components/CalculationBlock.tsx
@@ -4,30 +4,27 @@ const CalcBlock = require("calculation-block").default;
 
 // eslint-disable-next-line import/no-default-export
 export default () => (
-  <MockBlockDock>
-    <CalcBlock
-      graph={{
-        blockEntity: {
-          entityId: "calculation-block-1",
-          entityTypeId: "calculation-block",
-          properties: {
-            cells: [
-              [["A", 1], "Employees"],
-              [["B", 1], "Count"],
-              [["C", 1], "Average"],
-              [["A", 2], "=count()"],
-              [["B", 2], "=sum(employees)"],
-              [["C", 2], "=B2/A2"],
-              [["A", 3], "=count()"],
-            ],
-            rows: [
-              [1, null],
-              [2, "Company"],
-              [3, "Person"],
-            ],
-          },
-        },
-      }}
-    />
-  </MockBlockDock>
+  <MockBlockDock
+    blockDefinition={{ ReactComponent: CalcBlock }}
+    blockEntity={{
+      entityId: "calculation-block-1",
+      entityTypeId: "calculation-block",
+      properties: {
+        cells: [
+          [["A", 1], "Employees"],
+          [["B", 1], "Count"],
+          [["C", 1], "Average"],
+          [["A", 2], "=count()"],
+          [["B", 2], "=sum(employees)"],
+          [["C", 2], "=B2/A2"],
+          [["A", 3], "=count()"],
+        ],
+        rows: [
+          [1, null],
+          [2, "Company"],
+          [3, "Person"],
+        ],
+      },
+    }}
+  />
 );
