c56a9ea4a16c0926b8b2b65390a95fab9b29ce6b
Alexander Kachkaev <alexander@kachkaev.ru>
2022-06-15 14:08:36 +0100

    Bump Þ deps and rename `dev.js` to `dev.tsx` in blocks (#653)

--- packages/blocks/github-pr-overview/src/dev.tsx
+++ packages/blocks/github-pr-overview/src/dev.tsx
@@ -8,12 +8,17 @@ import ReactDOM from "react-dom";
 import { MockBlockDock } from "mock-block-dock";
 
 import Component from "./index";
+import exampleGraph from "../example-graph.json";
 
 const node = document.getElementById("app");
 
 const App = () => (
-  <MockBlockDock>
-    <Component entityId="timer" name="World" />
+  <MockBlockDock
+    initialEntities={exampleGraph.entities}
+    initialEntityTypes={exampleGraph.entityTypes}
+    initialLinks={exampleGraph.links}
+  >
+    <Component entityId="test-block-1" />
   </MockBlockDock>
 );
 
--- packages/blocks/toggle-item/src/dev.tsx
+++ packages/blocks/toggle-item/src/dev.tsx
@@ -8,17 +8,17 @@ import ReactDOM from "react-dom";
 import { MockBlockDock } from "mock-block-dock";
 
 import Component from "./index";
-import exampleGraph from "../example-graph.json";
 
 const node = document.getElementById("app");
 
 const App = () => (
-  <MockBlockDock
-    initialEntities={exampleGraph.entities}
-    initialEntityTypes={exampleGraph.entityTypes}
-    initialLinks={exampleGraph.links}
-  >
-    <Component entityId="test-block-1" name="World" />
+  <MockBlockDock>
+    <Component
+      entityId="test-block-1"
+      title="Test title"
+      content="Test content"
+      open="true"
+    />
   </MockBlockDock>
 );
 
