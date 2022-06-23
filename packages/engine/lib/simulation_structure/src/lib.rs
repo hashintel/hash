51984a88a0a61a562bb3088f221e246256341df2
Tim Diekmann <tim.diekmann@3dvision.de>
2022-06-23 09:17:05 +0200

    Add crate-docs for `simulation_structure`

--- packages/engine/lib/simulation_structure/src/lib.rs
+++ packages/engine/lib/simulation_structure/src/lib.rs
@@ -1,3 +1,11 @@
+//! Contains the structure for running the engine.
+//!
+//! The configuration of an experiment is defined in [`Manifest`]. A [`Manifest`] contains the
+//! definition to run an [`Experiment`], which is described as [`ExperimentRun`]. An [`Experiment`]
+//! has a [`Simulation`] associated with it. Depending on the [`ExperimentType`] the [`Experiment`]
+//! will run the [`Simulation`] with different configurations.
+// TODO: Add the Experiment config and Simulation Config and describe the difference here.
+
 mod dependencies;
 mod experiment;
 mod experiment_plan;
