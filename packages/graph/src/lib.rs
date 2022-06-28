c237ad0c4a420152a9d09712010ae15da06cb530
Tim Diekmann <tim.diekmann@3dvision.de>
2022-06-28 14:31:01 +0200

    Setup clippy lints

--- packages/graph/src/lib.rs
+++ packages/graph/src/lib.rs
@@ -1,3 +1,39 @@
+//! The Graph API for the HASH workspace
+
+#![feature(lint_reasons)]
+#![cfg_attr(all(doc, nightly), feature(doc_auto_cfg))]
+#![cfg_attr(not(miri), doc(test(attr(deny(warnings, clippy::all)))))]
+#![warn(
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::allow_attributes_without_reason,
+    clippy::as_underscore,
+    clippy::clone_on_ref_ptr,
+    clippy::create_dir,
+    clippy::dbg_macro,
+    clippy::default_union_representation,
+    clippy::deref_by_slicing,
+    clippy::empty_structs_with_brackets,
+    clippy::filetype_is_file,
+    clippy::get_unwrap,
+    clippy::print_stdout,
+    clippy::print_stderr,
+    clippy::rc_buffer,
+    clippy::rc_mutex,
+    clippy::same_name_method,
+    clippy::self_named_module_files,
+    clippy::str_to_string,
+    clippy::string_add,
+    clippy::string_slice,
+    clippy::string_to_string,
+    clippy::try_err,
+    clippy::undocumented_unsafe_blocks,
+    clippy::unnecessary_self_imports,
+    clippy::unwrap_used,
+    clippy::use_debug,
+    clippy::verbose_file_reads
+)]
+
 #[cfg(test)]
 mod tests {
     #[test]
