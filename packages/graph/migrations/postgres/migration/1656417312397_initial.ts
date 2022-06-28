64efb1f6a21417874d9f63545b46a8a8722e9132
Ben Werner <ben.werner01@gmail.com>
2022-06-28 18:22:10 +0100

    refactor: use pgm methods for db migration

--- packages/graph/migrations/postgres/migration/1656417312397_initial.ts
+++ packages/graph/migrations/postgres/migration/1656417312397_initial.ts
@@ -1,73 +1,196 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+import { stripNewLines } from "../util";
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.sql(`
-  -- Accounts are undecided and just here for satisfying the future schema
-  CREATE TABLE accounts (
-      account_id UUID PRIMARY KEY
+  pgm.createTable(
+    "accounts",
+    {
+      account_id: {
+        type: "UUID",
+        primaryKey: true,
+        comment:
+          "Accounts are undecided and just here for satisfying the future schema",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
   );
 
-  -- -- This table is a boundary to define the actual identification scheme for our kinds of types.
-  -- -- Assume that we use the UUIDs on the types to look up more specific ID details.
-  -- CREATE TABLE ids (
-  --    version_id UUID PRIMARY KEY,
-  --    id text NOT NULL UNIQUE
-  -- );
-
-  CREATE TABLE data_types (
-    version_id UUID PRIMARY KEY,
-    "schema" JSONB NOT NULL,
-    created_by UUID NOT NULL,
-    FOREIGN KEY (created_by) REFERENCES accounts (account_id)
-);
-
-  CREATE TABLE property_types (
-      version_id UUID PRIMARY KEY,
-      "schema" JSONB NOT NULL,
-      created_by UUID NOT NULL,
-      FOREIGN KEY (created_by) REFERENCES accounts (account_id)
+  pgm.createTable(
+    "ids",
+    {
+      version_id: {
+        type: "UUID",
+        primaryKey: true,
+      },
+      id: {
+        type: "UUID",
+        notNull: true,
+        unique: true,
+      },
+    },
+    {
+      ifNotExists: true,
+      comment: stripNewLines(`
+        This table is a boundary to define the actual identification scheme for our kinds of types.
+        Assume that we use the UUIDs on the types to look up more specific ID details.
+        `),
+    },
   );
 
-  CREATE TABLE property_type_property_type_references (
-      source_property_type_version_id UUID NOT NULL,
-      target_property_type_version_id UUID NOT NULL,
-      FOREIGN KEY (source_property_type_version_id) REFERENCES property_types (version_id),
-      FOREIGN KEY (target_property_type_version_id) REFERENCES property_types (version_id)
+  pgm.createTable(
+    "data_types",
+    {
+      version_id: {
+        type: "UUID",
+        primaryKey: true,
+      },
+      schema: {
+        type: "JSONB",
+        notNull: true,
+      },
+      created_by: {
+        type: "UUID",
+        notNull: true,
+        references: "accounts",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
   );
 
-  CREATE TABLE property_type_data_type_references (
-      source_property_type_version_id UUID NOT NULL,
-      target_data_type_version_id UUID NOT NULL,
-      FOREIGN KEY (source_property_type_version_id) REFERENCES property_types (version_id),
-      FOREIGN KEY (target_data_type_version_id) REFERENCES data_types (version_id)
+  pgm.createTable(
+    "property_types",
+    {
+      version_id: {
+        type: "UUID",
+        primaryKey: true,
+      },
+      schema: {
+        type: "JSONB",
+        notNull: true,
+      },
+      created_by: {
+        type: "UUID",
+        notNull: true,
+        references: "accounts",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
   );
 
-  CREATE TABLE entity_types (
-      version_id UUID PRIMARY KEY,
-      "schema" JSONB NOT NULL,
-      created_by UUID NOT NULL,
-      FOREIGN KEY (created_by) REFERENCES accounts (account_id)
+  pgm.createTable(
+    "property_type_property_type_references",
+    {
+      source_property_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "property_types",
+      },
+      target_property_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "property_types",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
   );
 
-  CREATE TABLE entity_type_property_types (
-      source_entity_type_version_id UUID NOT NULL,
-      target_property_type_version_id UUID NOT NULL,
-      FOREIGN KEY (source_entity_type_version_id) REFERENCES entity_types (version_id),
-      FOREIGN KEY (target_property_type_version_id) REFERENCES property_types (version_id)
+  pgm.createTable(
+    "property_type_data_type_references",
+    {
+      source_property_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "property_types",
+      },
+      target_data_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "data_types",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
   );
 
-  CREATE TABLE entities (
-      entity_id UUID PRIMARY KEY,
-      source_entity_type_version_id UUID NOT NULL,
-      properties JSONB NOT NULL,
-      created_by UUID NOT NULL,
-      FOREIGN KEY (source_entity_type_version_id) REFERENCES entity_types (version_id),
-      FOREIGN KEY (created_by) REFERENCES accounts (account_id)
+  pgm.createTable(
+    "entity_types",
+    {
+      version_id: {
+        type: "UUID",
+        primaryKey: true,
+      },
+      schema: {
+        type: "JSONB",
+        notNull: true,
+      },
+      created_by: {
+        type: "UUID",
+        notNull: true,
+        references: "accounts",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
   );
-  `);
+
+  pgm.createTable(
+    "entity_type_property_types",
+    {
+      source_entity_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "entity_types",
+      },
+      target_property_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "property_types",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
+  );
+
+  pgm.createTable(
+    "entities",
+    {
+      entity_id: {
+        type: "UUID",
+        primaryKey: true,
+      },
+      entity_type_version_id: {
+        type: "UUID",
+        notNull: true,
+        references: "property_types",
+      },
+      properties: {
+        type: "JSONB",
+        notNull: true,
+      },
+      created_by: {
+        type: "UUID",
+        notNull: true,
+        references: "accounts",
+      },
+    },
+    {
+      ifNotExists: true,
+    },
+  );
 }
 
 // A down migration would cause data loss.
