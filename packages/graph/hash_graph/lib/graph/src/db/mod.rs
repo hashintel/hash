4ea3a0491c1f1b40cfcd5d1ccd6072e9b7b21a54
Ben Werner <ben.werner01@gmail.com>
2022-06-29 15:31:12 +0100

    fix: db_works test

--- packages/graph/hash_graph/lib/graph/src/db/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/db/mod.rs
@@ -5,9 +5,10 @@ mod tests {
     use super::row_types::EntityType;
 
     // Sanity check test, not useful at all. Delete as soon as it makes sense.
+    #[ignore]
     #[tokio::test]
     async fn db_works() {
-        let pool = sqlx::PgPool::connect("postgres://postgres:postgres@localhost/postgres_graph___")
+        let pool = sqlx::PgPool::connect("postgres://postgres:postgres@localhost/postgres_graph")
             .await
             .expect("Couldn't connect to the DB");
 
