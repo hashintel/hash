6caabe631288985d414a021f465b70311db577fa
Tim Diekmann <tim.diekmann@3dvision.de>
2022-06-29 16:42:54 +0200

    Add documentation for `execution-control` crate

--- packages/engine/lib/experiment-control/src/lib.rs
+++ packages/engine/lib/experiment-control/src/lib.rs
@@ -1,4 +1,13 @@
-//! TODO: DOC
+//! Functionality for controlling experiments.
+//!
+//! This crate is responsible for the communication defined in the [`comms`] module to interact with
+//! the [`simnulation_control`] to run the engine. [`environment`] contains general functionality
+//! to drive the [`controller`] module, such [`Args`] or [`Environment`]. The [`controller`] module
+//! itself can then configure a simulation and run it.
+//!
+//! [`Args`]: environment::Args
+//! [`Environment`]: environment::Environment
+
 pub mod comms;
 pub mod controller;
 pub mod environment;
