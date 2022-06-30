2e9ccbd51c3cae9c94cf91ec31ba23b130ba9277
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-06-30 12:26:53 +0100

    Cargo fmt..

--- packages/engine/lib/experiment-control/src/lib.rs
+++ packages/engine/lib/experiment-control/src/lib.rs
@@ -2,8 +2,8 @@
 //!
 //! This crate is responsible for the communication defined in the [`comms`] module to interact with
 //! the [`simulation-control`] to run the engine. [`environment`] contains general functionality
-//! to drive the [`controller`] module, such as [`Args`] or [`Environment`]. The [`controller`] module
-//! itself can then configure a simulation and run it.
+//! to drive the [`controller`] module, such as [`Args`] or [`Environment`]. The [`controller`]
+//! module itself can then configure a simulation and run it.
 //!
 //! [`Args`]: environment::Args
 //! [`Environment`]: environment::Environment
