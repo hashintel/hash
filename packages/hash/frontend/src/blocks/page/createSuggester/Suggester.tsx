b6e350f3f4a7d2cfa2a6d71a41fe32626ec9da2a
teenoh <ugbalaachinike@gmail.com>
2022-07-04 11:27:55 +0100

    add improvements to block suggester

--- packages/hash/frontend/src/blocks/page/createSuggester/Suggester.tsx
+++ packages/hash/frontend/src/blocks/page/createSuggester/Suggester.tsx
@@ -68,9 +68,8 @@ export const Suggester = <T,>({
           borderRadius: "6px",
           boxShadow:
             "0px 20px 41px rgba(61, 78, 133, 0.07), 0px 16px 25px rgba(61, 78, 133, 0.0531481), 0px 12px 12px rgba(61, 78, 133, 0.0325), 0px 2px 3.13px rgba(61, 78, 133, 0.02)",
-          borderWidth: `1px`,
-          borderStyle: "solid",
-          borderColor: palette.gray[20],
+          border: `1px solid ${palette.gray[20]}`,
+          ...(options.length === 0 && { borderWidth: 0 }),
           display: "grid",
           gridTemplateRows: "1fr auto",
           overflow: "hidden",
