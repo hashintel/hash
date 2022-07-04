ccf3398b8fd15369ae845174ba0fce184d775a61
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2022-07-04 10:53:32 +0100

    Update packages/graph/hash_graph/lib/graph/src/datastore/postgres/mod.rs
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>

--- packages/graph/hash_graph/lib/graph/src/datastore/postgres/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/datastore/postgres/mod.rs
@@ -307,8 +307,8 @@ mod tests {
         Ok(())
     }
 
-    #[ignore]
     #[tokio::test]
+    #[cfg_attr(miri, ignore = "miri can't run in async context")]
     async fn update_existing_data_type() -> Result<(), DatastoreError> {
         let db = PostgresDatabase::new(&DB_INFO).await?;
 
