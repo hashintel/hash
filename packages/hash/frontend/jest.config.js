d0dcba1d57f448e727134cacbaf450afa3de65e7
Alexander Kachkaev <alexander@kachkaev.ru>
2022-07-06 14:16:26 +0100

    Bump deps & fix jest.config.js

--- packages/hash/frontend/jest.config.js
+++ packages/hash/frontend/jest.config.js
@@ -5,7 +5,7 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: __dirname });
 
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleDirectories: ["../../../node_modules", "node_modules", "<rootDir>/"],
   moduleNameMapper: {
     "@hashintel/hash-shared(.*)": "<rootDir>/../shared/src$1",
   },
@@ -13,4 +13,21 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
 };
 
-module.exports = createJestConfig(customJestConfig);
+// https://github.com/vercel/next.js/discussions/34589#discussioncomment-2510438
+module.exports = async () => {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  nextJestConfig.transformIgnorePatterns = [
+    "/node_modules/(?!uuid)",
+    ...nextJestConfig.transformIgnorePatterns.filter(
+      (pattern) => pattern !== "/node_modules/",
+    ),
+  ];
+  // https://github.com/vercel/next.js/issues/36230
+  nextJestConfig.moduleNameMapper = {
+    // Workaround to put our SVG stub first
+    "\\.svg": "<rootDir>/src/__mocks__/file.mock.ts",
+    ...nextJestConfig.moduleNameMapper,
+  };
+
+  return nextJestConfig;
+};
