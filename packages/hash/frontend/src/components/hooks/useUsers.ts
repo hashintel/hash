1fd2ba1985109b80c70302be950ffad78bff3bb2
teenoh <ugbalaachinike@gmail.com>
2022-07-06 13:01:06 +0100

    only display org members in mention suggester

--- packages/hash/frontend/src/components/hooks/useUsers.ts
+++ packages/hash/frontend/src/components/hooks/useUsers.ts
@@ -1,25 +1,48 @@
 import { useMemo } from "react";
 import { useQuery } from "@apollo/client";
+import { useUser } from "./useUser";
 import { GetAccountsQuery } from "../../graphql/apiTypes.gen";
 import { getAccounts } from "../../graphql/queries/account.queries";
+import { useRouteAccountInfo } from "../../shared/routing";
 
 export const useUsers = (): {
   loading: boolean;
   data: { entityId: string; shortname: string; name: string }[];
 } => {
   const { data, loading } = useQuery<GetAccountsQuery>(getAccounts);
+  const { accountId: workspaceAccountId } = useRouteAccountInfo();
+  const { user } = useUser();
 
   const accounts = useMemo(() => {
-    if (!data) return [];
+    // If user is in their workspace return user
+    if (user?.accountId === workspaceAccountId) {
+      return [
+        {
+          entityId: user.entityId,
+          shortname: user.properties.shortname!,
+          name:
+            "preferredName" in user.properties
+              ? user.properties.preferredName!
+              : user.properties.shortname!,
+        },
+      ];
+    }
+
     /**
      * Filter out org accounts
-     * org accounts do not have "preferredName" in their properties object
+     * user accounts do not have "membership" key in their object
      */
-    const userAccounts = data.accounts.filter(
-      (account) => "preferredName" in account.properties,
-    );
+    // if user is in their org's workspace, return members
+    const orgMembers =
+      data?.accounts.filter(
+        (account) =>
+          "memberOf" in account &&
+          account.memberOf?.some(
+            ({ org }) => org.accountId === workspaceAccountId,
+          ),
+      ) ?? [];
 
-    return userAccounts.map((account) => {
+    return orgMembers.map((account) => {
       return {
         entityId: account.entityId,
         shortname: account.properties.shortname!,
@@ -29,7 +52,7 @@ export const useUsers = (): {
             : account.properties.shortname!,
       };
     });
-  }, [data]);
+  }, [data, user, workspaceAccountId]);
 
   return {
     loading,
