68acf60031457587152e6f5ccabba5d9e36cdfb8
Alexander Kachkaev <alexander@kachkaev.ru>
2022-07-07 11:41:59 +0100

    Revert jest.config.js

--- packages/engine/stdlib/jest.config.js
+++ packages/engine/stdlib/jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      // Disable type checking when running tests
-      // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/isolatedModules
-      isolatedModules: true,
-    },
-  },
   roots: ["src/ts"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
