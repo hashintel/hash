9418d504996df7713b5577d3553be84d6d048f09
Alexander Kachkaev <alexander@kachkaev.ru>
2022-07-07 15:53:06 +0100

    Update resolver types

--- packages/hash/api/src/graphql/resolvers/file/fileUrlResolver.ts
+++ packages/hash/api/src/graphql/resolvers/file/fileUrlResolver.ts
@@ -1,11 +1,12 @@
-import { FileProperties, Resolver } from "../../apiTypes.gen";
+import { FileProperties, ResolverFn } from "../../apiTypes.gen";
 import { GraphQLContext } from "../../context";
 import { File } from "../../../model";
 
-export const fileUrlResolver: Resolver<
+export const fileUrlResolver: ResolverFn<
   string,
   FileProperties,
-  GraphQLContext
+  GraphQLContext,
+  []
 > = async (properties, _, _ctx, _info) => {
   const usedStorage = properties.storageType;
   const downloadURL = await File.getFileDownloadURL(usedStorage, {
