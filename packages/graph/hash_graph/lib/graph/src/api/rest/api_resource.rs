519a3492abb740d3bc0817c563c386eb5215dae6
Ahmad Sattar <thehabbos007@gmail.com>
2022-07-08 15:42:15 +0200

    Update packages/graph/hash_graph/lib/graph/src/api/rest/api_resource.rs
    Co-authored-by: Alfred Mountfield <am@hash.ai>

--- packages/graph/hash_graph/lib/graph/src/api/rest/api_resource.rs
+++ packages/graph/hash_graph/lib/graph/src/api/rest/api_resource.rs
@@ -5,7 +5,7 @@ use crate::datastore::Datastore;
 /// With REST, we define resources that can be acted on. These resources are defined through
 /// routes and HTTP methods.
 ///
-/// This trait encapsulates a way to define related resource opeerations
+/// This trait encapsulates a way to define related resource operations
 /// through a `Router`, making it explicitly clear we want to provide `OpenApi` specification as
 /// documentation for the routes.
 pub(super) trait RoutedResource: utoipa::OpenApi {
