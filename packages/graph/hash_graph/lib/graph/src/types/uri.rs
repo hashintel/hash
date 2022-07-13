1cd9c3abbcb3d36bdd7a580eaf5ec144496e1e5f
Tim Diekmann <tim.diekmann@3dvision.de>
2022-07-13 10:20:23 +0200

    Change URI parsing to only look for the last `"/v/"`

--- packages/graph/hash_graph/lib/graph/src/types/uri.rs
+++ packages/graph/hash_graph/lib/graph/src/types/uri.rs
@@ -1,6 +1,6 @@
 use std::{fmt, num::ParseIntError, result::Result as StdResult, str::FromStr};
 
-use error_stack::{ensure, Context, Report};
+use error_stack::{ensure, Context, IntoReport, Report, Result, ResultExt};
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 
 pub type BaseId = String;
@@ -8,14 +8,14 @@ pub type BaseIdRef = str;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Uri {
-    base_id: String,
+    base_id: BaseId,
     version: u32,
 }
 
 impl Uri {
     /// Creates a new `Uri` from the given `base_id` and `version`.
     #[must_use]
-    pub const fn new(base_id: String, version: u32) -> Self {
+    pub const fn new(base_id: BaseId, version: u32) -> Self {
         Self { base_id, version }
     }
 
@@ -37,25 +37,11 @@ impl fmt::Display for Uri {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-#[non_exhaustive]
-pub enum UriErrorKind {
-    NoVersion,
-    MultipleVersions,
-    InvalidVersion(ParseIntError),
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ParseUriError {
-    kind: UriErrorKind,
-}
+pub struct ParseUriError;
 
 impl fmt::Display for ParseUriError {
     fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.kind {
-            UriErrorKind::NoVersion => fmt.write_str("No version encoded in URI"),
-            UriErrorKind::MultipleVersions => fmt.write_str("Multiple versions encoded in URI"),
-            UriErrorKind::InvalidVersion(error) => fmt::Display::fmt(&error, fmt),
-        }
+        fmt.write_str("provided string does not contain a valid version")
     }
 }
 
@@ -64,23 +50,12 @@ impl Context for ParseUriError {}
 impl FromStr for Uri {
     type Err = Report<ParseUriError>;
 
-    fn from_str(uri: &str) -> std::result::Result<Self, Self::Err> {
-        let mut split = uri.split("/v/");
-        let base_id = split.next().ok_or(ParseUriError {
-            kind: UriErrorKind::NoVersion,
-        })?;
-        let version = split.next().ok_or(ParseUriError {
-            kind: UriErrorKind::NoVersion,
-        })?;
-        ensure!(split.next().is_none(), ParseUriError {
-            kind: UriErrorKind::MultipleVersions
-        });
+    fn from_str(uri: &str) -> Result<Self, ParseUriError> {
+        let (base_id, version) = uri.rsplit_once("/v/").ok_or(ParseUriError)?;
 
         Ok(Self::new(
             base_id.to_owned(),
-            version.parse().map_err(|error| ParseUriError {
-                kind: UriErrorKind::InvalidVersion(error),
-            })?,
+            version.parse().report().change_context(ParseUriError)?,
         ))
     }
 }
