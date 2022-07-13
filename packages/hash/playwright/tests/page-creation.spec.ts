75b98723b1ea067cc0407edfdc1adf5f3fdba942
teenoh <ugbalaachinike@gmail.com>
2022-07-13 14:12:51 +0100

    fix failing playwright tests

--- packages/hash/playwright/tests/page-creation.spec.ts
+++ packages/hash/playwright/tests/page-creation.spec.ts
@@ -109,7 +109,7 @@ test("user can create page", async ({ page }) => {
   const blockContextMenu = page.locator('[data-testid="block-context-menu"]');
 
   await blockContextMenu
-    .locator('[placeholder="Load Block from URL..."]')
+    .locator('[placeholder="Load block from URL..."]')
     .fill("https://blockprotocol.org/blocks/@hash/code");
 
   /**
@@ -122,7 +122,7 @@ test("user can create page", async ({ page }) => {
   await blockContextMenu.locator("text=Re-load block").click();
 
   await expect(
-    blockContextMenu.locator('[placeholder="Load Block from URL..."]'),
+    blockContextMenu.locator('[placeholder="Load block from URL..."]'),
   ).toHaveCount(0, { timeout: 1000 });
 
   await expect(
