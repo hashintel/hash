41ad082cfb3dc916c53411e8f3decf66a6cc4b4b
Ciaran Morinan <c@hash.ai>
2022-07-15 09:59:58 +0100

    update import

--- packages/hash/frontend/src/components/RemoteBlock/RemoteBlock.tsx
+++ packages/hash/frontend/src/components/RemoteBlock/RemoteBlock.tsx
@@ -2,8 +2,8 @@ import { BlockMetadata, UnknownRecord } from "@blockprotocol/core";
 import {
   BlockGraphProperties,
   EmbedderGraphMessageCallbacks,
-  useGraphEmbedderService,
 } from "@blockprotocol/graph";
+import { useGraphEmbedderService } from "@blockprotocol/graph/react";
 
 import React from "react";
 import { BlockRenderer } from "./blockRenderer";
