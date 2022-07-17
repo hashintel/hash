49b35108e402f8a9754e065f08d9712efb180db9
luisbettencourt <lmn.bettencourt@gmail.com>
2022-07-17 16:05:40 +0100

    progress

--- packages/blocks/shuffle/src/app.tsx
+++ packages/blocks/shuffle/src/app.tsx
@@ -1,8 +1,17 @@
 import { BlockComponent, useGraphBlockService } from "@blockprotocol/graph";
 import * as React from "react";
 import { useCallback, useRef } from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+// import ListItemButton from '@mui/material/ListItemButton'
+// import ListItemIcon from '@mui/material/ListItemIcon'
+// import ListItemText from '@mui/material/ListItemText'
+// import Divider from '@mui/material/Divider';
+// import InboxIcon from '@mui/icons-material/Inbox';
+// import DraftsIcon from '@mui/icons-material/Drafts';
 
-import styles from "./base.module.scss";
+// import styles from "./base.module.scss";
 
 type BlockEntityProperties = {
   name: string;
@@ -13,32 +22,28 @@ export const App: BlockComponent<BlockEntityProperties> = ({
     blockEntity: { entityId, properties },
   },
 }) => {
-  const blockRootRef = useRef<HTMLDivElement>(null);
-  const { graphService } = useGraphBlockService(blockRootRef);
+  // const blockRootRef = useRef<HTMLDivElement>(null);
+  // const { graphService } = useGraphBlockService(blockRootRef);
 
-  const updateSelf = useCallback(
-    (newProperties: Partial<BlockEntityProperties>) =>
-      graphService?.updateEntity({
-        data: { properties: newProperties, entityId },
-      }),
-    [entityId, graphService],
-  );
+  // const updateSelf = useCallback(
+  //   (newProperties: Partial<BlockEntityProperties>) =>
+  //     graphService?.updateEntity({
+  //       data: { properties: newProperties, entityId },
+  //     }),
+  //   [entityId, graphService],
+  // );
 
-  const { name } = properties;
+  // const { name } = properties;
 
   return (
-    <div className={styles.block} ref={blockRootRef}>
-      <h1>Hello, {name}!</h1>
-      <p>
-        The entityId of this block is {entityId}. Use it to update its data,
-        e.g. by calling <code>updateEntity</code>.
-      </p>
-      <button
-        onClick={() => updateSelf({ name: Math.random().toString() })}
-        type="button"
-      >
-        Update Name
-      </button>
-    </div>
+    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      test
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>Item 1</ListItem>
+          <ListItem disablePadding>Item 2</ListItem>
+        </List>
+      </nav>
+    </Box>
   );
 };
