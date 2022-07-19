344b00e7ab7f36c36ed50da3a64b5da117b4dc5d
luisbettencourt <37453800+luisbettencourt@users.noreply.github.com>
2022-07-19 14:39:22 +0100

    Update packages/hash/frontend/src/blocks/page/BlockContext.tsx
    Co-authored-by: Nate Higgins <nate@nateh.uk>

--- packages/hash/frontend/src/blocks/page/BlockContext.tsx
+++ packages/hash/frontend/src/blocks/page/BlockContext.tsx
@@ -6,17 +6,12 @@ type BlockContextType = {
   setError: (error: boolean) => void;
 };
 
-/** used to detect whether or not a context value was provided */
-const nullBlockView = {};
-
-export const BlockContext = React.createContext<BlockContextType>(
-  nullBlockView as BlockContextType,
-);
+export const BlockContext = React.createContext<BlockContextType | null>(null);
 
 export const useBlockContext = () => {
   const blockContext = useContext(BlockContext);
 
-  if (blockContext === nullBlockView) {
+  if (!blockContext) {
     throw new Error("no BlockViewContext value has been provided");
   }
 
