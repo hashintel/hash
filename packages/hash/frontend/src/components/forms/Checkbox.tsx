757c84c67419cc1582176083242ad6c07ed61972
Alexander Kachkaev <alexander@kachkaev.ru>
2022-07-25 12:54:34 +0100

    Enable reportUnusedDisableDirectives in .eslintrc.json

--- packages/hash/frontend/src/components/forms/Checkbox.tsx
+++ packages/hash/frontend/src/components/forms/Checkbox.tsx
@@ -15,7 +15,6 @@ export const Checkbox: FunctionComponent<CheckboxProps> = ({
   if (label) {
     return (
       <div>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label>
           {label}
           <input
