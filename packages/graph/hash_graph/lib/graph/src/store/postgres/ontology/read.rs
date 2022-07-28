57ac8d5eb63dae846f0e4ee7b037a5eaac88704c
Tim Diekmann <tim.diekmann@3dvision.de>
2022-07-28 20:11:14 +0200

    Implement a get-all for onthology types

--- packages/graph/hash_graph/lib/graph/src/store/postgres/ontology/read.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/ontology/read.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
 use error_stack::{IntoReport, Result, ResultExt};
+use futures::{StreamExt, TryStreamExt};
+use postgres_types::ToSql;
 use serde::Deserialize;
 use tokio_postgres::GenericClient;
 
@@ -44,5 +46,43 @@ where
         serde_json::from_value(row.get(0))
             .report()
             .change_context(QueryError)
+    }
+}
+
+#[async_trait]
+impl<C: AsClient, T> crud::Read<'_, (), T> for PostgresStore<C>
+where
+    for<'de> T: OntologyDatabaseType + Deserialize<'de> + Send,
+{
+    type Output = Vec<T>;
+
+    async fn get(&self, _: ()) -> Result<Self::Output, QueryError> {
+        // SAFETY: We insert a table name here, but `T::table()` is only accessible from within this
+        //   module.
+        let row_stream = self
+            .as_client()
+            .query_raw(
+                &format!(
+                    r#"
+                    SELECT schema
+                    FROM {};
+                    "#,
+                    T::table()
+                ),
+                [] as [&(dyn ToSql + Sync); 0],
+            )
+            .await
+            .report()
+            .change_context(QueryError)?;
+
+        row_stream
+            .map(|row_result| {
+                let row = row_result.report().change_context(QueryError)?;
+                serde_json::from_value(row.get(0))
+                    .report()
+                    .change_context(QueryError)
+            })
+            .try_collect()
+            .await
     }
 }
