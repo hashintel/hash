8ac6485ed35a53b030e15c892dcb63bd539bf4f8
Tim Diekmann <tim.diekmann@3dvision.de>
2022-08-02 16:29:41 +0200

    Add comment on `Read` trait

--- packages/graph/hash_graph/lib/graph/src/store/crud.rs
+++ packages/graph/hash_graph/lib/graph/src/store/crud.rs
@@ -14,6 +14,15 @@ use crate::store::{QueryError, Store};
 pub struct AllLatest;
 
 /// Read access to a [`Store`].
+// TODO: Change the way we are interacting with this trait (or change the trait).
+//   With our current design we need to specify a marker, what exactly we want to extract. For some
+//   things this is intuitive like a `&VersionedUri`, which will return exactly one type, but for
+//   other types, like `&BaseUri` this does not convey the meaning well enough. This could return
+//   only the latest version of a type or all types with this URI. This would imply, that we need
+//   a marker struct for each meaning, i.e. `Latest<'i, BaseUri>(&'i BaseUri)`. In addition to that,
+//   we want to extend our interface to structural querying, so something like an `AST` needs to be
+//   aware of the types.
+//   As an example why this is bad see the `AllLatest` marker struct.
 #[async_trait]
 pub trait Read<'i, I: Send + 'i, T>: Store {
     /// Output returned when getting the value by the identifier `I`.
