d912e28f839983d35f3214288351f0e1ae643fa3
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2022-08-03 17:29:05 +0100

    Update packages/hash/frontend/src/pages/shared/ory-kratos.ts
    Co-authored-by: Alfred Mountfield <am@hash.ai>

--- packages/hash/frontend/src/pages/shared/ory-kratos.ts
+++ packages/hash/frontend/src/pages/shared/ory-kratos.ts
@@ -28,7 +28,7 @@ export const oryKratosClient = new V0alpha2Api(
      * CRSF token from being set as an HTTP-Cookie, because the browser cannot send or
      * receive cookies via the browser `fetch` method.
      *
-     * Therefore requests to the ory kratos public endpoing are made on the server in a
+     * Therefore requests to the ory kratos public endpoint are made on the server in a
      * Next.js API handler.
      */
     basePath: "/api/ory",
