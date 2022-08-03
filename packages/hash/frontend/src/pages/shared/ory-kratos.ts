5c9076fdb6dd5188f5275feb106cfccc69aaea87
Ben Werner <ben.werner01@gmail.com>
2022-08-03 10:55:08 +0100

    ft: kratos signup form

--- packages/hash/frontend/src/pages/shared/ory-kratos.ts
+++ packages/hash/frontend/src/pages/shared/ory-kratos.ts
@@ -1,6 +1,9 @@
-import { Configuration, V0alpha2Api } from "@ory/client";
+import { Configuration, V0alpha2Api, SelfServiceError } from "@ory/client";
+import { AxiosError } from "axios";
+import { NextRouter } from "next/router";
+import { Dispatch, SetStateAction } from "react";
 
-export const oryCratosClient = new V0alpha2Api(
+export const oryKratosClient = new V0alpha2Api(
   new Configuration({
     /**
      * Directly connecting to kratos (using "http://127.0.0.1:4433") would prevent the
@@ -13,3 +16,87 @@ export const oryCratosClient = new V0alpha2Api(
     basePath: "/api/ory",
   }),
 );
+
+/**
+ * A helper type representing the traits defined by the kratos identity schema at `packages/hash/external-services/kratos/identity.schema.json`
+ */
+export type IdentityTraits = {
+  emails: string[];
+};
+
+/**
+ * A helper function that creates an error handling function for some common errors
+ * that may occur when fetching a flow.
+ */
+export const createFlowErrorHandler =
+  <S>(params: {
+    router: NextRouter;
+    flowType:
+      | "login"
+      | "registration"
+      | "settings"
+      | "recovery"
+      | "verification";
+    setFlow: Dispatch<SetStateAction<S | undefined>>;
+    setErrorMessage: Dispatch<SetStateAction<string | undefined>>;
+  }) =>
+  async (err: AxiosError<SelfServiceError>) => {
+    const { setErrorMessage, setFlow, router, flowType } = params;
+
+    switch (err.response?.data.error?.id) {
+      case "session_aal2_required":
+        // 2FA is enabled and enforced, but user did not perform 2fa yet!
+        window.location.href = err.response?.data.redirect_browser_to;
+        return;
+      case "session_already_available":
+        // User is already signed in, let's redirect them home!
+        await router.push("/");
+        return;
+      case "session_refresh_required":
+        // We need to re-authenticate to perform this action
+        window.location.href = err.response?.data.redirect_browser_to;
+        return;
+      case "self_service_flow_return_to_forbidden":
+        // The flow expired, let's request a new one.
+        setErrorMessage("The return_to address is not allowed.");
+        setFlow(undefined);
+        await router.push(`/${flowType}`);
+        return;
+      case "self_service_flow_expired":
+        // The flow expired, let's request a new one.
+        setErrorMessage(
+          "Your interaction expired, please fill out the form again.",
+        );
+        setFlow(undefined);
+        await router.push(`/${flowType}`);
+        return;
+      case "security_csrf_violation":
+        // A CSRF violation occurred. Best to just refresh the flow!
+        setErrorMessage(
+          "A security violation was detected, please fill out the form again.",
+        );
+        setFlow(undefined);
+        await router.push(`/${flowType}`);
+        return;
+      case "security_identity_mismatch":
+        // The requested item was intended for someone else. Let's request a new flow...
+        setFlow(undefined);
+        await router.push(`/${flowType}`);
+        return;
+      case "browser_location_change_required":
+        // Ory Kratos asked us to point the user to this URL.
+        window.location.href = err.response.data.redirect_browser_to;
+        return;
+    }
+
+    switch (err.response?.status) {
+      case 410:
+        // The flow expired, let's request a new one.
+        setFlow(undefined);
+        await router.push(`/${flowType}`);
+        return;
+    }
+
+    // We are not able to handle the error? Return it.
+    return Promise.reject(err);
+  };
