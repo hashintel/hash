59c574ddf27857e631dff7976201c0bc1bbff1f0
Ben Werner <ben.werner01@gmail.com>
2022-08-03 14:52:16 +0100

    chore: todo

--- packages/hash/api/src/auth/ory-kratos.ts
+++ packages/hash/api/src/auth/ory-kratos.ts
@@ -2,6 +2,7 @@ import { Configuration } from "@ory/client";
 import { V0alpha2Api as OpenSourceV0alpha2Api } from "@ory/kratos-client";
 import { getRequiredEnv } from "../util";
 
+/** @todo: connect to admin endpoint when admin methods are required */
 const publicUrl = getRequiredEnv("ORY_KATROS_PUBLIC_URL");
 
 export const kratosSdk = new OpenSourceV0alpha2Api(
