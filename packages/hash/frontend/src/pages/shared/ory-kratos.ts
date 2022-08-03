3b19b790b87bef21cda5a6980ab4ee7f67aa195d
Ben Werner <ben.werner01@gmail.com>
2022-08-03 17:08:45 +0100

    chore: fix frontend lint errors

--- packages/hash/frontend/src/pages/shared/ory-kratos.ts
+++ packages/hash/frontend/src/pages/shared/ory-kratos.ts
@@ -4,6 +4,9 @@ import {
   SelfServiceError,
   SelfServiceLoginFlow,
   SelfServiceRegistrationFlow,
+  SelfServiceBrowserLocationChangeRequiredError,
+  ErrorAuthenticatorAssuranceLevelNotSatisfied,
+  NeedsPrivilegedSessionError,
 } from "@ory/client";
 import { isUiNodeInputAttributes } from "@ory/integrations/ui";
 import { AxiosError } from "axios";
@@ -58,50 +61,72 @@ export const createFlowErrorHandler =
   async (err: AxiosError<SelfServiceError>) => {
     const { setErrorMessage, setFlow, router, flowType } = params;
 
-    switch (err.response?.data.error?.id) {
-      case "session_aal2_required":
-        // 2FA is enabled and enforced, but user did not perform 2fa yet!
-        window.location.href = err.response?.data.redirect_browser_to;
-        return;
-      case "session_already_available":
-        // User is already signed in, let's redirect them home!
-        await router.push("/");
-        return;
-      case "session_refresh_required":
-        // We need to re-authenticate to perform this action
-        window.location.href = err.response?.data.redirect_browser_to;
-        return;
-      case "self_service_flow_return_to_forbidden":
-        // The flow expired, let's request a new one.
-        setErrorMessage("The return_to address is not allowed.");
-        setFlow(undefined);
-        await router.push(`/${flowType}`);
-        return;
-      case "self_service_flow_expired":
-        // The flow expired, let's request a new one.
-        setErrorMessage(
-          "Your interaction expired, please fill out the form again.",
-        );
-        setFlow(undefined);
-        await router.push(`/${flowType}`);
-        return;
-      case "security_csrf_violation":
-        // A CSRF violation occurred. Best to just refresh the flow!
-        setErrorMessage(
-          "A security violation was detected, please fill out the form again.",
-        );
-        setFlow(undefined);
-        await router.push(`/${flowType}`);
-        return;
-      case "security_identity_mismatch":
-        // The requested item was intended for someone else. Let's request a new flow...
-        setFlow(undefined);
-        await router.push(`/${flowType}`);
-        return;
-      case "browser_location_change_required":
-        // Ory Kratos asked us to point the user to this URL.
-        window.location.href = err.response.data.redirect_browser_to;
-        return;
+    const kratosError = err.response?.data;
+
+    if (kratosError) {
+      switch ((kratosError.error as any | undefined)?.id) {
+        case "session_aal2_required": {
+          // 2FA is enabled and enforced, but user did not perform 2fa yet!
+          const { redirect_browser_to } =
+            kratosError as ErrorAuthenticatorAssuranceLevelNotSatisfied;
+
+          if (redirect_browser_to) {
+            await router.replace(redirect_browser_to);
+          }
+          return;
+        }
+        case "session_already_available":
+          // User is already signed in, let's redirect them home!
+          await router.push("/");
+          return;
+        case "session_refresh_required": {
+          // We need to re-authenticate to perform this action
+          const { redirect_browser_to } =
+            kratosError as NeedsPrivilegedSessionError;
+
+          if (redirect_browser_to) {
+            await router.replace(redirect_browser_to);
+          }
+          return;
+        }
+        case "self_service_flow_return_to_forbidden":
+          // The flow expired, let's request a new one.
+          setErrorMessage("The return_to address is not allowed.");
+          setFlow(undefined);
+          await router.push(`/${flowType}`);
+          return;
+        case "self_service_flow_expired":
+          // The flow expired, let's request a new one.
+          setErrorMessage(
+            "Your interaction expired, please fill out the form again.",
+          );
+          setFlow(undefined);
+          await router.push(`/${flowType}`);
+          return;
+        case "security_csrf_violation":
+          // A CSRF violation occurred. Best to just refresh the flow!
+          setErrorMessage(
+            "A security violation was detected, please fill out the form again.",
+          );
+          setFlow(undefined);
+          await router.push(`/${flowType}`);
+          return;
+        case "security_identity_mismatch":
+          // The requested item was intended for someone else. Let's request a new flow...
+          setFlow(undefined);
+          await router.push(`/${flowType}`);
+          return;
+        case "browser_location_change_required": {
+          // Ory Kratos asked us to point the user to this URL.
+          const { redirect_browser_to } =
+            kratosError as SelfServiceBrowserLocationChangeRequiredError;
+
+          if (redirect_browser_to) {
+            await router.replace(redirect_browser_to);
+          }
+          return;
+        }
+      }
     }
 
     switch (err.response?.status) {
