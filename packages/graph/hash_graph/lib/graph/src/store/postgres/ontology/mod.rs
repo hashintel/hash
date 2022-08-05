0ccbe7f30a93df3571b55105f239507b8a51d9fe
Tim Diekmann <tim.diekmann@3dvision.de>
2022-08-05 10:16:53 +0200

    Merge `database_type` module into the `ontology` module

--- packages/graph/hash_graph/lib/graph/src/store/postgres/ontology/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/ontology/mod.rs
@@ -1,8 +1,39 @@
 mod data_type;
-mod database_type;
 mod entity_type;
 mod link_type;
 mod property_type;
 mod read;
 
-pub use self::database_type::OntologyDatabaseType;
+use crate::ontology::types::{DataType, EntityType, LinkType, OntologyType, PropertyType};
+
+/// Provides an abstraction over elements of the Type System stored in the Database.
+///
+/// [`PostgresDatabase`]: crate::store::PostgresDatabase
+pub trait OntologyDatabaseType: OntologyType {
+    /// Returns the name of the table where this type is stored.
+    fn table() -> &'static str;
+}
+
+impl OntologyDatabaseType for DataType {
+    fn table() -> &'static str {
+        "data_types"
+    }
+}
+
+impl OntologyDatabaseType for PropertyType {
+    fn table() -> &'static str {
+        "property_types"
+    }
+}
+
+impl OntologyDatabaseType for EntityType {
+    fn table() -> &'static str {
+        "entity_types"
+    }
+}
+
+impl OntologyDatabaseType for LinkType {
+    fn table() -> &'static str {
+        "link_types"
+    }
+}
