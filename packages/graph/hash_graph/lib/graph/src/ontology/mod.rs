e5c1f954855c19710cc697aadf3b701cc0e53555
Tim Diekmann <tim.diekmann@3dvision.de>
2022-08-09 18:24:54 +0200

    Add `PersistedOntologyType`

--- packages/graph/hash_graph/lib/graph/src/ontology/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/ontology/mod.rs
@@ -28,3 +28,48 @@ impl fmt::Display for AccountId {
         write!(fmt, "{}", &self.0)
     }
 }
+
+/// The metadata required to uniquely identify an ontology element that has been persisted in the
+/// datastore.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Component)]
+#[serde(rename_all = "camelCase")]
+pub struct PersistedOntologyIdentifier {
+    created_by: AccountId,
+}
+
+impl PersistedOntologyIdentifier {
+    #[must_use]
+    pub const fn new(created_by: AccountId) -> Self {
+        Self { created_by }
+    }
+
+    #[must_use]
+    pub const fn created_by(&self) -> AccountId {
+        self.created_by
+    }
+}
+
+pub struct PersistedOntologyType<T> {
+    inner: T,
+    identifier: PersistedOntologyIdentifier,
+}
+
+impl<T> PersistedOntologyType<T> {
+    #[must_use]
+    pub const fn new(inner: T, created_by: AccountId) -> Self {
+        Self {
+            inner,
+            identifier: PersistedOntologyIdentifier::new(created_by),
+        }
+    }
+
+    #[must_use]
+    pub const fn inner(&self) -> &T {
+        &self.inner
+    }
+
+    #[must_use]
+    pub const fn identifier(&self) -> &PersistedOntologyIdentifier {
+        &self.identifier
+    }
+}
