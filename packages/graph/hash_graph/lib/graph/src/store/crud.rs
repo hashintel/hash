1a028878f3ead00d7c002dd68578b7ca37dda636
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-08-10 14:01:19 +0200

    Fix docs on `Read::read`

--- packages/graph/hash_graph/lib/graph/src/store/crud.rs
+++ packages/graph/hash_graph/lib/graph/src/store/crud.rs
@@ -21,7 +21,7 @@ pub trait Read<T: Send>: Sync {
     // TODO: Implement `Valuable` for queries and use them directly
     type Query<'q>: Debug + Sync;
 
-    /// Returns a value from the [`Store`] specified by `identifier`.
+    /// Returns a value from the [`Store`] specified by the passed `query`.
     ///
     /// [`Store`]: crate::store::Store
     async fn read<'query>(&self, query: &Self::Query<'query>) -> Result<Vec<T>, QueryError>;
