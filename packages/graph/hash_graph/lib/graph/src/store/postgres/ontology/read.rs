edcc6ee3627139900535dac7ca39628beb923bf8
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-08-10 15:53:42 +0200

    Fix rebase-issues

--- packages/graph/hash_graph/lib/graph/src/store/postgres/ontology/read.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/ontology/read.rs
@@ -2,11 +2,11 @@ use async_trait::async_trait;
 use error_stack::{IntoReport, Result, ResultExt};
 use futures::{StreamExt, TryStreamExt};
 use serde::Deserialize;
-use tokio_postgres::GenericClient;
+use tokio_postgres::{GenericClient, RowStream};
 
 use crate::{
     ontology::{
-        types::{DataType, EntityType, LinkType, PropertyType},
+        types::{uri::BaseUri, DataType, EntityType, LinkType, PropertyType},
         AccountId, PersistedDataType, PersistedEntityType, PersistedLinkType,
         PersistedOntologyIdentifier, PersistedPropertyType,
     },
@@ -56,6 +56,53 @@ impl PersistedOntologyType for PersistedEntityType {
     }
 }
 
+async fn by_uri_by_version<T: OntologyDatabaseType>(
+    client: &(impl GenericClient + Sync),
+    uri: &BaseUri,
+    version: u32,
+) -> Result<RowStream, QueryError> {
+    client
+        .query_raw(
+            &format!(
+                r#"
+                SELECT schema, created_by
+                FROM {}
+                WHERE version_id = (
+                    SELECT version_id
+                    FROM ids
+                    WHERE base_uri = $1 AND version = $2
+                )
+                "#,
+                T::table()
+            ),
+            parameter_list([uri, &i64::from(version)]),
+        )
+        .await
+        .into_report()
+        .change_context(QueryError)
+}
+
+async fn by_latest_version<T: OntologyDatabaseType>(
+    client: &(impl GenericClient + Sync),
+) -> Result<RowStream, QueryError> {
+    client
+        .query_raw(
+            &format!(
+                r#"
+                SELECT DISTINCT ON(base_uri) schema, created_by
+                FROM {table}
+                INNER JOIN ids ON ids.version_id = {table}.version_id
+                ORDER BY base_uri, version DESC;
+                "#,
+                table = T::table()
+            ),
+            parameter_list([]),
+        )
+        .await
+        .into_report()
+        .change_context(QueryError)
+}
+
 #[async_trait]
 impl<C: AsClient, T> Read<T> for PostgresStore<C>
 where
@@ -67,55 +114,24 @@ where
     async fn read<'query>(&self, query: &Self::Query<'query>) -> Result<Vec<T>, QueryError> {
         let row_stream = match (query.uri(), query.version()) {
             (Some(uri), Some(OntologyVersion::Exact(version))) => {
-                self.as_client()
-                    .query_raw(
-                        &format!(
-                            r#"
-                            SELECT schema, created_by
-                            FROM {}
-                            WHERE version_id = (
-                                SELECT version_id
-                                FROM ids
-                                WHERE base_uri = $1 AND version = $2
-                            )
-                            "#,
-                            <T::Inner as OntologyDatabaseType>::table()
-                        ),
-                        parameter_list([uri, &i64::from(version)]),
-                    )
-                    .await
+                by_uri_by_version::<T::Inner>(self.as_client(), uri, version).await?
             }
             (None, Some(OntologyVersion::Latest)) => {
-                self.as_client()
-                    .query_raw(
-                        &format!(
-                            r#"
-                            SELECT DISTINCT ON(base_uri) schema, created_by
-                            FROM {table}
-                            INNER JOIN ids ON ids.version_id = {table}.version_id
-                            ORDER BY base_uri, version DESC;
-                            "#,
-                            table = <T::Inner as OntologyDatabaseType>::table()
-                        ),
-                        parameter_list([]),
-                    )
-                    .await
+                by_latest_version::<T::Inner>(self.as_client()).await?
             }
             _ => todo!(),
         };
 
         row_stream
-            .into_report()
-            .change_context(QueryError)?
             .map(|row_result| {
                 let row = row_result.into_report().change_context(QueryError)?;
 
                 let element: T::Inner = serde_json::from_value(row.get(0))
                     .into_report()
                     .change_context(QueryError)?;
-                let account_id: AccountId = row.get(1);
 
                 let uri = element.versioned_uri().clone();
+                let account_id: AccountId = row.get(1);
 
                 Ok(T::new(
                     element,
