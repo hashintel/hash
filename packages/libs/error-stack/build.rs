c601ba56a919232ef8a5e1da8b9bb74de9780a3a
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-08-15 20:46:51 +0200

    feat: stabilized in 2022-08-10

--- packages/libs/error-stack/build.rs
+++ packages/libs/error-stack/build.rs
@@ -10,6 +10,18 @@ fn main() {
     version.pre = vec![];
     version.build = vec![];
 
+    if let Some(build_date) = version_meta().unwrap().commit_date {
+        let mut build_date = build_date.split('-');
+        let year: usize = build_date.next().unwrap().parse().unwrap();
+        let month: usize = build_date.next().unwrap().parse().unwrap();
+        let day: usize = build_date.next().unwrap().parse().unwrap();
+
+        eprintln!("{} {} {}", year, month, day);
+        if year >= 2022 && month >= 8 && day >= 10 {
+            println!("cargo:rustc-cfg=nightly_2022_08_10")
+        }
+    }
+
     if version >= Version::new(1, 65, 0) {
         println!("cargo:rustc-cfg=rust_1_65")
     }
