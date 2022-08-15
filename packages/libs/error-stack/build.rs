40ef158c1dd05279e85857c7044f5a7ec29c026b
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-08-15 20:11:13 +0200

    feat: remove stabilized feature

--- packages/libs/error-stack/build.rs
+++ packages/libs/error-stack/build.rs
@@ -1,7 +1,16 @@
-use rustc_version::{version_meta, Channel};
+use rustc_version::{version_meta, Channel, Version};
 
 fn main() {
     if version_meta().unwrap().channel == Channel::Nightly {
         println!("cargo:rustc-cfg=nightly")
+    }
+
+    let mut version = version_meta().unwrap().semver;
+    // remove the suffix (like nightly)
+    version.pre = vec![];
+    version.build = vec![];
+
+    if version >= Version::new(1, 65, 0) {
+        println!("cargo:rustc-cfg=rust_1_65")
     }
 }
