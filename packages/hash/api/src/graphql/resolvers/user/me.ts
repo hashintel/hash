2cf64a69ac671bb92bfbd5297fc935a0f545e492
Ben Werner <ben.werner01@gmail.com>
2022-08-15 10:19:49 +0100

    ft: me query

--- packages/hash/api/src/graphql/resolvers/user/me.ts
+++ packages/hash/api/src/graphql/resolvers/user/me.ts
@@ -1,9 +1,53 @@
-import { Resolver } from "../../apiTypes.gen";
+import { Resolver, User as GQLUser, Visibility } from "../../apiTypes.gen";
 import { LoggedInGraphQLContext } from "../../context";
-import { UnresolvedGQLEntity } from "../../../model";
+import { UserModel } from "../../../model";
+import { GraphApi } from "../../../graph";
+
+type GQLUserExternalResolvers =
+  | "accountSignupComplete"
+  | "memberOf"
+  | "entityType"
+  | "linkGroups"
+  | "linkedEntities"
+  | "linkedAggregations";
+
+type UnresolvedGQLUser = Omit<GQLUser, GQLUserExternalResolvers>;
+
+const mapUserModelToGQL = (
+  _graphApi: GraphApi,
+  user: UserModel,
+): UnresolvedGQLUser => {
+  return {
+    accountId: user.accountId,
+    id: user.entityId,
+    entityId: user.entityId,
+    entityVersionId: user.version,
+    entityTypeId: "" /** @todo: deprecate this field */,
+    entityTypeVersionId: user.entityTypeModel.schema.$id,
+    entityTypeName: user.entityTypeModel.schema.title,
+    properties: {
+      shortname: user.getShortname(),
+      preferredName: user.getPreferredName(),
+      emails: user.getEmails().map((address) => ({
+        address,
+        verified: true /** @todo: stop hardcoding this  */,
+        primary: true /** @todo: stop hardcoding this */,
+      })),
+    },
+    entityVersionCreatedAt:
+      new Date().toISOString() /** @todo: stop hardcoding this */,
+    createdAt: new Date().toISOString() /** @todo: stop hardcoding this */,
+    updatedAt: new Date().toISOString() /** @todo: stop hardcoding this */,
+    createdByAccountId: "" /** @todo: stop hardcoding this */,
+    visibility:
+      Visibility.Public /** @todo: potentially deprecate or stop hardcoding this */,
+    metadataId: "" /** @todo: deprecate this */,
+  };
+};
 
 export const me: Resolver<
-  UnresolvedGQLEntity,
+  UnresolvedGQLUser,
   {},
   LoggedInGraphQLContext
-> = async (_, __, { user }) => user.toGQLUnknownEntity();
+> = async (_, __, { user, dataSources: { graphApi } }) =>
+  mapUserModelToGQL(graphApi, user);
