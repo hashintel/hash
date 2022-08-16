eb117da88f1b22782ed82445035a751a69f4e83c
Nate Higgins <nate@nateh.uk>
2022-08-16 11:34:15 +0100

    Use a referentially stable ref merging util

--- packages/blocks/header/src/app.tsx
+++ packages/blocks/header/src/app.tsx
@@ -1,7 +1,7 @@
 import { useHookBlockService, useHookRef } from "@blockprotocol/hook";
 import { BlockComponent } from "@blockprotocol/graph/react";
 import { useRef } from "react";
-import { mergeRefs } from "react-merge-refs";
+import useForkRef from "@rooks/use-fork-ref";
 
 type BlockEntityProperties = {
   color?: string;
@@ -18,25 +18,22 @@ export const App: BlockComponent<BlockEntityProperties> = ({
 }) => {
   const headerRef = useRef<HTMLHeadingElement>(null);
   const { hookService } = useHookBlockService(headerRef);
-  const headerHookRef = useHookRef<HTMLHeadingElement>(
-    hookService,
-    "text",
-    "$.text",
-    (node) => {
-      if (node) {
-        // eslint-disable-next-line no-param-reassign
-        node.innerText = text ?? "";
-      }
-    },
-  );
+  const headerHookRef = useHookRef(hookService, "text", "$.text", (node) => {
+    if (node) {
+      // eslint-disable-next-line no-param-reassign
+      node.innerText = text ?? "";
+    }
+  });
 
   // @todo set type correctly
   const Header = `h${level}` as any;
 
+  const ref = useForkRef(headerRef, headerHookRef);
+
   return (
     <Header
       style={{ fontFamily: "Arial", color: color ?? "black", marginBottom: 0 }}
-      ref={mergeRefs([headerRef, headerHookRef])}
+      ref={ref}
     />
   );
 };
