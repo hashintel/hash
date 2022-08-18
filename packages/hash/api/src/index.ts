f558d6d2ecb70d82de7f238bafeaf1125dea5d77
Ahmad Sattar <thehabbos007@gmail.com>
2022-08-18 12:34:50 +0200

    Apply #919 (api)

--- packages/hash/api/src/index.ts
+++ packages/hash/api/src/index.ts
@@ -42,6 +42,8 @@ import { setupStorageProviders } from "./storage/storage-provider-lookup";
 import { getAwsRegion } from "./lib/aws-config";
 import { setupTelemetry } from "./telemetry/snowplow-setup";
 import { connectToTaskExecutor } from "./task-execution";
+import { createGraphClient } from "./graph";
+import { ensureWorkspaceTypesExist } from "./graph/workspace-types";
 
 const shutdown = new GracefulShutdown(logger, "SIGINT", "SIGTERM");
 
@@ -98,7 +100,13 @@ const main = async () => {
     10,
   );
 
-  await waitOnResource(`tcp:${redisHost}:${redisPort}`, logger);
+  const graphApiHost = getRequiredEnv("HASH_GRAPH_API_HOST");
+  const graphApiPort = parseInt(getRequiredEnv("HASH_GRAPH_API_PORT"), 10);
+
+  await Promise.all([
+    waitOnResource(`tcp:${redisHost}:${redisPort}`, logger),
+    waitOnResource(`tcp:${graphApiHost}:${graphApiPort}`, logger),
+  ]);
 
   // Connect to Redis
   const redis = new RedisCache(logger, {
@@ -113,6 +121,14 @@ const main = async () => {
     port: taskExecutorPort,
   };
   const taskExecutor = connectToTaskExecutor(taskExecutorConfig);
+
+  // Connect to the Graph API
+  const graphApi = createGraphClient(
+    { host: graphApiHost, port: graphApiPort },
+    logger,
+  );
+
+  await ensureWorkspaceTypesExist({ graphApi, logger });
 
   // Set sensible default security headers: https://www.npmjs.com/package/helmet
   // Temporarily disable contentSecurityPolicy for the GraphQL playground
@@ -142,8 +158,8 @@ const main = async () => {
         })
       : new AwsSesEmailTransporter({
           from: `${getRequiredEnv(
-            "SYSTEM_EMAIL_SENDER_NAME",
-          )} <${getRequiredEnv("SYSTEM_EMAIL_ADDRESS")}>`,
+            "WORKSPACE_EMAIL_SENDER_NAME",
+          )} <${getRequiredEnv("WORKSPACE_EMAIL_ADDRESS")}>`,
           region: getAwsRegion(),
           subjectPrefix: isProdEnv ? undefined : "[DEV SITE] ",
         });
