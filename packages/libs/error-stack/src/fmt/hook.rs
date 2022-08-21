c1ad71eef17f4f92d8f95cc52ddf1690611e375d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-08-21 12:11:16 +0200

    feat: change lint

--- packages/libs/error-stack/src/fmt/hook.rs
+++ packages/libs/error-stack/src/fmt/hook.rs
@@ -1,7 +1,7 @@
 // We allow dead-code here, because some of the functions are only exposed when `feature = "hooks"`
 // we could do cfg for everything, but that gets very messy, instead we only use a subset
 // and enable deadcode on `feature = "std"`.
-#![cfg_attr(not(feature = "std"), allow(dead_code))]
+#![cfg_attr(not(feature = "std"), allow(dead_code, unreachable_pub))]
 
 use alloc::{boxed::Box, collections::BTreeMap, string::String, vec::Vec};
 use core::{
