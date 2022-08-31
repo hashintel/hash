7e57c7d5caeab5097e2a0cf7706c50f4026c260e
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-08-31 13:27:13 +0300

    implement page content loading via BlankView

---
+++ packages/hash/frontend/src/blocks/page/BlankView.tsx
@@ -0,0 +1,40 @@
+import { Skeleton } from "@mui/material";
+import { ProsemirrorNode, Schema } from "prosemirror-model";
+import { NodeView } from "prosemirror-view";
+
+import { RenderPortal } from "./usePortals";
+
+const PageContentLoadingState = () => {
+  return (
+    <>
+      {[1, 2, 3].map((num) => (
+        <Skeleton key={num} animation="wave" height={50} />
+      ))}
+    </>
+  );
+};
+
+export class BlankView implements NodeView<Schema> {
+  dom: HTMLDivElement;
+
+  constructor(
+    public node: ProsemirrorNode<Schema>,
+    public renderPortal: RenderPortal,
+  ) {
+    this.dom = document.createElement("div");
+    this.update(node);
+  }
+
+  update(node: ProsemirrorNode<Schema>) {
+    if (node.type.name !== "blank") {
+      return false;
+    }
+
+    this.renderPortal(<PageContentLoadingState />, this.dom);
+    return true;
+  }
+
+  destroy() {
+    this.renderPortal(null, this.dom);
+  }
+}
