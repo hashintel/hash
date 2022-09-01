6c2cf15e2b336e059302677b37cc71447910b32d
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-09-01 10:09:51 +0300

    make sidebar loading pixel-perfect

--- packages/hash/frontend/src/shared/layout/layout-with-sidebar/account-page-list/pages-loading-state.tsx
+++ packages/hash/frontend/src/shared/layout/layout-with-sidebar/account-page-list/pages-loading-state.tsx
@@ -1,22 +1,31 @@
 import { Box, Skeleton } from "@mui/material";
+import { pageIconVariantSizes } from "../../../../components/PageIcon";
 
 const LoadingLine = () => {
   return (
-    <Box display="flex" alignItems="center" mb={0.4}>
-      <Box
-        component={Skeleton}
-        mr={0.75}
-        height={20}
-        width={20}
-        variant="circular"
+    <Box display="flex" alignItems="center" height={34}>
+      <Skeleton
+        animation="wave"
+        variant="rectangular"
+        sx={{
+          mr: 0.75,
+          height: pageIconVariantSizes.small.container,
+          width: pageIconVariantSizes.small.container,
+          borderRadius: 1,
+        }}
       />
-      <Box component={Skeleton} height={32} flex={1} />
+      <Skeleton
+        variant="rectangular"
+        animation="wave"
+        height={pageIconVariantSizes.small.container}
+        sx={{ flex: 1, borderRadius: 1 }}
+      />
     </Box>
   );
 };
 
 export const PagesLoadingState = () => (
-  <Box sx={{ mx: 0.75, px: 3.5 }}>
+  <Box mx={0.75} px={3.5}>
     <LoadingLine />
     <LoadingLine />
     <LoadingLine />
