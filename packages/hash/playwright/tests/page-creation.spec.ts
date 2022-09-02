2aecd1690907156c68e6fae9b2ed4043c06979f4
Yusuf Kınataş <yusufkinatas@gmail.com>
2022-09-02 10:13:45 +0300

    Fix integration tests got broken by `PageTitle` changes (#1018)
    * upgrade playwright version for VSCode extension
    
    * fix broken integration tests
    
    * fix broken entity-creation test
    
    * Update packages/hash/playwright/tests/page-creation.spec.ts
    
    remove stale comment line
    
    Co-authored-by: Alexander Kachkaev <alexander@kachkaev.ru>
    
    * add resetDb for playwright
    
    * rename util files to kebab-case
    
    * rename util files to kebab-case
    
    Co-authored-by: Alexander Kachkaev <alexander@kachkaev.ru>

--- packages/hash/playwright/tests/page-creation.spec.ts
+++ packages/hash/playwright/tests/page-creation.spec.ts
@@ -1,14 +1,23 @@
 import { test, expect } from "@playwright/test";
 import { sleep } from "@hashintel/hash-shared/sleep";
-import { loginUsingUi } from "./utils/loginUsingUi";
+import { loginUsingUi } from "./utils/login-using-ui";
+import { resetDb } from "./utils/reset-db";
 
 const pageNameSuffix = Date.now();
-const pageName = "Untitled";
+const pageNameFallback = "Untitled";
 
 const listOfPagesSelector = '[data-testid="pages-tree"]';
-const pageTitleInputSelector = '[placeholder="A title for the page"]';
+const pageTitleInputSelector = '[placeholder="Untitled"]';
 
 const modifierKey = process.platform === "darwin" ? "Meta" : "Control";
+
+/**
+ * not calling resetDb in beforeEach
+ * because "user can rename page" uses the page created at "user can create page"
+ */
+test.beforeAll(async () => {
+  await resetDb();
+});
 
 test("user can create page", async ({ page }) => {
   await loginUsingUi({
@@ -27,18 +36,15 @@ test("user can create page", async ({ page }) => {
 
   await page.waitForURL((url) => !!url.pathname.match(/^\/[\w-]+\/[\w-]+$/));
 
-  await expect(page.locator(pageTitleInputSelector)).toHaveValue(pageName);
-
   const blockRegionLocator = page.locator("#root");
   const listOfPagesLocator = page.locator(listOfPagesSelector);
-  const pageTitleLocator = page.locator(pageTitleInputSelector);
 
   // Wait for ProseMirror to load
   // TODO: investigate why page renaming before block loading is unstable
   await expect(
     blockRegionLocator.locator('[data-testid="block-handle"]'),
   ).toHaveCount(1);
-  await expect(listOfPagesLocator).toContainText(pageName);
+  await expect(listOfPagesLocator).toContainText(pageNameFallback);
 
   // Type in a paragraph block
   await blockRegionLocator.locator("p div").click();
@@ -135,8 +141,6 @@ test("user can create page", async ({ page }) => {
   // Check content stability after page reload
   await page.reload();
 
-  await expect(pageTitleLocator).toHaveValue(pageName);
-
   await expect(blockRegionLocator.locator("p").nth(0)).toContainText(
     "My test paragraph with bold and italics",
     { useInnerText: true }, // Prevents words from sticking to each other
@@ -162,42 +166,39 @@ test("user can create page", async ({ page }) => {
   ).toHaveCount(5);
 });
 
-// TODO: investigate flakiness of page renaming and enable the test in CI
-test.skip("user can rename page", async ({ page }) => {
-  const changedPageName = `Renamed test page ${pageNameSuffix}`;
+test("user can rename page", async ({ page }) => {
+  const pageName1 = `Page ${pageNameSuffix}`;
+  const pageName2 = `Page 2 ${pageNameSuffix}`;
 
   await loginUsingUi({ page, accountShortName: "alice" });
-  await page.click(`text=${pageName}`);
+  await page.click(`text=${pageNameFallback}`);
   await page.waitForURL((url) => !!url.pathname.match(/^\/[\w-]+\/[\w-]+$/));
 
   const listOfPagesLocator = page.locator(listOfPagesSelector);
   const pageTitleLocator = page.locator(pageTitleInputSelector);
 
   // Change page name (using Enter)
-  await pageTitleLocator.fill(changedPageName);
+  await pageTitleLocator.fill(pageName2);
   await pageTitleLocator.press("Enter");
   await expect(pageTitleLocator).toBeEnabled();
-  await expect(listOfPagesLocator).not.toContainText(pageName);
-  await expect(listOfPagesLocator).toContainText(changedPageName);
+  await expect(listOfPagesLocator).toContainText(pageName2);
 
   // Revert page name change (using Tab)
   await sleep(500); // TODO: Investigate why delay is required for <PageTitle /> state to work
-  await pageTitleLocator.fill(pageName);
+  await pageTitleLocator.fill(pageName1);
   await pageTitleLocator.press("Tab");
-  await expect(listOfPagesLocator).not.toContainText(changedPageName);
-  await expect(listOfPagesLocator).toContainText(pageName);
+  await expect(listOfPagesLocator).toContainText(pageName1);
 
   // Change page name (by clicking outside)
   await sleep(500); // TODO: Investigate why delay is required for <PageTitle /> state to work
-  await pageTitleLocator.fill(changedPageName);
+  await pageTitleLocator.fill(pageName2);
   await page.click("main");
-  await expect(listOfPagesLocator).not.toContainText(pageName);
-  await expect(listOfPagesLocator).toContainText(changedPageName);
+  await expect(listOfPagesLocator).toContainText(pageName2);
 
   // Revert page name change (using Esc)
   await sleep(500); // TODO: Investigate why delay is required for <PageTitle /> state to work
-  await pageTitleLocator.fill(pageName);
+  await pageTitleLocator.fill(pageName1);
   await pageTitleLocator.press("Escape");
-  await expect(listOfPagesLocator).not.toContainText(changedPageName);
-  await expect(listOfPagesLocator).toContainText(pageName);
+  await expect(listOfPagesLocator).not.toContainText(pageName2);
+  await expect(listOfPagesLocator).toContainText(pageName1);
 });
