4ece50f1703bd595f0dffe281c566b46510ad025
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-09-02 18:11:19 +0200

    Add entity database queries to `PostgresContext`

--- packages/graph/hash_graph/lib/graph/src/store/postgres/resolve/context.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/resolve/context.rs
@@ -2,12 +2,15 @@ use async_trait::async_trait;
 use error_stack::{Context, Result, ResultExt};
 use type_system::uri::VersionedUri;
 
-use crate::store::{
-    postgres::{
-        ontology::OntologyDatabaseType,
-        resolve::{ontology, ontology::RecordStream, OntologyRecord},
+use crate::{
+    knowledge::EntityId,
+    store::{
+        postgres::{
+            ontology::OntologyDatabaseType,
+            resolve::{entity, entity::EntityRecord, ontology, OntologyRecord},
+        },
+        AsClient, PostgresStore, QueryError,
     },
-    AsClient, PostgresStore, QueryError,
 };
 
 /// Context used for [`Resolve`].
@@ -20,7 +23,7 @@ use crate::store::{
 //   see https://app.asana.com/0/0/1202884883200946/f
 #[async_trait]
 pub trait PostgresContext {
-    async fn read_all_ontology_types<T>(&self) -> Result<RecordStream<T>, QueryError>
+    async fn read_all_ontology_types<T>(&self) -> Result<ontology::RecordStream<T>, QueryError>
     where
         T: OntologyDatabaseType + TryFrom<serde_json::Value, Error: Context>;
 
@@ -30,11 +33,18 @@ pub trait PostgresContext {
     ) -> Result<OntologyRecord<T>, QueryError>
     where
         T: OntologyDatabaseType + TryFrom<serde_json::Value, Error: Context>;
+
+    async fn read_all_entities(&self) -> Result<entity::RecordStream, QueryError>;
+
+    async fn read_latest_entity_by_id(
+        &self,
+        entity_id: EntityId,
+    ) -> Result<EntityRecord, QueryError>;
 }
 
 #[async_trait]
 impl<C: AsClient> PostgresContext for PostgresStore<C> {
-    async fn read_all_ontology_types<T>(&self) -> Result<RecordStream<T>, QueryError>
+    async fn read_all_ontology_types<T>(&self) -> Result<ontology::RecordStream<T>, QueryError>
     where
         T: OntologyDatabaseType + TryFrom<serde_json::Value, Error: Context>,
     {
@@ -53,5 +63,20 @@ impl<C: AsClient> PostgresContext for PostgresStore<C> {
         ontology::read_versioned_type(&self.client, T::table(), uri)
             .await
             .attach_printable("could not read ontology type")
+    }
+
+    async fn read_all_entities(&self) -> Result<entity::RecordStream, QueryError> {
+        Ok(entity::read_all_entities(&self.client)
+            .await
+            .attach_printable("could not read entities")?)
+    }
+
+    async fn read_latest_entity_by_id(
+        &self,
+        entity_id: EntityId,
+    ) -> Result<EntityRecord, QueryError> {
+        Ok(entity::read_latest_entity_by_id(&self.client, entity_id)
+            .await
+            .attach_printable("could not read entity")?)
     }
 }
--- packages/graph/hash_graph/lib/graph/src/store/postgres/resolve/entity.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/resolve/entity.rs
@@ -1,2 +1,94 @@
-// TODO: Implement structural queries for entities
-//   see https://app.asana.com/0/0/1202510174412977/f
+use chrono::{DateTime, Utc};
+use error_stack::{IntoReport, Result, ResultExt};
+use futures::{Stream, StreamExt};
+use tokio_postgres::{GenericClient, RowStream};
+use type_system::uri::{BaseUri, VersionedUri};
+
+use crate::{
+    knowledge::{Entity, EntityId},
+    ontology::AccountId,
+    store::{postgres::parameter_list, AsClient, QueryError},
+};
+
+pub struct EntityRecord {
+    pub entity: Entity,
+    pub id: EntityId,
+    pub version: DateTime<Utc>,
+    pub type_uri: VersionedUri,
+    pub account_id: AccountId,
+    pub is_latest: bool,
+}
+
+pub type RecordStream = impl Stream<Item = Result<EntityRecord, QueryError>>;
+
+fn row_stream_to_record_stream(row_stream: RowStream) -> RecordStream {
+    row_stream.map(|row_result| {
+        let row = row_result.into_report().change_context(QueryError)?;
+
+        Ok(EntityRecord {
+            entity: serde_json::from_value(row.get(0)).expect("invalid entity"),
+            id: row.get(1),
+            version: row.get(2),
+            type_uri: VersionedUri::new(
+                BaseUri::new(row.get(3)).expect("invalid BaseUri"),
+                row.get::<_, i64>(4) as u32,
+            ),
+            account_id: row.get(5),
+            is_latest: row.get(6),
+        })
+    })
+}
+
+pub async fn read_all_entities(client: &impl AsClient) -> Result<RecordStream, QueryError> {
+    let row_stream= client
+        .as_client()
+        .query_raw(
+            r#"
+            SELECT properties, entity_id, entities.version, ids.base_uri, ids.version, created_by, MAX(entities.version) OVER (PARTITION by entity_id) = entities.version as latest
+            FROM entities
+            INNER JOIN ids
+            ids.version_id = entities.entity_type_version_id
+            ORDER BY base_uri, version DESC;
+            "#,
+            parameter_list([]),
+        )
+        .await
+        .into_report().change_context(QueryError)?;
+    Ok(row_stream_to_record_stream(row_stream))
+}
+
+pub async fn read_latest_entity_by_id(
+    client: &impl AsClient,
+    entity_id: EntityId,
+) -> Result<EntityRecord, QueryError> {
+    let row = client
+        .as_client()
+        .query_one(
+            r#"
+            SELECT properties, entity_id, entities.version, ids.base_uri, ids.version, created_by
+            FROM entities
+            INNER JOIN ids ON ids.version_id = entities.entity_type_version_id
+            WHERE entity_id = $1 AND entities.version = (
+                SELECT MAX("version")
+                FROM entities
+                WHERE entity_id = $1
+            );
+            "#,
+            &[&entity_id],
+        )
+        .await
+        .into_report()
+        .change_context(QueryError)?;
+
+    Ok(EntityRecord {
+        entity: serde_json::from_value(row.get(0)).expect("invalid entity"),
+        id: row.get(1),
+        version: row.get(2),
+        type_uri: VersionedUri::new(
+            BaseUri::new(row.get(3)).expect("invalid BaseUri"),
+            row.get::<_, i64>(4) as u32,
+        ),
+        account_id: row.get(5),
+        is_latest: true,
+    })
+}
