96e8c57ddb3bcedc728667c85905bf1eb615d1f9
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-09-09 00:37:02 +0200

    Remove dependency on `pin-project`

--- packages/libs/error-stack/src/ext/mod.rs
+++ packages/libs/error-stack/src/ext/mod.rs
@@ -1,9 +1,6 @@
 //! Extension traits for foreign types.
 
-#[cfg(feature = "futures")]
 pub mod future;
-// false positive, is imported in `lib.rs`
-#[allow(unreachable_pub)]
 pub mod iter;
 pub mod result;
 #[cfg(feature = "futures")]
