acd1f230e8ba9dea3b80f1f85bd65fd28d4379b6
Ahmad Sattar <thehabbos007@gmail.com>
2022-09-08 13:26:41 +0200

    Realtime updates (#1029)
    * Add wal2json to postgres docker-compose service
    * Make realtime dev script consistent with workspace API
    * Refactor and fix realtime main code
    * Remove chatty postgres WAL logs
    * Refactor slot acquisition
    * Remove aggregationversion
    * Use `GracefulShutdown

--- packages/hash/api/src/collab/Instance.ts
+++ packages/hash/api/src/collab/Instance.ts
@@ -1,3 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+/**
+ * @todo remove above ts-nocheck as we start re-enabling collab
+ *   https://app.asana.com/0/1202805690238892/1202924026802709/f
+ */
 import { ApolloClient } from "@apollo/client";
 import {
   AggregationVersion,
