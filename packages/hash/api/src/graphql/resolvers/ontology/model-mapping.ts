2ead825ddfea3f1df8ecce118f6ba8b1512a9495
Tim Diekmann <tim.diekmann@3dvision.de>
2022-09-15 13:27:48 +0200

    Apply suggestions from code review

--- packages/hash/api/src/graphql/resolvers/ontology/model-mapping.ts
+++ packages/hash/api/src/graphql/resolvers/ontology/model-mapping.ts
@@ -61,24 +61,20 @@ export const entityTypeModelToGQL = (
   entityType: entityType.schema,
 });
 
-export const entityTypeRootedSubgraphToGQL = ({
-  entityType,
-  referencedDataTypes,
-  referencedPropertyTypes,
-  referencedLinkTypes,
-  referencedEntityTypes,
-}: {
+export const entityTypeRootedSubgraphToGQL = (params: {
   entityType: EntityTypeModel;
   referencedDataTypes: DataTypeModel[];
   referencedPropertyTypes: PropertyTypeModel[];
   referencedLinkTypes: LinkTypeModel[];
   referencedEntityTypes: EntityTypeModel[];
 }): EntityTypeRootedSubgraph => ({
-  accountId: entityType.accountId,
-  entityTypeVersionedUri: entityType.schema.$id,
-  entityType: entityType.schema,
-  referencedDataTypes: referencedDataTypes.map(dataTypeModelToGQL),
-  referencedPropertyTypes: referencedPropertyTypes.map(propertyTypeModelToGQL),
-  referencedLinkTypes: referencedLinkTypes.map(linkTypeModelToGQL),
-  referencedEntityTypes: referencedEntityTypes.map(entityTypeModelToGQL),
+  accountId: params.entityType.accountId,
+  entityTypeVersionedUri: params.entityType.schema.$id,
+  entityType: params.entityType.schema,
+  referencedDataTypes: params.referencedDataTypes.map(dataTypeModelToGQL),
+  referencedPropertyTypes: params.referencedPropertyTypes.map(
+    propertyTypeModelToGQL,
+  ),
+  referencedLinkTypes: params.referencedLinkTypes.map(linkTypeModelToGQL),
+  referencedEntityTypes: params.referencedEntityTypes.map(entityTypeModelToGQL),
 });
