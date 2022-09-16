520a60977e937dfb8a1689e9795b228199e405c4
Ben Werner <ben.werner01@gmail.com>
2022-09-16 16:54:18 +0100

    chore: comment out implementation of `createOrg` and throw error if it's called

--- packages/hash/api/src/graphql/resolvers/org/createOrg.ts
+++ packages/hash/api/src/graphql/resolvers/org/createOrg.ts
@@ -1,45 +1,47 @@
+import { ApolloError } from "apollo-server-errors";
 import { MutationCreateOrgArgs, ResolverFn } from "../../apiTypes.gen";
-import { Account, OrgModel } from "../../../model";
 import { LoggedInGraphQLContext } from "../../context";
-import { mapOrgModelToGQL, UnresolvedGQLOrg } from "../user/util";
+import { UnresolvedGQLOrg } from "./util";
 
 export const createOrg: ResolverFn<
   Promise<UnresolvedGQLOrg>,
   {},
   LoggedInGraphQLContext,
   MutationCreateOrgArgs
-> = async (_, { org: orgInput, responsibility }, { dataSources, user }) =>
-  dataSources.db.transaction(async (client) => {
-    const { graphApi } = dataSources;
-    const { shortname, name, orgSize } = orgInput;
+> = async (_, __, { dataSources }) =>
+  dataSources.db.transaction(async (_client) => {
+    throw new ApolloError("The joinOrg mutation is unimplemented");
 
-    /** @todo: potentially deprecate these method calls depending on Graph API transaction implementation */
-    await (user as any)
-      .acquireLock(client)
-      .then(() => (user as any).refetchLatestVersion(client));
+    // const { graphApi } = dataSources;
+    // const { shortname, name, orgSize } = orgInput;
 
-    await Account.validateShortname(client, shortname);
+    // /** @todo: potentially deprecate these method calls depending on Graph API transaction implementation */
+    // await user
+    //   .acquireLock(client)
+    //   .then(() => user.refetchLatestVersion(client));
 
-    const org = await OrgModel.createOrg(graphApi, {
-      providedInfo: {
-        orgSize,
-      },
-      shortname,
-      name,
-    });
+    // await Account.validateShortname(client, shortname);
 
-    /**
-     * @todo: potentially deprecate these method calls depending on Graph API
-     * transaction implementation (@see https://app.asana.com/0/1201095311341924/1202573572594586/f)
-     */
-    await (org as any)
-      .acquireLock(client)
-      .then(() => (org as any).refetchLatestVersion(client));
+    // const org = await OrgModel.createOrg(graphApi, {
+    //   providedInfo: {
+    //     orgSize,
+    //   },
+    //   shortname,
+    //   name,
+    // });
 
-    await user.joinOrg(graphApi, {
-      org,
-      responsibility,
-    });
+    // /**
+    //  * @todo: potentially deprecate these method calls depending on Graph API
+    //  * transaction implementation (@see https://app.asana.com/0/1201095311341924/1202573572594586/f)
+    //  */
+    // await org
+    //   .acquireLock(client)
+    //   .then(() => org.refetchLatestVersion(client));
 
-    return mapOrgModelToGQL(org);
+    // await user.joinOrg(graphApi, {
+    //   org,
+    //   responsibility,
+    // });
+
+    // return mapOrgModelToGQL(org);
   });
