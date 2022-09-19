f94c6bbc0f3da57814aa63de04c713174c1c2863
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-09-19 14:44:42 +0100

    Add `PersistedLink`

--- packages/graph/hash_graph/lib/graph/src/knowledge/link/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/knowledge/link/mod.rs
@@ -5,6 +5,7 @@ use type_system::uri::VersionedUri;
 use utoipa::Component;
 
 use super::EntityId;
+use crate::ontology::AccountId;
 
 /// A Link between a source and a target entity identified by [`EntityId`]s.
 ///
@@ -45,6 +46,36 @@ impl Link {
     #[must_use]
     pub const fn link_type_uri(&self) -> &VersionedUri {
         &self.link_type_uri
+    }
+}
+
+/// A record of a [`Link`] that has been persisted in the datastore, with its associated
+/// metadata.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Component)]
+#[serde(rename_all = "camelCase")]
+pub struct PersistedLink {
+    inner: Link,
+    // Note: this is inconsistent with `PersistedEntity` as the analog of
+    // `PersistedEntityIdentifier` is encapsulated within the `Link` struct..
+    owned_by_id: AccountId,
+    // TODO: add versioning information -
+    //   https://app.asana.com/0/1200211978612931/1203006164248577/f
+}
+
+impl PersistedLink {
+    #[must_use]
+    pub const fn new(inner: Link, owned_by_id: AccountId) -> Self {
+        Self { inner, owned_by_id }
+    }
+
+    #[must_use]
+    pub const fn inner(&self) -> &Link {
+        &self.inner
+    }
+
+    #[must_use]
+    pub const fn owned_by_id(&self) -> &AccountId {
+        &self.owned_by_id
     }
 }
 
