4f760eec9acfd0704b9405bda2cc13d5abc7459a
Ben Werner <ben.werner01@gmail.com>
2022-09-23 13:40:43 +0100

    ft: `PageModel` class tests

---
+++ packages/hash/integration/src/tests/model/knowledge/page.model.test.ts
@@ -0,0 +1,173 @@
+import { getRequiredEnv } from "@hashintel/hash-backend-utils/environment";
+import { createGraphClient } from "@hashintel/hash-api/src/graph";
+import {
+  ensureWorkspaceTypesExist,
+  WORKSPACE_TYPES,
+} from "@hashintel/hash-api/src/graph/workspace-types";
+import {
+  BlockModel,
+  EntityModel,
+  PageModel,
+  UserModel,
+} from "@hashintel/hash-api/src/model";
+import { Logger } from "@hashintel/hash-backend-utils/logger";
+import { createTestUser } from "../../util";
+
+jest.setTimeout(60000);
+
+const logger = new Logger({
+  mode: "dev",
+  level: "debug",
+  serviceName: "org-membership-tests",
+});
+
+const graphApiHost = getRequiredEnv("HASH_GRAPH_API_HOST");
+const graphApiPort = parseInt(getRequiredEnv("HASH_GRAPH_API_PORT"), 10);
+
+const graphApi = createGraphClient(logger, {
+  host: graphApiHost,
+  port: graphApiPort,
+});
+
+describe("Page model class", () => {
+  let testUser: UserModel;
+
+  beforeAll(async () => {
+    await ensureWorkspaceTypesExist({ graphApi, logger });
+    testUser = await createTestUser(graphApi, "pageModelTest", logger);
+  });
+
+  const createBlock = async () =>
+    await BlockModel.createBlock(graphApi, {
+      accountId: testUser.accountId,
+      componentId: "dummy-component-id",
+      blockData: await EntityModel.create(graphApi, {
+        accountId: testUser.accountId,
+        entityTypeModel: WORKSPACE_TYPES.entityType.dummy,
+        properties: {},
+      }),
+    });
+
+  let testPage: PageModel;
+
+  it("can create a page", async () => {
+    testPage = await PageModel.createPage(graphApi, {
+      accountId: testUser.entityId,
+      title: "Test Page",
+    });
+  });
+
+  it("can get a page by its entity id", async () => {
+    const fetchedPage = await PageModel.getPageById(graphApi, {
+      entityId: testPage.entityId,
+    });
+
+    expect(fetchedPage).toEqual(testPage);
+  });
+
+  it("can get all pages in an account", async () => {
+    const allPages = await PageModel.getAllPagesInAccount(graphApi, {
+      accountId: testUser.entityId,
+    });
+
+    expect(allPages).toEqual([testPage]);
+  });
+
+  let parentPage: PageModel;
+
+  it("can get/set a parent page", async () => {
+    parentPage = await PageModel.createPage(graphApi, {
+      accountId: testUser.entityId,
+      title: "Test Parent Page",
+    });
+
+    expect(await testPage.getParentPage(graphApi)).toBeNull();
+
+    await testPage.setParentPage(graphApi, {
+      parentPage,
+      setByAccountId: testUser.accountId,
+      prevIndex: null,
+      nextIndex: null,
+    });
+
+    expect(await testPage.getParentPage(graphApi)).toEqual(parentPage);
+  });
+
+  let testBlock1: BlockModel;
+
+  let testBlock2: BlockModel;
+
+  let testBlock3: BlockModel;
+
+  it("can insert blocks", async () => {
+    expect(await testPage.getBlocks(graphApi)).toEqual([]);
+
+    [testBlock1, testBlock2, testBlock3] = await Promise.all([
+      createBlock(),
+      createBlock(),
+      createBlock(),
+    ]);
+
+    await testPage.insertBlock(graphApi, {
+      block: testBlock1,
+      insertedBy: testUser.accountId,
+    });
+
+    // insert block at un-specified position
+    await testPage.insertBlock(graphApi, {
+      block: testBlock3,
+      insertedBy: testUser.accountId,
+    });
+
+    // insert bock at specified position
+    await testPage.insertBlock(graphApi, {
+      block: testBlock2,
+      insertedBy: testUser.accountId,
+      position: 1,
+    });
+
+    expect(await testPage.getBlocks(graphApi)).toEqual([
+      testBlock1,
+      testBlock2,
+      testBlock3,
+    ]);
+  });
+
+  it("can move a block", async () => {
+    await testPage.moveBlock(graphApi, {
+      currentPosition: 0,
+      newPosition: 2,
+      movedByAccountId: testUser.accountId,
+    });
+
+    expect(await testPage.getBlocks(graphApi)).toEqual([
+      testBlock2,
+      testBlock3,
+      testBlock1,
+    ]);
+
+    await testPage.moveBlock(graphApi, {
+      currentPosition: 2,
+      newPosition: 0,
+      movedByAccountId: testUser.accountId,
+    });
+
+    expect(await testPage.getBlocks(graphApi)).toEqual([
+      testBlock1,
+      testBlock2,
+      testBlock3,
+    ]);
+  });
+
+  it("can remove blocks", async () => {
+    await testPage.removeBlock(graphApi, {
+      position: 0,
+      removedByAccountId: testUser.accountId,
+    });
+
+    expect(await testPage.getBlocks(graphApi)).toEqual([
+      testBlock2,
+      testBlock3,
+    ]);
+  });
+});
