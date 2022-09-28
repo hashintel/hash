a3ed07855a310568686166153be5cccf35bd5d84
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-09-28 15:19:11 +0100

    Add TODOs

--- packages/graph/hash_graph/bench/benches/read_scaling/mod.rs
+++ packages/graph/hash_graph/bench/benches/read_scaling/mod.rs
@@ -1,1 +1,2 @@
-
+//! TODO: Introduce benchmarks testing the differing performance of operations as the graph's scale
+//!  changes
--- packages/graph/hash_graph/bench/benches/writes/mod.rs
+++ packages/graph/hash_graph/bench/benches/writes/mod.rs
@@ -1,1 +1,1 @@
-
+//! TODO: Introduce benchmarks testing the performance of various write operations
