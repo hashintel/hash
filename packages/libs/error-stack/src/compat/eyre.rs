708d4ea729bc6743c803aa8ca3f05a9765ece078
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-09-28 14:06:34 +0200

    Remove intermediate context for compatibility module (#1119)

--- packages/libs/error-stack/src/compat/eyre.rs
+++ packages/libs/error-stack/src/compat/eyre.rs
@@ -1,53 +1,15 @@
-#[cfg(nightly)]
-use core::any::Demand;
-use core::fmt;
+use core::panic::Location;
 
 use eyre::Report as EyreReport;
 
-use crate::{compat::IntoReportCompat, Context, Report, Result};
-
-/// A [`Context`] wrapper for [`eyre::Report`].
-///
-/// It provides the [`eyre::Report`] and forwards the [`Demand`] to [`Error::provide`].
-///
-/// [`Error::provide`]: core::error::Error::provide
-#[repr(transparent)]
-pub struct EyreContext(EyreReport);
-
-impl EyreContext {
-    /// Returns a reference to the underlying [`anyhow::Error`].
-    pub const fn as_eyre(&self) -> &EyreReport {
-        &self.0
-    }
-}
-
-impl fmt::Debug for EyreContext {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Debug::fmt(&self.0, fmt)
-    }
-}
-
-impl fmt::Display for EyreContext {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(&self.0, fmt)
-    }
-}
-
-impl Context for EyreContext {
-    #[cfg(nightly)]
-    fn provide<'a>(&'a self, demand: &mut Demand<'a>) {
-        demand.provide_ref(&self.0);
-
-        self.0.provide(demand);
-    }
-}
+use crate::{Frame, IntoReportCompat, Report, Result};
 
 impl<T> IntoReportCompat for core::result::Result<T, EyreReport> {
-    type Err = EyreContext;
+    type Err = EyreReport;
     type Ok = T;
 
     #[track_caller]
-    fn into_report(self) -> Result<T, EyreContext> {
+    fn into_report(self) -> Result<T, EyreReport> {
         match self {
             Ok(t) => Ok(t),
             Err(eyre) => {
@@ -58,7 +20,8 @@ impl<T> IntoReportCompat for core::result::Result<T, EyreReport> {
                     .collect::<alloc::vec::Vec<_>>();
 
                 #[cfg_attr(not(feature = "std"), allow(unused_mut))]
-                let mut report = Report::new(EyreContext(eyre));
+                let mut report =
+                    Report::from_frame(Frame::from_eyre(eyre, Location::caller(), Box::new([])));
 
                 for source in sources {
                     report = report.attach_printable(source);
