215cba3970579367a51babce2dfcf7a8d428453f
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-09-30 10:33:40 +0100

    Adding a benchmark for querying an entity type

--- packages/graph/hash_graph/bench/benches/representative_read/mod.rs
+++ packages/graph/hash_graph/bench/benches/representative_read/mod.rs
@@ -1,3 +1,18 @@
+//! Benchmarks to
+//!
+//! Across runs, Criterion compares benchmarks that share the same ID. We'd like to be able to
+//! track performance overtime, so we need to have consistent IDs. This means that if we're
+//! auto-generating entity type ID's, we can't use them as inputs to `bench_with_input`, as the
+//! [`BenchmarkId`] will vary between runs and we won't get comparative analysis.
+//!
+//! We also don't really want to measure how long it takes to query a sample of IDs (e.g. to make
+//! 100 entity type queries). As such, this function takes a list of [`EntityId`]s to sample from,
+//! and uses `iter_batched` to do a per-iteration setup whereby each benchmark iteration picks a
+//! random entity to sample. This should provide us with an idea of average performance for
+//! _querying a single entity type_ across the given sample.
+//!
+//! [`BenchmarkId`]: criterion::BenchmarkId
+
 use criterion::{BenchmarkId, Criterion};
 use criterion_macro::criterion;
 
@@ -11,8 +26,8 @@ mod seed;
 const DB_NAME: &str = "representative_read";
 
 #[criterion]
-fn bench_representative_read(c: &mut Criterion) {
-    let mut group = c.benchmark_group("representative_read");
+fn bench_representative_read_entity(c: &mut Criterion) {
+    let mut group = c.benchmark_group("representative_read_entity");
     let (runtime, mut store_wrapper) = setup(DB_NAME, false);
 
     let samples = runtime.block_on(setup_and_extract_samples(&mut store_wrapper));
@@ -21,15 +36,45 @@ fn bench_representative_read(c: &mut Criterion) {
     for (account_id, type_ids_and_entity_ids) in samples.entities {
         for (entity_type_id, entity_ids) in type_ids_and_entity_ids {
             group.bench_with_input(
-                BenchmarkId::from_parameter(format!(
-                    "Account ID: `{}`, Entity Type ID: `{}`",
-                    account_id, entity_type_id
-                )),
+                BenchmarkId::new(
+                    "get_entity_by_id",
+                    format!(
+                        "Account ID: `{}`, Entity Type ID: `{}`",
+                        account_id, entity_type_id
+                    ),
+                ),
                 &(account_id, entity_type_id, entity_ids),
                 |b, (_account_id, _entity_type_id, entity_ids)| {
                     knowledge::entity::bench_get_entity_by_id(b, &runtime, store, entity_ids);
                 },
             );
         }
+    }
+}
+
+#[criterion]
+fn bench_representative_read_entity_type(c: &mut Criterion) {
+    let mut group = c.benchmark_group("representative_read_entity_type");
+    let (runtime, mut store_wrapper) = setup(DB_NAME, false);
+
+    let samples = runtime.block_on(setup_and_extract_samples(&mut store_wrapper));
+    let store = &store_wrapper.store;
+
+    for (account_id, entity_type_ids) in samples.entity_types {
+        group.bench_with_input(
+            BenchmarkId::new(
+                "get_entity_type_by_id",
+                format!("Account ID: `{}`", account_id),
+            ),
+            &(account_id, entity_type_ids),
+            |b, (_account_id, entity_type_ids)| {
+                ontology::entity_type::bench_get_entity_type_by_id(
+                    b,
+                    &runtime,
+                    store,
+                    entity_type_ids,
+                );
+            },
+        );
     }
 }
---
+++ packages/graph/hash_graph/bench/benches/representative_read/ontology/entity_type.rs
@@ -0,0 +1,41 @@
+use criterion::{BatchSize::SmallInput, Bencher};
+use graph::{
+    ontology::EntityTypeQuery,
+    store::{query::Expression, EntityTypeStore},
+};
+use rand::{prelude::IteratorRandom, thread_rng};
+use tokio::runtime::Runtime;
+use type_system::uri::VersionedUri;
+
+use crate::util::Store;
+
+pub fn bench_get_entity_type_by_id(
+    b: &mut Bencher,
+    runtime: &Runtime,
+    store: &Store,
+    entity_type_ids: &[VersionedUri],
+) {
+    b.to_async(runtime).iter_batched(
+        || {
+            // Each iteration, *before timing*, pick a random entity from the sample to query
+            entity_type_ids
+                .iter()
+                .choose(&mut thread_rng())
+                .unwrap()
+                .clone()
+        },
+        |entity_type_id| async move {
+            store
+                .get_entity_type(&EntityTypeQuery {
+                    expression: Expression::for_versioned_uri(&entity_type_id),
+                    data_type_query_depth: 0,
+                    property_type_query_depth: 0,
+                    link_type_query_depth: 0,
+                    entity_type_query_depth: 0,
+                })
+                .await
+                .expect("failed to read entity type from store");
+        },
+        SmallInput,
+    );
+}
