6998678dc832cf475c8d6b66331f8a17b54605f0
Nate Higgins <nate@nateh.uk>
2022-10-04 06:16:33 +0100

    Remove theme reference

--- packages/hash/frontend/src/pages/[account-slug]/entity-types/styled-plus-circle-icon.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entity-types/styled-plus-circle-icon.tsx
@@ -2,7 +2,7 @@ import { experimental_sx, styled, Theme } from "@mui/material";
 import { CirclePlusIcon } from "../../../shared/icons/svg";
 
 export const StyledPlusCircleIcon = styled(CirclePlusIcon)(
-  experimental_sx<Theme>({
+  experimental_sx({
     height: "12px",
   }),
 );
