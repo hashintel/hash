a07974af64c733c593a09016d658929fe6a0b91c
Ben Werner <ben.werner01@gmail.com>
2022-10-04 17:23:50 +0100

    ft: `useBlockProtocolGetEntity` method

---
+++ packages/hash/frontend/src/components/hooks/blockProtocolFunctions/knowledge/knowledge-shim.ts
@@ -0,0 +1,43 @@
+/**
+ * This file contains "knowledge" function signatures of the new type system used to augment the
+ * existing set of Block Protocol.
+ *
+ * These signatures will eventually make their way into the @blockprotocol/graph
+ * package and be removed from here.
+ */
+
+import { MessageCallback } from "@blockprotocol/core";
+import { ReadOrModifyResourceError } from "@blockprotocol/graph";
+import {
+  UnknownKnowledgeEntity,
+  EntityTypeRootedSubgraph,
+} from "../../../../graphql/apiTypes.gen";
+
+export type KnolwedgeCallbacks = {
+  getEntity: GetEntityMessageCallback;
+};
+
+/* Entity CRU */
+
+/**
+ * @todo: remove this when we support the corresponding fields in the GQL API, or have implemented alternative fields.
+ * @see ...
+ */
+type UnsupportedKnowledgeEntityFields = "linkedEntities";
+
+type Entity = Omit<
+  UnknownKnowledgeEntity,
+  UnsupportedKnowledgeEntityFields | "entityType"
+> & {
+  entityTypeRootedSubgraph: EntityTypeRootedSubgraph;
+};
+
+export type EntityResponse = Entity;
+
+export type GetEntityRequest = Pick<EntityResponse, "entityId">;
+export type GetEntityMessageCallback = MessageCallback<
+  GetEntityRequest,
+  null,
+  EntityResponse,
+  ReadOrModifyResourceError
+>;
---
+++ packages/hash/frontend/src/components/hooks/blockProtocolFunctions/knowledge/useBlockProtocolGetEntity.ts
@@ -0,0 +1,97 @@
+import { useLazyQuery } from "@apollo/client";
+import { useCallback } from "react";
+import { getEntityTypeRootedSubgraphQuery } from "../../../../graphql/queries/ontology/entity-type.queries";
+
+import {
+  GetEntityTypeRootedSubgraphQuery,
+  GetEntityTypeRootedSubgraphQueryVariables,
+  Query,
+  QueryKnowledgeEntityArgs,
+} from "../../../../graphql/apiTypes.gen";
+import { getKnowledgeEntity } from "../../../../graphql/queries/knowledge/entity.queries";
+import { GetEntityMessageCallback } from "./knowledge-shim";
+
+export const useBlockProtocolGetEntity = (): {
+  getEntity: GetEntityMessageCallback;
+} => {
+  const [getEntityTypeRootedSubgraphFn] = useLazyQuery<
+    GetEntityTypeRootedSubgraphQuery,
+    GetEntityTypeRootedSubgraphQueryVariables
+  >(getEntityTypeRootedSubgraphQuery, {
+    /** @todo reconsider caching. This is done for testing/demo purposes. */
+    fetchPolicy: "no-cache",
+  });
+
+  const [getEntityFn] = useLazyQuery<Query, QueryKnowledgeEntityArgs>(
+    getKnowledgeEntity,
+    {
+      /** @todo reconsider caching. This is done for testing/demo purposes. */
+      fetchPolicy: "no-cache",
+    },
+  );
+
+  const getEntity = useCallback<GetEntityMessageCallback>(
+    async ({ data }) => {
+      if (!data) {
+        return {
+          errors: [
+            {
+              code: "INVALID_INPUT",
+              message: "'data' must be provided for getEntity",
+            },
+          ],
+        };
+      }
+
+      const { entityId } = data;
+
+      const { data: entityResponseData } = await getEntityFn({
+        query: getKnowledgeEntity,
+        variables: { entityId },
+      });
+
+      if (!entityResponseData) {
+        return {
+          errors: [
+            {
+              code: "INVALID_INPUT",
+              message: "Error calling getEntity",
+            },
+          ],
+        };
+      }
+
+      const { knowledgeEntity } = entityResponseData;
+
+      const { data: entityTypeResponseData } =
+        await getEntityTypeRootedSubgraphFn({
+          query: getEntityTypeRootedSubgraphQuery,
+          variables: {
+            entityTypeId: knowledgeEntity.entityTypeId,
+          },
+        });
+
+      if (!entityTypeResponseData) {
+        return {
+          errors: [
+            {
+              code: "INVALID_INPUT",
+              message:
+                "Error getting the entity type rooted sub graph of the entity",
+            },
+          ],
+        };
+      }
+
+      return {
+        data: {
+          ...knowledgeEntity,
+          entityTypeRootedSubgraph: entityTypeResponseData.getEntityType,
+        },
+      };
+    },
+    [getEntityFn, getEntityTypeRootedSubgraphFn],
+  );
+
+  return { getEntity };
+};
