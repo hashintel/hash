b0caacc28f0c3e87a071a31f970caa08f01d676d
Ben Werner <ben.werner01@gmail.com>
2022-10-05 16:33:36 +0100

    fix: spacing between consecutive headings

--- sites/hashdev/src/components/MdxPageContent.tsx
+++ sites/hashdev/src/components/MdxPageContent.tsx
@@ -1,4 +1,5 @@
 // @todo update from blockprotocol
+import { Box } from "@mui/material";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { FunctionComponent } from "react";
 import { mdxComponents } from "../util/mdxComponents";
@@ -10,6 +11,27 @@ type MdxPageContentProps = {
 export const MdxPageContent: FunctionComponent<MdxPageContentProps> = ({
   serializedPage,
 }) => {
-  // @ts-expect-error @todo fix this
-  return <MDXRemote {...serializedPage} components={mdxComponents} />;
+  return (
+    <Box
+      sx={{
+        /** Headers that come after headers shouldn't have a top margin */
+        [`& ${[
+          ".MuiTypography-hashHeading2 + .MuiTypography-hashHeading3",
+          ".MuiTypography-hashHeading2 + .MuiTypography-hashHeading4",
+          ".MuiTypography-hashHeading2 + .MuiTypography-hashHeading5",
+          ".MuiTypography-hashHeading3 + .MuiTypography-hashHeading4",
+          ".MuiTypography-hashHeading3 + .MuiTypography-hashHeading5",
+          ".MuiTypography-hashHeading4 + .MuiTypography-hashHeading5",
+        ]}`]: {
+          marginTop: 0,
+        },
+        "& > h1:first-of-type": {
+          marginTop: 0,
+        },
+      }}
+    >
+      {/* @ts-expect-error @todo fix this */}
+      <MDXRemote {...serializedPage} components={mdxComponents} />
+    </Box>
+  );
 };
