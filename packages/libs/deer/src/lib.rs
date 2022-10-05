bb95c166353807767082715d378f75bad27392b7
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-10-05 15:32:03 +0200

    feat: apply suggestions from code review

--- packages/libs/deer/src/lib.rs
+++ packages/libs/deer/src/lib.rs
@@ -1,15 +1,9 @@
 //! Intentionally left blank for now!
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![warn(
-    missing_docs,
-    unreachable_pub,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::undocumented_unsafe_blocks
-)]
+#![warn(missing_docs, unreachable_pub, clippy::pedantic, clippy::nursery)]
 #![allow(clippy::redundant_pub_crate)]
 #![allow(clippy::module_name_repetitions)]
+#![forbid(unsafe_code)]
 
 extern crate alloc;
-extern crate core;
