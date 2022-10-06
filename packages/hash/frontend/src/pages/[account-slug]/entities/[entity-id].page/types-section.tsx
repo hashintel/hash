9583d37a40c08fcd5a108de7bb5463dc080afb35
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-10-06 15:11:30 +0300

    fix style issue on type card

--- packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/types-section.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/types-section.tsx
@@ -1,28 +1,27 @@
 import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@hashintel/hash-design-system/fontawesome-icon";
-import { Box, Typography } from "@mui/material";
-import { WhiteCard } from "../../entity-types/white-card";
+import { Box, Paper, Typography } from "@mui/material";
 import { EntitySection } from "./shared/entity-section";
 
 export const TypesSection = () => {
   return (
     <EntitySection title="Type">
       <Box display="flex">
-        <WhiteCard>
-          <Box
-            display="flex"
-            alignItems="center"
-            px={1.5}
-            py={1.25}
-            gap={1.25}
-            color={({ palette }) => palette.black}
-          >
-            <FontAwesomeIcon icon={faAsterisk} />
-            <Typography variant="smallTextLabels" fontWeight={600}>
-              Company
-            </Typography>
-          </Box>
-        </WhiteCard>
+        <Paper
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            px: 1.5,
+            py: 1.25,
+            gap: 1.25,
+            color: ({ palette }) => palette.black,
+          }}
+        >
+          <FontAwesomeIcon icon={faAsterisk} />
+          <Typography variant="smallTextLabels" fontWeight={600}>
+            Company
+          </Typography>
+        </Paper>
       </Box>
     </EntitySection>
   );
