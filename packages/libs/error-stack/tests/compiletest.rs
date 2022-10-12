23419ee5b043d0c70bd83f810f4bd2a447149323
Tim Diekmann <tim.diekmann@3dvision.de>
2022-10-12 08:20:22 +0200

    Remove comment

--- packages/libs/error-stack/tests/compiletest.rs
+++ packages/libs/error-stack/tests/compiletest.rs
@@ -1,5 +1,3 @@
-//! Currently disabled because of https://github.com/dtolnay/trybuild/issues/171
-
 #[cfg_attr(not(nightly), ignore = "Outputs are different across toolchains")]
 #[cfg_attr(miri, ignore = "Miri does not support UI tests")]
 #[test]
