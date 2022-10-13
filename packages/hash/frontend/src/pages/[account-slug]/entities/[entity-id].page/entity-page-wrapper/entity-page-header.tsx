3bba4fe73be8f653ad99ecefffb41b3fd33412c2
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-10-13 10:04:09 +0300

    fix lint issue

--- packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-page-wrapper/entity-page-header.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-page-wrapper/entity-page-header.tsx
@@ -6,8 +6,8 @@ import { useRouter } from "next/router";
 import slugify from "slugify";
 import { generateEntityLabel } from "../../../../../lib/entities";
 import { TopContextBar } from "../../../../shared/top-context-bar";
+import { HashOntologyIcon } from "../../../types/entity-type/hash-ontology-icon";
 import { OntologyChip } from "../../../types/entity-type/ontology-chip";
-import { PlaceholderIcon } from "../../../types/entity-type/placeholder-icon";
 import { useEntityEditor } from "../entity-editor-context";
 
 export const EntityPageHeader = () => {
@@ -48,7 +48,7 @@ export const EntityPageHeader = () => {
       <Box py={3.75}>
         <Container>
           <OntologyChip
-            icon={<PlaceholderIcon />}
+            icon={<HashOntologyIcon />}
             domain="hash.ai"
             path={
               <>
