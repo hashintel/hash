e5e25825a21af3e7b4e7a8eb1a20298721e179de
Ben Werner <ben.werner01@gmail.com>
2022-10-14 14:33:27 +0100

    fix: allow the frontend domain to be overridden in the browser

--- packages/hash/shared/src/config.ts
+++ packages/hash/shared/src/config.ts
@@ -1,4 +1,7 @@
-export const frontendDomain = process.env.FRONTEND_DOMAIN ?? "localhost:3000";
+export const frontendDomain =
+  process.env.FRONTEND_DOMAIN ??
+  process.env.NEXT_PUBLIC_FRONTEND_URL ??
+  "localhost:3000";
 
 /** @todo: figure out how to properly determine the base URL of workspace types */
 export const frontendUrl = `http${
