5a17bb600db7c64b80fb15437e4885321a7ecdd3
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-10-18 14:01:49 +0100

    Change mind and delete `Edges::inner` function

--- packages/graph/hash_graph/lib/graph/src/store/postgres/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/mod.rs
@@ -184,15 +184,6 @@ impl Edges {
         Self(HashMap::new())
     }
 
-    #[expect(
-        dead_code,
-        reason = "It's going to be a helpful function, saving someone from needing to write it"
-    )]
-    #[must_use]
-    pub fn inner(&self) -> &HashMap<GraphElementIdentifier, HashSet<OutwardEdge>> {
-        &self.0
-    }
-
     pub fn insert(&mut self, identifier: GraphElementIdentifier, edge: OutwardEdge) -> bool {
         match self.0.raw_entry_mut().from_key(&identifier) {
             RawEntryMut::Vacant(entry) => {
