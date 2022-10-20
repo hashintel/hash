b5a24e5cc46932bccabaffbd51bc28f264228729
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-20 18:19:29 +0100

    Add `@sentry/replay`

--- packages/hash/frontend/sentry.client.config.ts
+++ packages/hash/frontend/sentry.client.config.ts
@@ -2,19 +2,36 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { Replay } from "@sentry/replay";
 
 const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-export const SENTRY_CONFIG = {
+const replaysSamplingRate = Number.parseInt(
+  process.env.NEXT_PUBLIC_SENTRY_REPLAYS_SAMPLING_RATE ?? "0",
+  10,
+);
+
+Sentry.init({
   dsn,
   enabled: !!dsn,
   environment:
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === "main"
       ? "production"
       : "development",
+  integrations:
+    replaysSamplingRate > 0 &&
+    replaysSamplingRate <= 1 &&
+    // @todo Remove when https://github.com/getsentry/sentry-replay/issues/246#issuecomment-1284472286 is resolved
+    typeof window !== "undefined"
+      ? [
+          new Replay({
+            captureOnlyOnError: true,
+            replaysSamplingRate,
+            stickySession: true,
+          }),
+        ]
+      : [],
   // release is set in next.config.js in the Sentry webpack plugin
   /** @todo reduce perf sample rate from 100% when we have more traffic */
   tracesSampleRate: 1.0,
-};
-
-Sentry.init(SENTRY_CONFIG);
+});
