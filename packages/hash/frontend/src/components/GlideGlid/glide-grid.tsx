230256e711c235919d6050c0f9b5fa3be350768a
Yusuf Kınataş <yusufkinatas@gmail.com>
2022-10-20 12:18:10 +0300

    Improve entity editor - 1 (#1207)
    * remove old pages
    
    * create entity view pages & related components WIP
    
    * redirect old entity link to overview tab
    
    * implement properties section - WIP
    
    * remove unused parts for MVP release
    
    * refactor folder structure
    
    * improve design
    
    * implement glide - wip
    
    * add missing dependency
    
    * improve glide implementation
    
    * fix style issue on type card
    
    * improve glide implementation
    
    * remove unused code
    
    * minor style change
    
    * implement BE for property editor - wip
    
    * implement entity editor context
    
    * use generateEntityLabel
    
    * revert using `convertApiEntityToBpEntity`
    
    * use account slug in OntologyChip
    
    * update yarn.lock
    
    * implement update entity
    
    * fix imports & add link to type card
    
    * define max-height for property table
    
    * Update generateEntityLabel to work with new API
    
    * remove unused components
    
    * fix lint issues
    
    * slugify ontology chip entity label
    
    * update property table
    
    * improve property table type support
    
    * update yarn.lock
    
    * create TypeCard component
    
    * clean up
    
    * move shared components into /shared folder
    
    * Revert "move shared components into /shared folder"
    
    This reverts commit 56c2c6daf668dea5dced3854e35c4de3711f3ae0.
    
    * fix lint issue
    
    * Add & use `useSnackbar` hook
    
    * Split `property-table.tsx` into multiple files
    
    * implement table sorting
    
    * refactor handleHeaderClicked
    
    * create GlideGrid for default theming & behavior
    
    * update yarn.lock
    
    * refactor glide grid utils
    
    * implement links table
    
    * refactor glide related utils & types
    
    * cleanup code
    
    * add missing error messages
    
    * memoize sorted row data

---
+++ packages/hash/frontend/src/components/GlideGlid/glide-grid.tsx
@@ -0,0 +1,44 @@
+import { DataEditor, DataEditorProps, Theme } from "@glideapps/glide-data-grid";
+import { useTheme } from "@mui/material";
+import { useMemo } from "react";
+
+export const GlideGrid = (props: DataEditorProps) => {
+  const { palette } = useTheme();
+
+  const gridTheme: Partial<Theme> = useMemo(
+    () => ({
+      bgHeader: "white",
+      borderColor: palette.gray[20],
+      headerBottomBorderColor: palette.gray[20],
+      horizontalBorderColor: "transparent",
+      accentColor: palette.blue[70],
+      textHeader: palette.gray[80],
+      bgHeaderHasFocus: "transparent",
+      textBubble: palette.gray[70],
+      bgBubble: palette.gray[20],
+      accentLight: palette.gray[20],
+      bgHeaderHovered: "white",
+      cellHorizontalPadding: 22,
+      baseFontStyle: "500 14px",
+      headerFontStyle: "600 14px",
+      editorFontSize: "14px",
+    }),
+    [palette],
+  );
+
+  return (
+    <DataEditor
+      theme={gridTheme}
+      width="100%"
+      headerHeight={42}
+      rowHeight={42}
+      drawFocusRing={false}
+      rangeSelect="cell"
+      columnSelect="none"
+      smoothScrollX
+      smoothScrollY
+      getCellsForSelection
+      {...props}
+    />
+  );
+};
