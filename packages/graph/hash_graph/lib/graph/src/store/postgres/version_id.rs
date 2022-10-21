77e98e0e5cfe9620b14fa06671f35a76051cfa30
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-10-21 16:16:02 +0100

    Add TODOs

--- packages/graph/hash_graph/lib/graph/src/store/postgres/version_id.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/version_id.rs
@@ -7,6 +7,7 @@ use uuid::Uuid;
 
 // TODO: rename this to TypeInternalVersionId or something to distinguish it from versioned
 //  URIs and from entity ids
+//  https://app.asana.com/0/1202805690238892/1203214689883089/f
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize, ToSchema, FromSql, ToSql)]
 #[repr(transparent)]
 #[postgres(transparent)]
