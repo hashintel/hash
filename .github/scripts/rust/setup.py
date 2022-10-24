5a6d5b650413aeb78f51f524507c8cad1713a8f8
Alexander Kachkaev <alexander@kachkaev.ru>
2022-10-24 12:07:01 +0100

    Fix 🐍

--- .github/scripts/rust/setup.py
+++ .github/scripts/rust/setup.py
@@ -206,7 +206,7 @@ def output_matrix(name, crates, **kwargs):
     if len(matrix["directory"]) == 0:
         matrix = {}
 
-    print(f"{name}={json.dumps(matrix)}", file="github_output.txt")
+    github_output_file.write(f"{name}={json.dumps(matrix)}")
     print(f"Job matrix for {name}: {json.dumps(matrix, indent=4)}")
 
 
@@ -217,16 +217,21 @@ def main():
     changed_parent_crates = filter_parent_crates(changed_crates)
     changed_docker_crates = filter_for_docker_crates(changed_parent_crates)
 
-    output_matrix("lint", changed_parent_crates)
-    output_matrix("test", changed_parent_crates, profile=["development", "production"])
-    output_matrix("docker", changed_docker_crates, profile=["production"])
+    github_output_file = open("github_output.txt", "w")
+
+    output_matrix("lint", github_output_file, changed_parent_crates)
+    output_matrix("test", github_output_file, changed_parent_crates, profile=["development", "production"])
+    output_matrix("docker", github_output_file, changed_docker_crates, profile=["production"])
     output_matrix(
         "publish",
+        github_output_file,
         filter_crates_by_changed_version(
             diffs, filter_for_publishable_crates(changed_crates)
         ),
         profile=["release"],
     )
+
+    github_output_file.close()
 
 
 if __name__ == "__main__":
