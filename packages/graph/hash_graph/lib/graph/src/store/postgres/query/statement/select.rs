6199d91847133128e20e0c41b4fd025ef9bd3d83
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-10-24 17:40:19 +0200

    Update packages/graph/hash_graph/lib/graph/src/store/postgres/query/statement/select.rs
    Co-authored-by: Ahmad Sattar <thehabbos007@gmail.com>

--- packages/graph/hash_graph/lib/graph/src/store/postgres/query/statement/select.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/query/statement/select.rs
@@ -361,7 +361,8 @@ mod tests {
             FROM "entity_types"
             JOIN "entity_type_link_type_references" AS "entity_type_link_type_references_0_0"
               ON "entity_type_link_type_references_0_0"."source_entity_type_version_id" = "entity_types"."version_id"
-            JOIN "link_types" AS "link_types_0_1" ON "link_types_0_1"."version_id" = "entity_type_link_type_references_0_0"."target_link_type_version_id"
+            JOIN "link_types" AS "link_types_0_1" 
+            ON "link_types_0_1"."version_id" = "entity_type_link_type_references_0_0"."target_link_type_version_id"
             WHERE "link_types_0_1"."schema"->>'title' = $1
             "#,
             &[&"Friend Of"],
