97c416e091289ed7ac7ed1580dd04cca58fc472f
Ahmad Sattar <thehabbos007@gmail.com>
2022-10-24 16:25:10 +0200

    Remove `seedUserContent`

--- packages/hash/api/src/seed-data/index.ts
+++ packages/hash/api/src/seed-data/index.ts
@@ -1,60 +1,9 @@
 import { Logger } from "@hashintel/hash-backend-utils/logger";
 import { GraphApi } from "@hashintel/hash-graph-client";
-import { OrgModel, OrgSize, UserModel } from "../model";
+import { OrgModel, OrgSize } from "../model";
 import { workspaceAccountId } from "../model/util";
 import { ensureDevUsersAreSeeded } from "./dev-users";
 import { PageDefinition, seedPages } from "./seed-pages";
-
-const seedUserContent = async (
-  user: UserModel,
-  sharedOrg: OrgModel,
-  params: {
-    graphApi: GraphApi;
-    logger: Logger;
-  },
-): Promise<void> => {
-  const { graphApi, logger } = params;
-
-  await user.joinOrg(graphApi, {
-    org: sharedOrg,
-    responsibility: "Member",
-    actorId: workspaceAccountId,
-  });
-
-  logger.info(
-    `User with shortname = "${user.getShortname()}" joined org with shortname = '${sharedOrg.getShortname()}'`,
-  );
-
-  const pageTitles: PageDefinition[] = [
-    {
-      title: "First",
-      nestedPages: [
-        {
-          title: "Middle",
-          nestedPages: [
-            {
-              title: "Leaf",
-              nestedPages: [],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      title: "Second",
-      nestedPages: [],
-    },
-    {
-      title: "Third",
-      nestedPages: [],
-    },
-  ];
-
-  await seedPages(pageTitles, user.entityId, params);
-  logger.info(
-    `Development User with shortname = "${user.getShortname()}" now has seeded pages.`,
-  );
-};
 
 // Seed Org with some pages.
 const seedOrg = async (params: {
@@ -104,12 +53,52 @@ export const seedOrgsAndUsers = async (params: {
   graphApi: GraphApi;
   logger: Logger;
 }): Promise<void> => {
+  const { graphApi, logger } = params;
+
   const createdUsers = await ensureDevUsersAreSeeded(params);
   if (createdUsers.length > 0) {
     const sharedOrgModel = await seedOrg(params);
 
     for (const user of createdUsers) {
-      await seedUserContent(user, sharedOrgModel, params);
+      await user.joinOrg(graphApi, {
+        org: sharedOrgModel,
+        responsibility: "Member",
+        actorId: workspaceAccountId,
+      });
+
+      logger.info(
+        `User with shortname = "${user.getShortname()}" joined org with shortname = '${sharedOrgModel.getShortname()}'`,
+      );
+
+      const pageTitles: PageDefinition[] = [
+        {
+          title: "First",
+          nestedPages: [
+            {
+              title: "Middle",
+              nestedPages: [
+                {
+                  title: "Leaf",
+                  nestedPages: [],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: "Second",
+          nestedPages: [],
+        },
+        {
+          title: "Third",
+          nestedPages: [],
+        },
+      ];
+
+      await seedPages(pageTitles, user.entityId, params);
+      logger.info(
+        `Development User with shortname = "${user.getShortname()}" now has seeded pages.`,
+      );
     }
   }
 };
