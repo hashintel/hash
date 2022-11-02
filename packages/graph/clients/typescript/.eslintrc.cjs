1672bcaeacc2d6e050655d799233bb1d372440a5
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-02 17:39:02 +0000

    Align ESLint scripts in `package.json`

--- packages/graph/clients/typescript/.eslintrc.cjs
+++ packages/graph/clients/typescript/.eslintrc.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   root: true,
   extends: ["@local/eslint-config/legacy-base-to-refactor.cjs"],
+  noInlineConfig: true,
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["tsconfig.json"],
