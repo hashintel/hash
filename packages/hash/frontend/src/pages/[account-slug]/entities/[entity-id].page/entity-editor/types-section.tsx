f05ef41c689ca3461fa89b93c30dc422659821de
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-11-07 12:07:25 +0300

    move `white-card` to `shared` folder

--- packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-editor/types-section.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-editor/types-section.tsx
@@ -2,7 +2,7 @@ import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@hashintel/hash-design-system/fontawesome-icon";
 import { Box, Typography } from "@mui/material";
 import { getPersistedEntityType } from "../../../../../lib/subgraph";
-import { WhiteCard } from "../../../types/entity-type/white-card";
+import { WhiteCard } from "../../../shared/white-card";
 import { useEntityEditor } from "./entity-editor-context";
 import { EntitySection } from "./shared/entity-section";
 
