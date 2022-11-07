fb544971e6f3d30aed061d1f56749dc527707d75
Ciaran Morinan <c@hash.ai>
2022-11-07 18:10:48 +0000

    fix: prettier

--- packages/hash/frontend/src/blocks/page/PageSectionContainer.tsx
+++ packages/hash/frontend/src/blocks/page/PageSectionContainer.tsx
@@ -7,8 +7,14 @@ export const PAGE_CONTENT_WIDTH = 696;
 export const PAGE_MIN_PADDING = 48;
 export const COMMENTS_WIDTH = 320;
 
-export const getPageSectionContainerStyles = (pageComments: PageThread[], readonlyMode?: boolean) => {
-  const commentsContainerWidth = !readonlyMode && pageComments?.length ? COMMENTS_WIDTH + PAGE_MIN_PADDING : 0;
+export const getPageSectionContainerStyles = (
+  pageComments: PageThread[],
+  readonlyMode?: boolean,
+) => {
+  const commentsContainerWidth =
+    !readonlyMode && pageComments?.length
+      ? COMMENTS_WIDTH + PAGE_MIN_PADDING
+      : 0;
 
   const paddingLeft = `max(calc((100% - ${
     PAGE_CONTENT_WIDTH + commentsContainerWidth
