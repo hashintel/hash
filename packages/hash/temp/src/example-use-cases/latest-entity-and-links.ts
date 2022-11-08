c8df0cd228783eb8b14be9f4a5a05d263f3473f1
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-08 19:57:42 +0000

    Add example of getting an entity tree as a linked list

---
+++ packages/hash/temp/src/example-use-cases/latest-entity-and-links.ts
@@ -0,0 +1,47 @@
+import { Entity, EntityId, Subgraph } from "@hashintel/subgraph/src/types";
+import { getEntityAtTimestamp } from "@hashintel/subgraph/src/element/entity";
+import { getOutgoingLinksForEntityAtTime } from "@hashintel/subgraph/src/edge/link";
+
+type EntityTree = {
+  entity: Entity;
+  siblings: EntityTree[];
+};
+export const getEntityTreeAtTimeToDepth = (
+  subgraph: Subgraph,
+  entityId: EntityId,
+  timestamp: string,
+  depth: number,
+): EntityTree => {
+  const entity = getEntityAtTimestamp(subgraph, entityId, timestamp);
+
+  if (!entity) {
+    throw new Error(
+      `failed to find entity ${entityId} at timestamp ${timestamp}`,
+    );
+  }
+
+  const links = getOutgoingLinksForEntityAtTime(
+    subgraph,
+    entity.metadata.identifier.entityIdentifier,
+    timestamp,
+  );
+  return {
+    entity,
+    siblings: links.map(({ linkEntity, endpointEntity }) => {
+      return {
+        entity: linkEntity,
+        siblings:
+          depth > 1
+            ? [
+                getEntityTreeAtTimeToDepth(
+                  subgraph,
+                  endpointEntity.metadata.identifier.entityIdentifier,
+                  timestamp,
+                  depth - 1,
+                ),
+              ]
+            : [],
+      };
+    }),
+  };
+};
