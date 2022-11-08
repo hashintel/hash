c396a350f04082843cb943232d90d343a3b439d0
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-08 19:36:04 +0000

    Override `Edges` types

--- packages/hash/subgraph/src/types.ts
+++ packages/hash/subgraph/src/types.ts
@@ -23,26 +23,20 @@ export type EntityVersion = EntityEditionId["version"];
 
 export type GraphElementEditionId = EntityEditionId | VersionedUri;
 
-// ------------------------------ Subgraph ------------------------------
+export type EntityAndTimestamp = { entityId: EntityId; timestamp: string };
 
-// TODO: Utoipa can't handle flattens so the type is wrong
-// TODO: We'd quite like to use `VersionedUri` in the type but Utoipa can't affirm that
+// ------------------------------ Elements ------------------------------
 
-export type Subgraph<
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  RootType extends GraphElement = GraphElement,
-> = Omit<GraphApiSubgraph, "vertices" | "roots"> & {
-  roots: GraphElementEditionId[];
-  vertices: {
-    [_: BaseUri]: {
-      [_: number]: OntologyVertex;
-    };
-  } & {
-    [_: EntityId]: {
-      [_: EntityVersion]: KnowledgeGraphVertex;
-    };
-  };
-};
+export type DataTypeWithMetadata = GraphApiPersistedDataType;
+export type PropertyTypeWithMetadata = GraphApiPersistedPropertyType;
+export type EntityTypeWithMetadata = GraphApiPersistedEntityType;
+export type Entity = GraphApiPersistedEntity;
+
+export type GraphElement =
+  | DataTypeWithMetadata
+  | PropertyTypeWithMetadata
+  | EntityTypeWithMetadata
+  | Entity;
 
 // ------------------------------ Vertices ------------------------------
 
@@ -74,15 +68,58 @@ export type KnowledgeGraphVertex = EntityVertex;
 
 export type Vertex = OntologyVertex | KnowledgeGraphVertex;
 
-// ------------------------------ Elements ------------------------------
+// ------------------------------ Edges ------------------------------
 
-export type DataTypeWithMetadata = GraphApiPersistedDataType;
-export type PropertyTypeWithMetadata = GraphApiPersistedPropertyType;
-export type EntityTypeWithMetadata = GraphApiPersistedEntityType;
-export type Entity = GraphApiPersistedEntity;
+type OntologyEdgeKind =
+  | "INHERITS_FROM"
+  | "CONSTRAINS_VALUES_ON"
+  | "CONSTRAINS_PROPERTIES_ON"
+  | "CONSTRAINS_LINKS_ON"
+  | "CONSTRAINS_LINK_DESTINATIONS_ON";
+type KnowledgeGraphEdgeKind = "HAS_LINK" | "HAS_ENDPOINT";
+type SharedEdgeKind = "IS_OF_TYPE";
 
-export type GraphElement =
-  | DataTypeWithMetadata
-  | PropertyTypeWithMetadata
-  | EntityTypeWithMetadata
-  | Entity;
+type GenericOutwardEdge<K, E> = {
+  kind: K;
+  reversed: boolean;
+  endpoint: E;
+};
+
+export type OntologyOutwardEdge =
+  | GenericOutwardEdge<OntologyEdgeKind, VersionedUri>
+  | GenericOutwardEdge<SharedEdgeKind, EntityEditionId>;
+export type KnowledgeGraphOutwardEdge =
+  | GenericOutwardEdge<KnowledgeGraphEdgeKind, EntityAndTimestamp>
+  | GenericOutwardEdge<SharedEdgeKind, VersionedUri>;
+
+export type OutwardEdge = OntologyOutwardEdge | KnowledgeGraphOutwardEdge;
+
+// ------------------------------ Subgraph ------------------------------
+
+// TODO: Utoipa can't handle flattens so the type is wrong
+// TODO: We'd quite like to use `VersionedUri` in the type but Utoipa can't affirm that
+// TODO: `edges` is completely buggered
+export type Subgraph<
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  RootType extends GraphElement = GraphElement,
+> = Omit<GraphApiSubgraph, "vertices" | "roots" | "edges"> & {
+  roots: GraphElementEditionId[];
+  vertices: {
+    [_: BaseUri]: {
+      [_: number]: OntologyVertex;
+    };
+  } & {
+    [_: EntityId]: {
+      [_: EntityVersion]: KnowledgeGraphVertex;
+    };
+  };
+  edges: {
+    [_: BaseUri]: {
+      [_: number]: OntologyOutwardEdge[];
+    };
+  } & {
+    [_: EntityId]: {
+      [_: EntityVersion]: KnowledgeGraphOutwardEdge[];
+    };
+  };
+};
