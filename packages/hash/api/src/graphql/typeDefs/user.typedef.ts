55d1c963425fc76a87544792ba270468ad74b649
Ben Werner <ben.werner01@gmail.com>
2022-11-08 14:58:42 +0000

    chore: add GQL documentation

--- packages/hash/api/src/graphql/typeDefs/user.typedef.ts
+++ packages/hash/api/src/graphql/typeDefs/user.typedef.ts
@@ -10,18 +10,48 @@ export const userTypedef = gql`
   }
 
   input UserEmailInput {
+    """
+    An email address (e.g. "alice@example.com").
+    """
     address: String!
+    """
+    Whether or not the email address is verified.
+    """
     verified: Boolean!
   }
 
   extend type Mutation {
+    """
+    Create a User.
+    """
     createUser(
+      """
+      The email of the user.
+      """
       email: UserEmailInput!
+      """
+      The password of the user.
+      """
       password: String!
+      """
+      Whether or not the user is a HASH instance admin.
+      """
       isInstanceAdmin: Boolean!
+      """
+      The shortname of the user.
+      """
       shortname: String
+      """
+      The preferred name of the user.
+      """
       preferredName: String
+      """
+      The depth of links that are returned in the response subgraph.
+      """
       linkResolveDepth: Int!
+      """
+      The depth of link target entities that are returned in the response subgraph.
+      """
       linkTargetEntityResolveDepth: Int!
     ): Subgraph!
   }
