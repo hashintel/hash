0379307c10c44e33dabd173dc629ed56b3b16bd7
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-11 19:23:11 +0000

    table block

--- blocks/table/src/lib/omit-typename-deep.ts
+++ blocks/table/src/lib/omit-typename-deep.ts
@@ -1,10 +1,10 @@
 import { cloneDeepWith } from "lodash";
 
 export function omitTypenameDeep<T>(data: T): T {
-  return cloneDeepWith(data, (value) => {
+  return cloneDeepWith(data, (value: Record<string, unknown>) => {
     if (value?.__typename) {
       const { __typename, ...valueWithoutTypename } = value;
-      return omitTypenameDeep<T>(valueWithoutTypename);
+      return omitTypenameDeep<T>(valueWithoutTypename as T);
     }
-  });
+  }) as T;
 }
