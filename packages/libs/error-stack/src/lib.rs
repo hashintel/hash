8d741d96c72867455e94d67c4c81f234dd2b242f
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-11-11 16:39:24 +0100

    fix: clippy

--- packages/libs/error-stack/src/lib.rs
+++ packages/libs/error-stack/src/lib.rs
@@ -450,6 +450,7 @@
     clippy::alloc_instead_of_core,
     clippy::std_instead_of_alloc,
     clippy::std_instead_of_core,
+    clippy::if_then_some_else_none,
     clippy::print_stdout,
     clippy::print_stderr
 )]
