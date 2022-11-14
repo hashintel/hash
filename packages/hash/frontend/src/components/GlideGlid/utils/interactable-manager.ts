4229c9ce88d2f1827a03d877b631964f03973a46
Yusuf Kinatas <yusufkinatas@gmail.com>
2022-11-14 12:10:54 +0300

    add comment lines

--- packages/hash/frontend/src/components/GlideGlid/utils/interactable-manager.ts
+++ packages/hash/frontend/src/components/GlideGlid/utils/interactable-manager.ts
@@ -13,29 +13,47 @@ import {
 class InteractableManagerClass {
   static instance: InteractableManagerClass | null = null;
 
+  /**
+   * @example A cell with single interactable element
+   * ```ts
+   * const interactableStore = {
+   *  "users-1-2": { button1: { ...interactable } },
+   * };
+   * ```
+   */
   private interactableStore: Record<CellPath, Record<string, Interactable>> =
     {};
 
+  /**
+   * @param args Draw args of cell
+   * @param props Properties which will will be used to create the `Interactable`
+   * @returns The created `Interactable`
+   */
   create(
     args: DrawArgs<CustomCell>,
-    interactable: Omit<Interactable, "hovered" | "path" | "cellRect">,
+    props: Omit<Interactable, "hovered" | "path" | "cellRect">,
   ): Interactable {
     const { hoverX = -100, hoverY = -100, rect } = args;
 
     const hovered = isCursorOnInteractable(
       { posX: hoverX, posY: hoverY },
-      interactable.pos,
+      props.pos,
       rect,
     );
 
     return {
-      ...interactable,
+      ...props,
       hovered,
       cellRect: rect,
       path: drawArgsToPath(args),
     };
   }
 
+  /**
+   * Call this function at the end of the `draw` function of the custom cell to register the interactables.
+   * @param args Draw args of cell
+   * @param interactables List of the interactables for a specific cell.
+   */
   setInteractablesForCell(
     args: DrawArgs<CustomCell>,
     interactables: Interactable[],
@@ -66,6 +84,8 @@ class InteractableManagerClass {
   }
 
   /**
+   * Checks if the cursor position is overlapping with an `Interactable` inside clicked cell.
+   * If it's overlapping, triggers the corresponding event handler for the clicked `Interactable`
    * @returns true if handled the click event, false if not
    */
   handleClick(path: CellPath, event: CursorPos): boolean {
