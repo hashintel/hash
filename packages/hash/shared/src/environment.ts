a9bd08483ae1e9522d65af95062bd515ba4a2f39
Ahmad Sattar <thehabbos007@gmail.com>
2022-11-14 17:45:11 +0100

    Simplify ApiOrigin and frontendUrl

--- packages/hash/shared/src/environment.ts
+++ packages/hash/shared/src/environment.ts
@@ -1,18 +1,14 @@
-export const apiOrigin = process.env.NEXT_PUBLIC_API_ORIGIN
-  ? process.env.NEXT_PUBLIC_API_ORIGIN
-  : process.env.API_ORIGIN
-  ? process.env.API_ORIGIN
-  : "http://localhost:5001";
+export const apiOrigin =
+  process.env.NEXT_PUBLIC_API_ORIGIN ??
+  process.env.API_ORIGIN ??
+  "http://localhost:5001";
 
 export const apiGraphQLEndpoint = `${apiOrigin}/graphql`;
 
-export const frontendUrl = process.env.NEXT_PUBLIC_FRONTEND_URL
-  ? process.env.NEXT_PUBLIC_FRONTEND_URL
-  : process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL
-  : "http://localhost:3000";
+export const frontendUrl =
+  process.env.NEXT_PUBLIC_FRONTEND_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : process.env.FRONTEND_URL ?? "http://localhost:3000";
 
 export const frontendDomain = new URL(frontendUrl).hostname;
 
