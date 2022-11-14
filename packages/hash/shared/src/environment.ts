20fb2af7b793adf68d325462eed0a8d626def358
Ahmad Sattar <thehabbos007@gmail.com>
2022-11-14 18:32:22 +0100

    Fix `frontendUrl` default chain

--- packages/hash/shared/src/environment.ts
+++ packages/hash/shared/src/environment.ts
@@ -6,9 +6,10 @@ export const apiOrigin =
 export const apiGraphQLEndpoint = `${apiOrigin}/graphql`;
 
 export const frontendUrl =
-  process.env.NEXT_PUBLIC_FRONTEND_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+  process.env.NEXT_PUBLIC_FRONTEND_URL ??
+  (process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : process.env.FRONTEND_URL ?? "http://localhost:3000";
+    : process.env.FRONTEND_URL ?? "http://localhost:3000");
 
 export const frontendDomain = new URL(frontendUrl).hostname;
 
