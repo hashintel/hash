ff4a82c3dca575e805d14553f992f0917fc19b84
Tim Diekmann <tim.diekmann@3dvision.de>
2022-11-14 17:52:45 +0100

    Fix doc links

--- packages/graph/hash_graph/lib/graph/src/shared/subgraph/edges/kind.rs
+++ packages/graph/hash_graph/lib/graph/src/shared/subgraph/edges/kind.rs
@@ -4,35 +4,53 @@ use utoipa::ToSchema;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, ToSchema)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum OntologyEdgeKind {
-    /// An [`OntologyType`] can inherit from another [`OntologyType`]
+    /// An ontology type can inherit from another ontology type.
     InheritsFrom,
-    /// A [`PropertyType`] or [`DataType`] can reference a [`DataType`] to constrain values
+    /// A [`PropertyType`] or [`DataType`] can reference a [`DataType`] to constrain values.
+    ///
+    /// [`DataType`]: type_system::DataType
+    /// [`PropertyType`]: type_system::PropertyType
     ConstrainsValuesOn,
     /// An [`EntityType`] or [`PropertyType`] can reference a [`PropertyType`] to constrain
-    /// properties
+    /// properties.
+    ///
+    /// [`PropertyType`]: type_system::PropertyType
+    /// [`EntityType`]: type_system::EntityType
     ConstrainsPropertiesOn,
     /// An [`EntityType`] can reference a link [`EntityType`] to constrain the existence of
-    /// certain kinds of links
+    /// certain kinds of links.
+    ///
+    /// [`EntityType`]: type_system::EntityType
     ConstrainsLinksOn,
     /// An [`EntityType`] can reference an [`EntityType`] to constrain the target entities of
-    /// certain kinds of links
+    /// certain kinds of links.
+    ///
+    /// [`EntityType`]: type_system::EntityType
     ConstrainsLinkDestinationsOn,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, ToSchema)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum KnowledgeGraphEdgeKind {
-    /// This link [`Entity`] has another [`Entity`] on its 'left' endpoint
+    /// This link [`Entity`] has another [`Entity`] on its 'left' endpoint.
+    ///
     /// The `reverse` of this would be the equivalent of saying an [`Entity`] has an outgoing
-    /// `Link` [`Entity`]
+    /// `Link` [`Entity`].
+    ///
+    /// [`Entity`]: crate::knowledge::Entity
     HasLeftEndpoint,
-    /// This link [`Entity`] has another [`Entity`] on its 'right' endpoint
+    /// This link [`Entity`] has another [`Entity`] on its 'right' endpoint.
+    ///
+    /// [`Entity`]: crate::knowledge::Entity
     HasRightEndpoint,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, ToSchema)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum SharedEdgeKind {
-    /// An [`Entity`] is of an [`EntityType`]
+    /// An [`Entity`] is of an [`EntityType`].
+    ///
+    /// [`Entity`]: crate::knowledge::Entity
+    /// [`EntityType`]: type_system::EntityType
     IsOfType,
 }
