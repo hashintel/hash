3f8005348776dc7762c11b158e0f978a00a6b2a2
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-11-14 13:16:43 +0100

    fix: loom errors

--- packages/libs/deer/append-vec/src/lock.rs
+++ packages/libs/deer/append-vec/src/lock.rs
@@ -3,10 +3,9 @@
 //! Most efficient when contention is low, acquiring the lock is a single atomic swap, and releasing
 //! is just one more atomic swap.
 //!
-//! This is heavily inspired from the excellent [try-lock](https://crates.io/crates/try-lock)
-//! crate, but without requiring a value to be guarded.
+//! This is adapted from the function outlined in the [std](https://doc.rust-lang.org/std/sync/atomic/fn.fence.html) documentation
 
-use crate::sync::{spin_loop, AtomicBool, Ordering};
+use crate::sync::{fence, spin_loop, AtomicBool, Ordering};
 
 pub(crate) struct LockGuard<'a> {
     lock: &'a AtomicLock,
@@ -14,48 +13,85 @@ pub(crate) struct LockGuard<'a> {
 
 impl Drop for LockGuard<'_> {
     fn drop(&mut self) {
-        self.lock.locked.store(false, Ordering::Release);
+        self.lock.unlock();
     }
 }
 
+// A mutual exclusion primitive based on spinlock.
 pub(crate) struct AtomicLock {
-    locked: AtomicBool,
+    flag: AtomicBool,
 }
 
 impl AtomicLock {
-    #[cfg(not(loom))]
-    #[inline]
-    pub const fn new() -> Self {
-        Self {
-            locked: AtomicBool::new(false),
-        }
-    }
-
-    #[cfg(loom)]
-    #[inline]
     pub fn new() -> Self {
         Self {
-            locked: AtomicBool::new(false),
+            flag: AtomicBool::new(false),
         }
     }
 
-    #[inline]
     pub(crate) fn lock(&self) -> LockGuard {
-        loop {
-            if let Some(lock) = self.try_lock() {
-                return lock;
-            }
-
-            spin_loop()
+        // Wait until the old value is `false`.
+        while self
+            .flag
+            .compare_exchange_weak(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            .is_err()
+        {
+            spin_loop();
         }
+
+        // This fence synchronizes-with store in `unlock`.
+        fence(Ordering::Acquire);
+
+        LockGuard { lock: self }
     }
 
-    #[inline]
-    pub(crate) fn try_lock(&self) -> Option<LockGuard> {
-        if !self.locked.swap(true, Ordering::Acquire) {
-            Some(LockGuard { lock: self })
-        } else {
-            None
-        }
+    fn unlock(&self) {
+        self.flag.store(false, Ordering::Release);
     }
 }
+//
+// pub(crate) struct AtomicLock {
+//     locked: AtomicBool,
+// }
+//
+// impl AtomicLock {
+//     #[cfg(not(loom))]
+//     #[inline]
+//     pub const fn new() -> Self {
+//         Self {
+//             locked: AtomicBool::new(false),
+//         }
+//     }
+//
+//     #[cfg(loom)]
+//     #[inline]
+//     pub fn new() -> Self {
+//         Self {
+//             locked: AtomicBool::new(false),
+//         }
+//     }
+//
+//     #[inline]
+//     pub(crate) fn lock(&self) -> LockGuard {
+//         loop {
+//             if let Some(lock) = self.try_lock() {
+//                 fence(Ordering::Acquire);
+//
+//                 return lock;
+//             }
+//
+//             warn!("D:");
+//
+//             spin_loop();
+//         }
+//     }
+//
+//     #[inline]
+//     pub(crate) fn try_lock(&self) -> Option<LockGuard> {
+//         if !self.locked.swap(true, Ordering::Relaxed) {
+//             Some(LockGuard { lock: self })
+//         } else {
+//             None
+//         }
+//     }
+// }
