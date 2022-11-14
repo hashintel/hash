64790b4678ffb54576c05b9bfd250ebe44bee682
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-11-14 11:16:40 +0100

    test: append-only

--- packages/libs/deer/append-vec/src/lib.rs
+++ packages/libs/deer/append-vec/src/lib.rs
@@ -3,24 +3,42 @@
 extern crate alloc;
 
 use alloc::boxed::Box;
-use core::mem::MaybeUninit;
+use core::mem::{ManuallyDrop, MaybeUninit};
 
 use crate::{
     lock::AtomicLock,
-    sync::{alloc, AtomicUsize, Layout, Ordering, UnsafeCell},
+    sync::{alloc, AtomicBool, AtomicUsize, Layout, Ordering, UnsafeCell},
 };
 
 mod lock;
 pub(crate) mod sync;
 
-pub struct AppendOnlyVec<T, const N: usize> {
+pub struct AppendOnlyVec<T, const N: usize = 16> {
     length: AtomicUsize,
     lock: AtomicLock,
 
     head: UnsafeCell<Node<T, N>>,
 }
 
+impl<T, const N: usize> Drop for AppendOnlyVec<T, N> {
+    fn drop(&mut self) {
+        let head = self.head.get_mut();
+        unsafe { head.uninit() };
+    }
+}
+
+unsafe impl<T: Send, const N: usize> Send for AppendOnlyVec<T, N> {}
+unsafe impl<T: Send + Sync, const N: usize> Sync for AppendOnlyVec<T, N> {}
+
 impl<T, const N: usize> AppendOnlyVec<T, N> {
+    pub fn new() -> Self {
+        Self {
+            length: AtomicUsize::new(0),
+            lock: AtomicLock::new(),
+            head: UnsafeCell::new(Node::new()),
+        }
+    }
+
     fn indices(&self, length: usize) -> (usize, usize) {
         (length / N, length % N)
     }
@@ -45,7 +63,8 @@ impl<T, const N: usize> AppendOnlyVec<T, N> {
 struct Node<T, const N: usize> {
     store: [MaybeUninit<T>; N],
 
-    next: Box<MaybeUninit<Node<T, N>>>,
+    has_next: AtomicBool,
+    next: ManuallyDrop<Box<MaybeUninit<Node<T, N>>>>,
 }
 
 fn uninit_box<T>() -> Box<MaybeUninit<T>> {
@@ -64,8 +83,17 @@ impl<T, const N: usize> Node<T, N> {
             // this is the same as MaybeUninit::uninit_array()
             store: unsafe { MaybeUninit::<[MaybeUninit<T>; N]>::uninit().assume_init() },
             // this is the same as Box::new_uninit()
-            next: uninit_box(),
+            next: ManuallyDrop::new(uninit_box()),
+            has_next: AtomicBool::new(false),
         }
+    }
+
+    unsafe fn uninit(&mut self) {
+        if self.has_next.load(Ordering::Relaxed) {
+            self.next.assume_init_mut().uninit();
+        }
+
+        ManuallyDrop::drop(&mut self.next)
     }
 
     // SAFETY: The following invariants must be upheld:
@@ -75,6 +103,7 @@ impl<T, const N: usize> Node<T, N> {
         if node == 1 && offset == 0 {
             // the next node is empty, and therefore needs to be allocated before we can use it
             self.next.write(Node::new());
+            self.has_next.store(true, Ordering::Relaxed);
         }
 
         if node == 0 {
@@ -127,5 +156,61 @@ impl<'a, T, const N: usize> Iterator for Iter<'a, T, N> {
         self.offset += 1;
 
         Some(item)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::{sync::Arc, vec, vec::Vec};
+
+    use super::AppendOnlyVec;
+
+    #[test]
+    fn push_single() {
+        let vec = AppendOnlyVec::<u8, 4>::new();
+        vec.push(1);
+
+        let vec: Vec<_> = vec.iter().copied().collect();
+        assert_eq!(vec, vec![1]);
+    }
+
+    #[test]
+    fn push_many() {
+        let vec = AppendOnlyVec::<u8, 4>::new();
+        let range = u8::MIN..u8::MAX;
+
+        for i in range.clone() {
+            vec.push(i);
+        }
+
+        let vec: Vec<_> = vec.iter().copied().collect();
+        assert_eq!(vec, range.collect::<Vec<_>>());
+    }
+
+    #[test]
+    #[cfg(loom)]
+    fn push_loom() {
+        loom::model(|| {
+            let v = Arc::new(AppendOnlyVec::<u64>::new());
+
+            let mut threads = Vec::new();
+            const N: u64 = 2;
+
+            for thread_num in 0..N {
+                let v = v.clone();
+                threads.push(loom::thread::spawn(move || {
+                    v.push(thread_num);
+                    v.push(thread_num);
+                }));
+            }
+
+            for t in threads {
+                t.join().expect("all threads could join")
+            }
+
+            for thread_num in 0..N {
+                assert_eq!(2, v.iter().copied().filter(|&x| x == thread_num).count());
+            }
+        })
     }
 }
