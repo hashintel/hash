95df93c98b25355a322427b57d528d724d9a5781
luisbettencourt <lmn.bettencourt@gmail.com>
2022-11-15 12:06:31 +0000

    simplify tabs values

--- packages/hash/frontend/src/pages/[account-slug]/types/entity-type/entity-type-tabs.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/types/entity-type/entity-type-tabs.tsx
@@ -1,41 +1,25 @@
-import { extractBaseUri, VersionedUri } from "@blockprotocol/type-system-web";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@hashintel/hash-design-system";
-import { frontendUrl } from "@hashintel/hash-shared/environment";
 import { Box, Tabs, tabsClasses } from "@mui/material";
 import { useRouter } from "next/router";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { EntityTypeDefinitionTab } from "./entity-type-definition-tab";
 import { EntityTypeEntitiesTab } from "./entity-type-entities-tab";
 import { TabButton } from "./tab-button";
-import { useEntityType } from "./use-entity-type";
 
 export const EntityTypeTabs = () => {
   const router = useRouter();
-  const entityType = useEntityType();
 
-  const [activeTab, setActiveTab] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    setActiveTab(`${frontendUrl}${router.asPath}`);
-  }, [router.asPath]);
-
-  const baseUri = useMemo(() => {
-    if (entityType?.$id) {
-      const entityTypeBaseUri = extractBaseUri(entityType.$id as VersionedUri);
-      return entityTypeBaseUri.substring(0, entityTypeBaseUri.length - 1);
-    }
-  }, [entityType]);
-
-  if (!baseUri) {
-    return null;
-  }
+  const baseUri = useMemo(
+    () =>
+      `/${router.query["account-slug"]}/types/entity-type/${router.query["entity-type-id"]}`,
+    [router.query],
+  );
 
   return (
     <Box display="flex" overflow="visible">
       <Tabs
-        value={activeTab}
-        onChange={(_, value) => setActiveTab(value)}
+        value={router.asPath}
         TabIndicatorProps={{
           sx: ({ palette }) => ({
             height: 3,
