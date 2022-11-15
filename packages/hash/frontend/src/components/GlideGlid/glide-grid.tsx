e30bfadbe51f8fc1333f96b6c88ed38d28591f4a
luisbettencourt <lmn.bettencourt@gmail.com>
2022-11-15 11:33:52 +0000

    remove resizableColumns implementation from this PR

--- packages/hash/frontend/src/components/GlideGlid/glide-grid.tsx
+++ packages/hash/frontend/src/components/GlideGlid/glide-grid.tsx
@@ -4,29 +4,17 @@ import {
   DataEditorProps,
   Theme,
   DataEditorRef,
-  GridColumn,
 } from "@glideapps/glide-data-grid";
 import { useTheme } from "@mui/material";
-import {
-  forwardRef,
-  ForwardRefRenderFunction,
-  useCallback,
-  useMemo,
-  useState,
-} from "react";
+import { forwardRef, ForwardRefRenderFunction, useMemo } from "react";
 import { getCellHorizontalPadding } from "./utils";
 import { customGridIcons } from "./utils/custom-grid-icons";
 
-interface CustomDataEditorProps {
-  resizableColumns?: boolean;
-}
-
-const GlideGrid: ForwardRefRenderFunction<
-  DataEditorRef,
-  DataEditorProps & CustomDataEditorProps
-> = ({ resizableColumns, columns, ...props }, ref) => {
+const GlideGrid: ForwardRefRenderFunction<DataEditorRef, DataEditorProps> = (
+  props,
+  ref,
+) => {
   const { palette } = useTheme();
-  const [cols, setCols] = useState(columns);
 
   const gridTheme: Partial<Theme> = useMemo(
     () => ({
@@ -51,23 +39,6 @@ const GlideGrid: ForwardRefRenderFunction<
     [palette],
   );
 
-  const onColumnResize = useCallback(
-    (col: GridColumn, newSize: number) => {
-      const index = cols.indexOf(col);
-      const newCols = [...cols];
-      const resizedCol = newCols[index];
-
-      if (resizedCol) {
-        newCols[index] = {
-          ...resizedCol,
-          width: newSize,
-        };
-      }
-      setCols(newCols);
-    },
-    [cols],
-  );
-
   return (
     <DataEditor
       ref={ref}
@@ -81,8 +52,6 @@ const GlideGrid: ForwardRefRenderFunction<
       smoothScrollX
       smoothScrollY
       getCellsForSelection
-      columns={cols}
-      onColumnResize={resizableColumns ? onColumnResize : undefined}
       {...props}
       /**
        * icons defined via `headerIcons` are avaiable to be drawn using
