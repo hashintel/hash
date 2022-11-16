241f1df4833b0ed1440782b26aea2b2cb68b2b39
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-16 11:39:39 +0000

    `@hashintel/hash-api` stats

--- packages/hash/api/.eslintrc.cjs
+++ packages/hash/api/.eslintrc.cjs
@@ -6,12 +6,12 @@ module.exports = {
     "@typescript-eslint/restrict-plus-operands": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
     ...require("@local/eslint-config/temporarily-disable-rules.cjs")([
-      /* 2022-11-15:  24 */ "@typescript-eslint/no-unsafe-argument",
-      /* 2022-11-15:  74 */ "@typescript-eslint/no-unsafe-assignment",
-      /* 2022-11-15:  59 */ "@typescript-eslint/no-unsafe-member-access",
-      /* 2022-11-15:  35 */ "@typescript-eslint/no-unsafe-return",
-      /* 2022-11-15:  11 */ "@typescript-eslint/require-await",
-      /* 2022-11-15:  59 */ "@typescript-eslint/restrict-template-expressions",
+      /* 2022-11-16:  25 */ "@typescript-eslint/no-unsafe-argument",
+      /* 2022-11-16:  72 */ "@typescript-eslint/no-unsafe-assignment",
+      /* 2022-11-16:  58 */ "@typescript-eslint/no-unsafe-member-access",
+      /* 2022-11-16:  33 */ "@typescript-eslint/no-unsafe-return",
+      /* 2022-11-16:  11 */ "@typescript-eslint/require-await",
+      /* 2022-11-16:  59 */ "@typescript-eslint/restrict-template-expressions",
     ]),
   },
   ignorePatterns: [
