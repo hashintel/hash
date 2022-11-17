0704686408add49059e5dfd589c61324781e2e77
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-17 12:51:03 +0000

    Improve `Entity` definition

--- packages/hash/subgraph/src/types/element.ts
+++ packages/hash/subgraph/src/types/element.ts
@@ -1,14 +1,20 @@
 import {
   DataTypeWithMetadata as DataTypeWithMetadataGraphApi,
-  Entity as EntityGraphApi,
   EntityTypeWithMetadata as EntityTypeWithMetadataGraphApi,
   PropertyTypeWithMetadata as PropertyTypeWithMetadataGraphApi,
+  Entity as EntityGraphApi,
+  ProvenanceMetadata as ProvenanceMetadataGraphApi,
+  LinkEntityMetadata,
+  ProvenanceMetadata,
 } from "@hashintel/hash-graph-client";
 import {
+  BaseUri,
   DataType,
   EntityType,
   PropertyType,
+  VersionedUri,
 } from "@blockprotocol/type-system-node";
+import { EntityEditionId, EntityId } from "./identifier";
 
 // Due to restrictions with how much OpenAPI can express, we patch the schemas with the better-typed ones from the
 // type-system package.
@@ -28,7 +34,36 @@ export type EntityTypeWithMetadata = Omit<
   "schema"
 > & { schema: EntityType };
 
-export type Entity = EntityGraphApi;
+/** Plain JSON value and object definitions */
+type JsonValue = string | number | boolean | JsonObject | JsonValue[];
+type JsonObject = {
+  [_: string]: JsonValue;
+};
+
+/**
+ * Entity Properties are JSON objects with `BaseUri`s as keys, _except_ when there is a Data Type of primitive type
+ * `object` in which case the nested objects become plain `JsonObject`s
+ */
+type PropertyValue = JsonValue | PropertyObject;
+export type PropertyObject = {
+  [_: BaseUri]: PropertyValue;
+};
+
+export type Entity = {
+  properties: PropertyObject;
+  metadata: {
+    archived: boolean;
+    editionId: EntityEditionId;
+    entityTypeId: VersionedUri;
+    linkMetadata?: {
+      leftOrder?: number;
+      rightOrder?: number;
+      leftEntityId: EntityId;
+      rightEntityId: EntityId;
+    };
+    provenance: ProvenanceMetadataGraphApi;
+  };
+};
 
 export type GraphElement =
   | DataTypeWithMetadata
