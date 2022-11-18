46f33f8469f46f5786ab8c73d76929f03d643c80
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-11-18 15:54:23 +0100

    Add more benchmarks for structural queries (`dev/links`) (#1437)

--- packages/graph/hash_graph/bench/benches/representative_read/knowledge/entity.rs
+++ packages/graph/hash_graph/bench/benches/representative_read/knowledge/entity.rs
@@ -1,8 +1,13 @@
+use std::borrow::Cow;
+
 use criterion::{BatchSize::SmallInput, Bencher};
 use graph::{
-    knowledge::EntityUuid,
+    knowledge::{EntityQueryPath, EntityUuid},
     shared::subgraph::{depths::GraphResolveDepths, query::StructuralQuery},
-    store::{query::Filter, EntityStore},
+    store::{
+        query::{Filter, FilterExpression, Parameter},
+        EntityStore,
+    },
 };
 use rand::{prelude::IteratorRandom, thread_rng};
 use tokio::runtime::Runtime;
@@ -21,7 +26,7 @@ pub fn bench_get_entity_by_id(
             *entity_uuids.iter().choose(&mut thread_rng()).unwrap()
         },
         |entity_uuid| async move {
-            store
+            let subgraph = store
                 .get_entity(&StructuralQuery {
                     filter: Filter::for_latest_entity_by_entity_uuid(entity_uuid),
                     graph_resolve_depths: GraphResolveDepths {
@@ -33,7 +38,60 @@ pub fn bench_get_entity_by_id(
                 })
                 .await
                 .expect("failed to read entity from store");
+            assert_eq!(subgraph.roots.len(), 1);
         },
         SmallInput,
     );
+}
+
+pub fn bench_get_entities_by_property(
+    b: &mut Bencher,
+    runtime: &Runtime,
+    store: &Store,
+    graph_resolve_depths: GraphResolveDepths,
+) {
+    b.to_async(runtime).iter(|| async move {
+        let subgraph = store
+            .get_entity(&StructuralQuery {
+                filter: Filter::Equal(
+                    Some(FilterExpression::Path(EntityQueryPath::Properties(Some(
+                        Cow::Borrowed("https://blockprotocol.org/@alice/types/property-type/name/"),
+                    )))),
+                    Some(FilterExpression::Parameter(Parameter::Text(Cow::Borrowed(
+                        "Alice",
+                    )))),
+                ),
+                graph_resolve_depths,
+            })
+            .await
+            .expect("failed to read entity from store");
+        assert_eq!(subgraph.roots.len(), 100);
+    });
+}
+
+pub fn bench_get_link_by_target_by_property(
+    b: &mut Bencher,
+    runtime: &Runtime,
+    store: &Store,
+    graph_resolve_depths: GraphResolveDepths,
+) {
+    b.to_async(runtime).iter(|| async move {
+        let subgraph = store
+            .get_entity(&StructuralQuery {
+                filter: Filter::Equal(
+                    Some(FilterExpression::Path(EntityQueryPath::RightEntity(
+                        Box::new(EntityQueryPath::Properties(Some(Cow::Borrowed(
+                            "https://blockprotocol.org/@alice/types/property-type/name/",
+                        )))),
+                    ))),
+                    Some(FilterExpression::Parameter(Parameter::Text(Cow::Borrowed(
+                        "Alice",
+                    )))),
+                ),
+                graph_resolve_depths,
+            })
+            .await
+            .expect("failed to read entity from store");
+        assert_eq!(subgraph.roots.len(), 100);
+    });
 }
