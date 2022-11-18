a6836f2dce2b37abbb77a0c06f586281a8ae6da4
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-18 17:08:42 +0000

    Add patch type-system functions

---
+++ packages/hash/subgraph/src/shared/type-system-patch.ts
@@ -0,0 +1,63 @@
+import {
+  BaseUri,
+  VersionedUri,
+  validateBaseUri as validateBaseUriGraphApi,
+  validateVersionedUri as validateVersionedUriGraphApi,
+} from "@blockprotocol/type-system-node";
+
+/**
+ * @todo - we are patching these so this package can be used in node and web environments. When we unify the packages
+ *   this will no longer be necessary return types / functionality may differ a bit in the meantime, these are just
+ *   hotfixes.
+ *   https://app.asana.com/0/1201095311341924/1202923896339225/f
+ *
+ * */
+
+export const extractBaseUri = (id: VersionedUri): BaseUri => {
+  return id.match(/(.+\/)v\/(\d+)(.*)/g)![1]!;
+};
+
+export const extractVersion = (id: VersionedUri): number => {
+  return Number(id.match(/(.+\/)v\/(\d+)(.*)/g)![2]!);
+};
+
+export const validateBaseUri = (
+  uri: string,
+): ReturnType<typeof validateBaseUriGraphApi> => {
+  try {
+    const _ = new URL(uri);
+    return {
+      type: "Ok",
+      inner: uri,
+    };
+  } catch {
+    return {
+      type: "Err",
+      inner: { reason: "UrlParseError", inner: "" },
+    };
+  }
+};
+
+export const validateVersionedUri = (
+  uri: string,
+): ReturnType<typeof validateVersionedUriGraphApi> => {
+  try {
+    const _ = new URL(uri);
+
+    if (/(.+)\/v\/(\d+)(.*)/.test(uri)) {
+      return {
+        type: "Ok",
+        inner: uri as VersionedUri,
+      };
+    }
+  } catch {
+    /* return error below */
+  }
+  return {
+    type: "Err",
+    inner: {
+      reason: "InvalidBaseUri",
+      inner: { reason: "UrlParseError", inner: "" },
+    },
+  };
+};
--- packages/hash/subgraph/src/stdlib/element/property-type.ts
+++ packages/hash/subgraph/src/stdlib/element/property-type.ts
@@ -1,13 +1,9 @@
-import {
-  BaseUri,
-  extractBaseUri,
-  extractVersion,
-  VersionedUri,
-} from "@blockprotocol/type-system-node";
+import { BaseUri, VersionedUri } from "@blockprotocol/type-system-node";
 import { OntologyTypeEditionId } from "../../types/identifier";
 import { Subgraph } from "../../types/subgraph";
 import { PropertyTypeWithMetadata } from "../../types/element";
 import { isPropertyTypeVertex } from "../../types/vertex";
+import { extractBaseUri, extractVersion } from "../../shared/type-system-patch";
 
 /**
  * Returns all `PropertyTypeWithMetadata`s within the vertices of the subgraph
