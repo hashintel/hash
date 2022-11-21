5dd670909fa55f5a50d18a1a34c675215fcf673c
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-21 13:39:55 +0000

    Fixup remove `block.model.ts`

--- packages/hash/api/src/model/index.ts
+++ packages/hash/api/src/model/index.ts
@@ -14,7 +14,6 @@ import OrgEmailInvitation from "./orgEmailInvitation.model";
 import File from "./file.model";
 import OrgMembership from "./orgMembership.model";
 import Page from "./page.model";
-import Block from "./block.model";
 import Link from "./link.model";
 import Aggregation from "./aggregation.model";
 
@@ -99,6 +98,3 @@ export { OrgMembership };
 
 export * from "./page.model";
 export { Page };
-
-export * from "./block.model";
-export { Block };
