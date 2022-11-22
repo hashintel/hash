898e5bc7d8019d4acd9dadeaf7f7d103550e77db
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-22 19:55:34 +0000

    Fix `entity-editor.tsx`

--- packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-editor.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-editor.tsx
@@ -1,13 +1,16 @@
-import { EntityWithMetadata } from "@hashintel/hash-subgraph";
+import {
+  EntityWithMetadata,
+  Subgraph,
+  SubgraphRootTypes,
+} from "@hashintel/hash-subgraph";
 import { EntityEditorContextProvider } from "./entity-editor/entity-editor-context";
 import { LinksSection } from "./entity-editor/links-section";
 import { PeersSection } from "./entity-editor/peers-section";
 import { PropertiesSection } from "./entity-editor/properties-section";
 import { TypesSection } from "./entity-editor/types-section";
-import { RootEntityAndSubgraph } from "../../../../lib/subgraph";
 
 export interface EntityEditorProps {
-  entitySubgraph: RootEntityAndSubgraph | undefined;
+  entitySubgraph: Subgraph<SubgraphRootTypes["entity"]> | undefined;
   setEntity: (entity: EntityWithMetadata | undefined) => void;
 }
 
