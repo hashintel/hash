751a1ffdb36a40f520a62ce02cfa15c1607f23a0
luisbettencourt <37453800+luisbettencourt@users.noreply.github.com>
2022-11-22 01:23:12 +0000

    Move sort logic to GlideGrid component (#1429)
    * move sort logic to GlideGrid component
    
    * cleanup
    
    * add sortable prop
    
    * lint fix
    
    * lint fix
    
    * remove linkSort
    
    * rename getCellContent to createGetCellContent
    
    * fix types on createGetCellContent
    
    * Change any to unknown on Row type
    
    * sortRowData and types
    
    * cleanup
    
    * revert change

--- packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-editor/properties-section/property-table.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entities/[entity-id].page/entity-editor/properties-section/property-table.tsx
@@ -1,7 +1,7 @@
 import { useMemo, useRef } from "react";
 import { DataEditorRef } from "@glideapps/glide-data-grid";
 import { useRowData } from "./property-table/use-row-data";
-import { useGetCellContent } from "./property-table/use-get-cell-content";
+import { useCreateGetCellContent } from "./property-table/use-create-get-cell-content";
 import { propertyGridColumns } from "./property-table/constants";
 import { useOnCellEdited } from "./property-table/use-on-cell-edited";
 import { useEntityEditor } from "../entity-editor-context";
@@ -10,8 +10,6 @@ import { useGridTooltip } from "../../../../../../components/GlideGlid/utils/use
 import { renderValueCell } from "./property-table/cells/value-cell";
 import { renderChipCell } from "./property-table/cells/chip-cell";
 import { createRenderPropertyNameCell } from "./property-table/cells/property-name-cell";
-import { useDrawHeader } from "../../../../../../components/GlideGlid/utils/use-draw-header";
-import { createHandleHeaderClicked } from "../../../../../../components/GlideGlid/utils/sorting";
 import { renderSummaryChipCell } from "./property-table/cells/summary-chip-cell";
 
 interface PropertyTableProps {
@@ -23,24 +21,15 @@ export const PropertyTable = ({
   showSearch,
   onSearchClose,
 }: PropertyTableProps) => {
-  const gridRef = useRef<DataEditorRef>(null);
-  const {
-    propertySort,
-    setPropertySort,
-    togglePropertyExpand,
-    propertyExpandStatus,
-  } = useEntityEditor();
-  const rowData = useRowData();
-  const { tooltipElement, showTooltip, hideTooltip } = useGridTooltip(gridRef);
-  const getCellContent = useGetCellContent(rowData, showTooltip, hideTooltip);
-  const onCellEdited = useOnCellEdited(rowData);
-  const drawHeader = useDrawHeader(propertySort, propertyGridColumns);
-
-  const handleHeaderClicked = createHandleHeaderClicked(
-    propertyGridColumns,
-    propertySort,
-    setPropertySort,
+  const tableRef = useRef<DataEditorRef>(null);
+  const { togglePropertyExpand, propertyExpandStatus } = useEntityEditor();
+  const [rowData, sortAndFlattenRowData] = useRowData();
+  const { tooltipElement, showTooltip, hideTooltip } = useGridTooltip(tableRef);
+  const createGetCellContent = useCreateGetCellContent(
+    showTooltip,
+    hideTooltip,
   );
+  const onCellEdited = useOnCellEdited(rowData);
 
   const customRenderers = useMemo(
     () => [
@@ -55,16 +44,15 @@ export const PropertyTable = ({
   return (
     <>
       <GlideGrid
-        ref={gridRef}
+        tableRef={tableRef}
         columns={propertyGridColumns}
-        rows={rowData.length}
-        getCellContent={getCellContent}
+        createGetCellContent={createGetCellContent}
         onCellEdited={onCellEdited}
-        drawHeader={drawHeader}
-        onHeaderClicked={handleHeaderClicked}
+        rowData={rowData}
         showSearch={showSearch}
         onSearchClose={onSearchClose}
         customRenderers={customRenderers}
+        sortRowData={sortAndFlattenRowData}
         // define max height if there are lots of rows
         height={rowData.length > 10 ? 500 : undefined}
       />
