d2b3bade72a2a2cb3f1c78522f6d0e7fc0eeecf3
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-23 12:37:16 +0000

    Fix use-route-namespace.tsx

--- packages/hash/frontend/src/pages/[account-slug]/types/entity-type/use-route-namespace.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/types/entity-type/use-route-namespace.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import { useMemo } from "react";
-import { extractEntityUuidFromEntityId } from "@hashintel/hash-subgraph";
 import { useAuthenticatedUser } from "../../../../components/hooks/useAuthenticatedUser";
 
 export const useRouteNamespace = ():
@@ -24,7 +23,7 @@ export const useRouteNamespace = ():
         accountId = authenticatedUser.userAccountId;
       } else {
         const org = authenticatedUser.memberOf?.find(
-          (org) => `@${org.shortname}` === shortname,
+          (potentialOrg) => `@${potentialOrg.shortname}` === shortname,
         );
 
         accountShortname = org?.shortname;
