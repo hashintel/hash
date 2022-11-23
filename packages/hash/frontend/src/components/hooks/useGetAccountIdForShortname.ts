20fa945458baf6d61cd2046795e6d675f45dd180
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-23 11:52:09 +0000

    Add useGetAccountIdForShortname.ts

---
+++ packages/hash/frontend/src/components/hooks/useGetAccountIdForShortname.ts
@@ -0,0 +1,49 @@
+import { useMemo } from "react";
+import { extractEntityUuidFromEntityId } from "@hashintel/hash-subgraph";
+import { nilUuid } from "@hashintel/hash-shared/types";
+import { useUsers } from "./useUsers";
+import { useOrgs } from "./useOrgs";
+
+export const useGetAccountIdForShortname = (
+  shortname: string | undefined,
+): { loading: boolean; accountId: string | undefined } => {
+  const { loading: usersLoading, users } = useUsers();
+  const { loading: orgsLoading, orgs } = useOrgs();
+
+  const accountId = useMemo(() => {
+    /**
+     * @todo - This is incredibly flakey right now.
+     *    We should be creating system types and entities under the actual org, _not_ the nilUuid
+     *    We should be making SYSTEM_ACCOUNT_SHORTNAME available in the frontend
+     */
+    if (shortname === "example-org" || shortname === "example") {
+      return nilUuid;
+    }
+
+    /** @todo - don't do extract anymore */
+    const userBaseId = users?.find((user) => user.shortname === shortname)
+      ?.entityEditionId.baseId;
+    const userAccountId = userBaseId
+      ? extractEntityUuidFromEntityId(userBaseId)
+      : undefined;
+
+    if (userAccountId !== undefined) {
+      return userAccountId;
+    }
+
+    const orgBaseId = orgs?.find((org) => org.shortname === shortname)
+      ?.entityEditionId.baseId;
+    const orgAccountId = orgBaseId
+      ? extractEntityUuidFromEntityId(orgBaseId)
+      : undefined;
+
+    if (orgAccountId !== undefined) {
+      return orgAccountId;
+    }
+  }, [users, orgs, shortname]);
+
+  return {
+    loading: usersLoading || orgsLoading,
+    accountId,
+  };
+};
