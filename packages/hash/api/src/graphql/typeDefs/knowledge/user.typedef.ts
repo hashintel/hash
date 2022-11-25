796e5bac07bdafbf8dea3d74f01b50abbf107d66
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-11-25 18:19:01 +0000

    Fix me query

--- packages/hash/api/src/graphql/typeDefs/knowledge/user.typedef.ts
+++ packages/hash/api/src/graphql/typeDefs/knowledge/user.typedef.ts
@@ -2,7 +2,10 @@ import { gql } from "apollo-server-express";
 
 export const userTypedef = gql`
   extend type Query {
-    me(hasLeftEntity: EdgeResolveDepthsInput! = 0): Subgraph!
+    me(
+      hasLeftEntity: EdgeResolveDepthsInput! = { incoming: 0, outgoing: 0 }
+      hasRightEntity: EdgeResolveDepthsInput! = { incoming: 0, outgoing: 0 }
+    ): Subgraph!
     """
     Determines whether a provided shortname is already taken
     """
