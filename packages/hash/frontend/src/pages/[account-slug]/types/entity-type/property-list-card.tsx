ab57d89a1d83c1da19bdbee9c0fe3d55f1f131c4
Nate Higgins <nate@nateh.uk>
2022-11-28 12:21:58 +0000

    Restructure property type modal so it can be customised

--- packages/hash/frontend/src/pages/[account-slug]/types/entity-type/property-list-card.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/types/entity-type/property-list-card.tsx
@@ -19,6 +19,7 @@ import {
 import { experimental_sx, styled } from "@mui/system";
 import {
   bindPopover,
+  bindToggle,
   bindTrigger,
   usePopupState,
 } from "material-ui-popup-state/hooks";
@@ -36,11 +37,12 @@ import { InsertPropertyRow } from "./insert-property-row";
 import { MultipleValuesCell } from "./multiple-values-cell";
 import { PropertyExpectedValues } from "./property-expected-values";
 import { PropertyMenu } from "./property-menu";
+import { PropertyTypeForm } from "./property-type-form";
 import { PropertyTypeModal } from "./property-type-modal";
 import { QuestionIcon } from "./question-icon";
 import { StyledPlusCircleIcon } from "./styled-plus-circle-icon";
 import { usePropertyTypes } from "./use-property-types";
-import { mustBeVersionedUri, useStateCallback } from "./util";
+import { mustBeVersionedUri, useStateCallback, withHandler } from "./util";
 
 const CenteredTableCell = styled(TableCell)(
   experimental_sx({
@@ -150,9 +152,21 @@ export const PropertyTypeRow = ({
       </TableRow>
       <PropertyTypeModal
         popupState={editModalPopupState}
+        onClose={() => {
+          menuPopupState.open();
+        }}
         title={<>Edit Property Type</>}
       >
-        foo
+        <PropertyTypeForm
+          popupState={editModalPopupState}
+          onClose={() => {
+            menuPopupState.open();
+          }}
+          onSubmit={async (data) => {
+            console.log(data);
+          }}
+          submitButtonProps={{ children: <>Edit property type</> }}
+        />
       </PropertyTypeModal>
     </>
   );
