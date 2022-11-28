0e622a088278eef85bb24d6eccd7deedc824440e
Nate Higgins <nate@nateh.uk>
2022-11-28 14:07:48 +0000

    Separate modal contents to make initial values work again

--- packages/hash/frontend/src/pages/[account-slug]/types/entity-type/property-type-modal-form.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/types/entity-type/property-type-modal-form.tsx
@@ -23,7 +23,7 @@ import {
   bindToggle,
   PopupState,
 } from "material-ui-popup-state/hooks";
-import { ReactNode, useEffect, useState } from "react";
+import { ComponentProps, ReactNode, useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { getPropertyTypeById } from "@hashintel/hash-subgraph/src/stdlib/element/property-type";
 import { versionedUriFromComponents } from "@hashintel/hash-subgraph/src/shared/type-system-patch";
@@ -68,7 +68,7 @@ type PropertyTypeModalFormSubmitProps = Omit<
   "size" | "variant" | "disabled" | "type" | "loading"
 >;
 
-export const PropertyTypeModalForm = ({
+const PropertyTypeForm = ({
   onClose,
   modalTitle,
   popupState,
@@ -139,251 +139,253 @@ export const PropertyTypeModalForm = ({
   const [descriptionValid, setDescriptionValid] = useState(false);
 
   return (
-    <Modal
-      {...bindDialog(popupState)}
-      disableEscapeKeyDown
-      contentStyle={(theme) => ({
-        p: "0px !important",
-        border: 1,
-        borderColor: theme.palette.gray[20],
-      })}
-    >
-      <>
-        <Box
-          sx={(theme) => ({
-            px: 2.5,
-            pr: 1.5,
-            pb: 1.5,
-            pt: 2,
-            borderBottom: 1,
-            borderColor: theme.palette.gray[20],
-            alignItems: "center",
-            display: "flex",
-          })}
+    <>
+      <Box
+        sx={(theme) => ({
+          px: 2.5,
+          pr: 1.5,
+          pb: 1.5,
+          pt: 2,
+          borderBottom: 1,
+          borderColor: theme.palette.gray[20],
+          alignItems: "center",
+          display: "flex",
+        })}
+      >
+        <Typography
+          variant="regularTextLabels"
+          sx={{ fontWeight: 500, display: "flex", alignItems: "center" }}
         >
-          <Typography
-            variant="regularTextLabels"
-            sx={{ fontWeight: 500, display: "flex", alignItems: "center" }}
-          >
-            {modalTitle}
-          </Typography>
-          <IconButton
-            {...withHandler(bindToggle(popupState), onClose)}
-            sx={(theme) => ({
-              ml: "auto",
-              svg: {
-                color: theme.palette.gray[50],
-                fontSize: 20,
+          {modalTitle}
+        </Typography>
+        <IconButton
+          {...withHandler(bindToggle(popupState), onClose)}
+          sx={(theme) => ({
+            ml: "auto",
+            svg: {
+              color: theme.palette.gray[50],
+              fontSize: 20,
+            },
+          })}
+          disabled={isSubmitting}
+        >
+          <FontAwesomeIcon icon={faClose} />
+        </IconButton>
+      </Box>
+      <Box
+        minWidth={500}
+        p={3}
+        component="form"
+        display="block"
+        onSubmit={handleSubmit}
+      >
+        <Stack
+          alignItems="stretch"
+          spacing={3}
+          sx={{
+            [`.${inputLabelClasses.root}`]: {
+              display: "flex",
+              alignItems: "center",
+            },
+          }}
+        >
+          <TextField
+            label="Singular name"
+            required
+            placeholder="e.g. Stock Price"
+            disabled={isSubmitting}
+            error={!!nameError}
+            helperText={nameError?.message}
+            success={titleValid}
+            {...register("name", {
+              required: true,
+              onChange() {
+                clearErrors("name");
+                setTitleValid(false);
+              },
+              async validate(value) {
+                const propertyTypeId = generateInitialPropertyTypeId(
+                  generatePropertyTypeBaseUriForUser(value),
+                );
+
+                const res = await getPropertyType({
+                  data: {
+                    propertyTypeId,
+                    graphResolveDepths: {
+                      constrainsValuesOn: { outgoing: 0 },
+                      constrainsPropertiesOn: { outgoing: 0 },
+                    },
+                  },
+                });
+
+                const exists =
+                  !res.data || !!getPropertyTypeById(res.data, propertyTypeId);
+
+                if (getValues("name") === value && !exists) {
+                  setTitleValid(true);
+                }
+
+                return exists ? "Property type name must be unique" : true;
               },
             })}
-            disabled={isSubmitting}
-          >
-            <FontAwesomeIcon icon={faClose} />
-          </IconButton>
-        </Box>
-        <Box
-          minWidth={500}
-          p={3}
-          component="form"
-          display="block"
-          onSubmit={handleSubmit}
-        >
-          <Stack
-            alignItems="stretch"
-            spacing={3}
-            sx={{
-              [`.${inputLabelClasses.root}`]: {
-                display: "flex",
-                alignItems: "center",
-              },
-            }}
-          >
-            <TextField
-              label="Singular name"
-              required
-              placeholder="e.g. Stock Price"
-              disabled={isSubmitting}
-              error={!!nameError}
-              helperText={nameError?.message}
-              success={titleValid}
-              {...register("name", {
-                required: true,
-                onChange() {
-                  clearErrors("name");
-                  setTitleValid(false);
-                },
-                async validate(value) {
-                  const propertyTypeId = generateInitialPropertyTypeId(
-                    generatePropertyTypeBaseUriForUser(value),
-                  );
-
-                  const res = await getPropertyType({
-                    data: {
-                      propertyTypeId,
-                      graphResolveDepths: {
-                        constrainsValuesOn: { outgoing: 0 },
-                        constrainsPropertiesOn: { outgoing: 0 },
-                      },
-                    },
-                  });
-
-                  const exists =
-                    !res.data ||
-                    !!getPropertyTypeById(res.data, propertyTypeId);
-
-                  if (getValues("name") === value && !exists) {
-                    setTitleValid(true);
-                  }
-
-                  return exists ? "Property type name must be unique" : true;
-                },
-              })}
-            />
-            <TextField
-              multiline
-              inputProps={{ minRows: 1 }}
-              label={
-                <>
-                  Description{" "}
-                  <Tooltip
-                    placement="top"
-                    title="Descriptions help people understand what property types can be used for, and help make them more discoverable (allowing for reuse)."
-                    PopperProps={{
-                      modifiers: [
-                        {
-                          name: "offset",
-                          options: {
-                            offset: [0, 8],
-                          },
+          />
+          <TextField
+            multiline
+            inputProps={{ minRows: 1 }}
+            label={
+              <>
+                Description{" "}
+                <Tooltip
+                  placement="top"
+                  title="Descriptions help people understand what property types can be used for, and help make them more discoverable (allowing for reuse)."
+                  PopperProps={{
+                    modifiers: [
+                      {
+                        name: "offset",
+                        options: {
+                          offset: [0, 8],
                         },
-                      ],
+                      },
+                    ],
+                  }}
+                >
+                  <Box
+                    sx={{
+                      order: 1,
+                      ml: 0.75,
+                      display: "flex",
+                      alignItems: "center",
                     }}
                   >
-                    <Box
-                      sx={{
-                        order: 1,
-                        ml: 0.75,
-                        display: "flex",
-                        alignItems: "center",
-                      }}
-                    >
-                      <QuestionIcon />
-                    </Box>
-                  </Tooltip>
-                </>
-              }
-              required
-              placeholder="Describe this property type in one or two sentences"
-              disabled={isSubmitting}
-              success={descriptionValid}
-              error={!!descriptionError && descriptionTouched}
-              {...register("description", {
-                required: true,
-                onChange() {
-                  clearErrors("description");
-                  setDescriptionValid(false);
-                },
-                validate(value) {
-                  setDescriptionValid(!!value);
+                    <QuestionIcon />
+                  </Box>
+                </Tooltip>
+              </>
+            }
+            required
+            placeholder="Describe this property type in one or two sentences"
+            disabled={isSubmitting}
+            success={descriptionValid}
+            error={!!descriptionError && descriptionTouched}
+            {...register("description", {
+              required: true,
+              onChange() {
+                clearErrors("description");
+                setDescriptionValid(false);
+              },
+              validate(value) {
+                setDescriptionValid(!!value);
 
-                  return value ? true : "You must choose a description";
-                },
-              })}
-            />
-            <Controller
-              render={({ field: { onChange, ...props } }) => (
-                <Autocomplete
-                  multiple
-                  popupIcon={null}
-                  clearIcon={null}
-                  forcePopupIcon={false}
-                  selectOnFocus={false}
-                  openOnFocus
-                  clearOnBlur={false}
-                  onChange={(_evt, data) => onChange(data)}
-                  {...props}
-                  renderTags={(value, getTagProps) =>
-                    value.map((option, index) => (
-                      <Chip
-                        {...getTagProps({ index })}
-                        key={option.dataTypeId}
-                        label={
-                          <Typography
-                            variant="smallTextLabels"
-                            sx={{ display: "flex", alignItems: "center" }}
-                          >
-                            <FontAwesomeIcon
-                              icon={{ icon: option.icon }}
-                              sx={{ fontSize: "1em", mr: "1ch" }}
-                            />
-                            {option.title}
-                          </Typography>
-                        }
-                        color="blue"
-                      />
-                    ))
-                  }
-                  renderInput={(inputProps) => (
-                    <TextField
-                      {...inputProps}
-                      label="Expected values"
-                      sx={{ alignSelf: "flex-start", width: "70%" }}
-                      placeholder="Select acceptable values"
+                return value ? true : "You must choose a description";
+              },
+            })}
+          />
+          <Controller
+            render={({ field: { onChange, ...props } }) => (
+              <Autocomplete
+                multiple
+                popupIcon={null}
+                clearIcon={null}
+                forcePopupIcon={false}
+                selectOnFocus={false}
+                openOnFocus
+                clearOnBlur={false}
+                onChange={(_evt, data) => onChange(data)}
+                {...props}
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <Chip
+                      {...getTagProps({ index })}
+                      key={option.dataTypeId}
+                      label={
+                        <Typography
+                          variant="smallTextLabels"
+                          sx={{ display: "flex", alignItems: "center" }}
+                        >
+                          <FontAwesomeIcon
+                            icon={{ icon: option.icon }}
+                            sx={{ fontSize: "1em", mr: "1ch" }}
+                          />
+                          {option.title}
+                        </Typography>
+                      }
+                      color="blue"
                     />
-                  )}
-                  options={propertyTypeDataTypes}
-                  getOptionLabel={(obj) => obj.title}
-                  disableCloseOnSelect
-                  renderOption={(optProps, opt) => (
-                    <Box
-                      component="li"
-                      {...optProps}
-                      sx={{ py: 1.5, px: 2.25 }}
+                  ))
+                }
+                renderInput={(inputProps) => (
+                  <TextField
+                    {...inputProps}
+                    label="Expected values"
+                    sx={{ alignSelf: "flex-start", width: "70%" }}
+                    placeholder="Select acceptable values"
+                  />
+                )}
+                options={propertyTypeDataTypes}
+                getOptionLabel={(obj) => obj.title}
+                disableCloseOnSelect
+                renderOption={(optProps, opt) => (
+                  <Box component="li" {...optProps} sx={{ py: 1.5, px: 2.25 }}>
+                    <FontAwesomeIcon
+                      icon={{ icon: opt.icon }}
+                      sx={(theme) => ({ color: theme.palette.gray[50] })}
+                    />
+                    <Typography
+                      variant="smallTextLabels"
+                      component="span"
+                      ml={1.5}
+                      color={(theme) => theme.palette.gray[80]}
                     >
-                      <FontAwesomeIcon
-                        icon={{ icon: opt.icon }}
-                        sx={(theme) => ({ color: theme.palette.gray[50] })}
-                      />
-                      <Typography
-                        variant="smallTextLabels"
-                        component="span"
-                        ml={1.5}
-                        color={(theme) => theme.palette.gray[80]}
-                      >
-                        {opt.title}
-                      </Typography>
-                      <Chip color="blue" label="DATA TYPE" sx={{ ml: 1.5 }} />
-                    </Box>
-                  )}
-                />
-              )}
-              control={control}
-              rules={{ required: true }}
-              name="expectedValues"
-            />
-          </Stack>
-          <Divider sx={{ mt: 2, mb: 3 }} />
-          <Stack direction="row" spacing={1.25}>
-            <Button
-              {...submitButtonProps}
-              loading={isSubmitting}
-              disabled={isSubmitting || !isValid}
-              type="submit"
-              size="small"
-            >
-              {submitButtonProps.children}
-            </Button>
-            <Button
-              {...withHandler(bindToggle(popupState), onClose)}
-              disabled={isSubmitting}
-              size="small"
-              variant="tertiary"
-            >
-              Discard draft
-            </Button>
-          </Stack>
-        </Box>
-      </>
-    </Modal>
+                      {opt.title}
+                    </Typography>
+                    <Chip color="blue" label="DATA TYPE" sx={{ ml: 1.5 }} />
+                  </Box>
+                )}
+              />
+            )}
+            control={control}
+            rules={{ required: true }}
+            name="expectedValues"
+          />
+        </Stack>
+        <Divider sx={{ mt: 2, mb: 3 }} />
+        <Stack direction="row" spacing={1.25}>
+          <Button
+            {...submitButtonProps}
+            loading={isSubmitting}
+            disabled={isSubmitting || !isValid}
+            type="submit"
+            size="small"
+          >
+            {submitButtonProps.children}
+          </Button>
+          <Button
+            {...withHandler(bindToggle(popupState), onClose)}
+            disabled={isSubmitting}
+            size="small"
+            variant="tertiary"
+          >
+            Discard draft
+          </Button>
+        </Stack>
+      </Box>
+    </>
   );
 };
+
+export const PropertyTypeModalForm = ({
+  popupState,
+  ...props
+}: ComponentProps<typeof PropertyTypeForm>) => (
+  <Modal
+    {...bindDialog(popupState)}
+    disableEscapeKeyDown
+    contentStyle={(theme) => ({
+      p: "0px !important",
+      border: 1,
+      borderColor: theme.palette.gray[20],
+    })}
+  >
+    <PropertyTypeForm {...props} popupState={popupState} />
+  </Modal>
+);
