169b49bb8dfba53cabebdf1dcb9431d979a7bf1a
Ben Werner <ben.werner01@gmail.com>
2022-11-29 14:14:41 +0000

    refactor: remove dev information from homepage, and add redirect to login page

--- packages/hash/frontend/src/pages/index.page.tsx
+++ packages/hash/frontend/src/pages/index.page.tsx
@@ -1,44 +1,25 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import { Box, Container, Typography } from "@mui/material";
-import { useLogoutFlow } from "../components/hooks/useLogoutFlow";
+import { Container, Typography } from "@mui/material";
 import { useLoggedInUser } from "../components/hooks/useAuthenticatedUser";
 import { NextPageWithLayout } from "../shared/layout";
-import { Button } from "../shared/ui";
 
 const Page: NextPageWithLayout = () => {
   const router = useRouter();
-  const { authenticatedUser, kratosSession } = useLoggedInUser();
-
-  const { logout } = useLogoutFlow();
+  const { authenticatedUser, loading } = useLoggedInUser();
 
   useEffect(() => {
     if (authenticatedUser) {
       void router.push(`/${authenticatedUser.userAccountId}`);
+    } else if (!loading && !authenticatedUser) {
+      void router.push("/login");
     }
-  }, [router, authenticatedUser]);
+  }, [router, authenticatedUser, loading]);
 
-  /** @todo: remove session developer information */
   return (
     <Container sx={{ pt: 10 }}>
-      {authenticatedUser && (
-        <Typography variant="h1">
-          Hi {authenticatedUser.preferredName}!
-        </Typography>
-      )}
-      <Typography variant="h2" gutterBottom>
-        {kratosSession
-          ? "You have a kratos session"
-          : "You don't have a kratos session"}
-      </Typography>
-      <Button onClick={logout}>Log out</Button>
-      <Typography gutterBottom>
-        This is what your kratos session looks like:
-      </Typography>
-      {kratosSession ? (
-        <Box component="pre">{JSON.stringify(kratosSession, null, 2)}</Box>
-      ) : null}
+      <Typography variant="h1">Homepage</Typography>
     </Container>
   );
 };
