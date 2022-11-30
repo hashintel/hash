51b34b8192d8293223e545dce785e9b1710e2ff7
Nate Higgins <nate@nateh.uk>
2022-11-30 02:52:31 +0000

    User auth & new entity improvements
    - Redirect should be a replace when coming from the `new?entity-type=` route
    - Shouldn't retrigger kratos fetching for every use of `useAuthenticatedUser`
    - The redirect when creating entity should be cancelled if the target entity-type-id changes
    - Remove `onAuthenticate` from `useAuthenticatedUser`, which is very difficult to implement cleanly with current state setup
    - Reintroduce effect to handle triggering create/redirect on `new?entity-type=` route – with comment explaining why doing it this way
    - `createNewEntityAndRedirect` should take an authenticated user, instead of taking it from parent scope – to avoid any risk caused by stale closures
    - Add full page layout to new entity page
    - Clean up state management on new entity page

--- packages/hash/frontend/src/pages/[account-slug]/entities/new.page.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/entities/new.page.tsx
@@ -1,9 +1,14 @@
 import { validateVersionedUri } from "@blockprotocol/type-system-web";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { useAuthenticatedUser } from "../../../components/hooks/useAuthenticatedUser";
 import { PageErrorState } from "../../../components/page-error-state";
-import { getPlainLayout, NextPageWithLayout } from "../../../shared/layout";
+import { useInitTypeSystem } from "../../../lib/use-init-type-system";
+import {
+  getLayoutWithSidebar,
+  getPlainLayout,
+  NextPageWithLayout,
+} from "../../../shared/layout";
 import { EntityPageLoadingState } from "./[entity-uuid].page/entity-page-loading-state";
 import { NewEntityPage } from "./[entity-uuid].page/new-entity-page";
 import { useCreateNewEntityAndRedirect } from "./[entity-uuid].page/shared/use-create-new-entity-and-redirect";
@@ -11,51 +16,81 @@ import { useCreateNewEntityAndRedirect } from "./[entity-uuid].page/shared/use-c
 const Page: NextPageWithLayout = () => {
   const router = useRouter();
   const createNewEntityAndRedirect = useCreateNewEntityAndRedirect();
-  const [loading, setLoading] = useState(true);
+  const loadingTypeSystem = useInitTypeSystem();
+  const queryEntityId = router.query["entity-type-id"]?.toString();
+  const entityTypeId =
+    loadingTypeSystem || !queryEntityId
+      ? null
+      : validateVersionedUri(queryEntityId);
+  const { authenticatedUser, loading: authenticatedUserLoading } =
+    useAuthenticatedUser(undefined, true);
+  const shouldBeCreatingEntity = entityTypeId?.type === "Ok";
+  const [creatingEntity, setCreatingEntity] = useState(shouldBeCreatingEntity);
 
-  const entityTypeId = String(router.query["entity-type-id"]);
-  const idResult = validateVersionedUri(entityTypeId);
+  const entityIdInvalid = !!queryEntityId && entityTypeId?.type === "Err";
 
-  const { authenticatedUser } = useAuthenticatedUser(
-    undefined,
-    true,
-    async () => {
-      if (idResult.type === "Ok") {
-        try {
-          await createNewEntityAndRedirect(idResult.inner);
-        } finally {
-          setLoading(false);
-        }
-      } else {
-        setLoading(false);
-      }
-    },
-  );
+  if (shouldBeCreatingEntity && !creatingEntity) {
+    setCreatingEntity(true);
+  } else if (entityIdInvalid && creatingEntity) {
+    setCreatingEntity(false);
+  }
+
+  /**
+   * This shouldn't be an effect, because we're relying on React re-renders after
+   * events, instead of just subscribing to the events. It's difficult to do that
+   * right now because the different pieces are stored in different places. We
+   * should have one component responsible for fetching user data and storing it
+   * in context, and which will also let child components subscribe to
+   * authenticatedUser becoming available
+   *
+   * @todo remove this effect when possible
+   */
+  useEffect(() => {
+    if (entityTypeId?.type === "Ok" && authenticatedUser) {
+      const controller = new AbortController();
+
+      void createNewEntityAndRedirect(
+        authenticatedUser,
+        entityTypeId.inner,
+        true,
+        controller.signal,
+      );
+
+      return () => {
+        controller.abort();
+      };
+    }
+  }, [
+    authenticatedUser,
+    createNewEntityAndRedirect,
+    entityTypeId?.inner,
+    entityTypeId?.type,
+  ]);
 
   const accountSlug = router.query["account-slug"] as string | undefined;
   const shortname = accountSlug?.slice(1);
+
+  if (creatingEntity || authenticatedUserLoading || loadingTypeSystem) {
+    return <EntityPageLoadingState />;
+  }
 
   if (!authenticatedUser) {
     return null;
   }
 
-  if (loading) {
-    return <EntityPageLoadingState />;
-  }
-
   // show error if url slug it's not users shortname, or shortname of one of users orgs
-  const atUsersNamespace = shortname === authenticatedUser?.shortname;
-  const atOrgsNamespace = !!authenticatedUser?.memberOf.find(
+  const atUsersNamespace = shortname === authenticatedUser.shortname;
+  const atOrgsNamespace = authenticatedUser.memberOf.some(
     (val) => val.shortname === shortname,
   );
 
-  if ((!atOrgsNamespace && !atUsersNamespace) || idResult.type === "Err") {
+  if ((!atOrgsNamespace && !atUsersNamespace) || entityIdInvalid) {
     return <PageErrorState />;
   }
 
   return <NewEntityPage />;
 };
 
-Page.getLayout = getPlainLayout;
+Page.getLayout = (page) => getLayoutWithSidebar(page, { fullWidth: true });
 
 export default Page;
