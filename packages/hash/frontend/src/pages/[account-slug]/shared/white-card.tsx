bfd0ca1118fc5d97364de6e0c42c4db9d483a3e8
Ben Werner <ben.werner01@gmail.com>
2022-12-01 15:32:41 +0000

    refactor: use `LinkComponent` prop

--- packages/hash/frontend/src/pages/[account-slug]/shared/white-card.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/shared/white-card.tsx
@@ -6,28 +6,7 @@ import {
   CardContent,
   CardContentProps,
 } from "@mui/material";
-import { FunctionComponent, ReactNode } from "react";
 import { Link } from "../../../shared/ui/link";
-
-const WhiteCardActionArea: FunctionComponent<
-  { children: ReactNode } & CardActionAreaProps
-> = ({ children, ...props }) => (
-  <CardActionArea
-    {...props}
-    disableRipple
-    disableTouchRipple
-    sx={[
-      {
-        [`&:hover .${cardActionAreaClasses.focusHighlight}`]: {
-          opacity: 0,
-        },
-      },
-      ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
-    ]}
-  >
-    {children}
-  </CardActionArea>
-);
 
 export const WhiteCard = ({
   onClick,
@@ -65,14 +44,20 @@ export const WhiteCard = ({
           : {},
       ]}
     >
-      {onClick ? (
-        <WhiteCardActionArea onClick={onClick}>
+      {onClick || href ? (
+        <CardActionArea
+          {...(onClick ? { onClick } : { href })}
+          LinkComponent={Link}
+          disableRipple
+          disableTouchRipple
+          sx={{
+            [`&:hover .${cardActionAreaClasses.focusHighlight}`]: {
+              opacity: 0,
+            },
+          }}
+        >
           {cardContent}
-        </WhiteCardActionArea>
-      ) : href ? (
-        <Link href={href} sx={{ textDecoration: "none" }}>
-          <WhiteCardActionArea>{cardContent}</WhiteCardActionArea>
-        </Link>
+        </CardActionArea>
       ) : (
         cardContent
       )}
