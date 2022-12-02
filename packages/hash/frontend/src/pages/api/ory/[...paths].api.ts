cbb1b00f42dc68259b7bfbce7c0edc589bb26914
Ahmad Sattar <thehabbos007@gmail.com>
2022-12-02 10:55:54 +0100

    Configure forced cookie domain (#1570)

--- packages/hash/frontend/src/pages/api/ory/[...paths].api.ts
+++ packages/hash/frontend/src/pages/api/ory/[...paths].api.ts
@@ -6,9 +6,11 @@ import { frontendDomain } from "@hashintel/hash-shared/environment";
 export { config };
 
 const publicUrl = process.env.ORY_KRATOS_PUBLIC_URL;
+const forceCookieDomain =
+  process.env.FRONTEND_FORCE_COOKIE_DOMAIN ?? frontendDomain;
 
 // And create the Ory Cloud API "bridge".
 export default createApiHandler({
   apiBaseUrlOverride: publicUrl,
-  forceCookieDomain: frontendDomain,
+  forceCookieDomain,
 });
