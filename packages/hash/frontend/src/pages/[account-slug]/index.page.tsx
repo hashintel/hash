2b28320405d7aa1f201a116a612b1395769229fe
Ben Werner <ben.werner01@gmail.com>
2022-12-02 16:15:50 +0000

    fix: padding on the homepage

--- packages/hash/frontend/src/pages/[account-slug]/index.page.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/index.page.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Container } from "@mui/material";
 import { faWarning } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@hashintel/hash-design-system";
 import { getLayoutWithSidebar, NextPageWithLayout } from "../../shared/layout";
@@ -61,51 +61,52 @@ const Page: NextPageWithLayout = () => {
     ? undefined
     : orgs?.find((org) => org.orgAccountId === routeAccountId);
 
-  if (!ownWorkspace && !thisOrg) {
-    return (
-      <h2>This workspace does not exist or you do not have access to it.</h2>
-    );
-  }
-
   return (
-    <>
-      <Typography mb={3} variant="h2">
-        Welcome to HASH
-      </Typography>
-      <Box maxWidth="75ch">
-        <Typography mb={3}>
-          HASH is an open-source, data-centric, all-in-one workspace built atop
-          the open <Link href="https://blockprotocol.org">Block Protocol</Link>.
-        </Typography>
-        <Typography mb={3}>
-          <strong>
-            <FontAwesomeIcon
-              icon={faWarning}
-              sx={({ palette }) => ({
-                color: palette.orange[50],
-                mr: 0.5,
-              })}
-            />{" "}
-            HASH is not ready for production use.
-          </strong>{" "}
-          It is not secure or optimized and is missing key features. Please
-          visit the{" "}
-          <Link href="https://github.com/hashintel/hash/tree/main/packages/hash">
-            GitHub repository
-          </Link>{" "}
-          for the latest updates, or learn about the long-term{" "}
-          <Link href="https://hash.ai">here</Link>.
-        </Typography>
-        <Typography>
-          This version of HASH is intended to be used as a test-harness for
-          developers building Block Protocol-compliant blocks. Please{" "}
-          <Link href="https://github.com/hashintel/hash/tree/main/packages/hash#integration-with-the-block-protocol">
-            read the documentation to get started
-          </Link>
-          .
-        </Typography>
-      </Box>
-    </>
+    <Container sx={{ pt: 7 }}>
+      {!ownWorkspace && !thisOrg ? (
+        <h2>This workspace does not exist or you do not have access to it.</h2>
+      ) : (
+        <>
+          <Typography mb={3} variant="h2">
+            Welcome to HASH
+          </Typography>
+          <Box maxWidth="75ch">
+            <Typography mb={3}>
+              HASH is an open-source, data-centric, all-in-one workspace built
+              atop the open{" "}
+              <Link href="https://blockprotocol.org">Block Protocol</Link>.
+            </Typography>
+            <Typography mb={3}>
+              <strong>
+                <FontAwesomeIcon
+                  icon={faWarning}
+                  sx={({ palette }) => ({
+                    color: palette.orange[50],
+                    mr: 0.5,
+                  })}
+                />{" "}
+                HASH is not ready for production use.
+              </strong>{" "}
+              It is not secure or optimized and is missing key features. Please
+              visit the{" "}
+              <Link href="https://github.com/hashintel/hash/tree/main/packages/hash">
+                GitHub repository
+              </Link>{" "}
+              for the latest updates, or learn about the long-term{" "}
+              <Link href="https://hash.ai">here</Link>.
+            </Typography>
+            <Typography>
+              This version of HASH is intended to be used as a test-harness for
+              developers building Block Protocol-compliant blocks. Please{" "}
+              <Link href="https://github.com/hashintel/hash/tree/main/packages/hash#integration-with-the-block-protocol">
+                read the documentation to get started
+              </Link>
+              .
+            </Typography>
+          </Box>
+        </>
+      )}
+    </Container>
   );
 };
 
