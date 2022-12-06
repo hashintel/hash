50d9993efdeac703416aa85d135451248fcb1090
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-12-06 16:14:48 +0100

    feat: continue work on hooks

--- packages/libs/error-stack/src/serde/hook.rs
+++ packages/libs/error-stack/src/serde/hook.rs
@@ -1,15 +1,38 @@
-use std::any::TypeId;
+use std::any::{Any, TypeId};
 
-use serde::{Serialize, Serializer};
+use serde::{Serialize, Serializer as _};
 
 use crate::Frame;
+
+struct HookContext {}
+
+struct Serializer {
+    inner: Box<dyn Any + Send + Sync>,
+    consume: fn(Box<dyn Any + Send + Sync>),
+}
+
+impl Serializer {
+    fn serialize<S: Serialize>(self, value: S) -> Result<(), ()> {}
+}
 
 fn serialize<T: serde::Serialize>(
     frame: &Frame,
     serializer: Box<dyn erased_serde::Serializer>,
-) -> Option<Result<erased_serde::>> {
+) -> Option<erased_serde::Result<()>> {
     let value: &T = frame.request_ref()?;
     Some(value.serialize(serializer))
+}
+
+// TODO: Storage coming later
+
+enum HookFn {
+    Static(fn(&Frame, Box<dyn erased_serde::Serializer>) -> Option<erased_serde::Result<()>>),
+    Dynamic(Box<dyn Fn(&Frame, &mut HookContext, Box<dyn erased_serde::Serializer>) -> ()>),
+}
+
+struct Hook {
+    ty: TypeId,
+    hook: fn(&Frame),
 }
 
 pub(crate) struct Hooks {
