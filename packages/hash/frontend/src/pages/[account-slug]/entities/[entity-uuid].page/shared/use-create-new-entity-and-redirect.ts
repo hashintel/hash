1628611c39381d5217d2f0d2bda4931db59d06c7
Yusuf Kınataş <yusufkinatas@gmail.com>
2022-12-06 23:43:49 +0300

    Generate property table rows using entity type schema (#1593)

--- packages/hash/frontend/src/pages/[account-slug]/entities/[entity-uuid].page/shared/use-create-new-entity-and-redirect.ts
+++ packages/hash/frontend/src/pages/[account-slug]/entities/[entity-uuid].page/shared/use-create-new-entity-and-redirect.ts
@@ -1,89 +1,13 @@
-import {
-  EntityType,
-  PropertyType,
-  VersionedUri,
-} from "@blockprotocol/type-system";
-import {
-  extractEntityUuidFromEntityId,
-  PropertyObject,
-  Subgraph,
-} from "@hashintel/hash-subgraph";
+import { VersionedUri } from "@blockprotocol/type-system";
+import { extractEntityUuidFromEntityId } from "@hashintel/hash-subgraph";
 import { getEntityTypeById } from "@hashintel/hash-subgraph/src/stdlib/element/entity-type";
-import { getPropertyTypeById } from "@hashintel/hash-subgraph/src/stdlib/element/property-type";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { useBlockProtocolCreateEntity } from "../../../../../components/hooks/blockProtocolFunctions/knowledge/useBlockProtocolCreateEntity";
 import { useBlockProtocolGetEntityType } from "../../../../../components/hooks/blockProtocolFunctions/ontology/useBlockProtocolGetEntityType";
+import { User } from "../../../../../lib/user";
 import { getOwnedById } from "../../../../../lib/get-owned-by-id";
 import { MinimalOrg } from "../../../../../lib/org";
-import {
-  isPropertyValueArray,
-  isPropertyValueNested,
-} from "../../../../../lib/typeguards";
-import { User } from "../../../../../lib/user";
-
-/**
- * @todo this will be deleted when https://app.asana.com/0/1203312852763953/1203433085114587/f (internal) is implemented
- */
-const getDefaultValueOfPropertyType = (
-  propertyType: PropertyType | undefined,
-  subgraph: Subgraph,
-) => {
-  /**
-   * if there are multiple expected types, those are not expected to be arrays or nested properties.
-   * So it's safe to return empty string on this case
-   */
-  if (!propertyType || propertyType.oneOf.length > 1) {
-    return "";
-  }
-
-  const propertyValue = propertyType.oneOf[0];
-
-  // return empty array for arrays
-  if (isPropertyValueArray(propertyValue)) {
-    return [];
-  }
-
-  // recursively get default properties for nested properties
-  if (isPropertyValueNested(propertyValue)) {
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    return generateDefaultProperties(propertyValue.properties, subgraph);
-  }
-
-  // empty string works for number, text & boolean
-  return "";
-};
-
-/**
- * @todo this will be deleted when https://app.asana.com/0/1203312852763953/1203433085114587/f (internal) is implemented
- */
-export const generateDefaultProperties = (
-  properties: EntityType["properties"],
-  subgraph: Subgraph,
-) => {
-  const result: PropertyObject = {};
-
-  for (const propertyKey of Object.keys(properties)) {
-    const property = properties[propertyKey];
-
-    if (property) {
-      const propertyTypeId =
-        "$ref" in property ? property.$ref : property.items.$ref;
-
-      const propertyType = getPropertyTypeById(
-        subgraph,
-        propertyTypeId,
-      )?.schema;
-
-      result[propertyKey] = getDefaultValueOfPropertyType(
-        propertyType,
-        subgraph,
-      );
-    }
-  }
-
-  return result;
-};
 
 export const useCreateNewEntityAndRedirect = () => {
   const router = useRouter();
@@ -129,15 +53,7 @@ export const useCreateNewEntityAndRedirect = () => {
         data: {
           entityTypeId: entityType.$id,
           ownedById,
-          /**
-           * @todo after implementing this ticket: https://app.asana.com/0/1203312852763953/1203433085114587/f (internal)
-           * we should just use `properties: {}` here, and delete `generateDefaultProperties` function,
-           * this is a temporary workaround for entity table to show the rows with empty values
-           */
-          properties: generateDefaultProperties(
-            entityType.properties,
-            subgraph,
-          ),
+          properties: {},
         },
       });
 
