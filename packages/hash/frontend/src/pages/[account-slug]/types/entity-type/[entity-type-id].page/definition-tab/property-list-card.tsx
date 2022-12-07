7f9b92bb6df693af4c5687363b65c98251ae4605
Nate Higgins <nate@nateh.uk>
2022-12-07 13:06:10 +0000

    Extract entity type table

--- packages/hash/frontend/src/pages/[account-slug]/types/entity-type/[entity-type-id].page/definition-tab/property-list-card.tsx
+++ packages/hash/frontend/src/pages/[account-slug]/types/entity-type/[entity-type-id].page/definition-tab/property-list-card.tsx
@@ -1,23 +1,13 @@
 import { VersionedUri } from "@blockprotocol/type-system";
 import {
-  Box,
-  ButtonBase,
   Checkbox,
-  checkboxClasses,
   iconButtonClasses,
-  svgIconClasses,
-  Table,
   TableBody,
-  tableBodyClasses,
   TableCell,
-  tableCellClasses,
   TableFooter,
   TableHead,
-  TableRow,
   tableRowClasses,
-  Typography,
 } from "@mui/material";
-import { experimental_sx, styled } from "@mui/system";
 import { bindTrigger, usePopupState } from "material-ui-popup-state/hooks";
 import { useId, useLayoutEffect, useRef } from "react";
 import {
@@ -27,7 +17,14 @@ import {
   useWatch,
 } from "react-hook-form";
 import { useBlockProtocolUpdatePropertyType } from "../../../../../../components/hooks/blockProtocolFunctions/ontology/useBlockProtocolUpdatePropertyType";
-import { WhiteCard } from "../../../../shared/white-card";
+import {
+  EntityTypeTable,
+  EntityTypeTableButtonRow,
+  EntityTypeTableCenteredCell,
+  EntityTypeTableHeaderRow,
+  EntityTypeTableRow,
+  EntityTypeTableTitleCellText,
+} from "./property-list-card/entity-type-table";
 import {
   usePropertyTypes,
   useRefetchPropertyTypes,
@@ -45,13 +42,6 @@ import {
 import { QuestionIcon } from "./property-list-card/shared/question-icon";
 import { StyledPlusCircleIcon } from "../../../../shared/styled-plus-circle-icon";
 import { useStateCallback } from "./property-list-card/use-state-callback";
-
-const CenteredTableCell = styled(TableCell)(
-  experimental_sx({
-    px: "0px !important",
-    textAlign: "center",
-  }),
-);
 
 export const PropertyTypeRow = ({
   propertyIndex,
@@ -102,31 +92,11 @@ export const PropertyTypeRow = ({
 
   return (
     <>
-      <TableRow
-        sx={[
-          (theme) => ({
-            [`.${tableCellClasses.root}`]: {
-              "&:first-of-type": {
-                borderTopLeftRadius: theme.borderRadii.md,
-                borderBottomLeftRadius: theme.borderRadii.md,
-              },
-              "&:last-of-type": {
-                borderTopRightRadius: theme.borderRadii.md,
-                borderBottomRightRadius: theme.borderRadii.md,
-              },
-            },
-          }),
-          (theme) => ({
-            [`&:hover .${tableCellClasses.root}`]: {
-              background: theme.palette.gray[10],
-            },
-          }),
-        ]}
-      >
+      <EntityTypeTableRow>
         <TableCell>
-          <Typography variant="smallTextLabels" fontWeight={500}>
+          <EntityTypeTableTitleCellText>
             {property.title}
-          </Typography>
+          </EntityTypeTableTitleCellText>
         </TableCell>
         <TableCell>
           <PropertyExpectedValues property={property} />
@@ -134,7 +104,7 @@ export const PropertyTypeRow = ({
 
         <MultipleValuesCell propertyIndex={propertyIndex} />
 
-        <CenteredTableCell sx={{ textAlign: "center" }}>
+        <EntityTypeTableCenteredCell>
           <Controller
             render={({ field: { value, ...field } }) => (
               <Checkbox {...field} checked={value} />
@@ -142,7 +112,7 @@ export const PropertyTypeRow = ({
             control={control}
             name={`properties.${propertyIndex}.required`}
           />
-        </CenteredTableCell>
+        </EntityTypeTableCenteredCell>
 
         <TableCell
           sx={{
@@ -161,7 +131,7 @@ export const PropertyTypeRow = ({
             popupState={menuPopupState}
           />
         </TableCell>
-      </TableRow>
+      </EntityTypeTableRow>
       <PropertyTypeForm
         popupState={editModalPopupState}
         modalTitle={<>Edit Property Type</>}
@@ -229,142 +199,74 @@ export const PropertyListCard = () => {
   }
 
   return (
-    <WhiteCard>
-      <Box sx={{ p: 0.5 }}>
-        <Table
-          sx={(theme) => ({
-            height: "100%",
-            [`.${tableCellClasses.root}`]: {
-              pl: 3.5,
-              pr: 1,
-              py: 0.5,
-              border: "none",
-            },
-            [`.${tableCellClasses.head}`]: {
-              py: 1.5,
-              borderBottom: 1,
-              borderColor: theme.palette.gray[20],
-              fontWeight: "inherit",
-              lineHeight: "inherit",
-
-              [`.${svgIconClasses.root}`]: {
-                verticalAlign: "middle",
-                ml: 0.75,
-              },
-            },
-            [`.${tableBodyClasses.root}:before`]: {
-              lineHeight: "6px",
-              content: `"\\200C"`,
-              display: "block",
-            },
-            [`.${tableCellClasses.body} .${checkboxClasses.root}`]: {
-              textAlign: "center",
-            },
-          })}
-        >
-          <TableHead>
-            <Typography
-              component={TableRow}
-              variant="smallTextLabels"
-              sx={{
-                fontWeight: 600,
-              }}
-            >
-              <TableCell>Property name</TableCell>
-              <TableCell width={180}>Expected values</TableCell>
-              <CenteredTableCell width={170}>
-                Allow multiple values <QuestionIcon />
-              </CenteredTableCell>
-              <CenteredTableCell width={100}>Required</CenteredTableCell>
-              <TableCell width={70} />
-            </Typography>
-          </TableHead>
-          <TableBody>
-            {fields.map((type, index) => (
-              <PropertyTypeRow
-                key={type.id}
-                propertyIndex={index}
-                onRemove={() => {
-                  remove(index);
-                }}
-                onUpdatePropertyTypeVersion={(nextId) => {
-                  setValue(`properties.${index}.$id`, nextId, {
-                    shouldDirty: true,
-                  });
-                }}
-              />
-            ))}
-          </TableBody>
-          <TableFooter>
-            {addingNewProperty ? (
-              <InsertPropertyRow
-                inputRef={addingNewPropertyRef}
-                onCancel={() => {
-                  setAddingNewProperty(false);
-                }}
-                onAdd={(propertyType) => {
-                  setAddingNewProperty(false);
-                  if (
-                    !getValues("properties").some(
-                      ({ $id }) => $id === propertyType.$id,
-                    )
-                  ) {
-                    append({
-                      $id: propertyType.$id,
-                      required: false,
-                      array: false,
-                      minValue: 0,
-                      maxValue: 1,
-                      infinity: true,
-                    });
-                  }
-                }}
-              />
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={
-                    // Sufficiently large to span full width
-                    100
-                  }
-                  sx={{
-                    p: "0 !important",
-                  }}
-                >
-                  <ButtonBase
-                    disableRipple
-                    disableTouchRipple
-                    onClick={() => {
-                      setAddingNewProperty(true, () => {
-                        addingNewPropertyRef.current?.focus();
-                      });
-                    }}
-                    sx={(theme) => ({
-                      color: theme.palette.gray[50],
-                      py: 1.5,
-                      width: "100%",
-                      borderRadius: 1,
-                      "&:hover": {
-                        backgroundColor: theme.palette.gray[10],
-                        color: theme.palette.gray[70],
-                      },
-                    })}
-                  >
-                    <StyledPlusCircleIcon />
-                    <Typography
-                      variant="smallTextLabels"
-                      fontWeight={500}
-                      ml={1}
-                    >
-                      Add a property
-                    </Typography>
-                  </ButtonBase>
-                </TableCell>
-              </TableRow>
-            )}
-          </TableFooter>
-        </Table>
-      </Box>
-    </WhiteCard>
+    <EntityTypeTable>
+      <TableHead>
+        <EntityTypeTableHeaderRow>
+          <TableCell>Property name</TableCell>
+          <TableCell width={180}>Expected values</TableCell>
+          <EntityTypeTableCenteredCell width={170}>
+            Allow multiple values <QuestionIcon />
+          </EntityTypeTableCenteredCell>
+          <EntityTypeTableCenteredCell width={100}>
+            Required
+          </EntityTypeTableCenteredCell>
+          <TableCell width={70} />
+        </EntityTypeTableHeaderRow>
+      </TableHead>
+      <TableBody>
+        {fields.map((type, index) => (
+          <PropertyTypeRow
+            key={type.id}
+            propertyIndex={index}
+            onRemove={() => {
+              remove(index);
+            }}
+            onUpdatePropertyTypeVersion={(nextId) => {
+              setValue(`properties.${index}.$id`, nextId, {
+                shouldDirty: true,
+              });
+            }}
+          />
+        ))}
+      </TableBody>
+      <TableFooter>
+        {addingNewProperty ? (
+          <InsertPropertyRow
+            inputRef={addingNewPropertyRef}
+            onCancel={() => {
+              setAddingNewProperty(false);
+            }}
+            onAdd={(propertyType) => {
+              setAddingNewProperty(false);
+              if (
+                !getValues("properties").some(
+                  ({ $id }) => $id === propertyType.$id,
+                )
+              ) {
+                append({
+                  $id: propertyType.$id,
+                  required: false,
+                  array: false,
+                  minValue: 0,
+                  maxValue: 1,
+                  infinity: true,
+                });
+              }
+            }}
+          />
+        ) : (
+          <EntityTypeTableButtonRow
+            icon={<StyledPlusCircleIcon />}
+            onClick={() => {
+              setAddingNewProperty(true, () => {
+                addingNewPropertyRef.current?.focus();
+              });
+            }}
+          >
+            Add a property
+          </EntityTypeTableButtonRow>
+        )}
+      </TableFooter>
+    </EntityTypeTable>
   );
 };
