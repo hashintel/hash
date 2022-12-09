dd7ab9e253b5a8471b53854b13cd5ec2b64e779e
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2022-12-09 16:59:02 +0100

    Fix benchmark seeding logic (#1631)

--- packages/graph/hash_graph/lib/graph/src/store/postgres/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/store/postgres/mod.rs
@@ -25,8 +25,6 @@ use uuid::Uuid;
 
 use self::context::OntologyRecord;
 pub use self::pool::{AsClient, PostgresStorePool};
-#[cfg(feature = "__internal_bench")]
-use crate::knowledge::{EntityProperties, EntityUuid, LinkData};
 use crate::{
     identifier::{account::AccountId, knowledge::EntityEditionId, ontology::OntologyTypeEditionId},
     ontology::OntologyElementMetadata,
@@ -40,6 +38,14 @@ use crate::{
         UpdateError,
     },
     subgraph::edges::GraphResolveDepths,
+};
+#[cfg(feature = "__internal_bench")]
+use crate::{
+    identifier::{
+        knowledge::{EntityId, EntityRecordId, EntityVersion},
+        DecisionTimespan, DecisionTimestamp, TransactionTimespan,
+    },
+    knowledge::{EntityProperties, LinkOrder},
 };
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -621,27 +627,53 @@ where
 impl PostgresStore<Transaction<'_>> {
     #[doc(hidden)]
     #[cfg(feature = "__internal_bench")]
-    async fn insert_entity_uuids(
+    async fn insert_entity_ids(
         &self,
-        entity_uuids: impl IntoIterator<Item = EntityUuid, IntoIter: Send> + Send,
+        entity_uuids: impl IntoIterator<
+            Item = (EntityId, Option<EntityId>, Option<EntityId>),
+            IntoIter: Send,
+        > + Send,
     ) -> Result<u64, InsertionError> {
         let sink = self
             .client
-            .copy_in("COPY entity_uuids (entity_uuid) FROM STDIN BINARY")
+            .copy_in(
+                "COPY entity_ids (
+                    owned_by_id,
+                    entity_uuid,
+                    left_owned_by_id,
+                    left_entity_uuid,
+                    right_owned_by_id,
+                    right_entity_uuid
+                ) FROM STDIN BINARY",
+            )
             .await
             .into_report()
             .change_context(InsertionError)?;
-        let writer = BinaryCopyInWriter::new(sink, &[Type::UUID]);
+        let writer = BinaryCopyInWriter::new(sink, &[
+            Type::UUID,
+            Type::UUID,
+            Type::UUID,
+            Type::UUID,
+            Type::UUID,
+            Type::UUID,
+        ]);
 
         futures::pin_mut!(writer);
-        for entity_uuid in entity_uuids {
+        for (entity_id, left_entity_id, right_entity_id) in entity_uuids {
             writer
                 .as_mut()
-                .write(&[&entity_uuid.as_uuid()])
+                .write(&[
+                    &entity_id.owned_by_id(),
+                    &entity_id.entity_uuid(),
+                    &left_entity_id.as_ref().map(EntityId::owned_by_id),
+                    &left_entity_id.as_ref().map(EntityId::entity_uuid),
+                    &right_entity_id.as_ref().map(EntityId::owned_by_id),
+                    &right_entity_id.as_ref().map(EntityId::entity_uuid),
+                ])
                 .await
                 .into_report()
                 .change_context(InsertionError)
-                .attach_printable(entity_uuid)?;
+                .attach_printable(entity_id.entity_uuid())?;
         }
 
         writer
@@ -653,22 +685,149 @@ impl PostgresStore<Transaction<'_>> {
 
     #[doc(hidden)]
     #[cfg(feature = "__internal_bench")]
-    async fn insert_entity_batch_by_type(
+    async fn insert_entity_records(
         &self,
-        entity_uuids: impl IntoIterator<Item = EntityUuid, IntoIter: Send> + Send,
-        entities: impl IntoIterator<Item = EntityProperties, IntoIter: Send> + Send,
-        link_datas: impl IntoIterator<Item = Option<LinkData>, IntoIter: Send> + Send,
+        entities: impl IntoIterator<
+            Item = (EntityProperties, Option<LinkOrder>, Option<LinkOrder>),
+            IntoIter: Send,
+        > + Send,
         entity_type_version_id: VersionId,
-        owned_by_id: OwnedById,
-        updated_by_id: UpdatedById,
-    ) -> Result<u64, InsertionError> {
+        actor_id: UpdatedById,
+    ) -> Result<Vec<EntityRecordId>, InsertionError> {
+        self.client
+            .simple_query(
+                "CREATE TEMPORARY TABLE entity_editions_temp (
+                    updated_by_id UUID NOT NULL,
+                    archived BOOLEAN NOT NULL,
+                    entity_type_version_id UUID NOT NULL,
+                    properties JSONB NOT NULL,
+                    left_to_right_order INT,
+                    right_to_left_order INT
+                );",
+            )
+            .await
+            .into_report()
+            .change_context(InsertionError)?;
+
         let sink = self
             .client
             .copy_in(
-                "COPY latest_entities (entity_uuid, entity_type_version_id, properties, \
-                 owned_by_id, updated_by_id, left_owned_by_id, left_entity_uuid, \
-                 right_owned_by_id, right_entity_uuid, left_to_right_order, right_to_left_order) \
-                 FROM STDIN BINARY",
+                "COPY entity_editions_temp (
+                    updated_by_id,
+                    archived,
+                    entity_type_version_id,
+                    properties,
+                    left_to_right_order,
+                    right_to_left_order
+                ) FROM STDIN BINARY",
+            )
+            .await
+            .into_report()
+            .change_context(InsertionError)?;
+        let writer = BinaryCopyInWriter::new(sink, &[
+            Type::UUID,
+            Type::BOOL,
+            Type::UUID,
+            Type::JSONB,
+            Type::INT4,
+            Type::INT4,
+        ]);
+        futures::pin_mut!(writer);
+        for (properties, left_to_right_order, right_to_left_order) in entities {
+            let properties = serde_json::to_value(properties)
+                .into_report()
+                .change_context(InsertionError)?;
+
+            writer
+                .as_mut()
+                .write(&[
+                    &actor_id,
+                    &false,
+                    &entity_type_version_id,
+                    &properties,
+                    &left_to_right_order,
+                    &right_to_left_order,
+                ])
+                .await
+                .into_report()
+                .change_context(InsertionError)?;
+        }
+
+        writer
+            .finish()
+            .await
+            .into_report()
+            .change_context(InsertionError)?;
+
+        let entity_record_ids = self
+            .client
+            .query(
+                "INSERT INTO entity_editions (
+                    updated_by_id,
+                    archived,
+                    entity_type_version_id,
+                    properties,
+                    left_to_right_order,
+                    right_to_left_order
+                )
+                SELECT
+                    updated_by_id,
+                    archived,
+                    entity_type_version_id,
+                    properties,
+                    left_to_right_order,
+                    right_to_left_order
+                FROM entity_editions_temp
+                RETURNING entity_record_id;",
+                &[],
+            )
+            .await
+            .into_report()
+            .change_context(InsertionError)?
+            .into_iter()
+            .map(|row| EntityRecordId::new(row.get::<_, i64>(0)))
+            .collect();
+
+        self.client
+            .simple_query("DROP TABLE entity_editions_temp;")
+            .await
+            .into_report()
+            .change_context(InsertionError)?;
+
+        Ok(entity_record_ids)
+    }
+
+    #[doc(hidden)]
+    #[cfg(feature = "__internal_bench")]
+    async fn insert_entity_versions(
+        &self,
+        entities: impl IntoIterator<
+            Item = (EntityId, EntityRecordId, Option<DecisionTimestamp>),
+            IntoIter: Send,
+        > + Send,
+    ) -> Result<Vec<EntityVersion>, InsertionError> {
+        self.client
+            .simple_query(
+                "CREATE TEMPORARY TABLE entity_versions_temp (
+                    owned_by_id UUID NOT NULL,
+                    entity_uuid UUID NOT NULL,
+                    entity_record_id BIGINT NOT NULL,
+                    decision_time TIMESTAMP WITH TIME ZONE
+                );",
+            )
+            .await
+            .into_report()
+            .change_context(InsertionError)?;
+
+        let sink = self
+            .client
+            .copy_in(
+                "COPY entity_versions_temp (
+                    owned_by_id,
+                    entity_uuid,
+                    entity_record_id,
+                    decision_time
+                ) FROM STDIN BINARY",
             )
             .await
             .into_report()
@@ -676,57 +835,72 @@ impl PostgresStore<Transaction<'_>> {
         let writer = BinaryCopyInWriter::new(sink, &[
             Type::UUID,
             Type::UUID,
-            Type::JSONB,
-            Type::UUID,
-            Type::UUID,
-            Type::UUID,
-            Type::UUID,
-            Type::UUID,
-            Type::UUID,
-            Type::INT4,
-            Type::INT4,
+            Type::INT8,
+            Type::TIMESTAMPTZ,
         ]);
         futures::pin_mut!(writer);
-        for ((entity_uuid, entity), link_data) in
-            entity_uuids.into_iter().zip(entities).zip(link_datas)
-        {
-            let value = serde_json::to_value(entity)
-                .into_report()
-                .change_context(InsertionError)?;
+        for (entity_id, entity_record_id, decision_time) in entities {
             writer
                 .as_mut()
                 .write(&[
-                    &entity_uuid.as_uuid(),
-                    &entity_type_version_id,
-                    &value,
-                    &owned_by_id.as_account_id(),
-                    &updated_by_id.as_account_id(),
-                    &link_data
-                        .as_ref()
-                        .map(|metadata| metadata.left_entity_id().owned_by_id().as_account_id()),
-                    &link_data
-                        .as_ref()
-                        .map(|metadata| metadata.left_entity_id().entity_uuid().as_uuid()),
-                    &link_data
-                        .as_ref()
-                        .map(|metadata| metadata.right_entity_id().owned_by_id().as_account_id()),
-                    &link_data
-                        .as_ref()
-                        .map(|metadata| metadata.right_entity_id().entity_uuid().as_uuid()),
-                    &link_data.as_ref().and_then(LinkData::left_to_right_order),
-                    &link_data.as_ref().and_then(LinkData::right_to_left_order),
+                    &entity_id.owned_by_id(),
+                    &entity_id.entity_uuid(),
+                    &entity_record_id,
+                    &decision_time,
                 ])
                 .await
                 .into_report()
-                .change_context(InsertionError)
-                .attach_printable(entity_uuid)?;
+                .change_context(InsertionError)?;
         }
 
         writer
             .finish()
             .await
             .into_report()
-            .change_context(InsertionError)
+            .change_context(InsertionError)?;
+
+        let entity_versions = self
+            .client
+            .query(
+                "INSERT INTO entity_versions (
+                    owned_by_id,
+                    entity_uuid,
+                    entity_record_id,
+                    decision_time,
+                    transaction_time
+                ) SELECT
+                    owned_by_id,
+                    entity_uuid,
+                    entity_record_id,
+                    tstzrange(
+                        CASE WHEN decision_time IS NULL THEN now() ELSE decision_time END,
+                        'infinity',
+                        '[)'
+                    ),
+                    tstzrange(now(), 'infinity', '[)')
+                FROM entity_versions_temp
+                RETURNING decision_time, transaction_time;",
+                &[],
+            )
+            .await
+            .into_report()
+            .change_context(InsertionError)?
+            .into_iter()
+            .map(|row| {
+                EntityVersion::new(
+                    DecisionTimespan::new(row.get(0)),
+                    TransactionTimespan::new(row.get(1)),
+                )
+            })
+            .collect();
+
+        self.client
+            .simple_query("DROP TABLE entity_versions_temp;")
+            .await
+            .into_report()
+            .change_context(InsertionError)?;
+
+        Ok(entity_versions)
     }
 }
 
