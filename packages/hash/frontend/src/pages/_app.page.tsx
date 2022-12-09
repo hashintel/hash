d51d4404a3c063a63e35e31dd1dfebea4084b9e0
Ben Werner <ben.werner01@gmail.com>
2022-12-09 15:08:50 +0000

    fix: next build

--- packages/hash/frontend/src/pages/_app.page.tsx
+++ packages/hash/frontend/src/pages/_app.page.tsx
@@ -167,6 +167,10 @@ const redirectInGetInitialProps = (params: {
   } = params;
 
   if (req && res) {
+    if (!res.writeHead) {
+      // `res.writeHead` is not a function whilst building next, so return instead.
+      return;
+    }
     res.writeHead(307, { Location: location });
     res.end();
   } else {
