f397dc64d71f136c20c338b8a83f662066656773
Ben Werner <ben.werner01@gmail.com>
2022-12-09 10:50:28 +0000

    ft: add `refetch` function to `AuthInfoContext`

--- packages/hash/frontend/src/pages/shared/auth-info-context.tsx
+++ packages/hash/frontend/src/pages/shared/auth-info-context.tsx
@@ -1,19 +1,32 @@
+import { useLazyQuery } from "@apollo/client";
+import { Subgraph, SubgraphRootTypes } from "@hashintel/hash-subgraph";
 import {
   createContext,
   FunctionComponent,
   ReactElement,
+  useCallback,
   useContext,
   useEffect,
   useMemo,
   useState,
 } from "react";
-import { AuthenticatedUser } from "../../lib/user";
+import { MeQuery } from "../../graphql/apiTypes.gen";
+import { meQuery } from "../../graphql/queries/user.queries";
+import { AuthenticatedUser, constructAuthenticatedUser } from "../../lib/user";
+import { fetchKratosSession } from "./ory-kratos";
+
+type RefetchAuthInfoFunction = () => Promise<{
+  authenticatedUser?: AuthenticatedUser;
+}>;
 
 type AuthInfoState = {
   authenticatedUser?: AuthenticatedUser;
+  refetch: RefetchAuthInfoFunction;
 };
 
-export const AuthInfoContext = createContext<AuthInfoState>({});
+export const AuthInfoContext = createContext<AuthInfoState | undefined>(
+  undefined,
+);
 
 type AuthInfoProviderProps = {
   initialAuthenticatedUser?: AuthenticatedUser;
@@ -32,11 +45,43 @@ export const AuthInfoProvider: FunctionComponent<AuthInfoProviderProps> = ({
     setAuthenticatedUser(initialAuthenticatedUser);
   }, [initialAuthenticatedUser]);
 
+  const [getMe] = useLazyQuery<MeQuery>(meQuery);
+
+  const refetch = useCallback<RefetchAuthInfoFunction>(async () => {
+    const [subgraph, kratosSession] = await Promise.all([
+      getMe()
+        .then(({ data }) => data?.me)
+        .catch(() => undefined),
+      fetchKratosSession(),
+    ]);
+
+    if (!subgraph || !kratosSession) {
+      setAuthenticatedUser(undefined);
+      return {};
+    }
+    const userEntityEditionId = (
+      subgraph as Subgraph<SubgraphRootTypes["entity"]>
+    ).roots[0]!;
+
+    const latestAuthenticatedUser = constructAuthenticatedUser({
+      userEntityEditionId,
+      subgraph,
+      kratosSession,
+    });
+
+    setAuthenticatedUser(latestAuthenticatedUser);
+
+    return {
+      authenticatedUser: latestAuthenticatedUser,
+    };
+  }, [getMe]);
+
   const value = useMemo(
     () => ({
       authenticatedUser,
+      refetch,
     }),
-    [authenticatedUser],
+    [authenticatedUser, refetch],
   );
 
   return (
@@ -49,17 +94,23 @@ export const AuthInfoProvider: FunctionComponent<AuthInfoProviderProps> = ({
 export const useAuthInfo = (): AuthInfoState => {
   const authInfo = useContext(AuthInfoContext);
 
+  if (!authInfo) {
+    throw new Error(
+      "Cannot use auth info because it has not been defined. Verify `useAuthInfo` is being called within a child of the `AuthInfoProvider`.",
+    );
+  }
+
   return authInfo;
 };
 
-export const useAuthenticatedUser = (): {
+export const useAuthenticatedUser = (): AuthInfoState & {
   authenticatedUser: AuthenticatedUser;
 } => {
-  const { authenticatedUser } = useAuthInfo();
+  const { authenticatedUser, ...remainingAuthInfo } = useAuthInfo();
 
   if (!authenticatedUser) {
     throw new Error("No authenticated user.");
   }
 
-  return { authenticatedUser };
+  return { authenticatedUser, ...remainingAuthInfo };
 };
