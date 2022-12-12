c7f7f16bef602d521e522604a310166c0efac19d
Ahmad Sattar <thehabbos007@gmail.com>
2022-12-12 18:39:59 +0100

    Add Uuid alias and remove entityId

--- packages/hash/shared/src/types.ts
+++ packages/hash/shared/src/types.ts
@@ -1,11 +1,10 @@
 // The types exported here are expected to be branded.
-// Perhaps some of these should be put into the subgraph stdlib instead?
 
-export type ownedById = string;
-export type AccountId = string;
+export type Uuid = string;
+export type AccountId = Uuid;
+export type ownedById = Uuid;
 
-export type EntityId = string;
-export type EntityUuid = string;
+export type EntityUuid = Uuid;
 export type EntityEditionId = string;
 
 // Since we already use a template string here, we will keep it. When we
