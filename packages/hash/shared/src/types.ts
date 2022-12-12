9d6df68b803356d48dcc40bd5ee5ae36172eea91
Ahmad Sattar <thehabbos007@gmail.com>
2022-12-12 18:41:35 +0100

    Remove definitions we have upstream too

--- packages/hash/shared/src/types.ts
+++ packages/hash/shared/src/types.ts
@@ -5,9 +5,3 @@ export type AccountId = Uuid;
 export type ownedById = Uuid;
 
 export type EntityUuid = Uuid;
-export type EntityEditionId = string;
-
-// Since we already use a template string here, we will keep it. When we
-// brand these types, this may not be needed anymore.
-export type VersionedUri = `${string}/v/${number}`;
-export type BaseUri = string;
