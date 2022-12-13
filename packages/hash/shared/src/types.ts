0cbf4ea8131d3dcc5391c175656366ee07b951da
Ahmad Sattar <thehabbos007@gmail.com>
2022-12-13 17:20:42 +0100

    Asserting functions for `extract*FromEntityId

--- packages/hash/shared/src/types.ts
+++ packages/hash/shared/src/types.ts
@@ -1,8 +1,41 @@
-// The types exported here are expected to be branded.
+import {
+  EntityId,
+  extractEntityUuidFromEntityId as extractEntityUuidFromEntityIdSubGraph,
+  extractOwnedByIdFromEntityId as extractOwnedByIdFromEntityIdSubGraph,
+} from "@hashintel/hash-subgraph";
 
-export type Uuid = string;
-export type AccountId = Uuid;
-export type OwnedById = Uuid;
-export type UpdatedById = Uuid;
+type Branded<Base, Kind extends string> = {
+  /** The type of the value space that is branded */
+  readonly _base: Base;
+  /** The unique name for the branded type. */
+  readonly _kind: Kind;
+};
 
-export type EntityUuid = Uuid;
+/**
+ * The type-branding type to support nominal (name based) types.
+ */
+export type Brand<Base, Kind extends string> = Base & Branded<Base, Kind>;
+
+/** An account ID of an actor that is the owner of something */
+export type OwnedById = Brand<string, "OwnedById">;
+/** An account ID of an actor that has updated something */
+export type UpdatedById = Brand<string, "UpdatedById">;
+/** An ID to uniquely identify an account (e.g. a User or an Org) */
+export type AccountId = Brand<string, "AccountId"> | OwnedById | UpdatedById;
+
+/** A Uuuid that points to an Entity without any edition */
+export type EntityUuid = Brand<string, "EntityUuid">;
+/** Valid Uuids of the system */
+export type Uuid = Brand<string, "Uuid"> | AccountId | EntityUuid;
+
+// These type overwrites are centralized for being able to swap out implementations.
+
+export const extractOwnedByIdFromEntityId =
+  extractOwnedByIdFromEntityIdSubGraph as (entityId: EntityId) => OwnedById;
+
+export const extractEntityUuidFromEntityId =
+  extractEntityUuidFromEntityIdSubGraph as (entityId: EntityId) => EntityUuid;
+
+/** If the underlying entityUuid is an accountId, use this cast to convert the type */
+export const extractAccountIdAsEntityUuid =
+  extractEntityUuidFromEntityIdSubGraph as (entityId: EntityId) => AccountId;
