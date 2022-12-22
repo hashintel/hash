5c44c7d00c50455d804a79b54f8d7526ceb45435
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-12-22 13:06:50 +0100

    feat: it works?

--- packages/libs/error-stack/src/serde/hook.rs
+++ packages/libs/error-stack/src/serde/hook.rs
@@ -3,7 +3,7 @@ use core::any::TypeId;
 
 use crate::Frame;
 
-struct Serde {}
+pub struct Serde {}
 
 pub type HookContext<T> = crate::hook::context::HookContext<Serde, T>;
 
@@ -14,52 +14,56 @@ fn serialize<'a, T: serde::Serialize + Send + Sync + 'static>(
     Some(Box::new(value))
 }
 
-trait ErasedFn<'a> {
-    fn call(
+// Thanks to https://users.rust-lang.org/t/hrtb-on-multiple-generics/34255/2 for the solution
+pub trait DynamicFn<'a, I>: 'static
+where
+    I: Send + Sync + 'static,
+{
+    type Output: serde::Serialize + 'a;
+
+    fn call(&self, value: &'a I, context: &mut HookContext<I>) -> Self::Output;
+
+    fn erase(
         &self,
         value: &'a Frame,
         context: &mut HookContext<Frame>,
     ) -> Option<Box<dyn erased_serde::Serialize + 'a>>;
 }
 
-impl<'a, F> ErasedFn<'a> for F
+impl<'a, U: 'a, T, F> DynamicFn<'a, T> for F
 where
-    F: Fn(&Frame, &mut HookContext<Frame>) -> Option<Box<dyn erased_serde::Serialize + 'a>>,
+    F: Fn(&'a T, &mut HookContext<T>) -> U,
+    F: 'static,
+    U: serde::Serialize,
+    T: Send + Sync + 'static,
 {
-    fn call(
+    type Output = U;
+
+    fn call(&self, value: &'a T, context: &mut HookContext<T>) -> Self::Output {
+        (self)(value, context)
+    }
+
+    fn erase(
         &self,
         value: &'a Frame,
         context: &mut HookContext<Frame>,
     ) -> Option<Box<dyn erased_serde::Serialize + 'a>> {
-        (self)(value, context)
-    }
-}
+        let value = value.request_ref::<T>()?;
 
-// Thanks to https://users.rust-lang.org/t/hrtb-on-multiple-generics/34255/2 for the solution
-pub trait DynamicFn<'a> {
-    type Input: Send + Sync + 'static;
-    type Output: serde::Serialize + 'a;
+        let value = <Self as DynamicFn<'a, T>>::call(self, value, context.cast());
 
-    fn call(&self, value: &'a Self::Input, context: &mut HookContext<Self::Input>) -> Self::Output;
-}
-
-impl<'a, U: 'a, T, F> DynamicFn<'a> for F
-where
-    F: Fn(&'a T) -> U,
-    U: serde::Serialize,
-    T: Send + Sync + 'static,
-{
-    type Input = T;
-    type Output = U;
-
-    fn call(&self, value: &'a Self::Input, context: &mut HookContext<Self::Input>) -> Self::Output {
-        (self)(value, context)
+        Some(Box::new(value))
     }
 }
 
 type HookFnReturn<'a> = Option<Box<dyn erased_serde::Serialize + 'a>>;
 type StaticHookFn = for<'a> fn(&'a Frame) -> HookFnReturn<'a>;
-type DynamicHookFn = Box<dyn for<'a> ErasedFn<'a>>;
+type DynamicHookFn = Box<
+    dyn for<'a> Fn(
+        &'a Frame,
+        &mut HookContext<Frame>,
+    ) -> Option<Box<dyn erased_serde::Serialize + 'a>>,
+>;
 
 enum HookFn {
     Static(StaticHookFn),
@@ -79,21 +83,35 @@ impl Hook {
         }
     }
 
-    fn new_dynamic<F>(closure: F) -> Self
+    fn new_dynamic<F, I>(closure: F) -> Self
     where
-        F: for<'a> DynamicFn<'a>,
-        for<'a> <F as DynamicFn<'a>>::Input: Send + Sync + 'static,
-        for<'a> <F as DynamicFn<'a>>::Output: serde::Serialize,
+        F: for<'a> DynamicFn<'a, I>,
+        for<'a> <F as DynamicFn<'a, I>>::Output: serde::Serialize + 'a,
+        I: Send + Sync + 'static,
     {
-        let closure: Box<dyn for<'a> ErasedFn<'a>> =
-            Box::new(move |frame: &Frame, context: &mut HookContext<Frame>| {
-                let value = frame.request_ref::<F::Input>()?;
+        fn dispatch<'a, F, I>(
+            closure: &F,
+            frame: &'a Frame,
+            context: &mut HookContext<Frame>,
+        ) -> Option<Box<dyn erased_serde::Serialize + 'a>>
+        where
+            F: for<'b> DynamicFn<'b, I>,
+            for<'b> <F as DynamicFn<'b, I>>::Output: serde::Serialize + 'b,
+            I: Send + Sync + 'static,
+        {
+            let value = frame.request_ref::<I>()?;
+            let value = closure.call(value, context.cast());
 
-                Box::new(closure.call(value, context.cast()))
+            Some(Box::new(value))
+        }
+
+        let closure: DynamicHookFn =
+            Box::new(move |frame: &Frame, context: &mut HookContext<Frame>| {
+                dispatch::<_, I>(&closure, frame, context)
             });
 
         Self {
-            ty: TypeId::of::<F::Input>(),
+            ty: TypeId::of::<I>(),
             hook: HookFn::Dynamic(closure),
         }
     }
@@ -123,18 +141,18 @@ impl Hooks {
         self.inner.push(Hook::new_static::<T>());
     }
 
-    // pub(crate) fn insert_dynamic<T: Send + Sync + 'static, U: serde::Serialize, F>(
-    //     &mut self,
-    //     closure: F,
-    // ) where
-    //     for<'a> F: Fn(&'a T, &mut HookContext<T>) -> U + 'a,
-    // {
-    //     let type_id = TypeId::of::<T>();
-    //
-    //     // make sure that previous hooks of the same TypeId are deleted
-    //     self.inner.retain(|hook| hook.ty != type_id);
-    //     self.inner.push(Hook::new_dynamic(closure));
-    // }
+    pub(crate) fn insert_dynamic<F, I>(&mut self, closure: F)
+    where
+        F: for<'a> DynamicFn<'a, I>,
+        for<'a> <F as DynamicFn<'a, I>>::Output: serde::Serialize + 'a,
+        I: Send + Sync + 'static,
+    {
+        let type_id = TypeId::of::<I>();
+
+        // make sure that previous hooks of the same TypeId are deleted
+        self.inner.retain(|hook| hook.ty != type_id);
+        self.inner.push(Hook::new_dynamic(closure));
+    }
 
     pub(crate) fn call<'a>(
         &'a self,
