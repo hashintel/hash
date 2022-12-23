72b1a56608f80128900876489ef443262b216074
Alexander Kachkaev <alexander@kachkaev.ru>
2022-12-23 19:26:47 +0000

    Add three custom Storybook stories

--- packages/hash/design-system/.storybook/preview.js
+++ packages/hash/design-system/.storybook/preview.js
@@ -1,3 +1,7 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { theme } from "../src/ui";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +11,12 @@ export const parameters = {
     },
   },
 };
+
+export const withMuiTheme = (Story) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Story />
+  </ThemeProvider>
+);
+
+export const decorators = [withMuiTheme];
