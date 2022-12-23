f967b19ba687ed8992afc1720ac2ec8dacd7857f
luisbettencourt <37453800+luisbettencourt@users.noreply.github.com>
2022-12-23 12:15:35 +0000

    Enable object property type creation (#1651)
    * renaming variables and files
    
    * further renaming
    
    * property object creation
    
    * Update packages/hash/frontend/src/pages/[account-slug]/types/entity-type/[entity-type-id].page/definition-tab/property-list-card/shared/property-type-form/data-type-selector/custom-expected-value-builder.tsx
    
    Co-authored-by: Nate Higgins <nate@nateh.uk>
    
    * remove type assertion
    
    * move object-expected-value-builder
    
    * styled components
    
    * adding and removing properties animations
    
    * change property id type to versionedUri
    
    * fix versionedUri
    
    * remove unecessary exports
    
    * table row use watch to get properties
    
    * lint fix
    
    * fix file structure
    
    * focus input when closing custom expected value builder
    
    * merge fixes
    
    * remove old files
    
    Co-authored-by: Nate Higgins <nate@nateh.uk>

--- packages/hash/frontend/src/pages/[shortname]/types/entity-type/[entity-type-id].page/definition-tab/property-list-card/shared/property-type-form/expected-value-selector/shared/expected-values-options.ts
+++ packages/hash/frontend/src/pages/[shortname]/types/entity-type/[entity-type-id].page/definition-tab/property-list-card/shared/property-type-form/expected-value-selector/shared/expected-values-options.ts
@@ -41,10 +41,11 @@ export const expectedValuesOptions = {
     icon: faSquareCheck,
     colors: chipColors.blue,
   },
-  [types.dataType.object.dataTypeId]: {
+  object: {
     title: "Property Object",
     icon: faCube,
     colors: chipColors.purple,
+    allowMultiple: true,
   },
   array: {
     title: "Array",
