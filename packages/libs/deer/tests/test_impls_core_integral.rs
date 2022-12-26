13dacf32734b8b3c04b4033cd07ff4faf71bc83c
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2022-12-26 10:52:53 +0100

    test: `u8` `Deserialize`

--- packages/libs/deer/tests/test_impls_core_integral.rs
+++ packages/libs/deer/tests/test_impls_core_integral.rs
@@ -1,13 +1,45 @@
-use deer::Number;
-use deer_desert::{assert_tokens, Token};
+use deer::{Deserialize, Number};
+use deer_desert::{assert_tokens, assert_tokens_error, Token};
+use proptest::prelude::*;
+use serde_json::json;
 
-#[test]
-fn u8_ok() {
-    assert_tokens(&0, &[Token::Number(Number::from(0u8))]);
+proptest! {
+    #[test]
+    fn u8_ok(value in any::<u8>()) {
+        assert_tokens(&value, &[Token::Number(Number::from(value))]);
+    }
 }
 
 #[test]
-fn u8_err_overflow() {}
+fn u8_err_overflow() {
+    assert_tokens_error::<u8>(
+        &json!([{
+            "id": ["value"],
+            "message": "received value is of correct type (integer), but does not fit constraints",
+            "namespace": "deer",
+            "properties": {
+                "expected": u8::reflection(),
+                "received": 256,
+                "location": []
+            }
+        }]),
+        &[Token::Number(Number::from(u8::MAX as u16 + 1))],
+    )
+}
 
 #[test]
-fn u8_err_underflow() {}
+fn u8_err_underflow() {
+    assert_tokens_error::<u8>(
+        &json!([{
+            "id": ["value"],
+            "message": "received value is of correct type (integer), but does not fit constraints",
+            "namespace": "deer",
+            "properties": {
+                "expected": u8::reflection(),
+                "received": -1,
+                "location": []
+            }
+        }]),
+        &[Token::Number(Number::from(-1))],
+    )
+}
