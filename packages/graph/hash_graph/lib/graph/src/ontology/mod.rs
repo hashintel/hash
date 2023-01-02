09ed7f06641a1240a7884baacf85f87f191b9ebe
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-01-02 14:46:08 +0100

    Define methods on `Subgraph` instead of `Record` (#1691)

--- packages/graph/hash_graph/lib/graph/src/ontology/mod.rs
+++ packages/graph/hash_graph/lib/graph/src/ontology/mod.rs
@@ -6,7 +6,6 @@ mod entity_type;
 mod property_type;
 
 use core::fmt;
-use std::collections::hash_map::{RandomState, RawEntryMut};
 
 use error_stack::{Context, IntoReport, Result, ResultExt};
 use serde::{Deserialize, Serialize, Serializer};
@@ -26,7 +25,6 @@ use crate::{
     identifier::ontology::OntologyTypeEditionId,
     provenance::{OwnedById, ProvenanceMetadata},
     store::{query::Filter, Record},
-    subgraph::Subgraph,
 };
 
 #[derive(Deserialize, ToSchema)]
@@ -209,17 +207,6 @@ impl Record for DataTypeWithMetadata {
     fn create_filter_for_edition_id(edition_id: &Self::EditionId) -> Filter<Self> {
         Filter::for_ontology_type_edition_id(edition_id)
     }
-
-    fn subgraph_entry<'s>(
-        subgraph: &'s mut Subgraph,
-        edition_id: &Self::EditionId,
-    ) -> RawEntryMut<'s, Self::EditionId, Self, RandomState> {
-        subgraph
-            .vertices
-            .data_types
-            .raw_entry_mut()
-            .from_key(edition_id)
-    }
 }
 
 impl OntologyTypeWithMetadata for DataTypeWithMetadata {
@@ -260,17 +247,6 @@ impl Record for PropertyTypeWithMetadata {
     fn create_filter_for_edition_id(edition_id: &Self::EditionId) -> Filter<Self> {
         Filter::for_ontology_type_edition_id(edition_id)
     }
-
-    fn subgraph_entry<'s>(
-        subgraph: &'s mut Subgraph,
-        edition_id: &Self::EditionId,
-    ) -> RawEntryMut<'s, Self::EditionId, Self, RandomState> {
-        subgraph
-            .vertices
-            .property_types
-            .raw_entry_mut()
-            .from_key(edition_id)
-    }
 }
 
 impl OntologyTypeWithMetadata for PropertyTypeWithMetadata {
@@ -310,17 +286,6 @@ impl Record for EntityTypeWithMetadata {
 
     fn create_filter_for_edition_id(edition_id: &Self::EditionId) -> Filter<Self> {
         Filter::for_ontology_type_edition_id(edition_id)
-    }
-
-    fn subgraph_entry<'s>(
-        subgraph: &'s mut Subgraph,
-        edition_id: &Self::EditionId,
-    ) -> RawEntryMut<'s, Self::EditionId, Self, RandomState> {
-        subgraph
-            .vertices
-            .entity_types
-            .raw_entry_mut()
-            .from_key(edition_id)
     }
 }
 
