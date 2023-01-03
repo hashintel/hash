de1718afd8b579d9022801adbaeb2ad8d5abed10
Yusuf Kinatas <yusufkinatas@gmail.com>
2023-01-03 20:41:02 +0300

    support min-max items for array properties

---
+++ packages/hash/frontend/src/pages/[shortname]/entities/[entity-uuid].page/entity-editor/properties-section/property-table/cells/value-cell/array-editor/item-limit-info.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@hashintel/hash-design-system";
+import { Box, experimental_sx as sx, styled } from "@mui/material";
+import { isNumber } from "lodash";
+import { ReactNode } from "react";
+
+const StyledBox = styled(Box)(
+  sx({
+    borderRadius: 1,
+    mr: 0.5,
+    backgroundColor: "gray.20",
+    height: 26,
+    display: "flex",
+    alignItems: "center",
+    px: 1,
+    color: "gray.60",
+  }),
+);
+
+export const ItemLimitInfo = ({ min, max }: { min?: number; max?: number }) => {
+  const nodesToRender: ReactNode[] = [];
+
+  if (isNumber(min)) {
+    nodesToRender.push(
+      <>
+        <StyledBox>{min}</StyledBox> values required
+      </>,
+    );
+  }
+
+  if (isNumber(max)) {
+    nodesToRender.push(
+      <>
+        {isNumber(min) && <Box mx={0.5}>{" and "}</Box>}
+        <StyledBox>{max}</StyledBox> values maximum
+      </>,
+    );
+  }
+
+  if (!nodesToRender.length) {
+    return null;
+  }
+
+  return (
+    <Button
+      disabled
+      size="small"
+      variant="tertiary_quiet"
+      fullWidth
+      sx={{
+        justifyContent: "flex-start",
+        borderRadius: 0,
+        background: ({ palette }) => `${palette.gray[10]} !important`,
+        fontSize: 12,
+      }}
+    >
+      {nodesToRender}
+    </Button>
+  );
+};
