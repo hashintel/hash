c22e87e745509f0dc6ce7aaa82a691659ebb045f
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-01-06 15:57:42 +0000

    Add asana integration

--- packages/hash/api/src/graphql/resolvers/task-executor/index.ts
+++ packages/hash/api/src/graphql/resolvers/task-executor/index.ts
@@ -3,6 +3,9 @@ import { ApolloError } from "apollo-server-express";
 
 import { Task } from "../../../task-execution";
 import {
+  MutationExecuteAsanaCheckTaskArgs,
+  MutationExecuteAsanaDiscoverTaskArgs,
+  MutationExecuteAsanaReadTaskArgs,
   MutationExecuteGithubCheckTaskArgs,
   MutationExecuteGithubDiscoverTaskArgs,
   MutationExecuteGithubReadTaskArgs,
@@ -10,6 +13,12 @@ import {
 } from "../../api-types.gen";
 import { GraphQLContext, LoggedInGraphQLContext } from "../../context";
 import { readFromAirbyte } from "./airbyte-read";
+
+/** @todo - Make Airbyte types available in api package */
+type AirbyteCatalog = Array<{
+  name?: string;
+  json_schema?: JsonObject;
+}>;
 
 export const executeDemoTask: ResolverFn<
   Promise<string>,
@@ -71,12 +80,6 @@ export const executeGithubCheckTask: ResolverFn<
   }
 };
 
-/** @todo - Make Airbyte types available in api package */
-type AirbyteCatalog = Array<{
-  name?: string;
-  json_schema?: JsonObject;
-}>;
-
 export const executeGithubDiscoverTask: ResolverFn<
   Promise<string>,
   {},
@@ -126,6 +129,101 @@ export const executeGithubReadTask: ResolverFn<
       graphApi,
       config,
       integrationName: "Github",
+    });
+  } catch (err: any) {
+    throw new ApolloError(`Task-execution failed: ${err}`);
+  }
+};
+
+export const executeAsanaSpecTask: ResolverFn<
+  Promise<string>,
+  {},
+  GraphQLContext,
+  {}
+> = async (_, __, { dataSources: { taskExecutor } }) => {
+  if (!taskExecutor) {
+    throw new ApolloError(
+      "A task-executor wasn't started, so external tasks can't be started",
+    );
+  } else {
+    return await taskExecutor
+      .runTask(Task.AsanaSpec)
+      .then((res) => JSON.stringify(res))
+      .catch((err: any) => {
+        throw new ApolloError(`Task-execution failed: ${err}`);
+      });
+  }
+};
+
+export const executeAsanaCheckTask: ResolverFn<
+  Promise<string>,
+  {},
+  GraphQLContext,
+  MutationExecuteAsanaCheckTaskArgs
+> = async (_, { config }, { dataSources: { taskExecutor } }) => {
+  if (!taskExecutor) {
+    throw new ApolloError(
+      "A task-executor wasn't started, so external tasks can't be started",
+    );
+  } else {
+    return await taskExecutor
+      .runTask(Task.AsanaCheck, config)
+      .then((res) => JSON.stringify(res))
+      .catch((err) => {
+        throw new ApolloError(`Task-execution failed: ${err}`);
+      });
+  }
+};
+
+export const executeAsanaDiscoverTask: ResolverFn<
+  Promise<string>,
+  {},
+  LoggedInGraphQLContext,
+  MutationExecuteAsanaDiscoverTaskArgs
+> = async (_, { config }, { dataSources: { taskExecutor } }) => {
+  if (!taskExecutor) {
+    throw new ApolloError(
+      "A task-executor wasn't started, so external tasks can't be started",
+    );
+  } else {
+    try {
+      const catalog: AirbyteCatalog = await taskExecutor.runTask(
+        Task.AsanaDiscover,
+        config,
+      );
+
+      return JSON.stringify(catalog);
+    } catch (err: any) {
+      throw new ApolloError(`Task-execution failed: ${err}`);
+    }
+  }
+};
+
+export const executeAsanaReadTask: ResolverFn<
+  Promise<string>,
+  {},
+  LoggedInGraphQLContext,
+  MutationExecuteAsanaReadTaskArgs
+> = async (
+  _,
+  { config },
+  { dataSources: { graphApi, taskExecutor }, user, logger },
+) => {
+  if (!taskExecutor) {
+    throw new ApolloError(
+      "A task-executor wasn't started, so external tasks can't be started",
+    );
+  }
+
+  try {
+    return await readFromAirbyte({
+      task: Task.AsanaRead,
+      user,
+      taskExecutor,
+      logger,
+      graphApi,
+      config,
+      integrationName: "Asana",
     });
   } catch (err: any) {
     throw new ApolloError(`Task-execution failed: ${err}`);
