6645aecc682fbb53b15d497c98c458d3983434e0
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-01-09 15:17:16 +0000

    Add Status

--- libs/status/crate/src/lib.rs
+++ libs/status/crate/src/lib.rs
@@ -3,4 +3,49 @@
 
 mod status_code;
 
+use std::fmt::Debug;
+
+use serde::{Deserialize, Serialize};
 pub use status_code::StatusCode;
+
+/**
+ * The canonical shape of a response object describing the status of a request between services.
+ */
+#[derive(Clone, Eq, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Status<D>
+where
+    D: Send + Sync + Debug,
+{
+    code: StatusCode,
+    /// A developer-facing description of the status.
+    ///
+    /// Where possible, this should provide guiding advice for debugging and/or handling the error.
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    message: Option<String>,
+    contents: Vec<D>,
+}
+
+impl<D> Status<D>
+where
+    D: Send + Sync + Debug + Serialize + for<'de> Deserialize<'de>,
+{
+    pub fn new(code: StatusCode, message: Option<String>, contents: Vec<D>) -> Self {
+        Self {
+            code,
+            message,
+            contents,
+        }
+    }
+
+    pub fn code(&self) -> StatusCode {
+        self.code
+    }
+
+    pub fn message(&self) -> &Option<String> {
+        &self.message
+    }
+
+    pub fn contents(&self) -> &[D] {
+        &self.contents
+    }
+}
--- libs/status/pkg/src/main.ts
+++ libs/status/pkg/src/main.ts
@@ -3,4 +3,20 @@
  * and RPC APIs.
  */
 
+import { StatusCode } from "./status-code";
+
 export { type StatusCode } from "./status-code";
+
+/**
+ * The canonical shape of a response object describing the status of a request between services.
+ */
+export type Status<D extends object> = {
+  code: StatusCode;
+  /**
+   * A developer-facing description of the status.
+   *
+   * Where possible, this should provide guiding advice for debugging and/or handling the error.
+   */
+  message?: string;
+  contents: D[];
+};
