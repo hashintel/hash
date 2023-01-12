c565e0c713ec5619c0782467d4ae1f8a7810b14d
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-01-12 08:43:03 +0000

    Add status codegen to the HASH graph

--- apps/hash-graph/lib/graph/src/api.rs
+++ apps/hash-graph/lib/graph/src/api.rs
@@ -1,1 +1,6 @@
 pub mod rest;
+
+pub mod error;
+
+#[allow(clippy::all, dead_code, unused_imports, unused_variables)]
+mod gen;
---
+++ apps/hash-graph/type-defs/status-payloads.ts
@@ -0,0 +1,2 @@
+export { ErrorInfo } from "@local/status/type-defs/status-payloads/error-info";
+export { RequestInfo } from "@local/status/type-defs/status-payloads/request-info";
