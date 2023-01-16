0a2954a2bb6f027e25149e0825d6dccddbe9a2a3
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-01-16 18:55:10 +0100

    chore: remove redundant comment

--- packages/libs/deer/src/impls/core/mod.rs
+++ packages/libs/deer/src/impls/core/mod.rs
@@ -6,16 +6,3 @@ mod integral;
 mod non_zero;
 mod string;
 mod unit;
-
-// DONE: f32, f64, i*, u*, char, str, array, Atomic*, NonZero, Unit
-// core:
-// tuple, LazyCell, OnceCell, SyncUnsafeCell, Cel, Ref,
-// RefCell, RefMut, UnsafeCell, PhantomData, ManuallyDrop, Option, Result, Wrapping, Bound, Range,
-// Ip*, Reverse, PhantomData
-
-// alloc:
-// Cow, Box, ThinBox, BinaryHeap, BTreeMap, BTreeSet, LinkedList, VecDequeue, Vec, CString, Rc,
-// String, Arc
-
-// std:
-// HashMap, Path, Barrier(?), Mutex, Once, RwLock
