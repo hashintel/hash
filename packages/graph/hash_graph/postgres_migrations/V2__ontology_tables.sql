0a3e0292f84cbf0dd1b109e7d68ab13f6622d0f7
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-01-19 11:14:32 +0100

    Split migrations across files (#1876)

---
+++ packages/graph/hash_graph/postgres_migrations/V2__ontology_tables.sql
@@ -0,0 +1,65 @@
+CREATE TABLE IF NOT EXISTS
+  "base_uris" ("base_uri" TEXT PRIMARY KEY);
+
+CREATE TABLE IF NOT EXISTS
+  "version_ids" ("version_id" UUID PRIMARY KEY);
+
+CREATE TABLE IF NOT EXISTS
+  "type_ids" (
+    "base_uri" TEXT NOT NULL REFERENCES "base_uris",
+    "version" BIGINT NOT NULL,
+    "version_id" UUID REFERENCES "version_ids",
+    PRIMARY KEY ("base_uri", "version"),
+    UNIQUE ("version_id")
+  );
+
+COMMENT
+  ON TABLE "type_ids" IS $pga$ This table is a boundary to define the actual identification scheme for our kinds of types. Assume that we use the UUIDs on the types to look up more specific ID details. $pga$;
+
+CREATE TABLE IF NOT EXISTS
+  "data_types" (
+    "version_id" UUID PRIMARY KEY REFERENCES "version_ids",
+    "schema" JSONB NOT NULL,
+    "owned_by_id" UUID NOT NULL REFERENCES "accounts",
+    "updated_by_id" UUID NOT NULL REFERENCES "accounts"
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "property_types" (
+    "version_id" UUID PRIMARY KEY REFERENCES "version_ids",
+    "schema" JSONB NOT NULL,
+    "owned_by_id" UUID NOT NULL REFERENCES "accounts",
+    "updated_by_id" UUID NOT NULL REFERENCES "accounts"
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "entity_types" (
+    "version_id" UUID PRIMARY KEY REFERENCES "version_ids",
+    "schema" JSONB NOT NULL,
+    "owned_by_id" UUID NOT NULL REFERENCES "accounts",
+    "updated_by_id" UUID NOT NULL REFERENCES "accounts"
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "property_type_property_type_references" (
+    "source_property_type_version_id" UUID NOT NULL REFERENCES "property_types",
+    "target_property_type_version_id" UUID NOT NULL REFERENCES "property_types"
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "property_type_data_type_references" (
+    "source_property_type_version_id" UUID NOT NULL REFERENCES "property_types",
+    "target_data_type_version_id" UUID NOT NULL REFERENCES "data_types"
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "entity_type_property_type_references" (
+    "source_entity_type_version_id" UUID NOT NULL REFERENCES "entity_types",
+    "target_property_type_version_id" UUID NOT NULL REFERENCES "property_types"
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "entity_type_entity_type_references" (
+    "source_entity_type_version_id" UUID NOT NULL REFERENCES "entity_types",
+    "target_entity_type_version_id" UUID NOT NULL REFERENCES "entity_types"
+  );
---
+++ packages/graph/hash_graph/postgres_migrations/V3__knowledge_tables.sql
@@ -0,0 +1,82 @@
+CREATE TABLE IF NOT EXISTS
+  "entity_ids" (
+    "owned_by_id" UUID NOT NULL REFERENCES "accounts",
+    "entity_uuid" UUID NOT NULL,
+    "left_owned_by_id" UUID,
+    "left_entity_uuid" UUID,
+    "right_owned_by_id" UUID,
+    "right_entity_uuid" UUID,
+    PRIMARY KEY ("owned_by_id", "entity_uuid"),
+    FOREIGN KEY ("left_owned_by_id", "left_entity_uuid") REFERENCES "entity_ids",
+    FOREIGN KEY ("right_owned_by_id", "right_entity_uuid") REFERENCES "entity_ids",
+    CHECK (
+      left_entity_uuid IS NULL
+      AND right_entity_uuid IS NULL
+      AND left_owned_by_id IS NULL
+      AND right_owned_by_id IS NULL
+      OR left_entity_uuid IS NOT NULL
+      AND right_entity_uuid IS NOT NULL
+      AND left_owned_by_id IS NOT NULL
+      AND right_owned_by_id IS NOT NULL
+    )
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "entity_editions" (
+    "entity_record_id" BIGINT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    "entity_type_version_id" UUID NOT NULL REFERENCES "entity_types",
+    "properties" JSONB NOT NULL,
+    "left_to_right_order" INTEGER,
+    "right_to_left_order" INTEGER,
+    "updated_by_id" UUID NOT NULL REFERENCES "accounts",
+    "archived" BOOLEAN NOT NULL
+  );
+
+CREATE TABLE IF NOT EXISTS
+  "entity_versions" (
+    "owned_by_id" UUID NOT NULL,
+    "entity_uuid" UUID NOT NULL,
+    "entity_record_id" BIGINT NOT NULL REFERENCES "entity_editions",
+    "decision_time" tstzrange NOT NULL,
+    "transaction_time" tstzrange NOT NULL,
+    FOREIGN KEY ("owned_by_id", "entity_uuid") REFERENCES "entity_ids",
+    CONSTRAINT entity_versions_overlapping EXCLUDE USING gist (
+      owned_by_id
+      WITH
+        =,
+        entity_uuid
+      WITH
+        =,
+        decision_time
+      WITH
+        &&,
+        transaction_time
+      WITH
+        &&
+    ) DEFERRABLE INITIALLY IMMEDIATE,
+    CHECK (LOWER(decision_time) <= LOWER(transaction_time))
+  );
+
+CREATE VIEW
+  "entities" AS
+SELECT
+  entity_versions.entity_record_id,
+  entity_versions.owned_by_id,
+  entity_versions.entity_uuid,
+  entity_versions.decision_time,
+  entity_versions.transaction_time,
+  entity_editions.entity_type_version_id,
+  entity_editions.updated_by_id,
+  entity_editions.properties,
+  entity_editions.archived,
+  entity_ids.left_owned_by_id,
+  entity_ids.left_entity_uuid,
+  entity_editions.left_to_right_order,
+  entity_ids.right_owned_by_id,
+  entity_ids.right_entity_uuid,
+  entity_editions.right_to_left_order
+FROM
+  entity_versions
+  JOIN entity_editions ON entity_versions.entity_record_id = entity_editions.entity_record_id
+  JOIN entity_ids ON entity_versions.owned_by_id = entity_ids.owned_by_id
+  AND entity_versions.entity_uuid = entity_ids.entity_uuid;
