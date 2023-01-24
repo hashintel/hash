0bce7ad79273bebeb174aaa22b64474b7b252fd4
Tim Diekmann <tim.diekmann@3dvision.de>
2023-01-24 23:29:52 +0100

    Fix pathes for test data

--- apps/hash-graph/tests/test_data/src/data_type.rs
+++ apps/hash-graph/tests/test_data/src/data_type.rs
@@ -1,7 +1,7 @@
-pub const BOOLEAN_V1: &str = include_str!("boolean.json");
-pub const EMPTY_LIST_V1: &str = include_str!("empty_list.json");
-pub const NULL_V1: &str = include_str!("null.json");
-pub const NUMBER_V1: &str = include_str!("number.json");
-pub const OBJECT_V1: &str = include_str!("object_v1.json");
-pub const OBJECT_V2: &str = include_str!("object_v2.json");
-pub const TEXT_V1: &str = include_str!("text.json");
+pub const BOOLEAN_V1: &str = include_str!("data_type/boolean.json");
+pub const EMPTY_LIST_V1: &str = include_str!("data_type/empty_list.json");
+pub const NULL_V1: &str = include_str!("data_type/null.json");
+pub const NUMBER_V1: &str = include_str!("data_type/number.json");
+pub const OBJECT_V1: &str = include_str!("data_type/object_v1.json");
+pub const OBJECT_V2: &str = include_str!("data_type/object_v2.json");
+pub const TEXT_V1: &str = include_str!("data_type/text.json");
