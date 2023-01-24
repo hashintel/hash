e7d19b7873f0585679c979b77e347a4ce5fd1cff
Ahmad Sattar <thehabbos007@gmail.com>
2023-01-24 15:47:54 +0100

    Initial citus setup

--- packages/graph/hash_graph/postgres_migrations/V1__base.sql
+++ packages/graph/hash_graph/postgres_migrations/V1__base.sql
@@ -1,5 +1,44 @@
 CREATE EXTENSION
   IF NOT EXISTS "btree_gist";
 
+CREATE EXTENSION
+  citus;
+
 CREATE TABLE IF NOT EXISTS
   "accounts" ("account_id" UUID PRIMARY KEY);
+
+CREATE
+OR REPLACE FUNCTION is_citus_enabled () RETURNS BOOLEAN AS $$ begin
+  perform 1 from pg_extension where extname='citus';
+  return found;
+end $$ LANGUAGE plpgsql;
+
+CREATE
+OR REPLACE FUNCTION is_citus_reference_table (table_name TEXT) RETURNS BOOLEAN AS $$ begin
+  perform 1 from citus_tables where
+    citus_tables.table_name = $1::regclass
+    and citus_table_type = 'reference';
+  return found;
+end $$ LANGUAGE plpgsql;
+
+CREATE
+OR REPLACE FUNCTION is_citus_distributed_table (table_name TEXT) RETURNS BOOLEAN AS $$ begin
+  perform 1 from citus_tables where
+    citus_tables.table_name = $1::regclass
+    and citus_table_type = 'distributed';
+  return found;
+end $$ LANGUAGE plpgsql;
+
+CREATE
+OR REPLACE FUNCTION create_fk_if_not_exists (
+  constraint_name TEXT,
+  from_table TEXT,
+  from_columns TEXT,
+  to_table TEXT,
+  to_columns TEXT
+) RETURNS void AS $$ begin
+  perform 1 from pg_constraint where conname = constraint_name;
+  if not found then
+    execute format('alter table %s add constraint %s foreign key %s references %s %s deferrable', from_table, constraint_name, from_columns, to_table, to_columns);
+  end if;
+end $$ LANGUAGE plpgsql;
---
+++ packages/graph/hash_graph/postgres_migrations/V6__shard.sql
@@ -0,0 +1,55 @@
+DO
+  $$
+begin
+  if is_citus_enabled() then
+    -- Base
+    if not is_citus_reference_table('accounts') then
+      perform create_reference_table('accounts');
+    end if;
+
+    -- Ontology tables (reference tables)
+    if not is_citus_reference_table('type_ids') then
+      perform create_reference_table('type_ids');
+    end if;
+
+    if not is_citus_reference_table('data_types') then
+      perform create_reference_table('data_types');
+    end if;
+
+    if not is_citus_reference_table('property_types') then
+      perform create_reference_table('property_types');
+    end if;
+
+    if not is_citus_reference_table('entity_types') then
+      perform create_reference_table('entity_types');
+    end if;
+
+
+    if not is_citus_reference_table('property_type_property_type_references') then
+      perform create_reference_table('property_type_property_type_references');
+    end if;
+
+    if not is_citus_reference_table('property_type_data_type_references') then
+      perform create_reference_table('property_type_data_type_references');
+    end if;
+
+    if not is_citus_reference_table('entity_type_property_type_references') then
+      perform create_reference_table('entity_type_property_type_references');
+    end if;
+
+    if not is_citus_reference_table('entity_type_entity_type_references') then
+      perform create_reference_table('entity_type_entity_type_references');
+    end if;
+
+
+    -- Knowledge tables
+    if not is_citus_distributed_table('entity_ids') then
+      perform create_distributed_table('entity_ids', 'owned_by_id');
+    end if;
+
+    if not is_citus_distributed_table('entity_versions') then
+      perform create_distributed_table('entity_versions', 'owned_by_id');
+    end if;
+
+  end if;
+end$$;
