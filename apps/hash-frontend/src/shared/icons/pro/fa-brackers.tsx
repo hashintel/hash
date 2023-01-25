c790f9615a33f1cc6f3f3f2f11927cf97876689a
Yusuf Kinatas <yusufkinatas@gmail.com>
2023-01-25 10:41:44 +0300

    support new data types on entity editor

---
+++ apps/hash-frontend/src/shared/icons/pro/fa-brackers.tsx
@@ -0,0 +1,9 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+export const faBrackets: IconDefinition["icon"] = [
+  448,
+  512,
+  [],
+  "f1b3",
+  "M416 32c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H320c-17.7 0-32-14.3-32-32s14.3-32 32-32h64V96H320c-17.7 0-32-14.3-32-32s14.3-32 32-32h96zM32 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H64V416h64c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V64C0 46.3 14.3 32 32 32z",
+];
