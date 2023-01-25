250b76af909e8ae1bb91f396b3a659a5bfe924fd
Tim Diekmann <tim.diekmann@3dvision.de>
2023-01-25 11:21:23 +0100

    Fix path in `jest.config.cjs`

--- tests/hash-backend-integration/jest.config.cjs
+++ tests/hash-backend-integration/jest.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
       "<rootDir>/../../libs/@local/hash-backend-utils/src$1",
     "@local/hash-isomorphic-utils(.*)":
       "<rootDir>/../../libs/@local/hash-isomorphic-utils/src$1",
-    "@local/hash-graph-client": "<rootDir>/../../apps/@local/hash-graph-client",
+    "@local/hash-graph-client": "<rootDir>/../../libs/@local/hash-graph-client",
     "@local/hash-subgraph(.*)": "<rootDir>/../../libs/@local/hash-subgraph$1",
   },
   setupFiles: ["@local/hash-backend-utils/environment"],
