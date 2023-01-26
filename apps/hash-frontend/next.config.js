62163e2a41c8d782d68d1b916a99971f491e783f
Ciaran Morinan <c@hash.ai>
2023-01-26 20:01:10 +0000

    permit cross-origin requests for types as json

--- apps/hash-frontend/next.config.js
+++ apps/hash-frontend/next.config.js
@@ -55,6 +55,30 @@ module.exports = withSentryConfig(
     withTM(
       /** @type {import('next').NextConfig} */
       {
+        async headers() {
+          return [
+            {
+              /**
+               * allow fetching types as JSON from anywhere
+               * @see ./src/middleware.page.ts for middleware which serves the JSON
+               */
+              source: "/:shortname/types/:path*",
+              has: [
+                {
+                  type: "header",
+                  key: "accept",
+                  value: "(application/json)",
+                },
+              ],
+              headers: [
+                {
+                  key: "access-control-allow-origin",
+                  value: "*",
+                },
+              ],
+            },
+          ];
+        },
         pageExtensions: [
           "page.tsx",
           "page.ts",
