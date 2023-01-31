c13b8f41cf01e775c6f1896f2ed2546e6c5832a4
Ahmad Sattar <thehabbos007@gmail.com>
2023-01-31 13:36:38 +0100

    Update `type_id` -> `ontology_id`

--- apps/hash-graph/postgres_migrations/V7__entity_type_closure.sql
+++ apps/hash-graph/postgres_migrations/V7__entity_type_closure.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS
   "closed_entity_types" (
-    "closed_type_id" UUID PRIMARY KEY NOT NULL,
+    "closed_ontology_id" UUID PRIMARY KEY NOT NULL,
     "closed_schema" JSONB NOT NULL,
     "is_link_type" BOOLEAN NOT NULL
   );
@@ -13,21 +13,21 @@ $pga$;
 
 CREATE TABLE IF NOT EXISTS
   "closed_entity_types_to_constituent_types" (
-    "closed_type_id" UUID NOT NULL REFERENCES "closed_entity_types",
+    "closed_ontology_id" UUID NOT NULL REFERENCES "closed_entity_types",
     -- An ancestor of the type in the closure 
     -- (e.g. a grandparent of the type under an inheritance chain)
-    -- We're referencing "type_ids" here because we don't want to box ourselves into only having
+    -- We're referencing "ontology_ids" here because we don't want to box ourselves into only having
     -- owned types here.
     -- We want to be able to have constitutent types that are cached external types, or owned types.
-    "constituent_type_id" UUID NOT NULL REFERENCES "type_ids",
+    "constituent_ontology_id" UUID NOT NULL REFERENCES "ontology_ids",
     -- For a normal (owned or cached external) entity type this will be true if this is the closure 
     -- of that entity type. 
     -- If this is a closure of an "anonymous" type, this will be true for all entity types that make
     -- up the anonymous type.
     -- This is therefore a *superset* of the inverse of the set of "entity_types_to_closed_entity_types"
     "direct" BOOLEAN NOT NULL,
-    -- Entity type closures cannot consist of multiples of the same type_id.
-    PRIMARY KEY ("closed_type_id", "constituent_type_id")
+    -- Entity type closures cannot consist of multiples of the same ontology_id.
+    PRIMARY KEY ("closed_ontology_id", "constituent_ontology_id")
   );
 
 COMMENT
@@ -39,8 +39,8 @@ $pga$;
 
 CREATE TABLE IF NOT EXISTS
   "entity_types_to_closed_entity_types" (
-    "constituent_type_id" UUID NOT NULL REFERENCES "entity_types",
-    "closed_type_id" UUID NOT NULL REFERENCES "closed_entity_types"
+    "constituent_ontology_id" UUID NOT NULL REFERENCES "entity_types",
+    "closed_ontology_id" UUID NOT NULL REFERENCES "closed_entity_types"
   );
 
 COMMENT
