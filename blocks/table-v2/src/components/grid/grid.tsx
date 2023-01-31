ea31a28b7771538a53441234a1dbf19ba96913b3
Yusuf Kinatas <yusufkinatas@gmail.com>
2023-01-31 17:01:24 +0300

    highlight selected row

--- blocks/table-v2/src/components/grid/grid.tsx
+++ blocks/table-v2/src/components/grid/grid.tsx
@@ -1,11 +1,22 @@
-import { DataEditor, DataEditorProps } from "@glideapps/glide-data-grid";
+import {
+  DataEditor,
+  DataEditorProps,
+  GridSelection,
+  CompactSelection,
+} from "@glideapps/glide-data-grid";
 import "@glideapps/glide-data-grid/dist/index.css";
 import { useRenderGridPortal } from "./use-render-grid-portal";
+import { useState } from "react";
 
 type GridProps = DataEditorProps;
 
 export const Grid = (props: GridProps) => {
   useRenderGridPortal();
+
+  const [selection, setSelection] = useState<GridSelection>({
+    columns: CompactSelection.empty(),
+    rows: CompactSelection.empty(),
+  });
 
   return (
     <DataEditor
@@ -17,7 +28,16 @@ export const Grid = (props: GridProps) => {
       smoothScrollX
       smoothScrollY
       getCellsForSelection
+      gridSelection={selection}
+      onGridSelectionChange={(newSelection) => setSelection(newSelection)}
       keybindings={{ search: true }}
+      getRowThemeOverride={(rowIndex) => {
+        const selectedRowIndex = selection.current?.cell[1];
+
+        if (rowIndex === selectedRowIndex) {
+          return { bgCell: "#f4f4f4" };
+        }
+      }}
     />
   );
 };
