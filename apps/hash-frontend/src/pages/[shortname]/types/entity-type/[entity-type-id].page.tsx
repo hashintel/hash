cf889541e8f9a93bf520f3773b9bffc8300eb75e
Alexander Kachkaev <alexander@kachkaev.ru>
2023-02-06 15:36:55 +0000

    Imports

--- apps/hash-frontend/src/pages/[shortname]/types/entity-type/[entity-type-id].page.tsx
+++ apps/hash-frontend/src/pages/[shortname]/types/entity-type/[entity-type-id].page.tsx
@@ -7,7 +7,7 @@ import {
 } from "@blockprotocol/type-system";
 import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@hashintel/design-system";
-import { EntityTypeContext } from "@hashintel/type-editor";
+import { DefinitionTab, EntityTypeContext } from "@hashintel/type-editor";
 import { Box, Container, Theme, Typography } from "@mui/material";
 import { GlobalStyles } from "@mui/system";
 // eslint-disable-next-line unicorn/prefer-node-protocol -- https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1931#issuecomment-1359324528
