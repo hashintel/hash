b81bc14efd80c033bb84fa2b8d8c8295c2c6cf3f
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-02-08 11:14:26 +0100

    chore: remove unused files

--- apps/intellij-plugin/src/main/kotlin/ai/hash/rustextra/projectView/Constants.kt
+++
@@ -1,7 +0,0 @@
-package ai.hash.rustextra.projectView
-
-import com.intellij.openapi.util.Key
-
-val ModuleFileNodeMarker = Key.create<Boolean>("ai.hash.rustextra.projectView.ModuleFileNodeMarker")
-
-val ModuleFileNodeRecursionMarker = Key.create<Boolean>("ai.hash.rustextra.projectView.ModuleFileNodeRecursionMarker")
