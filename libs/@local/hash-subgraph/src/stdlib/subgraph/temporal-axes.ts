3fe16701b4b707f0fe6bfc366e585ea846d5c6e4
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-13 13:52:56 +0000

    Reimplement the stdlib to shadow the BP implementations (interim broken state)

---
+++ libs/@local/hash-subgraph/src/stdlib/subgraph/temporal-axes.ts
@@ -0,0 +1,16 @@
+import { getLatestInstantIntervalForSubgraph as getLatestInstantIntervalForSubgraphBp } from "@blockprotocol/graph/stdlib";
+
+import { BoundedTimeInterval, Subgraph } from "../../main";
+
+/**
+ * For a given {@link Subgraph} that supports temporal versioning, this returns a {@link TimeInterval} that spans
+ * the instant in time which is at the end of the {@link Subgraph}'s {@link VariableTemporalAxis}. For a
+ * {@link Subgraph} that does _not_ support temporal versioning, an unbounded {@link TimeInterval} is returned
+ * that spans the whole axis.
+ *
+ * @param {Subgraph} subgraph
+ */
+export const getLatestInstantIntervalForSubgraph = (
+  subgraph: Subgraph,
+): BoundedTimeInterval =>
+  getLatestInstantIntervalForSubgraphBp(subgraph) as BoundedTimeInterval;
