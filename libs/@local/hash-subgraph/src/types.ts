32a66f6fdd8d5a36476c7946c02720f1efe8e69f
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-02-13 11:19:46 +0000

    Introduce `types.ts` file

---
+++ libs/@local/hash-subgraph/src/types.ts
@@ -0,0 +1,7 @@
+export * from "./types/branded";
+export * from "./types/edge";
+export * from "./types/element";
+export * from "./types/identifier";
+export * from "./types/subgraph";
+export * from "./types/time";
+export * from "./types/vertex";
