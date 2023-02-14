5205a73f923e3b37bd273563e3f150db4f30de2c
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-02-14 14:46:39 +0100

    feat: use config.toml

--- libs/antsi/src/lib.rs
+++ libs/antsi/src/lib.rs
@@ -17,22 +17,6 @@
 #![no_std]
 #![cfg_attr(all(doc, nightly), feature(doc_auto_cfg))]
 #![cfg_attr(nightly, feature(const_trait_impl))]
-#![warn(
-    missing_docs,
-    unreachable_pub,
-    clippy::pedantic,
-    clippy::nursery,
-    clippy::undocumented_unsafe_blocks,
-    clippy::dbg_macro,
-    clippy::print_stdout,
-    clippy::print_stderr,
-    clippy::alloc_instead_of_core,
-    clippy::std_instead_of_alloc,
-    clippy::std_instead_of_core,
-    clippy::if_then_some_else_none
-)]
-#![allow(clippy::redundant_pub_crate)] // This would otherwise clash with `unreachable_pub`
-#![allow(clippy::module_name_repetitions)]
 #![cfg_attr(
     not(miri),
     doc(test(attr(deny(warnings, clippy::pedantic, clippy::nursery))))
