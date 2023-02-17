42aee711e87d91552cb719a34486cc704e33159e
luisbettencourt <37453800+luisbettencourt@users.noreply.github.com>
2023-02-17 12:04:50 +0000

    Add `Address` block (#2014)
    ## 🌟 What is the purpose of this PR?
    
    This PR implements the Address block, powered by Mapbox.
    
    ## 🔗 Related links
    
    <!-- Add links to any context it is worth capturing (e.g. Issues, Discussions, Discord, Asana) -->
    <!-- Mark any links which are not publicly accessible as _(internal)_ -->
    <!-- Don't rely on links to explain the PR, especially internal ones: use the sections above -->
    
    - [Asana task](https://app.asana.com/0/1203543026679380/1203624215426857/f) _(internal)_
    
    ## ⚠️ Known issues
    
    <!-- Are there known issues / intentionally omitted functionality? Flag them here to save reviewers doing so -->
    When the user closes the selected address the link entities to the map image and address entity are deleted. In Mock block dock this is not correctly working as the `linkedEntities` are still being returned. This leads to a slow down when selecting multiple addresses as the `linkedEntities` array will grow in size.
    
    ## 🐾 Next steps
    
    - [x] Add mapbox watermark to map
    - [x] Zoom levels
    
    <!-- Are there are planned/suggested follow ups which are related but won't be done in this PR? -->
    
    Follow-up PRs:
    - Route Mapbox API calls via Þ middleware;
    - Add interactive map option for paid subscribers.
    
    ## ❓ How to test this?
    
    <!-- Tell reviewers how they can test the functionality -->
    
    1.  Run `yarn dev` inside `blocks/address`;
    2. Get a mapbox accessToken or ask for mine in slack;
    3. Test the block.
    
    ## 📹 Demo
    ![demo](https://user-images.githubusercontent.com/37453800/218232470-f1b2c827-411b-4f07-af44-880b693073f5.gif)
    
    <!-- Add a screenshot or video showcasing your work -->
    
    
    ![image](https://user-images.githubusercontent.com/37453800/218232043-ce90d0ea-a888-4e14-8db4-67aec82664bf.png)
    
    ![demo2](https://user-images.githubusercontent.com/37453800/218287482-5e8d600c-4a1f-41a9-95a4-189a39703051.gif)

---
+++ blocks/address/src/dev.tsx
@@ -0,0 +1,82 @@
+import {
+  EntityTemporalVersioningMetadata,
+  QueryTemporalAxes,
+} from "@blockprotocol/graph/.";
+import { MockBlockDock } from "mock-block-dock";
+import { createRoot } from "react-dom/client";
+
+import { VersionedUri } from "@blockprotocol/type-system/slim";
+import { default as packageJson } from "../package.json";
+import Component from "./index";
+import { RootEntity } from "./types";
+
+const node = document.getElementById("app");
+
+const intervalForAllTime =
+  (): EntityTemporalVersioningMetadata[keyof EntityTemporalVersioningMetadata] => {
+    return {
+      start: {
+        kind: "inclusive",
+        limit: new Date(0).toISOString(),
+      },
+      end: {
+        kind: "unbounded",
+      },
+    } as const;
+  };
+
+const entityTemporalMetadata = (): EntityTemporalVersioningMetadata => {
+  return {
+    transactionTime: intervalForAllTime(),
+    decisionTime: intervalForAllTime(),
+  };
+};
+
+const currentTime = new Date().toISOString();
+
+const temporalAxes: QueryTemporalAxes = {
+  pinned: {
+    axis: "transactionTime",
+    timestamp: currentTime,
+  },
+  variable: {
+    axis: "decisionTime",
+    interval: {
+      start: { kind: "unbounded" },
+      end: { kind: "inclusive", limit: currentTime },
+    },
+  },
+};
+
+const testEntity: RootEntity = {
+  metadata: {
+    recordId: {
+      entityId: "test-entity",
+      editionId: new Date().toISOString(),
+    },
+    entityTypeId: packageJson.blockprotocol.schema as VersionedUri,
+    temporalVersioning: entityTemporalMetadata(),
+  },
+  properties: {},
+} as const;
+
+const DevApp = () => {
+  return (
+    <MockBlockDock
+      blockDefinition={{ ReactComponent: Component }}
+      blockEntityRecordId={testEntity.metadata.recordId}
+      blockInfo={packageJson.blockprotocol}
+      initialData={{
+        initialEntities: [testEntity],
+        initialTemporalAxes: temporalAxes,
+      }}
+      debug
+    />
+  );
+};
+
+if (node) {
+  createRoot(node).render(<DevApp />);
+} else {
+  throw new Error("Unable to find DOM element with id 'app'");
+}
