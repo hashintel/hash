0fbf63fe4e7ea9502704922f2d9dc2174505d1b7
Tim Diekmann <tim.diekmann@3dvision.de>
2023-02-21 15:27:58 +0100

    Introduce `AdjacencyList` struct to hold edges

--- apps/hash-graph/lib/graph/src/shared/subgraph/edges.rs
+++ apps/hash-graph/lib/graph/src/shared/subgraph/edges.rs
@@ -1,6 +1,10 @@
-use std::collections::{hash_map::Entry, HashMap, HashSet};
+use std::{
+    collections::{hash_map::Entry, BTreeMap, HashMap, HashSet},
+    fmt::Debug,
+    hash::Hash,
+};
 
-use crate::identifier::{EntityVertexId, OntologyTypeVertexId};
+use crate::identifier::{EdgeEndpointSet, EntityVertexId, OntologyTypeVertexId, VertexId};
 
 mod edge;
 mod kind;
@@ -12,6 +16,120 @@ pub use self::{
         OutgoingEdgeResolveDepth, SharedEdgeKind,
     },
 };
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct EdgeKind<K> {
+    kind: K,
+    reversed: bool,
+}
+
+pub struct AdjacencyList<V, K, E>
+where
+    V: VertexId,
+    E: EdgeEndpointSet,
+{
+    #[expect(clippy::type_complexity)]
+    edges: HashMap<V::BaseId, BTreeMap<V::RevisionId, HashMap<EdgeKind<K>, E>>>,
+}
+
+impl<V, K, E> AdjacencyList<V, K, E>
+where
+    V: VertexId,
+    V::BaseId: Clone,
+    E: EdgeEndpointSet,
+{
+    pub fn insert(
+        &mut self,
+        vertex_id: &V,
+        edge_kind: K,
+        reversed: bool,
+        right_endpoint: E::EdgeEndpoint,
+    ) -> bool
+    where
+        V::BaseId: Hash + Eq + Clone,
+        V::RevisionId: Ord,
+        K: Hash + Eq,
+        E: Default,
+    {
+        let vertex_base_id = vertex_id.base_id();
+        self.edges
+            .raw_entry_mut()
+            .from_key(vertex_base_id)
+            .or_insert_with(|| (vertex_base_id.clone(), BTreeMap::new()))
+            .1
+            .entry(vertex_id.revision_id())
+            .or_default()
+            .entry(EdgeKind {
+                kind: edge_kind,
+                reversed,
+            })
+            .or_default()
+            .insert(right_endpoint)
+    }
+
+    pub fn into_flattened<O>(
+        self,
+    ) -> impl Iterator<Item = (V::BaseId, BTreeMap<V::RevisionId, Vec<O>>)>
+    where
+        V::RevisionId: Ord,
+        K: Copy,
+        O: From<OutwardEdge<K, E::EdgeEndpoint>>,
+    {
+        self.edges.into_iter().map(|(base_id, versions)| {
+            (
+                base_id,
+                versions
+                    .into_iter()
+                    .map(|(version, edges)| {
+                        (
+                            version,
+                            edges
+                                .into_iter()
+                                .flat_map(move |(edge, targets)| {
+                                    targets.into_iter().map(move |right_endpoint| {
+                                        OutwardEdge {
+                                            kind: edge.kind,
+                                            reversed: edge.reversed,
+                                            right_endpoint,
+                                        }
+                                        .into()
+                                    })
+                                })
+                                .collect(),
+                        )
+                    })
+                    .collect(),
+            )
+        })
+    }
+}
+
+impl<V, K, E> Default for AdjacencyList<V, K, E>
+where
+    V: VertexId,
+    E: EdgeEndpointSet,
+{
+    fn default() -> Self {
+        Self {
+            edges: HashMap::new(),
+        }
+    }
+}
+
+impl<V, K, E> Debug for AdjacencyList<V, K, E>
+where
+    V: VertexId,
+    V::BaseId: Debug,
+    V::RevisionId: Debug,
+    K: Debug,
+    E: EdgeEndpointSet + Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("AdjacencyList")
+            .field("edges", &self.edges)
+            .finish()
+    }
+}
 
 #[derive(Default, Debug)]
 pub struct Edges {
