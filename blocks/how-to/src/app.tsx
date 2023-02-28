a2a0784cb7c68ef5c6ad219e9df74fbb2160e85e
luisbettencourt <lmn.bettencourt@gmail.com>
2023-02-28 20:21:01 +0000

    add stepEntities to useEffect dependency array

--- blocks/how-to/src/app.tsx
+++ blocks/how-to/src/app.tsx
@@ -218,7 +218,7 @@ export const App: BlockComponent<RootEntity> = ({
     if (!stepEntities.length) {
       addStep();
     }
-  }, []);
+  }, [stepEntities]);
 
   const schema = useMemo(() => {
     const stepsWithTitle = stepEntities.filter(
