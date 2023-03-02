8d49c62168ef25c7b24076c1082b9688e29193e7
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-03-02 16:28:13 +0100

    Merge overlapping intervals when iterating through edge endpoints (#2116)
    ## 🌟 What is the purpose of this PR?
    
    This implements merging overlapping intervals on edge endpoints.
    
    ## 🔗 Related links
    
    - [Asana task](https://app.asana.com/0/1201095311341924/1203399924452451/f) _(internal)_
    
    ## 🚫 Blocked by
    
    - #2110 
    - #2115 
    
    ## 🔍 What does this change?
    
    - Implements unification of intervals when iterating the `EntityIdWithIntervalSet`
    - Simplify implementation of `Interval::union`
    
    ## ⚠️ Known issues
    
    Please see the TODO comment in `EntityIdWithIntervalSet::into_iter`

--- apps/hash-graph/lib/graph/src/shared/identifier.rs
+++ apps/hash-graph/lib/graph/src/shared/identifier.rs
@@ -4,7 +4,7 @@ pub mod ontology;
 pub mod time;
 
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{BTreeSet, HashMap, HashSet},
     hash::BuildHasher,
 };
 
@@ -153,20 +153,20 @@ impl EdgeEndpoint for EntityIdWithInterval {
 pub trait EdgeEndpointSet: IntoIterator<Item = Self::EdgeEndpoint> {
     type EdgeEndpoint: EdgeEndpoint;
 
-    fn insert(&mut self, target_id: Self::EdgeEndpoint) -> bool;
+    fn insert(&mut self, target_id: Self::EdgeEndpoint);
 }
 
 impl<S: BuildHasher> EdgeEndpointSet for HashSet<OntologyTypeVertexId, S> {
     type EdgeEndpoint = OntologyTypeVertexId;
 
-    fn insert(&mut self, edge_target_id: Self::EdgeEndpoint) -> bool {
-        self.insert(edge_target_id)
+    fn insert(&mut self, edge_target_id: Self::EdgeEndpoint) {
+        self.insert(edge_target_id);
     }
 }
 
 #[derive(Debug, Default)]
 pub struct EntityIdWithIntervalSet {
-    inner: HashMap<EntityId, HashSet<LeftClosedTemporalInterval<VariableAxis>>>,
+    inner: HashMap<EntityId, BTreeSet<LeftClosedTemporalInterval<VariableAxis>>>,
 }
 
 impl IntoIterator for EntityIdWithIntervalSet {
@@ -176,7 +176,46 @@ impl IntoIterator for EntityIdWithIntervalSet {
 
     fn into_iter(self) -> Self::IntoIter {
         self.inner.into_iter().flat_map(|(entity_id, intervals)| {
+            // This merges overlapping intervals
+            //  Examples   |       1       |       2       |       3
+            //  ===========|===============|===============|===============
+            //  Interval A | [--]          | (--]          | [--]
+            //  Interval B |      [-]      |      [-)      |      [-)
+            //  Interval C |  [--]         |  [--]         |  [---]
+            //  Interval D |           [-] |           [-] |        [----]
+            //  -----------|---------------|---------------|---------------
+            //  Union      | [------]  [-] | (------)  [-] | [-----------]
+
+            // TODO: This is a very primitive implementation. Instead of iterating over all elements
+            //       and merging them, we should use a data structure that allows for efficient
+            //       merging of intervals. This is probably a tree structure that allows for
+            //       efficient merging of overlapping intervals.
+            //   See https://en.wikipedia.org/wiki/Interval_tree
+            //    or https://en.wikipedia.org/wiki/Segment_tree
+            //       For simplicity, we stick with this fairly robust implementation for now, but
+            //       as a short-term optimization, we could utilize `#![feature(generators)]` to
+            //       avoid the allocation of the intermediate vector.
             intervals
+                .into_iter()
+                .fold(
+                    Vec::<LeftClosedTemporalInterval<VariableAxis>>::new(),
+                    |mut acc, interval| {
+                        // The intervals are sorted, it's only necessary to check the union of this
+                        // with the last interval, if it overlaps two of the previous ones (which
+                        // would make it necessary to check the union with more than just the last)
+                        // then those would have been merged into one in the previous iteration
+                        // (again because they are sorted).
+                        if let Some(last) = acc.pop() {
+                            // `union` either returns one or two intervals, depending on whether
+                            // they overlap or not. If two intervals are returned, the ordering is
+                            // stable, so we can just push them in order.
+                            acc.extend(last.union(interval));
+                        } else {
+                            acc.push(interval);
+                        }
+                        acc
+                    },
+                )
                 .into_iter()
                 .map(move |interval| EntityIdWithInterval {
                     entity_id,
@@ -189,12 +228,10 @@ impl IntoIterator for EntityIdWithIntervalSet {
 impl EdgeEndpointSet for EntityIdWithIntervalSet {
     type EdgeEndpoint = EntityIdWithInterval;
 
-    fn insert(&mut self, edge_target_id: Self::EdgeEndpoint) -> bool {
-        // TODO: Merge overlapping intervals
-        //   see https://app.asana.com/0/0/1203399924452451/f
+    fn insert(&mut self, edge_target_id: Self::EdgeEndpoint) {
         self.inner
             .entry(edge_target_id.entity_id)
             .or_default()
-            .insert(edge_target_id.interval)
+            .insert(edge_target_id.interval);
     }
 }
