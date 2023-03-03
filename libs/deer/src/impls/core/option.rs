dbd411e51a023e27dda8d461b1c7d17e3e58d3ae
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-03-03 18:46:48 +0100

    feat: implementation Option

--- libs/deer/src/impls/core/option.rs
+++ libs/deer/src/impls/core/option.rs
@@ -1,25 +1,16 @@
-use alloc::{string::String, vec::Vec};
 use core::marker::PhantomData;
 
 use error_stack::{Result, ResultExt};
 
 use crate::{
     error::{DeserializeError, VisitorError},
-    value::{
-        ArrayAccessDeserializer, BoolDeserializer, BorrowedBytesDeserializer,
-        BorrowedStrDeserializer, BytesBufferDeserializer, BytesDeserializer, CharDeserializer,
-        F32Deserializer, F64Deserializer, I128Deserializer, I16Deserializer, I32Deserializer,
-        I64Deserializer, I8Deserializer, IsizeDeserializer, NumberDeserializer,
-        ObjectAccessDeserializer, StrDeserializer, StringDeserializer, U128Deserializer,
-        U16Deserializer, U32Deserializer, U64Deserializer, U8Deserializer, UsizeDeserializer,
-    },
-    ArrayAccess, Context, Deserialize, Deserializer, Document, Number, ObjectAccess, Reflection,
-    Schema, Visitor,
+    Deserialize, Deserializer, Document, ObjectAccess, OptionalVisitor, Reflection, Schema,
+    Visitor,
 };
 
-struct OptionVisitor<'a, T>(PhantomData<fn() -> *const T>, &'a Context);
+struct OptionVisitor<T>(PhantomData<fn() -> *const T>);
 
-impl<'de, T: Deserialize<'de>> Visitor<'de> for OptionVisitor<'_, T> {
+impl<'de, T: Deserialize<'de>> OptionalVisitor<'de> for OptionVisitor<T> {
     type Value = Option<T>;
 
     fn expecting(&self) -> Document {
@@ -34,114 +25,17 @@ impl<'de, T: Deserialize<'de>> Visitor<'de> for OptionVisitor<'_, T> {
         Ok(None)
     }
 
-    fn visit_bool(self, v: bool) -> Result<Self::Value, VisitorError> {
-        T::deserialize(BoolDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_number(self, v: Number) -> Result<Self::Value, VisitorError> {
-        T::deserialize(NumberDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_char(self, v: char) -> Result<Self::Value, VisitorError> {
-        T::deserialize(CharDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_str(self, v: &str) -> Result<Self::Value, VisitorError> {
-        T::deserialize(StrDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_borrowed_str(self, v: &'de str) -> Result<Self::Value, VisitorError> {
-        T::deserialize(BorrowedStrDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_string(self, v: String) -> Result<Self::Value, VisitorError> {
-        T::deserialize(StringDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_bytes(self, v: &[u8]) -> Result<Self::Value, VisitorError> {
-        T::deserialize(BytesDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_borrowed_bytes(self, v: &'de [u8]) -> Result<Self::Value, VisitorError> {
-        T::deserialize(BorrowedBytesDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_bytes_buffer(self, v: Vec<u8>) -> Result<Self::Value, VisitorError> {
-        T::deserialize(BytesBufferDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_array<U>(self, v: U) -> Result<Self::Value, VisitorError>
+    fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, VisitorError>
     where
-        U: ArrayAccess<'de>,
+        D: Deserializer<'de>,
     {
-        T::deserialize(ArrayAccessDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_object<U>(self, v: U) -> Result<Self::Value, VisitorError>
-    where
-        U: ObjectAccess<'de>,
-    {
-        T::deserialize(ObjectAccessDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_i8(self, v: i8) -> Result<Self::Value, VisitorError> {
-        T::deserialize(I8Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_i16(self, v: i16) -> Result<Self::Value, VisitorError> {
-        T::deserialize(I16Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_i32(self, v: i32) -> Result<Self::Value, VisitorError> {
-        T::deserialize(I32Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_i64(self, v: i64) -> Result<Self::Value, VisitorError> {
-        T::deserialize(I64Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_i128(self, v: i128) -> Result<Self::Value, VisitorError> {
-        T::deserialize(I128Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_isize(self, v: isize) -> Result<Self::Value, VisitorError> {
-        T::deserialize(IsizeDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_u8(self, v: u8) -> Result<Self::Value, VisitorError> {
-        T::deserialize(U8Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_u16(self, v: u16) -> Result<Self::Value, VisitorError> {
-        T::deserialize(U16Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_u32(self, v: u32) -> Result<Self::Value, VisitorError> {
-        T::deserialize(U32Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_u64(self, v: u64) -> Result<Self::Value, VisitorError> {
-        T::deserialize(U64Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_u128(self, v: u128) -> Result<Self::Value, VisitorError> {
-        T::deserialize(U128Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_usize(self, v: usize) -> Result<Self::Value, VisitorError> {
-        T::deserialize(UsizeDeserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_f32(self, v: f32) -> Result<Self::Value, VisitorError> {
-        T::deserialize(F32Deserializer::new(v, &self.1)).change_context(VisitorError)
-    }
-
-    fn visit_f64(self, v: f64) -> Result<Self::Value, VisitorError> {
-        T::deserialize(F64Deserializer::new(v, &self.1)).change_context(VisitorError)
+        T::deserialize(deserializer)
+            .change_context(VisitorError)
+            .map(Some)
     }
 }
 
-impl<T: Reflection> Reflection for Option<T> {
+impl<T: Reflection + ?Sized> Reflection for Option<T> {
     fn schema(doc: &mut Document) -> Schema {
         // TODO: how?!
         todo!()
@@ -152,10 +46,7 @@ impl<'de, T: Deserialize<'de>> Deserialize<'de> for Option<T> {
     type Reflection = Option<T::Reflection>;
 
     fn deserialize<D: Deserializer<'de>>(de: D) -> Result<Self, DeserializeError> {
-        // TODO: deserialize_maybe?
-        // TODO: is deserialize_any ok here?
-        //  non self-describing formats _might_ fail here (e.g. bincode)
-        de.deserialize_any(OptionVisitor(PhantomData, de.context()))
+        de.deserialize_optional(OptionVisitor(PhantomData))
             .change_context(DeserializeError)
     }
 }
