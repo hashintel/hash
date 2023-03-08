879d57ce9b0b7945577c1e763ff0f7bfce3e57bb
luisbettencourt <lmn.bettencourt@gmail.com>
2023-03-08 13:56:02 +0000

    add model selector

--- blocks/ai-text/src/app/generate-text.tsx
+++ blocks/ai-text/src/app/generate-text.tsx
@@ -16,10 +16,13 @@ import {
 import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
 
 import { contentKey } from "../app";
-import { AbstractAiIcon } from "../icons/abstract-ai";
 import { ArrowTurnDownLeftIcon } from "../icons/arrow-turn-down-left";
 import { RootEntity } from "../types";
 import { BouncingDotsLoader } from "./generate-text/bouncing-dots-loader";
+import {
+  DEFAULT_MODEL_ID,
+  ModelSelector,
+} from "./generate-text/model-selector";
 import { TextPreview } from "./generate-text/text-preview";
 
 export const promptKey: keyof RootEntity["properties"] =
@@ -32,6 +35,7 @@ export const GenerateText = ({ blockEntity }: { blockEntity: RootEntity }) => {
   const blockRootRef = useRef<HTMLDivElement>(null);
 
   const initialPromptText = blockEntity.properties[promptKey];
+  const initialModel = blockEntity.properties[modelKey];
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -44,9 +48,7 @@ export const GenerateText = ({ blockEntity }: { blockEntity: RootEntity }) => {
   const [inputFocused, setInputFocused] = useState(false);
   const [hovered, setHovered] = useState(false);
 
-  // @todo implement model selection UI
-  // @see https://app.asana.com/0/1203358502199087/1203701786066059/f
-  const [model, _setModel] = useState("text-davinci-003");
+  const [model, setModel] = useState(initialModel ?? DEFAULT_MODEL_ID);
   const [promptText, setPromptText] = useState(initialPromptText ?? "");
   const [generatedText, setGeneratedText] = useState("");
 
@@ -71,15 +73,33 @@ export const GenerateText = ({ blockEntity }: { blockEntity: RootEntity }) => {
       setErrorMessage("");
       setLoading(true);
 
-      const { data, errors } = await serviceModule.openaiCompleteText({
-        data: {
-          max_tokens: 1000,
-          model,
-          prompt: promptText,
-        },
-      });
+      const isTurbo = model === "gpt-3.5-turbo";
+      const { data, errors } = await (isTurbo
+        ? serviceModule.openaiCompleteChat({
+            data: {
+              max_tokens: 1000,
+              messages: [{ role: "user", content: promptText }],
+              model: "gpt-3.5-turbo",
+            },
+          })
+        : serviceModule.openaiCompleteText({
+            data: {
+              max_tokens: 1000,
+              model,
+              prompt: promptText,
+            },
+          }));
 
-      const textResponse = data?.choices[0]?.text;
+      const choice = data?.choices[0];
+
+      let textResponse: string | undefined;
+      if (choice) {
+        if ("message" in choice) {
+          textResponse = choice.message?.content;
+        } else if ("text" in choice) {
+          textResponse = choice.text;
+        }
+      }
 
       if (errors || !textResponse) {
         setErrorMessage("An error occurred");
@@ -119,8 +139,8 @@ export const GenerateText = ({ blockEntity }: { blockEntity: RootEntity }) => {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <Fade in={hovered || inputFocused || animatingIn || animatingOut}>
-        <Box sx={{ display: "flex", columnGap: 3, flexWrap: "wrap" }}>
+      <Fade in={true || hovered || inputFocused || animatingIn || animatingOut}>
+        <Box sx={{ display: "flex", columnGap: 3, flexWrap: "wrap", mb: 1.5 }}>
           <Link
             href="https://blockprotocol.org/@hash/blocks/ai-text"
             target="_blank"
@@ -132,7 +152,6 @@ export const GenerateText = ({ blockEntity }: { blockEntity: RootEntity }) => {
               fontSize: 15,
               lineHeight: 1,
               letterSpacing: -0.02,
-              marginBottom: 1.5,
               whiteSpace: "nowrap",
               color: palette.gray[50],
               fill: palette.gray[40],
@@ -149,36 +168,11 @@ export const GenerateText = ({ blockEntity }: { blockEntity: RootEntity }) => {
             />
           </Link>
 
-          <Typography
-            variant="regularTextLabels"
-            sx={{
-              display: "inline-flex",
-              alignItems: "center",
-              textDecoration: "none",
-              fontSize: 15,
-              lineHeight: 1,
-              letterSpacing: -0.02,
-              marginBottom: 1.5,
-              flexWrap: "wrap",
-              color: ({ palette }) => palette.gray[50],
-            }}
-          >
-            <Box component="span" sx={{ mr: 1 }}>
-              Using
+          <Fade in={!generatedText}>
+            <Box display="flex" gap={1} alignItems="center">
+              <ModelSelector model={model} onModelChange={setModel} />
             </Box>
-            <Box
-              component="span"
-              sx={{
-                display: "inline-flex",
-                alignItems: "center",
-                color: ({ palette }) => palette.gray[60],
-                mr: 1,
-              }}
-            >
-              <AbstractAiIcon sx={{ fontSize: 16, mr: 0.375 }} />
-              OpenAI GPT-3 Davinci
-            </Box>
-          </Typography>
+          </Fade>
         </Box>
       </Fade>
 
---
+++ blocks/ai-text/src/app/generate-text/model-selector.tsx
@@ -0,0 +1,321 @@
+import { MenuItem, Select } from "@hashintel/design-system";
+import {
+  Box,
+  inputBaseClasses,
+  outlinedInputClasses,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { ReactNode, useMemo, useState } from "react";
+
+import { AbstractAiIcon } from "../../icons/abstract-ai";
+
+export const DEFAULT_MODEL_ID = "gpt-3.5-turbo";
+
+enum ModelGroupName {
+  OpenAi = "OpenAi",
+}
+
+interface ModelGroup {
+  name: string;
+  icon: ReactNode;
+  models: Model[];
+}
+
+interface Model {
+  id: string;
+  name: string;
+  description: string;
+}
+
+const MODELS_BY_GROUP: { [key in ModelGroupName]: ModelGroup } = {
+  [ModelGroupName.OpenAi]: {
+    name: ModelGroupName.OpenAi,
+    icon: <AbstractAiIcon sx={{ fontSize: "inherit" }} />,
+    models: [
+      {
+        id: "gpt-3.5-turbo",
+        name: "GPT-3.5 Turbo",
+        description:
+          "The best current language model for many use cases. Newer, faster and 10x cheaper than Davinci.",
+      },
+      {
+        id: "text-davinci-003",
+        name: "GPT-3 Davinci",
+        description:
+          "Great at writing long-form text, complex intent, cause and effect, summarization",
+      },
+      {
+        id: "text-curie-001",
+        name: "GPT-3 Curie",
+        description: "Good at language translation, Q&A",
+      },
+      {
+        id: "text-babbage-001",
+        name: "GPT-3 Babbage",
+        description: "Good at moderate classification tasks",
+      },
+      {
+        id: "text-ada-001",
+        name: "GPT-3 Ada",
+        description: "Good at parsing, correction, keywords",
+      },
+    ],
+  },
+};
+
+const INPUT_HEIGHT = 23;
+const INPUT_PADDING_TOP = 16;
+const INPUT_PADDING_BOTTOM = 12;
+
+export const ModelSelector = ({
+  model,
+  onModelChange,
+}: {
+  model: string;
+  onModelChange: (model: string) => void;
+}) => {
+  const [open, setOpen] = useState(false);
+  const [selectRef, setSelectRef] = useState<HTMLSelectElement | null>(null);
+  const [menuRef, setMenuRef] = useState<HTMLDivElement | null>(null);
+
+  const [selectedModel, selectedGroup] = useMemo(() => {
+    const groups = Object.values(MODELS_BY_GROUP);
+    for (const group of groups) {
+      for (const groupModel of group.models) {
+        if (groupModel.id === model) {
+          return [groupModel, group];
+        }
+      }
+    }
+
+    return [null, null];
+  }, [model]);
+
+  const paperProps = useMemo(() => {
+    const inputWidth = selectRef?.offsetWidth ?? 0;
+    const paperOffset = INPUT_HEIGHT + INPUT_PADDING_TOP;
+    const inputHeight = paperOffset + INPUT_PADDING_BOTTOM;
+    const paperWidth = Math.max(inputWidth, 340);
+    const paperHeight = (menuRef?.offsetHeight ?? 0) + inputHeight;
+
+    return {
+      sx: {
+        borderRadius: 1.5,
+        overflow: "hidden",
+        width: paperWidth,
+        height: paperHeight,
+        marginTop: `${-paperOffset}px`,
+        paddingTop: `${inputHeight}px`,
+        marginLeft: `${(paperWidth - inputWidth) / 2}px`,
+      },
+    };
+  }, [menuRef?.offsetHeight, selectRef?.offsetWidth]);
+
+  return (
+    <Select
+      open={open}
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+      value={model}
+      onChange={(event) => {
+        console.log(event.target.value);
+        onModelChange(event.target.value);
+      }}
+      ref={(ref: HTMLSelectElement | null) => {
+        if (ref) {
+          setSelectRef(ref);
+        }
+      }}
+      renderValue={() => {
+        if (selectedModel) {
+          const { name } = selectedModel;
+          const { name: groupName, icon } = selectedGroup;
+          return (
+            <Typography
+              variant="regularTextLabels"
+              sx={{
+                display: "inline-flex",
+                gap: 1,
+                alignItems: "center",
+                fontSize: 15,
+                lineHeight: 1,
+                letterSpacing: -0.02,
+                color: ({ palette }) => palette.gray[50],
+              }}
+            >
+              Using
+              <Box
+                component="span"
+                sx={{
+                  display: "inline-flex",
+                  gap: 0.375,
+                  color: ({ palette }) => palette.gray[60],
+                }}
+              >
+                <Box sx={{ fontSize: 16 }}>{icon}</Box>
+                {groupName} {name}
+              </Box>
+            </Typography>
+          );
+        }
+
+        return null;
+      }}
+      inputProps={{
+        sx: {
+          paddingY: 0,
+          [`~.${outlinedInputClasses.notchedOutline}`]: {
+            border: "none !important",
+            boxShadow: "none !important",
+            background: "none !important",
+          },
+        },
+      }}
+      MenuProps={{
+        PaperProps: paperProps,
+        MenuListProps: {
+          sx: {
+            padding: 0,
+          },
+        },
+      }}
+      sx={{
+        [`.${inputBaseClasses.root}`]: {
+          boxShadow: "none !important",
+        },
+        ...(open
+          ? {
+              position: "relative",
+              zIndex: 9999,
+            }
+          : {}),
+      }}
+    >
+      <Box
+        ref={(ref: HTMLDivElement | null) => {
+          if (ref) {
+            setMenuRef(ref);
+          }
+        }}
+      >
+        {Object.values(MODELS_BY_GROUP).map(
+          ({ name: groupName, icon, models }) => (
+            <Box key={groupName}>
+              <Typography
+                sx={{
+                  fontWeight: 500,
+                  fontSize: 12,
+                  lineHeight: 1.3,
+                  letterSpacing: "0.03em",
+                  textTransform: "uppercase",
+                  color: ({ palette }) => palette.gray[50],
+                  mb: 1,
+                  padding: 2,
+                  paddingBottom: 0,
+                }}
+              >
+                {groupName}
+              </Typography>
+
+              <Stack gap={1}>
+                {models.map(({ id, name, description }) => {
+                  const active = id === model;
+                  return (
+                    <MenuItem
+                      key={id}
+                      onClick={() => {
+                        onModelChange(id);
+                        setOpen(false);
+                      }}
+                      sx={({ palette }) => ({
+                        display: "flex",
+                        gap: 1.5,
+                        ":hover": {
+                          ".menu-item-icon": {
+                            color: palette.blue[50],
+                          },
+                          ".menu-item-name": {
+                            color: palette.blue[50],
+                          },
+                          ".menu-item-id": {
+                            color: palette.gray[70],
+                          },
+                          ".menu-item-description": {
+                            color: palette.common.black,
+                          },
+                        },
+                      })}
+                    >
+                      <Box
+                        className="menu-item-icon"
+                        sx={{
+                          paddingX: 1.125,
+                          fontSize: 22,
+                          color: ({ palette }) =>
+                            active ? palette.blue[70] : palette.gray[50],
+                        }}
+                      >
+                        {icon}
+                      </Box>
+
+                      <Box>
+                        <Box display="flex" gap={1} alignItems="center">
+                          <Typography
+                            className="menu-item-name"
+                            sx={{
+                              fontWeight: 500,
+                              fontSize: 14,
+                              lineHeight: "18px",
+                              color: ({ palette }) =>
+                                active ? palette.blue[70] : palette.gray[90],
+                            }}
+                          >
+                            {name}
+                          </Typography>
+                          <Box
+                            sx={{
+                              width: "4px",
+                              height: "4px",
+                              background: ({ palette }) => palette.gray[30],
+                              borderRadius: "50%",
+                            }}
+                          />
+                          <Typography
+                            className="menu-item-id"
+                            sx={{
+                              fontWeight: 500,
+                              fontSize: 13,
+                              lineHeight: "18px",
+                              color: ({ palette }) =>
+                                active ? palette.gray[70] : palette.gray[50],
+                            }}
+                          >
+                            {id}
+                          </Typography>
+                        </Box>
+                        <Typography
+                          className="menu-item-description"
+                          sx={{
+                            fontWeight: 500,
+                            fontSize: 13,
+                            lineHeight: "18px",
+                            color: ({ palette }) =>
+                              active ? palette.common.black : palette.gray[70],
+                            whiteSpace: "break-spaces",
+                          }}
+                        >
+                          {description}
+                        </Typography>
+                      </Box>
+                    </MenuItem>
+                  );
+                })}
+              </Stack>
+            </Box>
+          ),
+        )}
+      </Box>
+    </Select>
+  );
+};
