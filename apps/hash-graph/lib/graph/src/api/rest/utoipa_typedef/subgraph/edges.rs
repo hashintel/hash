e80f0c5e98d624c1b0840a952a289d4d60e4af3c
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-03-15 17:36:10 +0100

    fix: deep-merge

--- apps/hash-graph/lib/graph/src/api/rest/utoipa_typedef/subgraph/edges.rs
+++ apps/hash-graph/lib/graph/src/api/rest/utoipa_typedef/subgraph/edges.rs
@@ -1,5 +1,6 @@
 use std::{
-    collections::{BTreeMap, HashMap},
+    collections::{hash_map::Entry, BTreeMap, HashMap},
+    fmt::Debug,
     hash::Hash,
 };
 
@@ -142,12 +143,24 @@ pub struct Edges {
     pub knowledge_graph: KnowledgeGraphRootedEdges,
 }
 
-fn collect_merge<T: Hash + Eq, U: Ord, V>(
-    mut accumulator: HashMap<T, BTreeMap<U, V>>,
-    (key, mut value): (T, BTreeMap<U, V>),
-) -> HashMap<T, BTreeMap<U, V>> {
-    let entry = accumulator.entry(key).or_default();
-    entry.append(&mut value);
+fn collect_merge<T: Hash + Eq + Debug, U: Ord + Debug, V: Debug>(
+    mut accumulator: HashMap<T, BTreeMap<U, Vec<V>>>,
+    (key, value): (T, BTreeMap<U, Vec<V>>),
+) -> HashMap<T, BTreeMap<U, Vec<V>>> {
+    match accumulator.entry(key) {
+        Entry::Occupied(mut occupied) => {
+            let entry = occupied.get_mut();
+
+            for (revision, mut edges) in value {
+                let buffer = entry.entry(revision).or_default();
+                buffer.append(&mut edges);
+            }
+        }
+        Entry::Vacant(vacant) => {
+            // hot path, instead of merging one by one, just replace
+            vacant.insert(value);
+        }
+    }
 
     accumulator
 }
@@ -207,5 +220,75 @@ impl ToSchema<'_> for Edges {
                 )))
                 .into(),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use type_system::url::BaseUrl;
+    use uuid::Uuid;
+
+    use crate::{
+        api::rest::utoipa_typedef::subgraph::Edges,
+        identifier::{
+            account::AccountId,
+            knowledge::EntityId,
+            ontology::OntologyTypeVersion,
+            time::{ClosedTemporalBound, LeftClosedTemporalInterval, OpenTemporalBound, Timestamp},
+        },
+        knowledge::EntityUuid,
+        provenance::OwnedById,
+        subgraph::{
+            edges::{KnowledgeGraphEdgeKind, SharedEdgeKind},
+            identifier::{EntityIdWithInterval, EntityTypeVertexId, EntityVertexId},
+        },
+    };
+
+    #[test]
+    fn merge_ontology() {
+        let vertex_id = EntityVertexId {
+            base_id: EntityId {
+                owned_by_id: OwnedById::new(AccountId::new(Uuid::new_v4())),
+                entity_uuid: EntityUuid::new(Uuid::new_v4()),
+            },
+            revision_id: Timestamp::now(),
+        };
+
+        let mut edges = crate::subgraph::edges::Edges::default();
+
+        // the data used does not matter, what only matters is that we actually merged the data
+        edges.entity_to_entity.insert(
+            &vertex_id,
+            KnowledgeGraphEdgeKind::HasRightEntity,
+            false,
+            EntityIdWithInterval {
+                entity_id: EntityId {
+                    owned_by_id: OwnedById::new(AccountId::new(Uuid::new_v4())),
+                    entity_uuid: EntityUuid::new(Uuid::new_v4()),
+                },
+                interval: LeftClosedTemporalInterval::new(
+                    ClosedTemporalBound::Inclusive(Timestamp::now()),
+                    OpenTemporalBound::Unbounded,
+                ),
+            },
+        );
+
+        edges.entity_to_entity_type.insert(
+            &vertex_id,
+            SharedEdgeKind::IsOfType,
+            false,
+            EntityTypeVertexId {
+                base_id: BaseUrl::new("https://example.com/".to_owned()).unwrap(),
+                revision_id: OntologyTypeVersion::new(0),
+            },
+        );
+
+        let edges = Edges::from(edges);
+        assert_eq!(edges.knowledge_graph.0.len(), 1);
+        let (_, values) = edges.knowledge_graph.0.iter().next().unwrap();
+        assert_eq!(values.len(), 1);
+
+        let (_, edges) = values.first_key_value().unwrap();
+        assert_eq!(edges.len(), 2);
     }
 }
