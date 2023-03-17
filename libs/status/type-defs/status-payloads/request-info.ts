7f5aced4fbd7a5357128503fcf1430aa8c2998d6
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-03-17 12:02:12 +0000

    Fix casing

--- libs/status/type-defs/status-payloads/request-info.ts
+++ libs/status/type-defs/status-payloads/request-info.ts
@@ -7,11 +7,11 @@ export type RequestInfo = {
    * An opaque string that should only be interpreted by the service generating it. For example, it
    * can be used to identify requests in the service's logs.
    */
-  request_id: string;
+  requestId: string;
 
   /**
    * Any data that was used to serve this request. For example, an encrypted stack trace that can be
    * sent back to the service provider for debugging.
    */
-  serving_data: string;
+  servingData: string;
 };
