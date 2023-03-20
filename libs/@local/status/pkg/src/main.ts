b45a260a78e0f9bbb87ace0076a99ea2f2d220fe
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-03-20 17:58:06 +0000

    Add `isStatus` type guard

--- libs/@local/status/pkg/src/main.ts
+++ libs/@local/status/pkg/src/main.ts
@@ -3,5 +3,22 @@
  * and RPC APIs.
  */
 
+import { Status } from "../../type-defs/status";
+import { StatusCode } from "./status-code";
+
 export { Status } from "../../type-defs/status";
 export { convertStatusCodeToHttpCode, StatusCode } from "./status-code";
+
+export const isStatus = (value: unknown): value is Status<object> => {
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    "code" in value &&
+    typeof value.code === "string" &&
+    Object.values(StatusCode).includes(value.code as StatusCode) &&
+    (!("message" in value) || typeof value.message === "string") &&
+    "contents" in value &&
+    Array.isArray(value.contents) &&
+    value.contents.every((content) => typeof content === "object")
+  );
+};
