9b2891afbf6365b877c748d25cecf78e7f5ef90e
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-03-21 16:14:30 +0100

    feat: `impl_builder!(goodbye)`

--- libs/antsi/src/font.rs
+++ libs/antsi/src/font.rs
@@ -116,36 +116,6 @@ pub enum FontScript {
     Super,
 }
 
-macro_rules! impl_builder {
-    (
-        $(#[$meta_set:meta])*
-        $set:ident($set_var:ident : $set_ty:ty);
-        $(#[$meta_with:meta])*
-        $with:ident($with_var:ident : $with_ty:ty);
-        $(#[$meta_getter:meta])*
-        $get:ident() -> $ret:ty
-    ) => {
-        $(#[$meta_set])*
-        pub fn $set(&mut self, $set_var: $set_ty) -> &mut Self {
-            self.$set_var = Some($set_var);
-            self
-        }
-
-        $(#[$meta_with])*
-        #[must_use]
-        pub const fn $with(mut self, $with_var: $with_ty) -> Self {
-            self.$with_var = Some($with_var);
-            self
-        }
-
-        $(#[$meta_getter])*
-        #[must_use]
-        pub const fn $get(&self) -> $ret {
-            self.$with_var
-        }
-    };
-}
-
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub struct Font {
     weight: Option<FontWeight>,
@@ -170,37 +140,6 @@ pub struct Font {
 }
 
 impl Font {
-    impl_builder!(
-        set_weight(weight: FontWeight);
-        with_weight(weight: FontWeight);
-        weight() -> Option<FontWeight>
-    );
-
-    impl_builder!(
-        set_family(family: FontFamily);
-        with_family(family: FontFamily);
-        family() -> Option<FontFamily>
-    );
-
-    impl_builder!(
-        set_underline(underline: Underline);
-        with_underline(underline: Underline);
-        underline() -> Option<Underline>
-    );
-
-    impl_builder!(
-        set_blinking(blinking: Blinking);
-        with_blinking(blinking: Blinking);
-        blinking() -> Option<Blinking>
-    );
-
-    #[cfg(feature = "script")]
-    impl_builder!(
-        set_script(script: FontScript);
-        with_script(script: FontScript);
-        script() -> Option<FontScript>
-    );
-
     #[must_use]
     pub const fn new() -> Self {
         Self {
@@ -213,6 +152,60 @@ impl Font {
             blinking: None,
         }
     }
+
+    pub fn set_weight(&mut self, weight: FontWeight) -> &mut Self {
+        self.weight = Some(weight);
+
+        self
+    }
+
+    #[must_use]
+    pub const fn with_weight(mut self, weight: FontWeight) -> Self {
+        self.weight = Some(weight);
+
+        self
+    }
+
+    pub fn set_family(&mut self, family: FontFamily) -> &mut Self {
+        self.family = Some(family);
+
+        self
+    }
+
+    #[must_use]
+    pub const fn with_family(mut self, family: FontFamily) -> Self {
+        self.family = Some(family);
+
+        self
+    }
+
+    pub fn set_underline(&mut self, underline: Underline) -> &mut Self {
+        self.underline = Some(underline);
+
+        self
+    }
+
+    #[must_use]
+    pub const fn with_underline(mut self, underline: Underline) -> Self {
+        self.underline = Some(underline);
+
+        self
+    }
+
+    pub fn set_blinking(&mut self, blinking: Blinking) -> &mut Self {
+        self.blinking = Some(blinking);
+
+        self
+    }
+
+    #[must_use]
+    pub const fn with_blinking(mut self, blinking: Blinking) -> Self {
+        self.blinking = Some(blinking);
+
+        self
+    }
+
+    // TODO: set, get font-script
 
     pub fn set_strikethrough(&mut self) -> &mut Self {
         self.style |= 1 << 0;
@@ -227,10 +220,7 @@ impl Font {
         self
     }
 
-    #[must_use]
-    pub const fn is_strikethrough(&self) -> bool {
-        (self.style & 0b0000_0001) > 0
-    }
+    // TODO: is_strikethrough
 
     pub fn set_inverse(&mut self) -> &mut Self {
         self.style |= 1 << 1;
@@ -245,10 +235,7 @@ impl Font {
         self
     }
 
-    #[must_use]
-    pub const fn is_inverse(&self) -> bool {
-        (self.style & 0b0000_0010) > 0
-    }
+    // TODO: is_inverse
 
     pub fn set_hidden(&mut self) -> &mut Self {
         self.style |= 1 << 2;
@@ -263,10 +250,7 @@ impl Font {
         self
     }
 
-    #[must_use]
-    pub const fn is_hidden(&self) -> bool {
-        (self.style & 0b0000_0100) > 0
-    }
+    // TODO: is_hidden
 
     pub fn set_italic(&mut self) -> &mut Self {
         self.style |= 1 << 3;
@@ -281,10 +265,7 @@ impl Font {
         self
     }
 
-    #[must_use]
-    pub const fn is_italic(&self) -> bool {
-        (self.style & 0b0000_1000) > 0
-    }
+    // TODO: is_italic
 
     #[cfg(feature = "overstrike")]
     pub fn set_overstrike(&mut self) -> &mut Self {
@@ -301,11 +282,7 @@ impl Font {
         self
     }
 
-    #[cfg(feature = "overstrike")]
-    #[must_use]
-    pub const fn is_overstrike(&self) -> bool {
-        (self.style & 0b0001_0000) > 0
-    }
+    // TODO: is_overstrike
 
     pub fn set_overline(&mut self) -> &mut Self {
         self.style |= 1 << 5;
@@ -313,15 +290,6 @@ impl Font {
         self
     }
 
-    #[must_use]
-    pub const fn with_overline(mut self) -> Self {
-        self.style |= 1 << 5;
-
-        self
-    }
-
-    #[must_use]
-    pub const fn is_overline(&self) -> bool {
-        (self.style & 0b0010_0000) > 0
-    }
+    // TODO: with_overline
+    // TODO: is_overline
 }
