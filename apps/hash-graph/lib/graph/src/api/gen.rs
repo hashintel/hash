51923f7381b3ede53d2800266da5a1fb282cac6a
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-03-23 17:21:02 +0000

    Disable lints on included generated file

--- apps/hash-graph/lib/graph/src/api/gen.rs
+++ apps/hash-graph/lib/graph/src/api/gen.rs
@@ -1,5 +1,7 @@
-#![allow(dead_code)]
-
 pub mod status_payloads {
+    #![allow(dead_code)]
+    #![allow(warnings)]
+    #![allow(clippy::all)]
+    #![allow(rustdoc::all)]
     include!(concat!(env!("OUT_DIR"), "/status_payloads.rs"));
 }
