c9d901f4be2b680748a62159cabf0e7e46ebad85
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2023-03-29 17:57:28 +0100

    Add `AI Chat` block (MVP) (#2299)
    * ft: initial ai-chat block
    
    * refactor: use env vars
    
    * ft: add `DropdownSelector` to design system
    
    * ft: dropdown-selector improvements
    
    * ft: add `ChatModelSelector` component
    
    * ft: add `SystemPromptSelector` component
    
    * ft: add `ExamplePrompts` component
    
    * fix: use `anchorOrigin` to set position in dropdown selector
    
    * ft: add `Header` component
    
    * ft: add `ChatMessage` component
    
    * ft: add `ChatTextField` component
    
    * ft: add `CompleteChat` component
    
    * chore: upgrade mock-block-dock
    
    * ft: persist messages in the graph
    
    * ft: populate component from the graph state
    
    * ft: basic readonly support
    
    * chore: commit generated type file
    
    * refactor: graph types
    
    * refactor: split generated entity types into distinct files
    
    * ft: update README
    
    * refactor: remove satisfies TS keyword uses
    
    * chore: add `@todo` comment
    
    * chore: yarn fix
    
    * ft: improve description of GPT-3.5 Turbo model
    
    * refactor: remove `assets.d.ts`
    
    * chore: fix TS error
    
    * refactor: remove possibility for superfluous `updateEntity` calls
    
    * refactor: package.json script names
    
    * fix: missing styling in preview mode
    
    * ft: add block icon
    
    * refactor: left-align header items
    
    * refactor: hide header unless mouse is hovering over component or input is focused
    
    * refactor: use `uuid` package
    
    * chore: `yarn fix`
    
    * chore: add `@todo`
    
    * ft: add preview image
    
    * fix: mobile responsiveness of example prompts
    
    * refactor: add max-width for entire chat
    
    * chore: update preview image
    
    * ft: improve mobile responsiveness of header
    
    * refactor: use production types
    
    * fix: hide header in readonly
    
    * fix: add root link when creating a request without a parent

---
+++ blocks/ai-chat/README.md
@@ -0,0 +1,3 @@
+# AI Chat
+
+The **AI Chat** block allows users to engage in interactive and dynamic text-based conversations with an AI language model that can respond to questions, provide information, offer suggestions, and simulate natural language dialogue on a wide range of topics.
---
+++ blocks/ai-chat/src/complete-chat/header.tsx
@@ -0,0 +1,122 @@
+import { BlockSettingsButton, GetHelpLink } from "@hashintel/design-system";
+import {
+  Box,
+  Collapse,
+  Fade,
+  inputBaseClasses,
+  selectClasses,
+  Typography,
+} from "@mui/material";
+import { FunctionComponent, useState } from "react";
+
+import { ChatModelId, ChatModelSelector } from "./chat-model-selector";
+import { SystemPromptId, SystemPromptSelector } from "./system-prompt-selector";
+
+export const Header: FunctionComponent<{
+  readonly: boolean;
+  isMobile: boolean;
+  disabled: boolean;
+  hovered: boolean;
+  inputFocused: boolean;
+  chatModel: ChatModelId;
+  setChatModel: (chatModel: ChatModelId) => void;
+  systemPromptId: SystemPromptId;
+  setSystemPromptId: (systemPromptId: SystemPromptId) => void;
+}> = ({
+  readonly,
+  isMobile,
+  disabled,
+  hovered,
+  inputFocused,
+  chatModel,
+  setChatModel,
+  systemPromptId,
+  setSystemPromptId,
+}) => {
+  const [mobileSettingsExpanded, setMobileSettingsExpanded] = useState(false);
+  const [chatModelSelectorOpen, setChatModelSelectorOpen] = useState(false);
+  const [systemPromptSelectorOpen, setSystemPromptSelectorOpen] =
+    useState(false);
+
+  return readonly ? null : (
+    <Fade in={hovered || inputFocused || (isMobile && mobileSettingsExpanded)}>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          mb: 1.5,
+          columnGap: 3,
+          rowGap: 1,
+        }}
+      >
+        <GetHelpLink
+          sx={{ fontWeight: 500 }}
+          href="https://blockprotocol.org/@hash/blocks/ai-chat"
+        />
+
+        {isMobile ? (
+          <BlockSettingsButton
+            expanded={mobileSettingsExpanded}
+            onClick={() => setMobileSettingsExpanded(!mobileSettingsExpanded)}
+          />
+        ) : null}
+
+        <Collapse in={!isMobile || mobileSettingsExpanded}>
+          <Typography
+            variant="regularTextLabels"
+            sx={{
+              display: "inline-flex",
+              gap: 1,
+              alignItems: "center",
+              fontWeight: 500,
+              fontSize: 15,
+              lineHeight: 1,
+              letterSpacing: -0.02,
+              flexWrap: "wrap",
+              color: ({ palette }) => palette.gray[50],
+            }}
+          >
+            Using
+            <ChatModelSelector
+              open={chatModelSelectorOpen}
+              disabled={disabled}
+              onOpen={() => setChatModelSelectorOpen(true)}
+              onClose={() => setChatModelSelectorOpen(false)}
+              value={chatModel}
+              onChange={setChatModel}
+              sx={{
+                [`& .${selectClasses.icon}`]: {
+                  right: 0,
+                },
+                [`& .${selectClasses.select}.${selectClasses.outlined}.${inputBaseClasses.input}`]:
+                  {
+                    paddingLeft: 0,
+                    transition: (theme) =>
+                      theme.transitions.create("padding-right"),
+                    paddingRight: disabled ? 0 : 2,
+                  },
+              }}
+            />
+            in
+            <SystemPromptSelector
+              open={systemPromptSelectorOpen}
+              disabled={disabled}
+              onOpen={() => setSystemPromptSelectorOpen(true)}
+              onClose={() => setSystemPromptSelectorOpen(false)}
+              value={systemPromptId}
+              onChange={setSystemPromptId}
+              sx={{
+                [`& .${selectClasses.select}`]: {
+                  paddingLeft: 0,
+                  transition: (theme) =>
+                    theme.transitions.create("padding-right"),
+                  paddingRight: disabled ? 0 : undefined,
+                },
+              }}
+            />
+          </Typography>
+        </Collapse>
+      </Box>
+    </Fade>
+  );
+};
