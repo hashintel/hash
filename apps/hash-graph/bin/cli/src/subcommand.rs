12eda46a5b80ca669b35f61d67af8d885db051e8
Tim Diekmann <post@timdiekmann.de>
2023-03-29 15:50:37 +0200

    Add a `snapshot` subcommand to the CLI

--- apps/hash-graph/bin/cli/src/subcommand.rs
+++ apps/hash-graph/bin/cli/src/subcommand.rs
@@ -1,6 +1,7 @@
 mod completions;
 mod migrate;
 mod server;
+mod snapshot;
 #[cfg(feature = "type-fetcher")]
 mod type_fetcher;
 
@@ -10,6 +11,7 @@ pub use self::{
     completions::{completions, CompletionsArgs},
     migrate::{migrate, MigrateArgs},
     server::{server, ServerArgs},
+    snapshot::{snapshot, SnapshotArgs},
 };
 
 /// Subcommand for the program.
@@ -24,4 +26,6 @@ pub enum Subcommand {
     TypeFetcher(TypeFetcherArgs),
     /// Generate a completion script for the given shell and outputs it to stdout.
     Completions(CompletionsArgs),
+    /// Snapshot API for the database.
+    Snapshot(SnapshotArgs),
 }
