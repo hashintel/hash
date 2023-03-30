89957644f5f85822ffcc9dc1ea88f9d868e34cc0
Tim Diekmann <post@timdiekmann.de>
2023-03-30 18:39:29 +0200

    Provide a `snapshot restore` command

--- apps/hash-graph/bin/cli/src/subcommand/snapshot.rs
+++ apps/hash-graph/bin/cli/src/subcommand/snapshot.rs
@@ -3,15 +3,13 @@ use std::io::Write;
 use clap::Parser;
 use error_stack::{IntoReport, Result, ResultExt};
 use graph::{
-    knowledge::Entity,
     logging::{init_logger, LoggingArgs},
     store::{
-        crud::Read, query::Filter, test_graph, test_graph::BlockProtocolModuleVersions,
+        test_graph::{self, TestStore},
         DatabaseConnectionInfo, PostgresStorePool, StorePool,
     },
 };
 use tokio_postgres::NoTls;
-use type_system::{DataType, EntityType, PropertyType};
 
 use crate::error::GraphError;
 
@@ -19,8 +17,12 @@ use crate::error::GraphError;
 pub struct SnapshotDumpArgs;
 
 #[derive(Debug, Parser)]
+pub struct SnapshotRestoreArgs;
+
+#[derive(Debug, Parser)]
 pub enum SnapshotCommand {
     Dump(SnapshotDumpArgs),
+    Restore(SnapshotRestoreArgs),
 }
 
 #[derive(Debug, Parser)]
@@ -47,7 +49,7 @@ pub async fn snapshot(args: SnapshotArgs) -> Result<(), GraphError> {
             report
         })?;
 
-    let store = pool
+    let mut store = pool
         .acquire()
         .await
         .change_context(GraphError)
@@ -58,53 +60,26 @@ pub async fn snapshot(args: SnapshotArgs) -> Result<(), GraphError> {
 
     match args.command {
         SnapshotCommand::Dump(_) => {
-            let data_types = Read::<test_graph::OntologyTypeRecord<DataType>>::read(
-                &store,
-                &Filter::All(vec![]),
-                None,
+            serde_json::to_writer(
+                &mut std::io::stdout(),
+                &store.read_test_graph().await.change_context(GraphError)?,
             )
-            .await
+            .into_report()
             .change_context(GraphError)?;
-
-            let property_types = Read::<test_graph::OntologyTypeRecord<PropertyType>>::read(
-                &store,
-                &Filter::All(vec![]),
-                None,
-            )
-            .await
-            .change_context(GraphError)?;
-
-            let entity_types = Read::<test_graph::OntologyTypeRecord<EntityType>>::read(
-                &store,
-                &Filter::All(vec![]),
-                None,
-            )
-            .await
-            .change_context(GraphError)?;
-
-            let entities = Read::<Entity>::read(&store, &Filter::All(vec![]), None)
-                .await
-                .change_context(GraphError)?
-                .into_iter()
-                .map(test_graph::EntityRecord::from)
-                .collect();
-
-            let test_graph = test_graph::TestData {
-                block_protocol_module_versions: BlockProtocolModuleVersions {
-                    graph: semver::Version::new(0, 3, 0),
-                },
-                data_types,
-                property_types,
-                entity_types,
-                entities,
-                custom: test_graph::CustomGlobalMetadata::default(),
-            };
-
-            serde_json::to_writer(&mut std::io::stdout(), &test_graph)
-                .into_report()
-                .change_context(GraphError)?;
             writeln!(std::io::stdout())
                 .into_report()
+                .change_context(GraphError)?;
+        }
+        SnapshotCommand::Restore(_) => {
+            let test_data: test_graph::TestData = serde_json::from_reader(std::io::stdin())
+                .into_report()
+                .change_context(GraphError)?;
+
+            println!("{:#?}", test_data);
+
+            store
+                .write_test_graph(test_data)
+                .await
                 .change_context(GraphError)?;
         }
     }
