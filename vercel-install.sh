9ad778c3f2a8b9c892d1ce04ca6fdd0e7162d943
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-04-07 16:48:52 +0100

    Try to encapsulate vercel install command in script instead

---
+++ vercel-install.sh
@@ -0,0 +1,7 @@
+amazon-linux-extras install -y docker;
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker};
+mkdir -p "$DOCKER_CONFIG"/cli-plugins;
+curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o "$DOCKER_CONFIG"/cli-plugins/docker-compose;
+chmod +x "$DOCKER_CONFIG"/cli-plugins/docker-compose;
+yarn add -W just-install;
+yarn;
