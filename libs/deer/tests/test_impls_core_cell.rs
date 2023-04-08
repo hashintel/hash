88c80fe801277ab357cd8cbe5e7f3b15416f0213
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-08 21:46:53 +0200

    test: reflection passthrough

--- libs/deer/tests/test_impls_core_cell.rs
+++ libs/deer/tests/test_impls_core_cell.rs
@@ -3,9 +3,11 @@ use core::cell::{Cell, RefCell, UnsafeCell};
 #[cfg(nightly)]
 use core::cell::{OnceCell, SyncUnsafeCell};
 
-use deer::Number;
+use deer::{Deserialize, Number};
 use deer_desert::{assert_tokens, assert_tokens_with_assertion, Token};
 use proptest::prelude::*;
+use serde::Serialize;
+use similar_asserts::assert_serde_eq;
 
 #[cfg(not(miri))]
 proptest! {
@@ -52,5 +54,54 @@ proptest! {
             Number::from(value),
         )]);
     }
+}
 
+fn assert_json(lhs: impl Serialize, rhs: impl Serialize) {
+    let lhs = serde_json::to_value(lhs).expect("should be able to serialize lhs");
+    let rhs = serde_json::to_value(rhs).expect("should be able to serialize rhs");
+
+    assert_serde_eq!(lhs, rhs);
+}
+
+// test that the `Reflection` of all types are the same as their underlying type
+#[test]
+fn cell_reflection_same() {
+    let lhs = Cell::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
+}
+
+#[test]
+fn ref_cell_reflection_same() {
+    let lhs = RefCell::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
+}
+
+#[test]
+fn unsafe_cell_reflection_same() {
+    let lhs = UnsafeCell::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
+}
+
+#[cfg(nightly)]
+#[test]
+fn once_cell_reflection_same() {
+    let lhs = OnceCell::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
+}
+
+#[cfg(nightly)]
+#[test]
+fn sync_unsafe_cell_reflection_same() {
+    let lhs = SyncUnsafeCell::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
 }
