ab0799f7a90a67d4f20b488e59db5455ddd21506
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-10 17:49:48 +0200

    feat: import #1875

--- libs/deer/src/impls/core/sync.rs
+++ libs/deer/src/impls/core/sync.rs
@@ -1,1 +1,18 @@
+#[cfg(nightly)]
+use core::sync::Exclusive;
+
+use crate::{error::DeserializeError, Deserialize, Deserializer};
+
 mod atomic;
+
+#[cfg(nightly)]
+impl<'de, T> Deserialize<'de> for Exclusive<T>
+where
+    T: Deserialize<'de>,
+{
+    type Reflection = T::Reflection;
+
+    fn deserialize<D: Deserializer<'de>>(de: D) -> error_stack::Result<Self, DeserializeError> {
+        T::deserialize(de).map(Self::new)
+    }
+}
