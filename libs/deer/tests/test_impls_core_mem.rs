bbb650324f306eb62f123e169e49f5902943de79
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-10 17:14:26 +0200

    test: `ManuallyDrop`

---
+++ libs/deer/tests/test_impls_core_mem.rs
@@ -0,0 +1,30 @@
+use core::mem::ManuallyDrop;
+
+use deer::Deserialize;
+use deer_desert::{assert_tokens, Token};
+use proptest::prelude::*;
+use serde::Serialize;
+use similar_asserts::assert_serde_eq;
+
+proptest! {
+    #[test]
+    fn manually_drop_ok(value in any::<u8>()) {
+        assert_tokens(&ManuallyDrop::new(value), &[Token::Number(value.into())]);
+    }
+}
+
+fn assert_json(lhs: impl Serialize, rhs: impl Serialize) {
+    let lhs = serde_json::to_value(lhs).expect("should be able to serialize lhs");
+    let rhs = serde_json::to_value(rhs).expect("should be able to serialize rhs");
+
+    assert_serde_eq!(lhs, rhs);
+}
+
+// test that the `Reflection` of all types are the same as their underlying type
+#[test]
+fn manually_drop_reflection_same() {
+    let lhs = ManuallyDrop::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
+}
