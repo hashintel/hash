0aad86a29bef0b0baedb7bde6d21d2b46f4d42c0
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-10 17:33:48 +0200

    fix: `Reflection`

--- libs/deer/src/impls/core/mem.rs
+++ libs/deer/src/impls/core/mem.rs
@@ -4,14 +4,8 @@ use error_stack::Result;
 
 use crate::{error::DeserializeError, Deserialize, Deserializer, Document, Reflection, Schema};
 
-impl<T: Reflection + ?Sized> Reflection for ManuallyDrop<T> {
-    fn schema(doc: &mut Document) -> Schema {
-        T::schema(doc)
-    }
-}
-
 impl<'de, T: Deserialize<'de>> Deserialize<'de> for ManuallyDrop<T> {
-    type Reflection = ManuallyDrop<T::Reflection>;
+    type Reflection = T::Reflection;
 
     fn deserialize<D: Deserializer<'de>>(de: D) -> Result<Self, DeserializeError> {
         T::deserialize(de).map(Self::new)
