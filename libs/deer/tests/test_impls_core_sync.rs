79bf55f22dd84566b43017196895d0c21c19430e
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-10 17:50:03 +0200

    test: `Exclusive`

---
+++ libs/deer/tests/test_impls_core_sync.rs
@@ -0,0 +1,36 @@
+#![feature(exclusive_wrapper)]
+#![cfg(nightly)]
+
+use core::sync::Exclusive;
+
+use deer::{Deserialize, Number};
+use deer_desert::{assert_tokens, Token};
+use proptest::prelude::*;
+use serde::Serialize;
+use similar_asserts::assert_serde_eq;
+
+#[cfg(not(miri))]
+proptest! {
+    #[test]
+    fn exclusive_ok(value in any::<u8>()) {
+        let expected = Exclusive::new(value);
+
+        assert_tokens(&expected, &[Token::Number(Number::from(value))]);
+    }
+}
+
+fn assert_json(lhs: impl Serialize, rhs: impl Serialize) {
+    let lhs = serde_json::to_value(lhs).expect("should be able to serialize lhs");
+    let rhs = serde_json::to_value(rhs).expect("should be able to serialize rhs");
+
+    assert_serde_eq!(lhs, rhs);
+}
+
+// test that the `Reflection` of all types are the same as their underlying type
+#[test]
+fn exclusive_reflection_same() {
+    let lhs = Exclusive::<u8>::reflection();
+    let rhs = u8::reflection();
+
+    assert_json(lhs, rhs);
+}
