d6c9ef9ec70811040b6ba37cf8749721c7e20961
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-10 21:43:58 +0200

    test: time via approx

--- libs/deer/tests/test_impls_core_time.rs
+++ libs/deer/tests/test_impls_core_time.rs
@@ -1,6 +1,7 @@
 use core::time::Duration;
 
-use deer_desert::{assert_tokens, Token};
+use approx::assert_relative_eq;
+use deer_desert::{assert_tokens_with_assertion, Token};
 use proptest::prelude::*;
 
 proptest! {
@@ -8,6 +9,8 @@ proptest! {
     fn duration_ok(value in any::<Duration>()) {
         let input = value.as_secs_f64();
 
-        assert_tokens(&value, &[Token::Number(input.into())]);
+        assert_tokens_with_assertion(|received: Duration| {
+            assert_relative_eq!(received.as_secs_f64(), value.as_secs_f64());
+        }, &[Token::Number(input.into())]);
     }
 }
