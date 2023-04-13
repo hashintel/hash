f3815f89e6a69cc0d5bc27e60f80842556f19e1e
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-13 22:13:42 +0200

    feat: `FieldVisitor` for Bound

--- libs/deer/src/impls/core/ops.rs
+++ libs/deer/src/impls/core/ops.rs
@@ -1,6 +1,10 @@
+use core::{marker::PhantomData, ops::Bound};
+
+use error_stack::{Result, ResultExt};
+
 use crate::{
     error::{DeserializeError, VisitorError},
-    Deserialize, Deserializer, Document, Visitor,
+    Deserialize, Deserializer, Document, FieldVisitor, Visitor,
 };
 
 enum BoundDiscriminant {
@@ -26,7 +30,37 @@ impl<'de> Visitor<'de> for BoundDiscriminantVisitor {
 impl<'de> Deserialize<'de> for BoundDiscriminant {
     type Reflection = ();
 
-    fn deserialize<D: Deserializer<'de>>(de: D) -> error_stack::Result<Self, DeserializeError> {
+    fn deserialize<D: Deserializer<'de>>(de: D) -> Result<Self, DeserializeError> {
         todo!()
+    }
+}
+
+struct BoundFieldVisitor<T>(PhantomData<fn() -> *const T>);
+
+impl<'de, T> FieldVisitor<'de> for BoundFieldVisitor<T>
+where
+    T: Deserialize<'de>,
+{
+    type Key = BoundDiscriminant;
+    type Value = Bound<T>;
+
+    fn visit_value<D>(self, key: Self::Key, deserializer: D) -> Result<Self::Value, VisitorError>
+    where
+        D: Deserializer<'de>,
+    {
+        match key {
+            BoundDiscriminant::Included => T::deserialize(deserializer)
+                .map(Bound::Included)
+                .change_context(VisitorError),
+            BoundDiscriminant::Excluded => T::deserialize(deserializer)
+                .map(Bound::Included)
+                .change_context(VisitorError),
+            // unit variant, we need to make sure it is not there or null (or unit)
+            // deserializing Option<()> is perfect, we don't care about the end result
+            // just that it passed.
+            BoundDiscriminant::Unbounded => Option::<()>::deserialize(deserializer)
+                .map(|_| Bound::Unbounded)
+                .change_context(VisitorError),
+        }
     }
 }
