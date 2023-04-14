f8fa2eedac8d79bfbf0dc2a428a062bbddd4cf21
Ciaran Morinan <c@hash.ai>
2023-04-14 08:20:08 +0100

    better callout styling

--- blocks/callout/src/app.tsx
+++ blocks/callout/src/app.tsx
@@ -70,6 +70,8 @@ export const App: BlockComponent<RootEntity> = ({
         padding: "0.5em",
         background: "#f9fafc",
         border: "1px solid #dee7f3",
+        display: "flex",
+        alignItems: "center",
       }}
       ref={blockRef}
     >
@@ -81,7 +83,7 @@ export const App: BlockComponent<RootEntity> = ({
       <div
         style={{
           minHeight: "1.5em",
-          paddingLeft: "1.5em",
+          paddingLeft: "1em",
         }}
         ref={editableRef}
       />
