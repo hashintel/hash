000303db3d6ac953a7514b7d1dbd5d44b4aac98f
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-17 10:04:56 +0200

    feat: outline deserialization logic

--- libs/deer/src/content.rs
+++ libs/deer/src/content.rs
@@ -1,10 +1,14 @@
 use alloc::{borrow::ToOwned, string::String, vec::Vec};
 
-use error_stack::{Result, ResultExt};
+use error_stack::{Report, Result, ResultExt};
 
 use crate::{
-    error::{ArrayAccessError, DeserializeError, ObjectAccessError, VisitorError},
-    ArrayAccess, Deserialize, Deserializer, Document, Number, ObjectAccess, Visitor,
+    error::{
+        ArrayAccessError, DeserializeError, DeserializerError, Error, ExpectedType, MissingError,
+        ObjectAccessError, ReceivedValue, ValueError, Variant, VisitorError,
+    },
+    ArrayAccess, Context, Deserialize, Deserializer, Document, EnumVisitor, Number, ObjectAccess,
+    OptionalVisitor, Reflection, Visitor,
 };
 
 mod size_hint {
@@ -192,5 +196,249 @@ impl<'de> Deserialize<'de> for Content<'de> {
     }
 }
 
+// TODO: OptionalVisitor
+// TODO: EnumVisitor
+// TODO: FieldVisitor (?)
+// TODO: Reflection c:
+
 // TODO: to shortcircuit `Content` (if deserialized multiple times) serde actually uses a
 //  `__deserialize_content`
+
+fn invalid_type<T: Reflection + ?Sized>(received: Content) -> Report<Error> {
+    let received = match received {
+        Content::Bool(value) => Some(ReceivedValue::new(value)),
+        Content::Number(value) => Some(ReceivedValue::new(value)),
+        Content::I128(value) => Some(ReceivedValue::new(value)),
+        Content::U128(value) => Some(ReceivedValue::new(value)),
+        Content::Char(value) => Some(ReceivedValue::new(value)),
+        Content::String(value) => Some(ReceivedValue::new(value)),
+        Content::Str(value) => Some(ReceivedValue::new(value.to_owned())),
+        Content::ByteBuf(value) => Some(ReceivedValue::new(value)),
+        Content::Bytes(value) => Some(ReceivedValue::new(value.to_vec())),
+        Content::Null => Some(ReceivedValue::new(())),
+        Content::None => None,
+        // TODO: we need to "remove" `None` from the serialization
+        Content::Array(value) => todo!(),
+        Content::Object(value) => todo!(),
+    };
+
+    let expected = ExpectedType::new(T::document());
+
+    match received {
+        None => Report::new(MissingError.into_error()).attach(expected),
+        Some(received) => Report::new(ValueError.into_error())
+            .attach(expected)
+            .attach(received),
+    }
+}
+
+struct ContentDeserializer<'de> {
+    content: Content<'de>,
+    context: Context,
+}
+
+impl<'de> ContentDeserializer<'de> {
+    pub fn new(content: Content<'de>, context: Context) -> Self {
+        Self { content, context }
+    }
+}
+
+impl<'de> Deserializer<'de> for ContentDeserializer<'de> {
+    fn context(&self) -> &Context {
+        &self.context
+    }
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::Bool(value) => visitor.visit_bool(value),
+            Content::Number(value) => visitor.visit_number(value),
+            Content::I128(value) => visitor.visit_i128(value),
+            Content::U128(value) => visitor.visit_u128(value),
+            Content::Char(value) => visitor.visit_char(value),
+            Content::String(value) => visitor.visit_string(value),
+            Content::Str(value) => visitor.visit_borrowed_str(value),
+            Content::ByteBuf(value) => visitor.visit_bytes_buffer(value),
+            Content::Bytes(value) => visitor.visit_borrowed_bytes(value),
+            Content::Null => visitor.visit_null(),
+            Content::None => visitor.visit_none(),
+            Content::Array(_) => todo!(),  // SeqIterator
+            Content::Object(_) => todo!(), // SeqIterator
+        }
+        .change_context(DeserializerError)
+    }
+
+    fn deserialize_null<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::Null => visitor.visit_null().change_context(DeserializerError),
+            received => Err(invalid_type::<<() as Deserialize>::Reflection>(received)
+                .change_context(DeserializerError)),
+        }
+    }
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::Bool(value) => visitor.visit_bool(value).change_context(DeserializerError),
+            received => Err(invalid_type::<bool>(received).change_context(DeserializerError)),
+        }
+    }
+
+    fn deserialize_number<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::Number(value) => visitor
+                .visit_number(value)
+                .change_context(DeserializerError),
+            received => Err(invalid_type::<Number>(received).change_context(DeserializerError)),
+        }
+    }
+
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::Char(value) => visitor.visit_char(value).change_context(DeserializerError),
+            Content::String(value) => visitor
+                .visit_string(value)
+                .change_context(DeserializerError),
+            Content::Str(value) => visitor
+                .visit_borrowed_str(value)
+                .change_context(DeserializerError),
+            received => Err(invalid_type::<char>(received).change_context(DeserializerError)),
+        }
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_str(visitor)
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::String(value) => visitor
+                .visit_str(value.as_str())
+                .change_context(DeserializerError),
+            Content::Str(value) => visitor
+                .visit_borrowed_str(value)
+                .change_context(DeserializerError),
+            Content::ByteBuf(value) => visitor
+                .visit_bytes_buffer(value)
+                .change_context(DeserializerError),
+            Content::Bytes(value) => visitor
+                .visit_borrowed_bytes(value)
+                .change_context(DeserializerError),
+            received => Err(invalid_type::<bool>(received).change_context(DeserializerError)),
+        }
+    }
+
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        match self.content {
+            Content::String(value) => visitor
+                .visit_str(value.as_str())
+                .change_context(DeserializerError),
+            Content::Str(value) => visitor
+                .visit_borrowed_str(value)
+                .change_context(DeserializerError),
+            Content::ByteBuf(value) => visitor
+                .visit_bytes_buffer(value)
+                .change_context(DeserializerError),
+            Content::Bytes(value) => visitor
+                .visit_borrowed_bytes(value)
+                .change_context(DeserializerError),
+            Content::Array(value) => todo!(),
+            received => Err(invalid_type::<bool>(received).change_context(DeserializerError)),
+        }
+    }
+
+    fn deserialize_bytes_buffer<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_bytes(visitor)
+    }
+
+    fn deserialize_array<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        todo!()
+    }
+
+    fn deserialize_object<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: Visitor<'de>,
+    {
+        todo!()
+    }
+
+    fn deserialize_optional<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: OptionalVisitor<'de>,
+    {
+        match self.content {
+            Content::Null => visitor.visit_null(),
+            Content::None => visitor.visit_none(),
+            other => visitor.visit_some(ContentDeserializer::new(other, self.context)),
+        }
+        .change_context(DeserializerError)
+    }
+
+    fn deserialize_enum<V>(self, visitor: V) -> Result<V::Value, DeserializerError>
+    where
+        V: EnumVisitor<'de>,
+    {
+        let errors: Result<(), DeserializerError> = Ok(());
+
+        let (discriminant, value) = match self.content {
+            Content::Object(value) => {
+                // TODO: ensure map with a single key
+                let length = value.len();
+
+                match length {
+                    0 => (Content::None, Content::None),
+                    1 => value[0],
+                    // TODO: add to errors
+                    n => value[0],
+                }
+            }
+            other => (other, Content::None),
+        };
+
+        let context = self.context;
+
+        let discriminant =
+            visitor.visit_discriminant(ContentDeserializer::new(discriminant, context))?;
+
+        // todo: add to errors, but abort if any errors in discriminant
+
+        let value = visitor.visit_value(discriminant, ContentDeserializer::new(value, context))?;
+
+        Ok(value)
+    }
+}
+
+struct ContentRefDeserializer<'a, 'de: 'a> {
+    content: &'a Content<'de>,
+    context: Context,
+}
+
+impl<'de> Deserializer<'de> for ContentRefDeserializer<'_, 'de> {}
