9a45a14898736bafbd8809e65b1d3f02d4d51e14
Tim Diekmann <post@timdiekmann.de>
2023-04-18 12:10:58 +0200

    Move logic into function

--- apps/hash-agents/agents/__main__.py
+++ apps/hash-agents/agents/__main__.py
@@ -4,24 +4,30 @@ import sys
 import importlib
 from . import setup
 
-if __name__ == '__main__':
+
+def call_agent(agent: str, **kwargs) -> dict:
+    module = f"agents.{agent}"
+
+    io_types = importlib.import_module(f"{module}.io_types")
+
+    out = runpy.run_module(module, run_name='HASH', init_globals={
+        'IN': io_types.input_from_dict(dict(kwargs)),
+    }).get('OUT')
+
+    return io_types.output_to_dict(out)
+
+
+def main():
+    setup()
+
     args = sys.argv
     if len(args) < 2:
         raise Exception(f"Usage: {args[0]} <AGENT_NAME> [INPUT]")
 
-    setup()
+    agent_input = json.loads(args[2]) if len(args) > 2 else {}
+    agent_output = call_agent(args[1], **agent_input)
+    print(json.dumps(agent_output))
 
-    module_path = f"agents.{args[1]}"
 
-    io_types = importlib.import_module(f"{module_path}.io_types")
-
-    agent_input = None
-    if len(args) > 2:
-        agent_input = io_types.input_from_dict(json.loads(args[2]))
-
-    out = runpy.run_module(module_path, run_name='HASH', init_globals={
-        'IN': agent_input,
-    }).get('OUT')
-
-    agent_output = json.dumps(io_types.output_to_dict(out))
-    print(agent_output)
+if __name__ == '__main__':
+    main()
