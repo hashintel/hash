89965f425ddcd1c0d0ca2d211c361748b6e27906
Ahmad Sattar <thehabbos007@gmail.com>
2023-04-18 14:41:30 +0200

    Revamp endpoint

--- apps/hash-api/src/agents/executor.ts
+++ apps/hash-api/src/agents/executor.ts
@@ -1,20 +1,26 @@
 import { AgentType } from "@apps/hash-agents";
+import { DataSource } from "apollo-datasource";
 import fetch from "node-fetch";
 
 import { withEnabledAgentRunner } from "./config";
 
 export { agents } from "@apps/hash-agents";
 
-export const executeAgent = async <T extends AgentType>(
-  agent: T["Agent"],
-  input: T["Input"],
-  { url } = withEnabledAgentRunner(),
-): Promise<T["Output"]> => {
-  const endpoint = `${url}/agents/${agent}`;
-  return (
-    await fetch(endpoint, {
-      method: "POST",
-      body: JSON.stringify({ agent, input }),
-    })
-  ).json();
+export type AgentExecutor = ReturnType<typeof setupAgentExecutor> & DataSource;
+
+export const setupAgentExecutor = ({ url } = withEnabledAgentRunner()) => {
+  return {
+    executeAgent: async <T extends AgentType>(
+      agent: T["Agent"],
+      input: T["Input"],
+    ): Promise<T["Output"]> => {
+      const endpoint = `${url}/agents/${agent}`;
+      return (
+        await fetch(endpoint, {
+          method: "POST",
+          body: JSON.stringify({ agent, input }),
+        })
+      ).json();
+    },
+  };
 };
