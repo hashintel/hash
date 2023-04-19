78b406ec24268b034751352689f600a3f8a38513
Ahmad Sattar <thehabbos007@gmail.com>
2023-04-19 18:11:33 +0200

    Adjust existing usage

--- apps/hash-api/src/cache/redis.ts
+++ apps/hash-api/src/cache/redis.ts
@@ -1,11 +1,15 @@
 import { Logger } from "@local/hash-backend-utils/logger";
-import { AsyncRedisClient, RedisConfig } from "@local/hash-backend-utils/redis";
+import {
+  RedisClient,
+  RedisConfig,
+  setupRedisClient,
+} from "@local/hash-backend-utils/redis";
 import { DataSource } from "apollo-datasource";
 
 import { CacheAdapter } from "./adapter";
 
 export class RedisCache extends DataSource implements CacheAdapter {
-  private client: AsyncRedisClient;
+  private client: RedisClient;
   get: (key: string) => Promise<string | null>;
   set: (key: string, value: string) => Promise<void>;
   setExpiring: (
@@ -16,16 +20,17 @@ export class RedisCache extends DataSource implements CacheAdapter {
 
   rpush: (key: string, ...values: string[]) => Promise<number>;
 
-  constructor(logger: Logger, cfg: RedisConfig) {
+  constructor(_logger: Logger, cfg: RedisConfig) {
     super();
-    this.client = new AsyncRedisClient(logger, cfg);
-    this.get = this.client.get;
-    this.set = this.client.set;
-    this.setExpiring = this.client.setex;
-    this.rpush = this.client.rpush;
+    this.client = setupRedisClient(cfg);
+    this.get = (key) => this.client.get(key);
+    this.set = (key, value) => this.client.set(key, value).then();
+    this.setExpiring = (key, value, expiry) =>
+      this.client.setex(key, expiry, value).then();
+    this.rpush = (key, ...values) => this.client.rpush(key, ...values);
   }
 
   async close() {
-    await this.client.close();
+    await this.client.quit();
   }
 }
