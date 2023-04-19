902abe965e1ddb5a315ff25ee33185135f9b114f
Ciaran Morinan <c@hash.ai>
2023-04-19 09:33:49 +0100

    wip: custom shape

--- apps/hash-frontend/src/pages/[shortname]/[page-slug]/canvas-page.tsx
+++ apps/hash-frontend/src/pages/[shortname]/[page-slug]/canvas-page.tsx
@@ -3,7 +3,18 @@ import "@tldraw/tldraw/ui.css";
 
 import { BlockEntity } from "@local/hash-isomorphic-utils/entity";
 import { AccountId, EntityId } from "@local/hash-subgraph";
-import { Tldraw } from "@tldraw/tldraw";
+import { getEmbedInfo } from "@tldraw/editor/src/lib/utils";
+import { toDomPrecision } from "@tldraw/primitives";
+import {
+  App,
+  defineShape,
+  TLBaseShape,
+  TLBoxUtil,
+  Tldraw,
+} from "@tldraw/tldraw";
+import { TLEmbedShape } from "@tldraw/tlschema";
+import { useMemo } from "react";
+import * as React from "react";
 
 import { BlocksMap } from "../../../blocks/page/create-editor-view";
 import { PageThread } from "../../../components/hooks/use-page-comments";
@@ -15,7 +26,54 @@ type CanvasPageBlockProps = {
   accountId: AccountId;
   entityId: EntityId;
 };
+
+type BlockShape = TLBaseShape<
+  "bpBlock",
+  {
+    blockType: string;
+    w: number;
+    h: number;
+    opacity: string;
+    type: "bpBlock";
+  }
+>;
+
+class BlockComponent extends TLBoxUtil<BlockShape> {
+  defaultProps() {
+    return {
+      opacity: "1",
+      w: 100,
+      h: 100,
+      blockType: "unknown",
+    };
+  }
+
+  indicator(shape: BlockShape) {
+    return (
+      <rect
+        color="red"
+        width={toDomPrecision(shape.props.w)}
+        height={toDomPrecision(shape.props.h)}
+        rx={8}
+        ry={8}
+      />
+    );
+  }
+
+  render() {
+    return <div>hello</div>;
+  }
+}
+defineShape({
+  type: "bpBlock",
+  getShapeUtil: () => BlockComponent<BlockShape, BlockShape>,
+});
+
 export const CanvasPageBlock = ({ blocks, contents }: CanvasPageBlockProps) => {
+  const handleMount = (app: App) => {
+    app.createShapes([BlockComponent]);
+  };
+
   return (
     <div style={{ height: "100%" }}>
       <Tldraw />
