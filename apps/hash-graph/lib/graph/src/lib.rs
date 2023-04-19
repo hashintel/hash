2d6f656cd34cc7cbd85f9bbad8c4c0b99f749811
Tim Diekmann <post@timdiekmann.de>
2023-04-19 18:38:46 +0200

    Fix feature flag

--- apps/hash-graph/lib/graph/src/lib.rs
+++ apps/hash-graph/lib/graph/src/lib.rs
@@ -5,7 +5,7 @@
 // Not required, reason: Simpler than using blanket implementations
 #![feature(trait_alias)]
 // Not required, reason: much more simple bounds
-#![feature(associated_type_bounds)]
+#![feature(associated_type_bounds, impl_trait_in_assoc_type)]
 #![feature(try_find)]
 #![feature(type_alias_impl_trait)]
 #![feature(hash_raw_entry)]
