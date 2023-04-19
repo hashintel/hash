1c08ab896d5a24737974f4705d2c5a28632c8608
Tim Diekmann <post@timdiekmann.de>
2023-04-19 12:43:36 +0200

    Error on dots in agent names

--- apps/hash-agents/app/agents/__init__.py
+++ apps/hash-agents/app/agents/__init__.py
@@ -3,6 +3,9 @@ import importlib
 
 
 def call_agent(agent: str, **kwargs) -> dict:
+    if "." in agent:
+        raise Exception("Invalid agent name, an agent name must not contain a dot.")
+
     module = f"{__name__}.{agent}"
 
     io_types = importlib.import_module(f"{module}.io_types")
