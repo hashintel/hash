e4feb1c1359b5c3029f3a18baae736037ded9a67
Luís Bettencourt <lmn.bettencourt@gmail.com>
2023-04-20 17:09:35 +0100

    finetune agent

--- apps/hash-agents/app/agents/react-app/__main__.py
+++ apps/hash-agents/app/agents/react-app/__main__.py
@@ -1,7 +1,8 @@
-from langchain import  PromptTemplate, OpenAI, LLMChain
-from langchain.chat_models import ChatOpenAI
 from .io_types import *
-
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.schema import HumanMessage, SystemMessage
 
 def main(input: Input) -> Output:
     """
@@ -9,13 +10,14 @@ def main(input: Input) -> Output:
     :param agent_input: Input defined in `io_types.ts`
     :return: Output defined in `io_types.ts`
     """
-
-    template = """Generate the following react component using MUI components: {user_prompt}."""
-    prompt = PromptTemplate(template=template, input_variables=["user_prompt"])
-    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(model_name="gpt-3.5-turbo", temperature=0), verbose=True)
-    result = llm_chain.predict(user_prompt=input.user_prompt)
-    return Output(result=result)
-
+    chat = ChatOpenAI(model="gpt-3.5-turbo", streaming=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), verbose=True, temperature=0)
+    resp = chat([SystemMessage(content="You are ChatGPT, a large language model trained by OpenAI."), 
+                SystemMessage(content="Answer as concisely as possible."),
+                SystemMessage(content="Return only code blocks as ```{code:jsx}{code...}```."),
+                SystemMessage(content="Generate a react component using MUI components."), 
+                SystemMessage(content="Do NOT include an explanation or any text that is not part of the code block you are generating."),
+                HumanMessage(content=input.user_prompt)])
+    return Output(result=resp.content)
 
 if __name__ == "HASH":
     """This is used when running the agent from the server or the agent orchestrator"""
@@ -33,5 +35,4 @@ if __name__ == "__main__":
     print("Describe your application:")
     IN = input("")
     output = main((Input(user_prompt=IN)))
-    print(output.result)
     getLogger().info(f"output: {output.result}")
