6d8f44df84166974b35cc05bd5436168c9f678cd
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-21 17:41:51 +0200

    feat: dict-config

--- apps/hash-agents/app/__init__.py
+++ apps/hash-agents/app/__init__.py
@@ -1,10 +1,11 @@
 import logging
 
+from asgi_correlation_id import CorrelationIdMiddleware
 from beartype import beartype
 from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 
-from app.logger import setup_logging
+from app.logger import logging_middleware, setup_logging
 from app.routes import router
 
 logger = logging.getLogger(__name__)
@@ -26,5 +27,7 @@ def create_app(base_logger_name: str | None = None) -> FastAPI:
 
     app = FastAPI()
     app.include_router(router)
+    app.middleware(logging_middleware)
+    app.add_middleware(CorrelationIdMiddleware)
 
     return app
--- apps/hash-agents/app/logger.py
+++ apps/hash-agents/app/logger.py
@@ -1,12 +1,76 @@
 import logging
+import logging.config
 import os
-import sys
+import time
 from datetime import datetime
-from typing import Literal, assert_never
+from typing import Callable, Literal, assert_never
 
 import structlog
+from asgi_correlation_id import correlation_id
+from starlette.requests import Request
+from starlette.responses import Response
+from uvicorn.protocols.utils import get_path_with_query_string
 
 Environment = Literal['dev', 'prod']
+
+access_logger = structlog.stdlib.get_logger("api.access")
+
+
+async def logging_middleware(request: Request, call_next: Callable) -> Response:
+    """
+    Adapted from https://gist.github.com/nymous/f138c7f06062b7c43c060bf03759c29e
+    """
+
+    structlog.contextvars.clear_contextvars()
+    # These context vars will be added to all log entries emitted during the request
+    request_id = correlation_id.get()
+    structlog.contextvars.bind_contextvars(request_id=request_id)
+
+    start_time = time.perf_counter_ns()
+    # If the call_next raises an error, we still want to return our own 500 response,
+    # so we can add headers to it (process time, request ID...)
+    response = Response(status_code=500)
+    try:
+        response = await call_next(request)
+    except Exception:
+        structlog.stdlib.get_logger("api.error").exception("Uncaught exception")
+        raise
+    finally:
+        process_time = time.perf_counter_ns() - start_time
+
+        status_code = response.status_code
+        url = get_path_with_query_string(request.scope)
+        client_host = request.client.host
+        client_port = request.client.port
+        http_method = request.method
+        http_version = request.scope["http_version"]
+
+        # Recreate the Uvicorn access log format, but add all parameters as structured information
+        access_logger.info(
+            f"""{client_host}:{client_port} - "{http_method} {url} HTTP/{http_version}" {status_code}""",
+            http={
+                "url": str(request.url),
+                "status_code": status_code,
+                "method": http_method,
+                "request_id": request_id,
+                "version": http_version,
+            },
+            network={"client": {"ip": client_host, "port": client_port}},
+            duration=process_time,
+        )
+
+        return response
+
+
+def extract_from_record(_, __, event_dict: dict):
+    """
+    Extract thread and process names and add them to the event dict.
+    """
+    record = event_dict["_record"]
+    event_dict["thread_name"] = record.threadName
+    event_dict["process_name"] = record.processName
+
+    return event_dict
 
 
 def setup_logging(environment: Environment = 'dev') -> None:
@@ -83,58 +147,78 @@ def setup_logging(environment: Environment = 'dev') -> None:
     log_level = os.getenv("HASH_AGENT_RUNNER_LOG_LEVEL")
     log_level = log_level or logging.WARNING
 
-    handlers = [logging.StreamHandler()]
+    log_folder = os.environ.get("HASH_AGENT_RUNNER_LOG_FOLDER", "./logs")
+    if not os.path.exists(log_folder):
+        os.mkdir(log_folder)
 
-    if environment == 'prod':
-        log_folder = os.environ.get("HASH_AGENT_RUNNER_LOG_FOLDER", "./logs")
-        if not os.path.exists(log_folder):
-            os.mkdir(log_folder)
-
-        handlers.append(
-            logging.FileHandler(
-                f"{log_folder}/run-{datetime.now().isoformat()}.log", mode="w"
-            )
-        )
-
-    for handler in handlers:
-        handler.setFormatter(formatter)
-
-    handler = logging.StreamHandler()
-    # Use OUR `ProcessorFormatter` to format all `logging` entries.
-    handler.setFormatter(formatter)
-    root_logger = logging.getLogger()
-
-    for handler in handlers:
-        root_logger.addHandler(handler)
-
-    root_logger.setLevel(log_level)
-
-    for _log in ["uvicorn", "uvicorn.error"]:
-        # Clear the log handlers for uvicorn loggers, and enable propagation
-        # so the messages are caught by our root logger and formatted correctly
-        # by structlog
-        logging.getLogger(_log).handlers.clear()
-        logging.getLogger(_log).propagate = True
-
-    # Since we re-create the access logs ourselves, to add all information
-    # in the structured log (see the `logging_middleware` in main.py), we clear
-    # the handlers and prevent the logs to propagate to a logger higher up in the
-    # hierarchy (effectively rendering them silent).
-    logging.getLogger("uvicorn.access").handlers.clear()
-    logging.getLogger("uvicorn.access").propagate = False
-
-    def handle_exception(exc_type, exc_value, exc_traceback):
-        """
-        Log any uncaught exception instead of letting it be printed by Python
-        (but leave KeyboardInterrupt untouched to allow users to Ctrl+C to stop)
-        See https://stackoverflow.com/a/16993115/3641865
-        """
-        if issubclass(exc_type, KeyboardInterrupt):
-            sys.__excepthook__(exc_type, exc_value, exc_traceback)
-            return
-
-        root_logger.error(
-            "Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback)
-        )
-
-    sys.excepthook = handle_exception
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": True,
+            "formatters": {
+                "file": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processors": [
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        structlog.processors.JSONRenderer(),
+                    ],
+                    "foreign_pre_chain": shared,
+                },
+                "stdout": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processors": [
+                        extract_from_record,
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        processor,
+                    ],
+                    "foreign_pre_chain": shared,
+                },
+                "uvicorn_default": {
+                    "()": "uvicorn.logging.DefaultFormatter",
+                    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                },
+                "uvicorn_access": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "level": "DEBUG",
+                    "class": "logging.StreamHandler",
+                    "formatter": "stdout",
+                },
+                "file": {
+                    "level": "DEBUG",
+                    "class": "logging.handlers.WatchedFileHandler",
+                    "filename": f"{log_folder}/run-{datetime.now().isoformat()}.log",
+                    "formatter": "file",
+                },
+                "uvicorn_default": {
+                    "formatter": "uvicorn_default",
+                    "class": "logging.NullHandler",
+                },
+                "uvicorn_access": {
+                    "formatter": "uvicorn_access",
+                    "class": "logging.NullHandler",
+                },
+            },
+            "loggers": {
+                "": {
+                    "handlers": ["default", "file"],
+                    "level": "DEBUG",
+                    "propagate": True,
+                },
+                "uvicorn.error": {
+                    "level": "INFO",
+                    "handlers": ["uvicorn_default"],
+                    "propagate": False,
+                },
+                "uvicorn.access": {
+                    "level": "INFO",
+                    "handlers": ["uvicorn_access"],
+                    "propagate": False,
+                },
+            },
+        }
+    )
