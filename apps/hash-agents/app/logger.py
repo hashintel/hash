c8001e1ed68e7c9f2eec41e41039623fde265b9d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-21 16:49:52 +0200

    chore: remove unused `get_logger`

--- apps/hash-agents/app/logger.py
+++ apps/hash-agents/app/logger.py
@@ -3,11 +3,6 @@ import os
 from datetime import datetime
 
 
-def get_logger() -> logging.Logger:
-    logger = logging.getLogger("base_logger")
-    return logger
-
-
 def setup_logging(base_logger: logging.Logger | None = None) -> None:
     log_folder = os.environ.get("HASH_AGENT_RUNNER_LOG_FOLDER", "./logs")
     if not os.path.exists(log_folder):
