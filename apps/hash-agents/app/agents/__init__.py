df514595af4774f277907fe1d67bb5490b6b56bf
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-21 19:05:17 +0200

    feat: simplify c:

--- apps/hash-agents/app/agents/__init__.py
+++ apps/hash-agents/app/agents/__init__.py
@@ -59,12 +59,7 @@ def call_agent(agent: str, **kwargs: dict) -> dict:
     logger.debug(
         "Agent output",
         agent=agent,
-        output=(
-            # ensure that `to_dict` is present and is a callable
-            out.to_dict()
-            if hasattr(out, 'to_dict') and callable(out.to_dict)
-            else out
-        ),
+        output=io_types.output_to_dict(out),
     )
 
     try:
