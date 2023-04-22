92c9f1f542e3e64a04ad96346f1d3cdb8b39b095
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-22 20:48:24 +0200

    test: new behaviour

--- libs/deer/tests/test_impls_core_string.rs
+++ libs/deer/tests/test_impls_core_string.rs
@@ -15,3 +15,10 @@ fn string_ok() {
     let value_str = "example";
     assert_tokens(&value_str, &[Token::BorrowedStr("example")]);
 }
+
+#[test]
+fn single_char_str_ok() {
+    assert_tokens(&'A', &[Token::BorrowedStr("A")]);
+    assert_tokens(&'A', &[Token::Str("A")]);
+    assert_tokens(&'A', &[Token::String("A")]);
+}
