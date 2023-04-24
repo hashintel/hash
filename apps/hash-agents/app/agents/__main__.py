50178768a6bae34d5339b7816c7b1323d9d1acaa
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-04-24 15:07:42 +0200

    chore: docs

--- apps/hash-agents/app/agents/__main__.py
+++ apps/hash-agents/app/agents/__main__.py
@@ -20,21 +20,58 @@ def help():
 
 @beartype
 def register_command(name: str, agent: Agent):
+    """
+    Register a new command
+
+    Typer doesn't support creating commands directly from models,
+    therefore, we need to employ a bit of trickery.
+
+    This function takes a function that takes any arguments (`invoke`)
+    and transforms it in a way that typer recognizes each of the `Input` arguments.
+    """
+
+    # each pydantic models has an attribute `__fields__` which is a dict with the name
+    # and type of the field (resolved annotation).
     fields = agent.Input.__fields__
     args = ((name, field.type_) for name, field in fields.items())
 
     def invoke(**kwargs: dict):
         try:
 
-            async def wrap():
+            async def run():
+                # we know that kwargs will be a valid Input, due to Typer
                 return await call_agent(name, **kwargs)
 
-            agent_output = anyio.run(wrap)
+            agent_output = anyio.run(run)
         except Exception as e:
             agent_output = {"error": str(e)}
 
         pprint(agent_output)
 
+    # `inspect` caches the signature of a function using the `__signature__` key, which
+    # typer uses to figure out which arguments are allowed.
+    #
+    # Example:
+    # ```
+    # class Input(BaseModel):
+    #   expression: str
+    #
+    # def invoke(**kwargs):
+    #    ...
+    # ```
+    #
+    # here in this example kwargs can be any value, which removes one of the major
+    # benefits of typer, what we do instead is replace the **signature**
+    # (not the actual signature definition) to:
+    #
+    # ```
+    # def invoke(*, expression: str):
+    #   ...
+    # ```
+    #
+    # Nothing in the behaviour has changed, but now anything that inspects the signature
+    # (like typer) will see a typed representation of the kwargs attribute that we're
+    # expecting.
     signature = inspect.signature(invoke)
     invoke.__signature__ = signature.replace(
         parameters=[
