70fa510cd075b7803f6a2aa8a4e541d58b8e1746
Nate Higgins <nate@nateh.uk>
2023-04-24 09:57:04 +0100

    Add options

--- apps/hash-frontend/src/shared/command-bar.tsx
+++ apps/hash-frontend/src/shared/command-bar.tsx
@@ -1,7 +1,31 @@
 import { TextField } from "@hashintel/design-system";
-import { Autocomplete, Box, Modal } from "@mui/material";
+import {
+  Autocomplete,
+  autocompleteClasses,
+  Box,
+  Modal,
+  Paper,
+} from "@mui/material";
 import { bindPopover, usePopupState } from "material-ui-popup-state/hooks";
+import { HTMLAttributes } from "react";
 import { useKeys } from "rooks";
+
+const CustomPaperComponent = ({
+  children,
+  ...props
+}: HTMLAttributes<HTMLElement>) => (
+  <Paper
+    {...props}
+    sx={{
+      [`.${autocompleteClasses.listbox}`]: {
+        height: 461,
+        maxHeight: "none",
+      },
+    }}
+  >
+    {children}
+  </Paper>
+);
 
 export const CommandBar = () => {
   const popupState = usePopupState({
@@ -16,21 +40,79 @@ export const CommandBar = () => {
   return (
     <Modal {...bindPopover(popupState)}>
       <Box
-        maxWidth={560}
         width="100vw"
         height="100vh"
         display="flex"
         alignItems="center"
         margin="0 auto"
       >
-        <Autocomplete
-          options={[]}
-          openOnFocus
-          sx={{ width: "100%" }}
-          renderInput={(props) => (
-            <TextField onBlur={() => popupState.close()} autoFocus {...props} />
-          )}
-        />
+        <Box
+          height={518}
+          maxWidth={560}
+          width="100vw"
+          display="flex"
+          justifyContent="center"
+          margin="0 auto"
+        >
+          <Autocomplete
+            options={[
+              {
+                group: "Blocks",
+                label: "Find a block…",
+              },
+              {
+                group: "Blocks",
+                label: "Generate new block with AI…",
+              },
+              {
+                group: "Entities",
+                label: "Search for an entity…",
+              },
+              {
+                group: "Entities",
+                label: "Insert a link to an entity…",
+              },
+              {
+                group: "Entities",
+                label: "Create new entity…",
+              },
+              {
+                group: "Types",
+                label: "Create new type…",
+              },
+              {
+                group: "Apps",
+                label: "Find an app…",
+              },
+              {
+                group: "Apps",
+                label: "Create an app…",
+              },
+              {
+                group: "Apps",
+                label: "Generate new app…",
+              },
+            ]}
+            open
+            onClose={() => popupState.close()}
+            sx={{ width: "100%" }}
+            renderInput={(props) => (
+              <TextField
+                onBlur={() => popupState.close()}
+                autoFocus
+                {...props}
+              />
+            )}
+            onChange={(_, __, reason) => {
+              if (reason === "selectOption") {
+                popupState.close();
+              }
+            }}
+            groupBy={(option) => option.group}
+            getOptionLabel={(option) => option.label}
+            PaperComponent={CustomPaperComponent}
+          />
+        </Box>
       </Box>
     </Modal>
   );
