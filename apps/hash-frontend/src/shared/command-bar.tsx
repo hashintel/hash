bb0536540574754886ac88b396265414dc6bb5d5
Nate Higgins <nate@nateh.uk>
2023-04-24 12:28:18 +0100

    First attempt at rendering selected option

--- apps/hash-frontend/src/shared/command-bar.tsx
+++ apps/hash-frontend/src/shared/command-bar.tsx
@@ -8,7 +8,7 @@ import {
 } from "@mui/material";
 import { bindPopover, usePopupState } from "material-ui-popup-state/hooks";
 import { useRouter } from "next/router";
-import { HTMLAttributes, useRef, useState } from "react";
+import { HTMLAttributes, ReactNode, useRef, useState } from "react";
 import { useKeys } from "rooks";
 
 const CustomPaperComponent = ({
@@ -31,90 +31,104 @@ type Option = {
   group: string;
   label: string;
   href?: string;
+  selected?: (
+    props: HTMLAttributes<HTMLLIElement>,
+    option: Option,
+  ) => ReactNode;
   options?: Option[];
 };
 
-const options: Option[] = [
-  {
-    group: "Blocks",
-    label: "Find a block…",
-    options: [
-      {
-        group: "General",
-        label: "Option A",
-      },
-      {
-        group: "General",
-        label: "Option B",
-      },
-      {
-        group: "Other",
-        label: "Option C",
-      },
-    ],
-  },
-  {
-    group: "Blocks",
-    label: "Generate new block with AI…",
-    href: "/",
-  },
-  {
-    group: "Entities",
-    label: "Search for an entity…",
-    href: "/",
-  },
-  {
-    group: "Entities",
-    label: "Insert a link to an entity…",
-    href: "/",
-  },
-  {
-    group: "Entities",
-    label: "Create new entity…",
-    href: "/",
-  },
-  {
-    group: "Types",
-    label: "Create new type…",
-    href: "/",
-  },
-  {
-    group: "Apps",
-    label: "Find an app…",
-    href: "/",
-  },
-  {
-    group: "Apps",
-    label: "Create an app…",
-    href: "/",
-  },
-  {
-    group: "Apps",
-    label: "Generate new app…",
-    href: "/",
-  },
-];
-
 export const CommandBar = () => {
+  const options: Option[] = [
+    {
+      group: "Blocks",
+      label: "Find a block…",
+      options: [
+        {
+          group: "General",
+          label: "Option A",
+          selected: (props, { label }) => <li {...props}>Foo</li>,
+        },
+        {
+          group: "General",
+          label: "Option B",
+        },
+        {
+          group: "Other",
+          label: "Option C",
+        },
+      ],
+    },
+    {
+      group: "Blocks",
+      label: "Generate new block with AI…",
+      href: "/",
+    },
+    {
+      group: "Entities",
+      label: "Search for an entity…",
+      href: "/",
+    },
+    {
+      group: "Entities",
+      label: "Insert a link to an entity…",
+      href: "/",
+    },
+    {
+      group: "Entities",
+      label: "Create new entity…",
+      href: "/",
+    },
+    {
+      group: "Types",
+      label: "Create new type…",
+      href: "/",
+    },
+    {
+      group: "Apps",
+      label: "Find an app…",
+      href: "/",
+    },
+    {
+      group: "Apps",
+      label: "Create an app…",
+      href: "/",
+    },
+    {
+      group: "Apps",
+      label: "Generate new app…",
+      href: "/",
+    },
+  ];
+
   const popupState = usePopupState({
     popupId: "kbar",
     variant: "popover",
   });
 
-  useKeys(["Meta", "k"], () => {
-    popupState.toggle();
-  });
-
   const router = useRouter();
 
-  const [selectedOptionPath, setSelectedOptionPath] = useState<Option[]>([]);
+  const [selectedOptionPath, setSelectedOptionPath] = useState<string[]>([]);
   const selectedOptions =
-    selectedOptionPath[selectedOptionPath.length - 1]?.options ?? options;
+    selectedOptionPath.reduce<Option[] | null>((acc, path) => {
+      return acc?.find((option) => option.label === path)?.options ?? null;
+    }, options) ?? options;
+
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   const closeBar = () => {
+    setSelectedOption(null);
     setSelectedOptionPath([]);
     popupState.close();
   };
+
+  useKeys(["Meta", "k"], () => {
+    if (popupState.isOpen) {
+      closeBar();
+    } else {
+      popupState.open();
+    }
+  });
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -156,6 +170,20 @@ export const CommandBar = () => {
                 />
               );
             }}
+            renderOption={(props, option) => {
+              if (selectedOption) {
+                if (option.label === selectedOption) {
+                  if (!option.selected) {
+                    throw new Error("Cannot render selected option");
+                  }
+
+                  return option.selected(props, option);
+                }
+                return null;
+              }
+
+              return <li {...props}>{option.label}</li>;
+            }}
             onChange={(_, __, reason, details) => {
               if (details && reason === "selectOption") {
                 const option = details.option;
@@ -168,7 +196,9 @@ export const CommandBar = () => {
                     void router.push(option.href);
                   }
                 } else if (option.options) {
-                  setSelectedOptionPath([...selectedOptionPath, option]);
+                  setSelectedOptionPath([...selectedOptionPath, option.label]);
+                } else if (option.selected) {
+                  setSelectedOption(option.label);
                 } else {
                   closeBar();
                 }
