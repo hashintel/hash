656e1a7a0cdf507ac99cf834eaeced3e6ba42436
Ahmad Sattar <thehabbos007@gmail.com>
2023-04-26 13:04:23 +0200

    `setex` explanation comment

--- apps/hash-api/src/cache/redis.ts
+++ apps/hash-api/src/cache/redis.ts
@@ -25,6 +25,9 @@ export class RedisCache extends DataSource implements CacheAdapter {
     this.client = setupRedisClient(logger, cfg);
     this.get = (key) => this.client.get(key);
     this.set = (key, value) => this.client.set(key, value).then();
+    // The underlying redis library returns `Promise<"OK">` and we're
+    // interested in returning `Promise<void>` for this method.
+    // The empty `.then` accomplishes this.
     this.setExpiring = (key, value, expiry) =>
       this.client.setex(key, expiry, value).then();
     this.rpush = (key, ...values) => this.client.rpush(key, ...values);
