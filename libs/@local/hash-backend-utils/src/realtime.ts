b653229984910a1014a5185e2d7d7a8854780185
Ahmad Sattar <thehabbos007@gmail.com>
2023-04-26 17:56:36 +0200

    Move Realtime to Redis Streams and adjust its DB schemas (#2461)
    * Move to "ioredis"
    
    * Adjust (simplify) adapter to use streams
    
    * Add stream producer and consumer
    
    * Adjust existing usage
    
    * Adjust existing queue
    
    * Adjust type errors
    
    * Remove unneeded generic
    
    * Update stream config and table defs
    
    * Fix realtime implementation and wal2json
    
    * Mark deploys as persistent
    
    * Use turbo for realtime dockerfile
    
    * ENV adjustments and small logging improvements
    
    * Add redis to dev compose, realtime to prod
    
    * Rename env vars for .env
    
    * Adjust .env to run in dev mode
    
    * Add ontology type streams
    
    * Add max stream length and max read count
    
    * Remove redundant prefix
    
    * Readonly
    
    * Add build step to dockerfile
    
    * Anticipate #2463 merge
    
    * Add `EntityEditionId` branding
    
    * Reuse branded type
    
    * Update pg-tables readme
    
    * Readd negation
    
    * QueueProducer -> StreamProducer
    
    * Add Wal2JsonMsg `extends string`
    
    * Timeout typo
    
    * Prefix types for pg tables
    
    * `setex` explanation comment
    
    * Change findColumn return type

--- libs/@local/hash-backend-utils/src/realtime.ts
+++ libs/@local/hash-backend-utils/src/realtime.ts
@@ -1,22 +1,43 @@
-import { Entity, Link } from "@local/hash-backend-utils/pg-tables";
-
 /**
  * @todo Consider adding realtime handling for types
  *   https://app.asana.com/0/0/1202922776289399/f
  */
-export const supportedRealtimeTables = ["entities", "links"] as const;
+const supportedTables = {
+  entityTables: ["entity_editions", "entity_temporal_metadata"],
+  entityTypeTables: ["entity_types"],
+  propertyTypeTables: ["property_types"],
+} as const;
 
-export type SupportedRealtimeTable = (typeof supportedRealtimeTables)[number];
+export const supportedRealtimeTables: SupportedRealtimeTable[] =
+  Object.values(supportedTables).flat();
 
-export const isSupportedRealtimeTable = (
+export type SupportedRealtimeTable =
+  (typeof supportedTables)[keyof typeof supportedTables][number];
+
+export type SupportedRealtimeEntityTable =
+  (typeof supportedTables)["entityTables"][number];
+
+export const isSupportedRealtimeEntityTable = (
   table: string,
-): table is SupportedRealtimeTable =>
-  supportedRealtimeTables.includes(table as SupportedRealtimeTable);
+): table is SupportedRealtimeEntityTable =>
+  supportedTables.entityTables.includes(table as SupportedRealtimeEntityTable);
 
-export type RealtimeMessage =
-  | {
-      table: "entities";
-      record: Entity;
-    }
-  | { table: "links"; record: Link };
-// | { table: "aggregation_versions"; record: AggregationVersion }
+export type SupportedRealtimeEntityTypeTable =
+  (typeof supportedTables)["entityTypeTables"][number];
+
+export const isSupportedRealtimeEntityTypeTable = (
+  table: string,
+): table is SupportedRealtimeEntityTable =>
+  supportedTables.entityTypeTables.includes(
+    table as SupportedRealtimeEntityTypeTable,
+  );
+
+export type SupportedRealtimePropertyTypeTable =
+  (typeof supportedTables)["propertyTypeTables"][number];
+
+export const isSupportedRealtimePropertyTypeTable = (
+  table: string,
+): table is SupportedRealtimePropertyTypeTable =>
+  supportedTables.propertyTypeTables.includes(
+    table as SupportedRealtimePropertyTypeTable,
+  );
