ba31172140fbe8ba585b07b3a7fcb080c95fada5
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-04-28 16:44:55 +0100

    Version-Control Vercel build configuration

---
+++ apps/hash-frontend/vercel-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
+source ~/.bashrc
+
+set -e
+
+echo "Changing dir to root"
+cd ../..
+
+echo "Building frontend"
+turbo build --filter='@apps/hash-frontend'
