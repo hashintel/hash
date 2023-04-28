826b01478a02dedfd571deb52e33dd6524a7a51e
Alfred Mountfield <am@hash.ai>
2023-04-28 18:50:19 +0100

    Fixes and improved style in Agent Template (#2493)
    * Driveby: docs fixes
    
    * Driveby: codegen fix
    
    * Driveby: turbo codegen outputs fix
    
    * Greatly increase lint selection
    
    * Add `__init__` to agents template
    
    * Address lints
    
    * Extract `setup` function to remove circular dependency
    
    * Allow poetry in `yarn install` in HASH agent docker image
    
    * Use is_dir method
    
    * Remove `isort` in favour of `ruff`
    
    * Adjust gunicorn config to disable timeout
    
    * Remove unnecessary run-all scripts
    
    * Allow caching of venv
    
    * Install poetry in `hash-frontend` build
    
    * Convert `hash-agents` Dockerfile to Debian and fix it
    
    * Install Python and Poetry as part of `warm-up-repo` in CI
    
    * More Python fixes
    
    * Add better separation of Poetry install steps

--- apps/hash-agents/app/__init__.py
+++ apps/hash-agents/app/__init__.py
@@ -1,26 +1,20 @@
+"""A python web-server that allows the orchestration of various AI-assisted tasks."""
 import structlog.stdlib
 from asgi_correlation_id import CorrelationIdMiddleware
 from beartype import beartype
-from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 
-from app.logger import Environment, http_logging_middleware, setup_logging
-from app.routes import router
+from .logger import Environment, http_logging_middleware
+from .prerun import setup_prerun
+from .routes import router
 
 logger = structlog.stdlib.get_logger(__name__)
 
 
 @beartype
-def setup(environment: Environment) -> None:
-    load_dotenv()
-    load_dotenv(dotenv_path=find_dotenv(filename=".env.local"), override=True)
-
-    setup_logging(environment)
-
-
-@beartype
 def create_app(environment: Environment = "dev") -> FastAPI:
-    setup(environment)
+    """Runs the app."""
+    setup_prerun(environment)
 
     app = FastAPI()
     app.include_router(router)
