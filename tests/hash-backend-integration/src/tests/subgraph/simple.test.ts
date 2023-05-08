f70dfef3157405ce230fd1731600e3b8ce2c9202
Tim Diekmann <post@timdiekmann.de>
2023-05-08 18:25:42 +0200

    Add a failure case

--- tests/hash-backend-integration/src/tests/subgraph/simple.test.ts
+++ tests/hash-backend-integration/src/tests/subgraph/simple.test.ts
@@ -4,8 +4,16 @@ import { restoreSnapshot } from "../setup";
 
 jest.setTimeout(60000);
 
-describe("Simple test", () => {
+describe("Empty snapshot", () => {
   it("can upload a snapshot", async () => {
-    await restoreSnapshot(path.join(__dirname, "simple.jsonl"));
+    await expect(
+      restoreSnapshot(path.join(__dirname, "pass", "empty.jsonl")),
+    ).resolves.not.toThrowError();
+  });
+
+  it("cannot upload a snapshot without metadata", async () => {
+    await expect(
+      restoreSnapshot(path.join(__dirname, "fail", "empty.jsonl")),
+    ).rejects.toThrow("does not contain metadata");
   });
 });
