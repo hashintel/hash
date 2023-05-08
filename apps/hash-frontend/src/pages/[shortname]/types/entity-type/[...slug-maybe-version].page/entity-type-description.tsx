b6558f827674f7447c5d807aed6fa04d7afce5f2
Luís Bettencourt <lmn.bettencourt@gmail.com>
2023-05-08 11:12:32 +0100

    remove description title

--- apps/hash-frontend/src/pages/[shortname]/types/entity-type/[...slug-maybe-version].page/entity-type-description.tsx
+++ apps/hash-frontend/src/pages/[shortname]/types/entity-type/[...slug-maybe-version].page/entity-type-description.tsx
@@ -1,6 +1,5 @@
 import { EditableField } from "@hashintel/block-design-system";
 import { EntityTypeEditorFormData } from "@hashintel/type-editor";
-import { Typography } from "@mui/material";
 import { useController, useFormContext } from "react-hook-form";
 
 interface EntityTypeDescriptionProps {
@@ -18,17 +17,11 @@ export const EntityTypeDescription = ({
   });
 
   return (
-    <>
-      <Typography variant="h5" sx={{ mb: 1, fontWeight: 500 }}>
-        Description
-      </Typography>
-
-      <EditableField
-        {...descriptionController.field}
-        inputRef={descriptionController.field.ref}
-        placeholder="Enter a description"
-        readonly={readonly}
-      />
-    </>
+    <EditableField
+      {...descriptionController.field}
+      inputRef={descriptionController.field.ref}
+      placeholder="Enter a description"
+      readonly={readonly}
+    />
   );
 };
