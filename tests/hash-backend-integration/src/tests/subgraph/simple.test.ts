c2dd1100ca0f5aff1eb758dc90617753569aab06
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-05-09 18:01:11 +0200

    Allow resetting database from BE integration tests (#2538)

--- tests/hash-backend-integration/src/tests/subgraph/simple.test.ts
+++ tests/hash-backend-integration/src/tests/subgraph/simple.test.ts
@@ -1,19 +1,25 @@
 import path from "node:path";
 
-import { restoreSnapshot } from "../setup";
+import { resetToSnapshot } from "../test-server";
 
 jest.setTimeout(60000);
 
 describe("Empty snapshot", () => {
   it("can upload a snapshot", async () => {
     await expect(
-      restoreSnapshot(path.join(__dirname, "pass", "empty.jsonl")),
+      resetToSnapshot(path.join(__dirname, "pass", "empty.jsonl")),
     ).resolves.not.toThrowError();
   });
 
   it("cannot upload a snapshot without metadata", async () => {
     await expect(
-      restoreSnapshot(path.join(__dirname, "fail", "empty.jsonl")),
+      resetToSnapshot(path.join(__dirname, "fail", "empty.jsonl")),
     ).rejects.toThrow("does not contain metadata");
+  });
+
+  it("cannot upload a snapshot with wrong version", async () => {
+    await expect(
+      resetToSnapshot(path.join(__dirname, "fail", "wrong-version.jsonl")),
+    ).rejects.toThrow("contains unsupported entries");
   });
 });
