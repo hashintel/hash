2ec8d457ac47b6746ad11cf3763e9b9cbdd74171
Tim Diekmann <post@timdiekmann.de>
2023-05-09 12:09:05 +0200

    Allow resetting database from BE integration tests

--- tests/hash-backend-integration/src/tests/setup.ts
+++ tests/hash-backend-integration/src/tests/setup.ts
@@ -1,9 +1,5 @@
-import { promises as fs } from "node:fs";
-
-import { GraphStatus } from "@apps/hash-graph/type-defs/status";
 import { monorepoRootDir } from "@local/hash-backend-utils/environment";
 import execa from "execa";
-import fetch from "node-fetch";
 
 export const recreateDbAndRunSchemaMigrations = async () => {
   await execa(
@@ -19,23 +15,4 @@ export const recreateDbAndRunSchemaMigrations = async () => {
     ["workspace", "@hashintel/hash-datastore", "pg:migrate", "up"],
     { cwd: monorepoRootDir },
   );
-};
-
-export const restoreSnapshot = (snapshotPath: string) => {
-  return fs
-    .readFile(snapshotPath)
-    .then((snapshot) =>
-      fetch("http://localhost:4001/snapshot", {
-        method: "POST",
-        body: snapshot,
-      }),
-    )
-    .then(async (response) => {
-      const status: GraphStatus = await response.json();
-      if (status.code !== "OK") {
-        throw new Error(
-          `Snapshot restoration error: ${JSON.stringify(status)}`,
-        );
-      }
-    });
 };
