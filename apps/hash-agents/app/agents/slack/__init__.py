4af71a1bd7016e01b423fb6f2b4b0a7c3fbf230e
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-05-10 17:05:37 +0100

    Handle rate limiting and download threads too

--- apps/hash-agents/app/agents/slack/__init__.py
+++ apps/hash-agents/app/agents/slack/__init__.py
@@ -1,14 +1,47 @@
 """A template agent, which provides a simple interface into LangChain's LLMMathChain."""
 
 import os
+import time
+from collections.abc import Callable
 from datetime import datetime, timedelta, timezone
 from pprint import pprint
 
+import structlog
 from beartype import beartype
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from slack_sdk.web import SlackResponse
 
+logger = structlog.stdlib.get_logger(__name__)
+
 HASH_GRAPH_CHANNEL_ID = "C03F7V6DU9M"
+MESSAGES_PER_PAGE = 100
+MAX_MESSAGES = 1_000
+# It wants a _string_ of the epoch timestamp (number)...
+OLDEST = str((datetime.now(tz=timezone.utc) - timedelta(days=1)).timestamp())
+
+
+def handle_rate_limit(response_generator_func: Callable[[], SlackResponse]):
+    response_generator = None
+
+    while True:
+        try:
+            if response_generator is None:
+                response_generator = iter(response_generator_func())
+            response = next(response_generator)
+            response.validate()
+            yield response
+        except StopIteration:
+            break
+        except SlackApiError as error:
+            if error.response.status_code != 429:
+                raise
+            retry_after = int(error.response.headers["Retry-After"])
+            logger.warning(
+                "Rate limited, retrying",
+            )
+            time.sleep(retry_after)
+            continue
 
 
 def extract_messages(response: SlackResponse):
@@ -23,20 +56,54 @@ def extract_messages(response: SlackResponse):
     ]
 
 
+def get_threaded_replies(client: WebClient, channel_id, thread_ts):
+    replies = []
+    for response in handle_rate_limit(
+        lambda: client.conversations_replies(
+            channel=channel_id,
+            ts=thread_ts,
+            limit=MESSAGES_PER_PAGE,
+        ),
+    ):
+        response.validate()
+        replies.extend(response["messages"][1:])  # Exclude the parent message
+
+    return replies
+
+
 @beartype
 def execute() -> None:
     api_key = os.getenv("SLACK_API_KEY")
     client = WebClient(token=api_key)
 
-    # It wants a _string_ of the epoch timestamp (number)...
-    yesterday = str((datetime.now(tz=timezone.utc) - timedelta(days=1)).timestamp())
+    page = 0
 
-    response = client.conversations_history(
-        channel=HASH_GRAPH_CHANNEL_ID,
-        oldest=yesterday,
-        limit=10,
-    )
+    messages = []
 
-    channel_history = extract_messages(response)
+    for response in handle_rate_limit(
+        lambda: client.conversations_history(
+            channel=HASH_GRAPH_CHANNEL_ID,
+            limit=MESSAGES_PER_PAGE,
+        ),
+    ):
+        page += 1
 
-    pprint(channel_history)
+        logger.info(
+            "Retrieving page of channel history history",
+            page=page,
+            channel=HASH_GRAPH_CHANNEL_ID,
+        )
+        response.validate()
+        messages.extend(extract_messages(response))
+
+        if len(messages) >= MAX_MESSAGES:
+            break
+
+    for message in messages:
+        message["replies"] = get_threaded_replies(
+            client,
+            HASH_GRAPH_CHANNEL_ID,
+            message["ts"],
+        )
+
+    pprint(messages)
