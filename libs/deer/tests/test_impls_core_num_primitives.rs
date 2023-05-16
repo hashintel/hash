a0b1e5a5d7c53e0cdcb1040bed93cde774350eb4
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-05-16 19:09:02 +0200

    `deer`: improve how floats are handled (#2425)
    * feat: completely rework how integrals are parsed
    
    * feat: rework testing infrastructure for integrals
    
    * feat: test try_fit
    
    * fix: `(U|I)sizeDeserializer`
    
    * feat: remove `(u|i)size` from data model
    
    * feat: remove `(u|i)size` from `Visitor`
    
    * fix: tests
    
    * feat: implement floating conversion
    
    * drive-by: move `integral.rs` and `floating.rs` under `num` umbrella
    
    * feat: unify primitive tests
    
    * feat: implement `proptest_fit_lossy`
    
    * feat: convert between floats
    
    * fix: apply from #2423
    
    * fix: apply more meaningful names to macro output
    
    * fix: macro name
    
    ---------
    
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>

--- libs/deer/tests/test_impls_core_num_primitives.rs
+++ libs/deer/tests/test_impls_core_num_primitives.rs
@@ -1,9 +1,11 @@
 mod common;
 
+use approx::assert_relative_eq;
 use deer::{
     value::{
-        I128Deserializer, I16Deserializer, I32Deserializer, I64Deserializer, I8Deserializer,
-        U128Deserializer, U16Deserializer, U32Deserializer, U64Deserializer, U8Deserializer,
+        F32Deserializer, F64Deserializer, I128Deserializer, I16Deserializer, I32Deserializer,
+        I64Deserializer, I8Deserializer, U128Deserializer, U16Deserializer, U32Deserializer,
+        U64Deserializer, U8Deserializer,
     },
     Context, Deserialize, Number,
 };
@@ -13,6 +15,22 @@ use proptest::prelude::*;
 use serde_json::json;
 
 // we do not test atomics, as they only delegate to `Atomic*` and are not `PartialEq`
+
+// super small compatability layer that delegates to the approx crate when using floats
+// we're using the approx crate here, because of `inf` checks.
+macro_rules! approx_eq {
+    (f32 | $lhs:ident, $rhs:ident) => {
+        assert_relative_eq!($lhs, $rhs);
+    };
+
+    (f64 | $lhs:ident, $rhs:ident) => {
+        assert_relative_eq!($lhs, $rhs);
+    };
+
+    ($primitive:ident | $lhs:ident, $rhs:ident) => {
+        assert_eq!($lhs, $rhs);
+    };
+}
 
 // use the value deserializers to see if we can tolerate different `visit_` without any problems
 macro_rules! proptest_fit {
@@ -30,7 +48,7 @@ macro_rules! proptest_fit {
                     let received: $primitive = $primitive::deserialize(deserializer)
                         .expect("should be able to deserialize");
 
-                    assert_eq!(received, expected);
+                    approx_eq!($primitive | received, expected);
                 }
             }
         }
@@ -56,7 +74,7 @@ macro_rules! proptest_try_fit {
                             // assume that the value should also correctly deserialize
                             let received = received.expect("should be able to deserialize");
 
-                            assert_eq!(received, expected);
+                            approx_eq!($primitive | received, expected);
                         },
                         Err(_) => {
                             // the value wasn't able to fit, we should expect an error
@@ -71,7 +89,29 @@ macro_rules! proptest_try_fit {
     };
 }
 
-macro_rules! proptest_integral {
+macro_rules! proptest_fit_lossy {
+    ($primitive:ident | $value:ident:: $deserializer:ident) => {
+        paste::paste! {
+            #[cfg(not(miri))]
+            proptest! {
+                #[test]
+                fn [< $primitive _fit _ $value >](value in any::<$value>()) {
+                    let context = Context::new();
+                    let deserializer = $deserializer::new(value, &context);
+
+                    // ensures that the lossy conversion that would take place is the same that
+                    // takes place when `as f64/f32` is called.
+                    let expected = value as $primitive;
+                    let received = $primitive::deserialize(deserializer).expect("should be able to deserializer");
+
+                    approx_eq!($primitive | received, expected);
+                }
+            }
+        }
+    };
+}
+
+macro_rules! proptest_primitive {
     (
         Token::
         $token:ident($primitive:ident $(as $equivalent:ident)?);
@@ -91,24 +131,24 @@ macro_rules! proptest_integral {
     };
 }
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(u8);
     proptest_try_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(u16);
     proptest_fit!(u8 :: U8Deserializer);
     proptest_try_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(u32);
     proptest_fit!(u8 :: U8Deserializer, u16 :: U16Deserializer);
     proptest_try_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(u64);
     proptest_fit!(u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer);
     proptest_try_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u128 :: U128Deserializer);
@@ -120,36 +160,36 @@ proptest_integral!(
 // The reason we have no other use-cases for the other `target_pointer_width`s is because it is hard
 // to emulate those during testing.
 #[cfg(target_pointer_width = "64")]
-proptest_integral!(
+proptest_primitive!(
     Token::Number(usize as u64);
     proptest_fit!(u8 :: U8Deserializer, u16 :: U16Deserializer);
     proptest_try_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, i128 :: I128Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::U128(u128);
     proptest_fit!(u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer);
     proptest_try_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(i8);
     proptest_try_fit!(i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(i16);
     proptest_fit!(i8 :: I8Deserializer);
     proptest_try_fit!(i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(i32);
     proptest_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer);
     proptest_try_fit!(i64 :: I64Deserializer, i128 :: I128Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::Number(i64);
     proptest_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer);
     proptest_try_fit!(i128 :: I128Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
@@ -157,16 +197,28 @@ proptest_integral!(
 
 // for reason of cfg guard please refer to `usize` tests
 #[cfg(target_pointer_width = "64")]
-proptest_integral!(
+proptest_primitive!(
     Token::Number(isize as i64);
     proptest_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer);
     proptest_try_fit!(i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
-proptest_integral!(
+proptest_primitive!(
     Token::I128(i128);
     proptest_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer);
     proptest_try_fit!(u8 :: U8Deserializer, u16 :: U16Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
+);
+
+proptest_primitive!(
+    Token::Number(f32);
+    proptest_fit!(i8 :: I8Deserializer, i16 :: I16Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer);
+    proptest_fit_lossy!(f64 :: F64Deserializer, i32 :: I32Deserializer, i64 :: I64Deserializer, i128 :: I128Deserializer, u32 :: U32Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
+);
+
+proptest_primitive!(
+    Token::Number(f64);
+    proptest_fit!(f32 :: F32Deserializer, i8 :: I8Deserializer, i16 :: I16Deserializer, u8 :: U8Deserializer, u16 :: U16Deserializer, i32 :: I32Deserializer, u32 :: U32Deserializer);
+    proptest_fit_lossy!(i64 :: I64Deserializer, i128 :: I128Deserializer, u64 :: U64Deserializer, u128 :: U128Deserializer);
 );
 
 // we're not testing the individual error messages, as those are tested at the
