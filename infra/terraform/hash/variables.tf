b911d7501c9a0cf7f9559107bbaeb7ff8e009d19
Ahmad Sattar <thehabbos007@gmail.com>
2023-05-29 16:14:22 +0200

    Automate HASH infrastructure deployment (#2609)
    * Add STS AWS auth
    
    * Switch to Vault based secrets
    
    * Initial apply
    
    * Add SSH key
    
    * Add ssh key to workflow
    
    * Add auto apply
    
    * Plan -> Apply
    
    * Update .github/workflows/tf-apply-hash.yml
    
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
    
    * Redirect stderr to stdout
    
    * Remove branch condition for workflow
    
    * Remove path filtering
    
    * Explicit prod/merge to main condition
    
    * Fix env expansion
    
    * Default `plan` to run on prod without conditoin
    
    ---------
    
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>

--- infra/terraform/hash/variables.tf
+++ infra/terraform/hash/variables.tf
@@ -8,21 +8,6 @@ variable "region_az_count" {
   description = "Number of availability zones to use for the infrastructure"
 }
 
-variable "kratos_secrets_cookie" {
-  type        = string
-  description = "Kratos cookie secret"
-}
-
-variable "kratos_secrets_cipher" {
-  type        = string
-  description = "Kratos cookie cipher"
-}
-
-variable "kratos_api_key" {
-  type        = string
-  description = "Kratos webhook api key"
-}
-
 variable "kratos_env_vars" {
   type = list(object({
     name   = string,
@@ -50,41 +35,20 @@ variable "hash_api_env_vars" {
   description = "A list of environment variables to save as system parameters and inject into the API service"
 }
 
-variable "hash_seed_users" {
-  type        = any
-  description = "List of users to seed"
-}
-
-variable "hash_system_user_password" {
-  type        = string
-  description = "Password to give the system user in HASH"
-}
-
-variable "hash_block_protocol_api_key" {
-  type        = string
-  description = "API key to fetch blocks from the BP Hub"
-}
-
-variable "pg_superuser_password" {
-  type        = string
-  sensitive   = true
-  description = "Password for the 'superuser' user in the Postgres instance"
-}
-
-variable "pg_kratos_user_password" {
-  type        = object({ hash = string, raw = string })
-  sensitive   = true
-  description = "Hashed form of the 'kratos' user Postgres password."
-}
-
-variable "pg_graph_user_password" {
-  type        = object({ hash = string, raw = string })
-  sensitive   = true
-  description = "Hashed form of the 'graph' user Postgres password."
-}
-
 variable "ses_verified_domain_identity" {
   type        = string
   default     = ""
   description = "A verified AWS SES identity to use for email sending in the application."
+}
+
+variable "in_ci" {
+  type        = bool
+  default     = false
+  description = "Whether or not this is running in CI. If true, the AWS provider will be configured to use the default profile."
+}
+
+variable "vault_kvv2_secret_path" {
+  type        = string
+  default     = "pipelines/hash/"
+  description = "The _root_ path where secrets are stored in Vault for this service. The secrets shall be called the same as the Terraform Workspace name."
 }
