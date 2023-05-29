dacfac8ecbb0f6cf9bb9ec306d4481241e3a1e85
Ahmad Sattar <thehabbos007@gmail.com>
2023-05-29 18:43:58 +0200

    Remove redundant local modules

--- infra/terraform/modules/bastion/output.tf
+++
@@ -1,8 +0,0 @@
-output "ssh_info" {
-  value = {
-    host        = aws_instance.bastion.public_ip
-    private_key = tls_private_key.bastion_private_key.private_key_pem
-    # Default for Amazon Linux
-    user = "ec2-user"
-  }
-}
