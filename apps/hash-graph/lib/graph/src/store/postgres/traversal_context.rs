9024a04065736e7772dc708957b0b53ffcc4a88d
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-05-30 15:34:55 +0200

    Add a basic batch-filter for structural queries (#2614)
    * Add a basic batch-filter for structural queries
    
    * Use batched reading for vertices

--- apps/hash-graph/lib/graph/src/store/postgres/traversal_context.rs
+++ apps/hash-graph/lib/graph/src/store/postgres/traversal_context.rs
@@ -5,10 +5,13 @@ use error_stack::Result;
 use crate::{
     identifier::knowledge::EntityEditionId,
     knowledge::{Entity, EntityQueryPath},
-    ontology::{DataTypeWithMetadata, EntityTypeWithMetadata, PropertyTypeWithMetadata},
+    ontology::{
+        DataTypeQueryPath, DataTypeWithMetadata, EntityTypeQueryPath, EntityTypeWithMetadata,
+        PropertyTypeQueryPath, PropertyTypeWithMetadata,
+    },
     store::{
         crud::Read,
-        query::{Filter, FilterExpression, Parameter},
+        query::{Filter, FilterExpression, ParameterList},
         AsClient, PostgresStore, QueryError, Record,
     },
     subgraph::{
@@ -23,16 +26,25 @@ impl<C: AsClient> PostgresStore<C> {
         vertex_ids: impl IntoIterator<Item = &DataTypeVertexId, IntoIter: Send> + Send,
         subgraph: &mut Subgraph,
     ) -> Result<(), QueryError> {
-        for vertex_id in vertex_ids {
-            for data_type in <Self as Read<DataTypeWithMetadata>>::read_vec(
-                self,
-                &DataTypeWithMetadata::create_filter_for_vertex_id(vertex_id),
-                Some(&subgraph.temporal_axes.resolved),
-            )
-            .await?
-            {
-                subgraph.insert_vertex(vertex_id, data_type);
-            }
+        let ids = vertex_ids
+            .into_iter()
+            .map(|id| format!("{}v/{}", id.base_id, id.revision_id.inner()))
+            .collect::<Vec<_>>();
+
+        for data_type in <Self as Read<DataTypeWithMetadata>>::read_vec(
+            self,
+            &Filter::<DataTypeWithMetadata>::In(
+                FilterExpression::Path(DataTypeQueryPath::VersionedUrl),
+                ParameterList::VersionedUrls(&ids),
+            ),
+            Some(&subgraph.temporal_axes.resolved),
+        )
+        .await?
+        {
+            subgraph.insert_vertex(
+                data_type.vertex_id(subgraph.temporal_axes.resolved.variable_time_axis()),
+                data_type,
+            );
         }
 
         Ok(())
@@ -43,16 +55,25 @@ impl<C: AsClient> PostgresStore<C> {
         vertex_ids: impl IntoIterator<Item = &PropertyTypeVertexId, IntoIter: Send> + Send,
         subgraph: &mut Subgraph,
     ) -> Result<(), QueryError> {
-        for vertex_id in vertex_ids {
-            for property_type in <Self as Read<PropertyTypeWithMetadata>>::read_vec(
-                self,
-                &PropertyTypeWithMetadata::create_filter_for_vertex_id(vertex_id),
-                Some(&subgraph.temporal_axes.resolved),
-            )
-            .await?
-            {
-                subgraph.insert_vertex(vertex_id, property_type);
-            }
+        let ids = vertex_ids
+            .into_iter()
+            .map(|id| format!("{}v/{}", id.base_id, id.revision_id.inner()))
+            .collect::<Vec<_>>();
+
+        for property_type in <Self as Read<PropertyTypeWithMetadata>>::read_vec(
+            self,
+            &Filter::<PropertyTypeWithMetadata>::In(
+                FilterExpression::Path(PropertyTypeQueryPath::VersionedUrl),
+                ParameterList::VersionedUrls(&ids),
+            ),
+            Some(&subgraph.temporal_axes.resolved),
+        )
+        .await?
+        {
+            subgraph.insert_vertex(
+                property_type.vertex_id(subgraph.temporal_axes.resolved.variable_time_axis()),
+                property_type,
+            );
         }
 
         Ok(())
@@ -63,16 +84,25 @@ impl<C: AsClient> PostgresStore<C> {
         vertex_ids: impl IntoIterator<Item = &EntityTypeVertexId, IntoIter: Send> + Send,
         subgraph: &mut Subgraph,
     ) -> Result<(), QueryError> {
-        for vertex_id in vertex_ids {
-            for entity_type in <Self as Read<EntityTypeWithMetadata>>::read_vec(
-                self,
-                &EntityTypeWithMetadata::create_filter_for_vertex_id(vertex_id),
-                Some(&subgraph.temporal_axes.resolved),
-            )
-            .await?
-            {
-                subgraph.insert_vertex(vertex_id, entity_type);
-            }
+        let ids = vertex_ids
+            .into_iter()
+            .map(|id| format!("{}v/{}", id.base_id, id.revision_id.inner()))
+            .collect::<Vec<_>>();
+
+        for entity_type in <Self as Read<EntityTypeWithMetadata>>::read_vec(
+            self,
+            &Filter::<EntityTypeWithMetadata>::In(
+                FilterExpression::Path(EntityTypeQueryPath::VersionedUrl),
+                ParameterList::VersionedUrls(&ids),
+            ),
+            Some(&subgraph.temporal_axes.resolved),
+        )
+        .await?
+        {
+            subgraph.insert_vertex(
+                entity_type.vertex_id(subgraph.temporal_axes.resolved.variable_time_axis()),
+                entity_type,
+            );
         }
 
         Ok(())
@@ -80,27 +110,28 @@ impl<C: AsClient> PostgresStore<C> {
 
     async fn read_entities_by_ids(
         &self,
-        vertex_ids: impl IntoIterator<Item = &EntityEditionId, IntoIter: Send> + Send,
+        edition_ids: impl IntoIterator<Item = EntityEditionId, IntoIter: Send> + Send,
         subgraph: &mut Subgraph,
     ) -> Result<(), QueryError> {
-        for edition_id in vertex_ids {
-            for entity in <Self as Read<Entity>>::read_vec(
-                self,
-                &Filter::<Entity>::Equal(
-                    Some(FilterExpression::Path(EntityQueryPath::EditionId)),
-                    Some(FilterExpression::Parameter(Parameter::Uuid(
-                        edition_id.as_uuid(),
-                    ))),
-                ),
-                Some(&subgraph.temporal_axes.resolved),
-            )
-            .await?
-            {
-                subgraph.insert_vertex(
-                    &entity.vertex_id(subgraph.temporal_axes.resolved.variable_time_axis()),
-                    entity,
-                );
-            }
+        let ids = edition_ids
+            .into_iter()
+            .map(EntityEditionId::as_uuid)
+            .collect::<Vec<_>>();
+
+        for entity in <Self as Read<Entity>>::read_vec(
+            self,
+            &Filter::<Entity>::In(
+                FilterExpression::Path(EntityQueryPath::EditionId),
+                ParameterList::Uuid(&ids),
+            ),
+            Some(&subgraph.temporal_axes.resolved),
+        )
+        .await?
+        {
+            subgraph.insert_vertex(
+                entity.vertex_id(subgraph.temporal_axes.resolved.variable_time_axis()),
+                entity,
+            );
         }
 
         Ok(())
@@ -154,18 +185,27 @@ impl TraversalContext {
         store: &PostgresStore<C>,
         subgraph: &mut Subgraph,
     ) -> Result<(), QueryError> {
-        store
-            .read_data_types_by_ids(&self.data_types.vertex_ids, subgraph)
-            .await?;
-        store
-            .read_property_types_by_ids(&self.property_types.vertex_ids, subgraph)
-            .await?;
-        store
-            .read_entity_types_by_ids(&self.entity_types.vertex_ids, subgraph)
-            .await?;
-        store
-            .read_entities_by_ids(&self.entities.edition_ids, subgraph)
-            .await?;
+        if !self.data_types.vertex_ids.is_empty() {
+            store
+                .read_data_types_by_ids(&self.data_types.vertex_ids, subgraph)
+                .await?;
+        }
+        if !self.property_types.vertex_ids.is_empty() {
+            store
+                .read_property_types_by_ids(&self.property_types.vertex_ids, subgraph)
+                .await?;
+        }
+        if !self.entity_types.vertex_ids.is_empty() {
+            store
+                .read_entity_types_by_ids(&self.entity_types.vertex_ids, subgraph)
+                .await?;
+        }
+
+        if !self.entities.edition_ids.is_empty() {
+            store
+                .read_entities_by_ids(self.entities.edition_ids.iter().copied(), subgraph)
+                .await?;
+        }
 
         Ok(())
     }
