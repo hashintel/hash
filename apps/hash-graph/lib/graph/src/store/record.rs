d240264417a4163e44210284911a2366b7612e13
Ahmad Sattar <thehabbos007@gmail.com>
2023-06-20 18:30:43 +0200

    Disable VPC Flow Logs (#2677)
    * Disable flow logs for now
    
    * Disable flow logs by default
    
    * drive-by: pin AMi image
    
    * file-based stdout/err
    
    * relative dir
    
    * Remove stderr
    
    * Arbitrary rust change

--- apps/hash-graph/lib/graph/src/store/record.rs
+++ apps/hash-graph/lib/graph/src/store/record.rs
@@ -1,6 +1,6 @@
 use crate::{identifier::time::TimeAxis, store::query::QueryPath, subgraph::identifier::VertexId};
 
-/// A record stored in the [`store`].
+/// A record persisted in the [`store`].
 ///
 /// [`store`]: crate::store
 pub trait Record: Sized + Send {
