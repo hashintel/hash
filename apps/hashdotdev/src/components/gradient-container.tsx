348039f04ff0af2cb1d18d3dfaf38df5f5eb2029
Ben Werner <ben.werner01@gmail.com>
2023-06-28 13:17:55 +0100

    fix: background gradient

--- apps/hashdotdev/src/components/gradient-container.tsx
+++ apps/hashdotdev/src/components/gradient-container.tsx
@@ -18,14 +18,26 @@ export const GradientContainer: FunctionComponent<BoxProps> = ({
         "&:before": {
           position: "absolute",
           background:
-            "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(237,252,255,1) 53%)",
+            "linear-gradient(183deg, #CEE3E8 0%, rgba(206, 227, 232, 0.00) 100%)",
           width: "100%",
           height: 500,
           top: -NAV_HEIGHT,
-          zIndex: -1,
+          zIndex: -2,
           content: `""`,
           display: "block",
           left: 0,
+        },
+        /** ensure the gradient fades out smoothly */
+        "&:after": {
+          content: `""`,
+          position: "absolute",
+          zIndex: -1,
+          width: "100%",
+          height: 100,
+          bottom: 222,
+          left: 0,
+          background:
+            "linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255, 1) 90%)",
         },
       },
       ...(Array.isArray(sx) ? sx : [sx]),
