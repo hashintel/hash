f38df6b1efb04d1d9ed687839823be0c4f7f1ad8
Tim Diekmann <td@hash.ai>
2023-06-30 13:52:45 +0200

    Adjust Python workflow to also use take an actor id

--- apps/hash-ai-worker-py/app/workflows.py
+++ apps/hash-ai-worker-py/app/workflows.py
@@ -1,11 +1,19 @@
 """Temporal workflow definitions."""
 
+from pydantic import BaseModel, Field
 from temporalio import workflow
 
 from app.typesystem import DataTypeSchema, EntityTypeSchema, PropertyTypeSchema
 
 with workflow.unsafe.imports_passed_through():
     pass
+
+
+class DataTypeWorkflowParameters(BaseModel):
+    """Parameters for ontology type workflows."""
+
+    data_type_id: str = Field(..., alias="dataTypeId")
+    actor_id: str = Field(..., alias="actorId")
 
 
 @workflow.defn(name="getDataType")
@@ -15,16 +23,15 @@ class DataTypeWorkflow:
     # TODO: Use Temporal's guide to convert between JSON and Pydantic objects.
     # https://github.com/temporalio/samples-python/tree/main/pydantic_converter
     @workflow.run
-    async def get_data_type(self, versioned_url: str) -> str:
+    async def get_data_type(self, params: DataTypeWorkflowParameters) -> str:
+        print(params)
         """Calls the Graph API to get a data type schema."""
         data_type_schema = DataTypeSchema(
             **(
                 await workflow.execute_child_workflow(
                     task_queue="ai",
                     workflow="getDataType",
-                    arg={
-                        "dataTypeId": versioned_url,
-                    },
+                    arg=params.dict(by_alias=True, exclude_none=True),
                 )
             )["schema"],
         )
@@ -34,6 +41,13 @@ class DataTypeWorkflow:
         return data_type_schema.json(by_alias=True, exclude_none=True)
 
 
+class PropertyTypeWorkflowParameters(BaseModel):
+    """Parameters for ontology type workflows."""
+
+    proeprty_type_id: str = Field(..., alias="propertyTypeId")
+    actor_id: str = Field(..., alias="actorId")
+
+
 @workflow.defn(name="getPropertyType")
 class PropertyTypeWorkflow:
     """A workflow that reads a property type from the provided URL."""
@@ -41,16 +55,14 @@ class PropertyTypeWorkflow:
     # TODO: Use Temporal's guide to convert between JSON and Pydantic objects.
     # https://github.com/temporalio/samples-python/tree/main/pydantic_converter
     @workflow.run
-    async def get_property_type(self, versioned_url: str) -> str:
+    async def get_property_type(self, params: PropertyTypeWorkflowParameters) -> str:
         """Calls the Graph API to get a property type schema."""
         property_type_schema = PropertyTypeSchema(
             **(
                 await workflow.execute_child_workflow(
                     task_queue="ai",
                     workflow="getPropertyType",
-                    arg={
-                        "propertyTypeId": versioned_url,
-                    },
+                    arg=params.dict(by_alias=True, exclude_none=True),
                 )
             )["schema"],
         )
@@ -60,6 +72,13 @@ class PropertyTypeWorkflow:
         return property_type_schema.json(by_alias=True, exclude_none=True)
 
 
+class EntityTypeWorkflowParameters(BaseModel):
+    """Parameters for ontology type workflows."""
+
+    entity_type_id: str = Field(..., alias="entityTypeId")
+    actor_id: str = Field(..., alias="actorId")
+
+
 @workflow.defn(name="getEntityType")
 class EntityTypeWorkflow:
     """A workflow that reads an entity type from the provided URL."""
@@ -67,16 +86,14 @@ class EntityTypeWorkflow:
     # TODO: Use Temporal's guide to convert between JSON and Pydantic objects.
     # https://github.com/temporalio/samples-python/tree/main/pydantic_converter
     @workflow.run
-    async def get_entity_type(self, versioned_url: str) -> str:
+    async def get_entity_type(self, params: EntityTypeWorkflowParameters) -> str:
         """Calls the Graph API to get an entity type schema."""
         entity_type_schema = EntityTypeSchema(
             **(
                 await workflow.execute_child_workflow(
                     task_queue="ai",
                     workflow="getEntityType",
-                    arg={
-                        "entityTypeId": versioned_url,
-                    },
+                    arg=params.dict(by_alias=True, exclude_none=True),
                 )
             )["schema"],
         )
