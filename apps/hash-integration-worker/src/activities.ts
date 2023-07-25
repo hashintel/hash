8677c70ce405a38f05153166a7f08c1302aea7a1
Tim Diekmann <td@hash.ai>
2023-07-25 17:35:27 +0200

    Apply suggestions from code review

--- apps/hash-integration-worker/src/activities.ts
+++ apps/hash-integration-worker/src/activities.ts
@@ -26,72 +26,72 @@ const readNodes = async <T>(connection: Connection<T>): Promise<T[]> => {
   return nodes;
 };
 
-export const createLinearIntegrationActivities = (createInfo: {
+export const createLinearIntegrationActivities = ({
+  linearClient,
+}: {
   linearClient: LinearClient;
 }) => ({
   async me(): Promise<User> {
-    return await createInfo.linearClient.viewer;
+    return await linearClient.viewer;
   },
 
   async organization(): Promise<Organization> {
-    return await createInfo.linearClient.organization;
+    return await linearClient.organization;
   },
 
   async teams(): Promise<Team[]> {
-    return await createInfo.linearClient.teams().then(readNodes);
+    return await linearClient.teams().then(readNodes);
   },
 
   async issues(filter?: { teamId?: string }): Promise<Issue[]> {
     const issuesQueryVariables: LinearDocument.IssuesQueryVariables = {
       filter: {},
     };
-    if (filter && filter.teamId) {
+    if (filter?.teamId) {
       issuesQueryVariables.filter!.team = { id: { eq: filter.teamId } };
     }
-    return createInfo.linearClient.issues(issuesQueryVariables).then(readNodes);
+    return linearClient.issues(issuesQueryVariables).then(readNodes);
   },
 
   async users(): Promise<User[]> {
-    return createInfo.linearClient.users().then(readNodes);
+    return linearClient.users().then(readNodes);
   },
 
   async cycles(filter?: { teamId?: string }): Promise<Cycle[]> {
     const cyclesQueryVariables: LinearDocument.CyclesQueryVariables = {
       filter: {},
     };
-    if (filter && filter.teamId) {
+    if (filter?.teamId) {
       cyclesQueryVariables.filter!.team = { id: { eq: filter.teamId } };
     }
-    return createInfo.linearClient.cycles(cyclesQueryVariables).then(readNodes);
+    return linearClient.cycles(cyclesQueryVariables).then(readNodes);
   },
 
   async customViews(): Promise<CustomView[]> {
-    return createInfo.linearClient.customViews().then(readNodes);
+    return linearClient.customViews().then(readNodes);
   },
 
   async projects(): Promise<Project[]> {
-    return createInfo.linearClient.projects().then(readNodes);
+    return linearClient.projects().then(readNodes);
   },
 
   async comments(filter?: { teamId?: string }): Promise<Comment[]> {
     const commentsQueryVariables: LinearDocument.CommentsQueryVariables = {
       filter: {},
     };
-    if (filter && filter.teamId) {
+    if (filter?.teamId) {
       commentsQueryVariables.filter!.issue = {
         team: { id: { eq: filter.teamId } },
       };
     }
-    return createInfo.linearClient
-      .comments(commentsQueryVariables)
-      .then(readNodes);
+    return linearClient.comments(commentsQueryVariables).then(readNodes);
   },
 
   async projectMilestones(): Promise<ProjectMilestone[]> {
     return (
       await Promise.all(
         (
-          await createInfo.linearClient.projects().then(readNodes)
+          await linearClient.projects().then(readNodes)
         ).map(
           async (project) => await project.projectMilestones().then(readNodes),
         ),
@@ -100,21 +100,21 @@ export const createLinearIntegrationActivities = (createInfo: {
   },
 
   async documents(): Promise<Document[]> {
-    return await createInfo.linearClient.documents().then(readNodes);
+    return await linearClient.documents().then(readNodes);
   },
 
   async issueLabels(filter?: { teamId?: string }): Promise<IssueLabel[]> {
     const issueLabelsQueryVariables: LinearDocument.IssueLabelsQueryVariables =
       { filter: {} };
-    if (filter && filter.teamId) {
+    if (filter?.teamId) {
       issueLabelsQueryVariables.filter = {
         team: { id: { eq: filter.teamId } },
       };
     }
-    return await createInfo.linearClient.issueLabels().then(readNodes);
+    return await linearClient.issueLabels().then(readNodes);
   },
 
   async attachments(): Promise<Attachment[]> {
-    return await createInfo.linearClient.attachments().then(readNodes);
+    return await linearClient.attachments().then(readNodes);
   },
 });
