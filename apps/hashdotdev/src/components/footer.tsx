4e027642218b985fcd4ae8ae12faa4d66cc74e15
Ben Werner <ben.werner01@gmail.com>
2023-08-03 17:57:38 +0100

    ft: add `Docs` link to footer

--- apps/hashdotdev/src/components/footer.tsx
+++ apps/hashdotdev/src/components/footer.tsx
@@ -141,9 +141,8 @@ export const Footer: FunctionComponent = () => (
           <Grid item lg={4} md={6}>
             <FooterSection label="Resources">
               <FooterLink href="/blog">Blog</FooterLink>
+              <FooterLink href="/docs">Docs</FooterLink>
               <FooterLink href="/roadmap">Roadmap</FooterLink>
-              {/* @todo: add docs pages */}
-              {/* <FooterLink href="/docs">Docs</FooterLink> */}
               {/* @todo: add tutorials pages */}
               {/* <FooterLink href="/tutorials">Tutorials</FooterLink> */}
             </FooterSection>
