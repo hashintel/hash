ac859c0667cf8aa8b8884744aa187fdb63352075
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-08-07 17:02:20 +0200

    H-321: Add python package for `hash-graph-sdk` (#2864)

---
+++ libs/@local/hash-graph-sdk/python/graph_sdk/__init__.py
@@ -0,0 +1,6 @@
+"""Graph API SDK.
+
+Even though this is a software development kit for the Graph API,
+the graph API is still considered to be an implementation detail (for now),
+and no guarantees are made about the stability of the API.
+"""
