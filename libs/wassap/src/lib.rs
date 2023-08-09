35755b62cf9a0047fe507802eb2916073839d089
Tim Diekmann <td@hash.ai>
2023-08-09 12:55:35 +0200

    `wassap` template

---
+++ libs/wassap/src/lib.rs
@@ -0,0 +1,4 @@
+//! A placeholder crate for the `wassap` library - a complementary library to [`error-stack`]
+//! designed to provide a more ergonomic interface for status information.
+//!
+//! [`error-stack`]: https://crates.io/crates/error-stack
