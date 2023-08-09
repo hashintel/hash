70ef1a437bdc03f1573c317f1805d4e69b4378ca
Tim Diekmann <td@hash.ai>
2023-08-09 19:20:26 +0200

    Move `create_entity` and `update_entity` out of the migration files

--- apps/hash-graph/lib/graph/src/shared/identifier/time/timestamp.rs
+++ apps/hash-graph/lib/graph/src/shared/identifier/time/timestamp.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::{cmp::Ordering, error::Error, marker::PhantomData, str::FromStr, time::SystemTime};
+use std::{cmp::Ordering, error::Error, marker::PhantomData, str::FromStr};
 
 use derivative::Derivative;
 use postgres_types::{private::BytesMut, FromSql, ToSql, Type};
@@ -103,37 +103,27 @@ impl<A> FromStr for Timestamp<A> {
 }
 
 impl<'a> FromSql<'a> for Timestamp<()> {
+    postgres_types::accepts!(TIMESTAMPTZ);
+
     fn from_sql(ty: &Type, raw: &'a [u8]) -> Result<Self, Box<dyn Error + Sync + Send>> {
         Ok(Self {
             axis: PhantomData,
             time: OffsetDateTime::from_sql(ty, raw)?,
         })
     }
-
-    fn accepts(ty: &Type) -> bool {
-        <SystemTime as FromSql>::accepts(ty)
-    }
 }
 
 impl<A> ToSql for Timestamp<A> {
+    postgres_types::accepts!(TIMESTAMPTZ);
+
+    postgres_types::to_sql_checked!();
+
     fn to_sql(
         &self,
         ty: &Type,
         out: &mut BytesMut,
     ) -> Result<postgres_types::IsNull, Box<dyn Error + Sync + Send>> {
         self.time.to_sql(ty, out)
-    }
-
-    fn accepts(ty: &Type) -> bool {
-        <SystemTime as ToSql>::accepts(ty)
-    }
-
-    fn to_sql_checked(
-        &self,
-        ty: &Type,
-        out: &mut BytesMut,
-    ) -> Result<postgres_types::IsNull, Box<dyn Error + Sync + Send>> {
-        self.time.to_sql_checked(ty, out)
     }
 }
 
