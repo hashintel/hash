4fbee25b9e0fe3ef72551969b60d62e5488453ae
Ciaran Morinan <c@hash.ai>
2023-08-25 07:46:39 +0100

    h-534: update navbar logo

--- apps/hash-frontend/src/shared/icons.tsx
+++ apps/hash-frontend/src/shared/icons.tsx
@@ -1,5 +1,4 @@
 export * from "./icons/hash-icon";
-export { HashAlphaNavIcon } from "./icons/hash-nav-icon";
 export * from "./icons/search-icon";
 export * from "./icons/sidebar-toggle-icon";
 export * from "./icons/svg";
