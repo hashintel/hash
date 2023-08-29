abc63b99cf0b58b4341b472035cd30073d96c000
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-08-29 16:20:42 +0200

    H-469: Add basic Zanzibar structs and simple check functionality (#3014)

---
+++ apps/hash-graph/lib/authorization/src/backend.rs
@@ -0,0 +1,47 @@
+mod spicedb;
+
+use core::fmt;
+use std::error::Error;
+
+use error_stack::Report;
+
+pub use self::spicedb::{SpiceDb, SpiceDbConfig};
+use crate::zanzibar::{Affiliation, Consistency, Resource, StringTuple, Subject, Zookie};
+
+/// A backend for interacting with an authorization system based on the Zanzibar model.
+pub trait AuthorizationBackend {
+    /// Returns if the [`Subject`] has the specified permission or relation to an [`Resource`].
+    async fn check<S, A, R>(
+        &self,
+        subject: &S,
+        affiliation: &A,
+        resource: &R,
+        consistency: Consistency<'_>,
+    ) -> Result<CheckResponse, Report<CheckError>>
+    where
+        S: Subject + Sync,
+        A: Affiliation<R> + Sync,
+        R: Resource + Sync;
+}
+
+/// Return value for [`AuthorizationBackend::check`].
+#[derive(Debug)]
+pub struct CheckResponse {
+    /// If the [`Subject`] has the specified permission or relation to an [`Resource`].
+    pub has_permission: bool,
+    /// A token to determine the time at which the check was performed.
+    pub checked_at: Zookie<'static>,
+}
+
+#[derive(Debug)]
+pub struct CheckError {
+    pub tuple: StringTuple,
+}
+
+impl fmt::Display for CheckError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(fmt, "failed to check permission: `{}`", self.tuple)
+    }
+}
+
+impl Error for CheckError {}
