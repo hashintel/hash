6902846a4108d12a27ac02dd936fba2d474e4192
Ben Werner <ben.werner01@gmail.com>
2023-09-12 10:21:07 +0100

    fix: lighten `Next`/`Previous` copy on docs pages

--- apps/hashdotdev/src/pages/docs/page-nav-links.tsx
+++ apps/hashdotdev/src/pages/docs/page-nav-links.tsx
@@ -41,7 +41,7 @@ export const PageNavLinks: FunctionComponent<PageNavLinksProps> = ({
         {prevPage && (
           <Box display="flex" alignItems="flex-end">
             <Box>
-              <Typography sx={{ color: theme.palette.gray[70] }} component="p">
+              <Typography sx={{ color: theme.palette.gray[50] }} component="p">
                 Previous
               </Typography>
               <Box
@@ -88,7 +88,7 @@ export const PageNavLinks: FunctionComponent<PageNavLinksProps> = ({
             alignItems="flex-end"
             sx={{ position: "relative", left: hideIcons ? 0 : "31px" }}
           >
-            <Typography sx={{ color: theme.palette.gray[70] }} component="p">
+            <Typography sx={{ color: theme.palette.gray[50] }} component="p">
               Next
             </Typography>
             <Box
