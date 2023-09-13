9d7ff87a394d3b643a06a0d9b4f925e992016871
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2023-09-13 11:26:23 +0200

    H-705: Enable Sentry integration in `hash-graph` (#3091)
    Co-authored-by: Tim Diekmann <td@hash.ai>
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>

--- apps/hash-graph/bin/cli/src/main.rs
+++ apps/hash-graph/bin/cli/src/main.rs
@@ -3,29 +3,54 @@
 
 mod args;
 mod error;
+mod parser;
 mod subcommand;
 
-use error_stack::Result;
+use std::sync::Arc;
+
+use error_stack::{ensure, Report, Result};
 use graph::load_env;
 
-use self::{args::Args, error::GraphError, subcommand::Subcommand};
+use self::{args::Args, error::GraphError};
+use crate::error::SentryError;
 
-#[tokio::main]
-async fn main() -> Result<(), GraphError> {
+fn main() -> Result<(), GraphError> {
     load_env(None);
 
-    let args = Args::parse_args();
+    let Args {
+        sentry_dsn,
+        subcommand,
+    } = Args::parse_args();
 
-    match args.subcommand {
-        Subcommand::Server(args) => subcommand::server(args).await,
-        Subcommand::Migrate(args) => subcommand::migrate(args).await,
-        Subcommand::TypeFetcher(args) => subcommand::type_fetcher(args).await,
-        Subcommand::Completions(ref args) => {
-            subcommand::completions(args);
-            Ok(())
-        }
-        Subcommand::Snapshot(args) => subcommand::snapshot(args).await,
-        #[cfg(all(hash_graph_test_environment, feature = "test-server"))]
-        Subcommand::TestServer(args) => subcommand::test_server(args).await,
+    if let Some(dsn) = &sentry_dsn {
+        let client = sentry::Client::from_config(dsn);
+
+        ensure!(
+            client.is_enabled(),
+            Report::new(SentryError::InvalidDsn)
+                .attach_printable(dsn.clone())
+                .change_context(GraphError)
+        );
     }
+
+    // Initialize Sentry
+    // When initializing Sentry, a `Drop` guard is returned, once dropped any remaining events are
+    // flushed. This means we need to keep the guard around for the entire lifetime of the program.
+    let _sentry = sentry::init(sentry::ClientOptions {
+        dsn: sentry_dsn,
+        release: sentry::release_name!(),
+        session_mode: sentry::SessionMode::Request,
+        traces_sampler: Some(Arc::new(|ctx| {
+            if Some(true) == ctx.sampled() {
+                1.0
+            } else if ctx.operation() == "http.server" {
+                0.1
+            } else {
+                1.0
+            }
+        })),
+        ..Default::default()
+    });
+
+    subcommand.execute()
 }
