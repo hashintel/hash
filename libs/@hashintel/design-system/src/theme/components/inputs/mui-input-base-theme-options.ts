ac8910883d04885afabb489fb33c675676b60669
Ben Werner <ben.werner01@gmail.com>
2023-09-13 15:34:55 +0100

    ft: disable 1Password for inputs by default

---
+++ libs/@hashintel/design-system/src/theme/components/inputs/mui-input-base-theme-options.ts
@@ -0,0 +1,9 @@
+import { Components, Theme } from "@mui/material";
+
+export const MuiInputBaseThemeOptions: Components<Theme>["MuiInputBase"] = {
+  defaultProps: {
+    inputProps: {
+      "data-1p-ignore": true,
+    },
+  },
+};
