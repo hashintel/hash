fc3e01b195e1a2fe625bb6bef91930399153e7f0
Ben Werner <ben.werner01@gmail.com>
2023-09-13 17:02:50 +0100

    ft: add dropdown items to org context menu

--- apps/hash-frontend/src/pages/settings/organizations/index.page/org-row/org-context-menu.tsx
+++ apps/hash-frontend/src/pages/settings/organizations/index.page/org-row/org-context-menu.tsx
@@ -5,10 +5,17 @@ import {
   usePopupState,
 } from "material-ui-popup-state/hooks";
 
+import { MinimalOrg } from "../../../../../lib/user-and-org";
 import { MenuItem } from "../../../../../shared/ui/menu-item";
 import { ContextButton, contextMenuProps } from "../../../shared/context-menu";
 
-export const OrgContextMenu = ({ leaveOrg }: { leaveOrg: () => void }) => {
+export const OrgContextMenu = ({
+  leaveOrg,
+  org,
+}: {
+  leaveOrg: () => void;
+  org: MinimalOrg;
+}) => {
   const popupState = usePopupState({
     variant: "popover",
     popupId: "actions-dropdown-menu",
@@ -19,7 +26,22 @@ export const OrgContextMenu = ({ leaveOrg }: { leaveOrg: () => void }) => {
       <ContextButton {...bindTrigger(popupState)}>...</ContextButton>
 
       <Menu {...bindMenu(popupState)} {...contextMenuProps}>
+        <MenuItem href={`/@${org.shortname}`}>
+          <ListItemText primary="View profile" />
+        </MenuItem>
+        <MenuItem href={`/settings/organizations/${org.shortname}/general`}>
+          <ListItemText primary="Edit profile" />
+        </MenuItem>
+        <MenuItem href={`/settings/organizations/${org.shortname}/members`}>
+          <ListItemText primary="View members" />
+        </MenuItem>
         <MenuItem
+          href={`/settings/organizations/${org.shortname}/members#invite`}
+        >
+          <ListItemText primary="Invite new members" />
+        </MenuItem>
+        <MenuItem
+          dangerous
           onClick={() => {
             leaveOrg();
             popupState.close();
