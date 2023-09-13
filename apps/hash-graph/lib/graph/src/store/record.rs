65b669fd9a154e4a21abf418a20571be2a86aa9d
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-09-13 14:14:38 +0200

    H-748: Export accounts and account groups in snapshots (#3127)

--- apps/hash-graph/lib/graph/src/store/record.rs
+++ apps/hash-graph/lib/graph/src/store/record.rs
@@ -5,6 +5,8 @@ use crate::{store::query::QueryPath, subgraph::identifier::VertexId};
 /// A record persisted in the [`store`].
 ///
 /// [`store`]: crate::store
+// TODO: Split trait into subgraph part and query part
+//   see https://linear.app/hash/issue/H-754
 pub trait Record: Sized + Send {
     type VertexId: VertexId<Record = Self> + Send + Sync;
     type QueryPath<'p>: QueryPath + Send + Sync;
