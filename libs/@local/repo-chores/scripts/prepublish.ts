6ec4ecb449bdc5b5c5f64c1ad37d6a79823f065c
Ciaran Morinan <c@hash.ai>
2023-09-19 13:30:06 +0100

    fix check in prepublish script

--- libs/@local/repo-chores/scripts/prepublish.ts
+++ libs/@local/repo-chores/scripts/prepublish.ts
@@ -71,7 +71,7 @@ const script = async () => {
     (packageJson: Record<string, string | any>) => {
       if ("main" in packageJson) {
         /* eslint-disable @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions,no-param-reassign -- see comment on updateJson() for potential improvement */
-        if (packageJson.main === expectedMainName) {
+        if (packageJson.main !== expectedMainName) {
           throw new UserFriendlyError(
             `Unexpected value for field "main" in package.json. Please align this package with other publishable packages for consistency. Expected: "${expectedMainName}". Got: "${packageJson.main}"`,
           );
