509a843944f25a48a1f3638b64607b71b7023b6c
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2023-09-26 19:06:12 +0100

    H-480: Scaffold `hashdotai-new` app, upgrade Next and MUI (#3260)
    Co-authored-by: David @ HASH <6226576+nonparibus@users.noreply.github.com>

---
+++ apps/hashdotai-new/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Home</Typography>
+    </Box>
+  );
+};
+
+export default Home;
