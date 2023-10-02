dfe778b47db9672a6190802abdf56cde211ed66f
Ciaran Morinan <c@hash.ai>
2023-10-02 19:48:20 +0100

    switch to localStorage for storing user, add todo to clean up

--- apps/plugin-browser/src/pages/shared/storage.ts
+++ apps/plugin-browser/src/pages/shared/storage.ts
@@ -1,8 +1,9 @@
 import { Simplified } from "@local/hash-isomorphic-utils/simplify-properties";
 import { User } from "@local/hash-isomorphic-utils/system-types/shared";
 
+// @todo switch to browser.storage.session (async access), or clear this when user logs out
 export const retrieveUser = () => {
-  const user = window.sessionStorage.getItem("user");
+  const user = window.localStorage.getItem("user");
   if (user) {
     return JSON.parse(user) as Simplified<User>;
   } else {
@@ -11,8 +12,5 @@ export const retrieveUser = () => {
 };
 
 export const storeUser = (user: Simplified<User> | null) => {
-  return window.sessionStorage.setItem(
-    "user",
-    user ? JSON.stringify(user) : "",
-  );
+  return window.localStorage.setItem("user", user ? JSON.stringify(user) : "");
 };
