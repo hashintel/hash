65d23217015500008c8cf51096e2fc4507f73b26
Ciaran Morinan <c@hash.ai>
2023-10-04 16:14:20 +0100

    add s3 environment variable

--- infra/terraform/hash/main.tf
+++ infra/terraform/hash/main.tf
@@ -247,6 +247,7 @@ module "application" {
   api_image = module.api_ecr
   api_env_vars = concat(var.hash_api_env_vars, [
     { name = "AWS_REGION", secret = false, value = local.region },
+    { name = "AWS_S3_UPLOADS_BUCKET", secret = true, value = sensitive(data.vault_kv_secret_v2.secrets.data["aws_s3_uploads_bucket"]) },
     { name = "SYSTEM_USER_PASSWORD", secret = true, value = sensitive(data.vault_kv_secret_v2.secrets.data["hash_system_user_password"]) },
     { name = "BLOCK_PROTOCOL_API_KEY", secret = true, value = sensitive(data.vault_kv_secret_v2.secrets.data["hash_block_protocol_api_key"]) },
     { name = "KRATOS_API_KEY", secret = true, value = sensitive(data.vault_kv_secret_v2.secrets.data["kratos_api_key"]) },
