724913ba797dedf589b456986b4fbe8cdb5c4cf1
Ben Werner <ben.werner01@gmail.com>
2023-10-10 11:29:17 +0200

    ft: add profile tab with editable bio

---
+++ apps/hash-frontend/src/shared/icons/check-regular-icon.tsx
@@ -0,0 +1,10 @@
+import { SvgIcon, SvgIconProps } from "@mui/material";
+import { FunctionComponent } from "react";
+
+export const CheckRegularIcon: FunctionComponent<SvgIconProps> = (props) => {
+  return (
+    <SvgIcon {...props} viewBox="0 0 448 512" fill="none">
+      <path d="M441 103c9.4 9.4 9.4 24.6 0 33.9L177 401c-9.4 9.4-24.6 9.4-33.9 0L7 265c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l119 119L407 103c9.4-9.4 24.6-9.4 33.9 0z" />
+    </SvgIcon>
+  );
+};
