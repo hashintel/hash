9139b5558423b00a1bcaf8e11215cddfd2ef2382
Ciaran Morinan <c@hash.ai>
2023-10-10 20:27:54 +0100

    remove unused linkedAggregation resolvers

--- apps/hash-api/src/graphql/resolvers/index.ts
+++ apps/hash-api/src/graphql/resolvers/index.ts
@@ -41,11 +41,6 @@ import { updatePageResolver } from "./knowledge/page/update-page";
 import { createUserResolver } from "./knowledge/user/create-user";
 import { isShortnameTakenResolver } from "./knowledge/user/is-shortname-taken";
 import { meResolver } from "./knowledge/user/me";
-import { createLinkedAggregation } from "./linked-aggregation/create-linked-aggregation";
-import { deleteLinkedAggregation } from "./linked-aggregation/delete-linked-aggregation";
-import { getLinkedAggregation } from "./linked-aggregation/get-linked-aggregation";
-import { linkedAggregationResults } from "./linked-aggregation/linked-aggregation-results";
-import { updateLinkedAggregationOperation } from "./linked-aggregation/update-linked-aggregation-operation";
 import { loggedInMiddleware } from "./middlewares/logged-in";
 import { loggedInAndSignedUpMiddleware } from "./middlewares/logged-in-and-signed-up";
 import { loggedInAndSignedUpHashInstanceAdminMiddleware } from "./middlewares/logged-in-and-signed-up-hash-instance-admin";
@@ -72,7 +67,6 @@ export const resolvers = {
   Query: {
     // Logged in and signed up users only,
     getBlockProtocolBlocks: getBlockProtocolBlocksResolver,
-    getLinkedAggregation: loggedInAndSignedUpMiddleware(getLinkedAggregation),
     // Logged in users only
     me: loggedInMiddleware(meResolver),
     // Any user
@@ -104,15 +98,6 @@ export const resolvers = {
 
   Mutation: {
     // Logged in and signed up users only
-    createLinkedAggregation: loggedInAndSignedUpMiddleware(
-      createLinkedAggregation,
-    ),
-    updateLinkedAggregationOperation: loggedInAndSignedUpMiddleware(
-      updateLinkedAggregationOperation,
-    ),
-    deleteLinkedAggregation: loggedInAndSignedUpMiddleware(
-      deleteLinkedAggregation,
-    ),
     updatePageContents: loggedInAndSignedUpMiddleware(updatePageContents),
     requestFileUpload: loggedInAndSignedUpMiddleware(requestFileUpload),
     createFileFromUrl: loggedInAndSignedUpMiddleware(createFileFromUrl),
@@ -160,10 +145,6 @@ export const resolvers = {
   },
 
   JSONObject: JSONObjectResolver,
-
-  LinkedAggregation: {
-    results: linkedAggregationResults,
-  },
 
   // New knowledge field resolvers
   Page: {
