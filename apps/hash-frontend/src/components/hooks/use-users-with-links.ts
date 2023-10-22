1613bf37eee44fad54cab7b1caaeb65daa160371
Ben Werner <ben.werner01@gmail.com>
2023-10-22 20:50:29 +0100

    refactor: add entity type check to user and org queries

--- apps/hash-frontend/src/components/hooks/use-users-with-links.ts
+++ apps/hash-frontend/src/components/hooks/use-users-with-links.ts
@@ -1,4 +1,9 @@
 import { ApolloQueryResult, useQuery } from "@apollo/client";
+import {
+  currentTimeInstantTemporalAxes,
+  generateVersionedUrlMatchingFilter,
+} from "@local/hash-isomorphic-utils/graph-queries";
+import { types } from "@local/hash-isomorphic-utils/ontology-types";
 import { UserProperties } from "@local/hash-isomorphic-utils/system-types/shared";
 import {
   AccountId,
@@ -10,10 +15,10 @@ import { getRoots } from "@local/hash-subgraph/stdlib";
 import { useMemo } from "react";
 
 import {
-  QueryEntitiesQuery,
-  QueryEntitiesQueryVariables,
+  StructuralQueryEntitiesQuery,
+  StructuralQueryEntitiesQueryVariables,
 } from "../../graphql/api-types.gen";
-import { queryEntitiesQuery } from "../../graphql/queries/knowledge/entity.queries";
+import { structuralQueryEntitiesQuery } from "../../graphql/queries/knowledge/entity.queries";
 import { constructUser, User } from "../../lib/user-and-org";
 
 /**
@@ -26,40 +31,57 @@ export const useUsersWithLinks = ({
 }): {
   loading: boolean;
   users?: User[];
-  refetch: () => Promise<ApolloQueryResult<QueryEntitiesQuery>>;
+  refetch: () => Promise<ApolloQueryResult<StructuralQueryEntitiesQuery>>;
 } => {
   const { data, loading, refetch } = useQuery<
-    QueryEntitiesQuery,
-    QueryEntitiesQueryVariables
-  >(queryEntitiesQuery, {
+    StructuralQueryEntitiesQuery,
+    StructuralQueryEntitiesQueryVariables
+  >(structuralQueryEntitiesQuery, {
     variables: {
-      operation: {
-        multiFilter: {
-          filters:
-            userAccountIds?.map((accountId) => ({
-              field: ["metadata", "recordId", "uuid"],
-              operator: "EQUALS",
-              value: accountId,
-            })) ?? [],
-          operator: "OR",
+      includePermissions: false,
+      query: {
+        filter: {
+          all: [
+            ...(userAccountIds
+              ? [
+                  {
+                    any: userAccountIds.map((accountId) => ({
+                      equal: [
+                        { path: ["metadata", "recordId", "uuid"] },
+                        {
+                          parameter: accountId,
+                        },
+                      ],
+                    })),
+                  },
+                ]
+              : []),
+            generateVersionedUrlMatchingFilter(
+              types.entityType.user.entityTypeId,
+              { ignoreParents: true },
+            ),
+          ],
         },
+        graphResolveDepths: {
+          constrainsValuesOn: { outgoing: 0 },
+          constrainsPropertiesOn: { outgoing: 0 },
+          constrainsLinksOn: { outgoing: 0 },
+          constrainsLinkDestinationsOn: { outgoing: 0 },
+          inheritsFrom: { outgoing: 0 },
+          isOfType: { outgoing: 0 },
+          // These depths are chosen to cover the following:
+          // - the user's avatar (user -> [hasLeftEntity incoming 1] hasAvatar [hasRightEntity outgoing 1] -> avatar)
+          hasLeftEntity: { incoming: 1, outgoing: 0 },
+          hasRightEntity: { incoming: 0, outgoing: 1 },
+        },
+        temporalAxes: currentTimeInstantTemporalAxes,
       },
-      constrainsValuesOn: { outgoing: 0 },
-      constrainsPropertiesOn: { outgoing: 0 },
-      constrainsLinksOn: { outgoing: 0 },
-      constrainsLinkDestinationsOn: { outgoing: 0 },
-      inheritsFrom: { outgoing: 0 },
-      isOfType: { outgoing: 0 },
-      // These depths are chosen to cover the following:
-      // - the user's avatar (user -> [hasLeftEntity incoming 1] hasAvatar [hasRightEntity outgoing 1] -> avatar)
-      hasLeftEntity: { incoming: 1, outgoing: 0 },
-      hasRightEntity: { incoming: 0, outgoing: 1 },
     },
     fetchPolicy: "cache-and-network",
     skip: !userAccountIds || !userAccountIds.length,
   });
 
-  const subgraph = data?.queryEntities.subgraph as
+  const subgraph = data?.structuralQueryEntities.subgraph as
     | Subgraph<EntityRootType>
     | undefined;
 
