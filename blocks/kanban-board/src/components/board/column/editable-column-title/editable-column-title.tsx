018846aa2e1275e45e3f3b583ec107cadbaf54bb
Ben Werner <ben.werner01@gmail.com>
2023-10-30 09:57:40 +0000

    fix: eslint

--- blocks/kanban-board/src/components/board/column/editable-column-title/editable-column-title.tsx
+++ blocks/kanban-board/src/components/board/column/editable-column-title/editable-column-title.tsx
@@ -37,6 +37,7 @@ export const EditableColumnTitle = ({
 
   return (
     <input
+      // eslint-disable-next-line jsx-a11y/no-autofocus
       autoFocus
       ref={inputRef}
       className={styles.wrapper}
