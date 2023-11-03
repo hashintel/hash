34935c990afa5518d50ac3e1a4551374b43948f6
Ciaran Morinan <c@hash.ai>
2023-11-03 11:38:29 +0000

    update block depths comments

--- apps/hash-frontend/src/pages/shared/block-collection-contents.ts
+++ apps/hash-frontend/src/pages/shared/block-collection-contents.ts
@@ -22,6 +22,15 @@ import { BlockCollectionContentItem } from "../../graphql/api-types.gen";
  *
  * [BlockCollection] -[1]-> [Block] -[2]-> [Block Entity] -[3]-> [Linked Entity 1] -> [Linked Entity 2]
  *
+ * This also resolves _incoming_ links to the BlockCollection to a depth of 4, to allow for incoming links
+ * around the block entity's linked entities. This may result in fetching so much of the graph that we are
+ * better off splitting the request into two:
+ *
+ * 1. BlockCollection { hasLeftEntity: { incoming: 1 }, hasRightEntity: { outgoing: 1 } }
+ *    - fetches all Blocks in the collection (which have the Block Entities' ids as their rightEntity)
+ * 2. Block Entity[] { hasLeftEntity: { incoming: 2, outgoing: 2 }, hasRightEntity: { incoming: 2, outgoing: 2 } }
+ *    - fetches the entire graph of entities at a depth of 2 around the block entity
+ *
  * Equivalent to providing each block with the graph resolved to a depth of 2 around the block entity.
  * Most blocks will require at least 1 (e.g. a table entity with an attached query), and many 2
  */
