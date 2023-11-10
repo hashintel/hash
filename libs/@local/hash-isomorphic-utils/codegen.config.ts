e3d79ce8b561451764ff9be0df2a8c5ca0368994
Ciaran Morinan <c@hash.ai>
2023-11-10 19:29:20 +0000

    update import

--- libs/@local/hash-isomorphic-utils/codegen.config.ts
+++ libs/@local/hash-isomorphic-utils/codegen.config.ts
@@ -1,5 +1,6 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
-import { scalars } from "@local/hash-isomorphic-utils/scalar-mapping";
+
+import { scalars } from "./src/scalar-mapping";
 
 const config: CodegenConfig = {
   overwrite: true,
