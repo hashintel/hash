966c7be99c447dc939331b2cd7bb173d31aa3f72
Ben Werner <ben.werner01@gmail.com>
2023-11-14 11:14:50 +0000

    chore: regenerate system types

---
+++ libs/@local/hash-isomorphic-utils/src/system-types/blockprotocol/query.ts
@@ -0,0 +1,25 @@
+/**
+ * This file was automatically generated – do not edit it.
+ */
+
+import { Entity } from "@blockprotocol/graph";
+
+/**
+ * An opaque, untyped JSON object
+ */
+export type ObjectDataType = {};
+
+export type Query = Entity<QueryProperties>;
+
+export type QueryOutgoingLinkAndTarget = never;
+
+export type QueryOutgoingLinksByLinkEntityTypeId = {};
+
+export type QueryProperties = {
+  "https://blockprotocol.org/@hash/types/property-type/query/": QueryPropertyValue;
+};
+
+/**
+ * The query for something.
+ */
+export type QueryPropertyValue = ObjectDataType;
---
+++ libs/@local/hash-isomorphic-utils/src/system-types/blockprotocol/shared.ts
@@ -0,0 +1,13 @@
+/**
+ * This file was automatically generated – do not edit it.
+ */
+
+import { Entity } from "@blockprotocol/graph";
+
+export type Link = Entity<LinkProperties>;
+
+export type LinkOutgoingLinkAndTarget = never;
+
+export type LinkOutgoingLinksByLinkEntityTypeId = {};
+
+export type LinkProperties = {};
