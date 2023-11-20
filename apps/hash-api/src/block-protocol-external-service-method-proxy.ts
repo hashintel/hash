cbcd4c8a7ace64b66b3efafdb6f0ed1671d9740e
Ben Werner <ben.werner01@gmail.com>
2023-11-20 12:22:25 +0000

    ft: add `/external-service-method` proxy

---
+++ apps/hash-api/src/block-protocol-external-service-method-proxy.ts
@@ -0,0 +1,31 @@
+import { blockProtocolHubOrigin } from "@local/hash-isomorphic-utils/blocks";
+import { Express } from "express";
+import proxy from "express-http-proxy";
+
+/**
+ * Set up a proxy to the blockprotocol.org proxy for the internal API,
+ * attaching the HASH BLOCK_PROTOCOL_API key.
+ *
+ * @param app - the express app
+ */
+export const setupBlockProtocolExternalServiceMethodProxy = (app: Express) => {
+  app.use(
+    "/api/external-service-method",
+    proxy(blockProtocolHubOrigin, {
+      proxyReqPathResolver: () => "/api/external-service-method",
+      proxyReqOptDecorator: (proxyReqOpts) => {
+        const apiKey = process.env.BLOCK_PROTOCOL_API_KEY;
+
+        if (apiKey) {
+          // eslint-disable-next-line no-param-reassign
+          proxyReqOpts.headers = {
+            ...proxyReqOpts.headers,
+            "x-api-key": apiKey,
+          };
+        }
+
+        return proxyReqOpts;
+      },
+    }),
+  );
+};
