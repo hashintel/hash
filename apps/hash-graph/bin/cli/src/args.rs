caa98ac1624c1a2dc58a48c7475f77a5b2165f3a
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2023-11-22 11:20:16 +0100

    H-1350: Set a default variable for the sentry environment in `graph` (#3609)
    Signed-off-by: Tim Diekmann <post@timdiekmann.de>

--- apps/hash-graph/bin/cli/src/args.rs
+++ apps/hash-graph/bin/cli/src/args.rs
@@ -1,11 +1,23 @@
+use std::fmt;
+
 use clap::{Parser, ValueEnum};
 
 use crate::{parser::OptionalSentryDsnParser, subcommand::Subcommand};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, ValueEnum)]
 pub enum SentryEnvironment {
+    #[default]
     Development,
     Production,
+}
+
+impl fmt::Display for SentryEnvironment {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Development => fmt.write_str("development"),
+            Self::Production => fmt.write_str("production"),
+        }
+    }
 }
 
 /// Arguments passed to the program.
@@ -17,8 +29,12 @@ pub struct Args {
     #[arg(long, env = "HASH_GRAPH_SENTRY_DSN", value_parser = OptionalSentryDsnParser, default_value = "")]
     pub sentry_dsn: core::option::Option<sentry::types::Dsn>,
 
-    #[arg(long, env = "HASH_GRAPH_SENTRY_ENVIRONMENT")]
-    pub sentry_environment: Option<SentryEnvironment>,
+    #[arg(
+        long,
+        env = "HASH_GRAPH_SENTRY_ENVIRONMENT",
+        default_value_t = SentryEnvironment::default(),
+    )]
+    pub sentry_environment: SentryEnvironment,
 
     /// Specify a subcommand to run.
     #[command(subcommand)]
