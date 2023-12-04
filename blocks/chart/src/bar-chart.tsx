548a65274b7295ba4bcbec6cb172dd1b67766088
Ben Werner <ben.werner01@gmail.com>
2023-12-04 19:06:54 +0100

    refactor: bar chart population

--- blocks/chart/src/bar-chart.tsx
+++ blocks/chart/src/bar-chart.tsx
@@ -80,37 +80,34 @@ export const BarChart: FunctionComponent<{
       const { entityTypeId, labelPropertyTypeId, direction, linkEntityTypeId } =
         definition;
 
-      const numberOfLinkedEntitiesByEntity = queryResultsByType[entityTypeId]
-        ?.slice(0, 5)
-        .reduce<Record<string, number>>((prev, entity) => {
-          const entityId = entity.metadata.recordId.entityId;
+      const dataPoints = queryResultsByType[entityTypeId]?.reduce<
+        { label: string; value: number }[]
+      >((prev, entity) => {
+        const entityId = entity.metadata.recordId.entityId;
 
-          const links =
-            direction === "outgoing"
-              ? /** @todo: fix `getIncomingLinksForEntity` in the BP temporal package when passing non-temporal subgraphs */
-                getOutgoingLinkAndTargetEntities(queryResult, entityId).map(
-                  ({ linkEntity }) => linkEntity,
-                )
-              : getIncomingLinksForEntity(queryResult, entityId);
+        const links =
+          direction === "outgoing"
+            ? /** @todo: fix `getIncomingLinksForEntity` in the BP temporal package when passing non-temporal subgraphs */
+              getOutgoingLinkAndTargetEntities(queryResult, entityId).map(
+                ({ linkEntity }) => linkEntity,
+              )
+            : getIncomingLinksForEntity(queryResult, entityId);
 
-          const matchingLinks = links.filter(
-            ({ metadata }) => metadata.entityTypeId === linkEntityTypeId,
-          );
+        const matchingLinks = links.filter(
+          ({ metadata }) => metadata.entityTypeId === linkEntityTypeId,
+        );
 
-          const label = String(
-            entity.properties[extractBaseUrl(labelPropertyTypeId)] ?? "Unknown",
-          );
+        const label = String(
+          entity.properties[extractBaseUrl(labelPropertyTypeId)] ?? "Unknown",
+        );
 
-          return {
-            ...prev,
-            [label]: matchingLinks.length,
-          };
-        }, {});
+        return [...prev, { label, value: matchingLinks.length }];
+      }, []);
 
       return {
         xAxis: {
           type: "category",
-          data: Object.keys(numberOfLinkedEntitiesByEntity ?? {}),
+          data: dataPoints?.map(({ label }) => label) ?? [],
           name: definition.xAxisLabel,
           nameLocation: "middle",
           nameGap: 25,
@@ -122,9 +119,7 @@ export const BarChart: FunctionComponent<{
           nameGap: 25,
         },
         series: {
-          data: Object.values(numberOfLinkedEntitiesByEntity ?? {}).map(
-            (numberOfLinks) => numberOfLinks,
-          ),
+          data: dataPoints?.map(({ value }) => value) ?? [],
           type: "bar",
         },
       };
