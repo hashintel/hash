f7aa20f493dfb3be318611454ca777e7184e62e1
Ben Werner <ben.werner01@gmail.com>
2023-12-05 18:41:00 +0100

    ft: add preview button

--- apps/hash-frontend/src/pages/drafts.page/draft-entity.tsx
+++ apps/hash-frontend/src/pages/drafts.page/draft-entity.tsx
@@ -1,5 +1,5 @@
 import { useMutation } from "@apollo/client";
-import { CloseIcon } from "@hashintel/design-system";
+import { CaretDownSolidIcon, CloseIcon } from "@hashintel/design-system";
 import { generateEntityLabel } from "@local/hash-isomorphic-utils/generate-entity-label";
 import {
   Entity,
@@ -8,7 +8,7 @@ import {
   Subgraph,
 } from "@local/hash-subgraph";
 import { Box, buttonClasses, Typography } from "@mui/material";
-import { FunctionComponent, useCallback, useMemo } from "react";
+import { FunctionComponent, useCallback, useMemo, useState } from "react";
 
 import { useGetOwnerForEntity } from "../../components/hooks/use-get-owner-for-entity";
 import {
@@ -35,6 +35,8 @@ export const DraftEntity: FunctionComponent<{
   entity: Entity;
 }> = ({ entity, subgraph }) => {
   const getOwnerForEntity = useGetOwnerForEntity();
+
+  const [displayProperties, setDisplayProperties] = useState<boolean>(false);
 
   const href = useMemo(() => {
     const { shortname } = getOwnerForEntity(entity);
@@ -167,6 +169,40 @@ export const DraftEntity: FunctionComponent<{
         <Box display="flex" alignItems="center" columnGap={2}>
           <DraftEntityType entity={entity} subgraph={subgraph} />
           <DraftEntityViewers entity={entity} />
+          <Button
+            size="xs"
+            variant="tertiary_quiet"
+            endIcon={<CaretDownSolidIcon />}
+            onClick={() => setDisplayProperties(!displayProperties)}
+            sx={{
+              padding: 0,
+              minHeight: "unset",
+              "&:hover": {
+                background: "transparent",
+                [`.${buttonClasses.endIcon} svg`]: {
+                  color: ({ palette }) => palette.gray[80],
+                },
+              },
+              textTransform: "uppercase",
+              fontSize: 11,
+              fontWeight: 600,
+              color: ({ palette }) =>
+                displayProperties ? palette.common.black : palette.gray[50],
+              [`.${buttonClasses.endIcon}`]: {
+                marginLeft: 0.25,
+                marginTop: -0.25,
+                svg: {
+                  color: ({ palette }) =>
+                    displayProperties ? palette.common.black : palette.gray[50],
+                  transition: ({ transitions }) =>
+                    transitions.create(["transform", "color"]),
+                  transform: `rotate(${displayProperties ? 0 : -90}deg)`,
+                },
+              },
+            }}
+          >
+            Preview
+          </Button>
         </Box>
         <DraftEntityProvenance entity={entity} />
       </Box>
