86f329413e510afc893e24e0aca42261ddfbe95b
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2023-12-07 17:49:37 +0100

    H-1331: add ability to count the incoming/outgoing links of an entity in a bar graph (#3670)

---
+++ blocks/chart/src/edit-chart-definition/util.ts
@@ -0,0 +1,44 @@
+import { EntityType, PropertyType, Subgraph } from "@blockprotocol/graph";
+import {
+  getEntityTypeById,
+  getPropertyTypeById,
+} from "@blockprotocol/graph/stdlib";
+
+export const getEntityTypePropertyTypes = (
+  subgraph: Subgraph,
+  entityType: EntityType,
+): PropertyType[] => {
+  const propertyTypeIds = Object.values(entityType.properties).map((value) =>
+    "$ref" in value ? value.$ref : value.items.$ref,
+  );
+
+  const propertyTypes = propertyTypeIds.map((propertyTypeId) => {
+    const propertyType = getPropertyTypeById(subgraph, propertyTypeId)?.schema;
+
+    if (!propertyType) {
+      throw new Error(
+        `Could not get property type from subgraph: ${propertyTypeId}`,
+      );
+    }
+
+    return propertyType;
+  });
+
+  return [
+    ...propertyTypes,
+    ...(entityType.allOf
+      ?.map(({ $ref }) => {
+        const inheritsFromEntityType = getEntityTypeById(subgraph, $ref)
+          ?.schema;
+
+        if (!inheritsFromEntityType) {
+          throw new Error(
+            `Could not get inherited entity type from subgraph: ${$ref}`,
+          );
+        }
+
+        return getEntityTypePropertyTypes(subgraph, inheritsFromEntityType);
+      })
+      .flat() ?? []),
+  ];
+};
--- blocks/chart/src/types/chart-definition.ts
+++ blocks/chart/src/types/chart-definition.ts
@@ -1,6 +1,16 @@
 import { VersionedUrl } from "@blockprotocol/graph";
 
-export type BarChartDefinitionVariant = {
+export type BarChartCountLinkedEntitiesVariant = {
+  variant: "count-links";
+  entityTypeId: VersionedUrl;
+  labelPropertyTypeId: VersionedUrl;
+  direction: "incoming" | "outgoing";
+  linkEntityTypeId: VersionedUrl;
+  xAxisLabel?: string;
+  yAxisLabel?: string;
+};
+
+export type BarChartGroupByPropertyVariant = {
   variant: "group-by-property";
   entityTypeId: VersionedUrl;
   groupByPropertyTypeId: VersionedUrl;
@@ -8,7 +18,17 @@ export type BarChartDefinitionVariant = {
   yAxisLabel?: string;
 };
 
+export type BarChartDefinitionVariant =
+  | BarChartCountLinkedEntitiesVariant
+  | BarChartGroupByPropertyVariant;
+
 export type GraphChartDefinitionVariant = {
+  /**
+   * Without this TS can't infer between the bar chart and graph chart variants
+   *
+   * @todo: figure out a better approach for structuring and typing the chart definition
+   */
+  variant: "default";
   incomingLinksDepth?: number;
   outgoingLinksDepth?: number;
 };
