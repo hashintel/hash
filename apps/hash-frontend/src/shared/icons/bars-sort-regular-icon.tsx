953519aba56392a8ab28e29048980fa88023166c
Ben Werner <ben.werner01@gmail.com>
2023-12-08 15:01:57 +0100

    chore: remove redundant `opacity-"1"` from SVGs

--- apps/hash-frontend/src/shared/icons/bars-sort-regular-icon.tsx
+++ apps/hash-frontend/src/shared/icons/bars-sort-regular-icon.tsx
@@ -5,7 +5,6 @@ export const BarsSortRegularIcon: FunctionComponent<SvgIconProps> = (props) => {
   return (
     <SvgIcon {...props} height="16" width="14" viewBox="0 0 448 512">
       <path
-        opacity="1"
         fill="currentColor"
         d="M0 88C0 74.7 10.7 64 24 64H424c13.3 0 24 10.7 24 24s-10.7 24-24 24H24C10.7 112 0 101.3 0 88zM0 248c0-13.3 10.7-24 24-24H296c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24zM192 408c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24s10.7-24 24-24H168c13.3 0 24 10.7 24 24z"
       />
--- apps/hash-frontend/src/shared/icons/calendar-light-icon.tsx
+++ apps/hash-frontend/src/shared/icons/calendar-light-icon.tsx
@@ -5,7 +5,6 @@ export const CalendarLightIcon: FunctionComponent<SvgIconProps> = (props) => {
   return (
     <SvgIcon {...props} height="16" width="14" viewBox="0 0 448 512">
       <path
-        opacity="1"
         fill="currentColor"
         d="M112 0c8.8 0 16 7.2 16 16V64H320V16c0-8.8 7.2-16 16-16s16 7.2 16 16V64h32c35.3 0 64 28.7 64 64v32 32V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192 160 128C0 92.7 28.7 64 64 64H96V16c0-8.8 7.2-16 16-16zM416 192H32V448c0 17.7 14.3 32 32 32H384c17.7 0 32-14.3 32-32V192zM384 96H64c-17.7 0-32 14.3-32 32v32H416V128c0-17.7-14.3-32-32-32z"
       />
