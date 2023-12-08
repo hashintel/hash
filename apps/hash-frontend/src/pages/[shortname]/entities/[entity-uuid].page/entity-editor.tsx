1e0b6a1433a79a8ede133c80b1c625a33dc9c18d
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2023-12-08 18:24:19 +0100

    H-1498: Show source and destination on link page (#3699)

--- apps/hash-frontend/src/pages/[shortname]/entities/[entity-uuid].page/entity-editor.tsx
+++ apps/hash-frontend/src/pages/[shortname]/entities/[entity-uuid].page/entity-editor.tsx
@@ -1,8 +1,12 @@
 import { Entity, EntityRootType, Subgraph } from "@local/hash-subgraph";
+import { getRoots } from "@local/hash-subgraph/stdlib";
 import { Box } from "@mui/material";
+import { useMemo } from "react";
 
+import { useEntityTypesContextRequired } from "../../../../shared/entity-types-context/hooks/use-entity-types-context-required";
 import { EntityEditorContextProvider } from "./entity-editor/entity-editor-context";
 import { FilePreviewSection } from "./entity-editor/file-preview-section";
+import { LinkSection } from "./entity-editor/link-section";
 import { LinksSection } from "./entity-editor/links-section";
 import { PropertiesSection } from "./entity-editor/properties-section";
 import { TypesSection } from "./entity-editor/types-section";
@@ -17,16 +21,37 @@ export interface EntityEditorProps extends DraftLinkState {
 }
 
 export const EntityEditor = (props: EntityEditorProps) => {
+  const { entitySubgraph } = props;
+
+  const { isSpecialEntityTypeLookup } = useEntityTypesContextRequired();
+
+  const entity = useMemo(() => {
+    const roots = getRoots(entitySubgraph);
+
+    const [rootEntity] = roots;
+
+    if (!rootEntity) {
+      throw new Error("No root entity found in entity editor subgraph");
+    }
+
+    return rootEntity;
+  }, [entitySubgraph]);
+
+  const isLinkEntity = useMemo(
+    () => isSpecialEntityTypeLookup?.[entity.metadata.entityTypeId]?.isLink,
+    [entity, isSpecialEntityTypeLookup],
+  );
+
   return (
     <EntityEditorContextProvider {...props}>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 6.5 }}>
-        <TypesSection />
+        {isLinkEntity ? <LinkSection /> : <TypesSection />}
 
         <FilePreviewSection />
 
         <PropertiesSection />
 
-        <LinksSection />
+        {isLinkEntity ? null : <LinksSection />}
 
         {/* <PeersSection /> */}
       </Box>
