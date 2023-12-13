1bd0837dd374f924e60d9b9df901c811fc91136d
Ciaran Morinan <c@hash.ai>
2023-12-13 08:40:09 +0000

    add default plugin auto inference rules

--- apps/plugin-browser/src/pages/popup/popup-contents/action-center.tsx
+++ apps/plugin-browser/src/pages/popup/popup-contents/action-center.tsx
@@ -18,6 +18,7 @@ import {
 } from "../../shared/style-values";
 import { useLocalStorage } from "../../shared/use-local-storage";
 import { Automated } from "./action-center/automated";
+import { defaultRules } from "./action-center/default-rules";
 import { Log } from "./action-center/log";
 import { OneOff } from "./action-center/one-off";
 import { WandMagicSparklesIcon } from "./action-center/wand-magic-sparkles-icon";
@@ -71,7 +72,7 @@ export const ActionCenter = ({
       enabled: false,
       model: "gpt-4-turbo",
       ownedById: user.webOwnedById,
-      rules: [],
+      rules: defaultRules,
     });
 
   const [inferenceRequests] = useLocalStorage("inferenceRequests", []);
