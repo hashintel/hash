abd1d1c88fe16810665b3c022f8543be41395f88
Ben Werner <ben.werner01@gmail.com>
2024-01-04 15:02:01 +0000

    ft: add checkbox indeterminate icon, and fix other inconsistencies with designs

--- libs/@hashintel/design-system/src/theme/components/inputs/mui-checkbox-theme-options/checkbox-indeterminate-icon.tsx
+++ libs/@hashintel/design-system/src/theme/components/inputs/mui-checkbox-theme-options/checkbox-indeterminate-icon.tsx
@@ -1,7 +1,7 @@
 import { SvgIcon, SvgIconProps } from "@mui/material";
 import { FunctionComponent } from "react";
 
-export const CheckboxBlankIcon: FunctionComponent<SvgIconProps> = ({
+export const CheckboxIndeterminateIcon: FunctionComponent<SvgIconProps> = ({
   sx = [],
   ...otherProps
 }) => {
@@ -22,6 +22,10 @@ export const CheckboxBlankIcon: FunctionComponent<SvgIconProps> = ({
     >
       <rect x="0.5" y="0.5" width="17" height="17" rx="3.5" fill="white" />
       <rect x="0.5" y="0.5" width="17" height="17" rx="3.5" stroke="#DDE7F0" />
+      <path
+        d="M5 8.75C5 8.49609 5.15625 8.28125 5.375 8.28125H12.625C12.8281 8.28125 13 8.49609 13 8.75C13 9.02344 12.8281 9.21875 12.625 9.21875H5.375C5.15625 9.21875 5 9.02344 5 8.75Z"
+        fill="#0775E3"
+      />
     </SvgIcon>
   );
 };
