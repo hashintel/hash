dffa61069cdcf86730df12e0800b3c99ce4ab3e7
Ciaran Morinan <c@hash.ai>
2024-01-04 09:30:22 +0000

    remove 'build' as a dependency of system tests + dev command

--- tests/hash-playwright/tests/shared/reset-db.ts
+++ tests/hash-playwright/tests/shared/reset-db.ts
@@ -1,9 +1,3 @@
-import { monorepoRootDir } from "@local/hash-backend-utils/environment";
-import execa from "execa";
-
-// @todo reimplement this
 export const resetDb = async () => {
-  await execa("yarn", ["seed-data"], {
-    cwd: monorepoRootDir,
-  });
+  // @todo reimplement this
 };
