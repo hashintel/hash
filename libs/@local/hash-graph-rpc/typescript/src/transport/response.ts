c1485465b86a9ee04ca0f186d94338a70ca6a5ef
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-01-08 13:19:03 +0100

    feat: reading

--- libs/@local/hash-graph-rpc/typescript/src/transport/response.ts
+++ libs/@local/hash-graph-rpc/typescript/src/transport/response.ts
@@ -1,4 +1,5 @@
 import { PayloadSize, TransportVersion } from "./request";
+import { Reader } from "./reader";
 
 export enum ResponseError {
   DeadlineExceeded,
@@ -35,4 +36,37 @@ export type ResponseBody =
 export interface Response {
   header: ResponseHeader;
   body: ResponseBody;
+}
+
+function readFlags(reader: Reader): ResponseFlags | null {
+  const flags = reader.readBytes(1);
+  if (flags === null) {
+    return null;
+  }
+
+  const endOfStream = (flags[1] & 0b0000_0010) === 0b0000_0010;
+  const streaming = (flags[1] & 0b0000_0001) === 0b0000_0001;
+
+  return {
+    endOfStream,
+    streaming,
+  };
+}
+
+export function readResponse(buffer: Uint8Array): Response | null {
+  const reader = new Reader(buffer);
+
+  const version = reader.readByte();
+  if (version === null) {
+    return null;
+  }
+
+  // TODO: validate version
+
+  const flags = readFlags(reader);
+  if (flags === null) {
+    return null;
+  }
+
+  const status = reader.readByte();
 }
