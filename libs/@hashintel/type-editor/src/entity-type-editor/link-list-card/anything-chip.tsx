f81aa34a754744db67ff2ae418b1bb4a60cfbee0
Ben Werner <42802102+benwerner01@users.noreply.github.com>
2024-01-15 17:28:52 +0000

    H-590, H-1753, H-1678, H-1679: Style cleanup around the expected link types table on the entity types page + other minor tweaks (#3860)

--- libs/@hashintel/type-editor/src/entity-type-editor/link-list-card/anything-chip.tsx
+++ libs/@hashintel/type-editor/src/entity-type-editor/link-list-card/anything-chip.tsx
@@ -5,6 +5,7 @@ import { TypeChipLabel } from "./type-chip-label";
 export const AnythingChip = () => (
   <Chip
     color="blue"
+    sx={{ m: 0.25 }}
     variant="outlined"
     label={<TypeChipLabel>Anything</TypeChipLabel>}
   />
