14c24a3f9e700ad0c46a4b2f42cc2353d08e11d0
Ciaran Morinan <c@hash.ai>
2024-01-16 19:00:42 +0000

    filter out link entity types from inference where they are unusable given other types requested

--- apps/hash-ai-worker-ts/src/activities/infer-entities.ts
+++ apps/hash-ai-worker-ts/src/activities/infer-entities.ts
@@ -1,4 +1,5 @@
 import type { VersionedUrl } from "@blockprotocol/type-system";
+import { typedEntries } from "@local/advanced-types/typed-entries";
 import {
   getHashInstanceAdminAccountGroupId,
   isUserHashInstanceAdmin,
@@ -280,6 +281,47 @@ export const inferEntities = async ({
   }
 
   const model = modelAliasToSpecificModel[modelAlias];
+
+  const unusableTypeIds = entityTypeIds.filter((entityTypeId) => {
+    const details = entityTypes[entityTypeId];
+    if (!details) {
+      return true;
+    }
+
+    const { isLink } = details;
+
+    if (!isLink) {
+      /**
+       * If it's not a link we assume it can be satisfied.
+       * @todo consider checking if it has required links (minItems > 1) which cannot be satisfied
+       */
+      return false;
+    }
+
+    /**
+     * If this is a link type, only search for it if it can be used, given the other types of entities being sought
+     */
+    const linkCanBeSatisfied = Object.values(entityTypes).some((option) =>
+      typedEntries(option.schema.links ?? {}).some(
+        ([linkTypeId, targetSchema]) =>
+          // It must exist as a potential link on at least one of the other entity types being sought...
+          linkTypeId === entityTypeId &&
+          // ...and that link must not have destination constraints which exclude the link type
+          !(
+            "oneOf" in targetSchema.items &&
+            !targetSchema.items.oneOf.some(
+              (targetOption) => entityTypes[targetOption.$ref],
+            )
+          ),
+      ),
+    );
+
+    return !linkCanBeSatisfied;
+  });
+
+  for (const unusableTypeId of unusableTypeIds) {
+    delete entityTypes[unusableTypeId];
+  }
 
   const inferenceState: InferenceState = {
     iterationCount: 1,
