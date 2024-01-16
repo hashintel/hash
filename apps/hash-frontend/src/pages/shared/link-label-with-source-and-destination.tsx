83905cd8ce180d3d8c192ca99421bc1c371691e6
Ben Werner <ben.werner01@gmail.com>
2024-01-16 11:46:35 +0000

    fix: label with source and destination from crashing if source/destination can't be fetched

--- apps/hash-frontend/src/pages/shared/link-label-with-source-and-destination.tsx
+++ apps/hash-frontend/src/pages/shared/link-label-with-source-and-destination.tsx
@@ -1,5 +1,8 @@
 import { extractVersion } from "@blockprotocol/type-system";
-import { AsteriskRegularIcon } from "@hashintel/design-system";
+import {
+  AsteriskRegularIcon,
+  EyeSlashIconRegular,
+} from "@hashintel/design-system";
 import { generateEntityLabel } from "@local/hash-isomorphic-utils/generate-entity-label";
 import {
   Entity,
@@ -37,7 +40,7 @@ const ContentTypography = styled(Typography)(({ theme }) => ({
 }));
 
 const LeftOrRightEntity: FunctionComponent<{
-  entity: Entity;
+  entity?: Entity;
   subgraph: Subgraph<EntityRootType>;
   openInNew?: boolean;
   endAdornment?: ReactNode;
@@ -45,18 +48,25 @@ const LeftOrRightEntity: FunctionComponent<{
   sx?: BoxProps["sx"];
 }> = ({ subgraph, entity, endAdornment, sx, label, openInNew }) => {
   const entityLabel = useMemo(
-    () => generateEntityLabel(subgraph, entity),
+    () => (entity ? generateEntityLabel(subgraph, entity) : "Hidden entity"),
     [subgraph, entity],
   );
 
   const entityType = useMemo(
-    () => getEntityTypeById(subgraph, entity.metadata.entityTypeId)!,
+    () =>
+      entity
+        ? getEntityTypeById(subgraph, entity.metadata.entityTypeId)
+        : undefined,
     [subgraph, entity],
   );
 
   const getOwnerForEntity = useGetOwnerForEntity();
 
   const href = useMemo(() => {
+    if (!entity || entity.metadata.recordId.entityId.includes("draft")) {
+      return undefined;
+    }
+
     const { shortname: entityNamespace } = getOwnerForEntity(entity);
 
     return `/@${entityNamespace}/entities/${extractEntityUuidFromEntityId(
@@ -64,17 +74,55 @@ const LeftOrRightEntity: FunctionComponent<{
     )}`;
   }, [getOwnerForEntity, entity]);
 
+  const content = (
+    <Box
+      display="flex"
+      alignItems="center"
+      columnGap={1}
+      paddingX={1.5}
+      paddingY={0.75}
+      sx={[
+        {
+          borderRadius: "6px",
+          borderColor: ({ palette }) => palette.gray[30],
+          borderWidth: 1,
+          borderStyle: "solid",
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          svg: {
+            color: ({ palette }) => palette.gray[50],
+            fontSize: 16,
+            transition: ({ transitions }) => transitions.create("color"),
+          },
+        }}
+      >
+        {entityType ? (
+          entityType.metadata.icon ?? <AsteriskRegularIcon />
+        ) : (
+          <EyeSlashIconRegular />
+        )}
+      </Box>
+      <ContentTypography>{entityLabel}</ContentTypography>
+      {endAdornment}
+    </Box>
+  );
+
   return (
     <Box
       display="flex"
       flexDirection="column"
       sx={{
         minWidth: 0,
-        "&:first-of-type > a > div": {
+        "&:first-of-type > a > div, &:first-of-type > div": {
           borderTopRightRadius: 0,
           borderBottomRightRadius: 0,
         },
-        "&:last-of-type > a > div": {
+        "&:last-of-type > a > div, &:last-of-type > div": {
           borderTopLeftRadius: 0,
           borderBottomLeftRadius: 0,
         },
@@ -93,49 +141,24 @@ const LeftOrRightEntity: FunctionComponent<{
           {label}
         </Typography>
       ) : null}
-      <Link
-        openInNew={openInNew}
-        href={href}
-        noLinkStyle
-        sx={{
-          "&:hover": {
-            [`.${typographyClasses.root}, svg`]: {
-              color: ({ palette }) => palette.blue[70],
+      {href ? (
+        <Link
+          openInNew={openInNew}
+          href={href}
+          noLinkStyle
+          sx={{
+            "&:hover": {
+              [`.${typographyClasses.root}, svg`]: {
+                color: ({ palette }) => palette.blue[70],
+              },
             },
-          },
-        }}
-      >
-        <Box
-          display="flex"
-          alignItems="center"
-          columnGap={1}
-          paddingX={1.5}
-          paddingY={0.75}
-          sx={[
-            {
-              borderRadius: "6px",
-              borderColor: ({ palette }) => palette.gray[30],
-              borderWidth: 1,
-              borderStyle: "solid",
-            },
-            ...(Array.isArray(sx) ? sx : [sx]),
-          ]}
+          }}
         >
-          <Box display="flex">
-            {entityType.metadata.icon ?? (
-              <AsteriskRegularIcon
-                sx={{
-                  color: ({ palette }) => palette.gray[50],
-                  fontSize: 16,
-                  transition: ({ transitions }) => transitions.create("color"),
-                }}
-              />
-            )}
-          </Box>
-          <ContentTypography>{entityLabel}</ContentTypography>
-          {endAdornment}
-        </Box>
-      </Link>
+          {content}
+        </Link>
+      ) : (
+        content
+      )}
     </Box>
   );
 };
@@ -167,14 +190,11 @@ export const LinkLabelWithSourceAndDestination: FunctionComponent<{
         subgraph,
         linkEntity.metadata.entityTypeId,
       )!,
-      leftEntity: getEntityRevision(
-        subgraph,
-        linkEntity.linkData.leftEntityId,
-      )!,
+      leftEntity: getEntityRevision(subgraph, linkEntity.linkData.leftEntityId),
       rightEntity: getEntityRevision(
         subgraph,
         linkEntity.linkData.rightEntityId,
-      )!,
+      ),
     };
   }, [linkEntity, subgraph]);
 
