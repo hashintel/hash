0333b83702ee16f68eebc2f70a7e90da9c8d5b5e
Ben Werner <ben.werner01@gmail.com>
2024-01-18 15:24:21 +0000

    chore: improve type definition

--- apps/hash-frontend/src/pages/drafts.page/draft-entities/draft-entities-filters/types.ts
+++ apps/hash-frontend/src/pages/drafts.page/draft-entities/draft-entities-filters/types.ts
@@ -1,27 +1,29 @@
 import { ReactNode } from "react";
 
-type MultipleChoiceFilterSectionDefinition<Value = any> = {
+type FilterOption<Value = string> = {
+  icon?: ReactNode;
+  label: ReactNode;
+  value: Value;
+  /**
+   * The number of entities that match this filter option, with
+   * the filter state applied from all other filter sections.
+   */
+  count?: number;
+};
+
+type MultipleChoiceFilterSectionDefinition<Value = string> = {
   heading: string;
   kind: "multiple-choice";
-  options: {
-    icon?: ReactNode;
-    label: ReactNode;
-    value: Value;
+  options: (FilterOption<Value> & {
     checked: boolean;
-    count?: number;
-  }[];
+  })[];
   onChange: (values: Value[]) => void;
 };
 
 type SingleChoiceFilterSectionDefinition<Value = string> = {
   heading: string;
   kind: "single-choice";
-  options: {
-    icon?: ReactNode;
-    label: ReactNode;
-    value: Value;
-    count?: number;
-  }[];
+  options: FilterOption<Value>[];
   value: Value;
   onChange: (value: Value) => void;
 };
