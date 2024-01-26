b8ba09a95b5084bc4f81f90c3a5d5b896b315a55
Ben Werner <ben.werner01@gmail.com>
2024-01-26 11:30:09 +0000

    ft: improve stringifying the property value

--- apps/hash-frontend/src/pages/shared/entities-table/use-entities-table-generate-csv-file.ts
+++ apps/hash-frontend/src/pages/shared/entities-table/use-entities-table-generate-csv-file.ts
@@ -9,6 +9,7 @@ import {
 } from "@local/hash-isomorphic-utils/graph-queries";
 import {
   Entity,
+  EntityPropertyValue,
   EntityRootType,
   EntityTypeWithMetadata,
   extractEntityUuidFromEntityId,
@@ -32,6 +33,40 @@ import { structuralQueryEntitiesQuery } from "../../../graphql/queries/knowledge
 import { MinimalUser } from "../../../lib/user-and-org";
 import { GenerateCsvFileFunction } from "../../../shared/table-header/export-to-csv-button";
 import { TypeEntitiesRow } from "./use-entities-table";
+
+/**
+ * Stringify an arbitrary property value to be as human readable as possible, where:
+ *  - `null` values are treated as empty strings
+ *  - `boolean` values are converted to "True" or "False"
+ *  - arrays are converted to comma separated strings
+ *  - objects are stringified
+ */
+const stringifyPropertyValue = (propertyValue: EntityPropertyValue): string => {
+  if (typeof propertyValue === "string") {
+    // If the property value is a string, we can return it directly
+    return propertyValue;
+  } else if (typeof propertyValue === "object") {
+    if (propertyValue === null) {
+      // If the property value is null, we treat this as an empty string
+      return "";
+    } else if (Array.isArray(propertyValue)) {
+      /**
+       * If the property value is an array, we stringify each element and
+       * join them with a comma surrounding them with square brackets
+       */
+      return `[${propertyValue.map(stringifyPropertyValue).join(", ")}]`;
+    } else {
+      // If the property value is an object, we stringify it
+      return JSON.stringify(propertyValue);
+    }
+  } else if (typeof propertyValue === "boolean") {
+    // If the property value is a boolean, return "True" or "False"
+    return propertyValue ? "True" : "False";
+  }
+
+  // Otherwise directly stringify the property value (for example if it's a number)
+  return String(propertyValue);
+};
 
 export const useEntitiesTableGenerateCsvFile = (props: {
   currentlyDisplayedRowsRef: MutableRefObject<TypeEntitiesRow[] | null>;
@@ -206,19 +241,12 @@ export const useEntitiesTableGenerateCsvFile = (props: {
         const { entity } = row;
 
         const propertyValues = propertyColumns.map((propertyType) => {
-          /** @todo: stringify this better */
           const propertyValue =
             entity.properties[extractBaseUrl(propertyType.$id)];
 
-          if (typeof propertyValue === "string") {
-            return propertyValue;
-          } else if (typeof propertyValue === "object") {
-            return JSON.stringify(propertyValue);
-          } else if (propertyValue !== undefined) {
-            return String(propertyValue);
-          }
-
-          return "";
+          return typeof propertyValue === "undefined"
+            ? ""
+            : stringifyPropertyValue(propertyValue);
         });
 
         const outgoingLinks = outgoingLinksWithRightEntities.filter(
