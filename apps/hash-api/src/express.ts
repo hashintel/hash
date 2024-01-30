c0ccaf2b586d97d0ea15eb142d6c16089c0aa95c
Ben Werner <ben.werner01@gmail.com>
2024-01-30 16:13:19 +0000

    fix: `express` file type

--- apps/hash-api/src/express.ts
+++ apps/hash-api/src/express.ts
@@ -2,8 +2,10 @@ import { Session } from "@ory/client";
 
 import { ImpureGraphContext } from "./graph/context-types";
 import { User } from "./graph/knowledge/system-types/user";
+import { VaultClient } from "./vault";
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       context: ImpureGraphContext<true, true> & {
