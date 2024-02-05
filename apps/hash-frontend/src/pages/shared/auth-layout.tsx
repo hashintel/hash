fbc9bfe6654d74725a8e6123bb6e4893aafe8a7c
Ben Werner <ben.werner01@gmail.com>
2024-02-05 16:46:47 +0000

    refactor: update auth layout design

--- apps/hash-frontend/src/pages/shared/auth-layout.tsx
+++ apps/hash-frontend/src/pages/shared/auth-layout.tsx
@@ -1,120 +1,43 @@
-import { Box } from "@mui/material";
-import { FunctionComponent, ReactNode } from "react";
+import { Box, Container } from "@mui/material";
+import { FunctionComponent, PropsWithChildren, ReactNode } from "react";
 
-import bgPattern from "../../assets/images/auth-bg-pattern.png";
-import { HashIcon, LogoIcon } from "../../shared/icons";
+import { HashIcon } from "../../shared/icons/hash-icon";
 
-export type AuthLayoutProps = {
-  children: ReactNode;
-  onClose?: () => void;
-  showTopLogo?: boolean;
-  loading?: boolean;
-};
-
-export const AuthLayout: FunctionComponent<AuthLayoutProps> = ({
-  children,
-  onClose,
-  showTopLogo,
-  loading,
-}) => {
-  if (loading) {
-    return (
-      <div
-        style={{
-          alignItems: "center",
-          bottom: "0",
-          display: "flex",
-          justifyContent: "center",
-          left: "0",
-          position: "fixed",
-          right: "0",
-          top: "0",
-          zIndex: "10",
-        }}
-      >
-        <HashIcon
-          style={{ marginLeft: "0.25rem", width: "12rem", height: "12rem" }}
-        />
-      </div>
-    );
+export const AuthLayout: FunctionComponent<
+  PropsWithChildren & {
+    headerEndAdornment?: ReactNode;
   }
-
+> = ({ children, headerEndAdornment }) => {
   return (
-    <div
-      style={{
-        backgroundColor: "#ffffff",
-        bottom: "0",
-        left: "0",
-        position: "fixed",
-        right: "0",
-        top: "0",
+    <Box
+      sx={{
+        background: ({ palette }) => palette.gray[10],
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <div
-        style={{
-          borderWidth: "4px",
-          height: "100vh",
-          overflowY: "scroll",
-          position: "relative",
-          zIndex: "10",
+      <Container
+        sx={{
+          paddingY: 4.25,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            paddingBottom: "2.5rem",
-            paddingTop: "2.5rem",
-            visibility: showTopLogo ? undefined : "hidden",
-          }}
-        >
-          <LogoIcon />
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            paddingBottom: "2.5rem",
-            paddingTop: "6rem",
-          }}
-        >
-          {children}
-        </div>
-      </div>
-
-      <div style={{ position: "absolute", top: 0, right: 0, bottom: 0 }}>
-        <img alt="" src={bgPattern.src} style={{ height: "100vh" }} />
-      </div>
-      {onClose && (
-        <Box
-          component="button"
-          type="button"
-          sx={{
-            alignItems: "center",
-            borderRadius: "9999px",
-            display: "flex",
-            fontSize: "1.875rem",
-            height: "3rem",
-            justifyContent: "center",
-            lineHeight: 1,
-            position: "absolute",
-            right: "2rem",
-            top: "2rem",
-            width: "3rem",
-            zIndex: "10",
-
-            "&:hover, &:focus": {
-              backgroundColor: "black",
-              opacity: 0.1,
-              outline: "none",
-            },
-          }}
-          onClick={onClose}
-        >
-          &times;
-        </Box>
-      )}
-    </div>
+        <HashIcon />
+        <Box>{headerEndAdornment}</Box>
+      </Container>
+      <Container
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          alignItems: "center",
+          position: "relative",
+        }}
+      >
+        {children}
+      </Container>
+    </Box>
   );
 };
