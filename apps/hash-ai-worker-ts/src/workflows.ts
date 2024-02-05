d8bebc4163a2385de7a761d17be13438a9da7d7e
Tim Diekmann <post@timdiekmann.de>
2024-02-05 14:42:16 +0100

    Write workflow to update all embeddings

--- apps/hash-ai-worker-ts/src/workflows.ts
+++ apps/hash-ai-worker-ts/src/workflows.ts
@@ -1,4 +1,5 @@
 import type { Filter } from "@local/hash-graph-client";
+import { EntityQueryCursor } from "@local/hash-graph-client";
 import type {
   CreateEmbeddingsParams,
   CreateEmbeddingsReturn,
@@ -107,97 +108,147 @@ export const updateEntityEmbeddings = async (
   } as const;
 
   let entities: Entity[];
-
-  if ("entities" in params) {
-    entities = params.entities;
-  } else {
-    const subgraph = await graphActivities.getEntitiesByQuery({
-      authentication: params.authentication,
-      query: {
-        filter: params.filter,
-        graphResolveDepths: {
-          inheritsFrom: { outgoing: 0 },
-          constrainsValuesOn: { outgoing: 0 },
-          constrainsPropertiesOn: { outgoing: 0 },
-          constrainsLinksOn: { outgoing: 0 },
-          constrainsLinkDestinationsOn: { outgoing: 0 },
-          isOfType: { outgoing: 0 },
-          hasLeftEntity: { incoming: 0, outgoing: 0 },
-          hasRightEntity: { incoming: 0, outgoing: 0 },
-        },
-        temporalAxes,
-        includeDrafts: true,
-      },
-    });
-
-    entities = await graphActivities.getSubgraphEntities({
-      subgraph,
-    });
-  }
+  let cursor: EntityQueryCursor | undefined | null = undefined;
 
   const usage: CreateEmbeddingResponse.Usage = {
     prompt_tokens: 0,
     total_tokens: 0,
   };
 
-  for (const entity of entities) {
-    // TODO: The subgraph library does not have the required methods to do this client side so for simplicity we're
-    //       just making another request here. We should add the required methods to the library and do this client
-    //       side.
-    const subgraph = await graphActivities.getEntityTypesByQuery({
-      authentication: params.authentication,
-      query: {
-        filter: {
-          equal: [
-            { path: ["versionedUrl"] },
-            { parameter: entity.metadata.entityTypeId },
-          ],
-        },
-        graphResolveDepths: {
-          inheritsFrom: { outgoing: 255 },
-          constrainsValuesOn: { outgoing: 0 },
-          constrainsPropertiesOn: { outgoing: 1 },
-          constrainsLinksOn: { outgoing: 0 },
-          constrainsLinkDestinationsOn: { outgoing: 0 },
-          isOfType: { outgoing: 0 },
-          hasLeftEntity: { incoming: 0, outgoing: 0 },
-          hasRightEntity: { incoming: 0, outgoing: 0 },
-        },
-        temporalAxes,
-        includeDrafts: false,
-      },
-    });
-
-    const propertyTypes = await graphActivities.getSubgraphPropertyTypes({
-      subgraph,
-    });
-
-    const generatedEmbeddings =
-      await aiActivities.createEntityEmbeddingsActivity({
-        entityProperties: entity.properties,
-        propertyTypes,
-      });
-
-    if (generatedEmbeddings.embeddings.length > 0) {
-      await graphActivities.updateEntityEmbeddings({
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  while (true) {
+    if ("entities" in params) {
+      entities = params.entities;
+    } else {
+      const queryResponse = await graphActivities.getEntitiesByQuery({
         authentication: params.authentication,
-        embeddings: generatedEmbeddings.embeddings.map((embedding) => ({
-          ...embedding,
-          entityId: entity.metadata.recordId.entityId,
-        })),
-        updatedAtTransactionTime:
-          entity.metadata.temporalVersioning.transactionTime.start.limit,
-        updatedAtDecisionTime:
-          entity.metadata.temporalVersioning.decisionTime.start.limit,
+        query: {
+          filter: params.filter,
+          graphResolveDepths: {
+            inheritsFrom: { outgoing: 0 },
+            constrainsValuesOn: { outgoing: 0 },
+            constrainsPropertiesOn: { outgoing: 0 },
+            constrainsLinksOn: { outgoing: 0 },
+            constrainsLinkDestinationsOn: { outgoing: 0 },
+            isOfType: { outgoing: 0 },
+            hasLeftEntity: { incoming: 0, outgoing: 0 },
+            hasRightEntity: { incoming: 0, outgoing: 0 },
+          },
+          temporalAxes,
+          includeDrafts: true,
+        },
+        cursor,
+        limit: 100,
+      });
+      cursor = queryResponse.cursor;
+      entities = await graphActivities.getSubgraphEntities({
+        subgraph: queryResponse.subgraph,
       });
     }
 
-    usage.prompt_tokens += generatedEmbeddings.usage.prompt_tokens;
-    usage.total_tokens += generatedEmbeddings.usage.total_tokens;
+    if (entities.length === 0) {
+      break;
+    }
+
+    for (const entity of entities) {
+      // TODO: The subgraph library does not have the required methods to do this client side so for simplicity we're
+      //       just making another request here. We should add the required methods to the library and do this client
+      //       side.
+      const subgraph = await graphActivities.getEntityTypesByQuery({
+        authentication: params.authentication,
+        query: {
+          filter: {
+            equal: [
+              { path: ["versionedUrl"] },
+              { parameter: entity.metadata.entityTypeId },
+            ],
+          },
+          graphResolveDepths: {
+            inheritsFrom: { outgoing: 255 },
+            constrainsValuesOn: { outgoing: 0 },
+            constrainsPropertiesOn: { outgoing: 1 },
+            constrainsLinksOn: { outgoing: 0 },
+            constrainsLinkDestinationsOn: { outgoing: 0 },
+            isOfType: { outgoing: 0 },
+            hasLeftEntity: { incoming: 0, outgoing: 0 },
+            hasRightEntity: { incoming: 0, outgoing: 0 },
+          },
+          temporalAxes,
+          includeDrafts: false,
+        },
+      });
+
+      const propertyTypes = await graphActivities.getSubgraphPropertyTypes({
+        subgraph,
+      });
+
+      const generatedEmbeddings =
+        await aiActivities.createEntityEmbeddingsActivity({
+          entityProperties: entity.properties,
+          propertyTypes,
+        });
+
+      if (generatedEmbeddings.embeddings.length > 0) {
+        await graphActivities.updateEntityEmbeddings({
+          authentication: params.authentication,
+          embeddings: generatedEmbeddings.embeddings.map((embedding) => ({
+            ...embedding,
+            entityId: entity.metadata.recordId.entityId,
+          })),
+          updatedAtTransactionTime:
+            entity.metadata.temporalVersioning.transactionTime.start.limit,
+          updatedAtDecisionTime:
+            entity.metadata.temporalVersioning.decisionTime.start.limit,
+        });
+      }
+
+      usage.prompt_tokens += generatedEmbeddings.usage.prompt_tokens;
+      usage.total_tokens += generatedEmbeddings.usage.total_tokens;
+    }
+
+    if (!cursor) {
+      break;
+    }
   }
 
   return usage;
 };
+
+export const updateAllEntityEmbeddings =
+  async (): Promise<CreateEmbeddingResponse.Usage> => {
+    const accountIds = await graphActivities.getUserAccountIds();
+
+    const usage: CreateEmbeddingResponse.Usage = {
+      prompt_tokens: 0,
+      total_tokens: 0,
+    };
+
+    for (const accountId of accountIds) {
+      const this_usage = await updateEntityEmbeddings({
+        authentication: { actorId: accountId },
+        filter: {
+          all: [
+            {
+              // @ts-expect-error -- Support null in Path parameter in structural queries in Node
+              //                     see https://linear.app/hash/issue/H-1207
+              // We can skip entities for which the embeddings were already generated.
+              // If a full regeneration is desired, either the database should be wiped manually or the
+              // `updateEntityEmbeddings` workflow should be called manually.
+              equal: [{ path: ["embedding"] }, null],
+            },
+            {
+              // Only embeddings for non-empty properties are generated
+              notEqual: [{ path: ["properties"] }, { parameter: {} }],
+            },
+          ],
+        },
+      });
+      usage.prompt_tokens += this_usage.prompt_tokens;
+      usage.total_tokens += this_usage.total_tokens;
+    }
+
+    return usage;
+  };
 
 export const parseTextFromFile = async (
   params: ParseTextFromFileParams,
