222679f430a9c9d95055d8608269d3577df78997
Tim Diekmann <post@timdiekmann.de>
2024-02-27 21:24:14 +0100

    Attempt to fix ESLint

--- libs/@blockprotocol/type-system/typescript/src/index.ts
+++ libs/@blockprotocol/type-system/typescript/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable canonical/filename-no-index */
 import wasm from "@blockprotocol/type-system-rs/pkg/type-system_bg.wasm";
 
 import { setWasmInit } from "./common";
@@ -7,4 +8,5 @@ export * from "./native";
 export * from "@blockprotocol/type-system-rs/pkg/type-system";
 
 // @ts-expect-error -- The cause of this error is unknown, perhaps growing pains for the WASM ecosystem, or we need to do some custom TS declaration
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 setWasmInit(() => wasm());
