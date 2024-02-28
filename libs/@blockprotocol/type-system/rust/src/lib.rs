0470ef795fd794ca0e48535c0ba7ee11b738826e
Tim Diekmann <post@timdiekmann.de>
2024-02-28 23:58:44 +0100

    Ignore `non_local_definitions` lint on wasm target

--- libs/@blockprotocol/type-system/rust/src/lib.rs
+++ libs/@blockprotocol/type-system/rust/src/lib.rs
@@ -2,7 +2,11 @@
 #![allow(unsafe_code)]
 #![cfg_attr(
     target_arch = "wasm32",
-    allow(unreachable_pub, reason = "Used in the generated TypeScript types")
+    expect(unreachable_pub, reason = "Used in the generated TypeScript types"),
+    expect(
+        non_local_definitions,
+        reason = "Tsify does not take this lint into account"
+    )
 )]
 
 mod ontology;
