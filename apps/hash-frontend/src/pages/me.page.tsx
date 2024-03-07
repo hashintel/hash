8c0bbd731fae136cae8f1bdb0e0bd37fd748686a
Ciaran Morinan <c@hash.ai>
2024-03-07 08:47:03 +0000

    add /me router that redirect to user's profile page

---
+++ apps/hash-frontend/src/pages/me.page.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+
+import { getLayoutWithSidebar, NextPageWithLayout } from "../shared/layout";
+import { useAuthInfo } from "./shared/auth-info-context";
+
+const MePage: NextPageWithLayout = () => {
+  const router = useRouter();
+
+  const { authenticatedUser } = useAuthInfo();
+
+  if (authenticatedUser) {
+    void router.push(`/@${authenticatedUser.shortname}`);
+  } else {
+    void router.push("/");
+  }
+
+  return null;
+};
+
+MePage.getLayout = (page) =>
+  getLayoutWithSidebar(page, {
+    fullWidth: true,
+  });
+
+export default MePage;
