73e4384f483ce4b4c20abf3c4a70d576212d8289
Tim Diekmann <post@timdiekmann.de>
2024-03-11 11:58:09 +0100

    Change order of commands

--- apps/hash-frontend/vercel-install.sh
+++ apps/hash-frontend/vercel-install.sh
@@ -43,9 +43,9 @@ yarn global add "turbo@$(jq -r '.devDependencies.turbo' < package.json)"
 
 echo "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none --profile minimal
+source "$HOME/.cargo/env"
 # `rustup show` uses `rust-toolchain.toml` to install the correct toolchain.
 for _ in {1..5}; do rustup show && break || sleep 5; done
-source "$HOME/.cargo/env"
 
 # Install the pruned dependencies
 
--- apps/hashdotdev/vercel-install.sh
+++ apps/hashdotdev/vercel-install.sh
@@ -5,9 +5,9 @@ cd ../..
 
 echo "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none --profile minimal
+source "$HOME/.cargo/env"
 # `rustup show` uses `rust-toolchain.toml` to install the correct toolchain.
 for _ in {1..5}; do rustup show && break || sleep 5; done
-source "$HOME/.cargo/env"
 
 echo "Installing yarn dependencies"
 HUSKY=0 yarn install --frozen-lockfile --prefer-offline --force --build-from-source
