a5f568caafee60981c2e0c26d3bfa7ecb26efaf3
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-03-15 18:34:43 +0100

    feat: this isn't pretty
    but who said it would be pretty...

--- libs/@blockprotocol/type-system/typescript/src/main.ts
+++ libs/@blockprotocol/type-system/typescript/src/main.ts
@@ -1,7 +1,4 @@
-// Without the ?url query parameter, any project making using of `vite` (popular frontend tooling)
-// will fail to compile. This will force the project to use a URL instead but shouldn't affect any other
-// systems.
-import wasm from "@blockprotocol/type-system-rs/type-system.wasm?url";
+import wasm from "@blockprotocol/type-system-rs/type-system.wasm";
 
 import { setWasmInit } from "./common";
 
--- libs/@blockprotocol/type-system/typescript/vitest.config.mjs
+++ libs/@blockprotocol/type-system/typescript/vitest.config.mjs
@@ -1,8 +1,28 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
 
+// This is super hacky, but basically adds the `?url` suffix to any wasm import
+// this "fixes" the current issues with vite and wasm imports
+// The issue is actually more complicated than it seems:
+// 1) vite does not support the ESM wasm module yet
+// 2) vite-plugin-wasm does not seem to support wasm-pack properly
+//  (The issue exists in the `vite-plugin-wasm` repo, but with little help:
+//    https://github.com/Menci/vite-plugin-wasm/issues/9)
+// 3) `wasm-pack` generated WASM glue code is unable to load `file://` prefixed URLs and panics instead
+const rewriteWasmToUrl = {
+  name: "vite-replace-wasm-import",
+  enforce: "pre",
+  async load(id) {
+    if (!id.endsWith(".wasm")) {
+      return null;
+    }
+
+    return `export * from "${id}?url"`;
+  },
+};
+
 export default defineConfig({
-  plugins: [],
+  plugins: [rewriteWasmToUrl],
   build: {
     target: "esnext",
   },
