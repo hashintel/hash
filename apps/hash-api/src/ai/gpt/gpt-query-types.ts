0d9518073c621cd1ec537e0557e877f775483f82
Ciaran Morinan <c@hash.ai>
2024-03-15 19:49:30 +0000

    add semantic entity + type search. fix bug if empty webUuids array passed

---
+++ apps/hash-api/src/ai/gpt/gpt-query-types.ts
@@ -0,0 +1,136 @@
+import { currentTimeInstantTemporalAxes } from "@local/hash-isomorphic-utils/graph-queries";
+import { mapGraphApiSubgraphToSubgraph } from "@local/hash-subgraph/stdlib";
+import type { RequestHandler } from "express";
+
+import type { SimpleEntityType } from "./shared/entity-types";
+import { getSimpleEntityType } from "./shared/entity-types";
+import { stringifyResults } from "./shared/stringify-results";
+
+export type GptQueryTypesRequestBody = {
+  /**
+   * Limit the response to types within the specified webs, identified by the web's uuid.
+   * Note that a user may have entities of types from outside their web.
+   */
+  webUuids?: string[];
+  /**
+   * A natural language text query that looks for entity types which are semantically close to the query.
+   */
+  query?: string;
+};
+
+export type GptQueryTypesResponseBody =
+  | { error: string }
+  | {
+      /**
+       * The entity types that various entities in this response belong to.
+       * Each describes the properties and outgoing links that an entity of this type may have.
+       * They also have a unique id, which contains the name of the web that the entity type belongs to prefixed by an
+       * @, e.g. `@hash`
+       */
+      entityTypes: string;
+    };
+
+export const gptQueryTypes: RequestHandler<
+  Record<string, never>,
+  GptQueryTypesResponseBody,
+  GptQueryTypesRequestBody
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+> = async (req, res) => {
+  const { user } = req;
+
+  if (!user) {
+    res.status(401).send({ error: "No authenticated user" });
+    return;
+  }
+
+  if (!user.shortname) {
+    res.status(401).send({ error: "User has not completed signup." });
+    return;
+  }
+
+  const { query, webUuids } = req.body;
+
+  const queryResponse: GptQueryTypesResponseBody = await req.context.graphApi
+    .getEntityTypesByQuery(user.accountId, {
+      filter: {
+        all: [
+          ...(webUuids?.length
+            ? [
+                {
+                  any: webUuids.map((webUuid) => ({
+                    equal: [{ path: ["ownedById"] }, { parameter: webUuid }],
+                  })),
+                },
+              ]
+            : []),
+          ...(query
+            ? [
+                {
+                  cosineDistance: [
+                    { path: ["embedding"] },
+                    { parameter: query },
+                    { parameter: 0.9 },
+                  ],
+                },
+              ]
+            : []),
+          { equal: [{ path: ["archived"] }, { parameter: false }] },
+        ],
+      },
+      includeDrafts: false,
+      temporalAxes: currentTimeInstantTemporalAxes,
+      graphResolveDepths: {
+        inheritsFrom: { outgoing: 255 },
+        constrainsValuesOn: { outgoing: 255 },
+        constrainsPropertiesOn: { outgoing: 255 },
+        constrainsLinksOn: { outgoing: 255 },
+        constrainsLinkDestinationsOn: { outgoing: 255 },
+        isOfType: { outgoing: 1 },
+        hasLeftEntity: { incoming: 0, outgoing: 0 },
+        hasRightEntity: { incoming: 0, outgoing: 0 },
+      },
+    })
+    .then(async (response) => {
+      const entityTypes: SimpleEntityType[] = [];
+
+      const subgraph = mapGraphApiSubgraphToSubgraph(response.data);
+
+      const vertices = Object.values(subgraph.vertices)
+        .map((vertex) => Object.values(vertex))
+        .flat();
+
+      for (const vertex of vertices) {
+        if (vertex.kind === "entityType") {
+          const entityType = entityTypes.find(
+            (type) => type.entityTypeId === vertex.inner.metadata.entityTypeId,
+          );
+
+          if (!entityType) {
+            /**
+             * Resolve details of the entity type that the entity belongs to
+             */
+            const simpleEntityType = entityTypes.find(
+              (type) =>
+                type.entityTypeId === vertex.inner.metadata.entityTypeId,
+            );
+            if (!simpleEntityType) {
+              entityTypes.push(
+                getSimpleEntityType(
+                  subgraph,
+                  vertex.inner.metadata.entityTypeId,
+                ),
+              );
+            }
+          }
+        }
+      }
+
+      return {
+        entityTypes: `
+          ---- Entity Types ----
+        ${stringifyResults(entityTypes)}`,
+      };
+    });
+
+  res.status(200).json(queryResponse);
+};
