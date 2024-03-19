79cad62fdfc1466fed5c72ec98e0f4b27aed5668
Ciaran Morinan <c@hash.ai>
2024-03-19 19:17:39 +0000

    update getAccountPagesVariables to not look for drafts, add comment about future work

--- apps/hash-frontend/src/shared/account-pages-variables.ts
+++ apps/hash-frontend/src/shared/account-pages-variables.ts
@@ -9,11 +9,9 @@ import type { OwnedById } from "@local/hash-subgraph";
 export const getAccountPagesVariables = ({
   ownedById,
   includeArchived = false,
-  includeDrafts = false,
 }: {
   ownedById?: OwnedById;
   includeArchived?: boolean;
-  includeDrafts?: boolean;
 }) => ({
   query: {
     filter: {
@@ -34,7 +32,14 @@ export const getAccountPagesVariables = ({
       hasRightEntity: { incoming: 0, outgoing: 1 },
     },
     temporalAxes: currentTimeInstantTemporalAxes,
-    includeDrafts,
+    /**
+     * When we support draft/published versions of pages, this will need to be varied depending on the calling context,
+     * i.e. whether draft versions of pages are desired in the list of account pages.
+     * When drafts ARE included, callers will need handle the subgraph roots potentially containing multiple different
+     * versions of the same page – i.e. a live version and zero or more draft series
+     * H-2430
+     */
+    includeDrafts: false,
   },
   includePermissions: false,
 });
