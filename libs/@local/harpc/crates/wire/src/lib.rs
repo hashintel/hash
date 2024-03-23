689450ac372802dd5a298a6055226b78e4f1ac04
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-03-23 15:12:25 +0100

    feat: init write protocol

---
+++ libs/@local/harpc/crates/wire/src/lib.rs
@@ -0,0 +1,18 @@
+mod authorization;
+mod request;
+mod version;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
