7a84be67121923d563341750d231dd3d8fcc6c9d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-03-23 22:14:37 +0100

    feat: prevent abbreviations

--- libs/@repo/eslint/src/index.ts
+++ libs/@repo/eslint/src/index.ts
@@ -33,7 +33,7 @@ export interface Options {
   noRestrictedImports: () => NoRestrictedImportsRule[];
 }
 
-const noRestrictedImportsImpl =
+const noRestrictedImports =
   (overrides: () => NoRestrictedImportsRule[]) =>
   (config: FlatESLintConfig[]): FlatESLintConfig[] => {
     // get the latest current `no-restricted-imports` rule value and merge with the
@@ -91,6 +91,152 @@ const noRestrictedImportsImpl =
     ]);
   };
 
+const preventAbbreviations =
+  () =>
+  (config: FlatESLintConfig[]): FlatESLintConfig[] =>
+    defineFlatConfig([
+      ...config,
+      {
+        rules: {
+          "unicorn/prevent-abbreviations": [
+            "error",
+            {
+              checkFilenames: true,
+              checkProperties: true,
+              checkDefaultAndNamespaceImports: true,
+              checkShorthandImports: "internal",
+              extendDefaultReplacements: true,
+              replacement: {
+                // Offensive terms
+                whitelist: {
+                  include: true,
+                  allowList: true,
+                  permitList: true,
+                  passList: true,
+                },
+                blacklist: {
+                  exclude: true,
+                  denyList: true,
+                  blockList: true,
+                  rejectList: true,
+                },
+                master: {
+                  primary: true,
+                  host: true,
+                  leader: true,
+                  main: true,
+                  trunk: true,
+                },
+                slave: {
+                  secondary: true,
+                  guest: true,
+                  follower: true,
+                  replica: true,
+                  branch: true,
+                },
+
+                // Gendered terms
+                man: {
+                  person: true,
+                  human: true,
+                  individual: true,
+                  being: true,
+                  user: true,
+                },
+
+                // Reverse (some abbreviations are very well understood)
+                application: {
+                  app: true,
+                },
+                applications: {
+                  apps: true,
+                },
+
+                // Disable some default replacements that are well understood
+                // and commonly used (or from other languages)
+                env: false,
+                impl: false, // <- `rust`
+                iter: false, // <- `rust`
+                temp: false,
+                tmp: {
+                  temp: true,
+                },
+                gen: false, // <- `rust` + `effect`
+                ctx: false, // <- `rust` + `effect`
+                dev: false,
+                prod: false,
+                fn: false,
+                func: {
+                  fn: true, // mimic `rust`
+                },
+                ref: false, // <- `rust` + `effect`
+                refs: false, // <- `rust` + `effect`
+                arg: false,
+                args: false,
+                param: false,
+                params: false,
+                docs: false,
+                db: false,
+
+                // Not part of `eslint-plugin-unicorn`, copied from `xo`
+                // with some modifications
+                bin: {
+                  binary: true,
+                },
+                eof: {
+                  endOfFile: true,
+                },
+                anim: {
+                  animation: true,
+                },
+                calc: {
+                  calculate: true,
+                },
+                dict: {
+                  dictionary: true,
+                },
+                dup: {
+                  duplicate: true,
+                },
+                enc: {
+                  encode: true,
+                  encryption: true,
+                },
+                gfx: {
+                  graphics: true,
+                },
+                inc: {
+                  increment: true,
+                },
+                nav: {
+                  navigate: true,
+                  navigation: true,
+                },
+                norm: {
+                  normalize: true,
+                },
+                notif: {
+                  notification: true,
+                },
+                perf: {
+                  performance: true,
+                },
+                proc: {
+                  process: true,
+                },
+                rand: {
+                  random: true,
+                },
+                sys: {
+                  system: true,
+                },
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
 export function create(options: Options): FlatESLintConfig[] {
   const sheriffOptions = {
     react: false,
@@ -103,7 +249,8 @@ export function create(options: Options): FlatESLintConfig[] {
 
   return pipe(
     sheriff(sheriffOptions),
-    noRestrictedImportsImpl(options.noRestrictedImports),
+    noRestrictedImports(options.noRestrictedImports),
+    preventAbbreviations(),
     defineFlatConfig,
   );
 }
