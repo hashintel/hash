f34ff1958b9350f96055aaa6fe1961b5868fd7a9
Ben Werner <ben.werner01@gmail.com>
2024-03-26 12:48:59 +0000

    ft: add `createFlowActionActivities` with unimplemented missing actions

---
+++ apps/hash-ai-worker-ts/src/activities/flow-action-activites.ts
@@ -0,0 +1,32 @@
+import type { GraphApi } from "@local/hash-graph-client";
+import type { ActionId } from "@local/hash-isomorphic-utils/flows/step-definitions";
+
+import { generateWebQueryAction } from "./flow-action-activities/generate-web-query-action";
+import { getWebPageByUrlAction } from "./flow-action-activities/get-web-page-by-url-action";
+import { inferEntitiesFromContentAction } from "./flow-action-activities/infer-entities-from-content-action";
+import { persistEntityAction } from "./flow-action-activities/persist-entity-action";
+import type { FlowActionActivity } from "./flow-action-activities/types";
+import { webSearchAction } from "./flow-action-activities/web-search-action";
+
+export const createFlowActionActivities = ({
+  graphApiClient,
+}: {
+  graphApiClient: GraphApi;
+}): Record<`${ActionId}Action`, FlowActionActivity> => ({
+  generateWebQueryAction,
+  webSearchAction,
+  getWebPageByUrlAction,
+  async inferEntitiesFromContentAction(
+    params: Omit<
+      Parameters<typeof inferEntitiesFromContentAction>[0],
+      "graphApiClient"
+    >,
+  ) {
+    return inferEntitiesFromContentAction({ ...params, graphApiClient });
+  },
+  async persistEntityAction(
+    params: Omit<Parameters<typeof persistEntityAction>[0], "graphApiClient">,
+  ) {
+    return persistEntityAction({ ...params, graphApiClient });
+  },
+});
