0f41897a32e8cef3095bf2699dd0f1ae0c403e19
Tim Diekmann <post@timdiekmann.de>
2024-04-09 16:15:07 +0200

    Fix doc string

--- apps/hash-graph/libs/graph/src/store/knowledge.rs
+++ apps/hash-graph/libs/graph/src/store/knowledge.rs
@@ -267,7 +267,7 @@ pub trait EntityStore: crud::ReadPaginated<Entity> {
     /// # Errors:
     ///
     /// - if the [`EntityType`] doesn't exist
-    /// - if the [`EntityProperties`] is not valid with respect to the specified [`EntityType`]
+    /// - if the [`PropertyObject`] is not valid with respect to the specified [`EntityType`]
     /// - if the account referred to by `owned_by_id` does not exist
     /// - if an [`EntityUuid`] was supplied and already exists in the store
     ///
