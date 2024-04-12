075d48313d014a07de5d2c6b28672e977dbfba02
Ben Werner <ben.werner01@gmail.com>
2024-04-12 17:40:59 +0100

    refactor: make `question` input optional for research task flow

--- apps/hash-frontend/src/pages/ai.page/research-task-flow.tsx
+++ apps/hash-frontend/src/pages/ai.page/research-task-flow.tsx
@@ -1,10 +1,20 @@
 import { useMutation } from "@apollo/client";
 import { TextField } from "@hashintel/design-system";
-import { researchTaskFlowDefinition } from "@local/hash-isomorphic-utils/flows/example-flow-definitions";
+import type {
+  InputNameForAction,
+  OutputNameForAction,
+} from "@local/hash-isomorphic-utils/flows/action-definitions";
 import type { RunFlowWorkflowResponse } from "@local/hash-isomorphic-utils/flows/temporal-types";
-import type { PersistedEntities } from "@local/hash-isomorphic-utils/flows/types";
+import type {
+  FlowDefinition,
+  PersistedEntities,
+} from "@local/hash-isomorphic-utils/flows/types";
 import { stringifyPropertyValue } from "@local/hash-isomorphic-utils/stringify-property-value";
-import type { Entity, EntityTypeWithMetadata } from "@local/hash-subgraph";
+import type {
+  Entity,
+  EntityTypeWithMetadata,
+  EntityUuid,
+} from "@local/hash-subgraph";
 import { StatusCode } from "@local/status";
 import { Box, InputLabel, Typography } from "@mui/material";
 import type { FormEvent, FunctionComponent } from "react";
@@ -19,6 +29,133 @@ import { Button, Link } from "../../shared/ui";
 import { EntityTypeSelector } from "../shared/entity-type-selector";
 import { useEntityHref } from "../shared/use-entity-href";
 import { SectionContainer } from "./shared/section-container";
+
+const constructFlowDefinition = (params: {
+  includeQuestionAnswerAction: boolean;
+}): FlowDefinition => {
+  const { includeQuestionAnswerAction } = params;
+
+  return {
+    name: "Research Task",
+    flowDefinitionId: "research-task" as EntityUuid,
+    trigger: {
+      triggerDefinitionId: "userTrigger",
+      kind: "trigger",
+      outputs: [
+        {
+          payloadKind: "Text",
+          name: "prompt" as const,
+          array: false,
+          required: true,
+        },
+        {
+          payloadKind: "VersionedUrl",
+          name: "entityTypeIds",
+          array: true,
+          required: true,
+        },
+        ...(includeQuestionAnswerAction
+          ? [
+              {
+                payloadKind: "Text",
+                name: "question",
+                array: false,
+                required: true,
+              } as const,
+            ]
+          : []),
+      ],
+    },
+    steps: [
+      {
+        stepId: "1",
+        kind: "action",
+        actionDefinitionId: "researchEntities",
+        inputSources: [
+          {
+            inputName:
+              "prompt" satisfies InputNameForAction<"researchEntities">,
+            kind: "step-output",
+            sourceStepId: "trigger",
+            sourceStepOutputName: "prompt",
+          },
+          {
+            inputName:
+              "entityTypeIds" satisfies InputNameForAction<"researchEntities">,
+            kind: "step-output",
+            sourceStepId: "trigger",
+            sourceStepOutputName: "entityTypeIds",
+          },
+        ],
+      },
+      {
+        stepId: "2",
+        kind: "action",
+        actionDefinitionId: "persistEntities",
+        inputSources: [
+          {
+            inputName:
+              "proposedEntities" satisfies InputNameForAction<"persistEntities">,
+            kind: "step-output",
+            sourceStepId: "1",
+            sourceStepOutputName:
+              "proposedEntities" satisfies OutputNameForAction<"researchEntities">,
+          },
+        ],
+      },
+      ...(includeQuestionAnswerAction
+        ? [
+            {
+              stepId: "3",
+              kind: "action" as const,
+              actionDefinitionId: "answerQuestion" as const,
+              inputSources: [
+                {
+                  inputName:
+                    "question" satisfies InputNameForAction<"answerQuestion">,
+                  kind: "step-output" as const,
+                  sourceStepId: "trigger",
+                  sourceStepOutputName: "question",
+                },
+                {
+                  inputName:
+                    "entities" satisfies InputNameForAction<"answerQuestion">,
+                  kind: "step-output" as const,
+                  sourceStepId: "2",
+                  sourceStepOutputName:
+                    "persistedEntities" satisfies OutputNameForAction<"persistEntities">,
+                },
+              ],
+            },
+          ]
+        : []),
+    ],
+    outputs: [
+      {
+        stepId: "2",
+        stepOutputName:
+          "persistedEntities" satisfies OutputNameForAction<"persistEntities">,
+        name: "persistedEntities" as const,
+        payloadKind: "PersistedEntities",
+        array: false,
+        required: true,
+      },
+      ...(includeQuestionAnswerAction
+        ? [
+            {
+              stepId: "3",
+              stepOutputName:
+                "answer" satisfies OutputNameForAction<"answerQuestion">,
+              payloadKind: "Text",
+              name: "answer" as const,
+              array: false,
+              required: true,
+            } as const,
+          ]
+        : []),
+    ],
+  };
+};
 
 const EntityListItem: FunctionComponent<{
   persistedEntity: Entity;
@@ -79,13 +216,19 @@ export const ResearchTaskFlow: FunctionComponent = () => {
     async (event: FormEvent) => {
       event.preventDefault();
 
-      if (entityType && prompt && question) {
+      if (entityType && prompt) {
         setPersistedEntities(undefined);
         setAnswer(undefined);
 
+        const includeQuestionAnswerAction = !!question;
+
+        const flowDefinition = constructFlowDefinition({
+          includeQuestionAnswerAction,
+        });
+
         const { data } = await startFlow({
           variables: {
-            flowDefinition: researchTaskFlowDefinition,
+            flowDefinition,
             flowTrigger: {
               triggerDefinitionId: "userTrigger",
               outputs: [
@@ -103,13 +246,17 @@ export const ResearchTaskFlow: FunctionComponent = () => {
                     value: [entityType.schema.$id],
                   },
                 },
-                {
-                  outputName: "question",
-                  payload: {
-                    kind: "Text",
-                    value: question,
-                  },
-                },
+                ...(question
+                  ? [
+                      {
+                        outputName: "question",
+                        payload: {
+                          kind: "Text",
+                          value: question,
+                        },
+                      } as const,
+                    ]
+                  : []),
               ],
             },
           },
@@ -134,12 +281,14 @@ export const ResearchTaskFlow: FunctionComponent = () => {
               persistedEntitiesOutput.payload.value as PersistedEntities,
             );
 
-            const answerOutput = status.contents[0]?.flowOutputs?.find(
-              (output) => output.outputName === "answer",
-            );
+            if (includeQuestionAnswerAction) {
+              const answerOutput = status.contents[0]?.flowOutputs?.find(
+                (output) => output.outputName === "answer",
+              );
 
-            if (answerOutput) {
-              setAnswer(answerOutput.payload.value as string);
+              if (answerOutput) {
+                setAnswer(answerOutput.payload.value as string);
+              }
             }
           } else {
             throw new Error(status.message ?? "No error message");
@@ -150,7 +299,7 @@ export const ResearchTaskFlow: FunctionComponent = () => {
     [entityType, prompt, question, startFlow],
   );
 
-  const isDisabled = !entityType || !prompt || !question;
+  const isDisabled = !entityType || !prompt;
 
   return (
     <SectionContainer>
