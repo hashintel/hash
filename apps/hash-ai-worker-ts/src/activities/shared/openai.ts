d3b6bcaecb05c55f45739bbd00740c2ca85adb27
Ben Werner <ben.werner01@gmail.com>
2024-04-12 13:00:19 +0100

    refactor: switch to `gpt-4-turbo` as the default GPT-4 model

--- apps/hash-ai-worker-ts/src/activities/shared/openai.ts
+++ apps/hash-ai-worker-ts/src/activities/shared/openai.ts
@@ -13,12 +13,14 @@ export type PermittedOpenAiModel =
   | "gpt-3.5-turbo-1106"
   | "gpt-4-1106-preview"
   | "gpt-4-0125-preview"
+  | "gpt-4-turbo"
   | "gpt-4";
 
 const modelToContextWindow: Record<PermittedOpenAiModel, number> = {
   "gpt-3.5-turbo-1106": 16_385,
   "gpt-4-1106-preview": 128_000,
   "gpt-4-0125-preview": 128_000,
+  "gpt-4-turbo": 128_000,
   "gpt-4": 8_192,
 };
 
@@ -156,7 +158,7 @@ export const getOpenAiResponse = async (
 export const modelAliasToSpecificModel = {
   "gpt-3.5-turbo": "gpt-3.5-turbo-1106", // bigger context window, will be the resolved value for gpt-3.5-turbo from 11
   // Dec 2023
-  "gpt-4-turbo": "gpt-4-1106-preview", // 'gpt-4-turbo' is not a valid model name in the OpenAI API yet, it's in
+  "gpt-4-turbo": "gpt-4-turbo", // 'gpt-4-turbo' is not a valid model name in the OpenAI API yet, it's in preview only
   // preview only
   "gpt-4": "gpt-4", // this points to the latest available anyway as of 6 Dec 2023
 } as const satisfies Record<InferenceModelName, PermittedOpenAiModel>;
