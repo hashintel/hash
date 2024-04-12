1419b2eebbb37fd7c1ea1c914d0b21d7924b44c9
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2024-04-12 12:50:59 +0200

    Update apps/hash-ai-worker-ts/src/activities/infer-entities/propose-entities.ts
    Co-authored-by: Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>

--- apps/hash-ai-worker-ts/src/activities/infer-entities/propose-entities.ts
+++ apps/hash-ai-worker-ts/src/activities/infer-entities/propose-entities.ts
@@ -372,6 +372,7 @@ export const proposeEntities = async (params: {
                         await graphApiClient.validateEntity(validationActorId, {
                           entityTypes: [entityTypeId],
                           components: {
+                            linkData: false,
                             numItems: false,
                             requiredProperties: false,
                           },
