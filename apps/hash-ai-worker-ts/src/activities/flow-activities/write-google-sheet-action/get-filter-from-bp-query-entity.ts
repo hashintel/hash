27b6664e4bfd937101da66fb6d3db26bcd463043
Ciaran Morinan <c@hash.ai>
2024-04-29 12:36:16 +0100

    more lint fixes

--- apps/hash-ai-worker-ts/src/activities/flow-activities/write-google-sheet-action/get-filter-from-bp-query-entity.ts
+++ apps/hash-ai-worker-ts/src/activities/flow-activities/write-google-sheet-action/get-filter-from-bp-query-entity.ts
@@ -1,11 +1,11 @@
 import type { MultiFilter } from "@blockprotocol/graph";
 import { convertBpFilterToGraphFilter } from "@local/hash-backend-utils/convert-bp-filter-to-graph-filter";
 import type { GraphApi } from "@local/hash-graph-client";
+import { blockProtocolPropertyTypes } from "@local/hash-isomorphic-utils/ontology-type-ids";
 import type { QueryProperties } from "@local/hash-isomorphic-utils/system-types/blockprotocol/query";
 import type { AccountId, Entity, EntityId } from "@local/hash-subgraph";
 
 import { getLatestEntityById } from "../shared/graph-requests";
-import { blockProtocolPropertyTypes } from "@local/hash-isomorphic-utils/ontology-type-ids";
 
 export const getFilterFromBlockProtocolQueryEntity = async ({
   authentication,
@@ -32,6 +32,7 @@ export const getFilterFromBlockProtocolQueryEntity = async ({
       blockProtocolPropertyTypes.query.propertyTypeBaseUrl
     ];
 
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- additional check in case another entity is used that has this optional
   if (!multiFilter) {
     throw new Error(
       `No ${blockProtocolPropertyTypes.query.propertyTypeBaseUrl} property found on query entity with id ${queryEntityId}`,
