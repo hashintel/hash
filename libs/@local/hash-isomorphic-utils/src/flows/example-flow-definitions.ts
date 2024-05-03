f5ab5f24cec142ef63ffd7672a6b005744fa3ad5
Ciaran Morinan <c@hash.ai>
2024-05-03 15:08:12 +0100

    add research entities example flow definition

--- libs/@local/hash-isomorphic-utils/src/flows/example-flow-definitions.ts
+++ libs/@local/hash-isomorphic-utils/src/flows/example-flow-definitions.ts
@@ -214,6 +214,178 @@ export const researchTaskFlowDefinition: FlowDefinition = {
   ],
 };
 
+export const researchEntitiesFlowDefinition: FlowDefinition = {
+  name: "Research Entities",
+  flowDefinitionId: "research-entities" as EntityUuid,
+  description:
+    "Discover entities according to a research brief, save them to HASH and Google Sheets",
+  trigger: {
+    triggerDefinitionId: "userTrigger",
+    description:
+      "User provides research specification and entity types to discover",
+    kind: "trigger",
+    outputs: [
+      {
+        payloadKind: "Text",
+        name: "Research guidance" as const,
+        array: false,
+        required: true,
+      },
+      {
+        payloadKind: "VersionedUrl",
+        name: "Entity Types",
+        array: true,
+        required: true,
+      },
+      {
+        payloadKind: "GoogleAccountId",
+        name: "Google Account",
+        array: false,
+        required: true,
+      },
+      {
+        payloadKind: "GoogleSheet",
+        name: "Google Sheet",
+        array: false,
+        required: true,
+      },
+      {
+        payloadKind: "WebId",
+        name: "Create in",
+        array: false,
+        required: true,
+      },
+      {
+        payloadKind: "Boolean",
+        name: "Create as draft",
+        array: false,
+        required: true,
+      },
+    ],
+  },
+  groups: [
+    {
+      groupId: 1,
+      description: "Research and persist entities",
+    },
+    {
+      groupId: 2,
+      description: "Save data to Google Sheet",
+    },
+  ],
+  steps: [
+    {
+      stepId: "1",
+      kind: "action",
+      groupId: 1,
+      actionDefinitionId: "researchEntities",
+      description:
+        "Discover entities according to research specification, using public web sources",
+      inputSources: [
+        {
+          inputName: "prompt" satisfies InputNameForAction<"researchEntities">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Research guidance",
+        },
+        {
+          inputName:
+            "entityTypeIds" satisfies InputNameForAction<"researchEntities">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Entity Types",
+        },
+      ],
+    },
+    {
+      stepId: "2",
+      kind: "action",
+      groupId: 1,
+      description: "Save discovered entities and relationships to HASH graph",
+      actionDefinitionId: "persistEntities",
+      inputSources: [
+        {
+          inputName:
+            "proposedEntities" satisfies InputNameForAction<"persistEntities">,
+          kind: "step-output",
+          sourceStepId: "1",
+          sourceStepOutputName:
+            "proposedEntities" satisfies OutputNameForAction<"researchEntities">,
+        },
+        {
+          inputName: "webId" satisfies InputNameForAction<"persistEntities">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Create in",
+        },
+        {
+          inputName: "draft" satisfies InputNameForAction<"persistEntities">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Create as draft",
+        },
+      ],
+    },
+    {
+      stepId: "3",
+      groupId: 2,
+      kind: "action",
+      actionDefinitionId: "writeGoogleSheet",
+      description: "Save discovered entities to Google Sheet",
+      inputSources: [
+        {
+          inputName:
+            "audience" satisfies InputNameForAction<"writeGoogleSheet">,
+          kind: "hardcoded",
+          payload: {
+            kind: "ActorType",
+            value: "human",
+          },
+        },
+        {
+          inputName:
+            "googleAccountId" satisfies InputNameForAction<"writeGoogleSheet">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Google Account",
+        },
+        {
+          inputName:
+            "googleSheet" satisfies InputNameForAction<"writeGoogleSheet">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Google Sheet",
+        },
+        {
+          inputName: "webId" satisfies InputNameForAction<"writeGoogleSheet">,
+          kind: "step-output",
+          sourceStepId: "trigger",
+          sourceStepOutputName: "Create in",
+        },
+        {
+          inputName:
+            "dataToWrite" satisfies InputNameForAction<"writeGoogleSheet">,
+          kind: "step-output",
+          sourceStepId: "2",
+          sourceStepOutputName:
+            "persistedEntities" satisfies OutputNameForAction<"persistEntities">,
+        },
+      ],
+    },
+  ],
+  outputs: [
+    {
+      stepId: "3",
+      stepOutputName:
+        "googleSheetEntity" satisfies OutputNameForAction<"writeGoogleSheet">,
+      payloadKind: "Entity",
+      name: "googleSheetEntity" as const,
+      array: false,
+      required: true,
+    },
+  ],
+};
+
 export const ftseInvestorsFlowDefinition: FlowDefinition = {
   name: "FTSE350 Investors",
   flowDefinitionId: "ftse-350-investors" as EntityUuid,
