955d960c98299efe612f13e68b5a54efe9e74dd6
Tim Diekmann <post@timdiekmann.de>
2024-05-16 09:09:55 +0200

    Find corresponding tracing data when analyzing benchmarks

--- libs/@local/repo-chores/rust/bin/cli/subcommand/benches/upload.rs
+++ libs/@local/repo-chores/rust/bin/cli/subcommand/benches/upload.rs
@@ -1,11 +1,11 @@
-use std::error::Error;
+use std::{error::Error, path::PathBuf};
 
 use aws_config::BehaviorVersion;
 use clap::Parser;
 use error_stack::ResultExt;
-use repo_chores::benches::{report::Benchmark, storage::S3Storage};
+use repo_chores::benches::{analyze::BenchmarkAnalysis, report::Benchmark, storage::S3Storage};
 
-use crate::subcommand::benches::criterion_directory;
+use crate::subcommand::benches::{criterion_directory, target_directory};
 
 fn current_commit() -> Result<String, Box<dyn Error + Send + Sync>> {
     Ok(String::from_utf8(
@@ -25,20 +25,27 @@ pub(crate) struct Args {
     /// The region to use for the AWS client
     #[clap(long)]
     commit: Option<String>,
+
+    /// The path to the directory where the benchmark artifacts are stored.
+    #[clap(long)]
+    artifacts_path: Option<PathBuf>,
 }
 
 pub(super) async fn run(args: Args) -> Result<(), Box<dyn Error + Send + Sync>> {
     let config = aws_config::defaults(BehaviorVersion::latest()).load().await;
 
-    let benchmarks = Benchmark::gather(criterion_directory()?).collect::<Result<Vec<_>, _>>()?;
+    let benchmarks = Benchmark::gather(criterion_directory()).collect::<Result<Vec<_>, _>>()?;
+
+    let artifacts_path = args.artifacts_path.unwrap_or_else(target_directory);
 
     let s3 = S3Storage::new(&config, "benchmarks.hash.dev");
     let commit = args.commit.map_or_else(current_commit, Ok)?;
-    for measurement in benchmarks
-        .iter()
-        .filter_map(|report| report.measurements.get("new"))
-    {
-        s3.put_measurement(measurement, &commit).await?;
+    for benchmark in benchmarks {
+        s3.put_benchmark_analysis(
+            &BenchmarkAnalysis::from_benchmark(benchmark, "new", &artifacts_path)?,
+            &commit,
+        )
+        .await?;
     }
 
     Ok(())
