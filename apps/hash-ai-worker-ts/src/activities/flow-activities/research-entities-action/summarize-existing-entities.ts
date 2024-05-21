2f00d91a81b6ce8845659ac70a835ee9025d7ab2
Ben Werner <ben.werner01@gmail.com>
2024-05-21 16:24:17 +0100

    ft: add agent for providing summaries of existing entities

---
+++ apps/hash-ai-worker-ts/src/activities/flow-activities/research-entities-action/summarize-existing-entities.ts
@@ -0,0 +1,146 @@
+import type { VersionedUrl } from "@blockprotocol/type-system";
+import type { Entity, EntityId } from "@local/hash-subgraph";
+import dedent from "dedent";
+
+import { getFlowContext } from "../../shared/get-flow-context";
+import { getLlmResponse } from "../../shared/get-llm-response";
+import { getToolCallsFromLlmAssistantMessage } from "../../shared/get-llm-response/llm-message";
+import type { LlmToolDefinition } from "../../shared/get-llm-response/types";
+import { graphApiClient } from "../../shared/graph-api-client";
+import { simplifyEntity } from "../../shared/simplify-entity";
+
+export type ExistingEntitySummary = {
+  entityId: EntityId;
+  entityTypeId: VersionedUrl;
+  name: string;
+  summary: string;
+};
+
+const systemPrompt = dedent(`
+  You are an entity summary agent.
+
+  The user will provide you with:
+    - "entities": a list of entities for which you need to provide a "name" and a "summary".
+
+  You must provide a summary for every entity in the list provided by the user.
+`);
+
+const toolName = "submitEntitySummaries";
+
+const registerEntitySummariesToolDefinition: LlmToolDefinition<
+  typeof toolName
+> = {
+  name: toolName,
+  description: "Register entity summaries",
+  inputSchema: {
+    type: "object",
+    properties: {
+      entitySummaries: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            entityId: {
+              type: "string",
+              description: dedent(`
+                The "name" should be as unique as possible based on the properties of the entity.
+                For example for an entity of type "Person" with first name "John", middle name "Doe", and last name "Smith", the "name" could be "John Doe Smith".
+             `),
+            },
+            name: {
+              type: "string",
+              description: dedent(`
+                The "name" should be as unique as possible based on the properties of the entity.
+                For example for an entity of type "Person" with first name "John", middle name "Doe", and last name "Smith", the "name" could be "John Doe Smith".
+            `),
+            },
+            summary: {
+              type: "string",
+              description: dedent(`
+                The "summary" should be a brief description based on the properties of the entity.
+              `),
+            },
+          },
+          required: ["entityId", "name", "summary"],
+        },
+        description: "A list of all the entity summaries.",
+      },
+    },
+    required: ["entitySummaries"],
+  },
+};
+
+export const summarizeExistingEntities = async (params: {
+  existingEntities: Entity[];
+}): Promise<{ existingEntitySummaries: ExistingEntitySummary[] }> => {
+  const { existingEntities } = params;
+
+  const { flowEntityId, userAuthentication, webId } = await getFlowContext();
+
+  const llmResponse = await getLlmResponse(
+    {
+      systemPrompt,
+      tools: [registerEntitySummariesToolDefinition],
+      toolChoice: toolName,
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: `Entities: ${JSON.stringify(existingEntities.map(simplifyEntity))}`,
+            },
+          ],
+        },
+      ],
+      model: "gpt-4o-2024-05-13",
+    },
+    {
+      userAccountId: userAuthentication.actorId,
+      graphApiClient,
+      incurredInEntities: [{ entityId: flowEntityId }],
+      webId,
+    },
+  );
+
+  if (llmResponse.status !== "ok") {
+    throw new Error(
+      `Failed to get LLM response: ${JSON.stringify(llmResponse)}`,
+    );
+  }
+
+  const { message } = llmResponse;
+
+  const toolCalls = getToolCallsFromLlmAssistantMessage({ message });
+
+  const validEntitySummaries: ExistingEntitySummary[] = [];
+
+  for (const toolCall of toolCalls) {
+    const { entitySummaries: proposedEntitySummaries } = toolCall.input as {
+      entitySummaries: { entityId: string; name: string; summary: string }[];
+    };
+
+    for (const proposedEntitySummary of proposedEntitySummaries) {
+      const { entityId, name, summary } = proposedEntitySummary;
+      const existingEntity = existingEntities.find(
+        (entity) => entity.metadata.recordId.entityId === entityId,
+      );
+
+      if (!existingEntity) {
+        /** @todo: add retry logic */
+        throw new Error(
+          `Entity with entityId ${entityId} not found in the existing entities.`,
+        );
+      }
+
+      validEntitySummaries.push({
+        entityId: existingEntity.metadata.recordId.entityId,
+        entityTypeId: existingEntity.metadata.entityTypeId,
+        name,
+        summary,
+      });
+    }
+  }
+
+  return { existingEntitySummaries: validEntitySummaries };
+};
