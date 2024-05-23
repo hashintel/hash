a3096b870f4b96356a7fd0caf7191aec41a5e06a
Ciaran Morinan <c@hash.ai>
2024-05-23 19:26:17 +0100

    install turbo

--- apps/plugin-browser/utils/install-in-itero.sh
+++ apps/plugin-browser/utils/install-in-itero.sh
@@ -1,5 +1,6 @@
 # Install dependencies in the Itero Plasmo build environment
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
+yarn global add "turbo@$(jq -r '.devDependencies.turbo' < package.json)"
 yarn config set ignore-engines true
 yarn
