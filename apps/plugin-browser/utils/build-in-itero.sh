99fbf10aeacd5894a9e90a18807c3193c5031476
Ciaran Morinan <c@hash.ai>
2024-05-23 21:01:55 +0100

    remove sudo

--- apps/plugin-browser/utils/build-in-itero.sh
+++ apps/plugin-browser/utils/build-in-itero.sh
@@ -1,5 +1,5 @@
 # Build in the Itero environment
-sudo dnf install java-latest-openjdk
+dnf install java-latest-openjdk
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
 yarn config set ignore-engines true
