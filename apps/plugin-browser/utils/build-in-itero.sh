65cd565b4f6c5a8196104aebefacc06eb1ae257a
Ciaran Morinan <c@hash.ai>
2024-05-23 20:14:47 +0100

    install cargo in build script also

--- apps/plugin-browser/utils/build-in-itero.sh
+++ apps/plugin-browser/utils/build-in-itero.sh
@@ -1,4 +1,5 @@
 # Build in the Itero environment
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
 yarn config set ignore-engines true
 npx turbo build --filter @apps/plugin-browser
