8011efe923c82dc5d356813d487ff5937332ce85
Ciaran Morinan <c@hash.ai>
2024-05-24 09:40:00 +0100

    comments

--- apps/plugin-browser/utils/build-in-itero.sh
+++ apps/plugin-browser/utils/build-in-itero.sh
@@ -1,4 +1,5 @@
-# Build in the Itero environment
+# Build in the Itero Plasmo environment
+# This seems to be a separate VM or layer to where the install script is run, as it needs Cargo installing again
 apt update
 apt install default-jre -y
 curl https://sh.rustup.rs -sSf | sh -s -- -y
--- apps/plugin-browser/utils/install-in-itero.sh
+++ apps/plugin-browser/utils/install-in-itero.sh
@@ -1,4 +1,5 @@
 # Install dependencies in the Itero Plasmo build environment
+# A separate script is provided to 'build' the plugin once dependencies are installed
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
 yarn config set ignore-engines true
