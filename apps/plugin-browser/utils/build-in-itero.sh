209f017f428c67d8060c6a7cf12f88b59c24e9c6
Ciaran Morinan <c@hash.ai>
2024-05-24 09:36:17 +0100

    -y

--- apps/plugin-browser/utils/build-in-itero.sh
+++ apps/plugin-browser/utils/build-in-itero.sh
@@ -1,6 +1,6 @@
 # Build in the Itero environment
 apt update
-apt install default-jre
+apt install default-jre -y
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
 yarn config set ignore-engines true
