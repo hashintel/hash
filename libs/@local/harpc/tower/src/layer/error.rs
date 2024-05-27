b36f4d8a0d972f7846e46da9104a7789f0484093
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-05-27 16:04:55 +0200

    feat: `HandleError` and `HandleReport`

--- libs/@local/harpc/tower/src/layer/error.rs
+++ libs/@local/harpc/tower/src/layer/error.rs
@@ -1,7 +1,8 @@
 use core::task::{Context, Poll};
+use std::error::Error;
 
 use error_stack::Report;
-use harpc_net::codec::ErrorEncoder;
+use harpc_net::codec::{ErrorEncoder, PlainError};
 use tower::{Service, ServiceExt};
 
 use crate::{
@@ -10,13 +11,13 @@ use crate::{
     response::Response,
 };
 
-pub struct HandleError<S, E> {
+pub struct HandleReport<S, E> {
     inner: S,
 
     encoder: E,
 }
 
-impl<S, E, C, ReqBody, ResBody> Service<Request<ReqBody>> for HandleError<S, E>
+impl<S, E, C, ReqBody, ResBody> Service<Request<ReqBody>> for HandleReport<S, E>
 where
     S: Service<Request<ReqBody>, Error = Report<C>, Response = Response<ResBody>> + Clone + Send,
     E: ErrorEncoder + Clone,
@@ -47,6 +48,51 @@ where
                 Ok(response) => Ok(response.map_body(Either::Left)),
                 Err(report) => {
                     let error = encoder.encode_report(report).await;
+
+                    Ok(Response::new_error(session, error).map_body(Either::Right))
+                }
+            }
+        }
+    }
+}
+
+pub struct HandleError<S, E> {
+    inner: S,
+
+    encoder: E,
+}
+
+impl<S, E, ReqBody, ResBody> Service<Request<ReqBody>> for HandleError<S, E>
+where
+    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Clone + Send,
+    S::Error: PlainError,
+    E: ErrorEncoder + Clone,
+{
+    type Error = !;
+    type Response = Response<Either<ResBody, Full>>;
+
+    type Future = impl Future<Output = Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        // Taken from axum::HandleError
+        // we're always ready because we clone the inner service, therefore it is unused and always
+        // ready
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<ReqBody>) -> Self::Future {
+        let encoder = self.encoder.clone();
+
+        let clone = self.inner.clone();
+        let inner = std::mem::replace(&mut self.inner, clone);
+
+        let session = req.session();
+
+        async move {
+            match inner.oneshot(req).await {
+                Ok(response) => Ok(response.map_body(Either::Left)),
+                Err(error) => {
+                    let error = encoder.encode_error(error).await;
 
                     Ok(Response::new_error(session, error).map_body(Either::Right))
                 }
