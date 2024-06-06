7fe373827ed820bd9e671038e04dda46ad698e7f
Ciaran Morinan <c@hash.ai>
2024-06-06 14:59:01 +0100

    remove some flow code

--- apps/hash-ai-worker-ts/src/activities/flow-activities.ts
+++ apps/hash-ai-worker-ts/src/activities/flow-activities.ts
@@ -1,53 +1,55 @@
-import type { VaultClient } from "@local/hash-backend-utils/vault";
-import type { ActionDefinitionId } from "@local/hash-isomorphic-utils/flows/action-definitions";
-
-import { answerQuestionAction } from "./flow-activities/answer-question-action";
+// import type { VaultClient } from "@local/hash-backend-utils/vault";
+// import type { ActionDefinitionId } from "@local/hash-isomorphic-utils/flows/action-definitions";
+//
+// import { answerQuestionAction } from "./flow-activities/answer-question-action";
 import { generateFlowRunName } from "./flow-activities/generate-flow-run-name-activity";
-import { generateWebQueriesAction } from "./flow-activities/generate-web-queries-action";
-import { getFileFromUrlAction } from "./flow-activities/get-file-from-url-action";
-import { getWebPageByUrlAction } from "./flow-activities/get-web-page-by-url-action";
-import { getWebPageSummaryAction } from "./flow-activities/get-web-page-summary-action";
-import { inferEntitiesFromContentAction } from "./flow-activities/infer-entities-from-content-action";
-import { persistEntitiesAction } from "./flow-activities/persist-entities-action";
-import { persistEntityAction } from "./flow-activities/persist-entity-action";
+// import { generateWebQueriesAction } from "./flow-activities/generate-web-queries-action";
+// import { getFileFromUrlAction } from "./flow-activities/get-file-from-url-action";
+// import { getWebPageByUrlAction } from "./flow-activities/get-web-page-by-url-action";
+// import { getWebPageSummaryAction } from "./flow-activities/get-web-page-summary-action";
+// import { inferEntitiesFromContentAction } from "./flow-activities/infer-entities-from-content-action";
+// import { persistEntitiesAction } from "./flow-activities/persist-entities-action";
+// import { persistEntityAction } from "./flow-activities/persist-entity-action";
 import { persistFlowActivity } from "./flow-activities/persist-flow-activity";
-import { processAutomaticBrowsingSettingsAction } from "./flow-activities/process-automatic-browsing-settings-action";
-import { researchEntitiesAction } from "./flow-activities/research-entities-action";
-import type { FlowActionActivity } from "./flow-activities/types";
+// import { processAutomaticBrowsingSettingsAction } from "./flow-activities/process-automatic-browsing-settings-action";
+// import { researchEntitiesAction } from "./flow-activities/research-entities-action";
+// import type { FlowActionActivity } from "./flow-activities/types";
 import { userHasPermissionToRunFlowInWebActivity } from "./flow-activities/user-has-permission-to-run-flow-in-web-activity";
-import { webSearchAction } from "./flow-activities/web-search-action";
-import { writeGoogleSheetAction } from "./flow-activities/write-google-sheet-action";
+// import { webSearchAction } from "./flow-activities/web-search-action";
+// import { writeGoogleSheetAction } from "./flow-activities/write-google-sheet-action";
 
-export const createFlowActionActivities = ({
-  vaultClient,
-}: {
-  vaultClient: VaultClient;
-}): Record<`${ActionDefinitionId}Action`, FlowActionActivity> => ({
-  generateWebQueriesAction,
-  webSearchAction,
-  getWebPageByUrlAction,
-  processAutomaticBrowsingSettingsAction,
-  inferEntitiesFromContentAction,
-  persistEntityAction,
-  persistEntitiesAction,
-  getFileFromUrlAction,
-  researchEntitiesAction,
-  getWebPageSummaryAction,
-  answerQuestionAction,
-  writeGoogleSheetAction(
-    params: Omit<Parameters<typeof writeGoogleSheetAction>[0], "vaultClient">,
-  ) {
-    return writeGoogleSheetAction({ ...params, vaultClient });
-  },
-});
+// export const createFlowActionActivities = ({
+//   vaultClient,
+// }: {
+//   vaultClient: VaultClient;
+// }): Record<`${ActionDefinitionId}Action`, FlowActionActivity> => ({
+//   generateWebQueriesAction,
+//   webSearchAction,
+//   getWebPageByUrlAction,
+//   processAutomaticBrowsingSettingsAction,
+//   inferEntitiesFromContentAction,
+//   persistEntityAction,
+//   persistEntitiesAction,
+//   getFileFromUrlAction,
+//   researchEntitiesAction,
+//   getWebPageSummaryAction,
+//   answerQuestionAction,
+//   writeGoogleSheetAction(
+//     params: Omit<Parameters<typeof writeGoogleSheetAction>[0], "vaultClient">,
+//   ) {
+//     return writeGoogleSheetAction({ ...params, vaultClient });
+//   },
+// });
 
-export const createFlowActivities = ({
-  vaultClient,
-}: {
-  vaultClient: VaultClient;
-}) => ({
-  ...createFlowActionActivities({ vaultClient }),
-  generateFlowRunName,
-  persistFlowActivity,
-  userHasPermissionToRunFlowInWebActivity,
-});
+export const createFlowActivities = () =>
+  //   {
+  //   vaultClient,
+  // }: {
+  //   vaultClient: VaultClient;
+  // }
+  ({
+    // ...createFlowActionActivities({ vaultClient }),
+    generateFlowRunName,
+    persistFlowActivity,
+    userHasPermissionToRunFlowInWebActivity,
+  });
