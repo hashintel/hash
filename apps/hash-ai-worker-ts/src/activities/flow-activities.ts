923b99219f8988c7e1bba1f8df2d5701a015a532
Ciaran Morinan <c@hash.ai>
2024-06-07 12:14:46 +0100

    remove an activity

--- apps/hash-ai-worker-ts/src/activities/flow-activities.ts
+++ apps/hash-ai-worker-ts/src/activities/flow-activities.ts
@@ -1,7 +1,7 @@
 import type { VaultClient } from "@local/hash-backend-utils/vault";
 
 // import type { ActionDefinitionId } from "@local/hash-isomorphic-utils/flows/action-definitions";
-import { answerQuestionAction } from "./flow-activities/answer-question-action";
+// import { answerQuestionAction } from "./flow-activities/answer-question-action";
 import { generateFlowRunName } from "./flow-activities/generate-flow-run-name-activity";
 import { generateWebQueriesAction } from "./flow-activities/generate-web-queries-action";
 // import { getFileFromUrlAction } from "./flow-activities/get-file-from-url-action";
