39646f080b54e02b60542ea4ebc3740b2d59954e
Ciaran Morinan <c@hash.ai>
2024-06-07 12:08:47 +0100

    wip

--- libs/@local/dspy/infer-facts.py
+++ libs/@local/dspy/infer-facts.py
@@ -1,18 +1,32 @@
 import dspy
+import json
 from pydantic import BaseModel, Field
+from typing import List, Optional
 
-gpt4o = dspy.OpenAI(model='gpt-4o', max_tokens=80_000)
-dspy.settings.configure(lm=gpt4o)
-
+# Define the types we will need to refer to in the DSPy program signature's input and output fields
 class Fact(BaseModel):
     text: str = Field(description="A single fact expressed as a [subject] [predicate] [object] sentence")
-    prepositionalPhrases: str = Field(description="A list of prepositional phrases that provide additional context to the predicate in the fact")
-    date: datetime.date
-    confidence: float = Field(gt=0, lt=1)
+    prepositionalPhrases: List[str] = Field(description="A list of prepositional phrases that provide additional context to the predicate in the fact")
+    objectEntityLocalId: Optional[str] = Field(description="The localId of the entity that is the object if the fact, if any. Facts don't need to have entities as their object", default = None)
 
+class EntitySummary(BaseModel):
+    localId: str = Field(description="The localId of the entity")
+    name: str = Field(description="The name of the entity")
+
+# Define the DSPy Signature, which represents the input -> output expectations of the program
 class FactInferrer(dspy.Signature):
     """Infer facts about named entities from a piece of context"""
     
-    context = dspy.InputField
-    facts: list[Fact] = dspy.OutputField
+    context: str = dspy.InputField()
+    subjectEntity: EntitySummary = dspy.InputField(description="The entity that must be the subject of the facts")
+    potentialObjectEntities: List[EntitySummary] = dspy.InputField(description="The entities that could be the object of the facts")
+
+    facts: list[Fact] = dspy.OutputField()
   
+# Load the language model to use
+gpt4o = dspy.OpenAI(model='gpt-4o', max_tokens=80_000)
+dspy.settings.configure(lm=gpt4o)
+
+with open("input.json") as f:
+    input_data = json.load(f)
+    print(input_data)
