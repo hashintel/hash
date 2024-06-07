e1393888715d473a4a5b2d13e54bf5b9635c701e
Tim Diekmann <post@timdiekmann.de>
2024-06-07 23:44:36 +0200

    Fix entity type conversion to link type not working

--- apps/hash-frontend/src/pages/shared/entity-type-page.tsx
+++ apps/hash-frontend/src/pages/shared/entity-type-page.tsx
@@ -233,14 +233,14 @@ export const EntityTypePage = ({
     : extractVersion(entityType.schema.$id);
 
   const convertToLinkType = wrapHandleSubmit(async (data) => {
-    const entityTypeSchema = getEntityTypeFromFormData(data);
+    const { icon, labelProperty, schema } = getEntityTypeFromFormData(data);
 
     const res = await updateEntityType(
       {
-        ...entityTypeSchema,
+        ...schema,
         allOf: [{ $ref: linkEntityTypeUrl }],
       },
-      { icon: data.icon },
+      { icon, labelProperty: labelProperty as BaseUrl },
     );
 
     if (!res.errors?.length && res.data) {
