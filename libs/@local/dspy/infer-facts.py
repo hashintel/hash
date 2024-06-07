7b327c4d54910be6580677a7f9e5c3916f8f3035
Ciaran Morinan <c@hash.ai>
2024-06-07 11:35:20 +0100

    wip

--- libs/@local/dspy/infer-facts.py
+++ libs/@local/dspy/infer-facts.py
@@ -1,11 +1,18 @@
 import dspy
+from pydantic import BaseModel, Field
 
 gpt4o = dspy.OpenAI(model='gpt-4o', max_tokens=80_000)
 dspy.settings.configure(lm=gpt4o)
+
+class Fact(BaseModel):
+    text: str = Field(description="A single fact expressed as a [subject] [predicate] [object] sentence")
+    prepositionalPhrases: str = Field(description="A list of prepositional phrases that provide additional context to the predicate in the fact")
+    date: datetime.date
+    confidence: float = Field(gt=0, lt=1)
 
 class FactInferrer(dspy.Signature):
     """Infer facts about named entities from a piece of context"""
     
     context = dspy.InputField
-    facts = dspy.OutputField
+    facts: list[Fact] = dspy.OutputField
   
