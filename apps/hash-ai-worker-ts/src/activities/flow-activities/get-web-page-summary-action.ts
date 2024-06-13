9c602c09ca0868b4934fa0f916d93a837f639a91
Ben Werner <ben.werner01@gmail.com>
2024-06-13 15:51:47 +0200

    refactor: allow for specifying anthropic models as action inputs, use `haiku` to summarise web page content

--- apps/hash-ai-worker-ts/src/activities/flow-activities/get-web-page-summary-action.ts
+++ apps/hash-ai-worker-ts/src/activities/flow-activities/get-web-page-summary-action.ts
@@ -11,7 +11,7 @@ import { getFlowContext } from "../shared/get-flow-context";
 import { getLlmResponse } from "../shared/get-llm-response";
 import { getTextContentFromLlmMessage } from "../shared/get-llm-response/llm-message";
 import { graphApiClient } from "../shared/graph-api-client";
-import { modelAliasToSpecificModel } from "../shared/openai-client";
+import { inferenceModelAliasToSpecificModel } from "../shared/inference-model-alias-to-llm-model";
 import type { FlowActionActivity } from "./types";
 
 const generateSummarizeWebPageSystemPrompt = (params: {
@@ -66,7 +66,7 @@ export const getWebPageSummaryAction: FlowActionActivity = async ({
           ],
         },
       ],
-      model: modelAliasToSpecificModel[model],
+      model: inferenceModelAliasToSpecificModel[model],
     },
     {
       userAccountId: userAuthentication.actorId,
