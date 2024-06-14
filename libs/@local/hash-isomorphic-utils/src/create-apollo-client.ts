920944b9a480b186cdc4e9c28b0ddb9019e988b6
Ciaran Morinan <c@hash.ai>
2024-06-14 15:06:22 +0100

    type fix

--- libs/@local/hash-isomorphic-utils/src/create-apollo-client.ts
+++ libs/@local/hash-isomorphic-utils/src/create-apollo-client.ts
@@ -42,7 +42,7 @@ const errorLink = onError(({ graphQLErrors, operation }) => {
 export const createApolloClient = (params?: {
   name?: string;
   isBrowser: boolean;
-  additionalHeaders?: { [key: string]: string | undefined };
+  additionalHeaders?: { [key: string]: string };
 }): ApolloClient<NormalizedCacheObject> => {
   /**
    * This wraps fetch to inject the query operation name into the URL, which makes it easier
