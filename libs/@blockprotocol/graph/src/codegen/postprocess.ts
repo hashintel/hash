9eb43ae4031b361a6e8ad888f637d72181ac79c6
Ciaran Morinan <c@hash.ai>
2024-06-27 19:36:30 +0100

    lint fix

--- libs/@blockprotocol/graph/src/codegen/postprocess.ts
+++ libs/@blockprotocol/graph/src/codegen/postprocess.ts
@@ -1,6 +1,7 @@
-import type { PostprocessContext } from "./context/postprocess";
-import { allocateTypesToFiles } from "./postprocess/allocate-types-to-files";
-import { appendIdentifierDefinitionsToFileContents } from "./postprocess/append-identifier-definitions-to-file-contents";
+import type { PostprocessContext } from "./context/postprocess.js";
+import { addMetadataDependenciesToFiles } from "./postprocess/add-metadata-dependencies-to-files";
+import { allocateTypesToFiles } from "./postprocess/allocate-types-to-files.js";
+import { appendIdentifierDefinitionsToFileContents } from "./postprocess/append-identifier-definitions-to-file-contents.js";
 import { generateBlockEntityTypeAliases } from "./postprocess/generate-block-entity-type-aliases";
 import { generateBlockLinkTargetAliases } from "./postprocess/generate-block-link-target-aliases";
 import { generateEntityDefinitions } from "./postprocess/generate-entity-definitions";
@@ -9,7 +10,6 @@ import { prepareFileContents } from "./postprocess/prepare-file-contents";
 import { prependBannerComments } from "./postprocess/prepend-banner-comments";
 import { prependImportsAndExports } from "./postprocess/prepend-imports-and-exports";
 import { writeToFiles } from "./postprocess/write-to-files";
-import { addMetadataDependenciesToFiles } from "./postprocess/add-metadata-dependencies-to-files";
 
 export const postprocess = async (context: PostprocessContext) => {
   allocateTypesToFiles(context);
