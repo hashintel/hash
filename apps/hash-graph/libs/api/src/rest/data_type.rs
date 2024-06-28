c8474004e5917de928e67d1d37c9f30234c77d0a
Dei Vilkinsons <6226576+vilkinsons@users.noreply.github.com>
2024-06-28 12:59:30 +0100

    Update apps/hash-graph/libs/api/src/rest/data_type.rs
    Co-authored-by: Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>

--- apps/hash-graph/libs/api/src/rest/data_type.rs
+++ apps/hash-graph/libs/api/src/rest/data_type.rs
@@ -566,8 +566,8 @@ where
     let data_type = patch_id_and_parse(&type_to_update, schema).map_err(|report| {
         tracing::error!(error=?report, "Couldn't patch schema and convert to Data Type");
         StatusCode::UNPROCESSABLE_ENTITY
-        // @todo We should probably return more information to the client
-        // @see https://linear.app/hash/issue/H-3009
+        // TODO: We should probably return more information to the client
+        //   see https://linear.app/hash/issue/H-3009
     })?;
 
     let authorization_api = authorization_api_pool.acquire().await.map_err(|error| {
