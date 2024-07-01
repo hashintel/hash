dada9f1dcf7c6819a008cc5395d3a3c9f0d9ab2d
Ciaran Morinan <c@hash.ai>
2024-07-01 15:20:07 +0100

    fn signature fix

--- apps/hash-frontend/src/pages/[shortname]/entities/[entity-uuid].page/entity-editor/links-section/link-table/cells/sort-link-and-target-entities.ts
+++ apps/hash-frontend/src/pages/[shortname]/entities/[entity-uuid].page/entity-editor/links-section/link-table/cells/sort-link-and-target-entities.ts
@@ -1,8 +1,8 @@
 import type { LinkAndTargetEntity } from "../types";
 
-export const sortLinkAndTargetEntities = <T extends LinkAndTargetEntity[]>(
-  linkAndTargetEntities: T,
-) => {
+export const sortLinkAndTargetEntities = <T extends LinkAndTargetEntity>(
+  linkAndTargetEntities: T[],
+): T[] => {
   return linkAndTargetEntities.toSorted((a, b) =>
     a.linkEntity.metadata.temporalVersioning.decisionTime.start.limit.localeCompare(
       b.linkEntity.metadata.temporalVersioning.decisionTime.start.limit,
