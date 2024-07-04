9b227dc8dab77b2ad9d27024ccd4f2b57f2bec45
Ciaran Morinan <c@hash.ai>
2024-07-04 12:28:14 +0100

    more fixes

--- libs/@local/hash-isomorphic-utils/src/slugify.ts
+++ libs/@local/hash-isomorphic-utils/src/slugify.ts
@@ -1,5 +1,6 @@
 import slugifyLib from "slugify";
 
 export const slugify = (text: string) =>
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore -- https://github.com/simov/slugify/issues/173
   slugifyLib(text, { lower: true, strict: true });
