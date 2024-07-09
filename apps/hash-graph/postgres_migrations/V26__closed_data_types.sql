efc88537569b1b64e248cf4400a98c0be72b9ea8
Tim Diekmann <post@timdiekmann.de>
2024-07-09 19:16:42 +0200

    Adjust database to store a plain `DataType` as closed schema

--- apps/hash-graph/postgres_migrations/V26__closed_data_types.sql
+++ apps/hash-graph/postgres_migrations/V26__closed_data_types.sql
@@ -2,27 +2,18 @@ ALTER TABLE data_types
     ADD COLUMN closed_schema JSONB;
 
 UPDATE data_types
-   SET closed_schema = jsonb_strip_nulls(jsonb_build_object(
-        'schemas', jsonb_build_object(
-            schema->>'$id', jsonb_build_object(
-                'title', schema->'title',
-                'description', schema->'description',
-                'label', schema->'label'
-            )
-        ),
-        'type', schema->'type',
-        'const', schema->'const',
-        'enum', schema->'enum',
-        'multipleOf', schema->'multipleOf',
-        'maximum', schema->'maximum',
-        'exclusiveMaximum', schema->'exclusiveMaximum',
-        'minimum', schema->'minimum',
-        'exclusiveMinimum', schema->'exclusiveMinimum',
-        'minLength', schema->'minLength',
-        'maxLength', schema->'maxLength',
-        'pattern', schema->'pattern',
-        'format', schema->'format'
-   ));
+   SET closed_schema = schema;
 
 ALTER TABLE data_types
     ALTER COLUMN closed_schema SET NOT NULL;
+
+CREATE TABLE data_type_inherits_from (
+    source_data_type_ontology_id UUID NOT NULL REFERENCES data_types,
+    target_data_type_ontology_id UUID NOT NULL REFERENCES data_types,
+    depth INT NOT NULL
+);
+
+CREATE TABLE data_type_constrains_values_on (
+    source_data_type_ontology_id UUID NOT NULL REFERENCES data_types,
+    target_data_type_ontology_id UUID NOT NULL REFERENCES data_types
+);
