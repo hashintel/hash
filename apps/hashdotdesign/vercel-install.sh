c93a8fc7bde8a77d0510cc18aa5c6ea563706840
Tim Diekmann <post@timdiekmann.de>
2024-07-09 11:35:07 +0200

    Fix `jq` installation

--- apps/hashdotdesign/vercel-install.sh
+++ apps/hashdotdesign/vercel-install.sh
@@ -7,7 +7,7 @@ echo "Installing prerequisites"
 yum install -y jq
 
 echo "Installing turbo"
-yarn global add "turbo@$(jq -r '.devDependencies.turbo' < package.json)"cd
+yarn global add "turbo@$(jq -r '.devDependencies.turbo' < package.json)"
 
 echo "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none --profile minimal
