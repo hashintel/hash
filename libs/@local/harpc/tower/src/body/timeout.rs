a0f35fdef06b32510b1dbba4b9b57ca81d0a36aa
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-07-13 22:11:59 +0200

    test: timeout

--- libs/@local/harpc/tower/src/body/timeout.rs
+++ libs/@local/harpc/tower/src/body/timeout.rs
@@ -102,3 +102,198 @@ where
         self.inner.size_hint()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{assert_matches::assert_matches, time::Duration};
+
+    use bytes::Bytes;
+    use error_stack::Report;
+    use futures::{stream, StreamExt};
+    use tokio::pin;
+    use tokio_util::time::{delay_queue::Expired, DelayQueue};
+
+    use super::FrameTimeout;
+    use crate::body::{
+        full::Full, stream::StreamBody, timeout::TimeoutError, Body, BodyExt, BodyState, Frame,
+    };
+
+    const HELLO: &[u8] = b"hello";
+    const WORLD: &[u8] = b"world";
+
+    #[tokio::test]
+    async fn single_item() {
+        let body = FrameTimeout::new(
+            Full::new(Bytes::from_static(HELLO)).map_err::<_, Report<!>>(|error| match error {}),
+            Duration::from_secs(1),
+        );
+        pin!(body);
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == HELLO
+        );
+
+        assert_eq!(body.state(), Some(BodyState::Complete));
+    }
+
+    #[tokio::test]
+    async fn single_item_immediate_resolve() {
+        let body = FrameTimeout::new(
+            Full::new(Bytes::from_static(HELLO)).map_err::<_, Report<!>>(|error| match error {}),
+            Duration::from_millis(10),
+        );
+        pin!(body);
+
+        tokio::time::sleep(Duration::from_millis(50)).await;
+
+        // this works because we (just like tokio timeout) first give the item one last chance to
+        // resolve as to not to create any race conditions.
+        let value = body.frame().await;
+        assert_matches!(value, Some(Ok(Frame::Data(data))) if data.as_ref() == HELLO);
+        assert_eq!(body.state(), Some(BodyState::Complete));
+    }
+
+    #[tokio::test]
+    async fn single_itme_delayed() {
+        let mut queue = DelayQueue::new();
+        queue.insert(
+            Result::<_, Report<!>>::Ok(Frame::<_, !>::Data(Bytes::from_static(HELLO))),
+            Duration::from_millis(10),
+        );
+
+        let body = FrameTimeout::new(
+            StreamBody::new(queue.map(Expired::into_inner)),
+            Duration::from_millis(50),
+        );
+        pin!(body);
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == HELLO
+        );
+
+        let value = body.frame().await;
+        assert_matches!(value, None);
+        assert_eq!(body.state(), Some(BodyState::Complete));
+    }
+
+    #[tokio::test]
+    async fn single_item_timeout() {
+        let mut queue = DelayQueue::new();
+        queue.insert(
+            Result::<_, Report<!>>::Ok(Frame::<_, !>::Data(Bytes::from_static(HELLO))),
+            Duration::from_millis(50),
+        );
+
+        let body = FrameTimeout::new(
+            StreamBody::new(queue.map(Expired::into_inner)),
+            Duration::from_millis(10),
+        );
+        pin!(body);
+
+        let value = body.frame().await;
+        assert_matches!(value, Some(Err(error)) if *error.current_context() == TimeoutError::DeadlineExceeded {timeout: Duration::from_millis(10)});
+        assert_eq!(body.state(), Some(BodyState::Incomplete));
+    }
+
+    #[tokio::test]
+    async fn multiple_items() {
+        let body = FrameTimeout::new(
+            StreamBody::new(stream::iter(vec![
+                Result::<_, Report<!>>::Ok(Frame::<_, !>::Data(Bytes::from_static(HELLO))),
+                Ok(Frame::Data(Bytes::from_static(WORLD))),
+            ])),
+            Duration::from_secs(1),
+        );
+
+        pin!(body);
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == HELLO
+        );
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == WORLD
+        );
+
+        let value = body.frame().await;
+        assert_matches!(value, None);
+
+        assert_eq!(body.state(), Some(BodyState::Complete));
+    }
+
+    #[tokio::test]
+    async fn multiple_items_delayed() {
+        let mut queue = DelayQueue::new();
+        queue.insert(
+            Result::<_, Report<!>>::Ok(Frame::<_, !>::Data(Bytes::from_static(HELLO))),
+            Duration::from_millis(10),
+        );
+        queue.insert(
+            Ok(Frame::Data(Bytes::from_static(WORLD))),
+            Duration::from_millis(20),
+        );
+
+        let body = FrameTimeout::new(
+            StreamBody::new(queue.map(Expired::into_inner)),
+            Duration::from_millis(50),
+        );
+
+        pin!(body);
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == HELLO
+        );
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == WORLD
+        );
+
+        let value = body.frame().await;
+        assert_matches!(value, None);
+
+        assert_eq!(body.state(), Some(BodyState::Complete));
+    }
+
+    #[tokio::test]
+    async fn multiple_items_timeout() {
+        let mut queue = DelayQueue::new();
+        queue.insert(
+            Result::<_, Report<!>>::Ok(Frame::<_, !>::Data(Bytes::from_static(HELLO))),
+            Duration::from_millis(10),
+        );
+        queue.insert(
+            Ok(Frame::Data(Bytes::from_static(WORLD))),
+            Duration::from_millis(40),
+        );
+
+        let body = FrameTimeout::new(
+            StreamBody::new(queue.map(Expired::into_inner)),
+            Duration::from_millis(20),
+        );
+
+        pin!(body);
+
+        let value = body.frame().await;
+        assert_matches!(
+            value,
+            Some(Ok(Frame::Data(data))) if data.as_ref() == HELLO
+        );
+
+        let value = body.frame().await;
+        assert_matches!(value, Some(Err(error)) if *error.current_context() == TimeoutError::DeadlineExceeded {timeout: Duration::from_millis(20)});
+
+        assert_eq!(body.state(), Some(BodyState::Incomplete));
+    }
+}
