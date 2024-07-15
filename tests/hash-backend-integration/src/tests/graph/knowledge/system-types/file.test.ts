0a019d12886b10bd6843467d4c68667e04a6e5ec
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2024-07-15 08:59:11 +0200

    Add missing `dataTypeId`s

--- tests/hash-backend-integration/src/tests/graph/knowledge/system-types/file.test.ts
+++ tests/hash-backend-integration/src/tests/graph/knowledge/system-types/file.test.ts
@@ -73,7 +73,7 @@ describe("File", () => {
                   },
                 },
             },
-          },
+          } as const,
           presignedPut: { url: uploadUrl },
         }),
       ),
