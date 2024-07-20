9f056f58c8920352d2f9074adc7425f15048933a
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-07-20 18:17:33 +0200

    first: first CST parser

--- libs/@local/hql/cst/src/lib.rs
+++ libs/@local/hql/cst/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use justjson::parser::{Token, Tokenizer};
+use winnow::{
+    combinator::{delimited, opt, separated},
+    token, Parser,
+};
+
+// TODO: do we want instead to use the justjson visitor API? (probably)
+fn parse_expr() {}
+
+fn parse_string_literal() {
+    // TODO: is this correct?!
+    return token::any.try_map(|token: Token| {
+        if let Token::String(s) = token {
+            Ok(s)
+        } else {
+            // TODO: proper error w/ location
+            Err("Expected string literal".to_string())
+        }
+    });
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn parse_function_call(tokenizer: &mut Tokenizer<true>) {
+    delimited(
+        Token::Array,
+        (
+            parse_string_literal(), // first argument is a string literal
+            opt((
+                token::literal(Token::Comma),
+                separated(0.., parse_expr(), Token::Comma),
+            )),
+            opt(token::literal(Token::Comma)), // optional trailing comma
+        ),
+        Token::ArrayEnd,
+    );
+    // TODO: arguments
 }
