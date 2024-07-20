50523745ad26189ec37f97870d21dbef473c0851
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-07-20 17:54:17 +0200

    feat: documentation and CST

--- libs/@local/hql/ast/src/lib.rs
+++ libs/@local/hql/ast/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! # AST for the `hql` language
+//!
+//! This is the same as the AST, it still tries to retain any code location information, but
+//! resolves any special form functions into proper AST nodes.
