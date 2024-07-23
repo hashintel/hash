429fca1142b58dfbe9c6eaba43db31cec86d35b9
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-07-23 14:40:47 +0200

    feat: license correctly

--- libs/@local/hql/ast/src/lib.rs
+++
@@ -1,4 +0,0 @@
-//! # AST for the `hql` language
-//!
-//! This is the same as the AST, it still tries to retain any code location information, but
-//! resolves any special form functions into proper AST nodes.
