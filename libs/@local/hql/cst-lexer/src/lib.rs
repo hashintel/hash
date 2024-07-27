f192622a56be8ce728b4f558b550f9fe29f6703e
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-07-27 19:01:06 +0200

    feat: try justjson again
    man idk, I feel like I just… lost myself.
    
    I just wanna know where the contents is located. Okay?

--- libs/@local/hql/cst-lexer/src/lib.rs
+++ libs/@local/hql/cst-lexer/src/lib.rs
@@ -1,171 +1,217 @@
-use core::str;
-use std::borrow::Cow;
+use justjson::{parser::PeekableTokenKind, ErrorKind, JsonNumber, JsonString};
+use text_size::{TextRange, TextSize};
 
-use logos::{Lexer, Logos};
-
-macro_rules! eof {
-    ($bytes:ident) => {{
-        let Some((&byte, remainder)) = $bytes.split_first() else {
-            panic!("Unexpected end of input");
-        };
-
-        $bytes = remainder;
-        byte
-    }};
+macro_rules! size {
+    ($expr:expr) => {
+        TextSize::try_from($expr).expect("infallible, input is never larger than 4GiB")
+    };
+    ($start:expr, $end:expr) => {
+        TextRange::new(size!($start), size!($end))
+    };
 }
 
-fn parse_escape() {}
+pub struct Error {
+    kind: ErrorKind,
+    range: TextRange,
+}
 
-fn parse_byte(current: u8, remainder: &mut &[u8]) {
-    // lookup ASCII characters
-    match current {
-        0x20 | 0x21 | 0x23..=0x5B | 0x5D..=0x7F => {
-            // valid unicode characters, therefore we can just push them
-            current
-        }
-        b'"' => {
-            // closing quote
-            break;
-        }
-        b'\\' => {
-            // escape sequence
-            let next = eof!(remainder);
-            match next {
-                b'"' | b'\\' | b'/' => next,
-                b'b' => b'\x08',
-                b'f' => b'\x0C',
-                b'n' => b'\n',
-                b'r' => b'\r',
-                b't' => b'\t',
-                b'u' => {
-                    // unicode escape sequence
-                    // we need to read 4 hex digits
-                    // they follow one after the other, so we can just read them
-
-                    let mut codepoint = 0;
-                    for _ in 0..4 {
-                        let next = eof!(remainder);
-                        let digit = match next {
-                            b'0'..=b'9' => next - b'0',
-                            b'a'..=b'f' => next - b'a' + 10,
-                            b'A'..=b'F' => next - b'A' + 10,
-                            _ => panic!("Invalid unicode escape sequence"),
-                        };
-
-                        codepoint = codepoint * 16 + digit as u32;
-                    }
-
-                    codepoint
-                }
-                _ => panic!("Invalid escape sequence"),
-            }
+impl From<justjson::Error> for Error {
+    fn from(error: justjson::Error) -> Self {
+        Self {
+            kind: error.kind().clone(),
+            range: TextRange::empty(size!(error.offset())),
         }
     }
 }
 
-fn parse_string(lex: &mut Lexer<Token>) -> Result<(), ()> {
-    // The lexer has encountered a string token (by using a quote - ")
+pub enum SyntaxKind {
+    Null,
+    True,
+    False,
+    String,
+    Number,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+    Colon,
+    Comma,
+    Unrecognized,
+}
 
-    // iterate over all remaining characters, until we find the closing quote
-    let mut remaining = lex.remainder();
-    // TODO: problem are utf8 surrogates, we can't easily handle them here in the escape!
-    let mut output = Cow::Borrowed(&[]);
+impl From<PeekableTokenKind> for SyntaxKind {
+    fn from(value: PeekableTokenKind) -> Self {
+        match value {
+            PeekableTokenKind::Null => Self::Null,
+            PeekableTokenKind::True => Self::True,
+            PeekableTokenKind::False => Self::False,
+            PeekableTokenKind::String => Self::String,
+            PeekableTokenKind::Number => Self::Number,
+            PeekableTokenKind::Object => Self::LBrace,
+            PeekableTokenKind::ObjectEnd => Self::RBrace,
+            PeekableTokenKind::Array => Self::LBracket,
+            PeekableTokenKind::ArrayEnd => Self::RBracket,
+            PeekableTokenKind::Colon => Self::Colon,
+            PeekableTokenKind::Comma => Self::Comma,
+            PeekableTokenKind::Unrecognized => Self::Unrecognized,
+        }
+    }
+}
 
-    let mut bytes = remaining.as_bytes();
-    loop {
-        let byte = eof!(bytes);
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum TokenKind<'a> {
+    Null,
+    Bool(bool),
+    String(JsonString<'a>),
+    Number(JsonNumber<'a>),
+    Object,
+    ObjectEnd,
+    Array,
+    ArrayEnd,
+    Colon,
+    Comma,
+}
 
-        match char {
-            '\\' => {
-                // escape sequence, next character needs to be: " \ / b f n r t (4 hex digits)
-                let (index, char) = eof!(chars);
-                let to_push = match char {
-                    '"' | '\\' | '/' => {
-                        // these are the same in rust
-                        char
-                    }
-                    'b' => '\x08',
-                    'f' => '\x0C',
-                    'n' => '\n',
-                    'r' => '\r',
-                    't' => '\t',
-                    '0'..='9' | 'a'..='f' | 'A'..='F' => {
-                        // unicode escape sequence
-                        // we need to read 4 hex digits
-                        // they follow one after the other, so we can just read them
+impl<'a> From<justjson::parser::Token<'a>> for TokenKind<'a> {
+    fn from(value: justjson::parser::Token<'a>) -> Self {
+        match value {
+            justjson::parser::Token::Null => Self::Null,
+            justjson::parser::Token::Bool(value) => Self::Bool(value),
+            justjson::parser::Token::String(value) => Self::String(value),
+            justjson::parser::Token::Number(value) => Self::Number(value),
+            justjson::parser::Token::Object => Self::Object,
+            justjson::parser::Token::ObjectEnd => Self::ObjectEnd,
+            justjson::parser::Token::Array => Self::Array,
+            justjson::parser::Token::ArrayEnd => Self::ArrayEnd,
+            justjson::parser::Token::Colon => Self::Colon,
+            justjson::parser::Token::Comma => Self::Comma,
+        }
+    }
+}
 
-                        let mut until;
-                        for _ in 0..3 {
-                            let (index, char) = eof!(chars);
-                            if char.is_ascii_hexdigit() {
-                                until = index;
-                            } else {
-                                panic!("Invalid unicode escape sequence at index {}", index);
-                            }
-                        }
-                        let mut slice = &remaining[index..until];
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Nested {
+    Object,
+    Array,
+}
 
-                        let codepoint = u32::from_str_radix(slice, 16).map_err(|_| ())?;
-                        std::char::from_u32(codepoint).unwrap()
-                    }
-                    _ => panic!("Invalid escape sequence at index {}", index),
+pub struct Token<'a> {
+    pub kind: TokenKind<'a>,
+    pub range: TextRange,
+}
+
+pub struct Lexer<'a> {
+    inner: justjson::parser::Tokenizer<'a, true>,
+    stack: Vec<Nested>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn peek(&mut self) -> Option<SyntaxKind> {
+        self.inner.peek().map(From::from)
+    }
+
+    /// Returns the next token in the stream.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the next token is invalid.
+    pub fn bump(&mut self) -> Result<Option<Token<'a>>, Error> {
+        let start = self.inner.offset();
+        let Some(token) = self.inner.next() else {
+            return Ok(None);
+        };
+        let token = token?;
+        let end = self.inner.offset();
+
+        Ok(Some(Token {
+            kind: token.into(),
+            range: size!(start, end),
+        }))
+    }
+
+    pub fn recover(&mut self, depth: usize) -> Result<(), Error> {
+        while self.stack.len() > depth {
+            let Some(next) = self.stack.pop() else {
+                return Err(Error {
+                    kind: ErrorKind::UnexpectedEof,
+                    range: TextRange::empty(size!(self.inner.offset())),
+                });
+            };
+
+            loop {
+                let Some(token) = self.bump()? else {
+                    return Err(Error {
+                        kind: ErrorKind::UnexpectedEof,
+                        range: TextRange::empty(size!(self.inner.offset())),
+                    });
                 };
 
-                output.to_mut().push(to_push);
-            }
-            '"' => {
-                // end of string
-                break;
-            }
-            '\x20' | '\x21' | '\x23'..'\x5C' | '\x5D'.. => {
-                // valid characters
-                match &mut output {
-                    Cow::Borrowed(slice) => {
-                        *slice = &remaining[..index];
-                    }
-                    Cow::Owned(value) => {
-                        value.push(char);
-                    }
+                if next == Nested::Object && token.kind == TokenKind::ObjectEnd {
+                    break;
+                }
+
+                if next == Nested::Array && token.kind == TokenKind::ArrayEnd {
+                    break;
                 }
             }
         }
+
+        Ok(())
     }
 
-    // we have successfully parsed the string
-    Ok(output)
-}
+    pub fn on_array<E>(
+        &mut self,
+        mut on_item: impl FnMut(&mut Self, Token<'a>) -> Result<(), E>,
+    ) -> Result<(), Error>
+    where
+        Error: From<E>,
+    {
+        // TODO: make this a report instead!
+        let mut errors = Vec::new();
+        // we assume that the LBracket token has already been consumed
+        // add a recovery checkpoint
+        self.stack.push(Nested::Array);
+        let depth = self.stack.len();
 
-#[derive(Debug, Logos)]
-#[logos(skip r"[ \t\r\n\f]+")]
-enum Token<'source> {
-    #[token("false", |_| false)]
-    #[token("true", |_| true)]
-    Bool(bool),
+        loop {
+            let Some(token) = self.bump()? else {
+                self.stack.pop();
+                return Err(Error {
+                    kind: ErrorKind::UnexpectedEof,
+                    range: TextRange::empty(size!(self.inner.offset())),
+                });
+            };
 
-    #[token("{")]
-    BraceOpen,
+            if token.kind == TokenKind::ArrayEnd {
+                break;
+            }
 
-    #[token("}")]
-    BraceClose,
+            if let Err(error) = on_item(self, token) {
+                self.recover(depth)?;
+                errors.push(error);
+            }
 
-    #[token("[")]
-    BracketOpen,
+            let Some(token) = self.bump()? else {
+                self.stack.pop();
+                return Err(Error {
+                    kind: ErrorKind::UnexpectedEof,
+                    range: TextRange::empty(size!(self.inner.offset())),
+                });
+            };
 
-    #[token("]")]
-    BracketClose,
+            // we're gracious and allow a trailing comma (even tho it is not allowed by the spec)
+            if token.kind != TokenKind::Comma && token.kind != TokenKind::ArrayEnd {
+                self.stack.pop();
+                // TODO: part of error accumulation
+                return Err(Error {
+                    kind: ErrorKind::ExpectedCommaOrEndOfArray,
+                    range: token.range,
+                });
+            }
+        }
 
-    #[token(":")]
-    Colon,
-
-    #[token(",")]
-    Comma,
-
-    #[token("null")]
-    Null,
-
-    #[regex(r"-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?", |lex| lex.slice().parse::<f64>().unwrap())]
-    Number(f64),
-
-    #[regex(r#""([^"\\]|\\["\\bnfrt]|u[a-fA-F0-9]{4})*""#, |lex| lex.slice())]
-    String(&'source str),
+        // remove the recovery checkpoint
+        self.stack.pop();
+        todo!()
+    }
 }
