eb8751353f415170182150c034aa716f880ea645
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2024-07-30 17:16:16 +0200

    H-3180: Read user entity after logging in (#4769)

--- tests/hash-backend-performance/src/session.ts
+++ tests/hash-backend-performance/src/session.ts
@@ -1,5 +1,12 @@
+import { publicUserAccountId } from "@local/hash-backend-utils/public-user-account-id";
+import type { OwnedById } from "@local/hash-graph-types/web";
+import type { SimpleProperties } from "@local/hash-isomorphic-utils/simplify-properties";
+import { simplifyProperties } from "@local/hash-isomorphic-utils/simplify-properties";
+import type { User } from "@local/hash-isomorphic-utils/system-types/shared";
+import { extractOwnedByIdFromEntityId } from "@local/hash-subgraph";
 import { Configuration, FrontendApi } from "@ory/client";
 
+import { getUserByKratosIdentityId } from "./graph";
 import type { BeforeRequest, RequestParams } from "./types";
 
 const API_ORIGIN = "http://127.0.0.1:5001";
@@ -9,6 +16,8 @@ type LoginContext = {
   session: {
     token: string;
     expiresAt?: string;
+    user: SimpleProperties<User["properties"]>;
+    ownedById: OwnedById;
   };
 };
 
@@ -67,9 +76,19 @@ export const refreshSessionToken: BeforeRequest<LoginContext> = async (
     throw new Error("Login failed");
   }
 
+  const user = await getUserByKratosIdentityId({
+    authentication: { actorId: publicUserAccountId },
+    kratosIdentityId: fullLogin.session.identity.id,
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
   context.vars.session = {
     token: fullLogin.session_token,
     expiresAt: fullLogin.session.expires_at,
+    user: simplifyProperties(user.properties),
+    ownedById: extractOwnedByIdFromEntityId(user.entityId),
   };
   setAuthorizationHeader(request, context.vars.session.token);
 };
