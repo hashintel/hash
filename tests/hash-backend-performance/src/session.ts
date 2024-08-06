2e6370470bbde6628c2d1e517456b65a531e6cb9
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2024-08-06 22:50:29 +0200

    H-3166: Mimic Flow behavior in artillery (#4771)

--- tests/hash-backend-performance/src/session.ts
+++ tests/hash-backend-performance/src/session.ts
@@ -7,63 +7,45 @@ import { extractOwnedByIdFromEntityId } from "@local/hash-subgraph";
 import { Configuration, FrontendApi } from "@ory/client";
 
 import { getUserByKratosIdentityId } from "./graph";
-import type { BeforeRequest, RequestParams } from "./types";
+import type { ActionFn, BeforeRequestFn } from "./types";
 
 const API_ORIGIN = "http://127.0.0.1:5001";
 
 type LoginContext = {
-  flowId: string;
   session: {
     token: string;
-    expiresAt?: string;
+    expiresAt?: number;
     user: SimpleProperties<User["properties"]>;
     ownedById: OwnedById;
   };
 };
 
-const setAuthorizationHeader = (request: RequestParams, token: string) => {
-  if (!request.headers) {
-    request.headers = {};
+let __oryKratosClient: FrontendApi | undefined;
+const getOryKratosClient = () => {
+  if (!__oryKratosClient) {
+    __oryKratosClient = new FrontendApi(
+      new Configuration({
+        basePath: `${API_ORIGIN}/auth`,
+        baseOptions: {
+          withCredentials: true,
+        },
+      }),
+    );
   }
-  request.headers.Authorization = `Bearer ${token}`;
+
+  return __oryKratosClient;
 };
 
-export const refreshSessionToken: BeforeRequest<LoginContext> = async (
-  request,
-  context,
-) => {
-  if (context.vars.session) {
-    if (context.vars.session.expiresAt) {
-      if (context.vars.session.expiresAt > new Date().toISOString()) {
-        setAuthorizationHeader(request, context.vars.session.token);
-        return;
-      }
-    } else {
-      setAuthorizationHeader(request, context.vars.session.token);
-      return;
-    }
-  }
+export const login: ActionFn<LoginContext> = async (context) => {
+  const oryKratosClient = getOryKratosClient();
 
-  const oryKratosClient = new FrontendApi(
-    new Configuration({
-      basePath: `${API_ORIGIN}/auth`,
-      baseOptions: {
-        withCredentials: true,
-      },
-    }),
-  );
-
-  if (!context.vars.flowId) {
-    const loginFlow = await oryKratosClient
-      .createNativeLoginFlow()
-      .then(({ data }) => data);
-
-    context.vars.flowId = loginFlow.id;
-  }
+  const loginFlow = await oryKratosClient
+    .createNativeLoginFlow()
+    .then(({ data }) => data);
 
   const fullLogin = await oryKratosClient
     .updateLoginFlow({
-      flow: context.vars.flowId,
+      flow: loginFlow.id,
       updateLoginFlowBody: {
         method: "password",
         password_identifier: "email",
@@ -86,9 +68,27 @@ export const refreshSessionToken: BeforeRequest<LoginContext> = async (
 
   context.vars.session = {
     token: fullLogin.session_token,
-    expiresAt: fullLogin.session.expires_at,
+    expiresAt: fullLogin.session.expires_at
+      ? new Date(fullLogin.session.expires_at).valueOf()
+      : undefined,
     user: simplifyProperties(user.properties),
     ownedById: extractOwnedByIdFromEntityId(user.entityId),
   };
-  setAuthorizationHeader(request, context.vars.session.token);
+};
+
+export const refreshSessionToken: BeforeRequestFn<LoginContext> = async (
+  request,
+  context,
+  events,
+) => {
+  if (
+    !context.vars.session ||
+    (context.vars.session.expiresAt &&
+      context.vars.session.expiresAt < Date.now())
+  ) {
+    await login(context, events);
+  }
+
+  request.headers ??= {};
+  request.headers.Authorization = `Bearer ${context.vars.session!.token}`;
 };
