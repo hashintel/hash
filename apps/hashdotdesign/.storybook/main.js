726e5712a27dc56fc57d382121f14fd5e1715e74
Dei Vilkinsons <6226576+vilkinsons@users.noreply.github.com>
2024-08-06 22:29:22 +0100

    `yarn dlx storybook@latest upgrade` update `main.js`

--- apps/hashdotdesign/.storybook/main.js
+++ apps/hashdotdesign/.storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 module.exports = {
   stories: [
     "../../../libs/@hashintel/design-system/src/**/*.mdx",
@@ -5,19 +6,32 @@ module.exports = {
     "../../../libs/@hashintel/block-design-system/src/**/*.mdx",
     "../../../libs/@hashintel/block-design-system/src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    "@storybook/addon-webpack5-compiler-babel"
   ],
+
   framework: {
-    name: "@storybook/react-webpack5",
+    name: getAbsolutePath("@storybook/react-webpack5"),
     options: {},
   },
+
   core: {
     disableTelemetry: true,
   },
+
   docs: {
     docsPage: true,
   },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
