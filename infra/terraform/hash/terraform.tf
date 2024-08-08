527ec254a4bdfd630a269b5d6751ab3ae766c3df
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2024-08-08 12:24:37 +0100

    H-3055: Route prod traffic via Cloudflare; fix client IP identification (#4735)

--- infra/terraform/hash/terraform.tf
+++ infra/terraform/hash/terraform.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.67"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
     postgresql = {
       source  = "cyrilgdn/postgresql"
       version = "1.18.0"
