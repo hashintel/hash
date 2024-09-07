cf701f306a636a4fb8c6380bac22b16287026413
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-09-07 18:20:21 +0200

    feat: first impl

---
+++ libs/@local/harpc/service/src/account.rs
@@ -0,0 +1,146 @@
+//! Example Implementation just to get everything right, to be removed before PR merging
+
+use core::future::Future;
+
+use error_stack::Result;
+use graph_types::account::AccountId;
+use harpc_net::session::server::SessionId;
+use harpc_types::{service::ServiceId, version::Version};
+
+pub trait ProcedureId: Sized {
+    fn from_u32(id: u32) -> Option<Self>;
+    fn into_u32(self) -> u32;
+}
+
+pub struct Deprecation {
+    since: Version,
+}
+
+pub struct Removal {
+    r#in: Version,
+}
+
+pub trait Procedure {
+    type Id: ProcedureId;
+    type Response;
+
+    const ID: Self::Id;
+
+    fn since() -> Version;
+
+    fn deprecation() -> Option<Deprecation> {
+        None
+    }
+    fn removal() -> Option<Removal> {
+        None
+    }
+}
+
+pub struct CreateAccount {
+    id: Option<AccountId>,
+}
+
+impl Procedure for CreateAccount {
+    type Id = AccountProcedureId;
+    type Response = AccountId;
+
+    const ID: Self::Id = AccountProcedureId::CreateAccount;
+
+    fn since() -> Version {
+        Version {
+            major: 0x00,
+            minor: 0x00,
+        }
+    }
+}
+
+pub trait Session {
+    fn id(&self) -> SessionId;
+    // TODO: do we need session specific state?!
+}
+
+pub struct AccountError;
+
+pub enum AccountProcedureId {
+    CreateAccount,
+}
+
+impl ProcedureId for AccountProcedureId {
+    fn from_u32(id: u32) -> Option<Self> {
+        match id {
+            0 => Some(Self::CreateAccount),
+            _ => None,
+        }
+    }
+
+    fn into_u32(self) -> u32 {
+        match self {
+            Self::CreateAccount => 0,
+        }
+    }
+}
+
+pub struct State<A, S, T> {
+    account_api_pool: A,
+    store_pool: S,
+    temporal_client: T,
+}
+
+pub trait Role {
+    type Session: Session;
+}
+
+pub trait AccountService<R> {
+    fn create_account(
+        &self,
+        session: &R::Session,
+        payload: CreateAccount,
+    ) -> impl Future<Output = Result<AccountId, AccountError>>
+    where
+        R: Role;
+}
+
+pub struct AccountServiceServer<A, S, T> {
+    state: State<A, S, T>,
+}
+
+pub struct Server;
+
+impl Role for Server {
+    type Session = Server;
+}
+
+impl Session for Server {
+    fn id(&self) -> SessionId {
+        todo!()
+    }
+}
+
+impl<A, S, T> AccountService<Server> for AccountServiceServer<A, S, T>
+where
+    A: Send + Sync,
+    S: Send + Sync,
+    T: Send + Sync,
+{
+    async fn create_account(
+        &self,
+        session: &Server,
+        payload: CreateAccount,
+    ) -> Result<AccountId, AccountError> {
+        todo!("Implement me!")
+    }
+}
+
+pub trait Service {
+    const ID: ServiceId;
+    const VERSION: Version;
+
+    fn since() -> Version;
+
+    fn deprecation() -> Option<Deprecation> {
+        None
+    }
+    fn removal() -> Option<Removal> {
+        None
+    }
+}
