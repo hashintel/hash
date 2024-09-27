cccc260b8d5f15637202f7f0d65b331270f08402
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-09-28 01:13:10 +0200

    feat: example

--- libs/@local/harpc/service/examples/account.rs
+++ libs/@local/harpc/service/examples/account.rs
@@ -3,17 +3,23 @@ use core::marker::PhantomData;
 
 use error_stack::Report;
 use frunk::HList;
+use futures::pin_mut;
 use graph_types::account::AccountId;
+use harpc_net::codec::{Codec, Decoder, Encoder};
 use harpc_service::{
-    delegate::ServiceDelegate,
+    delegate::{CodecRequirement, ServiceDelegate},
     metadata::Metadata,
     procedure::{Procedure, ProcedureIdentifier},
     role::{Client, ClientSession, Role, Server},
     service::Service,
 };
-use harpc_tower::{body::Body, request::Request, response::Response};
+use harpc_tower::{
+    body::{Body, BodyExt},
+    request::Request,
+    response::Response,
+};
 use harpc_types::{procedure::ProcedureId, service::ServiceId, version::Version};
-use harpc_wire_protocol::response::kind::ResponseKind;
+use harpc_wire_protocol::{request::procedure::ProcedureDescriptor, response::kind::ResponseKind};
 
 enum AccountProcedureId {
     CreateAccount,
@@ -128,22 +134,70 @@ struct AccountServerDelegate<T, S> {
     _session: PhantomData<fn() -> *const S>,
 }
 
+// struct A;
+// struct B;
+// struct C;
+
+struct AccountCodecRequirement;
+impl<C> CodecRequirement<C> for AccountCodecRequirement where C: Codec<CreateAccount> {}
+
+// struct CodecRequirement;
+// impl<E> CodecSatisfies<E> for CodecRequirement where E: Codec<A> + Codec<B> + Codec<C> {}
+
+fn acquire_session<S>() -> S {
+    todo!()
+}
+
+// TODO: SessionStorage
 impl<T, S> ServiceDelegate for AccountServerDelegate<T, S>
 where
     T: AccountService<Server<S>>,
     S: Send + Sync,
 {
-    type Codec;
+    type CodecRequirement = AccountCodecRequirement;
     type Service = Account;
 
-    fn call<B>(
+    async fn call<B, C>(
+        &self,
         request: Request<B>,
-        codec: &Self::Codec,
-    ) -> impl Future<Output = Response<impl Body<Control: AsRef<ResponseKind>>>> + Send
+        codec: &C,
+    ) -> Response<impl Body<Control: AsRef<ResponseKind>>>
     where
         B: Body<Control = !> + Send + Sync,
+        AccountCodecRequirement: CodecRequirement<C>,
     {
-        todo!()
+        let ProcedureDescriptor { id } = request.procedure();
+        let id = AccountProcedureId::from_id(id).unwrap();
+
+        match id {
+            AccountProcedureId::CreateAccount => {
+                let encoder = <C as Codec<CreateAccount>>::encoder(codec);
+                let decoder = <C as Codec<CreateAccount>>::decoder(codec);
+
+                let body = request.into_body();
+                let data = body.into_stream().into_data_stream();
+
+                let stream = decoder.decode_stream(data).await;
+                pin_mut!(stream);
+
+                let payload = stream.next().await.unwrap().unwrap();
+
+                let result = self
+                    .service
+                    .create_account(&acquire_session(), payload)
+                    .await;
+
+                let response = match result {
+                    Ok(account_id) => {
+                        let data = encoder.encode(account_id).await;
+                        Response::ok(data)
+                    }
+                    Err(_) => Response::error(),
+                };
+
+                return response;
+            }
+        }
     }
 }
 
--- libs/@local/harpc/service/src/delegate.rs
+++ libs/@local/harpc/service/src/delegate.rs
@@ -3,14 +3,18 @@ use harpc_wire_protocol::response::kind::ResponseKind;
 
 use crate::service::Service;
 
+pub trait CodecRequirement<C> {}
+
 pub trait ServiceDelegate {
     type Service: Service;
-    type Codec;
+    type CodecRequirement;
 
-    fn call<B>(
+    fn call<B, C>(
+        &self,
         request: Request<B>,
-        codec: &Self::Codec,
+        codec: &C,
     ) -> impl Future<Output = Response<impl Body<Control: AsRef<ResponseKind>>>> + Send
     where
-        B: Body<Control = !> + Send + Sync;
+        B: Body<Control = !> + Send + Sync,
+        Self::CodecRequirement: CodecRequirement<C>;
 }
