350295c35004f4e27abc8ba6114adc8c42659e64
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-10-01 12:29:45 +0200

    chore: docs for delegate

--- libs/@local/harpc/service/src/delegate.rs
+++ libs/@local/harpc/service/src/delegate.rs
@@ -3,9 +3,21 @@ use harpc_wire_protocol::response::kind::ResponseKind;
 
 use crate::Service;
 
+/// Delegates service calls to an inner typed service.
+///
+/// This trait acts as a bridge between the generic RPC layer and a typed service implementation.
+/// It routes incoming requests to the appropriate methods of the inner service based on the
+/// request's procedure ID.
+///
+/// The implementation is responsible for encoding and decoding the request and response bodies.
+///
+/// The caller must verify that the version and service of the incoming request match those of
+/// [`Self::Service`].
 pub trait ServiceDelegate<S, C> {
+    /// The inner service type that this delegate wraps.
     type Service: Service;
 
+    /// Handles an incoming request by delegating it to the appropriate method of the inner service.
     fn call<B>(
         &self,
         request: Request<B>,
