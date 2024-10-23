2ab25235405035fe0e6165f2bb58ef0eaab67db1
Ciaran Morinan <c@hash.ai>
2024-10-23 21:25:28 +0100

    add simple paths to pathfinder

--- apps/hash-frontend/src/pages/shared/graph-visualizer/graph-container/path-finder-control.tsx
+++ apps/hash-frontend/src/pages/shared/graph-visualizer/graph-container/path-finder-control.tsx
@@ -1,20 +1,19 @@
 import { IconButton, IconDiagramNestedLight } from "@hashintel/design-system";
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { useSigma } from "@react-sigma/core";
-import { bidirectional, edgePathFromNodePath } from "graphology-shortest-path";
+import { dijkstra, edgePathFromNodePath } from "graphology-shortest-path";
 import { allSimplePaths } from "graphology-simple-path";
 import type { FunctionComponent } from "react";
 import { useCallback, useMemo, useState } from "react";
 
-import { SimpleAutocomplete } from "./path-finder-control/simple-autocomplete";
+import { SimpleAutocomplete } from "./shared/simple-autocomplete";
 import {
   controlButtonSx,
   ControlPanel,
   ControlSectionContainer,
-  ItemLabel,
 } from "./shared/control-components";
 import { useGraphContext } from "./shared/graph-context";
-import { GraphVizNode } from "./shared/types";
+import type { GraphVizNode } from "./shared/types";
 
 type TypeData = {
   label: string;
@@ -25,6 +24,7 @@ type TypeData = {
 type NodeData = GraphVizNode & {
   disabled?: boolean;
   nodePathToNode?: string[];
+  suffix?: string;
   valueForSelector: string;
 };
 
@@ -63,6 +63,7 @@ const PathTerminusSelector = ({
           value={type}
         />
         <SimpleAutocomplete
+          includeSuffix={label === "End"}
           key={type?.typeId ?? "no-type"}
           placeholder={`The node to ${lowercasedLabel} at`}
           options={nodeOptions}
@@ -75,7 +76,7 @@ const PathTerminusSelector = ({
 };
 
 type Path = {
-  edgePath: string[];
+  nodePath: string[];
   label: string;
   valueForSelector: string;
 };
@@ -150,29 +151,22 @@ const PathFinderPanel: FunctionComponent<{
 
     for (const node of endNodes) {
       if (startNode) {
-        const shortestPath = bidirectional(
+        const shortestPath = dijkstra.bidirectional(
           sigma.getGraph(),
           startNode.nodeId,
           node.nodeId,
         );
 
         let pathLength: string;
-        if (shortestPath) {
+        if (shortestPath.length > 0) {
           node.nodePathToNode = shortestPath;
-          pathLength = shortestPath.length.toString();
+          pathLength = (shortestPath.length - 1).toString();
         } else {
           pathLength = "None";
           node.disabled = true;
         }
 
-        /**
-         * replace the existing count if it exists, or add it if it doesn't
-         */
-        if (/\(.*\)$/.test(node.label)) {
-          node.label = node.label.replace(/\(.*\)$/, `(${pathLength})`);
-        } else {
-          node.label = `${node.label} (${pathLength})`;
-        }
+        node.suffix = `(${pathLength})`;
       }
     }
 
@@ -181,7 +175,7 @@ const PathFinderPanel: FunctionComponent<{
 
   const highlightPath = useCallback(
     (nodePath: string[] | null) => {
-      if (!nodePath) {
+      if (!nodePath || nodePath.length === 0) {
         setGraphState("highlightedEdgePath", null);
         sigma.refresh({ skipIndexation: true });
         return;
@@ -211,7 +205,7 @@ const PathFinderPanel: FunctionComponent<{
 
   const selectStartNode = (newStartNode: NodeData | null) => {
     if (newStartNode && endNode) {
-      const shortestPath = bidirectional(
+      const shortestPath = dijkstra.bidirectional(
         sigma.getGraph(),
         newStartNode.nodeId,
         endNode.nodeId,
@@ -220,6 +214,71 @@ const PathFinderPanel: FunctionComponent<{
     }
     setStartNode(newStartNode);
   };
+
+  const [selectedSimplePath, _setSelectedSimplePath] = useState<Path | null>(
+    null,
+  );
+
+  const setSelectedSimplePath = useCallback(
+    (path: Path | null) => {
+      _setSelectedSimplePath(path);
+      if (path) {
+        highlightPath(path.nodePath);
+      }
+    },
+    [highlightPath],
+  );
+
+  const simplePaths = useMemo(() => {
+    if (!startNode || !endNode) {
+      return [];
+    }
+
+    const graph = sigma.getGraph();
+
+    const unfilteredSimplePaths = allSimplePaths(
+      graph,
+      startNode.nodeId,
+      endNode.nodeId,
+      { maxDepth: 3 },
+    );
+
+    const pathOptions: Path[] = [];
+
+    // eslint-disable-next-line no-labels
+    pathsLoop: for (const path of unfilteredSimplePaths) {
+      const seenTypes = new Set<string>();
+
+      const labelParts: string[] = [];
+
+      for (const nodeId of path) {
+        const nodeData = graph.getNodeAttributes(nodeId);
+
+        labelParts.push(nodeData.label);
+
+        if (seenTypes.has(nodeData.nodeTypeId)) {
+          /**
+           * Even at maxDepth 3 there are hundreds of paths between any two nodes.
+           * Excluding paths which travel through the same type of node twice is a simple way of cutting them down.
+           */
+          // eslint-disable-next-line no-labels
+          continue pathsLoop;
+        }
+        seenTypes.add(nodeData.nodeTypeId);
+      }
+
+      const label = labelParts.join(" —> ");
+
+      pathOptions.push({
+        label,
+        valueForSelector: label,
+        nodePath: path,
+      });
+      setSelectedSimplePath(null);
+    }
+
+    return pathOptions;
+  }, [startNode, endNode, setSelectedSimplePath, sigma]);
 
   const selectEndNode = (newEndNode: NodeData | null) => {
     highlightPath(newEndNode?.nodePathToNode ?? null);
@@ -255,18 +314,24 @@ const PathFinderPanel: FunctionComponent<{
           typeOptions={sortedTypes}
         />
       </Stack>
-      {/* <ControlSectionContainer */}
-      {/*  label={"Paths"} */}
-      {/*  tooltip={"Select the path to highlight"} */}
-      {/* > */}
-      {/*  <SimpleAutocomplete placeholder="Select path" options= setValue= value= */}
-      {/* </ControlSectionContainer> */}
+      <ControlSectionContainer
+        label={`Simple paths${simplePaths.length ? ` (${simplePaths.length})` : ""}`}
+        tooltip={`These are paths in which each node is of a different type, with a maximum of 3 edges followed.${!endNode || !startNode ? " Select a start and end node to see paths." : ""}`}
+      >
+        <SimpleAutocomplete
+          key={`${startNode?.nodeId}-${endNode?.nodeId}`}
+          placeholder="Select path to highlight"
+          options={simplePaths}
+          setValue={setSelectedSimplePath}
+          value={selectedSimplePath}
+        />
+      </ControlSectionContainer>
     </ControlPanel>
   );
 };
 
 export const PathFinderControl = ({ nodes }: { nodes: GraphVizNode[] }) => {
-  const [pathFinderPanelOpen, setPathFinderPanelOpen] = useState(false);
+  const { pathFinderPanelOpen, setPathFinderPanelOpen } = useGraphContext();
 
   return (
     <>
