c3fe30c693968a5e5973ad7d1fe8861df95071d2
Ciaran Morinan <c@hash.ai>
2024-10-26 18:19:43 +0100

    add colour palettes

---
+++ libs/@hashintel/design-system/src/palettes.ts
@@ -0,0 +1,107 @@
+export const wong = [
+  "#000000",
+  "#E69F00",
+  "#56B4E9",
+  "#009E73",
+  "#F0E442",
+  "#0072B2",
+  "#D55E00",
+  "#CC79A7",
+] as const;
+
+export const ibm = [
+  "#648FFF",
+  "#785EF0",
+  "#DC267F",
+  "#FE6100",
+  "#FFB000",
+] as const;
+
+export const tol = [
+  "#332288",
+  "#117733",
+  "#44AA99",
+  "#88CCEE",
+  "#DDCC77",
+  "#CC6677",
+  "#AA4499",
+  "#882255",
+] as const;
+
+export const defaultPalette = [
+  "#0775E3",
+  "#9E52EA",
+  "#E839CA",
+  "#FF2AA3",
+  "#FF3E7A",
+  "#FF6154",
+  "#FF8530",
+  "#FFE800",
+] as const;
+
+export const bluePinkYellow = [
+  "#003F5C",
+  "#2F4B7C",
+  "#665191",
+  "#A05195",
+  "#D45087",
+  "#F95D6A",
+  "#FF7C43",
+  "#FFA600",
+] as const;
+
+export const blueGreenYellow = [
+  "#003F5C",
+  "#005671",
+  "#006D76",
+  "#00826B",
+  "#009652",
+  "#68A52F",
+  "#B2AE00",
+  "#FFAC00",
+] as const;
+
+export const bluePink = [
+  "#007BFF",
+  "#877AF3",
+  "#B87CE5",
+  "#D883D6",
+  "#ED8ECA",
+  "#F99DC1",
+  "#FEAFBE",
+  "#FFC1C1",
+] as const;
+
+export const blueGreen = [
+  "#3366FF",
+  "#008BFF",
+  "#00A8FF",
+  "#00C0FF",
+  "#00D4F1",
+  "#00E5CF",
+  "#00F4B0",
+  "#99FF99",
+] as const;
+
+export const purpleGreen = [
+  "#990078",
+  "#C80064",
+  "#E91345",
+  "#F74F17",
+  "#F28100",
+  "#D9AF00",
+  "#AAD900",
+  "#44FF00",
+] as const;
+
+export const palettes = [
+  wong,
+  ibm,
+  tol,
+  defaultPalette,
+  bluePinkYellow,
+  blueGreenYellow,
+  bluePink,
+  blueGreen,
+  purpleGreen,
+] as const;
