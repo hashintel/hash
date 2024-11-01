5db685ebb9717bbf424414ace0c7b12be6c02818
Jesus Fileto <jesus_filetojr@icloud.com>
2024-10-31 21:35:48 -0400

    Optimize page image conversion

--- libs/chonky/src/lib.rs
+++ libs/chonky/src/lib.rs
@@ -25,7 +25,7 @@ pub mod pdf_segmentation {
     ///
     /// # Errors
     ///
-    /// Will return `Err` if `filename` does not exist or the user does not have
+    /// Will return [`ChonkyError::Pdfium`] if `filename` does not exist or the user does not have
     /// permission to read it.
     pub fn load_pdf<'a>(
         pdfium: &'a Pdfium,
@@ -40,26 +40,70 @@ pub mod pdf_segmentation {
     /// is processed into a raw image that can be later converted to any image format
     ///
     /// # Errors
-    /// Return an `PdfiumError::ImageError` if there was a problem with the image processing
+    ///
+    /// Return an [`PdfiumError::ImageError`] if there was a problem with the image processing
     /// operation, occurs when image cannot be encoded into specific image format
     pub fn pdf_to_images(pdf: &PdfDocument) -> Result<Vec<DynamicImage>, Report<ChonkyError>> {
         let mut images: Vec<DynamicImage> = Vec::new();
 
-        for page in pdf.pages().iter() {
-            //may adjust resolution depending on need
-            let resolution_width = 1000;
+        //may adjust resolution depending on need
+        let resolution_width = 1000;
 
-            // Render the entire page to an image
-            let rendered_page = page
-                .render_with_config(&PdfRenderConfig::new().set_target_width(resolution_width))
-                .change_context(ChonkyError::Pdfium)?; // Renders the page to a PdfBitmap
+        // information about how to store image, like pixed resolution and aspect ratio
+        let config = PdfRenderConfig::new().set_target_width(resolution_width);
+
+        let mut bitmap = create_empty_bitmap(pdf, &config)?;
+
+        for page in pdf.pages().iter() {
+            // Render the entire page to a bitmap
+            page.render_into_bitmap_with_config(&mut bitmap, &config)
+                .change_context(ChonkyError::Pdfium)?;
 
             // Convert PdfBitmap to DynamicImage
-            let dynamic_image = rendered_page.as_image();
+            let dynamic_image = bitmap.as_image();
             images.push(dynamic_image);
         }
 
         Ok(images)
+    }
+
+    /// Takes in a config with desired resolution of image and a pdf and creates
+    /// an empty bitmap that can be used by the entire pdf
+    ///
+    /// # Errors
+    ///
+    /// Return an [`PdfiumError::CoordinateConversionFunctionIndicatedError`] if there was a problem
+    /// with converting the pdfs dimensions to pixels
+    fn create_empty_bitmap<'a>(
+        pdf: &'a PdfDocument,
+        config: &'a PdfRenderConfig,
+    ) -> Result<PdfBitmap<'a>, Report<ChonkyError>> {
+        // read the first page to get page dimesnions
+        let page_dimensions = pdf
+            .pages()
+            .page_sizes()
+            .change_context(ChonkyError::Pdfium)?[0];
+
+        //converts the boundings boxes to pixels that can be used for creating bitmap with proper
+        // for correct bitmap dimensions, height point must be set to 0 to get max pixel height
+        let page_dimensions = pdf
+            .pages()
+            .get(0)
+            .change_context(ChonkyError::Pdfium)?
+            .points_to_pixels(page_dimensions.width(), PdfPoints::new(0.0), config)
+            .change_context(ChonkyError::Pdfium)?;
+
+        //create an empty bitmap that follows dimensions of pdf
+        // to prevent repeated memory allocations (we assume all pdfs are same dimension)
+        let bitmap = PdfBitmap::empty(
+            page_dimensions.0,
+            page_dimensions.1,
+            PdfBitmapFormat::BGR,
+            pdf.bindings(),
+        )
+        .change_context(ChonkyError::Pdfium)?;
+
+        Ok(bitmap)
     }
 }
 
