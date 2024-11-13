d3e8d7c31f93fe7766855b4b3c26fc22bddd75d1
Ciaran Morinan <c@hash.ai>
2024-11-13 19:23:03 +0000

    wip: update pdf preview design / features

--- apps/hash-frontend/src/pages/shared/pdf-preview.tsx
+++ apps/hash-frontend/src/pages/shared/pdf-preview.tsx
@@ -2,9 +2,23 @@ import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 import dynamic from "next/dynamic";
-import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Document, Outline, Page, pdfjs, Thumbnail } from "react-pdf";
 import { Box, Stack, useTheme } from "@mui/material";
+import {
+  DashIcon,
+  IconButton,
+  PlusIcon,
+  TextField,
+} from "@hashintel/design-system";
+import { CustomTextRenderer } from "react-pdf/dist/cjs/shared/types";
 
 const pdfOptions = { cMapUrl: "/pdf-cmaps" };
 
@@ -13,12 +27,18 @@ type PdfViewerProps = {};
 const showThumbnails = true;
 const showZoom = true;
 
-const thumbnailWidth = 150;
-const horizontalGap = 16;
-const containerPadding = 16;
-const thumbnailXPadding = 16;
+/** @todo calculate this */
+const assumedThumbnailScrollWidth = 16;
+const thumbnailWidth = 165;
+const thumbnailXPadding = 18;
+
+const letterRatio = 1.2941176470588236;
 
 const pageOptions = { className: "react-pdf-page" };
+
+const highlightPattern = (text: string, pattern: string) => {
+  return text.replace(pattern, (value) => `<mark>${value}</mark>`);
+};
 
 export const PdfViewer = ({ url }: { url: string }) => {
   useEffect(() => {
@@ -28,6 +48,8 @@ export const PdfViewer = ({ url }: { url: string }) => {
     ).toString();
   });
 
+  const [scale, setScale] = useState(1);
+  const [searchText, setSearchText] = useState("");
   const [totalPages, setTotalPages] = useState<number>();
   const [selectedPageNumber, setSelectedPageNumber] = useState<number>(1);
 
@@ -37,114 +59,143 @@ export const PdfViewer = ({ url }: { url: string }) => {
 
   const theme = useTheme();
 
-  const pageOptions = useMemo(() => ({ className: "react-pdf-page" }), []);
-
   const containerRef = useRef<HTMLDivElement>(null);
-  const [containerHeight, setContainerHeight] = useState<number>();
+  const [containerWidth, setContainerWidth] = useState<number>();
 
   useLayoutEffect(() => {
     // Get the width of the component once it's rendered
     if (containerRef.current) {
       const container = containerRef.current;
       requestAnimationFrame(() => {
-        setContainerHeight(container.offsetHeight);
+        setContainerWidth(container.offsetWidth);
       });
     }
   }, [containerRef]);
 
-  const pageWidth = `calc(100% - ${thumbnailWidth}px - ${horizontalGap}px - ${thumbnailXPadding * 2}px)`;
+  const textRenderer: CustomTextRenderer = useCallback(
+    (textItem) => highlightPattern(textItem.str, searchText),
+    [searchText],
+  );
+
+  const pageWidth =
+    (containerWidth ?? 0) -
+    assumedThumbnailScrollWidth -
+    thumbnailWidth -
+    thumbnailXPadding * 2;
+
+  /**
+   * Default to a letter-sized page – will adjust via event handler on the page once it's loaded
+   */
+  const pageHeight = pageWidth * letterRatio;
 
   return (
-    <Stack
-      direction="row"
-      ref={containerRef}
-      sx={({ palette }) => ({
-        background: palette.gray[90],
-        border: `1px solid ${palette.gray[30]}`,
-        borderRadius: 2,
-        p: `${containerPadding}px`,
-        height: 600,
-        width: "100%",
-      })}
-    >
-      <Document
-        className="react-pdf-document"
-        file={url}
-        onLoadSuccess={onDocumentLoadSuccess}
-        options={pdfOptions}
+    <Box>
+      <TextField
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <Stack
+        direction="row"
+        ref={containerRef}
+        sx={({ palette }) => ({
+          background: palette.blue[10],
+          border: `1px solid ${palette.gray[30]}`,
+          borderRadius: 2,
+          height: pageHeight,
+          width: "60%",
+          m: 15,
+        })}
       >
-        <Stack
-          direction="row"
-          gap={`${horizontalGap}px`}
-          sx={{ width: "100%" }}
+        <Document
+          className="react-pdf-document"
+          file={url}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={pdfOptions}
         >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              height: "100%",
-              width: "100%",
-            }}
-          >
-            <Page
-              loading={
-                <Box
-                  sx={{
-                    background: ({ palette }) => palette.common.white,
-                    height: containerHeight,
-                  }}
-                />
-              }
-              pageNumber={selectedPageNumber}
-              renderTextLayer
-              {...pageOptions}
-              scale={1}
-              height={
-                Math.min(containerHeight ?? 0, 600) - containerPadding * 2
-              }
-            />
-          </Box>
-          {showThumbnails && totalPages !== undefined && (
+          <Stack direction="row" sx={{ width: "100%" }}>
+            {totalPages !== undefined && (
+              <Stack
+                gap={2}
+                sx={{
+                  minWidth:
+                    thumbnailWidth +
+                    thumbnailXPadding * 2 +
+                    assumedThumbnailScrollWidth,
+                  maxHeight: pageHeight,
+                  overflowY: "scroll",
+                  px: `${thumbnailXPadding}px`,
+                  py: 2,
+                }}
+              >
+                {Array.from({ length: totalPages }, (_, i) => (
+                  <Box
+                    key={i}
+                    sx={({ palette }) => ({
+                      outlineStyle: "solid",
+                      outlineOffset: 2,
+                      outlineWidth: 4,
+                      outlineColor:
+                        i + 1 === selectedPageNumber
+                          ? palette.blue[20]
+                          : "transparent",
+                    })}
+                  >
+                    <Thumbnail
+                      pageNumber={i + 1}
+                      width={thumbnailWidth}
+                      {...pageOptions}
+                      onItemClick={({ pageNumber }) =>
+                        setSelectedPageNumber(pageNumber)
+                      }
+                      // onMouseEnter={() => setSelectedPageNumber(i + 1)}
+                    />
+                  </Box>
+                ))}
+              </Stack>
+            )}
             <Stack
-              gap={3}
+              justifyContent="center"
+              alignItems="center"
               sx={{
-                maxHeight: containerHeight - containerPadding * 2,
-                overflowY: "auto",
-                px: `${thumbnailXPadding}px`,
-                py: "6px",
+                overflow: "hidden",
+                position: "relative",
+                width: pageWidth,
               }}
             >
-              {Array.from({ length: totalPages }, (_, i) => (
-                <Box
-                  key={i}
-                  sx={({ palette }) => ({
-                    outlineStyle: "solid",
-                    outlineOffset: 2,
-                    outlineWidth: 4,
-                    outlineColor:
-                      i + 1 === selectedPageNumber
-                        ? palette.blue[70]
-                        : "transparent",
-                  })}
-                >
-                  <Thumbnail
-                    pageNumber={i + 1}
-                    width={thumbnailWidth}
-                    {...pageOptions}
-                    onItemClick={({ pageNumber }) =>
-                      setSelectedPageNumber(pageNumber)
-                    }
-                    // onMouseEnter={() => setSelectedPageNumber(i + 1)}
+              <Page
+                customTextRenderer={textRenderer}
+                loading={
+                  <Box
+                    sx={{
+                      background: ({ palette }) => palette.common.white,
+                      height: pageHeight,
+                    }}
                   />
-                </Box>
-              ))}
+                }
+                pageNumber={selectedPageNumber}
+                renderTextLayer
+                {...pageOptions}
+                scale={scale}
+                width={pageWidth}
+              />
+              <Stack
+                direction="row"
+                sx={{ position: "absolute", bottom: 20, right: 20, zIndex: 20 }}
+              >
+                <IconButton onClick={() => setScale((prev) => prev - 0.2)}>
+                  <DashIcon />
+                </IconButton>
+                <IconButton onClick={() => setScale((prev) => prev + 0.2)}>
+                  <PlusIcon />
+                </IconButton>
+              </Stack>
             </Stack>
-          )}
-        </Stack>
-      </Document>
-      <p>
-        Page {selectedPageNumber} of {totalPages}
-      </p>
-    </Stack>
+          </Stack>
+        </Document>
+        <p>
+          Page {selectedPageNumber} of {totalPages}
+        </p>
+      </Stack>
+    </Box>
   );
 };
