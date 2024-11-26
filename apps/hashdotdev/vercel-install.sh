c6f9821abd3114505bcebccd2e1a5479ab01f840
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-11-26 19:23:37 +0100

    fix: vercel

--- apps/hashdotdev/vercel-install.sh
+++ apps/hashdotdev/vercel-install.sh
@@ -9,6 +9,9 @@ yum install -y jq
 echo "Installing turbo"
 npm install -g "turbo@$(jq -r '.devDependencies.turbo' < package.json)"
 
+echo "Enable corepack"
+corepack enable
+
 echo "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none --profile minimal
 source "$HOME/.cargo/env"
