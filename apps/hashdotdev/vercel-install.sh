5deccd1badbcba1b01a869ef02207794dcfedc63
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-11-26 19:04:42 +0100

    chore: use `npm install -g` instead of `yarn global`

--- apps/hashdotdev/vercel-install.sh
+++ apps/hashdotdev/vercel-install.sh
@@ -7,8 +7,7 @@ echo "Installing prerequisites"
 yum install -y jq
 
 echo "Installing turbo"
-corepack enable
-yarn global add "turbo@$(jq -r '.devDependencies.turbo' < package.json)"
+npm install -g "turbo@$(jq -r '.devDependencies.turbo' < package.json)"
 
 echo "Installing Rust"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none --profile minimal
