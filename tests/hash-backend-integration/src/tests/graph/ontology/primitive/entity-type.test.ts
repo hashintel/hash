a5c54142d25849bc92f1e7bcf5588c102ef54e8a
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2024-11-27 10:51:22 +0100

    H-3677: Include entity type in closed entity type's `allOf` (#5739)

--- tests/hash-backend-integration/src/tests/graph/ontology/primitive/entity-type.test.ts
+++ tests/hash-backend-integration/src/tests/graph/ontology/primitive/entity-type.test.ts
@@ -339,6 +339,16 @@ describe("Entity type CRU", () => {
             ...(userType.schema.links ?? {}),
             ...(userType.schema.links ?? {}),
           },
+          allOf: [
+            {
+              depth: 0,
+              $id: systemEntityTypes.user.entityTypeId,
+            },
+            {
+              depth: 1,
+              $id: systemEntityTypes.actor.entityTypeId,
+            },
+          ],
         } satisfies ClosedEntityType,
       },
     ]);
@@ -394,6 +404,7 @@ describe("Entity type CRU", () => {
             $id: closedEntityType.schema.$id,
             title: closedEntityType.schema.title,
             description: closedEntityType.schema.description,
+            allOf: closedEntityType.schema.allOf,
           }) as ClosedMultiEntityType["allOf"][0],
       ),
     );
