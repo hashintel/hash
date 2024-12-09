749f3e89e8abdb4d390116b823e9fc8da40e23b6
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-12-09 19:56:27 +0100

    feat: hash-realtime

--- apps/hash-realtime/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  ...require("@local/eslint-config/generate-workspace-config.cjs")(__dirname),
-  env: {
-    node: true,
-  },
-};
