2b92447ec9785a265eeb74afefe832c9798b2eae
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-12-09 19:32:11 +0100

    feat: eslint for hash-frontend

--- libs/@local/eslint/src/deprecated/index.ts
+++ libs/@local/eslint/src/deprecated/index.ts
@@ -1,4 +1,9 @@
+import type { ESLintRules } from "eslint/rules";
+
+import { defineConfig as defineConfig$ } from "../utils.js";
+
 export { create as createBase } from "./base.js";
 export { create as createBlock } from "./block.js";
 export { disableRules } from "./disable.js";
-export { defineConfig } from "../utils.js";
+
+export const defineConfig = defineConfig$<ESLintRules>;
