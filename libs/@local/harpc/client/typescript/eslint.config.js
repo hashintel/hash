b55fa15429284c04474bf05832c6501e335f61a1
Ciaran Morinan <c@hash.ai>
2024-12-16 12:18:09 +0000

    explicitly type eslint.config.js in harpc/client

--- libs/@local/harpc/client/typescript/eslint.config.js
+++ libs/@local/harpc/client/typescript/eslint.config.js
@@ -1,6 +1,13 @@
 import { createBase } from "@local/eslint/deprecated";
 
-export default [
+/**
+ * @todo why is this required in this package but not others with the same config? if not explicitly typed, we get:
+ *   TS2742: The inferred type of 'default' cannot be named without a reference to '../../../../../node_modules/@local/eslint/dist/utils.js'.
+ *   This is likely not portable. A type annotation is necessary.
+ *
+ * @type {import('eslint').Linter.Config[]}
+ */
+const config = [
   ...createBase(import.meta.dirname),
   {
     rules: {
@@ -15,3 +22,5 @@ export default [
     },
   },
 ];
+
+export default config;
