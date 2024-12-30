83401ffb4b3e829026b512268f45908cb59dffb6
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-12-30 22:58:12 +0100

    feat: significantely speedup CLI

--- libs/@local/repo-chores/node/scripts/sort-package-json.ts
+++ libs/@local/repo-chores/node/scripts/sort-package-json.ts
@@ -1,7 +1,9 @@
 import fs from "node:fs/promises";
+import path from "node:path";
 
+import chalk from "chalk";
 import execa from "execa";
-import globby from "globby";
+import ignore from "ignore";
 import sortPackageJson from "sort-package-json";
 
 import { monorepoRootDirPath } from "./shared/monorepo";
@@ -19,15 +21,16 @@ const fix = async (packageJsonPath: string, { lint }: { lint: boolean }) => {
   );
 
   if (contents === output) {
-    return;
+    return { failed: [] };
   }
 
   if (lint) {
-    throw new Error(
-      `Please run \`yarn fix:package-json\` to fix ${packageJsonPath}`,
-    );
+    return {
+      failed: [packageJsonPath],
+    };
   } else {
     await fs.writeFile(packageJsonPath, output);
+    return { failed: [] };
   }
 };
 
@@ -35,18 +38,20 @@ const fixTimed = async (
   packageJsonPath: string,
   { lint }: { lint: boolean },
 ) => {
+  const start = Date.now();
   const operation = lint ? "lint" : "fix";
 
-  const start = Date.now();
-  try {
-    await fix(packageJsonPath, { lint });
-  } finally {
-    const took = Date.now() - start;
-    console.log(`Took ${took}ms to ${operation} ${packageJsonPath}`);
-  }
+  const output = await fix(packageJsonPath, { lint });
+
+  const took = Date.now() - start;
+  console.log(chalk.gray(`Took ${took}ms to ${operation} ${packageJsonPath}`));
+
+  return output;
 };
 
 const main = async () => {
+  const now = Date.now();
+
   const args = process.argv.slice(2);
   if (args.length === 0) {
     throw new Error(
@@ -62,18 +67,62 @@ const main = async () => {
   let matches = args.slice(1);
 
   if (matches.length === 0) {
+    const nowGlob = Date.now();
+
     // find all package.json files
-    matches = await globby(["**/package.json"], {
-      cwd: monorepoRootDirPath,
-      gitignore: true,
-    });
+    const ignoreFile = ignore().add(
+      await fs.readFile(`${monorepoRootDirPath}/.gitignore`, "utf8"),
+    );
+
+    matches = await Array.fromAsync(
+      fs.glob("**/package.json", {
+        cwd: monorepoRootDirPath,
+        exclude: (directoryOrFile) => {
+          let pathname = path.relative(
+            monorepoRootDirPath,
+            `${directoryOrFile.parentPath}/${directoryOrFile.name}`,
+          );
+
+          if (directoryOrFile.isBlockDevice()) {
+            pathname = `${pathname}/`;
+          }
+
+          return ignoreFile.ignores(pathname);
+        },
+        withFileTypes: true,
+      }),
+    ).then((files) => files.map((file) => `${file.parentPath}/${file.name}`));
+
+    const tookGlob = Date.now() - nowGlob;
+    console.log(`Took ${tookGlob}ms to find all package.json files`);
+  } else {
+    matches = matches.map((match) =>
+      match.startsWith("/") ? match : `${monorepoRootDirPath}/${match}`,
+    );
   }
 
-  await Promise.all(
-    matches.map((match) =>
-      fixTimed(`${monorepoRootDirPath}/${match}`, { lint: isLint }),
+  console.log(`Found ${matches.length} package.json files`);
+  const { failed } = await Promise.all(
+    matches.map((match) => fixTimed(match, { lint: isLint })),
+  ).then((results) =>
+    results.reduce(
+      (acc, result) => ({
+        failed: [...acc.failed, ...result.failed],
+      }),
+      { failed: [] },
     ),
   );
+
+  const took = Date.now() - now;
+  console.log(`Total time: ${took}ms`);
+
+  if (failed.length > 0) {
+    for (const file of failed) {
+      console.log(chalk.red("[ERROR]"), file);
+    }
+
+    process.exit(1);
+  }
 };
 
 await main();
