1ddb917c2b84219d48188a50d086baf7be6d9aa6
Tim Diekmann <post@timdiekmann.de>
2025-01-21 11:07:29 +0100

    TMP

--- libs/@local/graph/authorization/opa/policies/test.rego
+++ libs/@local/graph/authorization/opa/policies/test.rego
@@ -7,193 +7,42 @@ user_policies contains policy if {
 	some user_principal, user in data.users
 	glob.match(user_principal, null, input.principal.id)
 
-	# policies = user.policies
-	# principal := concat(":", ["user", user_principal])
-
-	# policies := {policy |
 	some user_policy in user.policies
 	some policy in user.policies
 }
-
-# policies = is_entity_allowed(data.entities[input.resource], user_policy)
-# policy = user_policy
-# }
 
 user_policies contains policy if {
 	input.principal.type == "user"
 	some user_principal, user in data.users
 	user_principal == input.principal.id
 	some organization in user.organizations
-
-	# print("principal:", user_principal, " organization:", organization, "user:", user)
 	some role in graph.reachable(data.organizations[organization.name].roles, organization.roles)
-
-	# principal := concat(":", ["org", organization.name, "role"])
-
-	# policies := {policy |
 	some policy in data.organizations[organization.name].policies[role]
 }
 
-entity_policy if {
-	some entity_id, entity in data.entities
-	some policy in user_policies
-	entity_condition(entity, policy)
-}
-
-# policies = is_entity_allowed(data.entities[input.resource], policy)
-# }
-
-# allow_entity[entity_id] := policies if {}
-#
-allowed_policies contains policy if {
-	some policy in user_policies
-	some entity in data.entities
-	entity_condition(entity, policy)
-	policy.effect.type == "allow"
-}
-
-denied_policies contains policy if {
-	some policy in user_policies
-	some entity in data.entities
-	entity_condition(entity, policy)
+any_entity_policy(entity_id, policies, level) if {
+	some policy in policies
 	policy.effect.type == "deny"
-}
-
-has_higher_policy(policies, level) := policy if {
-	some policy in policies
-	policy.effect.level > level
-	policy.effect.type == "allow"
-}
-
-has_lower_negative_policy(policies, level) := policy if {
-	some policy in policies
-	policy.effect.level < level
-	policy.effect.type == "deny"
-}
-
-has_higher_negative_policy(policies, level) := policy if {
-	some policy in policies
 	policy.effect.level >= level
-	policy.effect.type == "deny"
+	entity_condition(data.entities[entity_id], policy)
 }
 
-all_policies if {
-	some policy in user_policies
-	some entity in data.entities
-	policy.effect.type == "allow"
-	entity_condition(entity, policy)
-	not has_higher_negative_policy(user_policies, policy.effect.level)
-}
-
-all_policies if {
-	some policy in user_policies
-	some entity in data.entities
-	policy.effect.type == "allow"
-	entity_condition(entity, policy)
-
-	every neg_policy in user_policies {
-		neg_policy.effect.level >= policy.effect.level
-		neg_policy.effect.type == "deny"
-		not_entity_condition(entity, neg_policy)
-	}
-}
-
-# all_policies if {
-# 	some policy in user_policies
-# 	some entity in data.entities
-# 	policy.effect.type == "allow"
-# 	entity_condition(entity, policy)
-# 	neg_policy := has_lower_negative_policy(user_policies, policy.effect.level)
-# 	not_entity_condition(entity, neg_policy)
-# }
-
-# all_policies if {
-# 	some neg_policy in user_policies
-# 	some entity in data.entities
-# 	neg_policy.effect.type == "deny"
-# 	not_entity_condition(entity, neg_policy)
-# 	policy := has_higher_policy(user_policies, neg_policy.effect.level)
-# 	entity_condition(entity, policy)
-# }
-
-# policy.effect.type == "deny"
-
-# all_policies := array.concat([policy | some policy in allowed_policies], [policy | some policy in denied_policies])
-
-allow contains entity_id if {
+all_policies contains entity_id if {
 	some entity_id, entity in data.entities
-
-	max({level |
-		some policy in allowed_policies
-		level = policy.effect.level
-	}) > max({level |
-		some policy in denied_policies
-		level = policy.effect.level
-	} | {-1})
+	some policy in user_policies
+	policy.effect.type == "allow"
+	entity_condition(entity, policy)
+	not any_entity_policy(entity_id, user_policies, policy.effect.level)
 }
-
-# print(is_entity_denied(entity, policy))
-
-# not is_entity_denied(entity, policy)
-# print(effect)
-
-# })
-
-# print(max({effect | some effect in user_policies}))
-
-# entity := input.entities[input.entity]
-#
-default entity_condition(entity, policy) := false
 
 entity_condition(entity, policy) if {
-	# print("positive owner", entity.owner, policy.resource.owner)
 	entity.owner == policy.resource.owner
 }
 
 entity_condition(entity, policy) if {
-	# print("positive value", entity.value, policy.resource.value)
-	entity.value == policy.resource.value
+	entity.property == policy.resource.property
 }
 
 entity_condition(entity, policy) if {
-	# print("positive type", entity.type, policy.resource.type)
 	entity.type == policy.resource.type
 }
-
-default not_entity_condition(entity, policy) := true
-
-not_entity_condition(entity, policy) if {
-	# print("negative owner", entity.owner, policy.resource.owner)
-	entity.owner != policy.resource.owner
-}
-
-not_entity_condition(entity, policy) if {
-	# print("negative value", entity.value, policy.resource.value)
-	entity.value != policy.resource.value
-}
-
-not_entity_condition(entity, policy) if {
-	# print("negative type", entity.type, policy.resource.type)
-	entity.type != policy.resource.type
-}
-
-# is_entity_allowed(entity, policy) contains level if {
-# entity_condition(entity, policy)
-# policy.effect.type == "allow"
-# level == policy.effect.level
-# }
-# is_entity_denied(entity, policy) contains level if {
-# 	entity_condition(entity, policy)
-# 	policy.effect.type == "deny"
-# 	level = policy.effect.level
-# }
-# is_entity_denied(entity, policy) := effect if {
-# 	# print(entity, policy)
-# 	entity.owner == policy.resource.owner
-# 	# print(policy.effect.level, level)
-# 	policy.effect.type == "deny"
-# 	# policy.effect.level >= level
-# 	effect = {"type": "deny", "level": policy.effect.level}
-# }
-# policy.owner == input.resource.owner
-# policy.effect == "allow"
--- libs/@local/graph/authorization/src/main.rs
+++ libs/@local/graph/authorization/src/main.rs
@@ -1,43 +1,424 @@
-use core::error::Error;
-use std::collections::HashMap;
+use core::{cell::OnceCell, error::Error, iter::empty, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
 
-use cedar_policy::*;
+use cedar_policy_core::{
+    ast::{
+        self, Annotations, Effect, Eid, Entity, EntityAttrEvaluationError, EntityUID,
+        EntityUIDEntry, Expr, Extension, Id, Name, PolicyID, PolicySet, Request,
+        RequestSchemaAllPass, ResourceConstraint, RestrictedExpr, SlotId, StaticPolicy,
+    },
+    authorizer::{Authorizer, PartialResponse, ResponseKind},
+    entities::{AllEntitiesNoAttrsSchema, Entities, NoEntitiesSchema, TCComputation},
+    est,
+    extensions::Extensions,
+    parser::parse_policyset,
+};
+use cedar_policy_validator::{CoreSchema, ValidationMode, Validator, ValidatorSchema};
+use serde::Deserialize;
+use serde_json::json;
+use smol_str::SmolStr;
+use uuid::Uuid;
+
+const POLICY_SRC: &str = include_str!("../cedar/policy.cedar");
+
+fn eid(namespace: &'static str, name: impl Into<SmolStr>) -> EntityUID {
+    EntityUID::from_components(
+        Name::new(
+            Id::from_str(namespace).expect("namespace should be valid"),
+            [Id::from_str("HASH").expect("namespace should be valid")],
+        ),
+        Eid::new(name),
+    )
+    .into()
+}
+
+pub struct User {
+    pub name: SmolStr,
+}
+
+impl From<User> for EntityUID {
+    fn from(user: User) -> Self {
+        eid("User", user.name)
+    }
+}
+
+fn user_euid(name: impl Into<SmolStr>) -> EntityUID {
+    User { name: name.into() }.into()
+}
+
+pub struct Organization {
+    pub name: SmolStr,
+}
+
+impl From<Organization> for EntityUID {
+    fn from(org: Organization) -> Self {
+        eid("Org", org.name)
+    }
+}
+
+fn organization_euid(name: impl Into<SmolStr>) -> EntityUID {
+    Organization { name: name.into() }.into()
+}
+
+pub struct OrganizationRole {
+    pub name: SmolStr,
+    pub role: SmolStr,
+}
+
+impl From<OrganizationRole> for EntityUID {
+    fn from(role: OrganizationRole) -> Self {
+        eid("OrgRole", &format!("{}::{}", role.name, role.role))
+    }
+}
+
+fn organization_role_euid(name: impl Into<SmolStr>, role: impl Into<SmolStr>) -> EntityUID {
+    OrganizationRole {
+        name: name.into(),
+        role: role.into(),
+    }
+    .into()
+}
+
+pub enum PrincipalConstraint {
+    Eq(User),
+    In(OrganizationRole),
+}
+
+pub enum Action {
+    Create,
+    View,
+    Update,
+    Instantiate,
+    Archive,
+    Delete,
+}
+
+impl From<Action> for EntityUID {
+    fn from(action: Action) -> Self {
+        eid(
+            "Action",
+            SmolStr::new_static(match action {
+                Action::Create => "Create",
+                Action::View => "View",
+                Action::Update => "Update",
+                Action::Instantiate => "Instantiate",
+                Action::Archive => "Archive",
+                Action::Delete => "Delete",
+            }),
+        )
+    }
+}
+
+pub enum ActionConstraint {
+    Eq(Action),
+    In(Vec<Action>),
+}
+
+pub enum ResourceType {
+    Entity,
+}
+
+impl From<ResourceType> for Name {
+    fn from(principal: Resource) -> Self {
+        let namespace = match principal {
+            ResourceType::Entity => "Entity",
+        };
+
+        Name::new(
+            Id::from_str(namespace).expect("namespace should be valid"),
+            [Id::from_str("HASH").expect("namespace should be valid")],
+        )
+    }
+}
+
+pub enum Resource {
+    Entity { id: SmolStr },
+}
+
+impl From<Resource> for EntityUID {
+    fn from(principal: Resource) -> Self {
+        match principal {
+            Resource::Entity { id } => eid("Entity", id.to_string()),
+        }
+    }
+}
+
+pub enum ResourceConstraint {
+    Eq(Entity),
+    In(Organization),
+    IsIn(ResourceType, Organization),
+}
+
+fn entity_euid(id: impl Into<SmolStr>) -> EntityUID {
+    Resource::Entity { id: id.into() }.into()
+}
+
+pub struct Policy {
+    pub principal: Option<PrincipalConstraint>,
+    pub action: Option<ActionConstraint>,
+    pub resource: Option<ResourceConstraint>,
+    pub conditions: Vec<()>,
+}
+
+impl From<Policy> for ast::Policy {
+    fn from(policy: Policy) -> Self {
+        let principal_constraint = policy.principal.map_or_else(
+            ast::PrincipalConstraint::any,
+            |principal| match principal {
+                PrincipalConstraint::Eq(user) => {
+                    ast::PrincipalConstraint::new(ast::PrincipalOrResourceConstraint::Eq(
+                        ast::EntityReference::EUID(Arc::new(user.into())),
+                    ))
+                }
+                PrincipalConstraint::In(role) => {
+                    ast::PrincipalConstraint::new(ast::PrincipalOrResourceConstraint::Eq(
+                        ast::EntityReference::EUID(Arc::new(role.into())),
+                    ))
+                }
+            },
+        );
+        let action_constraint = policy
+            .action
+            .map_or_else(ast::ActionConstraint::any, |action| match action {
+                ActionConstraint::Eq(action) => ast::ActionConstraint::Eq(Arc::new(action.into())),
+                ActionConstraint::In(actions) => ast::ActionConstraint::In(
+                    actions
+                        .into_iter()
+                        .map(|action| Arc::new(action.into()))
+                        .collect(),
+                ),
+            });
+        let resource_constraint =
+            policy
+                .resource
+                .map_or_else(ast::ResourceConstraint::any, |resource| match resource {
+                    ResourceConstraint::Eq(resource) => {
+                        ast::ResourceConstraint::new(ast::PrincipalOrResourceConstraint::Eq(
+                            ast::EntityReference::EUID(Arc::new(resource.into())),
+                        ))
+                    }
+                    ResourceConstraint::In(resource) => {
+                        ast::ResourceConstraint::new(ast::PrincipalOrResourceConstraint::In(
+                            ast::EntityReference::EUID(Arc::new(resource.into())),
+                        ))
+                    }
+                    ResourceConstraint::IsIn(ty, resource) => {
+                        ast::ResourceConstraint::new(ast::PrincipalOrResourceConstraint::IsIn(
+                            ty.into(),
+                            ast::EntityReference::EUID(Arc::new(resource.into())),
+                        ))
+                    }
+                });
+
+        ast::Policy::from(
+            ast::StaticPolicy::new(
+                PolicyID::from_string(Uuid::new_v4().to_string()),
+                Annotations::new(),
+                Effect::Permit,
+                principal_constraint,
+                action_constraint,
+                resource_constraint,
+                Expr::val(true),
+            )
+            .expect("should not contain any slot"),
+        )
+    }
+}
+
+pub enum Principal {
+    User(User),
+    OrganizationRole(OrganizationRole),
+}
+
+impl From<User> for Principal {
+    fn from(user: User) -> Self {
+        Principal::User(user)
+    }
+}
+
+impl From<OrganizationRole> for Principal {
+    fn from(role: OrganizationRole) -> Self {
+        Principal::OrganizationRole(role)
+    }
+}
+
+fn viewer(principal: impl Into<Principal>, resource: Resource) -> Policy {
+    Policy {
+        principal: match principal.into() {
+            Principal::User(user) => Some(PrincipalConstraint::Eq(user)),
+            Principal::OrganizationRole(role) => Some(PrincipalConstraint::In(role)),
+        },
+        action: Some(ActionConstraint::Eq(Action::View)),
+        resource: Some(ResourceConstraint::Eq(())),
+        conditions: vec![],
+    }
+}
+
+fn editor(principal: impl Into<Principal>, resource: Resource) -> Policy {
+    Policy {
+        principal: match principal.into() {
+            Principal::User(user) => Some(PrincipalConstraint::Eq(user)),
+            Principal::OrganizationRole(role) => Some(PrincipalConstraint::In(role)),
+        },
+        action: Some(ActionConstraint::In(vec![Action::View, Action::Update])),
+        resource: Some(resource),
+        conditions: vec![],
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    const POLICY_SRC: &str = r#"
-    // Principals in the Admin role can delete any resource
-    permit (
-        principal in PhotoFlash::Role::"Admin",
-        action == PhotoFlash::Action::"DeletePhoto",
-        resource
+    let mut policy_set = parse_policyset(POLICY_SRC)?;
+
+    policy_set.add(
+        viewer(User { name: "tim".into() }, Resource::Entity {
+            id: "photo".into(),
+        })
+        .into(),
+    );
+    policy_set.add(
+        editor(User { name: "tim".into() }, Resource::Entity {
+            id: "person".into(),
+        })
+        .into(),
     );
 
-    // Grants Bob permission to view photo-5678.jpg
-    permit (
-        principal == PhotoFlash::User::"Bob",
-        action == PhotoFlash::Action::"ViewPhoto",
-        resource == PhotoFlash::Photo::"photo-5678.jpg"
-    );
-"#;
-    let policy: PolicySet = POLICY_SRC.parse().unwrap();
-    // Policy
+    for policy in policy_set.policies() {
+        println!("{policy}");
+    }
+    use rand::distributions::{Alphanumeric, DistString};
 
-    let alice: EntityUid = r#"PhotoFlash::User::"Bob""#.parse()?;
-    let admin_role: EntityUid = r#"PhotoFlash::Role::"Admin""#.parse()?;
-    let request = RequestBuilder::default()
-        .action(Some(r#"PhotoFlash::Action::"DeletePhoto""#.parse()?))
-        .principal(Some(alice.clone()))
-        .build();
+    // let template_id = PolicyID::from_string("policy0");
+    // let template = policy_set
+    //     .get_template(&PolicyID::from_string("policy0"))
+    //     .clone()
+    //     .unwrap();
 
-    let entities = Entities::empty().partial().add_entities(
-        [Entity::new(alice, HashMap::new(), [admin_role].into())?],
-        None,
+    // println!("{template}, {:?}", template.slots().collect::<Vec<_>>());
+
+    // for i in 0..0 {
+    //     let string = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+
+    //     policy_set.link(
+    //         template_id.clone(),
+    //         PolicyID::from_string(format!("template-instance-{i}")),
+    //         HashMap::from([(SlotId::principal(), Principal::user(&string).into())]),
+    //     );
+    // }
+
+    let start = std::time::Instant::now();
+
+    let validator_schema = ValidatorSchema::from_json_value(
+        json!({
+            "HASH": {
+                "actions": {
+                    "Update": {
+                        "appliesTo": {
+                            "principalTypes": ["User"],
+                            "resourceTypes": ["Entity"],
+                        }
+                    },
+                    "View": {
+                        "appliesTo": {
+                            "principalTypes": ["User"],
+                            "resourceTypes": ["Entity"],
+                        }
+                    },
+                },
+                "entityTypes": {
+                    "OrgRole": {
+                        "memberOfTypes": [ "OrgRole" ],
+                    },
+                    "Org": {
+
+                    },
+                    "User": {
+                        "memberOfTypes": [ "OrgRole" ],
+                    },
+                    "Entity": {
+                        "memberOfTypes": [ "Org" ],
+                    }
+                }
+            }
+        }),
+        Extensions::all_available(),
     )?;
-    let authorizer = Authorizer::new();
-    let answer = authorizer.evaluate_policies_partial(&request, &policy, &entities);
+    let schema = CoreSchema::new(&validator_schema);
+    let validator = Validator::new(validator_schema.clone());
+    let (errors, warnings) = validator
+        .validate(&policy_set, ValidationMode::Strict)
+        .into_errors_and_warnings();
+    for error in errors {
+        eprintln!("error: {error:?}");
+    }
+    for warning in warnings {
+        eprintln!("warning: {warning:?}");
+    }
 
-    // Should output `Deny`
-    println!("{:#?}", answer);
+    const ITERS: u32 = 10000;
+    for i in 0..ITERS {
+        let authorizer = Authorizer::new();
+
+        let tim = user_euid("tim");
+        let hash = organization_euid("HASH");
+        let hash_admin = organization_role_euid("HASH", "admin");
+        let hash_member = organization_role_euid("HASH", "member");
+        let view = EntityUID::from(Action::View);
+        let update = EntityUID::from(Action::Update);
+
+        let request = Request::new_with_unknowns(
+            EntityUIDEntry::Known {
+                euid: Arc::new(tim.clone()),
+                loc: None,
+            },
+            EntityUIDEntry::Known {
+                euid: Arc::new(update.clone()),
+                loc: None,
+            },
+            EntityUIDEntry::Unknown { loc: None },
+            None,
+            Some(&schema),
+            Extensions::all_available(),
+        )?;
+
+        let entities = Entities::new().add_entities(
+            [
+                Entity::new(
+                    tim.clone(),
+                    HashMap::from([]),
+                    HashSet::from([hash_admin.clone()]),
+                    &Extensions::all_available(),
+                )?,
+                Entity::new(
+                    hash_admin.clone(),
+                    HashMap::new(),
+                    HashSet::from([hash_member.clone()]),
+                    &Extensions::all_available(),
+                )?,
+            ],
+            Some(&schema),
+            TCComputation::ComputeNow,
+            Extensions::all_available(),
+        )?;
+
+        let response = authorizer.is_authorized_core(request.clone(), &policy_set, &entities);
+        if i == 0 {
+            println!("{request}");
+            println!("{entities}");
+            match response {
+                ResponseKind::FullyEvaluated(response) => println!("{:?}", response.decision),
+                ResponseKind::Partial(residual) => {
+                    println!("Residuals:");
+                    for policy in residual.residuals.policies() {
+                        println!("{policy}");
+                    }
+                }
+            }
+        }
+    }
+    println!("Took ~{:?} to run a query", start.elapsed() / ITERS);
 
     Ok(())
 }
