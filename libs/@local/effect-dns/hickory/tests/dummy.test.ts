055c67f992172b452a59b5a101b7d2cda70c18a7
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-02-05 10:07:55 +0100

    fix: lint

--- libs/@local/effect-dns/hickory/tests/dummy.test.ts
+++ libs/@local/effect-dns/hickory/tests/dummy.test.ts
@@ -1,8 +1,9 @@
-import { describe, it, expect } from "vitest";
+import { describe, test, expect } from "vitest";
+
 import { sum } from "@local/effect-dns-hickory";
 
 describe("example", () => {
-  it("should work", () => {
+  test("should work", () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
