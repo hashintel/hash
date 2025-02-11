3aa62dbfaf0a3a8e8e1a10c66ac7247291fe6e3a
Tim Diekmann <post@timdiekmann.de>
2025-02-12 00:07:20 +0100

    Attempt to fix all vercel builds

--- apps/hashdotdev/vercel-build.sh
+++ apps/hashdotdev/vercel-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+source "$HOME/.cargo/env"
+
 set -euo pipefail
 
 echo "Changing dir to root"
 cd ../..
 
-echo "Building hash.design"
+echo "Building hash.dev"
 turbo build --filter='@apps/hashdotdev' --env-mode=loose
