264b1cb75985f6f6a3816f2ed333f01b1fe1b1f7
Tim Diekmann <post@timdiekmann.de>
2025-03-03 01:51:37 +0100

    Attempt to install Rust through mise in Vercel

--- apps/hashdotdev/vercel-install.sh
+++ apps/hashdotdev/vercel-install.sh
@@ -9,7 +9,9 @@ yum-config-manager --add-repo https://mise.jdx.dev/rpm/mise.repo
 yum install -y mise
 
 echo "Installing prerequisites"
-mise install node npm:turbo rust[profile=minimal]
+mise install node npm:turbo
+mise use --global yq
+mise use --global rust[profile=minimal]@$(yq '.toolchain.channel' rust-toolchain.toml)
 
 # echo "Installing Rust"
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none --profile minimal
