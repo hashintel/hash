73df77efcca2264ec42019c490897f94ad65f321
Tim Diekmann <post@timdiekmann.de>
2025-03-03 23:08:57 +0100

    Attempt to print more debug info

--- apps/hash-frontend/vercel-install.sh
+++ apps/hash-frontend/vercel-install.sh
@@ -13,15 +13,16 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://mise.jdx.dev/rpm/mise.repo
 yum install -y mise
 
+export MISE_VERBOSE=1
 echo "Installing prerequisites"
 mise install node npm:turbo java
 mise use --global yq
 mise ls
 eval "$(mise activate bash)"
 mise ls
-mise which protoc
-yq '.toolchain.channel' rust-toolchain.toml
+mise which yq
 mise exec yq -- yq '.toolchain.channel' rust-toolchain.toml
+yq '.toolchain.channel' rust-toolchain.toml
 mise use --global rust[profile=minimal]@$(yq '.toolchain.channel' rust-toolchain.toml)
 
 
--- apps/hashdotdev/vercel-install.sh
+++ apps/hashdotdev/vercel-install.sh
@@ -10,14 +10,17 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://mise.jdx.dev/rpm/mise.repo
 yum install -y mise
 
+export MISE_VERBOSE=1
 echo "Installing prerequisites"
-mise install node npm:turbo
+mise install node npm:turbo java
 mise use --global yq
 mise ls
 eval "$(mise activate bash)"
 mise ls
-mise which protoc
-mise use --global rust[profile=minimal]@$(mise exec yq -- yq '.toolchain.channel' rust-toolchain.toml)
+mise which yq
+mise exec yq -- yq '.toolchain.channel' rust-toolchain.toml
+yq '.toolchain.channel' rust-toolchain.toml
+mise use --global rust[profile=minimal]@$(yq '.toolchain.channel' rust-toolchain.toml)
 
 echo "Installing yarn dependencies"
 LEFTHOOK=0 yarn install --immutable
