2ab2c8b57b70769d52c2805f24eafe95c2125b0a
Tim Diekmann <post@timdiekmann.de>
2025-03-03 23:17:47 +0100

    Fix vercel

--- apps/hashdotdesign/vercel-install.sh
+++ apps/hashdotdesign/vercel-install.sh
@@ -9,17 +9,11 @@ echo "installing mise"
 yum install -y yum-utils
 yum-config-manager --add-repo https://mise.jdx.dev/rpm/mise.repo
 yum install -y mise
+eval "$(mise activate bash)"
 
-export MISE_VERBOSE=1
 echo "Installing prerequisites"
 mise install node npm:turbo java
 mise use --global yq
-mise ls
-eval "$(mise activate bash)"
-mise ls
-mise which yq
-mise exec yq -- yq '.toolchain.channel' rust-toolchain.toml
-yq '.toolchain.channel' rust-toolchain.toml
 mise use --global rust[profile=minimal]@$(yq '.toolchain.channel' rust-toolchain.toml)
 
 echo "Installing yarn dependencies"
