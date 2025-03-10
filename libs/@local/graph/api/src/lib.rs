cf03c9cca8c025b66c374801ecc1e791987bd0f7
Tim Diekmann <post@timdiekmann.de>
2025-03-10 13:08:47 +0100

    Add getAccountGroupRelations method to GraphAuth API
    - Adds new method to retrieve members and administrators of account groups
    - Implements the method in SpiceDB authorization backend
    - Exposes functionality through REST API endpoint
    - Updates OpenAPI specification
    - Documents OpenAPI generation in crate documentation

--- libs/@local/graph/api/src/lib.rs
+++ libs/@local/graph/api/src/lib.rs
@@ -4,6 +4,21 @@
     return_type_notation,
     error_generic_member_access
 )]
+//! # Graph API
+//!
+//! This crate provides the REST and RPC APIs for the HASH Graph.
+//!
+//! ## OpenAPI Documentation
+//!
+//! The API includes automatic OpenAPI specification generation. To generate the latest OpenAPI
+//! specification:
+//!
+//! ```bash
+//! yarn codegen:generate-openapi-specs
+//! ```
+//!
+//! This will generate the OpenAPI specification in the `openapi/` directory, which can be used
+//! to generate client SDKs or to document the API.
 
 extern crate alloc;
 
