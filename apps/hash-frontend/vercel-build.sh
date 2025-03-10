4bf4178ab9bf0b4ee31280872275a29cf32c7921
Tim Diekmann <post@timdiekmann.de>
2025-03-10 10:27:00 +0100

    Print frontend URL

--- apps/hash-frontend/vercel-build.sh
+++ apps/hash-frontend/vercel-build.sh
@@ -8,6 +8,9 @@ set -euo pipefail
 source "$HOME/.cargo/env"
 eval "$(mise activate bash --shims)"
 
+echo "FRONTEND_URL: $FRONTEND_URL"
+mise exec bash -- bash -c 'echo $FRONTEND_URL'
+
 echo "Changing dir to root"
 cd ../..
 
