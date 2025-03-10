18c839f9eee427e9e13817b9c3f84e4a2e12ed6c
Tim Diekmann <post@timdiekmann.de>
2025-03-10 17:44:22 +0100

    Move docs to `README.md` instead

--- libs/@local/graph/api/src/lib.rs
+++ libs/@local/graph/api/src/lib.rs
@@ -1,19 +1,4 @@
-//! # HASH Graph API
-//!
-//! This crate provides the REST and HaRPC APIs for the HASH Graph.
-//!
-//! ## OpenAPI Documentation
-//!
-//! The API includes automatic OpenAPI specification generation. To generate the latest OpenAPI
-//! specification:
-//!
-//! ```bash
-//! yarn codegen:generate-openapi-specs
-//! ```
-//!
-//! This will generate the OpenAPI specification in the `openapi/` directory, which can be used
-//! to generate client SDKs or to document the API.
-//!
+#![cfg_attr(doc, doc = include_str!("../README.md"))]
 //! ## Workspace dependencies
 #![cfg_attr(doc, doc = simple_mermaid::mermaid!("../docs/dependency-diagram.mmd"))]
 #![feature(
