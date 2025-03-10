26dfb6b15d5a983cbaa8d3c3b1809556ae893513
Tim Diekmann <post@timdiekmann.de>
2025-03-10 10:37:05 +0100

    Clean up code again

--- apps/hash-frontend/vercel-build.sh
+++ apps/hash-frontend/vercel-build.sh
@@ -5,9 +5,11 @@ set -euo pipefail
 source "$HOME/.cargo/env"
 eval "$(mise activate bash --shims)"
 
-echo "FRONTEND_URL: $FRONTEND_URL"
-mise exec bash -- bash -c 'echo $FRONTEND_URL'
-
+# TODO: Mise is picking up `.env` files. We need to overhaul our approach for
+#   environment variables. To avoid this in the meantime, we'll remove the
+#   `.env` file.
+# See: https://linear.app/hash/issue/H-3213/use-consistent-naming-schema-for-environment-variables
+# See: https://linear.app/hash/issue/H-4202/sort-out-which-environment-variables-are-defined-where
 rm .env
 
 echo "Changing dir to root"
