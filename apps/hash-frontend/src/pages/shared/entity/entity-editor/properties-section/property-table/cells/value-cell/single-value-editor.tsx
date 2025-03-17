737c5d7262b8a1c6c1b12029817d0db8e5fe9a70
Tim Diekmann <post@timdiekmann.de>
2025-03-17 10:08:09 +0100

    Revert change in value metadata modification

--- apps/hash-frontend/src/pages/shared/entity/entity-editor/properties-section/property-table/cells/value-cell/single-value-editor.tsx
+++ apps/hash-frontend/src/pages/shared/entity/entity-editor/properties-section/property-table/cells/value-cell/single-value-editor.tsx
@@ -1,5 +1,8 @@
 import { useQuery } from "@apollo/client";
-import type { ClosedDataType } from "@blockprotocol/type-system";
+import type {
+  ClosedDataType,
+  PropertyValueMetadata,
+} from "@blockprotocol/type-system";
 import { isValueMetadata } from "@blockprotocol/type-system";
 import { Autocomplete, Chip } from "@hashintel/design-system";
 import { GRID_CLICK_IGNORE_CLASS } from "@hashintel/design-system/constants";
@@ -125,10 +128,9 @@ export const SingleValueEditor: ValueCellEditorComponent = (props) => {
 
   useEffect(() => {
     if (
-      valueMetadata &&
-      isValueMetadata(valueMetadata) &&
       chosenDataType &&
-      valueMetadata.metadata.dataTypeId !== chosenDataType.dataType.$id
+      (valueMetadata as PropertyValueMetadata | undefined)?.metadata
+        .dataTypeId !== chosenDataType.dataType.$id
     ) {
       const { propertyMetadata } = generateNewMetadataObject({
         propertyKeyChain,
