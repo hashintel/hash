09166bca10ce1373b4926627e7763fd997bf06ca
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-03-26 10:44:08 +0100

    fix: merge error

--- libs/@local/hql/core/src/symbol/mod.rs
+++ libs/@local/hql/core/src/symbol/mod.rs
@@ -38,7 +38,6 @@ impl Symbol {
         self.0.as_str()
     }
 
-    #[expect(clippy::missing_const_for_fn, reason = "false positive")]
     #[must_use]
     pub fn as_bytes(&self) -> &[u8] {
         self.0.as_bytes()
