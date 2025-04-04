1d69d6490b78c3a109455f6ca47c1c6f5a4cb553
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-04-04 17:29:27 +0200

    feat: `find` infrastructure

---
+++ libs/@local/hashql/compiletest/src/suite/mod.rs
@@ -0,0 +1,31 @@
+mod parse_syntax_dump;
+
+use hashql_ast::node::expr::Expr;
+use hashql_core::span::SpanId;
+use hashql_diagnostics::{Diagnostic, category::DiagnosticCategory};
+
+use self::parse_syntax_dump::ParseSyntaxDumpSuite;
+
+type SuiteDiagnostic = Diagnostic<Box<dyn DiagnosticCategory>, SpanId>;
+
+pub(crate) trait Suite: Send + Sync + 'static {
+    fn name(&self) -> &'static str;
+
+    fn run(
+        &self,
+        expr: &Expr,
+        diagnostics: &mut Vec<SuiteDiagnostic>,
+    ) -> Result<String, SuiteDiagnostic>;
+}
+
+const SUITES: &[&dyn Suite] = &[&ParseSyntaxDumpSuite];
+
+pub(crate) fn suite(name: &str) -> Option<&'static dyn Suite> {
+    for suite in SUITES {
+        if suite.name() == name {
+            return Some(*suite);
+        }
+    }
+
+    None
+}
---
+++ libs/@local/hashql/compiletest/src/suite/parse_syntax_dump.rs
@@ -0,0 +1,15 @@
+use hashql_ast::{format::SyntaxDump, node::expr::Expr};
+
+use super::{Suite, SuiteDiagnostic};
+
+pub(crate) struct ParseSyntaxDumpSuite;
+
+impl Suite for ParseSyntaxDumpSuite {
+    fn name(&self) -> &'static str {
+        "parse/syntax-dump"
+    }
+
+    fn run(&self, expr: &Expr, _: &mut Vec<SuiteDiagnostic>) -> Result<String, SuiteDiagnostic> {
+        Ok(expr.syntax_dump_to_string())
+    }
+}
