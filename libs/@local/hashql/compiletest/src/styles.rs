b9cc4a3be50dd1da87e287508fc4c706edd8535e
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-04-05 18:17:33 +0200

    feat: try another way

--- libs/@local/hashql/compiletest/src/styles.rs
+++ libs/@local/hashql/compiletest/src/styles.rs
@@ -5,5 +5,6 @@ pub(crate) const BLUE: Style = Style::new().fg_color(Some(Color::Ansi(AnsiColor:
 pub(crate) const RED: Style = Style::new().fg_color(Some(Color::Ansi(AnsiColor::Red)));
 pub(crate) const GREEN: Style = Style::new().fg_color(Some(Color::Ansi(AnsiColor::Green)));
 pub(crate) const YELLOW: Style = Style::new().fg_color(Some(Color::Ansi(AnsiColor::Yellow)));
+pub(crate) const MAGENTA: Style = Style::new().fg_color(Some(Color::Ansi(AnsiColor::Magenta)));
 
 pub(crate) const BOLD: Style = Style::new().bold();
