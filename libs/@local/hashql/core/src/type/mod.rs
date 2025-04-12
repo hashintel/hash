a6a1edc25e82f534ce934b3076964b555b26240d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-04-13 00:09:11 +0200

    feat: `unify_type` infrastructure

--- libs/@local/hashql/core/src/type/mod.rs
+++ libs/@local/hashql/core/src/type/mod.rs
@@ -7,9 +7,9 @@ pub mod primitive;
 pub mod unify;
 
 use self::{
-    intrinsic::IntrinsicType,
+    intrinsic::{IntrinsicType, unify_intrinsic},
     pretty_print::{PrettyPrint, RecursionLimit},
-    primitive::PrimitiveType,
+    primitive::{PrimitiveType, unify_primitive},
     unify::UnificationContext,
 };
 use crate::{arena::Arena, id::HasId, newtype, span::SpanId};
@@ -18,20 +18,27 @@ newtype!(
     pub struct TypeId(u32 is 0..=0xFFFF_FF00)
 );
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct ClosureType {}
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct StructType {}
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct TupleType {}
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct OpaqueType {}
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct UnionType {}
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Param {}
 
 pub struct GenericArgument {}
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum TypeKind {
     Closure(ClosureType),
     Primitive(PrimitiveType),
@@ -80,5 +87,26 @@ impl HasId for Type {
 }
 
 pub(crate) fn unify_type(context: &mut UnificationContext, lhs: TypeId, rhs: TypeId) {
-    todo!()
+    let lhs = context.arena[lhs];
+    let rhs = context.arena[rhs];
+
+    match (lhs.kind, rhs.kind) {
+        (TypeKind::Primitive(lhs_kind), TypeKind::Primitive(rhs_kind)) => {
+            unify_primitive(context, lhs.map(|_| lhs_kind), rhs.map(|_| rhs_kind));
+        }
+        (TypeKind::Intrinsic(lhs_kind), TypeKind::Intrinsic(rhs_kind)) => {
+            unify_intrinsic(context, lhs.map(|_| lhs_kind), rhs.map(|_| rhs_kind));
+        }
+        (TypeKind::Error, _) => {
+            // do nothing, simply propagate the error up
+            context.mark_error(rhs);
+        }
+        (_, TypeKind::Error) => {
+            // do nothing, simply propagate the error up
+            context.mark_error(lhs);
+        }
+        _ => {
+            todo!()
+        }
+    }
 }
