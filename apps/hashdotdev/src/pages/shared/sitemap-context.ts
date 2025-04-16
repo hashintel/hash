6e07695fe67708917e5a99092a18ea1a6f464e20
Dei Vilkinsons <6226576+vilkinsons@users.noreply.github.com>
2025-04-16 08:51:10 +0100

    Move `apps/hashdotdev` directory
    Moved to `internal` (editable content to be extricated and moved back here later)

--- apps/hashdotdev/src/pages/shared/sitemap-context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-import type { SiteMap } from "./sitemap";
-
-export const SiteMapContext = createContext<SiteMap>({
-  pages: [],
-});
--- apps/hashdotdev/src/theme/components/mui-svg-icon-theme-overrides.tsx
+++
@@ -1,9 +0,0 @@
-import type { Components, Theme } from "@mui/material";
-
-export const MuiSvgIconThemeOptions: Components<Theme>["MuiSvgIcon"] = {
-  styleOverrides: {
-    root: {
-      fontSize: "var(--step-0)",
-    },
-  },
-};
