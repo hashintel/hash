fbe4da0bb40b0ba19f716d285d2b57ae2c44841b
Ciaran Morinan <c@hash.ai>
2025-04-28 13:20:32 +0100

    fix creating org membership link when user creates an org

--- apps/hash-api/src/seed-data/index.ts
+++ apps/hash-api/src/seed-data/index.ts
@@ -7,6 +7,7 @@ import {
   createOrg,
   getOrgByShortname,
 } from "../graph/knowledge/system-types/org";
+import { createOrgMembershipLinkEntity } from "../graph/knowledge/system-types/org-membership";
 import type { User } from "../graph/knowledge/system-types/user";
 import { joinOrg } from "../graph/knowledge/system-types/user";
 import type { PageDefinition } from "./seed-pages";
@@ -83,6 +84,12 @@ export const seedOrgsAndUsers = async (params: {
         extractWebIdFromEntityId(user.entity.metadata.recordId.entityId) !==
         orgOwner.accountId
       ) {
+        /**
+         * For users who AREN'T the org owner, we need to create their full membership,
+         * including both the permission system membership, and the link entity in the graph.
+         *
+         * @todo H-4441 have the Graph handle memberOf link entity creation, as well as permisison handling.
+         */
         await joinOrg(
           context,
           /** Only the org owner has permission to add members to the organizations */
@@ -90,6 +97,23 @@ export const seedOrgsAndUsers = async (params: {
           {
             userEntityId: user.entity.metadata.recordId.entityId,
             orgEntityId: sharedOrg.entity.metadata.recordId.entityId,
+          },
+        );
+      } else {
+        /**
+         * For the org owner, we only need to create the link entity in the graph,
+         * as the permission system membership is created automatically by the Graph when the org is created.
+         *
+         * @todo H-4441 have the Graph handle memberOf link entity creation, as well as permisison handling.
+         */
+        await createOrgMembershipLinkEntity(
+          context,
+          {
+            actorId: user.accountId,
+          },
+          {
+            orgEntityId: sharedOrg.entity.metadata.recordId.entityId,
+            userEntityId: user.entity.metadata.recordId.entityId,
           },
         );
       }
