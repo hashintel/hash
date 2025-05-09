87a4babaec8b69a6f2f84a4bdee2c0797643b213
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-05-09 23:06:12 +0200

    feat: documentation

--- libs/@local/hashql/hir/src/node/kind.rs
+++ libs/@local/hashql/hir/src/node/kind.rs
@@ -3,22 +3,31 @@ use super::variable::Variable;
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum NodeKind<'heap> {
     Call,
+
+    // Data Structures
     Struct,
     Dict,
     Tuple,
     List,
+
     Literal,
     Variable(Variable<'heap>),
     Binding, // local binding
     Input,
     Closure,
     // If - currently unsupported
+
+    // Indexing
     Field,
     Index,
-    Is, // Type Assertion (optimized out later?)
+
+    // Type Assertions
+    Is, // Type Assertion (optimized out later after typechk)
 
     // These ones are new
     Constructor, // ctor for opaque values
     BinaryOperation,
     UnaryOperation,
+    // TODO: graph operations
+    Graph,
 }
