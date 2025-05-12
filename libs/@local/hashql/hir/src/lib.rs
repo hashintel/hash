d0c6ee793559e90e45448901c133bd5a29672caf
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-05-12 09:37:53 +0200

    chore: mock test

--- libs/@local/hashql/hir/src/lib.rs
+++ libs/@local/hashql/hir/src/lib.rs
@@ -4,3 +4,9 @@
 #![cfg_attr(doc, doc = simple_mermaid::mermaid!("../docs/dependency-diagram.mmd"))]
 #![feature(never_type)]
 pub mod node;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
