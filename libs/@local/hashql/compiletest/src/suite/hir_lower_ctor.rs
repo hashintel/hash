1eeb841dbe80555c8be71004128437034b4605a3
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-05-29 15:04:30 +0200

    feat: compiletest integration

---
+++ libs/@local/hashql/compiletest/src/suite/hir_lower_ctor.rs
@@ -0,0 +1,81 @@
+use core::fmt::Write as _;
+
+use hashql_ast::{lowering::lower, node::expr::Expr};
+use hashql_core::{
+    heap::Heap,
+    module::ModuleRegistry,
+    pretty::{PrettyOptions, PrettyPrint as _},
+    span::SpanId,
+    r#type::environment::Environment,
+};
+use hashql_hir::{
+    fold::Fold as _,
+    intern::Interner,
+    lower::{alias::AliasReplacement, ctor::ConvertTypeConstructor},
+    node::Node,
+};
+
+use super::{Suite, SuiteDiagnostic, common::process_diagnostics};
+
+pub(crate) struct HirLowerCtorSuite;
+
+impl Suite for HirLowerCtorSuite {
+    fn name(&self) -> &'static str {
+        "hir/lower/ctor"
+    }
+
+    fn run<'heap>(
+        &self,
+        heap: &'heap Heap,
+        mut expr: Expr<'heap>,
+        diagnostics: &mut Vec<SuiteDiagnostic>,
+    ) -> Result<String, SuiteDiagnostic> {
+        let environment = Environment::new(SpanId::SYNTHETIC, heap);
+        let registry = ModuleRegistry::new(&environment);
+
+        let (types, lower_diagnostics) = lower(
+            heap.intern_symbol("::main"),
+            &mut expr,
+            &environment,
+            &registry,
+        );
+
+        process_diagnostics(diagnostics, lower_diagnostics)?;
+
+        let interner = Interner::new(heap);
+        let (node, reify_diagnostics) = Node::from_ast(expr, &environment, &interner, &types);
+        process_diagnostics(diagnostics, reify_diagnostics)?;
+
+        let node = node.expect("should be `Some` if there are non-fatal errors");
+
+        let mut output = node
+            .pretty_print(&environment, PrettyOptions::default().without_color())
+            .to_string();
+
+        output.push_str("\n\n--------------------------------------\n\n");
+
+        let mut replacement = AliasReplacement::new(&interner);
+        let Ok(node) = replacement.fold_node(node);
+
+        let mut converter =
+            ConvertTypeConstructor::new(&interner, &types.locals, &registry, &environment);
+
+        let node = match converter.fold_node(node) {
+            Ok(node) => node,
+            Err(reported) => {
+                let diagnostic = process_diagnostics(diagnostics, reported)
+                    .expect_err("reported diagnostics should always be fatal");
+                return Err(diagnostic);
+            }
+        };
+
+        write!(
+            &mut output,
+            "{}",
+            node.pretty_print(&environment, PrettyOptions::default().without_color())
+        )
+        .expect("infallible");
+
+        Ok(output)
+    }
+}
