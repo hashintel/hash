6664ba7ba29e05e2fd81a1c974502cf51983f535
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-06-06 18:28:11 +0200

    feat: header

--- libs/@local/hashql/compiletest/src/suite/common.rs
+++ libs/@local/hashql/compiletest/src/suite/common.rs
@@ -1,3 +1,5 @@
+use core::fmt::{self, Display, Write as _};
+
 use hashql_core::span::SpanId;
 use hashql_diagnostics::{Diagnostic, category::DiagnosticCategory};
 
@@ -30,3 +32,35 @@ where
 
     fatal.map_or(Ok(()), Err)
 }
+
+pub(crate) struct Header(&'static str);
+
+impl Header {
+    pub(crate) const fn new(title: &'static str) -> Self {
+        Self(title)
+    }
+}
+
+impl Display for Header {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        const HEADER: char = '\u{2550}';
+
+        let len = self.0.len();
+
+        for _ in 0..4 {
+            fmt.write_char(HEADER)?;
+        }
+
+        write!(fmt, " {} ", self.0)?;
+
+        let remaining = (80_usize - 4).saturating_sub(len + 2);
+
+        for _ in 0..remaining {
+            fmt.write_char(HEADER)?;
+        }
+
+        Ok(())
+    }
+}
+
+// TODO: one for the items c:
