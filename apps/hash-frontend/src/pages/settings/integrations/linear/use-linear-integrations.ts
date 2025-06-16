a714a9a8da167fa43f6452dd7df55eb2de95c268
Ciaran Morinan <c@hash.ai>
2025-06-16 18:06:01 +0100

    fix linear integrations query

--- apps/hash-frontend/src/pages/settings/integrations/linear/use-linear-integrations.ts
+++ apps/hash-frontend/src/pages/settings/integrations/linear/use-linear-integrations.ts
@@ -68,7 +68,7 @@ export const useLinearIntegrations = (): {
             },
             {
               equal: [
-                { path: ["metadata", "entityTypeId"] },
+                { path: ["type", "versionedUrl"] },
                 { parameter: systemEntityTypes.linearIntegration.entityTypeId },
               ],
             },
