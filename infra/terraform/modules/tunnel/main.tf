1b29e51a503b60b94e3fce60452408d2487984c3
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2025-06-17 12:29:01 +0200

    H-4811: Make the Terraform SSH-tunneling script more robust (#7449)

--- infra/terraform/modules/tunnel/main.tf
+++ infra/terraform/modules/tunnel/main.tf
@@ -11,13 +11,14 @@
 data "external" "ssh_tunnel" {
   program = ["${path.module}/ssh_tunnel.sh"]
   query = {
-    ssh_host           = var.ssh_host,
-    ssh_port           = var.ssh_port,
-    ssh_user           = var.ssh_user,
-    ssh_private_key    = var.ssh_private_key,
-    tunnel_target_host = var.tunnel_target_host,
-    tunnel_target_port = var.tunnel_target_port
-    local_tunnel_port  = 45678
-    timeout            = var.timeout,
+    ssh_host            = var.ssh_host,
+    ssh_port            = var.ssh_port,
+    ssh_user            = var.ssh_user,
+    ssh_private_key     = var.ssh_private_key,
+    tunnel_target_host  = var.tunnel_target_host,
+    tunnel_target_port  = var.tunnel_target_port,
+    tunnel_max_attempts = var.tunnel_max_attempts,
+    local_tunnel_port   = 45678,
+    timeout             = var.timeout,
   }
 }
