f7205922b07fe785fdcf670c782446e3fc3b2c65
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-06-26 13:13:38 +0200

    squash

---
+++ libs/@local/hashql/eval/src/graph/read/filter_expr.rs
@@ -0,0 +1,530 @@
+use alloc::borrow::Cow;
+use core::fmt::Debug;
+
+use hash_graph_store::filter::{FilterExpression, QueryRecord};
+use hashql_core::{
+    literal::LiteralKind,
+    span::SpanId,
+    value::{Opaque, Value},
+};
+use hashql_hir::node::{
+    Node,
+    access::{Access, AccessKind, field::FieldAccess, index::IndexAccess},
+    call::Call,
+    data::{Data, DataKind},
+    graph::GraphKind,
+    input::Input,
+    kind::NodeKind,
+    r#let::Let,
+    operation::{
+        BinaryOperation, Operation, OperationKind, TypeOperation,
+        binary::BinOpKind,
+        r#type::{TypeAssertion, TypeConstructor, TypeOperationKind},
+    },
+    variable::{LocalVariable, Variable, VariableKind},
+};
+
+use super::{
+    CompilationError, FilterCompilerContext, GraphReadCompiler,
+    convert::convert_value_to_parameter,
+    error::{
+        GraphReadCompilerDiagnostic, call_unsupported, closure_unsupported,
+        field_access_internal_error, nested_graph_read_unsupported, path_conversion_error,
+        path_indexing_unsupported, path_traversal_internal_error, qualified_variable_unsupported,
+        value_parameter_conversion_error,
+    },
+    path::{CompleteQueryPath, PartialQueryPath, traverse_into_field, traverse_into_index},
+};
+use crate::graph::read::error::{
+    binary_operation_unsupported, index_access_internal_error, type_constructor_unsupported,
+};
+
+pub(crate) enum IntermediateExpression<'env, 'heap, P> {
+    Value {
+        value: Cow<'env, Value<'heap>>,
+        span: SpanId,
+    },
+    Path {
+        path: Option<P>,
+        span: SpanId,
+    },
+}
+
+impl<'heap, P> IntermediateExpression<'_, 'heap, P> {
+    pub(crate) fn finish<R>(
+        self,
+        context: FilterCompilerContext<'heap>,
+        diagnostics: &mut Vec<GraphReadCompilerDiagnostic>,
+    ) -> Result<FilterExpression<'heap, R>, CompilationError>
+    where
+        R: QueryRecord<QueryPath<'heap>: CompleteQueryPath<'heap, PartialQueryPath = P>>,
+        P: PartialQueryPath<'heap, QueryPath = R::QueryPath<'heap>>,
+    {
+        match self {
+            Self::Value { value, span } => {
+                let parameter = match convert_value_to_parameter(&value) {
+                    Ok(value) => value,
+                    Err(error) => {
+                        diagnostics.push(value_parameter_conversion_error(context, span, error));
+                        return Err(CompilationError);
+                    }
+                };
+
+                Ok(FilterExpression::Parameter {
+                    parameter,
+                    convert: None,
+                })
+            }
+            Self::Path { path: None, span } => {
+                diagnostics.push(path_conversion_error(context, span));
+                Err(CompilationError)
+            }
+            Self::Path {
+                path: Some(path),
+                span,
+            } => {
+                let Some(path) = path.finish() else {
+                    diagnostics.push(path_conversion_error(context, span));
+                    return Err(CompilationError);
+                };
+
+                Ok(FilterExpression::Path { path })
+            }
+        }
+    }
+}
+
+impl<'env, 'heap: 'env> GraphReadCompiler<'env, 'heap> {
+    const fn compile_filter_expr_data<P>(
+        Data { span, kind }: &'heap Data<'heap>,
+    ) -> IntermediateExpression<'env, 'heap, P>
+    where
+        P: PartialQueryPath<'heap>,
+    {
+        match kind {
+            DataKind::Literal(literal) => IntermediateExpression::Value {
+                value: Cow::Owned(Value::Primitive(literal.kind)),
+                span: *span,
+            },
+        }
+    }
+
+    fn compile_filter_expr_variable<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        Variable { span: _, kind }: &'heap Variable<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        match kind {
+            &VariableKind::Local(LocalVariable {
+                span,
+                name,
+                arguments: _,
+            }) if name.value == context.param_name => {
+                Ok(IntermediateExpression::Path { path: None, span })
+            }
+            VariableKind::Local(LocalVariable {
+                span,
+                name,
+                arguments: _,
+            }) => {
+                let value = self.locals[&name.value];
+                self.compile_filter_expr(context.with_current_span(*span), value)
+            }
+            VariableKind::Qualified(qualified_variable) => {
+                self.diagnostics
+                    .push(qualified_variable_unsupported(context, qualified_variable));
+
+                Err(CompilationError)
+            }
+        }
+    }
+
+    fn compile_filter_expr_let<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        Let {
+            span: _,
+            name,
+            value,
+            body,
+        }: &'heap Let<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        self.locals.insert(name.value, value);
+        let result = self.compile_filter_expr(context, body);
+        self.locals.remove(&name.value);
+
+        result
+    }
+
+    fn compile_filter_expr_input<P>(
+        &self,
+        Input {
+            span,
+            name,
+            r#type: _,
+            default: _,
+        }: &'heap Input<'heap>,
+    ) -> IntermediateExpression<'env, 'heap, P>
+    where
+        P: PartialQueryPath<'heap>,
+    {
+        let value = &self.inputs[&name.value];
+
+        IntermediateExpression::Value {
+            value: Cow::Borrowed(value),
+            span: *span,
+        }
+    }
+
+    fn compile_filter_expr_operation_type<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        TypeOperation { span: _, kind }: &'heap TypeOperation<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        match kind {
+            TypeOperationKind::Assertion(TypeAssertion {
+                span: _,
+                value,
+                r#type: _,
+                force: _,
+            }) => self.compile_filter_expr(context, value),
+            &TypeOperationKind::Constructor(TypeConstructor {
+                span,
+                name: _,
+                closure: _,
+                arguments: _,
+            }) => {
+                self.diagnostics
+                    .push(type_constructor_unsupported(context, span));
+
+                Err(CompilationError)
+            }
+        }
+    }
+
+    fn compile_filter_expr_operation_binary<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        &BinaryOperation {
+            span: _,
+            op,
+            left: _,
+            right: _,
+        }: &'heap BinaryOperation<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap>,
+    {
+        match op.kind {
+            BinOpKind::And
+            | BinOpKind::Or
+            | BinOpKind::Eq
+            | BinOpKind::Ne
+            | BinOpKind::Lt
+            | BinOpKind::Lte
+            | BinOpKind::Gt
+            | BinOpKind::Gte => {
+                self.diagnostics
+                    .push(binary_operation_unsupported(context, op));
+
+                Err(CompilationError)
+            }
+        }
+    }
+
+    fn compile_filter_expr_operation<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        Operation { span: _, kind }: &'heap Operation<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        match kind {
+            OperationKind::Type(r#type) => self.compile_filter_expr_operation_type(context, r#type),
+            OperationKind::Binary(binary) => {
+                self.compile_filter_expr_operation_binary(context, binary)
+            }
+        }
+    }
+
+    fn compile_filter_expr_access_field<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        FieldAccess {
+            span,
+            expr: expr_node,
+            field,
+        }: &'heap FieldAccess<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        let expr = self.compile_filter_expr::<P>(context, expr_node)?;
+
+        let output = match expr {
+            IntermediateExpression::Value { value, span: _ } => {
+                let value = match value {
+                    Cow::Borrowed(value) => value.access_by_field(field.value).map(Cow::Borrowed),
+                    Cow::Owned(value) => {
+                        value.access_by_field(field.value).cloned().map(Cow::Owned)
+                    }
+                };
+
+                let value = match value {
+                    Ok(value) => value,
+                    Err(error) => {
+                        self.diagnostics.push(field_access_internal_error(
+                            expr_node.span,
+                            field,
+                            &error,
+                        ));
+                        return Err(CompilationError);
+                    }
+                };
+
+                IntermediateExpression::Value { value, span: *span }
+            }
+
+            IntermediateExpression::Path { path, span: _ } => {
+                let path = match traverse_into_field(path, self.heap, field.value) {
+                    Ok(path) => path,
+                    Err(path) => {
+                        self.diagnostics.push(path_traversal_internal_error(
+                            expr_node.span,
+                            field.span,
+                            path.as_ref(),
+                        ));
+
+                        return Err(CompilationError);
+                    }
+                };
+
+                IntermediateExpression::Path {
+                    path: Some(path),
+                    span: *span,
+                }
+            }
+        };
+
+        Ok(output)
+    }
+
+    fn compile_filter_expr_access_index<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        IndexAccess {
+            span,
+            expr: expr_node,
+            index: index_node,
+        }: &'heap IndexAccess<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        let expr = self.compile_filter_expr::<P>(context, expr_node);
+        let index = self.compile_filter_expr::<P>(context, index_node);
+
+        let (expr, index) = (expr?, index?);
+
+        let index = match index {
+            IntermediateExpression::Value { value, span: _ } => value,
+            IntermediateExpression::Path { path: _, span: _ } => {
+                self.diagnostics.push(path_indexing_unsupported(
+                    context,
+                    expr_node.span,
+                    index_node.span,
+                ));
+
+                return Err(CompilationError);
+            }
+        };
+
+        let output = match expr {
+            IntermediateExpression::Value { value, span: _ } => {
+                let value = match value {
+                    Cow::Borrowed(value) => value
+                        .access_by_index(&index)
+                        .map(|value| value.map(Cow::Borrowed)),
+                    Cow::Owned(value) => value
+                        .access_by_index(&index)
+                        .map(|value| value.cloned().map(Cow::Owned)),
+                };
+
+                let value = match value {
+                    Ok(value) => value,
+                    Err(error) => {
+                        self.diagnostics.push(index_access_internal_error(
+                            expr_node.span,
+                            index_node.span,
+                            &error,
+                        ));
+                        return Err(CompilationError);
+                    }
+                };
+
+                let value =
+                    value.unwrap_or_else(|| Cow::Owned(Value::Primitive(LiteralKind::Null)));
+
+                IntermediateExpression::Value { value, span: *span }
+            }
+            IntermediateExpression::Path { path, span: _ } => {
+                let path = match traverse_into_index(path, self.heap, index) {
+                    Ok(path) => path,
+                    Err(path) => {
+                        self.diagnostics.push(path_traversal_internal_error(
+                            expr_node.span,
+                            index_node.span,
+                            path.as_ref(),
+                        ));
+
+                        return Err(CompilationError);
+                    }
+                };
+
+                IntermediateExpression::Path {
+                    path: Some(path),
+                    span: *span,
+                }
+            }
+        };
+
+        Ok(output)
+    }
+
+    fn compile_filter_expr_access<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        Access { span: _, kind }: &'heap Access<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        match kind {
+            AccessKind::Field(field) => self.compile_filter_expr_access_field(context, field),
+            AccessKind::Index(index) => self.compile_filter_expr_access_index(context, index),
+        }
+    }
+
+    fn compile_filter_expr_call_ctor(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        span: SpanId,
+        node: &'heap Node<'heap>,
+    ) -> Result<&'heap TypeConstructor<'heap>, CompilationError> {
+        match node.kind {
+            NodeKind::Operation(Operation {
+                span: _,
+                kind:
+                    OperationKind::Type(TypeOperation {
+                        span: _,
+                        kind: TypeOperationKind::Constructor(ctor),
+                    }),
+            }) => Ok(ctor),
+            NodeKind::Variable(Variable {
+                span: _,
+                kind: VariableKind::Local(local),
+            }) => self.compile_filter_expr_call_ctor(context, span, self.locals[&local.name.value]),
+            _ => {
+                self.diagnostics.push(call_unsupported(context, span));
+                Err(CompilationError)
+            }
+        }
+    }
+
+    // in theory they could be in the narrow context that they take a *single* argument,
+    // and that argument happens to be the entity being filtered, because then we can
+    // statically analyze them real easy.
+    fn compile_filter_expr_call<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        Call {
+            span,
+            function,
+            arguments,
+        }: &'heap Call<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        let ctor = self.compile_filter_expr_call_ctor(context, *span, function)?;
+
+        match &**arguments {
+            [] => Ok(IntermediateExpression::Value {
+                value: Cow::Owned(Value::Opaque(Opaque::new(
+                    ctor.name,
+                    Value::Primitive(LiteralKind::Null),
+                ))),
+                span: *span,
+            }),
+            [argument] => {
+                let argument =
+                    self.compile_filter_expr(context.without_current_span(), &argument.value)?;
+
+                match argument {
+                    IntermediateExpression::Value { value, span } => {
+                        Ok(IntermediateExpression::Value {
+                            value: Cow::Owned(Value::Opaque(Opaque::new(
+                                ctor.name,
+                                value.into_owned(),
+                            ))),
+                            span,
+                        })
+                    }
+                    // paths simply "pass through"
+                    path @ IntermediateExpression::Path { .. } => Ok(path),
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    // We can't return a `FilterExpression` instead we require to return our own `Expression` that
+    // can then be converted to a `FilterExpression`. That allows us to carry the values using
+    // `&'heap` lifetimes.
+    pub(super) fn compile_filter_expr<P>(
+        &mut self,
+        context: FilterCompilerContext<'heap>,
+        node: &'heap Node<'heap>,
+    ) -> Result<IntermediateExpression<'env, 'heap, P>, CompilationError>
+    where
+        P: PartialQueryPath<'heap> + Debug,
+    {
+        match node.kind {
+            NodeKind::Data(data) => Ok(Self::compile_filter_expr_data(data)),
+            NodeKind::Variable(variable) => self.compile_filter_expr_variable(context, variable),
+            NodeKind::Let(r#let) => self.compile_filter_expr_let(context, r#let),
+            NodeKind::Input(input) => Ok(self.compile_filter_expr_input(input)),
+            NodeKind::Operation(operation) => {
+                self.compile_filter_expr_operation(context, operation)
+            }
+            NodeKind::Access(access) => self.compile_filter_expr_access(context, access),
+            NodeKind::Call(call) => self.compile_filter_expr_call(context, call),
+            NodeKind::Closure(_) => {
+                self.diagnostics.push(closure_unsupported(
+                    context,
+                    context.current_span.unwrap_or(node.span),
+                ));
+                Err(CompilationError)
+            }
+            NodeKind::Graph(graph) => match graph.kind {
+                GraphKind::Read(_) => {
+                    self.diagnostics.push(nested_graph_read_unsupported(
+                        context,
+                        context.current_span.unwrap_or(node.span),
+                    ));
+                    Err(CompilationError)
+                }
+            },
+        }
+    }
+}
---
+++ libs/@local/hashql/eval/src/graph/read/mod.rs
@@ -0,0 +1,213 @@
+mod convert;
+pub mod error;
+mod filter;
+mod filter_expr;
+mod path;
+
+use core::{fmt::Debug, ops::Range};
+
+use hash_graph_store::filter::{Filter, QueryRecord};
+use hashql_core::{
+    collection::FastHashMap, heap::Heap, span::SpanId, symbol::Symbol, value::Value,
+};
+use hashql_hir::{
+    node::{
+        HirId, Node,
+        graph::read::{GraphRead, GraphReadBody, GraphReadHead},
+        kind::NodeKind,
+        r#let::Let,
+    },
+    visit::{self, Visitor},
+};
+use type_system::knowledge::Entity;
+
+use self::{error::GraphReadCompilerDiagnostic, path::CompleteQueryPath};
+
+// The FilterSlice is an indirect approach to allow us to easily copy a filter between different
+// nodes.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum FilterSlice {
+    Entity { range: Range<usize> },
+}
+
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct Filters<'heap> {
+    entity: Vec<Filter<'heap, Entity>>,
+}
+
+impl<'heap> Filters<'heap> {
+    #[must_use]
+    pub fn entity(&self, range: Range<usize>) -> &[Filter<'heap, Entity>] {
+        &self.entity[range]
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct FilterCompilerContext<'heap> {
+    span: SpanId,
+    current_span: Option<SpanId>,
+    param_name: Symbol<'heap>,
+}
+
+impl FilterCompilerContext<'_> {
+    const fn without_current_span(self) -> Self {
+        Self {
+            span: self.span,
+            current_span: None,
+            param_name: self.param_name,
+        }
+    }
+
+    const fn with_current_span(self, span: SpanId) -> Self {
+        Self {
+            span: self.span,
+            current_span: Some(span),
+            param_name: self.param_name,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct GraphReadCompilerResidual<'heap> {
+    pub filters: Filters<'heap>,
+    pub output: FastHashMap<HirId, FilterSlice>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct CompilationError;
+
+pub struct GraphReadCompiler<'env, 'heap> {
+    current: HirId,
+    heap: &'heap Heap,
+    filters: Filters<'heap>,
+
+    diagnostics: Vec<GraphReadCompilerDiagnostic>,
+
+    locals: FastHashMap<Symbol<'heap>, &'heap Node<'heap>>,
+    inputs: &'env FastHashMap<Symbol<'heap>, Value<'heap>>,
+    output: FastHashMap<HirId, FilterSlice>,
+}
+
+impl<'env, 'heap: 'env> GraphReadCompiler<'env, 'heap> {
+    pub fn new(heap: &'heap Heap, inputs: &'env FastHashMap<Symbol<'heap>, Value<'heap>>) -> Self {
+        Self {
+            current: HirId::PLACEHOLDER,
+            heap,
+            filters: Filters::default(),
+            diagnostics: Vec::new(),
+            locals: FastHashMap::default(),
+            inputs,
+            output: FastHashMap::default(),
+        }
+    }
+
+    /// Finish the compilation process.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if any diagnostics were collected during compilation.
+    pub fn finish(
+        self,
+    ) -> Result<GraphReadCompilerResidual<'heap>, Vec<GraphReadCompilerDiagnostic>> {
+        if !self.diagnostics.is_empty() {
+            return Err(self.diagnostics);
+        }
+
+        Ok(GraphReadCompilerResidual {
+            filters: self.filters,
+            output: self.output,
+        })
+    }
+
+    fn compile_graph_body<R>(
+        &mut self,
+        body: &'heap [GraphReadBody<'heap>],
+    ) -> Result<Vec<Filter<'heap, R>>, CompilationError>
+    where
+        R: QueryRecord<QueryPath<'heap>: CompleteQueryPath<'heap, PartialQueryPath: Debug>>,
+    {
+        let mut filters = Ok(Vec::new());
+
+        for body in body {
+            match body {
+                GraphReadBody::Filter(node) => {
+                    let NodeKind::Closure(closure) = node.kind else {
+                        unreachable!()
+                    };
+
+                    let filter = self.compile_filter::<R>(
+                        FilterCompilerContext {
+                            span: closure.body.span,
+                            current_span: None,
+                            param_name: closure.signature.params[0].name.value,
+                        },
+                        &closure.body,
+                    );
+
+                    match (&mut filters, filter) {
+                        (Ok(filters), Ok(filter)) => filters.push(filter),
+                        (filters @ Ok(_), Err(error)) => *filters = Err(error),
+
+                        // There is no point in pushing it, if we already errored out, we still
+                        // process it to collect more diagnostics
+                        (Err(_), _) => {}
+                    }
+                }
+            }
+        }
+
+        filters
+    }
+
+    fn compile_graph_read(
+        &mut self,
+        read: &'heap GraphRead<'heap>,
+    ) -> Result<FilterSlice, CompilationError> {
+        match read.head {
+            GraphReadHead::Entity { axis: _ } => {
+                let filters = self.compile_graph_body(&read.body)?;
+
+                let start = self.filters.entity.len();
+                self.filters.entity.extend(filters);
+                let end = self.filters.entity.len();
+
+                Ok(FilterSlice::Entity { range: start..end })
+            }
+        }
+    }
+}
+
+impl<'heap> Visitor<'heap> for GraphReadCompiler<'_, 'heap> {
+    fn visit_node(&mut self, node: &Node<'heap>) {
+        if self.output.contains_key(&node.id) {
+            return; // We've already processed this node, so skip it.
+        }
+
+        let previous = self.current;
+        self.current = node.id;
+
+        visit::walk_node(self, node);
+
+        self.current = previous;
+    }
+
+    fn visit_let(&mut self, r#let: &'heap Let<'heap>) {
+        self.locals.insert(r#let.name.value, &r#let.value);
+        visit::walk_let(self, r#let);
+        self.locals.remove(&r#let.name.value);
+
+        if let Some(value) = self.output.get(&r#let.body.id) {
+            self.output.insert(self.current, value.clone());
+        }
+    }
+
+    fn visit_graph_read(&mut self, graph_read: &'heap GraphRead<'heap>) {
+        visit::walk_graph_read(self, graph_read);
+
+        if let Ok(filter) = self.compile_graph_read(graph_read) {
+            self.output
+                .try_insert(self.current, filter)
+                .expect("Same node shouldn't be processed multiple times");
+        }
+    }
+}
