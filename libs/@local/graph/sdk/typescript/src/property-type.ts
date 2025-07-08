e1050c20e57f72dbad8190e232f066fd11ea7b7a
Tim Diekmann <post@timdiekmann.de>
2025-07-08 09:57:39 +0200

    Fix return type on `hasPermissionForPropertyTypes`

--- libs/@local/graph/sdk/typescript/src/property-type.ts
+++ libs/@local/graph/sdk/typescript/src/property-type.ts
@@ -8,24 +8,20 @@ import type { ActionName } from "@rust/hash-graph-authorization/types";
 
 import type { AuthenticationContext } from "./authentication-context.js";
 
-export const hasPermissionForPropertyTypes = <
-  T extends [VersionedUrl, ...VersionedUrl[]],
->(
+export const hasPermissionForPropertyTypes = (
   graphAPI: GraphApi,
   authentication: AuthenticationContext,
   params: Subtype<
     HasPermissionForPropertyTypesParams,
     {
-      propertyTypeIds: T;
+      propertyTypeIds: VersionedUrl[];
       action: Subtype<
         ActionName,
         "viewPropertyType" | "updatePropertyType" | "archivePropertyType"
       >;
     }
   >,
-): Promise<{ [K in keyof T]: VersionedUrl }> =>
+): Promise<VersionedUrl[]> =>
   graphAPI
     .hasPermissionForPropertyTypes(authentication.actorId, params)
-    .then(
-      ({ data: permitted }) => permitted as { [K in keyof T]: VersionedUrl },
-    );
+    .then(({ data: permitted }) => permitted as VersionedUrl[]);
