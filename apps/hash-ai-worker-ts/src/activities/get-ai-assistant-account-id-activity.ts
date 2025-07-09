cf9ee058f6fada65578c90b297771ffda393fa14
Tim Diekmann <post@timdiekmann.de>
2025-07-09 14:05:58 +0200

    Make AI member role assignment more robust

--- apps/hash-ai-worker-ts/src/activities/get-ai-assistant-account-id-activity.ts
+++ apps/hash-ai-worker-ts/src/activities/get-ai-assistant-account-id-activity.ts
@@ -1,7 +1,10 @@
 import type { ActorEntityUuid, AiId, WebId } from "@blockprotocol/type-system";
 import { getAiIdByIdentifier } from "@local/hash-backend-utils/machine-actors";
 import type { GraphApi } from "@local/hash-graph-client";
-import { addActorGroupMember } from "@local/hash-graph-sdk/principal/actor-group";
+import {
+  addActorGroupMember,
+  getActorGroupRole,
+} from "@local/hash-graph-sdk/principal/actor-group";
 
 export const getAiAssistantAccountIdActivity = async (params: {
   authentication: { actorId: ActorEntityUuid };
@@ -20,11 +23,34 @@ export const getAiAssistantAccountIdActivity = async (params: {
     return null;
   }
 
+  // TODO: We want to add a toggle in the web settings to allow the AI assistant to be a member of the web.
+  //       With that, this branch can be removed.
+  //   see https://linear.app/hash/issue/H-4972/add-toggle-in-org-settings-to-enable-ai-flows-in-a-web
   if (grantCreatePermissionForWeb) {
-    await addActorGroupMember(graphApiClient, authentication, {
+    const webRole = await getActorGroupRole(graphApiClient, authentication, {
       actorId: aiAssistantAccountId,
       actorGroupId: grantCreatePermissionForWeb,
     });
+
+    // If the AI assistant is not a member of the web, we need to add them as a member.
+    // This can only be done if the user is an administrator of the web.
+    if (!webRole) {
+      const isWebAdmin = await getActorGroupRole(
+        graphApiClient,
+        authentication,
+        {
+          actorId: authentication.actorId,
+          actorGroupId: grantCreatePermissionForWeb,
+        },
+      ).then((role) => role === "administrator");
+
+      if (isWebAdmin) {
+        await addActorGroupMember(graphApiClient, authentication, {
+          actorId: aiAssistantAccountId,
+          actorGroupId: grantCreatePermissionForWeb,
+        });
+      }
+    }
   }
 
   return aiAssistantAccountId;
