c85df01b6ef843b34f34722b71a2edf80c4ed1cf
Tim Diekmann <post@timdiekmann.de>
2025-07-24 22:27:57 +0200

    Fix KMS permissions

--- infra/terraform/hash/observability/logs.tf
+++ infra/terraform/hash/observability/logs.tf
@@ -1,11 +1,49 @@
 # CloudWatch Log Group for all observability services
 # Shared by otel_collector, tempo, loki, prometheus, grafana
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 # KMS key for CloudWatch Log Group encryption
 resource "aws_kms_key" "log_group" {
   description             = "KMS key for observability CloudWatch log group encryption"
   deletion_window_in_days = 7
   enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableIAMUserPermissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowCloudWatchLogs"
+        Effect = "Allow"
+        Principal = {
+          Service = "logs.${data.aws_region.current.name}.amazonaws.com"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+        Condition = {
+          ArnEquals = {
+            "kms:EncryptionContext:aws:logs:arn" = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/ecs/${var.prefix}"
+          }
+        }
+      }
+    ]
+  })
 
   tags = {
     Purpose = "CloudWatch log group encryption for observability cluster"
