8fa1ec42e0e08631c82755dd3f9595e2c7ce0a96
Ciaran Morinan <c@hash.ai>
2025-08-26 19:22:49 +0100

    remove dependency of @blockprotocol/graph on local package

--- libs/@blockprotocol/graph/eslint.config.js
+++ libs/@blockprotocol/graph/eslint.config.js
@@ -5,6 +5,20 @@ export default [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      rules: {
+        "no-restricted-imports": [
+          "error",
+          {
+            patterns: [
+              {
+                group: ["@local/*"],
+                message:
+                  "You cannot use unpublished local packages in a published package.",
+              },
+            ],
+          },
+        ],
+      },
     },
   },
 ];
