3c9e3196525db524402b2553b8dda19d5418e6da
Tim Diekmann <post@timdiekmann.de>
2025-08-30 23:12:04 +0200

    Fix tests and docs

--- libs/error-stack/src/result.rs
+++ libs/error-stack/src/result.rs
@@ -2,7 +2,7 @@ use core::error::Error;
 
 use crate::{Attachment, IntoReport, OpaqueAttachment, Report};
 
-/// Extension trait for [`Result`][core::result::Result] to provide context information on
+/// Extension trait for [`Result`] to provide context information on
 /// [`Report`]s.
 pub trait ResultExt {
     /// The context type of the [`Result`] which becomes the `C` in `Report<C>`.
@@ -15,7 +15,7 @@ pub trait ResultExt {
     ///
     /// Applies [`Report::attach`] on the [`Err`] variant, refer to it for more
     /// information.
-    fn attach<A>(self, attachment: A) -> core::result::Result<Self::Ok, Report<Self::Context>>
+    fn attach<A>(self, attachment: A) -> Result<Self::Ok, Report<Self::Context>>
     where
         A: Attachment;
 
@@ -23,10 +23,7 @@ pub trait ResultExt {
     ///
     /// Applies [`Report::attach`] on the [`Err`] variant, refer to it for more
     /// information.
-    fn attach_with<A, F>(
-        self,
-        attachment: F,
-    ) -> core::result::Result<Self::Ok, Report<Self::Context>>
+    fn attach_with<A, F>(self, attachment: F) -> Result<Self::Ok, Report<Self::Context>>
     where
         A: Attachment,
         F: FnOnce() -> A;
@@ -34,20 +31,14 @@ pub trait ResultExt {
     /// Adds a new attachment to the [`Report`] inside the [`Result`].
     ///
     /// Applies [`Report::attach_opaque`] on the [`Err`] variant, refer to it for more information.
-    fn attach_opaque<A>(
-        self,
-        attachment: A,
-    ) -> core::result::Result<Self::Ok, Report<Self::Context>>
+    fn attach_opaque<A>(self, attachment: A) -> Result<Self::Ok, Report<Self::Context>>
     where
         A: OpaqueAttachment;
 
     /// Lazily adds a new attachment to the [`Report`] inside the [`Result`].
     ///
     /// Applies [`Report::attach_opaque`] on the [`Err`] variant, refer to it for more information.
-    fn attach_opaque_with<A, F>(
-        self,
-        attachment: F,
-    ) -> core::result::Result<Self::Ok, Report<Self::Context>>
+    fn attach_opaque_with<A, F>(self, attachment: F) -> Result<Self::Ok, Report<Self::Context>>
     where
         A: OpaqueAttachment,
         F: FnOnce() -> A;
@@ -55,20 +46,20 @@ pub trait ResultExt {
     /// Changes the context of the [`Report`] inside the [`Result`].
     ///
     /// Applies [`Report::change_context`] on the [`Err`] variant, refer to it for more information.
-    fn change_context<C>(self, context: C) -> core::result::Result<Self::Ok, Report<C>>
+    fn change_context<C>(self, context: C) -> Result<Self::Ok, Report<C>>
     where
         C: Error + Send + Sync + 'static;
 
     /// Lazily changes the context of the [`Report`] inside the [`Result`].
     ///
     /// Applies [`Report::change_context`] on the [`Err`] variant, refer to it for more information.
-    fn change_context_lazy<C, F>(self, context: F) -> core::result::Result<Self::Ok, Report<C>>
+    fn change_context_lazy<C, F>(self, context: F) -> Result<Self::Ok, Report<C>>
     where
         C: Error + Send + Sync + 'static,
         F: FnOnce() -> C;
 }
 
-impl<T, E> ResultExt for core::result::Result<T, E>
+impl<T, E> ResultExt for Result<T, E>
 where
     E: IntoReport,
 {
@@ -76,7 +67,7 @@ where
     type Ok = T;
 
     #[track_caller]
-    fn attach<A>(self, attachment: A) -> core::result::Result<T, Report<E::Context>>
+    fn attach<A>(self, attachment: A) -> Result<T, Report<E::Context>>
     where
         A: Attachment,
     {
@@ -87,7 +78,7 @@ where
     }
 
     #[track_caller]
-    fn attach_with<A, F>(self, attachment: F) -> core::result::Result<T, Report<E::Context>>
+    fn attach_with<A, F>(self, attachment: F) -> Result<T, Report<E::Context>>
     where
         A: Attachment,
         F: FnOnce() -> A,
@@ -99,7 +90,7 @@ where
     }
 
     #[track_caller]
-    fn attach_opaque<A>(self, attachment: A) -> core::result::Result<T, Report<E::Context>>
+    fn attach_opaque<A>(self, attachment: A) -> Result<T, Report<E::Context>>
     where
         A: OpaqueAttachment,
     {
@@ -110,7 +101,7 @@ where
     }
 
     #[track_caller]
-    fn attach_opaque_with<A, F>(self, attachment: F) -> core::result::Result<T, Report<E::Context>>
+    fn attach_opaque_with<A, F>(self, attachment: F) -> Result<T, Report<E::Context>>
     where
         A: OpaqueAttachment,
         F: FnOnce() -> A,
@@ -122,7 +113,7 @@ where
     }
 
     #[track_caller]
-    fn change_context<C>(self, context: C) -> core::result::Result<T, Report<C>>
+    fn change_context<C>(self, context: C) -> Result<T, Report<C>>
     where
         C: Error + Send + Sync + 'static,
     {
@@ -133,7 +124,7 @@ where
     }
 
     #[track_caller]
-    fn change_context_lazy<C, F>(self, context: F) -> core::result::Result<T, Report<C>>
+    fn change_context_lazy<C, F>(self, context: F) -> Result<T, Report<C>>
     where
         C: Error + Send + Sync + 'static,
         F: FnOnce() -> C,
