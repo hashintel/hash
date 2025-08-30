bf592db247051f568aed93a0016d967a184e1840
Tim Diekmann <post@timdiekmann.de>
2025-08-31 00:15:52 +0200

    Revert "Exclude `unstable` in compile tests"
    This reverts commit b2245af35a1853a31840160b67fc9670ace8a834.

--- libs/error-stack/tests/compiletest.rs
+++ libs/error-stack/tests/compiletest.rs
@@ -1,8 +1,4 @@
 #[cfg_attr(not(nightly), ignore = "Outputs are different across toolchains")]
-#[cfg_attr(
-    feature = "unstable",
-    ignore = "Outputs are different for the `bail!` macro as the implementation is different"
-)]
 #[cfg_attr(miri, ignore = "Miri does not support UI tests")]
 #[test]
 fn ui() {
