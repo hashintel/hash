b9510c90880405c4d7f52447b41b58c85d4c5652
Ciaran Morinan <c@hash.ai>
2025-09-22 18:28:32 +0100

    wip upgrade storybook

--- libs/@hashintel/block-design-system/src/editable-field.stories.tsx
+++ libs/@hashintel/block-design-system/src/editable-field.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryFn } from "@storybook/react";
+import type { Meta, StoryFn } from "@storybook/react-webpack5";
 import { useState } from "react";
 
 import { EditableField } from "./editable-field";
--- libs/@hashintel/block-design-system/src/get-help-link.stories.tsx
+++ libs/@hashintel/block-design-system/src/get-help-link.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
 
 import { GetHelpLink } from "./get-help-link";
 
