9e071a55ea0971fdab234b6c6a11859c5d8509d9
Chris Feijoo <chris@kube.io>
2025-09-24 13:55:21 +0200

    Reconfigure Storybook

--- libs/@hashintel/component-library/eslint.config.js
+++ libs/@hashintel/component-library/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { createBase, disableRules } from "@local/eslint/deprecated";
 
 export default [
@@ -20,4 +23,5 @@ export default [
       ],
     },
   },
+  ...storybook.configs["flat/recommended"],
 ];
