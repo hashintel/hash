38a7e81202289cd032aaa144f65167e0b568b347
Ciaran Morinan <c@hash.ai>
2025-09-29 15:57:12 +0100

    cleanup

--- libs/@local/advanced-types/src/brand.ts
+++
@@ -1,6 +0,0 @@
-import type { Brand } from "@blockprotocol/type-system";
-
-/**
- * The type-branding type to support opaque (name based) types
- */
-export type Opaque<Kind extends string> = Brand<symbol, Kind>;
