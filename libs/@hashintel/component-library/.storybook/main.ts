7c3fc4d644ae1cf1741ccee89f8cd00b0edce096
Chris Feijoo <kube@users.noreply.github.com>
2025-09-29 23:49:02 +0200

    Update libs/@hashintel/component-library/.storybook/main.ts
    Co-authored-by: Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>

--- libs/@hashintel/component-library/.storybook/main.ts
+++ libs/@hashintel/component-library/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-import { dirname, join } from "path";
+import { dirname, join } from "node:path";
 
 /**
  * This function is used to resolve the absolute path of a package.
