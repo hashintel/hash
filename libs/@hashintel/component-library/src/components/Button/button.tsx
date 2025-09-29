4ed600e4cc0f8bb5b4ed50b900c162d4d1c4c090
Chris Feijoo <chris@kube.io>
2025-09-29 22:17:26 +0200

    Refactor Button component to use React.FC and simplify props handling

--- libs/@hashintel/component-library/src/components/Button/button.tsx
+++ libs/@hashintel/component-library/src/components/Button/button.tsx
@@ -1,5 +1,4 @@
 import { css, cx } from "@hashintel/styled-system/css";
-import { forwardRef } from "react";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -12,36 +11,33 @@ export interface ButtonProps
   bezelWidth?: number;
 }
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, disabled, style, ...props }) => {
-    return (
-      <button
-        type="button"
-        className={cx(
-          css({
-            position: "relative",
-            display: "inline-flex",
-            alignItems: "center",
-            justifyContent: "center",
-            fontWeight: "semibold",
-            borderRadius: "md",
-            cursor: disabled ? "not-allowed" : "pointer",
-            transition: "all",
-            transitionDuration: "200ms",
-            overflow: "hidden",
-            paddingX: "3",
-            paddingY: "2",
-            fontSize: "base",
-          }),
-          className
-        )}
-        {...props}
-        style={style}
-      >
-        {children}
-      </button>
-    );
-  }
-);
-
-Button.displayName = "Button";
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      type="button"
+      className={cx(
+        css({
+          position: "relative",
+          fontWeight: "medium",
+          borderRadius: "lg",
+          cursor: "pointer",
+          overflow: "hidden",
+          paddingX: "3",
+          paddingY: "1.5",
+          fontSize: "sm",
+          _disabled: {
+            cursor: "not-allowed",
+          },
+        }),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
