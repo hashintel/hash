b56d4ff7ac272460acb2e7c486be242c547e72e4
Chris Feijoo <chris@kube.io>
2025-09-30 01:41:02 +0200

    Remove Context from Switch

--- libs/@hashintel/ds-components/src/components/Switch/switch.tsx
+++ libs/@hashintel/ds-components/src/components/Switch/switch.tsx
@@ -64,65 +64,62 @@ export const Switch: React.FC<SwitchProps> = ({
           onCheckedChange?.(details.checked);
         }}
       >
-        <BaseSwitch.Context>
-          {(context) => (
-            <>
-              <BaseSwitch.Control
-                style={{
-                  width: SLIDER_WIDTH,
-                  height: SLIDER_HEIGHT,
-                  borderRadius: SLIDER_RADIUS,
-                }}
-                className={css({
-                  position: "relative",
-                  display: "inline-block",
-                  cursor: disabled ? "not-allowed" : "pointer",
-                  transition: "all 0.2s ease",
-                  backgroundColor: context.checked ? "green.40" : "gray.40",
-                })}
-              >
-                <BaseSwitch.Thumb
-                  className={css({
-                    position: "absolute",
-                    top: "[50%]",
-                    left: `[${PADDING}px]`,
-                    transition: "[all 0.2s ease]",
-                    "&[data-state='checked']": {
-                      transform: `translateY(-50%) translateX(${TRAVEL}px)`,
-                    },
-                    "&[data-state='unchecked']": {
-                      top: "[50%]",
-                      transform: "translateY(-50%)",
-                    },
-                    "& > div": {
-                      backgroundColor: "[rgba(255, 255, 255, 1)]",
-                    },
-                    "&[data-active] > div": {
-                      transform: `scale(${THUMB_SCALE_ACTIVE})`,
-                      backgroundColor: "[rgba(255, 255, 255, 0.1)]",
-                      boxShadow:
-                        "0 2px 4px rgba(0,0,0,0.1), inset 0 1px 3px rgba(0,0,0,0.1), inset 0 -1px 3px rgba(255,255,255,0.1)",
-                    },
-                  })}
-                >
-                  <div
-                    className={css({
-                      display: "block",
-                      width: `[${THUMB_WIDTH}px]`,
-                      height: `[${THUMB_HEIGHT}px]`,
-                      borderRadius: `[${THUMB_RADIUS}px]`,
-                      boxShadow: "[0 4px 22px rgba(0,0,0,0.1)]",
-                      transition:
-                        "[all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)]",
-                    })}
-                    style={{ backdropFilter: `url(#${filterId})` }}
-                  />
-                </BaseSwitch.Thumb>
-              </BaseSwitch.Control>
-              <BaseSwitch.HiddenInput />
-            </>
-          )}
-        </BaseSwitch.Context>
+        <BaseSwitch.Control
+          style={{
+            width: SLIDER_WIDTH,
+            height: SLIDER_HEIGHT,
+            borderRadius: SLIDER_RADIUS,
+          }}
+          className={css({
+            position: "relative",
+            display: "inline-block",
+            cursor: disabled ? "not-allowed" : "pointer",
+            transition: "[all 0.2s ease]",
+            backgroundColor: "gray.40",
+            _checked: {
+              backgroundColor: "green.40",
+            },
+          })}
+        >
+          <BaseSwitch.Thumb
+            className={css({
+              position: "absolute",
+              top: "[50%]",
+              left: `[${PADDING}px]`,
+              transition: "[all 0.2s ease]",
+              "&[data-state='checked']": {
+                transform: `translateY(-50%) translateX(${TRAVEL}px)`,
+              },
+              "&[data-state='unchecked']": {
+                top: "[50%]",
+                transform: "translateY(-50%)",
+              },
+              "& > div": {
+                backgroundColor: "[rgba(255, 255, 255, 1)]",
+              },
+              "&[data-active] > div": {
+                transform: `scale(${THUMB_SCALE_ACTIVE})`,
+                backgroundColor: "[rgba(255, 255, 255, 0.1)]",
+                shadow:
+                  "[0 2px 4px rgba(0,0,0,0.1), inset 0 1px 3px rgba(0,0,0,0.1), inset 0 -1px 3px rgba(255,255,255,0.1)]",
+              },
+            })}
+          >
+            <div
+              className={css({
+                display: "block",
+                width: `[${THUMB_WIDTH}px]`,
+                height: `[${THUMB_HEIGHT}px]`,
+                borderRadius: `[${THUMB_RADIUS}px]`,
+                boxShadow: "[0 4px 22px rgba(0,0,0,0.1)]",
+                transition:
+                  "[all 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275)]",
+              })}
+              style={{ backdropFilter: `url(#${filterId})` }}
+            />
+          </BaseSwitch.Thumb>
+        </BaseSwitch.Control>
+        <BaseSwitch.HiddenInput />
       </BaseSwitch.Root>
     </>
   );
