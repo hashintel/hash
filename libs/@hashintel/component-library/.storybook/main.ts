a8aaac513876d68416fc6725c91ea715096846a5
Chris Feijoo <chris@kube.io>
2025-09-30 00:36:18 +0200

    Revert "Update libs/@hashintel/component-library/.storybook/main.ts"
    This reverts commit 7c3fc4d644ae1cf1741ccee89f8cd00b0edce096.

--- libs/@hashintel/component-library/.storybook/main.ts
+++ libs/@hashintel/component-library/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-import { dirname, join } from "node:path";
+import { dirname, join } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
