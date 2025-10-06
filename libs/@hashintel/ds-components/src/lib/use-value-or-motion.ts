27308fd31581f623c6bda7e875ba30079e654aa9
Chris Feijoo <chris@kube.io>
2025-10-06 15:10:15 +0200

    import from motion/react

--- libs/@hashintel/ds-components/src/lib/use-value-or-motion.ts
+++ libs/@hashintel/ds-components/src/lib/use-value-or-motion.ts
@@ -1,5 +1,4 @@
-import { MotionValue } from "motion";
-import { useTransform } from "motion/react";
+import { MotionValue, useTransform } from "motion/react";
 
 export function getValueOrMotion<T>(value: T | MotionValue<T>): T {
   return value instanceof MotionValue ? value.get() : value;
