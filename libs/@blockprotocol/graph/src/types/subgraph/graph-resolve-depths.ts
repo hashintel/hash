4ef45ff7d48d651b52296c61771b3f6e7c2d6ac3
Tim Diekmann <post@timdiekmann.de>
2025-10-08 17:20:30 +0200

    Update BP library

--- libs/@blockprotocol/graph/src/types/subgraph/graph-resolve-depths.ts
+++ libs/@blockprotocol/graph/src/types/subgraph/graph-resolve-depths.ts
@@ -1,21 +1,13 @@
-export interface OutgoingEdgeResolveDepth {
-  outgoing: number;
-}
-
 export interface EdgeResolveDepths {
-  incoming: number;
   outgoing: number;
 }
 
 /** @todo - Add documentation */
 export type GraphResolveDepths = {
-  hasLeftEntity: EdgeResolveDepths;
-  hasRightEntity: EdgeResolveDepths;
-
-  constrainsLinkDestinationsOn: OutgoingEdgeResolveDepth;
-  constrainsLinksOn: OutgoingEdgeResolveDepth;
-  constrainsPropertiesOn: OutgoingEdgeResolveDepth;
-  constrainsValuesOn: OutgoingEdgeResolveDepth;
-  inheritsFrom: OutgoingEdgeResolveDepth;
-  isOfType: OutgoingEdgeResolveDepth;
+  constrainsLinkDestinationsOn: EdgeResolveDepths;
+  constrainsLinksOn: EdgeResolveDepths;
+  constrainsPropertiesOn: EdgeResolveDepths;
+  constrainsValuesOn: EdgeResolveDepths;
+  inheritsFrom: EdgeResolveDepths;
+  isOfType: EdgeResolveDepths;
 };
