06a5b030d60be2674ae63e305646342789b1f4c2
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-10-08 17:22:24 +0200

    feat: libify repo-chores

--- libs/@local/repo-chores/rust/src/cli/subcommand/dependency_diagram/mod.rs
+++ libs/@local/repo-chores/rust/src/cli/subcommand/dependency_diagram/mod.rs
@@ -2,10 +2,11 @@ use std::{fs, io::Write as _, path::PathBuf};
 
 use clap::Parser;
 use error_stack::{Report, ResultExt as _};
-use hash_repo_chores::dependency_diagram::{
+use tracing::{debug, info, instrument};
+
+use crate::dependency_diagram::{
     DependencyDiagramConfig, DependencyDiagramError, LinkMode, generate_dependency_diagram,
 };
-use tracing::{debug, info, instrument};
 
 /// Arguments for the diagram subcommand.
 ///
