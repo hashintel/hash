ad3697812a744ca67011425fb0b5f846cab6df44
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-10-17 20:07:26 +0200

    feat: use recycler in hoist

--- libs/@local/hashql/core/src/collections/pool.rs
+++ libs/@local/hashql/core/src/collections/pool.rs
@@ -1,3 +1,5 @@
+use crate::id::{Id, bit_vec::MixedBitSet};
+
 pub trait Recycler<T> {
     type Config;
 
@@ -6,26 +8,37 @@ pub trait Recycler<T> {
     fn prepare(&mut self, item: &mut T, config: Self::Config);
 }
 
+#[derive(Debug)]
 pub struct Pool<T, R> {
     free: Vec<T>,
     recycler: R,
-    max_size: usize,
+    capacity: usize,
+}
+
+impl<T, R> Pool<T, R> {
+    #[must_use]
+    pub fn new(capacity: usize) -> Self
+    where
+        R: Default,
+    {
+        Self::with_recycler(capacity, R::default())
+    }
+
+    pub fn with_recycler(capacity: usize, recycler: R) -> Self {
+        Self {
+            free: Vec::with_capacity(capacity),
+            recycler,
+            capacity,
+        }
+    }
 }
 
 impl<T, R> Pool<T, R>
 where
     R: Recycler<T>,
 {
-    pub fn new(recycler: R, max_size: usize) -> Self {
-        Self {
-            free: Vec::with_capacity(max_size),
-            recycler,
-            max_size,
-        }
-    }
-
     pub fn resize(&mut self, new_max_size: usize) {
-        self.max_size = new_max_size;
+        self.capacity = new_max_size;
         if self.free.len() > new_max_size {
             self.free.truncate(new_max_size);
         }
@@ -35,7 +48,11 @@ where
         }
     }
 
-    pub fn acquire(&mut self, config: R::Config) -> T {
+    pub const fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn acquire_with(&mut self, config: R::Config) -> T {
         let Some(mut item) = self.free.pop() else {
             return self.recycler.acquire(config);
         };
@@ -44,8 +61,15 @@ where
         item
     }
 
+    pub fn acquire(&mut self) -> T
+    where
+        R: Recycler<T, Config = ()>,
+    {
+        self.acquire_with(())
+    }
+
     pub fn release(&mut self, mut item: T) {
-        if self.free.len() >= self.max_size {
+        if self.free.len() >= self.capacity {
             return;
         }
 
@@ -53,3 +77,52 @@ where
         self.free.push(item);
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
+pub struct VecRecycler;
+
+impl<T> Recycler<Vec<T>> for VecRecycler {
+    type Config = usize;
+
+    fn recycle(&mut self, item: &mut Vec<T>) {
+        item.clear();
+    }
+
+    fn acquire(&mut self, config: Self::Config) -> Vec<T> {
+        Vec::with_capacity(config)
+    }
+
+    fn prepare(&mut self, item: &mut Vec<T>, config: Self::Config) {
+        // Only *grow* the vector if it's smaller than the requested capacity
+        if item.capacity() < config {
+            item.reserve(config - item.capacity());
+        }
+    }
+}
+
+pub type VecPool<T> = Pool<Vec<T>, VecRecycler>;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Default)]
+pub struct MixedBitSetRecycler {
+    pub domain_size: usize,
+}
+
+impl<I> Recycler<MixedBitSet<I>> for MixedBitSetRecycler
+where
+    I: Id,
+{
+    type Config = ();
+
+    fn recycle(&mut self, item: &mut MixedBitSet<I>) {
+        item.clear();
+    }
+
+    fn acquire(&mut self, (): ()) -> MixedBitSet<I> {
+        MixedBitSet::new_empty(self.domain_size)
+    }
+
+    // We do not need to change anything when preparing, because the domain size is constant
+    fn prepare(&mut self, _: &mut MixedBitSet<I>, (): ()) {}
+}
+
+pub type MixedBitSetPool<I> = Pool<MixedBitSet<I>, MixedBitSetRecycler>;
