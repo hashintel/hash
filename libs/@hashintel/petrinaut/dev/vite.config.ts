4c8b822f367890fec28bc0d2b504f5a59c375ec7
Ciaran Morinan <37743469+CiaranMn@users.noreply.github.com>
2025-10-25 11:55:40 +0100

    Update libs/@hashintel/petrinaut/dev/vite.config.ts
    Co-authored-by: Chris Feijoo <kube@users.noreply.github.com>

--- libs/@hashintel/petrinaut/dev/vite.config.ts
+++ libs/@hashintel/petrinaut/dev/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  mode: "development",
   plugins: [react()],
   root: "dev",
   server: {
