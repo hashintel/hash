208c78f673337e4b220d2f40663e421f21cf85e1
Bilal Mahmoud <bilalmahmoud@posteo.net>
2025-10-29 18:10:37 +0100

    Update dom-shim.d.ts

--- apps/hash-api/src/dom-shim.d.ts
+++ apps/hash-api/src/dom-shim.d.ts
@@ -1,1 +1,14 @@
-declare const window: unknown;
+declare const window: {
+  location: {
+    href: string;
+    origin: string;
+    protocol: string;
+    host: string;
+    hostname: string;
+    port: string;
+    pathname: string;
+    search: string;
+    hash: string;
+  };
+  [key: string]: unknown;
+};
