d5625766fb27a108a0ca8954d885654f54c947d9
Chris Feijoo <chris@kube.io>
2025-11-01 19:41:37 +0100

    Add some Refraction and quick and dirty fix for FlexibleFilter

--- libs/@hashintel/petrinaut/src/petrinaut-sdcpn.tsx
+++ libs/@hashintel/petrinaut/src/petrinaut-sdcpn.tsx
@@ -1,6 +1,8 @@
 import "reactflow/dist/style.css";
 import "./petrinaut-sdcpn/index.css";
 
+import { RefractivePane } from "@hashintel/ds-components/refractive-pane";
+import { css } from "@hashintel/ds-helpers/css";
 import { Box, Stack } from "@mui/material";
 import type { DragEvent } from "react";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
@@ -381,6 +383,43 @@ const PetrinautInner = ({
             }}
           >
             <ModeSelector mode={mode} onChange={setMode} />
+          </div>
+
+          {/* Properties Panel - Right Side */}
+          <div
+            style={{
+              display: "flex",
+              position: "fixed",
+              top: 0,
+              right: 0,
+              bottom: 0,
+              padding: "24px",
+              height: "calc(100% - 48px)",
+              zIndex: 1000,
+            }}
+          >
+            <RefractivePane
+              radius={16}
+              blur={7}
+              specularOpacity={0.2}
+              scaleRatio={1}
+              bezelWidth={65}
+              glassThickness={120}
+              refractiveIndex={1.5}
+              className={css({
+                height: "100%",
+                width: "320px",
+                backgroundColor: "rgba(255, 255, 255, 0.7)",
+                boxShadow: "0 4px 30px rgba(0, 0, 0, 0.15)",
+                border: "1px solid rgba(255, 255, 255, 0.8)",
+              })}
+              style={{
+                borderRadius: 16,
+                padding: 8,
+              }}
+            >
+              Hello
+            </RefractivePane>
           </div>
 
           <ReactFlow
