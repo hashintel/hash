72851cf5ce11d27351f156a0c8f05b09f8b421b8
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-11-21 13:57:10 +0100

    fix: ignore snapshot that adds additional notes

--- libs/error-stack/tests/compiletest.rs
+++ libs/error-stack/tests/compiletest.rs
@@ -1,7 +1,22 @@
+use std::ffi::OsStr;
+
 #[cfg_attr(not(nightly), ignore = "Outputs are different across toolchains")]
 #[cfg_attr(miri, ignore = "Miri does not support UI tests")]
 #[test]
-fn ui() {
+fn ui() -> Result<(), Box<dyn core::error::Error>> {
+    let paths = glob::glob("tests/ui/*.rs")?.collect::<Result<Vec<_>, _>>()?;
+
     let test_cases = trybuild::TestCases::new();
-    test_cases.compile_fail("tests/ui/*.rs");
+
+    for path in paths {
+        if cfg!(feature = "serde") && path.file_name() == Some(OsStr::new("macro_invalid_args.rs"))
+        {
+            // This test adds additional notes when serde is enabled
+            continue;
+        }
+
+        test_cases.compile_fail(path);
+    }
+
+    Ok(())
 }
