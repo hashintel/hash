c9936e9f6f999241f6cd8ace179fb2619fdba5ea
Chris Feijoo <chris@kube.io>
2025-11-28 17:02:48 +0100

    Update from @indietyp review on other PR, which was merged by mistake

--- libs/@hashintel/ds-components/src/components/Switch/switch.tsx
+++ libs/@hashintel/ds-components/src/components/Switch/switch.tsx
@@ -36,7 +36,7 @@ const switchThumbStyles = css({
   top: "[50%]",
   left: `[${PADDING}px]`,
   transition: "[all 0.2s ease]",
-  "&[data-state='checked']": {
+  _checked: {
     transform: `translateY(-50%) translateX(${TRAVEL}px)`,
   },
   "&[data-state='unchecked']": {
