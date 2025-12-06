81aba703b9175c2062570fae1dad1ef217542831
Dei Vilkinsons <6226576+vilkinsons@users.noreply.github.com>
2025-12-06 18:20:42 +0100

    Update account dropdown menu items

--- apps/hash-frontend/src/shared/layout/layout-with-header/account-dropdown.tsx
+++ apps/hash-frontend/src/shared/layout/layout-with-header/account-dropdown.tsx
@@ -164,15 +164,6 @@ export const AccountDropdown: FunctionComponent<AccountDropdownProps> = ({
           >
             <ListItemText primary="Organizations" />
           </MenuItem>,
-          isInstanceAdmin ? (
-            <MenuItem
-              href="/admin"
-              key="admin"
-              onClick={() => popupState.close()}
-            >
-              <ListItemText primary="Instance Settings" />
-            </MenuItem>
-          ) : null,
           <MenuItem
             href="/settings/integrations"
             key="integrations"
@@ -180,6 +171,16 @@ export const AccountDropdown: FunctionComponent<AccountDropdownProps> = ({
           >
             <ListItemText primary="Integrations" />
           </MenuItem>,
+          <Divider key="divider" />,
+          isInstanceAdmin ? (
+            <MenuItem
+              href="/admin"
+              key="admin"
+              onClick={() => popupState.close()}
+            >
+              <ListItemText primary="Instance Admin" />
+            </MenuItem>
+          ) : null,
           <Divider key="divider" />,
         ]}
 
