e20eef022538768bb0a8a3aca2739b3f87a7a533
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-12-11 18:04:06 +0100

    chore: checkpoint

--- libs/@local/hashql/mir/src/pass/transform/sroa/mod.rs
+++ libs/@local/hashql/mir/src/pass/transform/sroa/mod.rs
@@ -1,4 +1,4 @@
-use core::mem;
+use core::{convert::Infallible, mem};
 
 use hashql_core::heap::{Heap, Scratch};
 
@@ -8,32 +8,69 @@ use crate::{
         basic_block::BasicBlockId,
         local::{Local, LocalVec},
         location::Location,
-        place::{DefUse, PlaceContext},
+        place::{DefUse, Place, PlaceContext},
     },
     context::MirContext,
-    pass::{AnalysisPass, TransformPass, analysis::DataDependencyAnalysis},
-    visit::Visitor,
+    intern::Interner,
+    pass::{
+        AnalysisPass as _, TransformPass,
+        analysis::{DataDependencyAnalysis, DataDependencyGraph},
+    },
+    visit::{VisitorMut, r#mut::filter},
 };
 
-struct DefVisitor<'scratch> {
-    locations: LocalVec<Location, &'scratch Scratch>,
+struct PlaceVisitor<'env, 'heap, 'scratch> {
+    interner: &'env Interner<'heap>,
+    graph: DataDependencyGraph<'heap, &'scratch Scratch>,
 }
 
-impl Visitor<'_> for DefVisitor<'_> {
-    type Result = Result<(), !>;
+impl<'heap> VisitorMut<'heap> for PlaceVisitor<'_, 'heap, '_> {
+    type Filter = filter::Deep;
+    type Residual = Result<Infallible, !>;
+    type Result<T>
+        = Result<T, !>
+    where
+        T: 'heap;
+
+    fn interner(&self) -> &Interner<'heap> {
+        self.interner
+    }
+
+    fn visit_place(
+        &mut self,
+        location: Location,
+        context: PlaceContext,
+        place: &mut Place<'heap>,
+    ) -> Self::Result<()> {
+        // Only rewrite in the case that this is a `use`
+        if context.into_def_use() != Some(DefUse::Use) {
+            // If we go into the place this is going to exit as well.
+            return Ok(());
+        }
+
+        *place = self.graph.resolve(self.interner, *place);
+        Ok(())
+    }
 
     fn visit_local(
         &mut self,
         location: Location,
         context: PlaceContext,
-        local: Local,
-    ) -> Self::Result {
-        if context.into_def_use() != Some(DefUse::Def) {
+        local: &mut Local,
+    ) -> Self::Result<()> {
+        // only called if not called directly from `place`
+        if context.into_def_use() != Some(DefUse::Use) {
             return Ok(());
         }
 
-        let old = mem::replace(&mut self.locations[local], location);
-        debug_assert_ne!(old, Location::PLACEHOLDER, "SSA property has been violated");
+        let place = Place {
+            local: *local,
+            projections: self.interner.projections.intern_slice(&[]),
+        };
+        let place = self.graph.resolve(self.interner, place);
+        *local = place.local;
+
+        // TODO: DataConstantGraph resolve
 
         Ok(())
     }
@@ -52,36 +89,16 @@ impl Sroa {
     }
 }
 
+// TODO: DataConstantGraph
 impl<'env, 'heap> TransformPass<'env, 'heap> for Sroa {
     fn run(&mut self, context: &mut MirContext<'env, 'heap>, body: &mut Body<'heap>) {
-        // The first run of the pass is to remove any intermediate locals
-        let mut visitor = DefVisitor {
-            locations: LocalVec::from_domain_in(
-                Location::PLACEHOLDER,
-                &body.local_decls,
-                &self.scratch,
-            ),
-        };
-        visitor.visit_body(body);
+        self.scratch.reset();
 
         let mut analysis = DataDependencyAnalysis::new_in(&self.scratch);
         analysis.run(context, body);
         let analysis = analysis.finish();
         let transient = analysis.transient(context.interner);
 
-        for (local, &location) in visitor.locations.iter_enumerated() {
-            if location == Location::PLACEHOLDER {
-                continue; // unused variable
-            }
-        }
-
-        // SROA via fixpoint iteration, unlike other passes, which use block-based fixpoint
-        // iteration, this pass uses body-based fixpoint iteration.
-        // This more coarse-grained approach is required, as analysis only happens over a body, in
-        // most cases the iteration count is at most 2.
-        for block in body.basic_blocks.reverse_postorder() {
-            // Perform SROA on the block
-            todo!()
-        }
+        // TODO
     }
 }
