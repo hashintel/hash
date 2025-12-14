ee6833f3fa7e5c985c4c6d05d15c8b452dde02ab
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-12-14 21:14:13 +0100

    feat: dead code elimination

--- libs/@local/hashql/mir/src/pass/transform/dse/mod.rs
+++ libs/@local/hashql/mir/src/pass/transform/dse/mod.rs
@@ -1,27 +1,28 @@
-use core::alloc::Allocator;
+use core::{alloc::Allocator, convert::Infallible};
 
 use hashql_core::{
     collections::WorkQueue,
     graph::{LinkedGraph, NodeId},
     heap::Scratch,
-    id::{
-        Id as _,
-        bit_vec::{DenseBitSet, SparseBitMatrix},
-    },
+    id::{Id as _, bit_vec::DenseBitSet},
+    intern::Interned,
 };
 
 use crate::{
     body::{
         Body,
-        local::{Local, LocalSlice, LocalVec},
+        basic_block::{BasicBlock, BasicBlockId, BasicBlockVec},
+        local::{Local, LocalVec},
         location::Location,
+        operand::Operand,
         place::{DefUse, PlaceContext},
-        statement::Assign,
-        terminator::Target,
+        statement::{Assign, Statement, StatementKind},
+        terminator::{GraphRead, Target},
     },
     context::MirContext,
-    pass::{AnalysisPass as _, TransformPass, analysis::DataDependencyAnalysis},
-    visit::{self, Visitor},
+    intern::Interner,
+    pass::TransformPass,
+    visit::{self, Visitor, VisitorMut, r#mut::filter},
 };
 
 pub struct DeadStatementElimination {
@@ -134,9 +135,120 @@ impl<'heap, A: Allocator> Visitor<'heap> for DependencyVisitor<'_, 'heap, A> {
                     );
                 }
 
-                self.uses[local] += 1;
+                self.uses[local] = self.uses[local].saturating_add(1);
             }
         }
+
+        Ok(())
+    }
+
+    fn visit_terminator_graph_read(
+        &mut self,
+        location: Location,
+        graph_read: &GraphRead<'heap>,
+    ) -> Self::Result {
+        Ok(()) = visit::r#ref::walk_terminator_graph_read(self, location, graph_read);
+
+        // Important: the actual block param of the graph read is static and *cannot* easily be
+        // eliminated. To be eliminated we would need to eliminate the graph effect, which wouldn't
+        // be safe, because that's an actual side effect.
+        let target_block = &self.body.basic_blocks[graph_read.target];
+        debug_assert_eq!(target_block.params.len(), 1);
+
+        let param = target_block.params[0];
+
+        // We do this in a simple fashion, we simply say that the target param has "infinite" uses
+        self.uses[param] = usize::MAX;
+        Ok(())
+    }
+}
+
+struct EliminationVisitor<'env, 'heap, A: Allocator> {
+    dead: DenseBitSet<Local>,
+    params: BasicBlockVec<Interned<'heap, [Local]>>,
+    interner: &'env Interner<'heap>,
+    scratch_locals: Vec<Local, A>,
+    scratch_operands: Vec<Operand<'heap>, A>,
+}
+
+impl<'heap, A: Allocator> VisitorMut<'heap> for EliminationVisitor<'_, 'heap, A> {
+    type Filter = filter::Deep;
+    type Residual = Result<Infallible, !>;
+    type Result<T>
+        = Result<T, !>
+    where
+        T: 'heap;
+
+    fn interner(&self) -> &Interner<'heap> {
+        self.interner
+    }
+
+    fn visit_basic_block(
+        &mut self,
+        id: BasicBlockId,
+        block: &mut BasicBlock<'heap>,
+    ) -> Self::Result<()> {
+        Ok(()) = visit::r#mut::walk_basic_block(self, id, block);
+
+        // Remove any no-ops
+        block
+            .statements
+            .retain(|statement| !matches!(statement.kind, StatementKind::Nop));
+
+        // Remove any params that are dead
+        if block.params.iter().any(|&param| self.dead.contains(param)) {
+            self.scratch_locals.clear();
+            self.scratch_locals.extend_from_slice(&block.params);
+            self.scratch_locals
+                .retain(|&param| !self.dead.contains(param));
+
+            block.params = self.interner.locals.intern_slice(&self.scratch_locals);
+        }
+
+        Ok(())
+    }
+
+    fn visit_statement(
+        &mut self,
+        _: Location,
+        statement: &mut Statement<'heap>,
+    ) -> Self::Result<()> {
+        let local = match &statement.kind {
+            StatementKind::Assign(assign) => assign.lhs.local,
+            &StatementKind::StorageLive(local) | &StatementKind::StorageDead(local) => local,
+            StatementKind::Nop => return Ok(()),
+        };
+
+        if self.dead.contains(local) {
+            statement.kind = StatementKind::Nop;
+        }
+
+        Ok(())
+    }
+
+    fn visit_target(&mut self, _: Location, target: &mut Target<'heap>) -> Self::Result<()> {
+        let target_params = self.params[target.block];
+        debug_assert_eq!(target_params.len(), target.args.len());
+
+        // Check if there are even any params which we need to remove, if not, then we can skip this
+        // target
+        if !target_params.iter().any(|&param| self.dead.contains(param)) {
+            return Ok(());
+        }
+
+        self.scratch_operands.clear();
+        self.scratch_operands.extend_from_slice(&target.args);
+
+        let mut index = 0;
+        self.scratch_operands.retain(|_| {
+            let should_retain = !self.dead.contains(target_params[index]);
+            index += 1;
+
+            should_retain
+        });
+        let operands = self.interner.operands.intern_slice(&self.scratch_operands);
+
+        target.args = operands;
 
         Ok(())
     }
