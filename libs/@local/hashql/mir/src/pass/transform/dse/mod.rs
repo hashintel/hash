fc814cda70200c97ff38f3ee81e11a5d7aaf940d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-12-14 20:48:35 +0100

    feat: target elimination

--- libs/@local/hashql/mir/src/pass/transform/dse/mod.rs
+++ libs/@local/hashql/mir/src/pass/transform/dse/mod.rs
@@ -2,6 +2,7 @@ use core::alloc::Allocator;
 
 use hashql_core::{
     collections::WorkQueue,
+    graph::{LinkedGraph, NodeId},
     heap::Scratch,
     id::{
         Id as _,
@@ -16,6 +17,7 @@ use crate::{
         location::Location,
         place::{DefUse, PlaceContext},
         statement::Assign,
+        terminator::Target,
     },
     context::MirContext,
     pass::{AnalysisPass as _, TransformPass, analysis::DataDependencyAnalysis},
@@ -28,7 +30,7 @@ pub struct DeadStatementElimination {
 
 impl<'env, 'heap> TransformPass<'env, 'heap> for DeadStatementElimination {
     fn run(&mut self, context: &mut MirContext<'env, 'heap>, body: &mut Body<'heap>) {
-        let mut dependencies = DependencyVisitor::new_in(&body.local_decls, &self.scratch);
+        let mut dependencies = DependencyVisitor::new_in(&body, &self.scratch);
         dependencies.visit_body(body);
 
         let mut dead = DenseBitSet::new_empty(body.local_decls.len());
@@ -43,7 +45,12 @@ impl<'env, 'heap> TransformPass<'env, 'heap> for DeadStatementElimination {
         }
 
         while let Some(local) = queue.dequeue() {
-            for dependent in dependencies.matrix.iter(local) {
+            for edge in dependencies
+                .graph
+                .outgoing_edges(NodeId::new(local.as_usize()))
+            {
+                let dependent = Local::new(edge.target().as_usize());
+
                 dependencies.uses[dependent] -= 1;
 
                 if dependencies.uses[dependent] == 0 {
@@ -57,37 +64,60 @@ impl<'env, 'heap> TransformPass<'env, 'heap> for DeadStatementElimination {
     }
 }
 
-struct DependencyVisitor<A: Allocator> {
-    matrix: SparseBitMatrix<Local, Local, A>,
+struct DependencyVisitor<'body, 'heap, A: Allocator> {
+    body: &'body Body<'heap>,
+    graph: LinkedGraph<(), (), A>,
     uses: LocalVec<usize, A>,
     current_def: Local,
 }
 
-impl<A: Allocator> DependencyVisitor<A> {
-    fn new_in(domain: &LocalSlice<impl Sized>, alloc: A) -> Self
+impl<'body, 'heap, A: Allocator> DependencyVisitor<'body, 'heap, A> {
+    fn new_in(body: &'body Body<'heap>, alloc: A) -> Self
     where
         A: Clone,
     {
+        let mut graph = LinkedGraph::new_in(alloc.clone());
+        graph.derive(&body.local_decls, |_, _| ());
+
         Self {
-            matrix: SparseBitMatrix::new_in(domain.len(), alloc.clone()),
-            uses: LocalVec::from_domain_in(0, domain, alloc),
+            body,
+            graph,
+            uses: LocalVec::from_domain_in(0, &body.local_decls, alloc),
             current_def: Local::MAX,
         }
     }
 }
 
-impl<A: Allocator> Visitor<'_> for DependencyVisitor<A> {
+impl<'heap, A: Allocator> Visitor<'heap> for DependencyVisitor<'_, 'heap, A> {
     type Result = Result<(), !>;
 
-    fn visit_statement_assign(&mut self, location: Location, assign: &Assign<'_>) -> Self::Result {
+    fn visit_statement_assign(
+        &mut self,
+        location: Location,
+        assign: &Assign<'heap>,
+    ) -> Self::Result {
         Ok(()) = visit::r#ref::walk_statement_assign(self, location, assign);
 
         self.current_def = Local::MAX;
         Ok(())
     }
 
+    fn visit_target(&mut self, location: Location, target: &Target<'heap>) -> Self::Result {
+        // We don't walk the target to avoid double counting.
+        let target_block = &self.body.basic_blocks[target.block];
+        let params = &target_block.params;
+        debug_assert_eq!(params.len(), target.args.len());
+
+        for (&param, arg) in params.iter().zip(target.args) {
+            self.current_def = param;
+            Ok(()) = self.visit_operand(location, arg);
+        }
+
+        self.current_def = Local::MAX;
+        Ok(())
+    }
+
     fn visit_local(&mut self, _: Location, context: PlaceContext, local: Local) -> Self::Result {
-        // TODO: increment by 1 for block param that receives effect
         let Some(def_use) = context.into_def_use() else {
             return Ok(());
         };
@@ -97,7 +127,11 @@ impl<A: Allocator> Visitor<'_> for DependencyVisitor<A> {
             DefUse::PartialDef => unimplemented!("MIR must be in SSA"),
             DefUse::Use => {
                 if self.current_def != Local::MAX {
-                    self.matrix.insert(self.current_def, local);
+                    self.graph.add_edge(
+                        NodeId::new(self.current_def.as_usize()),
+                        NodeId::new(local.as_usize()),
+                        (),
+                    );
                 }
 
                 self.uses[local] += 1;
