c6ce6de37d39efeb44a564ffa296a91d5e2b5250
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2025-12-25 17:39:09 +0100

    chore: fheckpoint

--- libs/@local/hashql/core/src/type/inference/variable.rs
+++ libs/@local/hashql/core/src/type/inference/variable.rs
@@ -20,8 +20,7 @@ pub struct Variable {
 }
 
 impl Variable {
-    #[cfg(test)]
-    pub(crate) const fn synthetic(kind: VariableKind) -> Self {
+    pub const fn synthetic(kind: VariableKind) -> Self {
         Self {
             span: SpanId::SYNTHETIC,
             kind,
