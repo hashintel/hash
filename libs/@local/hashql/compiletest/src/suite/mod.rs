f14a34edae86be3456f4ba56780f81dd66d1244c
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-01-03 21:11:17 +0100

    feat: tests

--- libs/@local/hashql/compiletest/src/suite/mod.rs
+++ libs/@local/hashql/compiletest/src/suite/mod.rs
@@ -27,6 +27,7 @@ mod mir_pass_transform_dse;
 mod mir_pass_transform_forward_substitution;
 mod mir_pass_transform_inline;
 mod mir_pass_transform_inst_simplify;
+mod mir_pass_transform_post_inline;
 mod mir_pass_transform_pre_inline;
 mod mir_reify;
 mod parse_syntax_dump;
@@ -64,6 +65,7 @@ use self::{
     mir_pass_transform_forward_substitution::MirPassTransformForwardSubstitution,
     mir_pass_transform_inline::MirPassTransformInline,
     mir_pass_transform_inst_simplify::MirPassTransformInstSimplify,
+    mir_pass_transform_post_inline::MirPassTransformPostInline,
     mir_pass_transform_pre_inline::MirPassTransformPreInline, mir_reify::MirReifySuite,
     parse_syntax_dump::ParseSyntaxDumpSuite,
 };
@@ -166,6 +168,7 @@ const SUITES: &[&dyn Suite] = &[
     &MirPassTransformForwardSubstitution,
     &MirPassTransformInline,
     &MirPassTransformInstSimplify,
+    &MirPassTransformPostInline,
     &MirPassTransformPreInline,
     &MirReifySuite,
     &ParseSyntaxDumpSuite,
