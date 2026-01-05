0a1f9c5d03aac86b241c4ddc34fca12e1aeb7aab
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-01-05 21:18:39 +0100

    feat: runtime v0

--- libs/@local/hashql/mir/src/interpret/mod.rs
+++ libs/@local/hashql/mir/src/interpret/mod.rs
@@ -1,15 +1,20 @@
-use std::{borrow::Cow, rc::Rc};
+use std::{assert_matches::debug_assert_matches, borrow::Cow, rc::Rc};
+
+use hashql_core::{collections::FastHashMap, id::Id, symbol::Symbol};
+use hashql_hir::node::operation::InputOp;
 
 use self::value::{Dict, List, Opaque, Struct, Tuple, Value};
 use crate::{
     body::{
         Body,
         basic_block::{BasicBlock, BasicBlockId},
+        constant::Int,
         local::{Local, LocalVec},
         operand::Operand,
-        place::{Place, Projection, ProjectionKind},
-        rvalue::{Aggregate, AggregateKind, RValue},
+        place::{FieldIndex, Place, Projection, ProjectionKind},
+        rvalue::{Aggregate, AggregateKind, Apply, Input, RValue},
         statement::{Assign, StatementKind},
+        terminator::{Goto, Return, SwitchInt, Target, TerminatorKind},
     },
     def::{DefId, DefIdSlice},
 };
@@ -123,24 +128,27 @@ fn load_aggregate<'locals, 'heap>(
         AggregateKind::Opaque(symbol) => {
             debug_assert_eq!(aggregate.operands.len(), 1);
 
-            let value = load_operand(locals, aggregate.operands[0]).into_owned();
+            let value =
+                load_operand(locals, aggregate.operands[FieldIndex::OPAQUE_VALUE]).into_owned();
             Value::Opaque(Opaque::new(symbol, value))
         }
         AggregateKind::Closure => {
             debug_assert_eq!(aggregate.operands.len(), 2);
-            // TODO: properly track them?!
+            // TODO: properly track them as a separate construct? probably?
             todo!()
         }
     }
 }
 
+// TODO: actually panic/diagnostic on issue (it's all ICE anyway), with proper unwinding.
+
 fn run<'heap>(
     bodies: &DefIdSlice<Body<'heap>>,
+    inputs: &FastHashMap<Symbol<'heap>, Value<'heap>>,
     main: DefId,
     locals: impl ExactSizeIterator<Item = Value<'heap>>,
-) {
+) -> Value<'heap> {
     let mut callstack = vec![];
-    let mut returns = vec![];
 
     let main = &bodies[main];
     debug_assert_eq!(main.args, locals.len());
@@ -158,6 +166,74 @@ fn run<'heap>(
     while let Some(state) = callstack.last_mut() {
         if state.statement_index >= state.block.statements.len() {
             // TODO: we have reached a terminator
+            let terminator = &state.block.terminator.kind;
+            match terminator {
+                &TerminatorKind::Goto(Goto {
+                    target: Target { block, args },
+                }) => {
+                    debug_assert_eq!(args.len(), state.body.basic_blocks[block].params.len());
+
+                    for (&param, &arg) in state.body.basic_blocks[block].params.iter().zip(args) {
+                        state
+                            .locals
+                            .insert(param, load_operand(&state.locals, arg).into_owned());
+                    }
+
+                    state.block = &state.body.basic_blocks[block];
+                    state.statement_index = 0;
+                }
+                TerminatorKind::SwitchInt(SwitchInt {
+                    discriminant,
+                    targets,
+                }) => {
+                    let discriminant = load_operand(&state.locals, *discriminant).into_owned();
+                    let Value::Integer(value) = discriminant else {
+                        // TODO: should be a diagnostic instead.
+                        panic!("diagnostic: SwitchInt discriminant must be an integer");
+                    };
+
+                    let Some(Target { block, args }) = targets.target(value.as_uint()) else {
+                        // TODO: should be a diagnostic instead.
+                        panic!("diagnostic: SwitchInt target not found");
+                    };
+
+                    debug_assert_eq!(args.len(), state.body.basic_blocks[block].params.len());
+                    for (&param, &arg) in state.body.basic_blocks[block].params.iter().zip(args) {
+                        state
+                            .locals
+                            .insert(param, load_operand(&state.locals, arg).into_owned());
+                    }
+
+                    state.block = &state.body.basic_blocks[block];
+                    state.statement_index = 0;
+                }
+                &TerminatorKind::Return(Return { value }) => {
+                    // Remove the current state from the callstack
+                    let state = callstack.pop().unwrap_or_else(|| unreachable!());
+                    let value = load_operand(&state.locals, value).into_owned();
+
+                    let Some(top) = callstack.last_mut() else {
+                        // We have reached the top of the callstack, which means that the return
+                        // value is the result of the function;
+                        return value;
+                    };
+
+                    let statement = &top.block.statements[top.statement_index];
+                    let StatementKind::Assign(Assign { lhs, rhs }) = &statement.kind else {
+                        unreachable!("we can only be called from an apply");
+                    };
+                    debug_assert_matches!(rhs, RValue::Apply(_));
+
+                    // let value = load_place_mut(&mut state.locals, lhs); // TODO: needs to be able
+                    // to do assignment on dict
+                }
+                TerminatorKind::GraphRead(graph_read) => {
+                    unimplemented!("this still needs to be implemented")
+                }
+                TerminatorKind::Unreachable => todo!("diagnostic for this"),
+            }
+
+            continue;
         }
 
         // Execute the next statement (aka the statement we're pointing to)
@@ -168,14 +244,54 @@ fn run<'heap>(
             // to put it.
             let rhs = match rhs {
                 RValue::Load(operand) => load_operand(&state.locals, *operand),
-                RValue::Binary(binary) => todo!(),
-                RValue::Unary(unary) => todo!(),
-                RValue::Aggregate(aggregate) => todo!(),
-                RValue::Input(input) => todo!(),
-                RValue::Apply(apply) => todo!(),
+                RValue::Binary(binary) => todo!(), // TODO: implement binary operations
+                RValue::Unary(unary) => todo!(),   // TODO: implement unary operations
+                RValue::Aggregate(aggregate) => todo!(), // TODO: implement aggregate operations
+                RValue::Input(Input { op, name }) => match op {
+                    InputOp::Exists => {
+                        Cow::Owned(Value::Integer(Int::from(inputs.contains_key(name))))
+                    }
+                    InputOp::Load { required: _ } => {
+                        Cow::Borrowed(&inputs[name]) // TODO: unwind if not exists
+                    }
+                },
+                RValue::Apply(Apply {
+                    function,
+                    arguments,
+                }) => {
+                    // Crucially on a function call, we do *not* advance the index, we do so on
+                    // re-entry.
+                    let function = load_operand(&state.locals, *function);
+                    let Value::Pointer(pointer) = function.as_ref() else {
+                        panic!("diagnostic: error");
+                    };
+
+                    let function_body = &bodies[pointer.def()];
+
+                    let mut function_locals =
+                        LocalVec::with_capacity(function_body.local_decls.len());
+                    arguments
+                        .iter()
+                        .map(|&argument| load_operand(&state.locals, argument))
+                        .map(Cow::into_owned)
+                        .map(Some)
+                        .collect_into(&mut function_locals);
+
+                    let state = State {
+                        locals: function_locals,
+                        body: function_body,
+                        block: &function_body.basic_blocks[BasicBlockId::START],
+                        statement_index: 0,
+                    };
+
+                    callstack.push(state);
+                    continue; // We skip the next statement, so we can assign on re-entry.
+                }
             };
         }
 
         state.statement_index += 1;
     }
+
+    todo!("how")
 }
