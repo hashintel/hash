f113e115efbb17b445183decaff4c53957428b2d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-01-06 18:26:30 +0100

    feat: allocator api support for value tree

--- libs/@local/hashql/mir/src/interpret/error.rs
+++ libs/@local/hashql/mir/src/interpret/error.rs
@@ -1,23 +1,28 @@
+use std::alloc::{Allocator, Global};
+
 use hashql_core::symbol::Symbol;
 
 use super::value::ValueTypeName;
 use crate::body::{local::Local, place::FieldIndex};
 
 #[derive(Debug, Copy, Clone)]
-pub enum RuntimeError<'value, 'index, 'heap> {
+pub enum RuntimeError<'value, 'index, 'heap, A: Allocator = Global> {
     // Local hasn't been initialized yet, by all intents and purposes this is an ICE, because
     // *any* step before should have handled this. Be it the MIR or the HIR.
     UninitializedLocal(Local),
     // Again: this is an ICE. typechk should have handled this.
-    InvalidIndexType(ValueTypeName<'value, 'heap>, ValueTypeName<'index, 'heap>),
+    InvalidIndexType(
+        ValueTypeName<'value, 'heap, A>,
+        ValueTypeName<'index, 'heap, A>,
+    ),
     // Again: this is an ICE. typechk should have handled this.
-    InvalidSubscriptType(ValueTypeName<'value, 'heap>),
+    InvalidSubscriptType(ValueTypeName<'value, 'heap, A>),
     // Again: this is an ICE. typechk should have handled this.
-    InvalidProjectionType(ValueTypeName<'value, 'heap>),
+    InvalidProjectionType(ValueTypeName<'value, 'heap, A>),
     // Again: this is an ICE. typechk should have handled this.
-    InvalidProjectionByNameType(ValueTypeName<'value, 'heap>),
+    InvalidProjectionByNameType(ValueTypeName<'value, 'heap, A>),
     // Again: this is an ICE. typechk should have handled this.
-    UnknownField(ValueTypeName<'value, 'heap>, FieldIndex),
+    UnknownField(ValueTypeName<'value, 'heap, A>, FieldIndex),
     // Again: this is an ICE. typechk should have handled this.
-    UnknownFieldByName(ValueTypeName<'value, 'heap>, Symbol<'heap>),
+    UnknownFieldByName(ValueTypeName<'value, 'heap, A>, Symbol<'heap>),
 }
