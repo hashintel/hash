69a15d2c9d6df1308d5be9d03beebcd6afe93553
Lu Nelson <ln@hash.ai>
2026-01-12 16:40:41 +0100

    get semantic colors in the export; fix codegen pkg script

--- libs/@hashintel/ds-theme/src/theme/colors/blue.ts
+++ libs/@hashintel/ds-theme/src/theme/colors/blue.ts
@@ -1,14 +1,3 @@
 import { defineSemanticTokens } from "@pandacss/dev";
 
-export const blue = defineSemanticTokens.colors({
-  "10": { value: { _light: "#daf0ff", _dark: "#0e2e8c" } },
-  "20": { value: { _light: "#bee6ff", _dark: "#1541b0" } },
-  "30": { value: { _light: "#78c0fc", _dark: "#1c62e3" } },
-  "40": { value: { _light: "#56b0fb", _dark: "#266df0" } },
-  "50": { value: { _light: "#34a0fa", _dark: "#629df0" } },
-  "60": { value: { _light: "#2a80c8", _dark: "#a3cff7" } },
-  "70": { value: { _light: "#0666c6", _dark: "#c5e3fa" } },
-  "80": { value: { _light: "#05529e", _dark: "#e5eeff" } },
-  "90": { value: { _light: "#043d77", _dark: "#f5fbff" } },
-  "00": { value: { _light: "#eff9ff", _dark: "#071e69" } },
-});
+export const blue = defineSemanticTokens.colors({ "10": { value: { _light: "#daf0ff", _dark: "#0e2e8c" } }, "20": { value: { _light: "#bee6ff", _dark: "#1541b0" } }, "30": { value: { _light: "#78c0fc", _dark: "#1c62e3" } }, "40": { value: { _light: "#56b0fb", _dark: "#266df0" } }, "50": { value: { _light: "#34a0fa", _dark: "#629df0" } }, "60": { value: { _light: "#2a80c8", _dark: "#a3cff7" } }, "70": { value: { _light: "#0666c6", _dark: "#c5e3fa" } }, "80": { value: { _light: "#05529e", _dark: "#e5eeff" } }, "90": { value: { _light: "#043d77", _dark: "#f5fbff" } }, "00": { value: { _light: "#eff9ff", _dark: "#071e69" } } });
