8dfe27c8d6c6132e92b14f4d1c1f1667d9138d95
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-01-16 17:14:57 +0100

    feat: static size estimation

--- libs/@local/hashql/mir/src/pass/analysis/mod.rs
+++ libs/@local/hashql/mir/src/pass/analysis/mod.rs
@@ -1,6 +1,7 @@
 mod callgraph;
 mod data_dependency;
 pub mod dataflow;
+mod size_estimation;
 pub use self::{
     callgraph::{CallGraph, CallGraphAnalysis, CallKind, CallSite},
     data_dependency::{DataDependencyAnalysis, DataDependencyGraph, TransientDataDependencyGraph},
