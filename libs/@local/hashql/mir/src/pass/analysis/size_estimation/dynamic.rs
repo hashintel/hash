37ef29ddeb92984b2e8c1478cf1e747b33da156a
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-01-18 18:24:47 +0100

    feat: dynamic analysis

--- libs/@local/hashql/mir/src/pass/analysis/size_estimation/dynamic.rs
+++ libs/@local/hashql/mir/src/pass/analysis/size_estimation/dynamic.rs
@@ -1,21 +1,187 @@
-use core::alloc::Allocator;
+use core::{alloc::Allocator, cell::RefCell};
 
-use hashql_core::heap::CloneIn as _;
+use hashql_core::{heap::CloneIn as _, id::bit_vec::DenseBitSet, r#type::environment::Environment};
+use hashql_hir::node::operation::{InputOp, UnOp};
 
-use super::footprint::{BodyFootprint, BodyFootprintSemilattice};
+use super::{
+    footprint::{BodyFootprint, BodyFootprintSemilattice, Footprint},
+    r#static::StaticSizeEstimationCache,
+};
 use crate::{
     body::{
-        Body, basic_block::BasicBlockId, local::Local, location::Location, operand::Operand,
-        statement::Statement, terminator::Terminator,
+        Body,
+        basic_block::BasicBlockId,
+        local::{Local, LocalDecl, LocalSlice},
+        location::Location,
+        operand::Operand,
+        place::Place,
+        rvalue::{Aggregate, Apply, BinOp, Binary, Input, RValue, Unary},
+        statement::{Assign, Statement, StatementKind},
     },
-    pass::analysis::dataflow::framework::{DataflowAnalysis, Direction},
+    def::{DefId, DefIdSlice},
+    pass::analysis::{
+        dataflow::{
+            framework::{DataflowAnalysis, Direction},
+            lattice::{AdditiveMonoid as _, SaturatingSemiring},
+        },
+        size_estimation::{estimate::Estimate, range::Cardinality, r#static::StaticSizeEstimation},
+    },
 };
 
-struct SizeEstimationDataflowAnalysis<A: Allocator> {
-    boundary: BodyFootprint<A>,
+enum Eval {
+    Footprint(Footprint),
+    Copy(Local),
 }
 
-impl<'heap, B: Allocator> DataflowAnalysis<'heap> for SizeEstimationDataflowAnalysis<B> {
+impl Eval {
+    fn apply<A: Allocator>(self, target: Local, domain: &mut BodyFootprint<A>) {
+        match self {
+            Self::Footprint(footprint) => {
+                domain.locals[target] = footprint;
+            }
+            Self::Copy(local) => {
+                let Ok([target, local]) = domain.locals.get_disjoint_mut([target, local]) else {
+                    // error means that they are overlapping, therefore we would copy into copy,
+                    // which just means, nothing is happening
+                    return;
+                };
+
+                target.clone_from(local);
+            }
+        }
+    }
+
+    fn as_ref<'domain, A: Allocator>(
+        &'domain self,
+        domain: &'domain BodyFootprint<A>,
+    ) -> &'domain Footprint {
+        match self {
+            Self::Footprint(footprint) => footprint,
+            &Self::Copy(local) => &domain.locals[local],
+        }
+    }
+}
+
+struct SizeEstimationDataflowAnalysis<'ctx, 'env, 'heap, A: Allocator> {
+    env: &'env Environment<'heap>,
+    decl: &'env LocalSlice<LocalDecl<'heap>>,
+
+    body: DefId,
+    footprints: &'ctx DefIdSlice<BodyFootprint<A>>,
+    dynamic: DenseBitSet<Local>,
+    cache: RefCell<&'ctx mut StaticSizeEstimationCache<A>>,
+}
+
+impl<'ctx, 'env, 'heap, A: Allocator> SizeEstimationDataflowAnalysis<'ctx, 'env, 'heap, A> {
+    fn eval_operand(&self, operand: &Operand<'heap>) -> Eval {
+        match operand {
+            Operand::Constant(_) => Eval::Footprint(Footprint::scalar()),
+            Operand::Place(place) => self.footprint(place),
+        }
+    }
+
+    fn eval_rvalue<B: Allocator>(&self, domain: &BodyFootprint<B>, rvalue: &RValue<'heap>) -> Eval {
+        #[expect(clippy::match_same_arms, reason = "intent")]
+        match rvalue {
+            RValue::Load(operand) => self.eval_operand(operand),
+            RValue::Binary(Binary {
+                op:
+                    BinOp::Add
+                    | BinOp::Sub
+                    | BinOp::BitAnd
+                    | BinOp::BitOr
+                    | BinOp::Eq
+                    | BinOp::Lt
+                    | BinOp::Lte
+                    | BinOp::Ne
+                    | BinOp::Gte
+                    | BinOp::Gt,
+                left: _,
+                right: _,
+            }) => {
+                Eval::Footprint(Footprint::scalar()) // All of these return scalars by definition
+            }
+            RValue::Unary(Unary {
+                op: UnOp::BitNot | UnOp::Neg | UnOp::Not,
+                operand: _,
+            }) => Eval::Footprint(Footprint::scalar()), // All of these return scalars by definition
+            RValue::Aggregate(Aggregate { kind: _, operands }) => {
+                let mut total: Footprint = SaturatingSemiring.zero();
+
+                for operand in operands {
+                    let eval = self.eval_operand(operand);
+
+                    SaturatingSemiring.plus(&mut total, eval.as_ref(domain));
+                }
+
+                Eval::Footprint(total)
+            }
+            RValue::Input(Input {
+                op: InputOp::Exists,
+                name: _,
+            }) => Eval::Footprint(Footprint::scalar()), // exists is just a boolean
+            RValue::Input(Input {
+                op: InputOp::Load { .. },
+                name: _,
+            }) => {
+                // the only way we can know the size of a load is if we know the type, which would
+                // mean that static analysis would've caught this. This means that dynamic analysis
+                // is unable to determine the size of the load accurately.
+                // In theory it could be specified, if we specify the inputs during planning, but
+                // that would defeat the purpose of compilation.
+                Eval::Footprint(Footprint::unknown())
+            }
+            RValue::Apply(Apply {
+                function,
+                arguments, // TODO: needs a remapping anyway
+            }) => {
+                // TODO: lookup the size of the function's output type
+                todo!()
+            }
+        }
+    }
+
+    fn footprint(&self, place: &Place<'heap>) -> Eval {
+        if place.projections.is_empty() {
+            return Eval::Copy(place.local);
+        }
+
+        let type_id = place.type_id(self.decl);
+        let static_size = {
+            let mut cache = self.cache.borrow_mut();
+            let mut analyzer = StaticSizeEstimation::new(self.env, *cache);
+            analyzer.run(type_id)
+        };
+
+        static_size.map_or_else(
+            // Over-estimate by using the size of the actual domain
+            || Eval::Copy(place.local),
+            |size| {
+                Eval::Footprint(Footprint {
+                    units: Estimate::Constant(size),
+                    cardinality: Estimate::Constant(Cardinality::one()),
+                })
+            },
+        )
+    }
+
+    fn requires_transfer(&self, place: &Place<'heap>) -> bool {
+        if place.projections.is_empty() {
+            self.dynamic.contains(place.local)
+        } else {
+            // We cannot recompute the size of a projection
+            false
+        }
+    }
+
+    fn requires_transfer_local(&self, local: Local) -> bool {
+        self.dynamic.contains(local)
+    }
+}
+
+impl<'heap, B: Allocator> DataflowAnalysis<'heap>
+    for SizeEstimationDataflowAnalysis<'_, '_, 'heap, B>
+{
     type Domain<A: Allocator> = BodyFootprint<A>;
     type Lattice<A: Allocator + Clone> = BodyFootprintSemilattice<A>;
     type SwitchIntData = !;
@@ -31,44 +197,65 @@ impl<'heap, B: Allocator> DataflowAnalysis<'heap> for SizeEstimationDataflowAnal
 
     fn initialize_boundary<A: Allocator>(
         &self,
-        _: &Body<'heap>,
+        body: &Body<'heap>,
         domain: &mut Self::Domain<A>,
         alloc: A,
     ) {
-        self.boundary.clone_into(domain, alloc);
+        self.footprints[body.id].clone_into(domain, alloc);
     }
 
     fn transfer_statement<A: Allocator>(
         &self,
-        location: Location,
+        _: Location,
         statement: &Statement<'heap>,
         state: &mut Self::Domain<A>,
     ) {
-        // check if the statement location requires adjustment, otherwise just... don't.
+        let StatementKind::Assign(Assign { lhs, rhs }) = &statement.kind else {
+            return;
+        };
+
+        if !self.requires_transfer(lhs) {
+            return;
+        }
+
+        // Time to transfer the statement
+        let footprint = self.eval_rvalue(state, rhs);
+        footprint.apply(lhs.local, state);
     }
 
     fn transfer_edge<A: Allocator>(
         &self,
-        source_block: BasicBlockId,
+        _: BasicBlockId,
         source_args: &[Operand<'heap>],
 
-        target_block: BasicBlockId,
+        _: BasicBlockId,
         target_params: &[Local],
 
         state: &mut Self::Domain<A>,
     ) {
-        // our domain must be the `BodyBandwidth` I think, with an efficient clone from?
-        // for places we estimate, the root is easy for places we take a look at the resulting type,
-        // if it's easy? pog, otherwise? We just take the place as the worst estimate, if anything
-        // we narrow in places, so this is a good estimation.
+        for (&param, arg) in target_params.iter().zip(source_args) {
+            if self.requires_transfer_local(param) {
+                let footprint = self.eval_operand(arg);
+                footprint.apply(param, state);
+            }
+        }
     }
 
-    fn transfer_terminator<A: Allocator>(
+    fn transfer_graph_read_edge<A: Allocator>(
         &self,
-        location: Location,
-        terminator: &Terminator<'heap>,
+        _: BasicBlockId,
+
+        _: BasicBlockId,
+        target_params: &[Local],
+
         state: &mut Self::Domain<A>,
     ) {
-        // This is only needed on return
+        let &[param] = target_params else {
+            unreachable!("the target param may only have a single param");
+        };
+
+        // TODO: heuristic estimate about the size of the graph read operation, for now just
+        // unbounded
+        state.locals[param] = Footprint::unknown();
     }
 }
