07e77d3766a4078d1f55c01b77bfdc08a096b98d
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-01-22 21:00:43 +0100

    chore: outline

--- libs/@local/hashql/mir/src/pass/analysis/execution/statement_placement/mod.rs
+++ libs/@local/hashql/mir/src/pass/analysis/execution/statement_placement/mod.rs
@@ -1,6 +1,6 @@
 use std::alloc::Allocator;
 
-use super::target::ExecutionTarget;
+use super::target::{ExecutionTarget, Interpreter, Postgres};
 use crate::{
     body::{Body, basic_block::BasicBlockId, location::Location, statement::Statement},
     context::MirContext,
@@ -37,4 +37,73 @@ pub trait StatementPlacementVisitor<'env, 'heap> {
         statement: &Statement<'heap>,
         state: &mut Self::State<'_, A>,
     ) -> Option<Cost>;
+}
+
+struct InterpreterVisitor;
+
+impl<'env, 'heap> StatementPlacementVisitor<'env, 'heap> for InterpreterVisitor {
+    type State<'ctx, A: Allocator> = ();
+    type Target = Interpreter;
+
+    fn target(&self) -> &Self::Target {
+        &Interpreter
+    }
+
+    fn initialize_state<'ctx, A: Allocator>(
+        &self,
+        _: &'ctx MirContext<'env, 'heap>,
+        _: &Body<'heap>,
+        _: A,
+    ) -> Self::State<'ctx, A> {
+    }
+
+    fn visit_statement<A: Allocator>(
+        &self,
+        _: Location,
+        _: &Statement<'heap>,
+        (): &mut Self::State<'_, A>,
+    ) -> Option<Cost> {
+        // By design, the interpreter must support any statement (but not terminator), to allow for
+        // execution in case no other exists.
+        // By default, the cost is a bit high, to encourage other strategies to be chosen.
+        Some(Cost::new(8))
+    }
+}
+
+struct PostgresVisitor;
+
+// The postgres visitor must make sure, that:
+// 1) the value is representable
+// 2) either through direct access of the vertex, or if data is either locally defined OR used
+//    through a function. There may be some things that cannot be run, such as embeddings.
+// 3) apply calls are *not* supported
+// 4) Therefore we *must* have a list of items that are supported, then represent them, check if
+//    they are part, if not, we must bail.
+// 5) At this point we *do not* need to do alias analysis, which is nice.
+// This must happen depending on the source, be it filter or whatever
+impl<'env, 'heap> StatementPlacementVisitor<'env, 'heap> for PostgresVisitor {
+    type State<'ctx, A: Allocator> = ();
+    type Target = Postgres;
+
+    fn target(&self) -> &Self::Target {
+        &Postgres
+    }
+
+    fn initialize_state<'ctx, A: Allocator>(
+        &self,
+        context: &'ctx MirContext<'env, 'heap>,
+        body: &Body<'heap>,
+        alloc: A,
+    ) -> Self::State<'ctx, A> {
+        todo!()
+    }
+
+    fn visit_statement<A: Allocator>(
+        &self,
+        location: Location,
+        statement: &Statement<'heap>,
+        state: &mut Self::State<'_, A>,
+    ) -> Option<Cost> {
+        todo!()
+    }
 }
