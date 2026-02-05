59b98811faddfc7d924d43332d7dcd70e4523ebc
Lu Nelson <ln@hash.ai>
2026-02-05 12:21:25 +0100

    tokens2 stories for the core radix colors

---
+++ libs/@hashintel/ds-helpers/stories/tokens2.stories.ts
@@ -0,0 +1,8 @@
+export default {
+  title: "Tokens (Radix)",
+};
+
+export { RadixSolidScales } from "./tokens2.story";
+export { RadixAlphaScales } from "./tokens2.story";
+export { StaticColors } from "./tokens2.story";
+export { GlobalAliases } from "./tokens2.story";
