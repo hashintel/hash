e17a95d5c958c655a4add685c2d5da9cc43194f5
Ciaran Morinan <c@hash.ai>
2026-02-11 14:32:47 +0000

    shorten rate limiter window

--- apps/hash-api/src/index.ts
+++ apps/hash-api/src/index.ts
@@ -101,8 +101,8 @@ const httpServer = http.createServer(app);
 const shutdown = new GracefulShutdown(logger, "SIGINT", "SIGTERM");
 
 const baseRateLimitOptions: Partial<RateLimitOptions> = {
-  windowMs: process.env.NODE_ENV === "test" ? 10 : 1000 * 30, // 30 seconds
-  limit: 10, // Limit each IP to 10 requests every 30 seconds
+  windowMs: process.env.NODE_ENV === "test" ? 10 : 1000 * 10, // 10 seconds
+  limit: 10, // Limit each IP to 10 requests every 10 seconds
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 };
