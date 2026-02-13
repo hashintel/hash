064a0b33b241f330457f50a9d569c618a9b602ab
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-02-13 16:06:04 +0100

    feat: move cost estimation out

--- libs/@local/hashql/mir/src/pass/execution/placement/condense/estimate.rs
+++ libs/@local/hashql/mir/src/pass/execution/placement/condense/estimate.rs
@@ -1,10 +1,16 @@
 use core::{alloc::Allocator, cmp};
 
-use hashql_core::graph::linked::Node;
+use hashql_core::{
+    graph::{
+        Predecessors, Successors,
+        linked::{Edge, Node},
+    },
+    id::{HasId, Id},
+};
 
-use super::{Condense, CondenseContext, PlacementRegion};
+use super::{BoundaryEdge, Condense, CondenseContext, PlacementRegion};
 use crate::{
-    body::basic_block::BasicBlockId,
+    body::{Body, basic_block::BasicBlockId},
     pass::execution::{
         ApproxCost, Cost,
         target::{TargetArray, TargetId},
@@ -12,9 +18,9 @@ use crate::{
 };
 
 #[derive(Debug, Copy, Clone)]
-struct HeapElement {
-    target: TargetId,
-    cost: ApproxCost,
+pub(crate) struct HeapElement {
+    pub target: TargetId,
+    pub cost: ApproxCost,
 }
 
 impl PartialEq for HeapElement {
@@ -37,7 +43,8 @@ impl Ord for HeapElement {
     }
 }
 
-struct TargetHeap {
+#[derive(Debug, Copy, Clone)]
+pub(crate) struct TargetHeap {
     targets: [HeapElement; TargetId::VARIANT_COUNT],
 
     index: u8,
@@ -54,11 +61,11 @@ impl TargetHeap {
         length: 0,
     };
 
-    const fn new() -> Self {
+    pub(crate) const fn new() -> Self {
         Self::EMPTY
     }
 
-    fn insert(&mut self, target: TargetId, cost: ApproxCost) {
+    pub(crate) fn insert(&mut self, target: TargetId, cost: ApproxCost) {
         assert!(self.length < TargetId::VARIANT_COUNT_U8);
 
         let index = self.length as usize;
@@ -68,12 +75,12 @@ impl TargetHeap {
         self.targets[(self.index as usize)..(self.length as usize)].sort_unstable();
     }
 
-    const fn reset(&mut self) {
+    pub(crate) const fn reset(&mut self) {
         self.length = 0;
         self.index = 0;
     }
 
-    const fn pop(&mut self) -> Option<HeapElement> {
+    pub(crate) const fn pop(&mut self) -> Option<HeapElement> {
         if self.index >= self.length {
             return None;
         }
@@ -84,19 +91,196 @@ impl TargetHeap {
         Some(element)
     }
 
-    const fn is_empty(&self) -> bool {
+    pub(crate) const fn is_empty(&self) -> bool {
         self.index >= self.length
     }
 
-    const fn len(&self) -> usize {
+    pub(crate) const fn len(&self) -> usize {
         self.length.saturating_sub(self.index) as usize
     }
 }
 
-pub(crate) struct CostEstimation<'ctx, 'parent, 'scc, A: Allocator, B: Allocator> {
-    condense: &'ctx Condense<'parent, A>,
-    context: &'ctx CondenseContext<'scc, B>,
+pub(crate) struct CostEstimationConfig {
+    boundary_multiplier: f32,
+}
 
-    region: &'ctx Node<PlacementRegion<'scc>>,
-    block: BasicBlockId,
+impl CostEstimationConfig {
+    pub const TRIVIAL: Self = Self {
+        boundary_multiplier: 1.0,
+    };
+}
+
+pub(crate) struct CostEstimation<'ctx, 'parent, 'scc, A: Allocator, B: Allocator> {
+    pub config: CostEstimationConfig,
+
+    pub condense: &'ctx Condense<'parent, A>,
+    pub context: &'ctx CondenseContext<'scc, B>,
+
+    pub region: &'ctx Node<PlacementRegion<'scc>>,
+}
+
+impl<A: Allocator, B: Allocator> CostEstimation<'_, '_, '_, A, B> {
+    fn transition_cost(
+        &self,
+        source: Option<TargetId>,
+        target: Option<TargetId>,
+        edge: &Edge<BoundaryEdge>,
+    ) -> Option<Cost> {
+        match (source, target) {
+            (Some(source), None) => {
+                // Find the target for which the transition is the minimum
+                let mut current_minimum = ApproxCost::INF;
+                let mut minimum_transition_cost = None;
+
+                for target in &self.condense.targets[edge.data.target] {
+                    let Some(cost) = edge.data.matrix.get(source, target) else {
+                        continue;
+                    };
+
+                    let mut block_cost =
+                        self.condense.statements[target].sum_approx(edge.data.target);
+                    block_cost += cost;
+
+                    if block_cost < current_minimum {
+                        current_minimum = block_cost;
+                        minimum_transition_cost = Some(cost);
+                    }
+                }
+
+                minimum_transition_cost
+            }
+            (None, Some(target)) => {
+                // Find the source for which the transition is the minimum
+                let mut current_minimum = ApproxCost::INF;
+                let mut minimum_transition_cost = None;
+
+                for source in &self.condense.targets[edge.data.source] {
+                    let Some(cost) = edge.data.matrix.get(source, target) else {
+                        continue;
+                    };
+
+                    let mut block_cost =
+                        self.condense.statements[source].sum_approx(edge.data.source);
+                    block_cost += cost;
+
+                    if block_cost < current_minimum {
+                        current_minimum = block_cost;
+                        minimum_transition_cost = Some(cost);
+                    }
+                }
+
+                minimum_transition_cost
+            }
+            (Some(source), Some(target)) => edge.data.matrix.get(source, target),
+            (None, None) => {
+                let mut current_minimum = ApproxCost::INF;
+                let mut minimum_transition_cost = None;
+
+                for (source, target, cost) in edge.data.matrix.iter() {
+                    let &Some(cost) = cost else {
+                        continue;
+                    };
+
+                    let mut block_cost =
+                        self.condense.statements[source].sum_approx(edge.data.source);
+                    block_cost += self.condense.statements[target].sum_approx(edge.data.target);
+                    block_cost += cost;
+
+                    if block_cost < current_minimum {
+                        current_minimum = block_cost;
+                        minimum_transition_cost = Some(cost);
+                    }
+                }
+
+                minimum_transition_cost
+            }
+        }
+    }
+
+    fn estimate_target(
+        &self,
+        body: &Body<'_>,
+        block: BasicBlockId,
+        target: TargetId,
+    ) -> Option<ApproxCost> {
+        // The total cost of the target is any transition *to it* and the actual execution of the
+        // target. For predecessors or successors which do not yet have a cost, we assume that they
+        // are going to choose the optimal (local) option, this may not be the actual chosen
+        // option, but is a good approximation.
+        // In case that any fixed predecessor or successor lacks a transition to the backend we
+        // abort.
+        // We do **not** include the cost of the actual statement we come from, only the transition.
+        // This means that we double count (as both the predecessor and successor are used), but
+        // this is required to be able to create a proper decision.
+        // Statements outside of the current placement region get a penalty via a multiplier. For
+        // scc operations this is 1, whereas inside of a node this may be lower, to account for the
+        // fact that the cost inside of a node is more important than the transition cost.
+        let mut cost = self.condense.statements[target].sum_approx(block);
+
+        for pred in body.basic_blocks.predecessors(block) {
+            let edges = self
+                .context
+                .graph
+                .incoming_edges(self.region.id())
+                .filter(|edge| edge.data.source == pred && edge.data.target == self.block);
+
+            let pred_target = self.context.targets[pred];
+
+            for edge in edges {
+                let Some(trans_cost) = self.transition_cost(pred_target, Some(target), edge) else {
+                    // Transition to this backend is not possible from this predecessor to the
+                    // chosen target
+                    return None;
+                };
+
+                let mut trans_cost = trans_cost.as_approx();
+                if edge.source() != edge.target() {
+                    // not in the same SCC
+                    trans_cost *= self.config.boundary_multiplier;
+                }
+
+                cost += trans_cost;
+            }
+        }
+
+        for succ in body.basic_blocks.successors(block) {
+            let edges = self
+                .context
+                .graph
+                .outgoing_edges(self.region.id())
+                .filter(|edge| edge.data.source == block && edge.data.target == succ);
+
+            let succ_target = self.context.targets[succ];
+
+            for edge in edges {
+                let Some(trans_cost) = self.transition_cost(Some(target), succ_target, edge) else {
+                    // Transition to this backend is not possible from this chosen target to the
+                    // successor
+                    return None;
+                };
+
+                let mut trans_cost = trans_cost.as_approx();
+                if edge.source() != edge.target() {
+                    // not in the same SCC
+                    trans_cost *= self.config.boundary_multiplier;
+                }
+
+                cost += trans_cost;
+            }
+        }
+
+        Some(cost)
+    }
+
+    pub(crate) fn run(&self, body: &Body<'_>, block: BasicBlockId) -> TargetHeap {
+        let mut heap = TargetHeap::new();
+
+        for target in &self.condense.targets[block] {
+            if let Some(cost) = self.estimate_target(body, block, target) {
+                heap.insert(target, cost);
+            }
+        }
+
+        heap
+    }
 }
