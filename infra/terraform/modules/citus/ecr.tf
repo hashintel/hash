b1d013def1ac27f4726710e41ff8375dbf788c8e
Tim Diekmann <21277928+TimDiekmann@users.noreply.github.com>
2026-02-13 17:17:25 +0100

    SRE-486: Remove Terraform AWS infrastructure modules (#8419)

--- infra/terraform/modules/citus/ecr.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "citus" {
-  name = "${local.prefix}-citusecr"
-  tags = {}
-}
--- infra/terraform/modules/container_registry/terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.67"
-    }
-  }
-}
-
