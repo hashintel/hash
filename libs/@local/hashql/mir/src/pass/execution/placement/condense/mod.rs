7e15c318105955ea28583ece9c986400183ece57
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2026-02-13 17:47:29 +0100

    feat: move to bump allocator

--- libs/@local/hashql/mir/src/pass/execution/placement/condense/mod.rs
+++ libs/@local/hashql/mir/src/pass/execution/placement/condense/mod.rs
@@ -1,17 +1,18 @@
 mod estimate;
+mod nested;
 
 use core::alloc::Allocator;
 
 use hashql_core::{
     graph::{
-        DirectedGraph, LinkedGraph, NodeId, Predecessors, Successors,
+        DirectedGraph, LinkedGraph, NodeId,
         algorithms::{
             Tarjan,
             tarjan::{Members, StronglyConnectedComponents},
         },
-        linked::Node,
     },
-    id::{self, HasId, Id, IdVec},
+    heap::BumpAllocator,
+    id::{self, Id},
 };
 
 use self::estimate::{CostEstimation, CostEstimationConfig, TargetHeap};
@@ -21,7 +22,7 @@ use crate::{
         basic_block::{BasicBlockId, BasicBlockSlice},
     },
     pass::execution::{
-        Cost, StatementCostVec,
+        StatementCostVec,
         target::{TargetArray, TargetBitSet, TargetId},
         terminator_placement::{TerminatorCostVec, TransMatrix},
     },
@@ -40,16 +41,16 @@ pub struct BoundaryEdge {
     matrix: TransMatrix,
 }
 
-struct CondenseContext<'scc, A: Allocator> {
-    scc: &'scc StronglyConnectedComponents<BasicBlockId, PlacementRegionId, (), A>,
-    scc_members: &'scc Members<BasicBlockId, PlacementRegionId, A>,
+struct CondenseContext<'scc, 'alloc, A: Allocator> {
+    scc: &'scc StronglyConnectedComponents<BasicBlockId, PlacementRegionId, (), &'alloc A>,
+    scc_members: &'scc Members<BasicBlockId, PlacementRegionId, &'alloc A>,
 
-    graph: LinkedGraph<PlacementRegion<'scc>, BoundaryEdge, A>,
+    graph: LinkedGraph<PlacementRegion<'scc>, BoundaryEdge, &'alloc A>,
 
-    options: Box<BasicBlockSlice<TargetHeap>, A>,
-    targets: Box<BasicBlockSlice<Option<TargetId>>, A>,
+    options: &'alloc mut BasicBlockSlice<TargetHeap>,
+    targets: &'alloc mut BasicBlockSlice<Option<TargetId>>,
 
-    alloc: A,
+    alloc: &'alloc A,
 }
 
 pub struct Condense<'ctx, A: Allocator> {
@@ -60,43 +61,29 @@ pub struct Condense<'ctx, A: Allocator> {
 }
 
 impl<'ctx, A: Allocator> Condense<'ctx, A> {
-    fn run_in<B>(&self, body: &Body<'_>, alloc: B)
+    fn run_in<'alloc, B>(&self, body: &Body<'_>, alloc: &'alloc B)
     where
-        B: Allocator + Clone,
+        B: BumpAllocator,
     {
-        let scc = Tarjan::new_in(&body.basic_blocks, alloc.clone()).run();
+        let scc = Tarjan::new_in(&body.basic_blocks, alloc).run();
         let scc_members = scc.members();
 
         // We use a backup slice, instead of directly operating on the target set, so that we're
         // able to switch and backup easily between iterations.
-        #[expect(unsafe_code)]
         let targets = {
-            let mut uninit = Box::new_uninit_slice_in(body.basic_blocks.len(), alloc.clone());
-            uninit.write_filled(None::<TargetId>);
-
-            // SAFETY: The slice is fully initialized
-            let boxed = unsafe { uninit.assume_init() };
-            BasicBlockSlice::from_boxed_slice(boxed)
+            let uninit = alloc.allocate_slice_uninit(body.basic_blocks.len());
+            BasicBlockSlice::from_raw_mut(uninit.write_filled(None::<TargetId>))
         };
 
-        #[expect(unsafe_code)]
         let options = {
-            let mut uninit = Box::new_uninit_slice_in(body.basic_blocks.len(), alloc.clone());
-            uninit.write_filled(TargetHeap::new());
-
-            // SAFETY: The slice is fully initialized
-            let boxed = unsafe { uninit.assume_init() };
-            BasicBlockSlice::from_boxed_slice(boxed)
+            let uninit = alloc.allocate_slice_uninit(body.basic_blocks.len());
+            BasicBlockSlice::from_raw_mut(uninit.write_filled(TargetHeap::new()))
         };
 
         let mut context = CondenseContext {
             scc: &scc,
             scc_members: &scc_members,
-            graph: LinkedGraph::with_capacity_in(
-                scc.node_count(),
-                self.terminators.len(),
-                alloc.clone(),
-            ),
+            graph: LinkedGraph::with_capacity_in(scc.node_count(), self.terminators.len(), alloc),
             options,
             targets,
             alloc,
@@ -107,17 +94,24 @@ impl<'ctx, A: Allocator> Condense<'ctx, A> {
         unimplemented!()
     }
 
-    fn run_forwards_loop<B: Allocator + Clone>(
+    fn run_forwards_loop<B: BumpAllocator>(
         &self,
         body: &Body<'_>,
-        context: &mut CondenseContext<'_, B>,
+        context: &mut CondenseContext<'_, '_, B>,
     ) {
+        // TODO: when re-computing the CSP we would shrimply take our queue that we have created
+        // (need bump alloc for that), and then just change the first, and do that until the first
+        // is completely exhausted (we need to re-compute anyway). A bit of a brute-force but I
+        // don't see a better way.
+        // NO WAIT, we just choose a mutation that has the lowest cost, and then re-compute the
+        // branch.
+
         // Now that we have all the edges we must do a forwards and backwards sweep, scc gives us
         // the reverse topological order, meaning that the forwards is simply the backwards
         // traversal of the scc.
 
         // TODO: we can probably re-use this buffer
-        let mut regions = Vec::with_capacity_in(context.scc.node_count(), context.alloc.clone());
+        let mut regions = Vec::with_capacity_in(context.scc.node_count(), context.alloc);
         context.scc.iter_nodes().collect_into(&mut regions);
 
         debug_assert!(!regions.is_empty()); // there is always at least one region because the start block exists
@@ -156,7 +150,7 @@ impl<'ctx, A: Allocator> Condense<'ctx, A> {
         }
     }
 
-    fn fill_graph<B: Allocator>(&self, body: &Body<'_>, context: &mut CondenseContext<'_, B>) {
+    fn fill_graph<B: Allocator>(&self, body: &Body<'_>, context: &mut CondenseContext<'_, '_, B>) {
         for scc in context.scc.iter_nodes() {
             let id = context.graph.add_node(PlacementRegion {
                 id: scc,
