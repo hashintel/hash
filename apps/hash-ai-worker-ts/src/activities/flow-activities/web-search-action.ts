0ad9d3b097c04946ccbe434b8351a67297e87c7b
Ciaran Morinan <c@hash.ai>
2026-02-19 15:00:26 +0000

    better error stringification

--- apps/hash-ai-worker-ts/src/activities/flow-activities/web-search-action.ts
+++ apps/hash-ai-worker-ts/src/activities/flow-activities/web-search-action.ts
@@ -2,6 +2,7 @@ import type { Url } from "@blockprotocol/type-system";
 import type { AiFlowActionActivity } from "@local/hash-backend-utils/flows";
 import { internalApiClient } from "@local/hash-backend-utils/internal-api-client";
 import { getSimplifiedAiFlowActionInputs } from "@local/hash-isomorphic-utils/flows/action-definitions";
+import { stringifyError } from "@local/hash-isomorphic-utils/stringify-error";
 import type { GetWebSearchResults200ResponseWebSearchResultsInner } from "@local/internal-api-client";
 import { StatusCode } from "@local/status";
 import { backOff } from "exponential-backoff";
@@ -38,9 +39,9 @@ export const webSearchAction: AiFlowActionActivity<"webSearch"> = async ({
         return await internalApiClient.getWebSearchResults(query);
       } catch (error) {
         logger.error(
-          `Error fetching web search results for query "${query}": ${
-            error instanceof Error ? error.message : JSON.stringify(error)
-          }`,
+          `Error fetching web search results for query "${query}": ${stringifyError(
+            error,
+          )}`,
         );
         throw error;
       }
