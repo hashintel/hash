8c61cd07c7eee61a91d1127d051b46d2f29ce3c4
Chris Feijoo <chris@kube.io>
2026-02-26 17:42:34 +0100

    Lint

--- libs/@hashintel/petrinaut/src/views/Editor/subviews/diagnostics.tsx
+++ libs/@hashintel/petrinaut/src/views/Editor/subviews/diagnostics.tsx
@@ -3,8 +3,8 @@ import { use, useCallback, useMemo, useState } from "react";
 import { FaChevronDown, FaChevronRight } from "react-icons/fa6";
 import type { Diagnostic } from "vscode-languageserver-types";
 
-import { LanguageClientContext } from "../../../lsp/context";
 import type { SubView } from "../../../components/sub-view/types";
+import { LanguageClientContext } from "../../../lsp/context";
 import { parseDocumentUri } from "../../../monaco/editor-paths";
 import { EditorContext } from "../../../state/editor-context";
 import { SDCPNContext } from "../../../state/sdcpn-context";
