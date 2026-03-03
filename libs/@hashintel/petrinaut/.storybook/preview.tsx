d47dfb5f3215c1671886f483e8c9136f411cd331
Chris Feijoo <chris@kube.io>
2026-03-03 16:40:49 +0100

    Fix PortalContainer in Stories

--- libs/@hashintel/petrinaut/.storybook/preview.tsx
+++ libs/@hashintel/petrinaut/.storybook/preview.tsx
@@ -1,16 +1,36 @@
 import "../src/index.css";
 
 import type { Preview } from "@storybook/react-vite";
+import { useRef } from "react";
+
+import { PortalContainerContext } from "../src/state/portal-container-context";
 
 const preview: Preview = {
   decorators: [
     (Story) => {
+      const portalContainerRef = useRef<HTMLDivElement>(null);
+
       return (
         <div
           // Required (for now) given design tokens are scoped to .petrinaut-root
           className="petrinaut-root"
         >
-          <Story />
+          <div
+            ref={portalContainerRef}
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "full",
+              height: "full",
+              zIndex: "99999",
+              pointerEvents: "none",
+            }}
+          />
+
+          <PortalContainerContext value={portalContainerRef}>
+            <Story />
+          </PortalContainerContext>
         </div>
       );
     },
