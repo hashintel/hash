6ae601bb00b3df4f1be3427217a2c57b5a029e0b
Tim Diekmann <post@timdiekmann.de>
2026-03-19 16:24:45 +0100

    Bump transitive cargo dependencies

--- libs/@local/hashql/compiletest/src/runner/ui/human.rs
+++ libs/@local/hashql/compiletest/src/runner/ui/human.rs
@@ -315,7 +315,7 @@ impl Human {
 
         let reports = run_trials(set, context, tx);
 
-        let mut writer = anstream::stdout();
+        let mut writer = std::io::stdout();
 
         for event in rx {
             let Event::TrialFinished(index, success, trial_stats) = event;
