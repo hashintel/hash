bb7f3a7d76a4cbd30c78ef607bb8e8cc94b82be7
alex leon <alex@leon.st>
2026-04-08 15:11:50 +0200

    Switch useDebounceCallback to use flush instead of cancel

--- libs/@hashintel/petrinaut/src/views/SDCPN/hooks/util/useDebounceCallback.tsx
+++ libs/@hashintel/petrinaut/src/views/SDCPN/hooks/util/useDebounceCallback.tsx
@@ -2,20 +2,19 @@ import { debounce, type DebouncedFunc } from "lodash-es";
 import { useEffect, useMemo } from "react";
 
 // debounces a function, holding the debounced function between re-renders
-// when unmounting, we cancel any unresolved debounced to avoid resolving
-// on possibly stale data
+// when unmounting, we flush any unresolved debounced calls when updating the debounced
+// function or unmounting to avoid resolving on stale data or refs
 export function useDebounceCallback<T extends (...args: any) => ReturnType<T>>(
   func: T,
   delay = 500,
 ): DebouncedFunc<T> {
   const debounced = useMemo(() => debounce(func, delay), [func, delay]);
 
-  // Update the debounced function ref whenever func, wait, or options change
   useEffect(() => {
     return () => {
-      debounced.cancel();
+      debounced.flush();
     };
-  }, []);
+  }, [func, delay]);
 
   return debounced;
 }
