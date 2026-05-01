af8397a62ce06e11e87f015ae53b69bb127a4cb6
Alex Leon <alex@leon.st>
2026-05-01 11:34:21 +0200

    Add petrinaut icons to DS (#8683)

--- libs/@hashintel/ds-components/src/components/Icon/icon.tsx
+++ libs/@hashintel/ds-components/src/components/Icon/icon.tsx
@@ -1,10 +1,9 @@
 /* eslint simple-import-sort/imports: "off", sort-imports: ["error", { ignoreDeclarationSort: true, ignoreMemberSort: true, ignoreCase: false }] */
 import { cx } from "@hashintel/ds-helpers/css";
-import AngleDown from "./svgs/solid/angle-down.svg";
-import AngleRight from "./svgs/regular/angle-right.svg";
 import ArrowDown from "./svgs/regular/arrow-down.svg";
 import ArrowDownLeftAndArrowUpRightToCenter from "./svgs/solid/arrow-down-left-and-arrow-up-right-to-center.svg";
 import ArrowLeft from "./svgs/regular/arrow-left.svg";
+import ArrowPointer from "./svgs/regular/arrow-pointer.svg";
 import ArrowRight from "./svgs/regular/arrow-right.svg";
 import ArrowRotateLeft from "./svgs/regular/arrow-rotate-left.svg";
 import ArrowUp from "./svgs/regular/arrow-up.svg";
@@ -16,7 +15,9 @@ import ArrowsRotate from "./svgs/regular/arrows-rotate.svg";
 import Asterisk from "./svgs/regular/asterisk.svg";
 import At from "./svgs/regular/at.svg";
 import Barcode from "./svgs/regular/barcode.svg";
+import Bars from "./svgs/regular/bars.svg";
 import Bell from "./svgs/light/bell.svg";
+import Bolt from "./svgs/regular/bolt.svg";
 import BracketsCurly from "./svgs/regular/brackets-curly.svg";
 import BracketsSquare from "./svgs/regular/brackets-square.svg";
 import Bug from "./svgs/solid/bug.svg";
@@ -24,7 +25,13 @@ import Bullseye from "./svgs/light/bullseye.svg";
 import Calendar from "./svgs/regular/calendar.svg";
 import CalendarClock from "./svgs/regular/calendar-clock.svg";
 import CaretDown from "./svgs/solid/caret-down.svg";
+import ChartColumn from "./svgs/regular/chart-column.svg";
+import ChartLine from "./svgs/regular/chart-line.svg";
 import Check from "./svgs/regular/check.svg";
+import ChevronDown from "./svgs/regular/chevron-down.svg";
+import ChevronLeft from "./svgs/regular/chevron-left.svg";
+import ChevronRight from "./svgs/regular/chevron-right.svg";
+import ChevronUp from "./svgs/regular/chevron-up.svg";
 import Circle from "./svgs/solid/circle.svg";
 import CircleCheck from "./svgs/regular/circle-check.svg";
 import CircleEllipsis from "./svgs/regular/circle-ellipsis.svg";
@@ -34,6 +41,7 @@ import CircleNodes from "./svgs/light/circle-nodes.svg";
 import CircleOne from "./svgs/regular/circle-1.svg";
 import CirclePlus from "./svgs/regular/circle-plus.svg";
 import Clock from "./svgs/regular/clock.svg";
+import ClockRotateLeft from "./svgs/regular/clock-rotate-left.svg";
 import Code from "./svgs/regular/code.svg";
 import Copy from "./svgs/regular/copy.svg";
 import Cube from "./svgs/regular/cube.svg";
@@ -42,16 +50,24 @@ import DiagramNested from "./svgs/light/diagram-nested.svg";
 import DiagramProject from "./svgs/regular/diagram-project.svg";
 import DiagramSubtask from "./svgs/regular/diagram-subtask.svg";
 import Download from "./svgs/regular/download.svg";
+import Ellipsis from "./svgs/regular/ellipsis.svg";
+import EllipsisVertical from "./svgs/regular/ellipsis-vertical.svg";
 import EmptySet from "./svgs/regular/empty-set.svg";
 import Eye from "./svgs/regular/eye.svg";
 import EyeSlash from "./svgs/regular/eye-slash.svg";
 import Feather from "./svgs/regular/feather.svg";
 import File from "./svgs/regular/file.svg";
 import FileSpreadsheet from "./svgs/regular/file-spreadsheet.svg";
+import Flask from "./svgs/regular/flask.svg";
 import ForwardStep from "./svgs/solid/forward-step.svg";
+import FunctionSymbol from "./svgs/regular/function.svg";
+import Gear from "./svgs/regular/gear.svg";
+import Hand from "./svgs/regular/hand.svg";
 import Image from "./svgs/regular/image.svg";
 import InfinityLoop from "./svgs/light/infinity.svg";
 import InputPipe from "./svgs/regular/input-pipe.svg";
+import Lambda from "./svgs/regular/lambda.svg";
+import LayerGroup from "./svgs/regular/layer-group.svg";
 import LightbulbOn from "./svgs/regular/lightbulb-on.svg";
 import List from "./svgs/regular/list.svg";
 import ListTree from "./svgs/regular/list-tree.svg";
@@ -62,25 +78,32 @@ import MemoCircleCheck from "./svgs/regular/memo-circle-check.svg";
 import Microscope from "./svgs/regular/microscope.svg";
 import Minus from "./svgs/solid/minus.svg";
 import OneHundred from "./svgs/regular/100.svg";
+import Pause from "./svgs/regular/pause.svg";
 import Pen from "./svgs/regular/pen.svg";
 import PenToSquare from "./svgs/solid/pen-to-square.svg";
 import PersonRunning from "./svgs/regular/person-running.svg";
 import Play from "./svgs/solid/play.svg";
 import Plug from "./svgs/regular/plug.svg";
 import Plus from "./svgs/regular/plus.svg";
+import RightToLine from "./svgs/regular/right-to-line.svg";
 import Rotate from "./svgs/regular/rotate.svg";
 import Ruler from "./svgs/regular/ruler.svg";
+import Scribble from "./svgs/regular/scribble.svg";
 import Shapes from "./svgs/regular/shapes.svg";
 import Sidebar from "./svgs/regular/sidebar.svg";
+import Sliders from "./svgs/regular/sliders.svg";
 import Sparkles from "./svgs/light/sparkles.svg";
+import Square from "./svgs/regular/square.svg";
 import SquareCheck from "./svgs/regular/square-check.svg";
+import SquarePlus from "./svgs/regular/square-plus.svg";
 import Star from "./svgs/regular/star.svg";
 import Stop from "./svgs/solid/stop.svg";
 import Table from "./svgs/light/table.svg";
 import Terminal from "./svgs/light/terminal.svg";
 import Text from "./svgs/regular/text.svg";
 import ThoughtBubble from "./svgs/light/thought-bubble.svg";
-import Trash from "./svgs/regular/trash.svg";
+import ThreeCircles from "./svgs/solid/circles-overlap-3.svg";
+import TrashCan from "./svgs/regular/trash-can.svg";
 import TriangleExclamation from "./svgs/solid/triangle-exclamation.svg";
 import UserPlus from "./svgs/regular/user-plus.svg";
 import WandMagicSparkles from "./svgs/regular/wand-magic-sparkles.svg";
@@ -92,8 +115,6 @@ export const sizes = ["xs", "sm", "md", "lg"] as const;
 export type Size = (typeof sizes)[number];
 
 const IconMap = {
-  angleDown: AngleDown,
-  angleRight: AngleRight,
   arrowDown: ArrowDown,
   arrowLeft: ArrowLeft,
   arrowRight: ArrowRight,
@@ -102,6 +123,7 @@ const IconMap = {
   asterisk: Asterisk,
   at: At,
   barcode: Barcode,
+  bars: Bars,
   bell: Bell,
   bracketsCurly: BracketsCurly,
   bracketsSquare: BracketsSquare,
@@ -110,7 +132,13 @@ const IconMap = {
   calendar: Calendar,
   calendarClock: CalendarClock,
   caretDown: CaretDown,
+  chartColumn: ChartColumn,
+  chartLine: ChartLine,
   check: Check,
+  chevronDown: ChevronDown,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
+  chevronUp: ChevronUp,
   circle: Circle,
   circleCheck: CircleCheck,
   circleEllipsis: CircleEllipsis,
@@ -118,18 +146,22 @@ const IconMap = {
   circleOne: CircleOne,
   circlePlus: CirclePlus,
   clock: Clock,
+  clockRotateLeft: ClockRotateLeft,
   close: Xmark,
   code: Code,
   collapse: ArrowDownLeftAndArrowUpRightToCenter,
   copy: Copy,
   cube: Cube,
   cubes: Cubes,
+  cursor: ArrowPointer,
   dash: Minus,
   diagramNested: DiagramNested,
   diagramProject: DiagramProject,
   diagramSubtask: DiagramSubtask,
   download: Download,
   edit: PenToSquare,
+  ellipsis: Ellipsis,
+  ellipsisVertical: EllipsisVertical,
   emptySet: EmptySet,
   error: CircleExclamation,
   expand: ArrowUpRightAndArrowDownLeftFromCenter,
@@ -139,39 +171,54 @@ const IconMap = {
   feather: Feather,
   file: File,
   fileSpreadsheet: FileSpreadsheet,
+  flask: Flask,
+  function: FunctionSymbol,
+  gear: Gear,
+  hand: Hand,
   image: Image,
   infinity: InfinityLoop,
   info: CircleInfo,
   inputPipe: InputPipe,
+  lambda: Lambda,
+  layerGroup: LayerGroup,
   lightbulbOn: LightbulbOn,
+  lightning: Bolt,
   list: List,
   listTree: ListTree,
   magic: WandMagicSparkles,
   memoCircleCheck: MemoCircleCheck,
   microscope: Microscope,
   oneHundred: OneHundred,
-  pen: Pen,
+  pause: Pause,
+  pencil: Pen,
   personRunning: PersonRunning,
   play: Play,
   plug: Plug,
   plus: Plus,
   refresh: ArrowsRotate,
+  rightToLine: RightToLine,
   rotate: Rotate,
+  rotateLeft: ArrowRotateLeft,
   ruler: Ruler,
+  scribble: Scribble,
   search: MagnifyingGlass,
   shapes: Shapes,
   sidebar: Sidebar,
   skipForward: ForwardStep,
+  sliders: Sliders,
   sortUp: ArrowUpWideShort,
   sparkles: Sparkles,
+  square: Square,
   squareCheck: SquareCheck,
+  squarePlus: SquarePlus,
   star: Star,
   stop: Stop,
   table: Table,
   terminal: Terminal,
   text: Text,
   thoughtBubble: ThoughtBubble,
-  trash: Trash,
+  threeCircles: ThreeCircles,
+  trash: TrashCan,
   undo: ArrowRotateLeft,
   userPlus: UserPlus,
   warning: TriangleExclamation,
