8ebaec186c245c3ac87c3b2ac4c5110a1816f7f1
Ciaran Morinan <c@hash.ai>
2026-05-21 13:44:43 +0100

    restore next.config.js

--- apps/hash-frontend/next.config.js
+++ apps/hash-frontend/next.config.js
@@ -143,6 +143,10 @@ export default withSentryConfig(
       },
       pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.jsx", "api.ts"],
 
+      // We call linters in GitHub Actions for all pull requests. By not linting
+      // again during `next build`, we save CI minutes and unlock more feedback.
+      // Thus, we can get Playwright test results and Preview releases for WIP PRs.
+      eslint: { ignoreDuringBuilds: true },
       typescript: { ignoreBuildErrors: true },
 
       experimental: {
