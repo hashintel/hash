0307103c43ae16e0618964fb8688d9e8b204ec6d
alex leon <alex@leon.st>
2026-06-03 16:52:16 +0200

    Fix cropped selects in petrinaut by removing portaling behaviour

--- libs/@hashintel/petrinaut/src/ui/views/Editor/panels/SimulateView/experiments/experiment-metric-timeline.tsx
+++ libs/@hashintel/petrinaut/src/ui/views/Editor/panels/SimulateView/experiments/experiment-metric-timeline.tsx
@@ -1456,7 +1456,6 @@ export const ExperimentMetricTimeline = ({
                 options={runAggregationOptions}
                 size="xs"
                 className={aggregationSelectStyle}
-                portal={false}
               />
             ) : (
               <Select
@@ -1467,7 +1466,6 @@ export const ExperimentMetricTimeline = ({
                 options={distributionViewOptions}
                 size="xs"
                 className={aggregationSelectStyle}
-                portal={false}
               />
             )}
           </div>
@@ -1489,7 +1487,6 @@ export const ExperimentMetricTimeline = ({
               options={timeAggregationOptions}
               size="xs"
               className={aggregationSelectStyle}
-              portal={false}
             />
           ) : (
             <Select
@@ -1498,7 +1495,6 @@ export const ExperimentMetricTimeline = ({
               options={timeTraceOptions}
               size="xs"
               className={aggregationSelectStyle}
-              portal={false}
             />
           )}
         </div>
