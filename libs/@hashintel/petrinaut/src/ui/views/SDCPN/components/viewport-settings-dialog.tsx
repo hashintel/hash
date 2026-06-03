bf702166bc710a3b0ad00e4a6d7e697fbeb5dfbc
Alex Leon <alex@leon.st>
2026-06-03 19:53:07 +0200

    FE-801: Fix cropped selects in petrinaut by removing portaling behaviour (#8817)

--- libs/@hashintel/petrinaut/src/ui/views/SDCPN/components/viewport-settings-dialog.tsx
+++ libs/@hashintel/petrinaut/src/ui/views/SDCPN/components/viewport-settings-dialog.tsx
@@ -161,7 +161,6 @@ export const ViewportSettingsDialog: React.FC<ViewportSettingsDialogProps> = ({
               { value: "bezier", label: "Bezier" },
               { value: "custom", label: "Adaptive Bezier" },
             ]}
-            portal={false}
           />
         </SettingRow>
 
