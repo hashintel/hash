a9d7b510e4ce817b0383a90d99287d1828583934
Tim Diekmann <post@timdiekmann.de>
2026-08-12 17:46:01 +0200

    Fix CI

--- libs/@local/graph/authentication/src/lib.rs
+++ libs/@local/graph/authentication/src/lib.rs
@@ -1,3 +1,11 @@
 #![cfg_attr(doc, doc = include_str!("../README.md"))]
 //! ## Workspace dependencies
 #![doc = simple_mermaid::mermaid!("../docs/dependency-diagram.mmd")]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
