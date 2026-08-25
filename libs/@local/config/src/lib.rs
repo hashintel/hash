dfc119db28fb06052ee24a40207b3752b11b2b3a
Tim Diekmann <post@timdiekmann.de>
2026-08-25 22:51:52 +0200

    BE-801: Add hash-config unit test baseline

--- libs/@local/config/src/lib.rs
+++ libs/@local/config/src/lib.rs
@@ -2,3 +2,11 @@
 //!
 //! # Workspace dependencies
 #![doc = simple_mermaid::mermaid!("../docs/dependency-diagram.mmd")]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn crate_uses_expected_package_name() {
+        assert_eq!(env!("CARGO_PKG_NAME"), "hash-config");
+    }
+}
