8f50d028fd7cabbe5d6aaa3a4f185ceb9af09c17
Bilal Mahmoud <7252775+indietyp@users.noreply.github.com>
2026-09-07 11:38:20 +0200

    feat: add atlas interning table

---
+++ libs/@local/graph/atlas/src/serve2/intern.rs
@@ -0,0 +1,64 @@
+use core::hash::{BuildHasher as _, Hash};
+
+use hashbrown::{DefaultHashBuilder, HashTable};
+use hashql_core::id::{Id, IdSlice, IdVec, newtype};
+use moka::Equivalent;
+
+newtype! {
+    #[id(const)]
+    pub(crate) struct TableIndex<T>(u32)
+}
+
+pub(crate) struct InternTable<T> {
+    hasher: DefaultHashBuilder,
+    table: IdVec<TableIndex<T>, T>,
+    reverse: HashTable<TableIndex<T>>,
+}
+
+impl<T> InternTable<T>
+where
+    T: 'static,
+{
+    pub(crate) fn new() -> Self {
+        Self {
+            hasher: DefaultHashBuilder::default(),
+            table: IdVec::new(),
+            reverse: HashTable::new(),
+        }
+    }
+
+    pub(crate) fn intern(&mut self, value: T) -> TableIndex<T>
+    where
+        T: Hash + PartialEq,
+    {
+        let hash = self.hasher.hash_one(&value);
+
+        if let Some(&value) = self.reverse.find(hash, |&index| self.table[index] == value) {
+            value
+        } else {
+            let index = TableIndex::from_usize(self.table.len());
+
+            self.table.push(value);
+            self.reverse.insert_unique(hash, index, |&index| {
+                self.hasher.hash_one(&self.table[index])
+            });
+
+            index
+        }
+    }
+
+    pub(super) fn index_of<K>(&self, value: &K) -> Option<TableIndex<T>>
+    where
+        K: Equivalent<T> + Hash + ?Sized,
+    {
+        let hash = self.hasher.hash_one(value);
+
+        self.reverse
+            .find(hash, |&index| value.equivalent(&self.table[index]))
+            .copied()
+    }
+
+    pub(super) const fn entries(&self) -> &IdSlice<TableIndex<T>, T> {
+        &self.table
+    }
+}
