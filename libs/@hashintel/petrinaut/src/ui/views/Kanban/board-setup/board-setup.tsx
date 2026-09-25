4d05571e17821e77ab5a2789052d84c7b18e2416
Aakarsh Singh <as15037@nyu.edu>
2026-09-26 02:48:47 +0700

    Keep board setup controls on one line each

--- libs/@hashintel/petrinaut/src/ui/views/Kanban/board-setup/board-setup.tsx
+++ libs/@hashintel/petrinaut/src/ui/views/Kanban/board-setup/board-setup.tsx
@@ -1752,8 +1752,10 @@ export const BoardSetup = ({
               <div
                 className={css({
                   display: "flex",
+                  flexWrap: "wrap",
                   gap: "2",
                   alignItems: "center",
+                  whiteSpace: "nowrap",
                 })}
               >
                 <button
